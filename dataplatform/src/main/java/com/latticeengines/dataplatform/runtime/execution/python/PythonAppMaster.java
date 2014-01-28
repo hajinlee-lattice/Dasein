d708170ffc9dfd6d86b3c192a99d202c3f3c8923
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2014-01-28 06:13:54 +0000

    Python script submission support

--- dataplatform/src/main/java/com/latticeengines/dataplatform/runtime/execution/python/PythonAppMaster.java
+++ dataplatform/src/main/java/com/latticeengines/dataplatform/runtime/execution/python/PythonAppMaster.java
@@ -1,24 +1,12 @@
 package com.latticeengines.dataplatform.runtime.execution.python;
 
-import java.io.IOException;
 import java.util.Map;
 import java.util.Properties;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.CommonConfigurationKeysPublic;
-import org.apache.hadoop.fs.FileStatus;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.yarn.api.records.Container;
 import org.apache.hadoop.yarn.api.records.ContainerLaunchContext;
-import org.apache.hadoop.yarn.api.records.LocalResource;
-import org.apache.hadoop.yarn.api.records.LocalResourceType;
-import org.apache.hadoop.yarn.api.records.LocalResourceVisibility;
-import org.apache.hadoop.yarn.conf.YarnConfiguration;
-import org.apache.hadoop.yarn.util.ConverterUtils;
-import org.apache.hadoop.yarn.util.Records;
 import org.springframework.yarn.am.ContainerLauncherInterceptor;
 import org.springframework.yarn.am.StaticEventingAppmaster;
 import org.springframework.yarn.am.container.AbstractLauncher;
@@ -45,45 +33,10 @@ public class PythonAppMaster extends StaticEventingAppmaster implements Containe
 		}
 		super.setParameters(parameters);
 	}
-
-	@Override
+	
 	public ContainerLaunchContext preLaunch(Container container,
 			ContainerLaunchContext context) {
-		if (context == null) {
-			log.info("Container launch context is null.");
-			return context;
-		}
-		Configuration conf = new YarnConfiguration();
-		String fs = conf.get(CommonConfigurationKeysPublic.FS_DEFAULT_NAME_KEY);
-		Properties props = getParameters();
-		Map<String, LocalResource> resources = context.getLocalResources();
-		
-		String trainingDataPath = fs + props.getProperty(PythonContainerProperty.TRAINING_HDFS_PATH.name());
-		String trainingFileName = props.getProperty(PythonContainerProperty.TRAINING_FILE_NAME.name());
-		String testDataPath = fs + props.getProperty(PythonContainerProperty.TEST_HDFS_PATH.name());
-		String testFileName = props.getProperty(PythonContainerProperty.TEST_FILE_NAME.name());
-		
-		try {
-			LocalResource trainingDataRes = createLocalResource(new Path(trainingDataPath), conf);
-			LocalResource testDataRes = createLocalResource(new Path(testDataPath), conf);
-			resources.put(trainingFileName, trainingDataRes);
-			resources.put(testFileName, testDataRes);
-		} catch (Exception e) {
-			throw new IllegalStateException(e);
-		}
-		
 		return context;
 	}
-	
-    private LocalResource createLocalResource(Path dataPath, Configuration conf) throws IOException {
-    	LocalResource localResource = Records.newRecord(LocalResource.class);
-        FileStatus fileStat = FileSystem.get(conf).getFileStatus(dataPath);
-        localResource.setResource(ConverterUtils.getYarnUrlFromPath(dataPath));
-        localResource.setSize(fileStat.getLen());
-        localResource.setTimestamp(fileStat.getModificationTime());
-        localResource.setType(LocalResourceType.FILE);
-        localResource.setVisibility(LocalResourceVisibility.PUBLIC);
-        return localResource;
-    }
 
 }
--- dataplatform/src/main/java/com/latticeengines/dataplatform/yarn/client/PythonClientCustomization.java
+++ dataplatform/src/main/java/com/latticeengines/dataplatform/yarn/client/PythonClientCustomization.java
@@ -4,12 +4,8 @@ import java.util.Collection;
 import java.util.Properties;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.CommonConfigurationKeysPublic;
-import org.apache.hadoop.yarn.api.records.LocalResourceType;
-import org.apache.hadoop.yarn.api.records.LocalResourceVisibility;
 import org.springframework.yarn.fs.LocalResourcesFactoryBean;
 import org.springframework.yarn.fs.LocalResourcesFactoryBean.CopyEntry;
-import org.springframework.yarn.fs.LocalResourcesFactoryBean.TransferEntry;
 
 import com.latticeengines.dataplatform.runtime.execution.python.PythonContainerProperty;
 
@@ -36,23 +32,6 @@ public class PythonClientCustomization extends DefaultYarnClientCustomization {
 		copyEntries.add(new LocalResourcesFactoryBean.CopyEntry("file:" + pythonScript,
 				"/app/dataplatform", false));
 		return copyEntries;
-	}
-
-	@Override
-	public Collection<TransferEntry> getHdfsEntries(Properties containerProperties) {
-		String defaultFs = configuration.get(CommonConfigurationKeysPublic.FS_DEFAULT_NAME_KEY);
-		Collection<LocalResourcesFactoryBean.TransferEntry> hdfsEntries = super.getHdfsEntries(containerProperties);
-		String trainingHdfsFilePath = containerProperties.getProperty(PythonContainerProperty.TRAINING_HDFS_PATH.name());
-		String testHdfsFilePath = containerProperties.getProperty(PythonContainerProperty.TEST_HDFS_PATH.name());
-		hdfsEntries.add(new LocalResourcesFactoryBean.TransferEntry(
-				LocalResourceType.FILE, //
-				LocalResourceVisibility.PUBLIC, trainingHdfsFilePath,
-				defaultFs, defaultFs, false));
-		hdfsEntries.add(new LocalResourcesFactoryBean.TransferEntry(
-				LocalResourceType.FILE, //
-				LocalResourceVisibility.PUBLIC, testHdfsFilePath,
-				defaultFs, defaultFs, false));
-		return hdfsEntries;
 	}
 
 }
