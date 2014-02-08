22323e355a0fad065672eed6979258550eb29b44
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2014-02-08 06:06:24 +0000

    Api functional framework for tests.

---
+++ api/src/test/java/com/latticeengines/api/controller/ModelResourceTestNG.java
@@ -0,0 +1,13 @@
+package com.latticeengines.api.controller;
+
+import org.testng.annotations.Test;
+
+import com.latticeengines.api.functionalframework.ApiFunctionalTestNGBase;
+
+public class ModelResourceTestNG extends ApiFunctionalTestNGBase {
+
+	@Test(groups = "functional")
+	public void submit() {
+		throw new RuntimeException("Test not implemented");
+	}
+}
---
+++ api/src/test/java/com/latticeengines/api/functionalframework/ApiFunctionalTestNGBase.java
@@ -0,0 +1,41 @@
+package com.latticeengines.api.functionalframework;
+
+import org.apache.hadoop.conf.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.web.client.RestTemplate;
+import org.testng.annotations.BeforeClass;
+
+import com.latticeengines.dataplatform.functionalframework.DataPlatformFunctionalTestNGBase;
+
+//import com.latticeengines.dataplatform.functionalframework.DataPlatformFunctionalTestNGBase;
+
+@TestExecutionListeners({
+    DirtiesContextTestExecutionListener.class
+})
+@ContextConfiguration(locations = {
+    "classpath:test-api-context.xml"
+})
+public class ApiFunctionalTestNGBase extends AbstractTestNGSpringContextTests {
+	
+	protected RestTemplate restTemplate = new RestTemplate();
+	
+	@Autowired
+	private Configuration yarnConfiguration;
+	
+	protected boolean doYarnClusterSetup() {
+		return true;
+	}
+	
+	@BeforeClass(groups="functional")
+	public void setupRunEnvironment() throws Exception {
+		if (!doYarnClusterSetup()) {
+			return;
+		}
+		DataPlatformFunctionalTestNGBase platformTestBase = new DataPlatformFunctionalTestNGBase(yarnConfiguration);
+		platformTestBase.setupRunEnvironment();
+	}
+}
