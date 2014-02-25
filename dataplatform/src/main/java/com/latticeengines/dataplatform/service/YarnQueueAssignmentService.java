03c00a571abf8468862d6d7f72582d7c34028270
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2014-02-25 01:52:56 +0000

    Reformat code to comply with dataplatform standards.

--- dataplatform/src/main/java/com/latticeengines/dataplatform/service/YarnQueueAssignmentService.java
+++ dataplatform/src/main/java/com/latticeengines/dataplatform/service/YarnQueueAssignmentService.java
@@ -1,46 +1,48 @@
 package com.latticeengines.dataplatform.service;
 
 import java.io.UnsupportedEncodingException;
+
 import org.apache.commons.lang.NotImplementedException;
 
-public interface YarnQueueAssignmentService  {
+public interface YarnQueueAssignmentService {
 
-	public enum AssignmentPolicy { 
-		DEFAULT, 
-		// by active + pending
-		SHORTESTQUEUE, 
-		// round robin
-		ROUNDROBIN, 
-		// random
-		RANDOM, 
-		// by pending
-		SHORTESTPENDINGQUEUE,
-		// by total active + pending resources (memory)
-		SHORTESTQUEUEBYRESOURCES,
-		// sticky to minimize data movement
-		DATALOCALITY;
+    public enum AssignmentPolicy {
+        DEFAULT,
+        // by active + pending
+        SHORTESTQUEUE,
+        // round robin
+        ROUNDROBIN,
+        // random
+        RANDOM,
+        // by pending
+        SHORTESTPENDINGQUEUE,
+        // by total active + pending resources (memory)
+        SHORTESTQUEUEBYRESOURCES,
+        // sticky to minimize data movement
+        DATALOCALITY;
 
-		public static AssignmentPolicy getEnum(String s) {
-			if (DEFAULT.name().equalsIgnoreCase(s)) {
-	                return DEFAULT;
-	            } else if (SHORTESTQUEUE.name().equalsIgnoreCase(s)) {
-	                return SHORTESTQUEUE;
-	            } else if (ROUNDROBIN.name().equalsIgnoreCase(s)) {
-	                return ROUNDROBIN;
-	            } else if (RANDOM.name().equalsIgnoreCase(s)) {
-	                return RANDOM;
-	            } else if (SHORTESTPENDINGQUEUE.name().equalsIgnoreCase(s)) {
-	                return SHORTESTPENDINGQUEUE;
-	            } else if (SHORTESTQUEUEBYRESOURCES.name().equalsIgnoreCase(s)) {
-	                return SHORTESTQUEUEBYRESOURCES;
-	            } else if (DATALOCALITY.name().equalsIgnoreCase(s)) {
-	                return DATALOCALITY;
-	            }
-	            throw new IllegalArgumentException(
+        public static AssignmentPolicy getEnum(String s) {
+            if (DEFAULT.name().equalsIgnoreCase(s)) {
+                return DEFAULT;
+            } else if (SHORTESTQUEUE.name().equalsIgnoreCase(s)) {
+                return SHORTESTQUEUE;
+            } else if (ROUNDROBIN.name().equalsIgnoreCase(s)) {
+                return ROUNDROBIN;
+            } else if (RANDOM.name().equalsIgnoreCase(s)) {
+                return RANDOM;
+            } else if (SHORTESTPENDINGQUEUE.name().equalsIgnoreCase(s)) {
+                return SHORTESTPENDINGQUEUE;
+            } else if (SHORTESTQUEUEBYRESOURCES.name().equalsIgnoreCase(s)) {
+                return SHORTESTQUEUEBYRESOURCES;
+            } else if (DATALOCALITY.name().equalsIgnoreCase(s)) {
+                return DATALOCALITY;
+            }
+            throw new IllegalArgumentException(
                     "No Enum specified for this string");
-	        }
-	}
-	
-	String assignToQueue(String assignmentToken, AssignmentPolicy policy) throws UnsupportedEncodingException, NotImplementedException; 
+        }
+    }
+
+    String assignToQueue(String assignmentToken, AssignmentPolicy policy)
+            throws UnsupportedEncodingException, NotImplementedException;
 
 }
