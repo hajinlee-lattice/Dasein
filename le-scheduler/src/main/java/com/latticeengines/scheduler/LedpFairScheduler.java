156ec9f8b37486d4db2da8b4dae40f91ad7dcdca
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2014-04-04 19:38:45 +0000

    Created separate scheduler project

---
+++ le-scheduler/src/main/java/com/latticeengines/scheduler/LedpFairScheduler.java
@@ -0,0 +1,104 @@
+package com.latticeengines.scheduler;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.apache.hadoop.yarn.api.records.Resource;
+import org.apache.hadoop.yarn.factory.providers.RecordFactoryProvider;
+import org.apache.hadoop.yarn.server.resourcemanager.rmcontainer.RMContainer;
+import org.apache.hadoop.yarn.server.resourcemanager.scheduler.fair.AppSchedulable;
+import org.apache.hadoop.yarn.server.resourcemanager.scheduler.fair.FSLeafQueue;
+import org.apache.hadoop.yarn.server.resourcemanager.scheduler.fair.FairScheduler;
+import org.apache.hadoop.yarn.util.resource.DefaultResourceCalculator;
+import org.apache.hadoop.yarn.util.resource.ResourceCalculator;
+import org.apache.hadoop.yarn.util.resource.Resources;
+
+public class LedpFairScheduler extends FairScheduler {
+    private static final ResourceCalculator resourceCalculator = new DefaultResourceCalculator();
+    private static final Resource clusterCapacity = RecordFactoryProvider.getRecordFactory(null).newRecordInstance(Resource.class);
+
+    @Override
+    protected Resource resToPreempt(FSLeafQueue sched, long curTime) {
+        if (isP0(sched)) {
+            return super.resToPreempt(sched, curTime);
+        }
+        return Resources.none();
+    }
+
+    private boolean canPreemptUsingNonP0Resources(Collection<FSLeafQueue> scheds, Resource toPreempt) {
+        List<RMContainer> runningContainers = new ArrayList<RMContainer>();
+        for (FSLeafQueue sched : scheds) {
+            if (isP0(sched)) {
+                continue;
+            }
+
+            for (AppSchedulable as : sched.getRunnableAppSchedulables()) {
+                for (RMContainer c : as.getApp().getLiveContainers()) {
+                    runningContainers.add(c);
+                }
+            }
+
+        }
+
+        // Sort containers into reverse order of priority
+        Collections.sort(runningContainers, new Comparator<RMContainer>() {
+            public int compare(RMContainer c1, RMContainer c2) {
+                int ret = c1.getContainer().getPriority().compareTo(c2.getContainer().getPriority());
+                if (ret == 0) {
+                    return c2.getContainerId().compareTo(c1.getContainerId());
+                }
+                return ret;
+            }
+        });
+
+        for (RMContainer container : runningContainers) {
+            Resources.subtractFrom(toPreempt, container.getContainer().getResource());
+        }
+
+        if (Resources.greaterThan(resourceCalculator, clusterCapacity, toPreempt, Resources.none())) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    protected void preemptResources(Collection<FSLeafQueue> scheds, Resource toPreempt) {
+        Resource resToPreempt = Resource.newInstance(toPreempt.getMemory(), toPreempt.getVirtualCores());
+        Collection<FSLeafQueue> mrScheds = new ArrayList<FSLeafQueue>();
+
+        for (FSLeafQueue sched : scheds) {
+            if (!isMapReduce(sched)) {
+                mrScheds.add(sched);
+            }
+        }
+        
+        
+        if (canPreemptUsingNonP0Resources(mrScheds, resToPreempt)) {
+            Collection<FSLeafQueue> noP0Scheds = new ArrayList<FSLeafQueue>();
+
+            for (FSLeafQueue sched : mrScheds) {
+                if (!isP0(sched)) {
+                    noP0Scheds.add(sched);
+                }
+            }
+            
+            super.preemptResources(noP0Scheds, toPreempt);
+        } else {
+            super.preemptResources(mrScheds, toPreempt);
+        }
+    }
+    
+    private boolean isP0(FSLeafQueue queue) {
+        return queue.getQueueName().contains("Priority0");
+    }
+    
+    private boolean isMapReduce(FSLeafQueue queue) {
+        return queue.getQueueName().contains("MapReduce");
+    }
+
+
+}
