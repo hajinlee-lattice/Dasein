6047990b1d8e8156a42878d2c8243bf7a535569d
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2014-04-08 02:33:13 +0000

    Fix functional tests.

--- le-dataplatform/src/test/resources/com/latticeengines/dataplatform/service/impl/nn_train.py
+++ le-dataplatform/src/test/resources/com/latticeengines/dataplatform/service/impl/nn_train.py
@@ -1,2 +1,5 @@
-def train(trainingData, testData, schema, modelFile):
+def train(trainingData, testData, schema, modelDir):
+    fo = open(modelDir + "model.txt", "w")
+    fo.write("this is the generated model.")
+    fo.close()
     print(trainingData)
