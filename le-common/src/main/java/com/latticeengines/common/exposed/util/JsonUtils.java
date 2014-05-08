63075b39699330297108080e03a8acb430001578
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2014-05-08 15:27:04 +0000

    Initial commit for le-common project.

---
+++ le-common/src/main/java/com/latticeengines/common/exposed/util/JsonUtils.java
@@ -0,0 +1,36 @@
+package com.latticeengines.common.exposed.util;
+
+import java.io.StringWriter;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+public class JsonUtils {
+
+    public static <T> String serialize(T object) {
+        if (object == null) {
+            return null;
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        StringWriter writer = new StringWriter();
+        try {
+            objectMapper.writeValue(writer, object);
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        return writer.toString();
+    }
+
+    public static <T> T deserialize(String jsonStr, Class<T> clazz) {
+        if (jsonStr == null) {
+            return null;
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        T deserializedSchema;
+        try {
+            deserializedSchema = objectMapper.readValue(jsonStr.getBytes(), clazz);
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        return deserializedSchema;
+    }
+}
