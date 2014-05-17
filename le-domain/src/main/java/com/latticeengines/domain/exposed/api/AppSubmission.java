11d7e3a0d534324a5dba5a74f72a0f91419a1f4a
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2014-05-17 18:58:44 +0000

    Add api domain objects.

---
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/api/AppSubmission.java
@@ -0,0 +1,33 @@
+package com.latticeengines.domain.exposed.api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.apache.hadoop.yarn.api.records.ApplicationId;
+
+@XmlRootElement(name = "appSubmission")
+public class AppSubmission {
+
+    private List<String> applicationIds = new ArrayList<String>();
+
+    public AppSubmission() {
+    }
+
+    public AppSubmission(List<ApplicationId> applicationIds) {
+        setApplicationIds(applicationIds);
+    }
+
+    @XmlElement(name = "applicationId")
+    public List<String> getApplicationIds() {
+        return applicationIds;
+    }
+
+    public void setApplicationIds(List<ApplicationId> appIds) {
+        for (ApplicationId appId : appIds) {
+            applicationIds.add(appId.toString());
+        }
+    }
+}
