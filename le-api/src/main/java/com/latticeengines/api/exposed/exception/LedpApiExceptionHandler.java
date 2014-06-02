15f6cd465a296d87d0adfcab1d11773adb5c979e
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2014-06-02 23:51:03 +0000

    add logging to capture the error and exception on the server side

--- le-api/src/main/java/com/latticeengines/api/exposed/exception/LedpApiExceptionHandler.java
+++ le-api/src/main/java/com/latticeengines/api/exposed/exception/LedpApiExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.latticeengines.api.exposed.exception;
 
 import org.apache.commons.lang.exception.ExceptionUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,13 +11,14 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.json.MappingJacksonJsonView;
 
 import com.google.common.collect.ImmutableMap;
+import com.latticeengines.api.controller.ModelResource;
 import com.latticeengines.dataplatform.exposed.exception.LedpCode;
 import com.latticeengines.dataplatform.exposed.exception.LedpException;
 
 @SuppressWarnings("deprecation")
 @ControllerAdvice
 public class LedpApiExceptionHandler {
-
+    private static final Log log = LogFactory.getLog(LedpApiExceptionHandler.class);
     public LedpApiExceptionHandler() {
     }
     
@@ -24,6 +27,7 @@ public class LedpApiExceptionHandler {
     public ModelAndView handleException(LedpException e) {
         MappingJacksonJsonView jsonView = new MappingJacksonJsonView();
         String stackTrace = e.getCause() != null ? ExceptionUtils.getFullStackTrace(e.getCause()) : "";
+        log.error(stackTrace);
         return new ModelAndView(jsonView, ImmutableMap.of("errorCode", e.getCode().name(), //
                 "errorMsg", e.getMessage() + "\n" + stackTrace));
 
@@ -34,6 +38,7 @@ public class LedpApiExceptionHandler {
     public ModelAndView handleException(Exception e) {
         MappingJacksonJsonView jsonView = new MappingJacksonJsonView();
         String stackTrace = ExceptionUtils.getFullStackTrace(e);
+        log.error(stackTrace);
         return new ModelAndView(jsonView, ImmutableMap.of("errorCode", LedpCode.LEDP_00002.name(), //
                 "errorMsg", stackTrace));
     }
