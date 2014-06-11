caa53e2b87595cf38bac1b9d6a0a204ded7a42ca
mho <mho@479f5059-62a3-4c39-9342-9852e3a68435>
2014-06-11 23:14:13 +0000

    This is the final cut of the data access layer;  removed properties file persistence and switched to Hibernate - include ddl - DAOs - entitymanager - unit test and funcational test fix - functional tests and apitests are passing

---
+++ le-common/src/main/java/com/latticeengines/common/exposed/util/StringTokenUtils.java
@@ -0,0 +1,44 @@
+package com.latticeengines.common.exposed.util;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class StringTokenUtils  {
+
+    /**
+     * convert a List to String with comma-separated values
+     * 
+     * @param stringInList
+     * @return
+     */
+    public static <T> String listToString(List<T> stringInList) {
+        StringBuilder sb = null;
+        if (stringInList != null && stringInList.size() > 0) {
+            sb = new StringBuilder(stringInList.size());
+            for (Iterator<T> iter = stringInList.iterator(); iter.hasNext();) {
+                T feature = iter.next();
+                sb.append(feature);
+                sb.append(',');
+            }
+        }
+        
+        String str = (sb == null) ? "" : sb.toString();
+        return str;
+    }
+
+    public static List<String> stringToList(String csv) {
+        List<String> strList = new ArrayList<String>();
+        if (csv != null) {            
+            StringTokenizer sTokenizer = new StringTokenizer(csv, ",");
+            while (sTokenizer.hasMoreTokens()) {
+                String id = sTokenizer.nextToken();
+                strList.add(id);
+            }
+        }
+        
+        return strList;
+    }
+
+}
