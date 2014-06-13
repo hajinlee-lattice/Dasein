84b0ef75b633e7e8afb90810ae5a8b4d5fb02f42
bnguyen <bnguyen@479f5059-62a3-4c39-9342-9852e3a68435>
2014-06-13 03:09:14 +0000

    Http client that can post gzipped content with exponential retry.  Not currently used but may be useful later.

---
+++ le-common/src/main/java/com/latticeengines/common/exposed/util/HttpUtils.java
@@ -0,0 +1,60 @@
+package com.latticeengines.common.exposed.util;
+
+import java.io.IOException;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+import com.google.api.client.http.EmptyContent;
+import com.google.api.client.http.GZipEncoding;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpBackOffUnsuccessfulResponseHandler;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.util.ExponentialBackOff;
+
+public class HttpUtils {
+    static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+    static HttpRequestFactory requestFactory;
+
+    static {
+        requestFactory = HTTP_TRANSPORT.createRequestFactory(new HttpRequestInitializer() {
+
+            @Override
+            public void initialize(HttpRequest request) throws IOException {
+                request.setEncoding(new GZipEncoding());
+                request.setConnectTimeout(60000);
+                request.setNumberOfRetries(10);
+
+                ExponentialBackOff backoff = new ExponentialBackOff.Builder().setInitialIntervalMillis(1000)
+                        .setMultiplier(2).setMaxElapsedTimeMillis(180000).build();
+                request.setUnsuccessfulResponseHandler(new HttpBackOffUnsuccessfulResponseHandler(backoff));
+            }
+        });
+
+    }
+
+    /*
+     * Request will retry with exponential backoff on any 5xx server error
+     * response.
+     * 
+     * @return Returns the HTTP status code or 0 for none.
+     */
+    public static int executePostRequest(String url) throws IOException {
+        HttpRequest request = requestFactory.buildPostRequest(new GenericUrl(url), new EmptyContent());
+        HttpResponse response = request.execute();            
+        return response.getStatusCode();
+    }
+    
+    public static int executeGetRequest(String url) throws IOException {
+        HttpRequest request = requestFactory.buildGetRequest(new GenericUrl(url));        
+        HttpResponse response = request.execute();
+        return response.getStatusCode();
+    }
+
+}
