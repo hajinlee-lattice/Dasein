28d0112faddaa0ac1d4279cfbe734793c51b6fd0
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2014-06-17 06:03:17 +0000

    Set name for model definition of data profile.

--- le-dataplatform/src/main/java/com/latticeengines/dataplatform/exposed/service/impl/ModelingServiceImpl.java
+++ le-dataplatform/src/main/java/com/latticeengines/dataplatform/exposed/service/impl/ModelingServiceImpl.java
@@ -339,7 +339,7 @@ public class ModelingServiceImpl implements ModelingService {
         }
         m.setModelHdfsDir(m.getMetadataHdfsPath());
         ModelDefinition modelDefinition = new ModelDefinition();
-        modelDefinition.setName("data-profile-" + System.currentTimeMillis());
+        modelDefinition.setName("DataProfile-" + System.currentTimeMillis());
         Algorithm dataProfileAlgorithm = new DataProfilingAlgorithm();
         dataProfileAlgorithm.setSampleName("all");
         dataProfileAlgorithm.setContainerProperties("VIRTUALCORES=1 MEMORY=64 PRIORITY=1");
