ff292fb908e69460b79e6ca28d733c8948c70f91
bnguyen <bnguyen@479f5059-62a3-4c39-9342-9852e3a68435>
2014-06-18 00:02:12 +0000

    1. Integrated DLOrchestration with Db via Hibernate.
    2. Added Profile Data step.

--- le-dataplatform/src/test/java/com/latticeengines/dataplatform/service/impl/dlorchestration/ModelCommandCallableTestNG.java
+++ le-dataplatform/src/test/java/com/latticeengines/dataplatform/service/impl/dlorchestration/ModelCommandCallableTestNG.java
@@ -2,54 +2,83 @@ package com.latticeengines.dataplatform.service.impl.dlorchestration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.latticeengines.dataplatform.entitymanager.ModelCommandEntityMgr;
+import com.latticeengines.dataplatform.entitymanager.ModelCommandLogEntityMgr;
 import com.latticeengines.dataplatform.entitymanager.ModelCommandStateEntityMgr;
 import com.latticeengines.dataplatform.functionalframework.DataPlatformFunctionalTestNGBase;
 import com.latticeengines.dataplatform.service.JobService;
 import com.latticeengines.dataplatform.service.ModelCommandLogService;
 import com.latticeengines.dataplatform.service.ModelStepProcessor;
-import com.latticeengines.domain.exposed.dataplatform.dlorchestration.ModelCommand;
+
 
 public class ModelCommandCallableTestNG extends DataPlatformFunctionalTestNGBase {
-    
+
     @Autowired
     private JobService jobService;
-    
+
     @Autowired
     private ModelCommandEntityMgr modelCommandEntityMgr;
-   
+
     @Autowired
     private ModelCommandStateEntityMgr modelCommandStateEntityMgr;
-    
+
     @Autowired
     private ModelStepProcessor modelStepProcessor;
-    
+
     @Autowired
     private ModelCommandLogService modelCommandLogService;
-        
-    private ModelCommandCallable modelCommandCallable;
- 
-    protected boolean doYarnClusterSetup() {
+    
+    @Autowired
+    private ModelCommandLogEntityMgr modelCommandLogEntityMgr;
+
+    protected boolean doDependencyLibraryCopy() {
         return false;
     }
-    
+
+    @BeforeMethod(groups = "functional")
+    public void beforeMethod() {
+    }
+
     @BeforeClass(groups = "functional")
     public void setup() throws Exception {
-        ModelCommand modelCommand = new ModelCommand();
-        modelCommandCallable = new ModelCommandCallable(modelCommand, jobService, modelCommandEntityMgr, modelCommandStateEntityMgr, modelStepProcessor, modelCommandLogService);
     }
-    
+   
     @Test(groups = "functional")
     public void testWorkflow() throws Exception {
-     // TODO uncomment this when persistence is comlete - modelCommandCallable.call();
-        // call this in a loop (to simulate) multiple Scheduler invocations until modelCommand reaches SUCCESS state.
-        
-        // validate log table output
-        
-        // validate full set of step-appIds in State table
+        // Commented out tests until we get test rollback
+/*        
+//        ModelCommand command = ModelingServiceTestUtils.createModelCommandWithCommandParameters();
+//        modelCommandEntityMgr.create(command);
+        List<ModelCommand> commands = modelCommandEntityMgr.getNewAndInProgress();
+        ModelCommand command = commands.get(0);
+        ModelCommandCallable modelCommandCallable = new ModelCommandCallable(command, jobService,
+                modelCommandEntityMgr, modelCommandStateEntityMgr, modelStepProcessor, modelCommandLogService);
+
+        int iterations = 0;
+        while ((command.getCommandStatus() == ModelCommandStatus.NEW || command.getCommandStatus() == ModelCommandStatus.IN_PROGRESS)
+                && iterations < 400) {            
+            modelCommandCallable.call();
+            iterations++;
+            Thread.sleep(3000);
+        }
+
+        assertTrue(command.getCommandStatus() == ModelCommandStatus.SUCCESS);
+               
+        List<ModelCommandLog> logs = modelCommandLogEntityMgr.findAll();
+        assertEquals(logs.size(), 10);
+        List<ModelCommandState> states = modelCommandStateEntityMgr.findAll();
+        assertEquals(states.size(), 5); 
+       */     
     }
-    
-    // TODO Test Failure cases
+/*    
+    @Test(groups = "functional")
+    public void testModelCommandEntityMgr() {
+        ModelCommand command = createModelCommand();
+        List<ModelCommand> commands = modelCommandEntityMgr.getNewAndInProgress();
+        assertEquals(commands.size(), 1);
+    }
+*/
 }
