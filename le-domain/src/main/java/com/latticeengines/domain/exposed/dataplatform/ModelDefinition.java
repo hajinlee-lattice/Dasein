bec75a5b98f48404e53b25ef76edae4fe3160f02
mho <mho@479f5059-62a3-4c39-9342-9852e3a68435>
2014-06-20 22:14:16 +0000

    fixing build

--- le-domain/src/main/java/com/latticeengines/domain/exposed/dataplatform/ModelDefinition.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/dataplatform/ModelDefinition.java
@@ -58,24 +58,23 @@ public class ModelDefinition implements HasName, HasPid {
     }
 
     @JsonProperty("algorithms")
-    @OneToMany(mappedBy = "modelDefinition", targetEntity = AlgorithmBase.class, fetch = FetchType.EAGER, cascade = {CascadeType.ALL})
+    @OneToMany(mappedBy = "modelDefinition", targetEntity = AlgorithmBase.class, fetch = FetchType.EAGER, cascade = { CascadeType.ALL })
     public List<Algorithm> getAlgorithms() {
         return algorithms;
     }
 
-    @JsonProperty("algorithms")
     public void setAlgorithms(List<Algorithm> algos) {
-        this.algorithms = algos;        
+        this.algorithms = algos;
     }
-    
+
     /**
-     * add algorithm(s) to the model definition
-     * - not intended to be used by ORM
-     *  
+     * add algorithm(s) to the model definition - not intended to be used by ORM
+     * 
      * @param algos
-     */    
+     */
+    @JsonProperty("algorithms")
     public void addAlgorithms(List<Algorithm> algos) {
-        this.algorithms = (this.algorithms == null) ? new ArrayList<Algorithm>() : this.algorithms; 
+        this.algorithms = (this.algorithms == null) ? new ArrayList<Algorithm>() : this.algorithms;
         if (algos != null && !algos.isEmpty()) {
             this.algorithms.addAll(algos);
             for (Algorithm a : algos) {
@@ -83,7 +82,16 @@ public class ModelDefinition implements HasName, HasPid {
             }
         }
     }
-    
+
+    /**
+     * add algorithm to the model definition 
+     * 
+     * @param algos
+     */
+    public void addAlgorithm(Algorithm algo) {
+        algo.setModelDefinition(this);
+        this.algorithms.add(algo);
+    }
 
     @JsonIgnore
     @OneToMany(mappedBy = "modelDefinition", fetch = FetchType.LAZY)
