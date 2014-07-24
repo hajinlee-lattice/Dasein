b3475d495665bcfb6535456ab9b3b9f49adffea5
bnguyen <bnguyen@479f5059-62a3-4c39-9342-9852e3a68435>
2014-07-24 18:59:15 +0000

    Match fair-scheduler settings to 72 max apps in prod and qe.

--- le-scheduler/src/main/java/com/latticeengines/scheduler/fairscheduler/generator/AllocationsGenerator.java
+++ le-scheduler/src/main/java/com/latticeengines/scheduler/fairscheduler/generator/AllocationsGenerator.java
@@ -8,67 +8,69 @@ import javax.xml.bind.Marshaller;
 
 public class AllocationsGenerator {
 
-    private static final String MAX_RUNNING_APPS = "75";
-    private static final int NUM_MR_LEAF_QUEUES = 25;
-    private static final int NUM_NON_MR_LEAF_QUEUES = 75;
+    private static final String MAX_RUNNING_APPS = "72";
+    private static final int NUM_MR_LEAF_QUEUES = 24;
+    private static final int NUM_NON_MR_LEAF_QUEUES = 72;
     private static final String FIFO = "fifo";
+    private static final String MR_MIN_RESOURCES = "21504";
+    private static final String NON_MR_MIN_RESOURCES = "7168";
 
     public static void main(String[] args) throws Exception {
 
         ObjectFactory factory = new ObjectFactory();
 
-        Allocations allocations = factory.createAllocations();      
-        
+        Allocations allocations = factory.createAllocations();
+
         Queue p0MRQ = factory.createQueue();
         p0MRQ.setName("Priority0.MapReduce");
         p0MRQ.setWeight(new BigInteger("1000"));
         for (int i = 0; i < NUM_MR_LEAF_QUEUES; i++) {
             Queue leaf = factory.createQueue();
             leaf.setName(Integer.toString(i));
-            leaf.setMinResources("24576 mb, 6 vcores");
+            leaf.setMinResources(MR_MIN_RESOURCES + " mb, 6 vcores");
             leaf.setSchedulingPolicy(FIFO);
             p0MRQ.getQueue().add(leaf);
         }
         allocations.getQueue().add(p0MRQ);
-        
+
         Queue p0 = factory.createQueue();
         p0.setName("Priority0");
         p0.setWeight(new BigInteger("100"));
         addLeaves(factory, p0, true, NUM_NON_MR_LEAF_QUEUES);
         allocations.getQueue().add(p0);
-        
+
         Queue p1 = factory.createQueue();
         p1.setName("Priority1");
         p1.setWeight(new BigInteger("10"));
         addLeaves(factory, p1, false, NUM_NON_MR_LEAF_QUEUES);
-        allocations.getQueue().add(p1);        
-        
+        allocations.getQueue().add(p1);
+
         Queue p2 = factory.createQueue();
         p2.setName("Priority2");
-        p2.setWeight(new BigInteger("1"));        
+        p2.setWeight(new BigInteger("1"));
         addLeaves(factory, p2, false, NUM_NON_MR_LEAF_QUEUES);
         allocations.getQueue().add(p2);
-        
+
         allocations.setDefaultMinSharePreemptionTimeout(new BigInteger("30"));
         allocations.setFairSharePreemptionTimeout(new BigInteger("30"));
-        
+
         User user = factory.createUser();
         user.setName("yarn");
         user.setMaxRunningApps(new BigInteger(MAX_RUNNING_APPS));
 
         allocations.setUser(user);
-        
+
         Marshaller marshaller = JAXBContext.newInstance("com.latticeengines.scheduler.fairscheduler.generator").createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
         marshaller.marshal(allocations, new File("./fair-scheduler.xml"));
     }
-    
+
     static void addLeaves(ObjectFactory factory, Queue q, boolean minResources, int numLeaves) {
         for (int i = 0; i < numLeaves; i++) {
             Queue leaf = factory.createQueue();
             leaf.setName(Integer.toString(i));
             if (minResources) {
-                leaf.setMinResources("8192 mb, 2 vcores");
+                leaf.setMinResources(NON_MR_MIN_RESOURCES + " mb, 2 vcores");
             }
             leaf.setSchedulingPolicy(FIFO);
             q.getQueue().add(leaf);
