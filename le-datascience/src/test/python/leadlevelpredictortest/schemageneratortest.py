9437f20b4737877440177e153ef5832d07580160
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2014-08-27 23:50:03 +0000

    Minor changes

--- le-datascience/src/test/python/leadlevelpredictortest/schemageneratortest.py
+++ le-datascience/src/test/python/leadlevelpredictortest/schemageneratortest.py
@@ -5,6 +5,6 @@ import leadlevelpredictor.schemagenerator as sgen
 class SchemaGeneratorTest(unittest.TestCase):
 
     def testGenerate(self):
-        schemagen = sgen.SchemaGenerator("/home/rgonzalez/Documents/Customers/Nutanix/Nutanix.json",
-                                         "sqlserverdatasource", "DataLoader_Dep", "L@ttice1", "EventTableTestingDB")
-        schemagen.generate("P1_Event", "Nutanix_EventTable")
+        schemagen = sgen.SchemaGenerator("/home/rgonzalez/Documents/Customers/MuleSoft/MulesoftProduction_6_29_14_updated.json",
+                                         "sqlserverdatasource", "Leaf", "Lattice1", "MuleSoft_Analytics")
+        schemagen.generate("P1_Event", "JakeHack_RemoveDuplicateDomains")
