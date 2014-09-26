377d29dc86577f23c0bec97cd988e6fa64c3d06c
bross <bross@479f5059-62a3-4c39-9342-9852e3a68435>
2014-09-26 19:36:34 +0000

    Remove unnecessary comment.

--- le-camille/src/main/java/com/latticeengines/camille/paths/DirectoryConstants.java
+++ le-camille/src/main/java/com/latticeengines/camille/paths/DirectoryConstants.java
@@ -1,7 +1,6 @@
 package com.latticeengines.camille.paths;
 
 public final class DirectoryConstants {
-    // Directories
     public static final String PODS = "Pods";
     public static final String INTERFACES = "Interfaces";
     public static final String DATA = "Data";
