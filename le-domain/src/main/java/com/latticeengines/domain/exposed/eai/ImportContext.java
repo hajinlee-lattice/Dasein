0ca0ab80e7889e90c66995521fd391fa91f2eef7
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2014-09-30 19:17:08 +0000

    ImportContext for eai.

---
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/eai/ImportContext.java
@@ -0,0 +1,28 @@
+package com.latticeengines.domain.exposed.eai;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ImportContext {
+
+    private Map<String, Object> properties = new HashMap<String, Object>();
+    
+    public void setProperty(String name, Object value) {
+        properties.put(name, value);
+    }
+    
+    
+    public <T> T getProperty(String name, Class<T> valueClass) {
+        Object value = properties.get(name);
+        
+        if (value == null) {
+            return null;
+        }
+        
+        if (valueClass.isInstance(value)) {
+            return valueClass.cast(value);
+        } else {
+            throw new RuntimeException("Value is not of type " + valueClass);
+        }
+    }
+}
