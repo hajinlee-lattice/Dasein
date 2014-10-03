9585a970c67e9b7f20fe21a6d507fbec6bd3d53a
pscuderi <pscuderi@479f5059-62a3-4c39-9342-9852e3a68435>
2014-10-03 16:52:58 +0000

    fixed some method documentation

--- le-camille/src/main/java/com/latticeengines/camille/Camille.java
+++ le-camille/src/main/java/com/latticeengines/camille/Camille.java
@@ -48,25 +48,24 @@ public class Camille {
     }
 
     public Stat set(Path path, Document doc, boolean force) throws Exception {
-        final SetDataBuilder builder = client.setData();
+        SetDataBuilder builder = client.setData();
         if (!force)
             builder.withVersion(doc.getVersion());
-        final Stat stat = builder.forPath(path.toString(), DocumentSerializer.toByteArray(doc));
+        Stat stat = builder.forPath(path.toString(), DocumentSerializer.toByteArray(doc));
         doc.setVersion(stat.getVersion());
         return stat;
     }
 
     public Document get(Path path) throws Exception {
-        final Stat stat = new Stat();
-        final Document doc = DocumentSerializer.toDocument(client.getData().storingStatIn(stat)
-                .forPath(path.toString()));
+        Stat stat = new Stat();
+        Document doc = DocumentSerializer.toDocument(client.getData().storingStatIn(stat).forPath(path.toString()));
         doc.setVersion(stat.getVersion());
         return doc;
     }
 
     public Document get(Path path, CuratorWatcher watcher) throws Exception {
-        final Stat stat = new Stat();
-        final Document doc = DocumentSerializer.toDocument(client.getData().storingStatIn(stat).usingWatcher(watcher)
+        Stat stat = new Stat();
+        Document doc = DocumentSerializer.toDocument(client.getData().storingStatIn(stat).usingWatcher(watcher)
                 .forPath(path.toString()));
         doc.setVersion(stat.getVersion());
         return doc;
@@ -78,11 +77,11 @@ public class Camille {
      * @throws Exception
      */
     public List<Pair<Document, Path>> getChildren(Path path) throws Exception {
-        final List<String> relativeChildPaths = client.getChildren().forPath(path.toString());
+        List<String> relativeChildPaths = client.getChildren().forPath(path.toString());
 
-        final List<Pair<Document, Path>> out = new ArrayList<Pair<Document, Path>>(relativeChildPaths.size());
+        List<Pair<Document, Path>> out = new ArrayList<Pair<Document, Path>>(relativeChildPaths.size());
 
-        for (final String relativePath : relativeChildPaths) {
+        for (String relativePath : relativeChildPaths) {
             final Path childPath = new Path(String.format("%s/%s", path, relativePath));
             out.add(Pair.of(get(childPath), childPath));
         }
@@ -91,21 +90,21 @@ public class Camille {
     }
 
     public DocumentHierarchy getHierarchy(Path path) throws Exception {
-        final DocumentHierarchy h = new DocumentHierarchy(get(path));
+        DocumentHierarchy h = new DocumentHierarchy(get(path));
         addChildren(h.getRoot(), path);
         return h;
     }
 
     private void addChildren(DocumentHierarchy.Node parentNode, Path parentPath) throws Exception {
-        final List<Pair<Document, Path>> children = getChildren(parentPath);
+        List<Pair<Document, Path>> children = getChildren(parentPath);
         Collections.sort(children, new Comparator<Pair<Document, Path>>() {
             @Override
             public int compare(Pair<Document, Path> p0, Pair<Document, Path> p1) {
                 return p0.getRight().toString().compareToIgnoreCase(p1.getRight().toString());
             }
         });
-        for (final Pair<Document, Path> child : children) {
-            final DocumentHierarchy.Node n = new DocumentHierarchy.Node(child.getLeft());
+        for (Pair<Document, Path> child : children) {
+            DocumentHierarchy.Node n = new DocumentHierarchy.Node(child.getLeft());
             parentNode.getChildren().add(n);
             addChildren(n, child.getRight());
         }
@@ -115,9 +114,6 @@ public class Camille {
         client.delete().forPath(path.toString());
     }
 
-    /**
-     * Returns a Stat object if exists, otherwise returns null.
-     */
     public boolean exists(Path path) throws Exception {
         return client.checkExists().forPath(path.toString()) != null;
     }
