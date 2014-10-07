0a55199414a015d1e562c5d7f8072956d84332e1
pscuderi <pscuderi@479f5059-62a3-4c39-9342-9852e3a68435>
2014-10-07 16:51:17 +0000

    data interface skeleton

---
+++ le-camille/src/main/java/com/latticeengines/camille/interfaces/data/DataInterfacePublisher.java
@@ -0,0 +1,13 @@
+package com.latticeengines.camille.interfaces.data;
+
+import com.latticeengines.domain.exposed.camille.Path;
+
+public class DataInterfacePublisher {
+    public DataInterfacePublisher(String interfaceName) {
+
+    }
+
+    public void remove(Path path) {
+
+    }
+}
---
+++ le-camille/src/main/java/com/latticeengines/camille/interfaces/data/DataInterfaceSubscriber.java
@@ -0,0 +1,14 @@
+package com.latticeengines.camille.interfaces.data;
+
+import com.latticeengines.domain.exposed.camille.Document;
+import com.latticeengines.domain.exposed.camille.Path;
+
+public class DataInterfaceSubscriber {
+    public DataInterfaceSubscriber(String interfaceName) {
+
+    }
+
+    public Document get(Path path) {
+        return null;
+    }
+}
