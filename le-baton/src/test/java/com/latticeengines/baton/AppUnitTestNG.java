6daf8bd0776f082baccc92ca41962246c2e7f6ae
pscuderi <pscuderi@479f5059-62a3-4c39-9342-9852e3a68435>
2014-10-18 05:08:21 +0000

    more dead code removal

--- le-baton/src/test/java/com/latticeengines/baton/AppUnitTestNG.java
+++ le-baton/src/test/java/com/latticeengines/baton/AppUnitTestNG.java
@@ -1,17 +1,11 @@
 package com.latticeengines.baton;
 
 import org.junit.Assert;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class AppUnitTestNG {
-    @SuppressWarnings("unused")
-    private static final Logger log = LoggerFactory.getLogger(new Object() {
-    }.getClass().getEnclosingClass());
-
     @BeforeMethod(groups = "unit")
     public void setUp() throws Exception {
 
@@ -23,7 +17,7 @@ public class AppUnitTestNG {
     }
 
     @Test(groups = "unit")
-    public void testSerializeDocument() {
+    public void test() {
         Assert.assertTrue(true);
     }
 }
