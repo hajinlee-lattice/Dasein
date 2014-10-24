26da4697395e9dd9fe3fd464be0b9c311f930e8f
bnguyen <bnguyen@479f5059-62a3-4c39-9342-9852e3a68435>
2014-10-24 20:59:08 +0000

    Remove unnecessary @Table annotation from AlgorithmBase children.

--- le-domain/src/main/java/com/latticeengines/domain/exposed/modeling/algorithm/DecisionTreeAlgorithm.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/modeling/algorithm/DecisionTreeAlgorithm.java
@@ -2,10 +2,8 @@ package com.latticeengines.domain.exposed.modeling.algorithm;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "ALGORITHM")
 @DiscriminatorValue("DecisionTree")
 public class DecisionTreeAlgorithm extends AlgorithmBase {
 
