cb88603abb1ec9b59ee1b450aac065013cc2830a
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2014-10-30 17:19:35 +0000

    commit the modelpredictorextraction script

---
+++ le-dataplatform/src/test/resources/com/latticeengines/dataplatform/python/modelpredictorextraction.py
@@ -0,0 +1,121 @@
+import os
+import sys
+import json
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def main(argv):
+    
+    ModelJSONFilePath = sys.argv[1]
+    CSVFilePath = sys.argv[2]
+ 
+    with open (ModelJSONFilePath, "r") as myfile:
+        ModelJSON=myfile.read().replace('\n', '')
+        
+    contentJSON = json.loads(ModelJSON)
+    if contentJSON["AverageProbability"] is None:
+        print "-------------------------------------------"
+        print "No AverageProbability in the model!!"
+        print "-------------------------------------------"                
+        return 1
+
+    averageProb = contentJSON["AverageProbability"]
+
+    if contentJSON["Summary"] is None:
+        print "-------------------------------------------"
+        print "No ModelSummary information in the model!!"
+        print "-------------------------------------------"                
+        return 1
+    
+    if contentJSON["Summary"]["Predictors"] is None:
+        print "-------------------------------------------"
+        print "No Predictors information in the model!!"
+        print "-------------------------------------------"
+        return 1
+    
+    with open (CSVFilePath, "w") as csvFile:
+	
+        csvFile.write("Feature_Name,Predictive_Power,Bucket_Lower_Bound,Bucket_Upper_Bound,Bucket_Value,Bucket_Probability,Bucket_Lift,Bucket_Frequency,Total#Lead\n")
+        for predictor in contentJSON["Summary"]["Predictors"]:
+            totalCount = 0
+            for predictorElement in predictor["Elements"]:    
+                if 'Name' in predictor:   
+                    if predictor["Name"] is None:
+                        csvFile.write("null")
+                    else:
+                        csvFile.write('"')                    
+                        csvFile.write(unicode(predictor["Name"]).replace('"', '""'))
+                        csvFile.write('"')                        
+                csvFile.write(",")
+            
+                if 'UncertaintyCoefficient' in predictor:
+                    if predictor["UncertaintyCoefficient"] is None:
+                        csvFile.write("null")
+                    else:                     
+                        csvFile.write('"' + unicode(predictor["UncertaintyCoefficient"]) + '"')            
+                csvFile.write(",")           
+
+                length = 0
+                if predictorElement["Values"] is not None:
+                    length = len(predictorElement["Values"])
+                
+                if (length == 0):
+                    if predictorElement["LowerInclusive"] is None:
+                        csvFile.write("NA")
+                    else:
+                        csvFile.write(unicode(predictorElement["LowerInclusive"]))    
+                    csvFile.write(",")                         
+                    
+                    if predictorElement["UpperExclusive"] is None:
+                        csvFile.write("NA")
+                    else:
+                        csvFile.write(unicode(predictorElement["UpperExclusive"]))    
+                    csvFile.write(",")
+                    csvFile.write(",")                    
+                else:
+                    if (predictorElement["Values"])[length - 1] is None:
+                        csvFile.write(",")                    
+                        csvFile.write(",")    
+                        csvFile.write("[null],")                    
+                    else:
+                        csvFile.write(",")                    
+                        csvFile.write(",")
+                        csvFile.write('"[')
+                        for val in predictorElement["Values"]:    
+                            if val is not None:
+                                csvFile.write('""')                           
+                                csvFile.write(unicode(val).replace('"', '""'))
+                                csvFile.write('""')
+                                if val != (predictorElement["Values"])[length - 1]:
+                                    csvFile.write(';')                         
+                        csvFile.write(']",')
+                if 'Lift' in predictorElement:
+		    if predictorElement["Lift"] is None:
+                        csvFile.write("null")
+			csvFile.write(",")
+			csvFile.write("null")
+                    else:                         
+                        csvFile.write(unicode(averageProb*predictorElement["Lift"]))
+			csvFile.write(",")
+			csvFile.write(unicode(predictorElement["Lift"]))          
+                csvFile.write(",")          
+             
+                if 'Count' in predictorElement:
+                    if predictorElement["Count"] is None:
+                        csvFile.write("null")
+                    else:     
+                        csvFile.write(unicode(predictorElement["Count"]))        
+                        totalCount = totalCount + predictorElement["Count"]                        
+                csvFile.write(",") 
+
+                csvFile.write(unicode(totalCount))                
+                csvFile.write("\n")
+    
+    #print "----------------------------------------------------------------------------"
+    #print "successfully extracted predictors information to " + CSVFilePath
+    #print "----------------------------------------------------------------------------"
+    
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
+    
