4b62c3a6259a86631c8aa497cf7fcc0097eac111
smeng <smeng@479f5059-62a3-4c39-9342-9852e3a68435>
2014-11-02 23:59:07 +0000

    change paths in the script to be relative

--- le-dataplatform/deployPythonHDFS.sh
+++ le-dataplatform/deployPythonHDFS.sh
@@ -6,13 +6,13 @@ $HADOOP_HOME/bin/hadoop fs -mkdir /app
 rm -rf /tmp/app
 mkdir -p /tmp/app/dataplatform/scripts/algorithm
 
-cp ~/workspace/ledp/le-dataplatform/conf/env/dev/dataplatform.properties /tmp/app/dataplatform/
-cp ~/workspace/ledp/le-dataplatform/conf/env/dev/hadoop-metrics2.properties /tmp/app/dataplatform/
-cp ~/workspace/ledp/le-dataplatform/target/leframework.tar.gz /tmp/app/dataplatform/scripts
-cp ~/workspace/ledp/le-dataplatform/target/lepipeline.tar.gz /tmp/app/dataplatform/scripts
-cp ~/workspace/ledp/le-dataplatform/src/main/python/launcher.py /tmp/app/dataplatform/scripts 
-cp ~/workspace/ledp/le-dataplatform/src/main/python/pipelinefwk.py /tmp/app/dataplatform/scripts 
-cp ~/workspace/ledp/le-dataplatform/src/main/python/pipeline/pipeline.py /tmp/app/dataplatform/scripts 
-cp ~/workspace/ledp/le-dataplatform/src/main/python/algorithm/*.py /tmp/app/dataplatform/scripts/algorithm
+cp conf/env/dev/dataplatform.properties /tmp/app/dataplatform/
+cp conf/env/dev/hadoop-metrics2.properties /tmp/app/dataplatform/
+cp target/leframework.tar.gz /tmp/app/dataplatform/scripts
+cp target/lepipeline.tar.gz /tmp/app/dataplatform/scripts
+cp src/main/python/launcher.py /tmp/app/dataplatform/scripts 
+cp src/main/python/pipelinefwk.py /tmp/app/dataplatform/scripts 
+cp src/main/python/pipeline/pipeline.py /tmp/app/dataplatform/scripts 
+cp src/main/python/algorithm/*.py /tmp/app/dataplatform/scripts/algorithm
 
 $HADOOP_HOME/bin/hadoop fs -copyFromLocal /tmp/app/dataplatform /app/dataplatform
