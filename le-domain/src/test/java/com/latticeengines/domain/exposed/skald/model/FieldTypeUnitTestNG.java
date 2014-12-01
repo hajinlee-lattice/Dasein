9e43a0bd651774a823d287f037a096a2b0c3127f
bross <bross@479f5059-62a3-4c39-9342-9852e3a68435>
2014-12-01 16:43:21 +0000

    Adding unit test for FieldType.

---
+++ le-domain/src/test/java/com/latticeengines/domain/exposed/skald/model/FieldTypeUnitTestNG.java
@@ -0,0 +1,52 @@
+package com.latticeengines.domain.exposed.skald.model;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FieldTypeUnitTestNG {
+    @Test(groups = "unit")
+    public void testParseBoolean() {
+        FieldType type = FieldType.BOOLEAN;
+        Object actual = FieldType.parse(type, "1");
+        Assert.assertEquals(actual, Boolean.TRUE);
+        actual = FieldType.parse(type, "true");
+        Assert.assertEquals(actual, Boolean.TRUE);
+        actual = FieldType.parse(type, "false");
+        Assert.assertEquals(actual, Boolean.FALSE);
+    }
+    
+    @Test(groups = "unit", expectedExceptions = RuntimeException.class)
+    public void testParseInvalidBoolean() {
+        FieldType type = FieldType.BOOLEAN;
+        FieldType.parse(type, "fasle");
+    }
+    
+    @Test(groups = "unit")
+    public void testParseFloat() {
+        FieldType type = FieldType.FLOAT;
+        Object actual = FieldType.parse(type, "1.03");
+        Double dactual = (Double)actual;
+        Assert.assertTrue(Math.abs(dactual.doubleValue() - 1.03) < 1e-10);
+    }
+    
+    @Test(groups = "unit")
+    public void testParseInteger() {
+        FieldType type = FieldType.INTEGER;
+        Object actual = FieldType.parse(type, "42");
+        Assert.assertEquals(actual, new Integer(42));
+    }
+    
+    @Test(groups = "unit")
+    public void testParseString() {
+        FieldType type = FieldType.STRING;
+        Object actual = FieldType.parse(type, "foo");
+        Assert.assertEquals(actual, "foo");
+    }
+    
+    @Test(groups = "unit")
+    public void testParseTemporal() {
+        FieldType type = FieldType.TEMPORAL;
+        Object actual = FieldType.parse(type, "1234");
+        Assert.assertEquals(actual, new Long(1234));
+    }
+}
