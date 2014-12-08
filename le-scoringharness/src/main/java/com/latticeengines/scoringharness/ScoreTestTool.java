30ff9817f3acbfa5b58a9ab31f1752ebcec5c10f
mendelsohn <mendelsohn@479f5059-62a3-4c39-9342-9852e3a68435>
2014-12-08 23:29:40 +0000

    Scoring Harness stub project containing a main class for command line and service execution.

---
+++ le-scoringharness/src/main/java/com/latticeengines/scoringharness/ScoreTestTool.java
@@ -0,0 +1,22 @@
+package com.latticeengines.scoringharness;
+
+public class ScoreTestTool {
+
+	public static void main(String[] args) {
+		ScoreTestTool tool = new ScoreTestTool(args);
+		tool.Execute();
+	}
+	
+	public ScoreTestTool() {
+		
+	}
+
+	public ScoreTestTool(String[] args) {
+		
+	}
+	
+	public void Execute() {
+		System.out.println("Executed the Lattice Scoring Harness Stub...");
+	}
+
+}
