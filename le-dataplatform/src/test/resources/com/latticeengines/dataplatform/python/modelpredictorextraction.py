c43bc0cd5f416b2c2952c840bd56483cc18ab31f
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2014-12-15 18:59:20 +0000

    latest script from Tejas

--- le-dataplatform/src/test/resources/com/latticeengines/dataplatform/python/modelpredictorextraction.py
+++ le-dataplatform/src/test/resources/com/latticeengines/dataplatform/python/modelpredictorextraction.py
@@ -1,3 +1,7 @@
+# This version combines the LowerInclusive, UpperExclusive and Bucket Value fields into a single field. 
+# For the original output, use v3 version of this script
+
+
 import os
 import sys
 import json
@@ -11,6 +15,9 @@ def main(argv):
     
     ModelJSONFilePath = sys.argv[1]
     CSVFilePath = sys.argv[2]
+    debugFlag = ''
+    if len(sys.argv) > 3:
+        debugFlag = sys.argv[3].lower()
     nameArray = []
     sumArray = []
  
@@ -30,7 +37,7 @@ def main(argv):
     if contentJSON["Summary"] is None:
         print "-------------------------------------------"
         print "No ModelSummary information in the model!!"
-        print "-------------------------------------------"
+        print "-------------------------------------------"                
         return 1
     
     if contentJSON["Summary"]["Predictors"] is None:
@@ -40,65 +47,68 @@ def main(argv):
         return 1
     
     with open (CSVFilePath, "w") as csvFile:
-        csvFile.write("Feature Name,Predictive Power,Bucket Lower Bound,Bucket Upper Bound,Bucket Value, Conversion Rate,Lift,Total Leads,Frequency(#),Frequency/Total Leads\n")
-
-    #This section calculates the total #leads for each predictor. Ideally it should be the same for each predictor, but there seem to be some exceptions
+        if debugFlag == "-debug":
+            csvFile.write("Column Name,Attribute Name,Category,Predictive Power,Attribute Value,Conversion Rate,Lift,Total Leads,Frequency(#),Frequency/Total Leads,ApprovedUsage\n")
+        else:        
+            csvFile.write("Attribute Name,Category,Predictive Power,Attribute Value,Conversion Rate,Lift,Total Leads,Frequency(#),Frequency/Total Leads\n")
+            
+        #This section calculates the total #leads for each predictor. Ideally it should be the same for each predictor, but there seem to be some exceptions
         siddata = sorted(contentJSON["Summary"]["Predictors"], key=operator.itemgetter('Name'))
         sidgroups = itertools.groupby(siddata, operator.itemgetter('Name'))
-        for key, group in sidgroups: #print('{}\t{}'.format(key, sum(int(value["Count"]) for value in group)))
+        
+        for key, group in sidgroups: 
             for value in group:
                 total = 0
                 for i in value['Elements']:
                     total = total + i['Count']
-                print value['Name'], ":\t\t", total
+                #print value['Name'], ":\t\t", total
                 nameArray.append(value['Name'])
                 sumArray.append(total)
         dictArray = dict(zip(nameArray,sumArray))
         #dictArray is a list of dictionaries with attributeName = #Total Leads
 
         for predictor in contentJSON["Summary"]["Predictors"]:
-            for predictorElement in predictor["Elements"]:
-                if 'Name' in predictor:   
-                    if predictor["Name"] is None:
-                        csvFile.write("null")
-                    else:
-                        csvFile.write('"')
+            if predictor["ApprovedUsage"] in ["None","Model"] and debugFlag != "-debug": continue;
+            for predictorElement in predictor["Elements"]:    
+                if debugFlag == "-debug":
+                    if 'Name' in predictor:
+                        csvFile.write('"')                    
                         csvFile.write(unicode(predictor["Name"]).replace('"', '""'))
-                        csvFile.write('"')
+                        csvFile.write('"')                        
+                    csvFile.write(",")
+
+                if 'DisplayName' in predictor:   
+                    csvFile.write('"')                    
+                    csvFile.write(unicode(predictor["DisplayName"]).replace('"', '""'))
+                    csvFile.write('"')                        
                 csvFile.write(",")
-            
+                
+                if 'Category' in predictor:   
+                    csvFile.write('"')                    
+                    csvFile.write(unicode(predictor["Category"]).replace('"', '""'))
+                    csvFile.write('"')                        
+                csvFile.write(",")
+                                                
                 if 'UncertaintyCoefficient' in predictor:
-                    if predictor["UncertaintyCoefficient"] is None:
-                        csvFile.write("null")
-                    else:                     
-                        csvFile.write('"' + unicode(predictor["UncertaintyCoefficient"]) + '"')
+                    csvFile.write('"' + unicode(predictor["UncertaintyCoefficient"]) + '"')            
                 csvFile.write(",")           
 
                 length = 0
                 if predictorElement["Values"] is not None:
                     length = len(predictorElement["Values"])
                 
-                if (length == 0):
-                    if predictorElement["LowerInclusive"] is None:
-                        csvFile.write("NA")
+                if (length == 0):   #This implies that value is of type bucket and not categorical
+                    if predictorElement["UpperExclusive"] is not None:
+                        csvFile.write("< ")
+                        csvFile.write(unicode(predictorElement["UpperExclusive"]))                        
                     else:
+                        csvFile.write(">= ")
                         csvFile.write(unicode(predictorElement["LowerInclusive"]))    
-                    csvFile.write(",")                         
-                    
-                    if predictorElement["UpperExclusive"] is None:
-                        csvFile.write("NA")
-                    else:
-                        csvFile.write(unicode(predictorElement["UpperExclusive"]))    
-                    csvFile.write(",")
                     csvFile.write(",")                    
                 else:
                     if (predictorElement["Values"])[length - 1] is None:
-                        csvFile.write(",")                    
-                        csvFile.write(",")    
-                        csvFile.write("[null],")                    
+                        csvFile.write("Not Available,")                    
                     else:
-                        csvFile.write(",")                    
-                        csvFile.write(",")
                         csvFile.write('"[')
                         for val in predictorElement["Values"]:    
                             if val is not None:
@@ -108,15 +118,13 @@ def main(argv):
                                 if val != (predictorElement["Values"])[length - 1]:
                                     csvFile.write(';')                         
                         csvFile.write(']",')
+                
                 if 'Lift' in predictorElement:
-                    if predictorElement["Lift"] is None:
-                        csvFile.write("null")
-                        csvFile.write(",")
-                        csvFile.write("null")
-                    else:                         
-                        csvFile.write(unicode(averageProb*predictorElement["Lift"]))
-                        csvFile.write(",")
-                        csvFile.write(unicode(predictorElement["Lift"]))          
+                    csvFile.write(unicode(averageProb*predictorElement["Lift"]))
+                csvFile.write(",")
+                
+                if 'Lift' in predictorElement:
+                    csvFile.write(unicode(predictorElement["Lift"]))          
                 csvFile.write(",")
 
                 if predictor['Name'] in dictArray.keys():
@@ -125,17 +133,22 @@ def main(argv):
                     else:                     
                     #write total #leads to csvFile
                         csvFile.write('"' + str(dictArray[predictor['Name']]) + '"')            
-                csvFile.write(",")
+                csvFile.write(",")    
              
                 if 'Count' in predictorElement:
                     if predictorElement["Count"] is None:
                         csvFile.write("null")
-                    else:     
+                    else:
                         csvFile.write(unicode(predictorElement["Count"]))
                         csvFile.write(",")     
                         csvFile.write(unicode(predictorElement["Count"]/float(dictArray[predictor['Name']])))                   
-                csvFile.write(",") 
+                csvFile.write(",")
                 
+                if debugFlag == "-debug":
+                    if 'DisplayName' in predictor: 
+                        csvFile.write('"')                    
+                        csvFile.write(unicode(predictor["ApprovedUsage"]).replace('"', '""'))
+                        csvFile.write('"')                                                
                 csvFile.write("\n")
     
     #print "----------------------------------------------------------------------------"
