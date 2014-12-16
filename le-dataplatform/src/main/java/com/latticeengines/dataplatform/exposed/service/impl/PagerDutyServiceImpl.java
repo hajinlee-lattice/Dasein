df0fdbeb28900748be7573dd4dbba445ccd54ac1
bnguyen <bnguyen@479f5059-62a3-4c39-9342-9852e3a68435>
2014-12-16 21:13:11 +0000

    DP-623 Recent Modeling failures have not been triggering PagerDuty alerts Review 301
    Root cause due to malformed json when exception stacktraces were passed into alert.  This would have been fixed with Yunlong's changes but those are on pause until 2015.

--- le-dataplatform/src/main/java/com/latticeengines/dataplatform/exposed/service/impl/PagerDutyServiceImpl.java
+++ le-dataplatform/src/main/java/com/latticeengines/dataplatform/exposed/service/impl/PagerDutyServiceImpl.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Component;
 
 import com.google.common.annotations.VisibleForTesting;
@@ -33,31 +35,28 @@ public class PagerDutyServiceImpl implements PagerDutyService {
         return triggerEvent(description, clientUrl, Arrays.asList(details));
     }
 
+    @SuppressWarnings("unchecked")
     public String triggerEvent(String description, String clientUrl, Iterable<? extends BasicNameValuePair> details)
             throws ClientProtocolException, IOException {
-        StringBuilder sb = new StringBuilder();
-        sb.append("{");
-        sb.append("\"service_key\": \"" + serviceApiKey + "\",");
-        sb.append("\"event_type\": \"trigger\",");
-        sb.append("\"description\": \"" + description + "\",");
-        sb.append("\"client\": \"Modeling Platform\",");
-        sb.append("\"client_url\": \"" + clientUrl + "\",");
-        sb.append("\"details\": {");
+        LinkedHashMap<String, String> mainMap = new LinkedHashMap<>();
+        LinkedHashMap<String, String> detailsMap = new LinkedHashMap<>();
+        mainMap.put("service_key", serviceApiKey);
+        mainMap.put("event_type", "trigger");
+        mainMap.put("description", description);
+        mainMap.put("client", "Modeling Platform");
+        mainMap.put("client_url", clientUrl);
+        JSONObject obj=new JSONObject(mainMap);
 
         for (Iterator<? extends BasicNameValuePair> iterator = details.iterator(); iterator.hasNext();) {
             BasicNameValuePair detail = iterator.next();
-            sb.append("\"" + detail.getName() + "\":");
-            sb.append("\"" + detail.getValue() + "\"");
-            if (iterator.hasNext()) {
-                sb.append(",");
-            }
+            detailsMap.put(detail.getName(), detail.getValue());
         }
-        sb.append("}}");
+        obj.put("details", detailsMap);
 
         // response should look like this -
         // {"status":"success","message":"Event processed","incident_key":”acdcfa307f3e47d1b42b37edcbf22ae7"}
         String response = HttpClientWithOptionalRetryUtils.sendPostRequest(
-                "https://events.pagerduty.com/generic/2010-04-15/create_event.json", true, headers, sb.toString());
+                "https://events.pagerduty.com/generic/2010-04-15/create_event.json", true, headers, obj.toString());
 
         return response;
     }
--- le-dataplatform/src/test/java/com/latticeengines/dataplatform/exposed/service/impl/PagerDutyServiceTestNG.java
+++ le-dataplatform/src/test/java/com/latticeengines/dataplatform/exposed/service/impl/PagerDutyServiceTestNG.java
@@ -23,8 +23,8 @@ public class PagerDutyServiceTestNG extends DataPlatformFunctionalTestNGBase {
 
     @Test(groups = "functional", enabled = true)
     public void testTriggerOneDetail() throws ClientProtocolException, IOException, ParseException {
-        String result = pagerDutyService.triggerEvent("PagerDutyServiceTestNG", "http://PagerDutyServiceTestNG", new BasicNameValuePair("testmetric",
-                "testvalue"));
+        String result = pagerDutyService.triggerEvent("PagerDutyServiceTestNG", "http://PagerDutyServiceTestNG",
+                new BasicNameValuePair("testmetric", "testvalue"));
         PagerDutyTestUtils.confirmPagerDutyIncident(result);
     }
 
@@ -36,8 +36,19 @@ public class PagerDutyServiceTestNG extends DataPlatformFunctionalTestNGBase {
 
     @Test(groups = "functional", enabled = true)
     public void testTriggerMultipleDetail() throws ClientProtocolException, IOException, ParseException {
-        String result = pagerDutyService.triggerEvent("PagerDutyServiceTestNG", "http://PagerDutyServiceTestNG", new BasicNameValuePair("testmetric",
-                "testvalue"), new BasicNameValuePair("anothertestmetric", "anothertestvalue"));
+        String result = pagerDutyService.triggerEvent("PagerDutyServiceTestNG", "http://PagerDutyServiceTestNG",
+                new BasicNameValuePair("testmetric", "testvalue"), new BasicNameValuePair("anothertestmetric",
+                        "anothertestvalue"));
+        PagerDutyTestUtils.confirmPagerDutyIncident(result);
+    }
+
+    @Test(groups = "functional", enabled = true)
+    public void testTriggerDetailWithUnescapedJsonConflicts() throws ClientProtocolException, IOException,
+            ParseException {
+        String result = pagerDutyService.triggerEvent("PagerDutyServiceTestNG", "localhost:8088/cluster/",
+                new BasicNameValuePair("commandLogId91", "errorCode:LEDP_00002 errorMessage:Generic system error.\n"
+                        + "com.latticeengines.domain.exposed.exception.LedpException: Generic system error.\n"),
+                new BasicNameValuePair("anothertestmetric", "anothertestvalue"));
         PagerDutyTestUtils.confirmPagerDutyIncident(result);
     }
 
