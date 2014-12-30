bf9f9cd67b7b237814f2c39ee3551f4996691980
lming <lming@479f5059-62a3-4c39-9342-9852e3a68435>
2014-12-30 23:09:25 +0000

    Review Request 340: Top Predictors Bucketing Issues

--- le-dataplatform/src/main/python/leframework/bucketers/bucketerdispatcher.py
+++ le-dataplatform/src/main/python/leframework/bucketers/bucketerdispatcher.py
@@ -2,9 +2,8 @@ import os
 import copy
 import sys
 import pkgutil as pkg
-import numpy as np
-import scipy.stats
 from geometricbucketer import GeometricBucketer
+from bucketconsolidator import BucketConsolidator
 
 class BucketerDispatcher(object):
 
@@ -15,14 +14,16 @@ class BucketerDispatcher(object):
         self.defaultBucketer = GeometricBucketer()
         self.defaultMaxBuckets = 7
         self.defaultParams = {'minValue': 1, 'multiplierList': [2, 2.5, 2]}
+        self.defaultLiftThreshold = 0.2
+        self.defaultRatioThreshold = 0.2
 
     def __importAllBucketers(self):
         curDir = os.path.dirname(os.path.abspath(__file__))
         for _, name, _ in pkg.iter_modules([curDir]):
             if name.endswith(self.suffix) and len(name) > len(self.suffix):
                 # Get class name from module name, i.e linearbucketer -> LinearBucketer
-                className = name.replace(self.suffix,'').title()+self.suffix.title()
-                if className not in globals(): 
+                className = name.replace(self.suffix, '').title() + self.suffix.title()
+                if className not in globals():
                     path = list(sys.path)
                     sys.path.append(curDir)
                     try:
@@ -31,15 +32,17 @@ class BucketerDispatcher(object):
                         globals()[className] = getattr(mod, className)
                     finally:
                         # Restore
-                        sys.path[:] = path 
+                        sys.path[:] = path
 
-    def bucketColumn(self, columnSeries, eventSeries, methodType = None, methodParams = None):
+    def bucketColumn(self, columnSeries, eventSeries, methodType=None, methodParams=None):
         '''
         Input i.e. methodType = 'linear' -> method = 'LinearBucketer'
         If the bucketing method is not found, default bucketing algorithm will be invoked
         '''
-        bucketer =  self.defaultBucketer
+        bucketer = self.defaultBucketer
         maxBuckets = self.defaultMaxBuckets
+        liftThreshold = self.defaultLiftThreshold
+        ratioThreshold = self.defaultRatioThreshold
         params = self.defaultParams
 
         if methodType is not None:
@@ -50,40 +53,11 @@ class BucketerDispatcher(object):
                 # Set type specific parameters
                 copyParams = copy.deepcopy(methodParams)
                 maxBuckets = copyParams.pop("maxBuckets", maxBuckets)
+                liftThreshold = copyParams.pop("liftThreshold", liftThreshold)
+                ratioThreshold = copyParams.pop("ratioThreshold", ratioThreshold)
                 params = copyParams
 
         bucketList = bucketer.bucketColumn(columnSeries, params)
-        return self.consolidateBins(columnSeries, eventSeries, bucketList, maxBuckets)
 
-    def consolidateBins(self, columnSeries, eventSeries, bucketList, maxBuckets):
-        """
-        Method to consolidate a list of continuous attribute buckets given an attribute column and event column.  Ensures that the bucket count is 
-        less than maxBuckets by removing adjacent ranges that have the maximum probability of resulting from a binomial distribution of the combined 
-        ranges
-        """
-        if maxBuckets <= 0:
-            raise ValueError("maxBuckets cannot be less than or equal to zero.")
-
-        zippedList = zip(bucketList, bucketList[1::])    
-        getSelector = lambda x, nextX: (columnSeries > x) & (columnSeries <= nextX)
-        eventSums = [(eventSeries[getSelector(x, nextX)].sum(), np.count_nonzero(getSelector(x, nextX))) for x, nextX in zippedList]
-
-        while (len(zippedList) > maxBuckets):
-            maxDelta = 0
-            removeIndex = 0
-            for i in range(len(zippedList)-1):
-                newConversionRate = float(eventSums[i][0] + eventSums[i + 1][0] + 1) / float(eventSums[i][1] + eventSums[i + 1][1] + 1)
-                delta = scipy.stats.binom.pmf(eventSums[i][0], eventSums[i][1], newConversionRate) * scipy.stats.binom.pmf(eventSums[i + 1][0], eventSums[i + 1][1], newConversionRate)
-                if np.isnan(delta):
-                    raise RuntimeError("Invalid event series distribution.")
-                if delta > maxDelta:
-                    maxDelta = delta
-                    removeIndex = i
-
-            # combine the bins around the removed threshold 
-            zippedList[removeIndex] = (zippedList[removeIndex][0], zippedList[removeIndex + 1][1])
-            eventSums[removeIndex] = (eventSums[removeIndex][0] + eventSums[removeIndex + 1][0], eventSums[removeIndex][1] + eventSums[removeIndex + 1][1])
-            zippedList.remove(zippedList[removeIndex + 1])
-            eventSums.remove(eventSums[removeIndex + 1])
-
-        return [y[0] for y in zippedList] + [zippedList[-1][1]]    
+        bc = BucketConsolidator(maxBuckets, liftThreshold, ratioThreshold)
+        return bc.consolidateBins(columnSeries, eventSeries, bucketList)
