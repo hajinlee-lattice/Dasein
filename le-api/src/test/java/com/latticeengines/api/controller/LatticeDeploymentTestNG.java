11c61342a43f3ec382673da2466d9b981ed60ca5
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2015-01-21 04:34:42 +0000

    Remove unused Pair data structure from the test code.

--- le-api/src/test/java/com/latticeengines/api/controller/LatticeDeploymentTestNG.java
+++ le-api/src/test/java/com/latticeengines/api/controller/LatticeDeploymentTestNG.java
@@ -160,30 +160,4 @@ public class LatticeDeploymentTestNG extends ApiFunctionalTestNGBase {
         }
     }
 
-    class Pair<K, V> {
-        private K key;
-        private V value;
-
-        public Pair(K key, V value) {
-            this.setKey(key);
-            this.setValue(value);
-        }
-
-        public K getKey() {
-            return key;
-        }
-
-        public void setKey(K key) {
-            this.key = key;
-        }
-
-        public V getValue() {
-            return value;
-        }
-
-        public void setValue(V value) {
-            this.value = value;
-        }
-    }
-
 }
--- le-api/src/test/java/com/latticeengines/api/controller/NutanixDeploymentTestNG.java
+++ le-api/src/test/java/com/latticeengines/api/controller/NutanixDeploymentTestNG.java
@@ -167,30 +167,4 @@ public class NutanixDeploymentTestNG extends ApiFunctionalTestNGBase {
         }
     }
 
-    class Pair<K, V> {
-        private K key;
-        private V value;
-
-        public Pair(K key, V value) {
-            this.setKey(key);
-            this.setValue(value);
-        }
-
-        public K getKey() {
-            return key;
-        }
-
-        public void setKey(K key) {
-            this.key = key;
-        }
-
-        public V getValue() {
-            return value;
-        }
-
-        public void setValue(V value) {
-            this.value = value;
-        }
-    }
-
 }
