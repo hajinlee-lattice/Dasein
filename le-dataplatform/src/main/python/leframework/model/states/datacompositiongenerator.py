d528774837a36983efdb6e039966d880582a8db4
wbaumann <wbaumann@479f5059-62a3-4c39-9342-9852e3a68435>
2015-01-22 23:40:58 +0000

    Cease swallowing exceptions with generators, and add a missing metadata check.

--- le-dataplatform/src/main/python/leframework/model/states/datacompositiongenerator.py
+++ le-dataplatform/src/main/python/leframework/model/states/datacompositiongenerator.py
@@ -30,38 +30,42 @@ class DataCompositionGenerator(State):
         schema = self.getMediator().schema
         config_metadata = schema["config_metadata"]["Metadata"]
         
-        for name, data_type in schema["fields"].iteritems():
-            if name in schema["targets"]:
+        result = list()
+        for name, data_type in schema["fields"].iteritems():            
+            metadata = [x for x in config_metadata if x["ColumnName"] == name]
+            if len(metadata) == 0:
                 continue
-        
-            metadata = next(x for x in config_metadata if x["ColumnName"] == name)
-            details = OrderedDict()
+            metadata = metadata[0]
             
+            details = OrderedDict()
             details["source"] = "REQUEST"
             if metadata["DataSource"] is not None:
                 if "DerivedColumns" in metadata["DataSource"]:
                     details["source"] = "PROPRIETARY"
             
             # TODO Decide if we need to handle TEMPORAL types or not.
-            details["type"] = "STRING"
             if data_type == "boolean":
                 details["type"] = "BOOLEAN"
-            if data_type == "int" or data_type == "long":
+            elif data_type == "int" or data_type == "long":
                 details["type"] = "INTEGER"
-            if data_type == "float" or data_type == "double":
+            elif data_type == "float" or data_type == "double":
                 details["type"] = "FLOAT"
+            else:
+                details["type"] = "STRING"
             
-            details["interpretation"] = "FEATURE"
-            # TODO Should identify these and similar columns through metadata.
-            if name == "Email":
+            # TODO Should identify interesting non-feature columns through metadata.
+            if name in schema["features"]:
+                details["interpretation"] = "FEATURE"
+            elif name == "Email":
                 details["interpretation"] = "EMAIL_ADDRESS"
-            if name == "LeadID":
+            elif name == "LeadID":
                 details["interpretation"] = "RECORD_ID"
-               
-            if name in schema["readouts"] and details["interpretation"] == "FEATURE":
+            else:
                 continue
 
-            yield (name, details)
+            result.append((name, details))
+
+        return result
 
     def _get_transforms(self, fields):
         pipeline = self.getMediator().pipeline.getPipeline()
@@ -69,22 +73,25 @@ class DataCompositionGenerator(State):
         step = next(x for x in pipeline if x.__class__.__name__ == "ImputationStep")
         imputations = step._enumMappings
     
+        result = list()
         for name, details in fields.iteritems():
             if details["interpretation"] != "FEATURE":
                 continue
                 
             if details["type"] == "STRING":
-                yield self._make_transform(
-                    "encode_string", name, "INTEGER", [("column", name)])
+                result.append(self._make_transform(
+                    "encode_string", name, "INTEGER", [("column", name)]))
             
             if details["type"] != "FLOAT":
-                yield self._make_transform(
-                    "make_float", name, "FLOAT", [("column", name)])
+                result.append(self._make_transform(
+                    "make_float", name, "FLOAT", [("column", name)]))
             
             if name in imputations:
-                yield self._make_transform(
+                result.append(self._make_transform(
                     "replace_null_value", name, "FLOAT",
-                    [("column", name), ("value", imputations[name])])
+                    [("column", name), ("value", imputations[name])]))
+
+        return result
     
     def _make_transform(self, name, output, type, arguments):
         result = OrderedDict()
