abd27b0fcaf8ecbf10317a0ec3ad3ec4dec22a21
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2015-01-23 23:00:59 +0000

    Move db-related base interfaces and classes to le-db.

---
+++ le-db/src/main/java/com/latticeengines/db/exposed/dao/BaseDao.java
@@ -0,0 +1,25 @@
+package com.latticeengines.db.exposed.dao;
+
+import java.util.List;
+
+public interface BaseDao<T> {
+
+    void create(T entity);
+
+    void createOrUpdate(T entity);
+
+    void update(T entity);
+
+    void delete(T entity);
+
+    void deleteAll();
+
+    boolean containInSession(T entity);
+
+    List<T> findAll();
+
+    T findByKey(T entity);
+
+    T findByKey(Class<T> entityClz, Long key);
+
+}
---
+++ le-db/src/main/java/com/latticeengines/db/exposed/entitymgr/BaseEntityMgr.java
@@ -0,0 +1,28 @@
+package com.latticeengines.db.exposed.entitymgr;
+
+import java.util.List;
+
+import com.latticeengines.db.exposed.dao.BaseDao;
+
+public interface BaseEntityMgr<T> {
+
+    BaseDao<T> getDao();
+    
+    void create(T entity);
+
+    void createOrUpdate(T entity);
+     
+    void update(T entity);
+    
+    void delete(T entity);
+
+    void deleteAll();
+    
+    boolean containInSession(T entity);
+    
+    T findByKey(T entity);
+ 
+    List<T> findAll();
+
+    
+}
