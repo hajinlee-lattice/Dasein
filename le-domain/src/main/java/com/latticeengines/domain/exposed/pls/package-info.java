6d40b99ef8bfc1f4797c8680f0593c3a0d416003
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2015-02-04 17:42:30 +0000

    Change filter param type to Long.

--- le-domain/src/main/java/com/latticeengines/domain/exposed/pls/package-info.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/pls/package-info.java
@@ -6,7 +6,7 @@
  *
  */
 @FilterDef(name = "tenantFilter", //
-           parameters = @ParamDef(name = "tenantFilterId", type = "java.lang.Integer"))
+           parameters = @ParamDef(name = "tenantFilterId", type = "java.lang.Long"))
 package com.latticeengines.domain.exposed.pls;
 import org.hibernate.annotations.FilterDef;
 import org.hibernate.annotations.ParamDef;
