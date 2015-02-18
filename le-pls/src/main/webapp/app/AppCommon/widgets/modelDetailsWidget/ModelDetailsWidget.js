66c6f3344d5ac184270b3e1a1dd1e4bcf089d426
pierce <pierce@479f5059-62a3-4c39-9342-9852e3a68435>
2015-02-18 15:08:12 +0000

    Adding in the External/Internal attributes to the ModelDetailsWidget

--- le-pls/src/main/webapp/app/AppCommon/widgets/modelDetailsWidget/ModelDetailsWidget.js
+++ le-pls/src/main/webapp/app/AppCommon/widgets/modelDetailsWidget/ModelDetailsWidget.js
@@ -2,18 +2,20 @@ angular.module('mainApp.appCommon.widgets.ModelDetailsWidget', [
     'mainApp.appCommon.utilities.ResourceUtility',
     'mainApp.appCommon.utilities.DateTimeFormatUtility',
     'mainApp.core.utilities.GriotNavUtility',
-    'mainApp.appCommon.utilities.StringUtility'
+    'mainApp.appCommon.utilities.StringUtility',
+    'mainApp.appCommon.services.TopPredictorService'
 ])
 
-.controller('ModelDetailsWidgetController', function ($scope, $rootScope, ResourceUtility, DateTimeFormatUtility, GriotNavUtility, StringUtility) {
+.controller('ModelDetailsWidgetController', function ($scope, $rootScope, ResourceUtility, DateTimeFormatUtility, GriotNavUtility, StringUtility, TopPredictorService) {
     $scope.ResourceUtility = ResourceUtility;
     
     var widgetConfig = $scope.widgetConfig;
     var metadata = $scope.metadata;
-    var data = $scope.data.ModelDetails;
+    var data = $scope.data;
+    var modelDetails = $scope.data.ModelDetails;
 
-    $scope.displayName = data[widgetConfig.NameProperty];
-    var isActive = data[widgetConfig.StatusProperty];
+    $scope.displayName = modelDetails[widgetConfig.NameProperty];
+    var isActive = modelDetails[widgetConfig.StatusProperty];
     
     if (isActive) {
         $scope.status = ResourceUtility.getString("MODEL_DETAILS_ACTIVE_LABEL");
@@ -21,39 +23,39 @@ angular.module('mainApp.appCommon.widgets.ModelDetailsWidget', [
         $scope.status = ResourceUtility.getString("MODEL_DETAILS_INACTIVE_LABEL");
     }
     
-    $scope.score = data[widgetConfig.ScoreProperty];
+    $scope.score = modelDetails[widgetConfig.ScoreProperty];
     if ($scope.score != null && $scope.score < 1) {
         $scope.score = Math.round($scope.score * 100);
     }
     
     //Need to calculate this
-    $scope.externalAttributes = data[widgetConfig.ExternalAttributesProperty];
+    $scope.externalAttributes = TopPredictorService.GetNumberOfAttributesByType(data.Predictors, "External");
     //Need to calculate this
-    $scope.internalAttributes = data[widgetConfig.InternalAttributesProperty];
+    $scope.internalAttributes = TopPredictorService.GetNumberOfAttributesByType(data.Predictors, "Internal");
     
-    $scope.createdDate = data[widgetConfig.CreatedDateProperty];
+    $scope.createdDate = modelDetails[widgetConfig.CreatedDateProperty];
     $scope.createdDate = $scope.createdDate * 1000;
     $scope.createdDate = new Date($scope.createdDate).toLocaleDateString();
     
-    $scope.totalLeads = data[widgetConfig.TotalLeadsProperty];
+    $scope.totalLeads = modelDetails[widgetConfig.TotalLeadsProperty];
     $scope.totalLeads = StringUtility.AddCommas($scope.totalLeads);
     
-    $scope.testSet = data[widgetConfig.TestSetProperty];
+    $scope.testSet = modelDetails[widgetConfig.TestSetProperty];
     $scope.testSet = StringUtility.AddCommas($scope.testSet);
     
-    $scope.trainingSet = data[widgetConfig.TrainingSetProperty];
+    $scope.trainingSet = modelDetails[widgetConfig.TrainingSetProperty];
     $scope.trainingSet = StringUtility.AddCommas($scope.trainingSet);
 
-    $scope.totalSuccessEvents = data[widgetConfig.TotalSuccessEventsProperty];
+    $scope.totalSuccessEvents = modelDetails[widgetConfig.TotalSuccessEventsProperty];
     $scope.totalSuccessEvents = StringUtility.AddCommas($scope.totalSuccessEvents);
     
-    $scope.conversionRate = data[widgetConfig.TotalSuccessEventsProperty] / (data[widgetConfig.TestSetProperty] + data[widgetConfig.TrainingSetProperty]);
+    $scope.conversionRate = modelDetails[widgetConfig.TotalSuccessEventsProperty] / (modelDetails[widgetConfig.TestSetProperty] + modelDetails[widgetConfig.TrainingSetProperty]);
     if ($scope.conversionRate != null && $scope.conversionRate < 1) {
         $scope.conversionRate = $scope.conversionRate * 100;
         $scope.conversionRate = $scope.conversionRate.toFixed(2);
     }
-    $scope.leadSource = data[widgetConfig.LeadSourceProperty];
-    $scope.opportunity = data[widgetConfig.OpportunityProperty];
+    $scope.leadSource = modelDetails[widgetConfig.LeadSourceProperty];
+    $scope.opportunity = modelDetails[widgetConfig.OpportunityProperty];
     
     $scope.dataExpanded = false;
     $scope.showMoreLabel = ResourceUtility.getString('MODEL_DETAILS_SHOW_MORE_BUTTON');
--- le-pls/src/main/webapp/app/AppCommon/widgets/topPredictorWidget/TopPredictorWidget.js
+++ le-pls/src/main/webapp/app/AppCommon/widgets/topPredictorWidget/TopPredictorWidget.js
@@ -1,230 +1,8 @@
 angular.module('mainApp.appCommon.widgets.TopPredictorWidget', [
     'mainApp.appCommon.utilities.ResourceUtility',
-    'mainApp.appCommon.utilities.StringUtility',
-    'mainApp.appCommon.utilities.AnalyticAttributeUtility',
-    'mainApp.appCommon.services.WidgetFrameworkService'
+    'mainApp.appCommon.services.WidgetFrameworkService',
+    'mainApp.appCommon.services.TopPredictorService'
 ])
-
-.service('TopPredictorService', function (StringUtility, AnalyticAttributeUtility) {
-    
-    this.ShowBasedOnTags = function (predictor, tag) {
-        
-        //TODO:pierce This is a hack because DataLoader is not providing internal metadata
-        if (predictor.Tags == null) {
-            predictor.Tags = ["External"];
-        }
-        
-        var toReturn = false;
-        for (var x=0; x<predictor.Tags.length; x++) {
-            if (tag == predictor.Tags[x]) {
-                toReturn = true;
-                break;
-            }
-        }
-        
-        return toReturn;
-    };
-    
-    this.GetCategoryListByType = function (categoryList, type, fullPredictorList) {
-        var toReturn = {
-            total: 0,
-            categories: []
-        };
-        if (categoryList == null || type == null || fullPredictorList == null) {
-            return toReturn;
-        }
-        for (var x = 0; x < categoryList.length; x++) {
-            var category = categoryList[x];
-            var displayCategory = {
-                name: category.name,
-                count: 0,
-                color: category.color
-            };
-            for (var i = 0; i < fullPredictorList.length; i++) {
-                var predictor = fullPredictorList[i];
-                if (predictor.Category == category.name && 
-                    this.ShowBasedOnTags(predictor, type) &&
-                    AnalyticAttributeUtility.IsAllowedForInsights(predictor)) {
-                        //TODO:pierce Might need to check frequency on categoricals before adding the number
-                        toReturn.total += predictor.Elements.length;
-                        displayCategory.count = displayCategory.count + predictor.Elements.length;
-                    }
-            }
-            
-            if (displayCategory.count > 0) {
-                toReturn.categories.push(displayCategory);
-            }
-        }
-        
-        return toReturn;
-    };
-    
-    this.GetColorByCategory = function (categoryName) {
-        var toReturn;
-        switch (categoryName) {
-            case "Firmographics":
-                toReturn = "#27D2AE";
-                break;
-            case "Growth Trends":
-                toReturn = "#3279DF";
-                break;
-            case "Online Presence":
-                toReturn = "#FF9403";
-                break;
-            case "Technologies":
-                toReturn = "#BD8DF6";
-                break;
-            case "Behind Firewall Tech":
-                toReturn = "#96E01E";
-                break;
-            case "Financial":
-                toReturn = "#A8A8A8";
-                break;
-            case "Marketing Activity":
-                toReturn = "#3279DF";
-                break;
-            case "Lead Information":
-                toReturn = "#FF7A44";
-                break;
-            default:
-                toReturn = "#000000";
-                break;
-        }
-        
-        return toReturn;
-    };
-    
-    this.SortByPredictivePower = function (a, b) {
-        if (a.UncertaintyCoefficient > b.UncertaintyCoefficient) {
-            return -1;
-        }
-        if (a.UncertaintyCoefficient < b.UncertaintyCoefficient) {
-            return 1;
-        }
-        // a must be equal to b
-        return 0;
-    };
-    
-    this.GetAttributesByCategory = function (predictorList, categoryName, categoryColor, maxNumber) {
-        if (StringUtility.IsEmptyString(categoryName) || predictorList == null) {
-            return [];
-        }
-        
-        var totalPredictors = [];
-        for (var i = 0; i < predictorList.length; i++) {
-            if (categoryName == predictorList[i].Category) {
-                totalPredictors.push(predictorList[i]);
-            }
-        }
-        totalPredictors = totalPredictors.sort(this.SortByPredictivePower);
-        
-        var toReturn = [];
-        for (var y = 0; y < totalPredictors.length; y++) {
-            var predictor = totalPredictors[y];
-            if (maxNumber == null || toReturn.length < maxNumber) {
-                var displayPredictor = {
-                  name: predictor.Name,
-                  power: predictor.UncertaintyCoefficient,
-                  size: 1,
-                  color: categoryColor
-                };
-                toReturn.push(displayPredictor);
-            } else {
-                break;
-            }
-        }
-        return toReturn;
-        
-    };
-    
-    this.CalculateAttributeSize = function (attributeList) {
-        if (attributeList == null || attributeList.length === 0) {
-            return null;
-        }
-        var numLargeCategories = Math.round(attributeList.length * 0.16);
-        var numMediumCategories = Math.round(attributeList.length * 0.32);
-        
-        for (var i = 0; i < attributeList.length; i++) {
-            var attribute = attributeList[i];
-            if (numLargeCategories > 0) {
-                attribute.size = 6.55;
-                numLargeCategories--;
-            } else if (numMediumCategories > 0) {
-                attribute.size = 2.56;
-                numMediumCategories--;
-            } else {
-                attribute.size = 1;
-            }
-        }
-    };
-    
-    this.FormatDataForChart = function (modelSummary) {
-        if (modelSummary == null || modelSummary.Predictors == null || modelSummary.Predictors.length === 0) {
-            return null;
-        }
-        
-        // First sort all predictors by UncertaintyCoefficient
-        modelSummary.Predictors = modelSummary.Predictors.sort(this.SortByPredictivePower);
-        
-        // Then pull all unique categories
-        var topCategories = [];
-        var topCategoryNames = [];
-        var category;
-        for (var i = 0; i < modelSummary.Predictors.length; i++) {
-            var predictor = modelSummary.Predictors[i];
-            if (!StringUtility.IsEmptyString(predictor.Category) &&  topCategoryNames.indexOf(predictor.Category) === -1 && topCategoryNames.length < 8) {
-                topCategoryNames.push(predictor.Category);
-                category = {
-                    name: predictor.Category,
-                    power: predictor.UncertaintyCoefficient,
-                    size: 1, // This doesn't matter because the inner ring takes on the saze of the outer
-                    color: this.GetColorByCategory(predictor.Category),
-                    children: []
-                };
-                topCategories.push(category);
-            }
-        }
-        
-        //And finally calculate the size based on predictive power
-        var attributesPerCategory = topCategories.length >= 7 ? 3 : 5;
-        var numLargeCategories = Math.round((topCategories.length * attributesPerCategory) * 0.16);
-        var numMediumCategories = Math.round((topCategories.length * attributesPerCategory) * 0.32);
-        var totalAttributes = [];
-        for (var x = 0; x < topCategories.length; x++) {
-            category = topCategories[x];
-            category.children = this.GetAttributesByCategory(modelSummary.Predictors, category.name, category.color, attributesPerCategory);
-            for (var y = 0; y < category.children.length; y++) {
-                totalAttributes.push(category.children[y]);
-            }
-        }
-        
-        totalAttributes.Predictors = totalAttributes.sort(this.SortByPredictivePower);
-        
-        for (var z = 0; z < totalAttributes.length; z++) {
-            var attribute = totalAttributes[z];
-            if (numLargeCategories > 0) {
-                attribute.size = 6.55;
-                numLargeCategories--;
-            } else if (numMediumCategories > 0) {
-                attribute.size = 2.56;
-                numMediumCategories--;
-            } else {
-                attribute.size = 1;
-            }
-            
-        }
-        
-        var toReturn = {
-            name: "root",
-            size : 1,
-            color: "#FFFFFF",
-            attributesPerCategory: attributesPerCategory,
-            children: topCategories
-        };
-        
-        return toReturn;
-    };
- })
 
 .controller('TopPredictorWidgetController', function ($scope, $element, ResourceUtility, WidgetFrameworkService, TopPredictorService) {
     
@@ -252,14 +30,16 @@ angular.module('mainApp.appCommon.widgets.TopPredictorWidget', [
     var chartTitle2 = ResourceUtility.getString("TOP_PREDICTORS_CHART_TITLE_2");
     
     // Get Internal category list
-    var internalCategoryObj = TopPredictorService.GetCategoryListByType(chartData.children, "Internal", data.Predictors);
+    var internalCategoryObj = TopPredictorService.GetNumberOfAttributesByCategory(chartData.children, "Internal", data.Predictors);
     $scope.internalPredictorTotal = internalCategoryObj.total + " " + ResourceUtility.getString("TOP_PREDICTORS_INTERNAL_TITLE");
     $scope.internalCategories = internalCategoryObj.categories;
+    $scope.showInternalCategories = internalCategoryObj.total > 0;
     
     // Get External category list
-    var externalCategoryObj = TopPredictorService.GetCategoryListByType(chartData.children, "External", data.Predictors);
+    var externalCategoryObj = TopPredictorService.GetNumberOfAttributesByCategory(chartData.children, "External", data.Predictors);
     $scope.externalPredictorTotal = externalCategoryObj.total + " " + ResourceUtility.getString("TOP_PREDICTORS_EXTERNAL_TITLE");
     $scope.externalCategories = externalCategoryObj.categories;
+    $scope.showExternalCategories = externalCategoryObj.total > 0;
     
     // Calculate total
     var totalAttributes = internalCategoryObj.total + externalCategoryObj.total;
