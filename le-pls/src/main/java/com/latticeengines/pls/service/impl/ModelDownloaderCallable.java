ffa5c73ae608b378cccab786fb06e77f490cfc07
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2015-02-21 19:23:52 +0000

    Add more logging.

--- le-pls/src/main/java/com/latticeengines/pls/service/impl/ModelDownloaderCallable.java
+++ le-pls/src/main/java/com/latticeengines/pls/service/impl/ModelDownloaderCallable.java
@@ -79,6 +79,7 @@ public class ModelDownloaderCallable implements Callable<Boolean> {
                 summary.setTenant(tenant);
 
                 if (!set.contains(summary.getId())) {
+                    log.info(String.format("Creating model summary with id %s.", summary.getId()));
                     modelSummaryEntityMgr.create(summary);
                     foundFilesToDownload = true;
                 }
