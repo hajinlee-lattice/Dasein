edc3ad4da4be2626cd3277ea4df75968db5ae331
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2015-02-24 16:01:23 +0000

    Reformat.

--- le-domain/src/main/java/com/latticeengines/domain/exposed/pls/ModelSummaryStatus.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/pls/ModelSummaryStatus.java
@@ -8,7 +8,9 @@ import com.fasterxml.jackson.annotation.JsonValue;
 public enum ModelSummaryStatus {
     
     // new status has to be at the end
-    ACTIVE(0), INACTIVE(1), DELETED(2);
+    ACTIVE(0), //
+    INACTIVE(1), //
+    DELETED(2);
     
     private ModelSummaryStatus(int statusId) {
         this.statusId = statusId;
