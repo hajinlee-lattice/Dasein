b1f5eab05f5e989b757d2ccb5703ba5f22eb0a77
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2015-02-25 22:16:32 +0000

    modified based on Ron's comment

---
+++ le-pls/src/main/java/com/latticeengines/pls/mbean/TimeStampContainer.java
@@ -0,0 +1,18 @@
+package com.latticeengines.pls.mbean;
+
+import org.apache.commons.net.ntp.TimeStamp;
+import org.springframework.stereotype.Component;
+
+@Component("timeStampContainer")
+public class TimeStampContainer {
+
+    private TimeStamp ts = TimeStamp.getCurrentTime();
+
+    public void setTimeStamp(){
+       ts = TimeStamp.getCurrentTime();
+    }
+
+    public TimeStamp getTimeStamp(){
+       return this.ts;
+    }
+}
