3b6a8a8de9f03c72250d51486392eac51a8c7591
pierce <pierce@479f5059-62a3-4c39-9342-9852e3a68435>
2015-02-25 18:06:59 +0000

    PLS-317 - Adding in finishing touches to the Top and Bottom Sample lead grids

--- le-pls/src/main/webapp/app/AppCommon/widgets/simpleGridWidget/SimpleGridWidget.js
+++ le-pls/src/main/webapp/app/AppCommon/widgets/simpleGridWidget/SimpleGridWidget.js
@@ -19,16 +19,40 @@ angular.module('mainApp.appCommon.widgets.SimpleGridWidget', [
         return;
     }
     
-    
+    // Build up the list of column headings
     $scope.columnTitles = [];
-    $scope.columns = [];
     for (var i = 0; i < widgetConfig.Columns.length; i++) {
         var column = widgetConfig.Columns[i];
         $scope.columnTitles.push(ResourceUtility.getString(column.TitleString));
-        $scope.columns.push(column.PropertyName);
     }
     
-    $scope.columnData = data[widgetConfig.DataProvider];
+    // Build up the list of rows and columns
+    var gridBody = $('.js-simple-grid-rows', $element);
+    var dataProvider = data[widgetConfig.DataProvider];
+    for (var y = 0; y < dataProvider.length; y++) {
+        var gridRow = $('<tr></tr>');
+        gridBody.append(gridRow);
+        var rowData = dataProvider[y];
+        
+        for (var x = 0; x < widgetConfig.Columns.length; x++) {
+            var columnMetaData = widgetConfig.Columns[x];
+            var gridColumn = $('<td></td>');
+            var columnData = rowData[columnMetaData.PropertyName];
+            
+            switch (columnMetaData.DisplayType) {
+                case "Boolean":
+                    if (columnData === true) {
+                        columnData = '<i class="fa fa-check"></i>';
+                    } else {
+                        columnData = '';
+                    }
+                    break;
+            }
+            gridColumn.html(columnData);
+            gridRow.append(gridColumn);
+        }
+    }
+    
     
 })
 
