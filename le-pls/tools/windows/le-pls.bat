9c9e28df001c5c846f51bf4da4cb9d462b641c8d
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-02-27 07:00:46 +0000

    populating data using ModelSummaryResourceTestNG

--- le-pls/tools/windows/le-pls.bat
+++ le-pls/tools/windows/le-pls.bat
@@ -2,7 +2,6 @@ echo off
 set cmd=%1
 
 IF /i [%cd:~-6%] NEQ [le-pls] (
-    echo Pleas run this batch in the "ledp\le-pls" directory as "tools\windows\le-pls.bat {command}"
     goto eof
 )
 
@@ -29,6 +28,16 @@ if "%cmd%"=="compile" (
         -DPLS_PROPDIR=conf\env\dev-windows ^
         -DargLine="" ^
         clean verify
+
+) else if "%cmd%"=="popdata" (
+
+    echo populate testing data using shared GlobalAuth ...
+
+    mvn -Pfunctional -Pgenerate ^
+        -Djavax.net.ssl.trustStore=certificates\%certificate% ^
+        -DPLS_PROPDIR=conf\env\dev-windows ^
+        -DargLine="" ^
+        -Dtest=*ModelSummaryResourceTestNG
 
 ) else if "%cmd%"=="run" (
 
@@ -62,21 +71,40 @@ if "%cmd%"=="compile" (
         -DargLine="" ^
         clean verify
 
+) else if "%cmd%"=="popdata-local" (
+
+    echo populate testing data using localhost GlobalAuth ...
+
+    mvn -Pfunctional -Pgenerate ^
+        -Djavax.net.ssl.trustStore=certificates\localhost.jks ^
+        -DPLS_PROPDIR=conf\env\dev-windows-localGA ^
+        -Dcertificate.name=localhost.jks ^
+        -Dglobalauth.base.url=https://localhost/Bard ^
+        -DargLine="" ^
+        -Dtest=*ModelSummaryResourceTestNG
+
 ) else if "%cmd%"=="run-local" (
 
     echo running clean verify using localhost GlobalAuth ...
 
     mvn -Pfunctional ^
         -Djavax.net.ssl.trustStore=certificates\localhost.jks ^
+        -DargLine="" ^
         -DPLS_PROPDIR=conf\env\dev-windows-localGA ^
         jetty:run
 
 ) else (
 
     echo unknown command: %cmd%
-    echo in ledp/le-pls directory run: tools\windows\le-pls.bat {command}
-    echo currently available {command} are: compile, verify, run, compile-local, verify-local, run-local
 
 )
 
 :eof
+
+echo in "ledp/le-pls" directory run "tools\windows\le-pls.bat {command}"
+echo currently available {command} are:
+echo    1. compile: compile using remote GlobalAuth, a fast way of checking GlobalAuth
+echo    2. verify: full functional test, using remote GlobalAuth
+echo    3. popdata: populate testing data (faster than "verify"), using remote GlobalAuth
+echo    4. run: jetty:run using remote GlobalAuth
+echo    5. compile-local, verify-local, popdata-local, run-local: same as above, but using localhost GlobalAuth
