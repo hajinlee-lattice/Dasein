87568b7c0634c3843bb37e5c42c8cc19aae2f276
lming <lming@479f5059-62a3-4c39-9342-9852e3a68435>
2015-02-27 06:17:14 +0000

    fix the tests

--- le-pls/src/test/java/com/latticeengines/pls/service/impl/DataFileProviderServiceTestNG.java
+++ le-pls/src/test/java/com/latticeengines/pls/service/impl/DataFileProviderServiceTestNG.java
@@ -5,8 +5,6 @@ import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNotNull;
 
 import java.net.URL;
 import java.util.List;
@@ -15,7 +13,6 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
@@ -31,23 +28,15 @@ import com.latticeengines.domain.exposed.exception.LedpCode;
 import com.latticeengines.domain.exposed.exception.LedpException;
 import com.latticeengines.domain.exposed.pls.ModelSummary;
 import com.latticeengines.domain.exposed.security.Tenant;
-import com.latticeengines.domain.exposed.security.Ticket;
 import com.latticeengines.pls.entitymanager.ModelSummaryEntityMgr;
 import com.latticeengines.pls.entitymanager.TenantEntityMgr;
 import com.latticeengines.pls.functionalframework.PlsFunctionalTestNGBase;
-import com.latticeengines.pls.globalauth.authentication.impl.GlobalAuthenticationServiceImpl;
-import com.latticeengines.pls.security.GrantedRight;
 import com.latticeengines.pls.service.DataFileProviderService;
 
 public class DataFileProviderServiceTestNG extends PlsFunctionalTestNGBase {
 
     @SuppressWarnings("unused")
     private static final Log log = LogFactory.getLog(DataFileProviderServiceTestNG.class);
-
-    private Ticket ticket = null;
-
-    @Autowired
-    private GlobalAuthenticationServiceImpl globalAuthenticationService;
 
     @Autowired
     private TenantEntityMgr tenantEntityMgr;
@@ -64,22 +53,33 @@ public class DataFileProviderServiceTestNG extends PlsFunctionalTestNGBase {
     @Autowired
     private DataFileProviderService dataFileProviderService;
 
+    private String modelId;
+
     @BeforeClass(groups = { "functional" })
     public void setup() throws Exception {
-        ticket = globalAuthenticationService.authenticateUser("admin", DigestUtils.sha256Hex("admin"));
-        assertEquals(ticket.getTenants().size(), 2);
-        assertNotNull(ticket);
-        String tenant1 = ticket.getTenants().get(0).getId();
-        String tenant2 = ticket.getTenants().get(1).getId();
-        grantRight(GrantedRight.VIEW_PLS_CONFIGURATION, tenant1, "admin");
-        grantRight(GrantedRight.VIEW_PLS_CONFIGURATION, tenant2, "admin");
 
-        setupDb(tenant1, tenant2);
+        setupDb("TENANT1", "TENANT2");
 
-        String dir = modelingServiceHdfsBaseDir
-                + "/BD2_ADEDTBDd69264296nJ26263627n12/models/Q_PLS_Modeling_BD2_ADEDTBDd69264296nJ26263627n12/8e3a9d8c-3bc1-4d21-9c91-0af28afc5c9a/1423547416066_0001/";
+        Tenant tenant;
+        List<Tenant> tenants = tenantEntityMgr.findAll();
+        String tenantName = tenants.get(0).getName();
+        if (tenantName.equals("TENANT2")) {
+            tenant = tenants.get(0);
+        } else {
+            tenant = tenants.get(1);
+        }
+
+        setupSecurityContext(tenant);
+
+        List<ModelSummary> summaries = modelSummaryEntityMgr.findAllValid();
+        ModelSummary summary = summaries.get(0);
+
+        String tokens[] = summary.getLookupId().split("\\|");
+        String dir = modelingServiceHdfsBaseDir + "/" + tokens[0] + "/models/" + tokens[1] + "/" + tokens[2] + "/container_01/";
         URL modelSummaryUrl = ClassLoader
                 .getSystemResource("com/latticeengines/pls/functionalframework/modelsummary-eloqua.json");
+
+        modelId = summary.getId();
 
         HdfsUtils.mkdir(yarnConfiguration, dir);
         HdfsUtils.mkdir(yarnConfiguration, dir + "/enhancements");
@@ -92,32 +92,15 @@ public class DataFileProviderServiceTestNG extends PlsFunctionalTestNGBase {
 
     }
 
-    @Test(groups = { "functional" }, dataProvider = "dataFileProvider", enabled = false)
+    @Test(groups = { "functional" }, dataProvider = "dataFileProvider", enabled = true)
     public void downloadFile(final String mimeType, final String filter) {
-
-        Tenant tenant;
-        List<Tenant> tenants = tenantEntityMgr.findAll();
-        if (tenants.get(0).getName().contains("BD_ADEDTBDd70064499nF26263627n1")) {
-            tenant = tenants.get(0);
-        } else {
-            tenant = tenants.get(1);
-        }
-        setupSecurityContext(tenant);
-
-        List<ModelSummary> summaries = modelSummaryEntityMgr.findAllValid();
-        ModelSummary summary;
-        if (summaries.get(0).getLookupId().contains("BD2_ADEDTBDd69264296nJ26263627n12")) {
-            summary = summaries.get(0);
-        } else {
-            summary = summaries.get(1);
-        }
 
         HttpServletRequest request = mock(HttpServletRequest.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
         ServletOutputStream os = mock(ServletOutputStream.class);
         try {
             when(response.getOutputStream()).thenReturn(os);
-            dataFileProviderService.downloadFile(request, response, summary.getId(), mimeType, filter);
+            dataFileProviderService.downloadFile(request, response, modelId, mimeType, filter);
             verify(response).setHeader(eq("Content-Disposition"), anyString());
             verify(response).setContentType(mimeType);
 
@@ -127,32 +110,15 @@ public class DataFileProviderServiceTestNG extends PlsFunctionalTestNGBase {
         }
     }
 
-    @Test(groups = { "functional" }, dataProvider = "dataFileProviderNotFound", enabled = false)
+    @Test(groups = { "functional" }, dataProvider = "dataFileProviderNotFound", enabled = true)
     public void downloadFileNotFound(final String mimeType, final String filter) {
-
-        Tenant tenant;
-        List<Tenant> tenants = tenantEntityMgr.findAll();
-        if (tenants.get(0).getName().contains("BD_ADEDTBDd70064499nF26263627n1")) {
-            tenant = tenants.get(0);
-        } else {
-            tenant = tenants.get(1);
-        }
-        setupSecurityContext(tenant);
-
-        List<ModelSummary> summaries = modelSummaryEntityMgr.findAllValid();
-        ModelSummary summary;
-        if (summaries.get(0).getLookupId().contains("BD2_ADEDTBDd69264296nJ26263627n12")) {
-            summary = summaries.get(0);
-        } else {
-            summary = summaries.get(1);
-        }
 
         HttpServletRequest request = mock(HttpServletRequest.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
         ServletOutputStream os = mock(ServletOutputStream.class);
         try {
             when(response.getOutputStream()).thenReturn(os);
-            dataFileProviderService.downloadFile(request, response, summary.getId(), mimeType, filter);
+            dataFileProviderService.downloadFile(request, response, modelId, mimeType, filter);
 
         } catch (Exception ex) {
             Assert.assertTrue(ex instanceof LedpException);
