10dd522ac322bfec42c5670825a5492d99075efe
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-02-27 06:37:57 +0000

    read certificate.name from pom.xml

--- le-pls/tools/windows/le-pls.bat
+++ le-pls/tools/windows/le-pls.bat
@@ -1,35 +1,45 @@
 echo off
 set cmd=%1
 
-if "%1" == "compile" (
+IF /i [%cd:~-6%] NEQ [le-pls] (
+    echo Pleas run this batch in the "ledp\le-pls" directory as "tools\windows\le-pls.bat {command}"
+    goto eof
+)
+
+FOR /f "tokens=3 delims=><" %%A IN ('type pom.xml ^| find "certificate.name"') DO (
+    SET certificate=%%A & goto endLoop
+)
+:endLoop
+
+if "%cmd%"=="compile" (
 
     echo compiling using shared GlobalAuth ...
 
     mvn -Pfunctional -Pgenerate ^
-        -Djavax.net.ssl.trustStore=certificates\bodcdevvint209.dev.lattice.local.jks ^
+        -Djavax.net.ssl.trustStore=certificates\%certificate% ^
         -DPLS_PROPDIR=conf\env\dev-windows ^
         clean compile
 
-) else if "%1" == "verify" (
+) else if "%cmd%"=="verify" (
 
     echo running clean verify using shared GlobalAuth ...
 
     mvn -Pfunctional -Pgenerate ^
-        -Djavax.net.ssl.trustStore=certificates\bodcdevvint209.dev.lattice.local.jks ^
+        -Djavax.net.ssl.trustStore=certificates\%certificate% ^
         -DPLS_PROPDIR=conf\env\dev-windows ^
         -DargLine="" ^
         clean verify
 
-) else if "%1" == "run" (
+) else if "%cmd%"=="run" (
 
     echo running clean verify using shared GlobalAuth ...
 
     mvn -Pfunctional ^
-        -Djavax.net.ssl.trustStore=certificates\bodcdevvint209.dev.lattice.local.jks ^
+        -Djavax.net.ssl.trustStore=certificates\%certificate% ^
         -DPLS_PROPDIR=conf\env\dev-windows ^
         jetty:run
 
-) else if "%1" == "compile-local" (
+) else if "%cmd%"=="compile-local" (
 
     echo compiling using localhost GlobalAuth ...
 
@@ -40,7 +50,7 @@ if "%1" == "compile" (
         -Dglobalauth.base.url=https://localhost/Bard ^
         clean compile
 
-) else if "%1" == "verify-local" (
+) else if "%cmd%"=="verify-local" (
 
     echo running clean verify using localhost GlobalAuth ...
 
@@ -52,7 +62,7 @@ if "%1" == "compile" (
         -DargLine="" ^
         clean verify
 
-) else if "%1" == "run-local" (
+) else if "%cmd%"=="run-local" (
 
     echo running clean verify using localhost GlobalAuth ...
 
@@ -63,8 +73,10 @@ if "%1" == "compile" (
 
 ) else (
 
-    echo unknown command: %
+    echo unknown command: %cmd%
     echo in ledp/le-pls directory run: tools\windows\le-pls.bat {command}
     echo currently available {command} are: compile, verify, run, compile-local, verify-local, run-local
 
 )
+
+:eof
