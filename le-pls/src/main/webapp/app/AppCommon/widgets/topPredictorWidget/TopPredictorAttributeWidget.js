595b83d9208a912fddbe67a514c253126efdff7e
pierce <pierce@479f5059-62a3-4c39-9342-9852e3a68435>
2015-02-27 16:44:15 +0000

    Working on the hover chart

--- le-pls/src/main/webapp/app/AppCommon/widgets/topPredictorWidget/TopPredictorAttributeWidget.js
+++ le-pls/src/main/webapp/app/AppCommon/widgets/topPredictorWidget/TopPredictorAttributeWidget.js
@@ -24,9 +24,9 @@ angular.module('mainApp.appCommon.widgets.TopPredictorAttributeWidget', [
     var chart,
         width = 350,
         left_width = 140,
-        bar_height = 20,
+        bar_height = 24,
         height = bar_height * bucketNames.length,
-        gap = 2;
+        gap = 5;
     
     var x = d3.scale.linear()
         .domain([0, d3.max(liftValues)])
@@ -34,11 +34,11 @@ angular.module('mainApp.appCommon.widgets.TopPredictorAttributeWidget', [
     
     var y = d3.scale.ordinal()
         .domain(liftValues)
-        .rangeBands([0, height]);
+        .rangeBands([0, (bar_height + 2 * gap) * liftValues.length]);
     
     var nameY = d3.scale.ordinal()
             .domain(bucketNames)
-            .rangeBands([0, height]);
+            .rangeBands([0, (bar_height + 2 * gap) * bucketNames.length]);
     
     chart = d3.select("#attributeChart") 
       .append('svg')
@@ -47,15 +47,35 @@ angular.module('mainApp.appCommon.widgets.TopPredictorAttributeWidget', [
       .attr('height', (bar_height + gap * 2) * bucketNames.length + 30)
       .append("g")
       .attr("transform", "translate(10, 20)");
+      
+    chart.selectAll("line")
+        .data(x.ticks(d3.max(liftValues)))
+        .enter().append("line")
+        .attr("x1", function(d) { return x(d) + left_width; })
+        .attr("x2", function(d) { return x(d) + left_width; })
+        .attr("y1", 0)
+        .attr("y2", (bar_height + gap * 2) * bucketNames.length);
+
+    chart.selectAll(".rule")
+        .data(x.ticks(d3.max(liftValues)))
+        .enter().append("text")
+        .attr("class", "rule")
+        .attr("x", function(d) { return x(d) + left_width; })
+        .attr("y", 0)
+        .attr("dy", -6)
+        .attr("text-anchor", "middle")
+        .attr("font-size", 10)
+        .text(String);
     
     chart.selectAll("rect")
         .data(liftValues)
         .enter().append("rect")
         .attr("x", left_width)
-        .attr("y", y)
+        .attr("y", function(d) { return y(d) + gap; })
         .attr("width", x)
-        .attr("height", y.rangeBand())
-        .style("fill", data.color);
+        .attr("height", bar_height)
+        .style("fill", data.color)
+        .attr('opacity', 0.7);
    
     chart.selectAll("text.lift")
         .data(liftValues)
@@ -66,6 +86,7 @@ angular.module('mainApp.appCommon.widgets.TopPredictorAttributeWidget', [
         .attr("dy", ".36em")
         .attr("text-anchor", "end")
         .attr("class", "lift")
+        .style("fill", "black")
         .text(String);
   
     chart.selectAll("text.name")
