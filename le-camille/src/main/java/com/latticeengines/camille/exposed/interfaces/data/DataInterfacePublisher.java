5f0b29272290d498bce7cf1808643aeee733386c
bross <bross@479f5059-62a3-4c39-9342-9852e3a68435>
2015-03-02 15:24:31 +0000

    Refactor

--- le-camille/src/main/java/com/latticeengines/camille/exposed/interfaces/data/DataInterfacePublisher.java
+++ le-camille/src/main/java/com/latticeengines/camille/exposed/interfaces/data/DataInterfacePublisher.java
@@ -1,6 +1,5 @@
 package com.latticeengines.camille.exposed.interfaces.data;
 
-import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooDefs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,12 +21,7 @@ public class DataInterfacePublisher extends DataInterfaceBase {
     public void publish(Path localPath, Document doc) throws Exception {
         Camille c = CamilleEnvironment.getCamille();
         Path path = getBasePath().append(localPath);
-        try {
-            c.create(path, doc, ZooDefs.Ids.OPEN_ACL_UNSAFE);
-        } catch (KeeperException.NodeExistsException e) {
-            log.debug("Node already existed @ {}, forcing update", path);
-            c.set(path, doc, true);
-        }
+        c.upsert(path, doc, ZooDefs.Ids.OPEN_ACL_UNSAFE);
     }
 
     public void remove(Path localPath) throws Exception {
