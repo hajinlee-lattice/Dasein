3ee95e54e8aa62140f2778476276d7d1600d6bfd
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2015-03-04 17:40:44 +0000

    Add method for status code for ModelSummaryStatus.

--- le-domain/src/main/java/com/latticeengines/domain/exposed/pls/ModelSummaryStatus.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/pls/ModelSummaryStatus.java
@@ -1,5 +1,8 @@
 package com.latticeengines.domain.exposed.pls;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.commons.lang.StringUtils;
 
 import com.fasterxml.jackson.annotation.JsonValue;
@@ -7,22 +10,40 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum ModelSummaryStatus {
     
-    // new status has to be at the end
-    ACTIVE(0), //
-    INACTIVE(1), //
-    DELETED(2);
+    // New status has to be at the end
+    ACTIVE(0, "UpdateAsActive"), //
+    INACTIVE(1, "UpdateAsInactive"), //
+    DELETED(2, "UpdateAsDeleted");
     
-    private ModelSummaryStatus(int statusId) {
+    private ModelSummaryStatus(int statusId, String status) {
         this.statusId = statusId;
+        this.statusCode = status;
     }
     private int statusId;
+    private String statusCode;
     
     public int getStatusId() {
         return statusId;
     }
     
+    public String getStatusCode() {
+        return statusCode;
+    }
+    
+    private static Map<String, ModelSummaryStatus> statusCodeMap = new HashMap<>();
+    
+    static {
+        for (ModelSummaryStatus summaryStatus : values()) {
+            statusCodeMap.put(summaryStatus.getStatusCode(), summaryStatus);
+        }
+    }
+    
     @JsonValue
     public String getName() {
         return StringUtils.capitalize(super.name().toLowerCase());
+    }
+    
+    public static ModelSummaryStatus getByStatusCode(String statusCode) {
+        return statusCodeMap.get(statusCode);
     }
 }
