f28708c4b20ae7fef945b587eb3198d2587ac7df
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-03-09 21:38:22 +0000

    add Rights to UserUpdateData

--- le-domain/src/main/java/com/latticeengines/domain/exposed/pls/UserUpdateData.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/pls/UserUpdateData.java
@@ -2,10 +2,14 @@ package com.latticeengines.domain.exposed.pls;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.latticeengines.common.exposed.util.JsonUtils;
+import com.latticeengines.domain.exposed.security.EntityAccessRightsData;
+
+import java.util.Map;
 
 public class UserUpdateData {
     private String oldPassword;
     private String newPassword;
+    private Map<String, EntityAccessRightsData> rights;
 
     @JsonProperty("OldPassword")
     public String getOldPassword() {
@@ -25,6 +29,16 @@ public class UserUpdateData {
     @JsonProperty("NewPassword")
     public void setNewPassword(String newPassword) {
         this.newPassword = newPassword;
+    }
+
+    @JsonProperty("Rights")
+    public Map<String, EntityAccessRightsData> getRights() {
+        return rights;
+    }
+
+    @JsonProperty("Rights")
+    public void setRights(Map<String, EntityAccessRightsData> newPassword) {
+        this.rights = rights;
     }
 
     @Override
