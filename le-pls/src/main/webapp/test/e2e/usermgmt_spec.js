8068fb640a4f0168101b02fd4a29d4160d0cb9a4
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-03-12 02:23:23 +0000

    try to fix e2e tests

--- le-pls/src/main/webapp/test/e2e/usermgmt_spec.js
+++ le-pls/src/main/webapp/test/e2e/usermgmt_spec.js
@@ -22,15 +22,13 @@ describe('user management', function() {
     }
 
     it('should login as a non-admin user', function () {
-            loginPage.loginAsNonAdmin();
-    }, 60000);
-
-    it('should verify user management is invisible to non-admin users', function () {
-        // choose tenant
+        loginPage.loginAsNonAdmin();
         tenants.getTenantByIndex(params.tenantIndex).click();
         browser.waitForAngular();
         browser.driver.sleep(1000);
+    }, 60000);
 
+    it('should verify user management is invisible to non-admin users', function () {
         // check existence of Manage Users link
         userDropdown.getUserLink(params.nonAdminDisplayName).click().then(function(){
             expect(element(by.linkText('Manage Users')).isPresent()).toBe(false);
@@ -45,21 +43,24 @@ describe('user management', function() {
         });
     });
 
-    it('should verify user management is visible to admin users', function () {
-
+    it('should login as an admin users', function () {
         loginPage.loginAsAdmin();
 
         // choose tenant
         tenants.getTenantByIndex(params.tenantIndex).click();
         browser.waitForAngular();
         browser.driver.sleep(1000);
+    });
 
+    it('should see user management link', function () {
         // check existence of Manage Users link
         userDropdown.getUserLink(params.adminDisplayName).click();
         browser.waitForAngular();
         browser.driver.sleep(1000);
         expect(element(by.linkText('Manage Users')).isDisplayed()).toBe(true);
+    });
 
+    it('should see user management page', function () {
         // check existence of users table
         element(by.linkText('Manage Users')).click();
         browser.waitForAngular();
@@ -69,17 +70,17 @@ describe('user management', function() {
         element.all(by.repeater('user in data')).then(function(elements){
             numOfUsers = elements.length;
         });
+    });
 
-    }, 45000);
-
-    it('should verify canceling adding new user will not add user', function () {
+    it('should see add new user modal', function () {
         // popup add user
         userManagement.getAddNewUserButton().click();
         browser.waitForAngular();
         browser.driver.sleep(1000);
-
         expect(userManagement.getAddNewUserModal().isDisplayed()).toBe(true);
+    });
 
+    it('should be able to canceling by clicking cancel button', function () {
         var email = randomName() + '@e2e.com';
         element(by.model('user.FirstName')).sendKeys('E2E');
         element(by.model('user.LastName')).sendKeys('Tester');
@@ -93,7 +94,10 @@ describe('user management', function() {
 
         // check cancel by button
         expect(element.all(by.repeater('user in data')).count()).toEqual(numOfUsers);
+    });
 
+    it('should be able to canceling by clicking cross symbol', function () {
+        var email = randomName() + '@e2e.com';
         // add a user
         userManagement.getAddNewUserButton().click();
         browser.waitForAngular();
@@ -109,7 +113,7 @@ describe('user management', function() {
 
         // check cancel by cross symbol
         expect(element.all(by.repeater('user in data')).count()).toEqual(numOfUsers);
-    }, 60000);
+    });
 
     it('should verify create user', function () {
         // popup add user
