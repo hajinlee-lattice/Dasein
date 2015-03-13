a6b9a7e1abf2605341f74ee3bbcc931ca3de20de
rkalkert <rkalkert@479f5059-62a3-4c39-9342-9852e3a68435>
2015-03-13 17:31:16 +0000

    Spike: Threshold-Explorer tests.

---
+++ le-pls/src/main/webapp/app/AppCommon/test/unit/services/ThresholdExplorerServiceSpec.js
@@ -0,0 +1,196 @@
+'use strict';
+
+describe('ThresholdExplorerServiceSpec Tests', function () {
+    var thresholdExplorerService,
+        modelSummary,
+        thresholdData,
+        thresholdChartData,
+        thresholdDecileData,
+        thresholdExportData;
+
+    beforeEach(function () {
+        module('mainApp.appCommon.services.ThresholdExplorerService');
+        module('test.testData.TopPredictorTestDataService');
+
+        inject(['ThresholdExplorerService', 'TopPredictorTestDataService',
+            function (ThresholdExplorerService, TopPredictorTestDataService) {
+                thresholdExplorerService = ThresholdExplorerService;
+                modelSummary = TopPredictorTestDataService.GetSampleModelSummary();
+                thresholdData = ThresholdExplorerService.PrepareData(modelSummary);
+                thresholdChartData = thresholdData.ChartData;
+                thresholdDecileData = thresholdData.DecileData;
+                thresholdExportData = ThresholdExplorerService.PrepareExportData(modelSummary);
+            }
+        ]);
+    });
+
+    //==================================================
+    // ChartData Tests
+    //==================================================
+    describe('chart-data tests', function () {
+        it('chart-data should have 101 elements', function () {
+            expect(thresholdChartData.length).toEqual(101);
+        });
+
+        it('chart-data leads should increment by 1', function () {
+            for (i = 1; i < 101; i++) {
+                expect(thresholdChartData[i].leads).toEqual(i);
+            }
+        });
+
+        it('chart-data leads should start with 0,1', function () {
+            expect(thresholdChartData[0].leads).toEqual(0);
+            expect(thresholdChartData[1].leads).toEqual(1);
+        });
+
+        it('chart-data leads should end with 99,100', function () {
+            expect(thresholdChartData[99].leads).toEqual(99);
+            expect(thresholdChartData[100].leads).toEqual(100);
+        });
+
+        it('chart-data score should decrement by 1', function () {
+            expect(thresholdChartData[0].score).toEqual(0);
+            for (i = 1; i < 101; i++) {
+                expect(thresholdChartData[i].score).toEqual(101 - i);
+            }
+        });
+
+        it('chart-data score should start with 0,100', function () {
+            expect(thresholdChartData[0].score).toEqual(0);
+            expect(thresholdChartData[1].score).toEqual(100);
+        });
+
+        it('chart-data score should end with 2,1', function () {
+            expect(thresholdChartData[99].score).toEqual(2);
+            expect(thresholdChartData[100].score).toEqual(1);
+        });
+
+        it('chart-data conversions should increase', function () {
+            for (i = 1; i < 101; i++) {
+                expect(parseInt(thresholdChartData[i].conversions.toFixed(0)) >=
+                    parseInt(thresholdChartData[i - 1].conversions.toFixed(0))).toBe(true);
+            }
+        });
+
+        it('chart-data conversions should start with 0', function () {
+            expect(thresholdChartData[0].conversions).toEqual(0);
+        });
+
+        it('chart-data conversions should end with 100', function () {
+            expect(thresholdChartData[100].conversions).toEqual(100);
+        });
+
+        it('chart-data leftLift should start with 0', function () {
+            expect(thresholdChartData[0].leftLift).toEqual(0);
+        });
+
+        it('chart-data leftLift should end with 1', function () {
+            expect(thresholdChartData[100].leftLift).toEqual(1);
+        });
+
+        it('chart-data rightLift should start with 1', function () {
+            expect(thresholdChartData[0].rightLift).toEqual(1);
+        });
+
+        it('chart-data rightLift should end with 0', function () {
+            expect(thresholdChartData[100].rightLift).toEqual(0);
+        });
+    });
+
+    //==================================================
+    // DecileData Tests
+    //==================================================
+    describe('decile-data tests', function () {
+        it('decile-data should have 10 conversions', function () {
+            for (i = 1; i < 10; i++) {
+                expect(thresholdDecileData[0].hasOwnProperty("D" + String(i))).toBe(true);
+            }
+        });
+
+        it('decile-data should have 10 lifts', function () {
+            for (i = 1; i < 10; i++) {
+                expect(thresholdDecileData[1].hasOwnProperty("D" + String(i))).toBe(true);
+            }
+        });
+
+        it('decile-data conversions should increase', function () {
+            for (i = 1; i < 10; i++) {
+                expect(parseInt(thresholdDecileData[0]["D" + String(i + 1)].slice(0, -1)) >=
+                    parseInt(thresholdDecileData[0]["D" + String(i)].slice(0, -1))).toBe(true);
+            }
+        });
+    });
+
+    //==================================================
+    // ChartDecile Tests
+    //==================================================
+    describe('chart-decile tests', function () {
+        it('chart-decile conversions should match', function () {
+            for (i = 1; i < 10; i++) {
+                expect(thresholdChartData[10 * i].conversions.toFixed(0)).
+                    toEqual(thresholdDecileData[0]["D" + String(i)].slice(0, -1));
+            }
+        });
+
+        it('chart-decile leftLift should match', function () {
+            for (i = 1; i < 10; i++) {
+                expect(thresholdChartData[10 * i].leftLift.toFixed(2)).
+                    toEqual(thresholdDecileData[1]["D" + String(i)].slice(0, -1));
+            }
+        });
+    });
+
+    //==================================================
+    // Export Tests (Good Enough For Now)
+    //==================================================
+    describe('export tests', function () {
+        it('chart-export score should match', function () {
+            for (i = 1; i < 101; i++) {
+                expect(thresholdChartData[i].score).
+                    toEqual(thresholdExportData[i][0]);
+            }
+        });
+
+        it('chart-export leads should match', function () {
+            for (i = 1; i < 101; i++) {
+                expect(thresholdChartData[i].leads).
+                    toEqual(parseInt(thresholdExportData[i][1].slice(0, -1)));
+            }
+        });
+
+        it('chart-export conversions should match', function () {
+            for (i = 1; i < 101; i++) {
+                expect(thresholdChartData[i].conversions.toFixed(0)).
+                    toEqual(thresholdExportData[i][2].slice(0, -1));
+            }
+        });
+
+        it('chart-export leftLift should match', function () {
+            for (i = 1; i < 101; i++) {
+                expect(thresholdChartData[i].leftLift.toFixed(2)).
+                    toEqual(thresholdExportData[i][3]);
+            }
+        });
+
+        it('chart-export rightLift should match', function () {
+            for (i = 1; i < 101; i++) {
+                expect(thresholdChartData[i].rightLift.toFixed(2)).
+                    toEqual(thresholdExportData[i][4]);
+            }
+        });
+
+        it('segment-export count should match', function () {
+            for (i = 1; i < 101; i++) {
+                expect(modelSummary.Segmentations[0].Segments[i - 1].Count).
+                    toEqual(thresholdExportData[i][5]);
+            }
+        });
+
+        it('segment-export converted should match', function () {
+            for (i = 1; i < 101; i++) {
+                expect(modelSummary.Segmentations[0].Segments[i - 1].Converted).
+                    toEqual(thresholdExportData[i][6]);
+            }
+        });
+    });
+});
--- le-pls/src/main/webapp/test/e2e/thresholdexplorer_spec.js
+++ le-pls/src/main/webapp/test/e2e/thresholdexplorer_spec.js
@@ -3,14 +3,108 @@ describe('threshold explorer', function() {
     var params = browser.params;
 
     var loginPage = require('./po/login.po');
+    var tenants = require('./po/tenantselection.po');
     var modelList = require('./po/modellist.po');
     var modelTabs = require('./po/modeltabs.po');
+    var logoutPage = require('./po/logout.po');
 
     it('should validate functional threshold explorer chart', function () {
-        loginPage.loginAsNonAdmin();    
+        //==================================================
+        // Login
+        //==================================================
+        loginPage.loginAsNonAdmin();
 
-        // choose specific model
+        //==================================================
+        // Select Tenant
+        //==================================================
+        tenants.getTenantByIndex(params.tenantIndex).click();
+        browser.waitForAngular();
 
-    });   
+        //==================================================
+        // Select Model
+        //==================================================
+        modelList.getAnyModel().click();
+        browser.waitForAngular();
 
+        //==================================================
+        // Select Threshold Explorer Tab
+        //==================================================
+        modelTabs.getTabByIndex(1).click();
+        browser.waitForAngular();
+
+        //==================================================
+        // Check Threshold Explorer Chart
+        //==================================================
+        expect(element(by.id('thresholdExplorerChart')).isDisplayed()).toBe(true);
+
+        //==================================================
+        // Get Default Values
+        //==================================================
+        chartConversions = element(by.css(".xtext")).getText();
+        chartLeftLift = element(by.css(".ltext")).getText();
+        chartLeads = element(by.css(".lytext")).getText();
+        chartScore = element(by.css(".rytext")).getText();
+
+        //==================================================
+        // Check Some Labels
+        //==================================================
+        rightLiftLabel = element(by.css(".rltext"));
+        expect(rightLiftLabel.isDisplayed()).toBe(true);
+        expect(rightLiftLabel.getText()).toEqual("LIFT");
+
+        leftLiftLabel = element(by.css(".lltext"));
+        expect(leftLiftLabel.isDisplayed()).toBe(true);
+        expect(leftLiftLabel.getText()).toEqual("LIFT");
+
+        convLabel = element(by.css(".xltext"));
+        expect(convLabel.isDisplayed()).toBe(true);
+        expect(convLabel.getText()).toEqual("% CONV");
+
+        scoreLabel = element(by.css(".ryltext"));
+        expect(scoreLabel.isDisplayed()).toBe(true);
+        expect(scoreLabel.getText()).toEqual("SCORE");
+
+        leadsLabel = element(by.css(".lyltext"));
+        expect(leadsLabel.isDisplayed()).toBe(true);
+        expect(leadsLabel.getText()).toEqual("TOP");
+
+        //==================================================
+        // Check Default Leads
+        //==================================================
+        expect(chartLeads).toEqual("20%");
+
+        //==================================================
+        // Check Default Score
+        //==================================================
+        expect(chartScore).toEqual("> 80");
+
+        //==================================================
+        // Check DecileGrid
+        //==================================================
+        tab = element(by.id("modelDetailsExplorerTab"));
+        body = tab.element(by.tagName("tbody"));
+        rows = body.all(by.tagName("tr"));
+        expect(rows.count()).toEqual(2);
+
+        //==================================================
+        // Check Default/Max Conversions
+        //==================================================
+        tds = rows.get(0).all(by.tagName("td"));
+        expect(tds.get(0).getText()).toEqual("% TOTAL CONVERSIONS");
+        expect(tds.get(2).getText()).toEqual(chartConversions);
+        expect(tds.get(10).getText()).toEqual("100%");
+
+        //==================================================
+        // Check Default/Max Lift
+        //==================================================
+        tds = rows.get(1).all(by.tagName("td"));
+        expect(tds.get(0).getText()).toEqual("LIFT");
+        expect(tds.get(2).getText()).toEqual(chartLeftLift);
+        expect(tds.get(10).getText()).toEqual("1.00x");
+
+        //==================================================
+        // Logout
+        //==================================================
+        logoutPage.logoutAsNonAdmin();
+    });
 });
