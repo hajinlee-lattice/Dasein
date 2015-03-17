f596b4ea3b18ef55106962d92c8d65e4bdf660a9
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2015-03-17 02:55:50 +0000

    Add UserRegistrationWithTenant.

---
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/security/UserRegistrationWithTenant.java
@@ -0,0 +1,30 @@
+package com.latticeengines.domain.exposed.security;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class UserRegistrationWithTenant {
+
+    private UserRegistration userRegistration;
+    private String tenant;
+    
+    @JsonProperty("UserRegistration")
+    public UserRegistration getUserRegistration() {
+        return userRegistration;
+    }
+    
+    @JsonProperty("UserRegistration")
+    public void setUserRegistration(UserRegistration userRegistration) {
+        this.userRegistration = userRegistration;
+    }
+
+    @JsonProperty("Tenant")
+    public String getTenant() {
+        return tenant;
+    }
+
+    @JsonProperty("Tenant")
+    public void setTenant(String tenant) {
+        this.tenant = tenant;
+    }
+    
+}
