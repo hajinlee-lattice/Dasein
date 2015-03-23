a858caaa7872314955105b55b89e8c9bf4644670
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-03-23 19:18:21 +0000

    add EmailSettings

---
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/security/EmailSettings.java
@@ -0,0 +1,72 @@
+package com.latticeengines.domain.exposed.security;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.latticeengines.common.exposed.util.JsonUtils;
+
+public class EmailSettings {
+
+    private String from;
+    private String password;
+    private int port;
+    private String server;
+    private boolean useSSL;
+    private String username;
+
+    @JsonProperty("From")
+    public String getFrom() { return from; }
+
+    @JsonProperty("From")
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    @JsonProperty("Password")
+    public String getPassword() { return password; }
+
+    @JsonProperty("Password")
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @JsonProperty("Port")
+    public int getPort() { return port; }
+
+    @JsonProperty("Port")
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    @JsonProperty("Server")
+    public String getServer() {
+        return server;
+    }
+
+    @JsonProperty("Server")
+    public void setServer(String server) {
+        this.server = server;
+    }
+
+    @JsonProperty("UseSSL")
+    public boolean isUseSSL() { return useSSL; }
+
+    @JsonProperty("UseSSL")
+    public void setUseSSL(boolean useSSL) {
+        this.useSSL = useSSL;
+    }
+
+    @JsonProperty("Username")
+    public String getUsername() {
+        return username;
+    }
+
+    @JsonProperty("Username")
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return JsonUtils.serialize(this);
+    }
+
+}
