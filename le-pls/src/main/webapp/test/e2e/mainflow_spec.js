e22f662f476dfebb40404aa83789d25c6c65b089
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-03-27 19:30:26 +0000

    add hidden link check to mainflow

--- le-pls/src/main/webapp/test/e2e/mainflow_spec.js
+++ le-pls/src/main/webapp/test/e2e/mainflow_spec.js
@@ -14,60 +14,68 @@ describe('smoketest main flow of app', function() {
         loginPage.loginAsAdmin();
 
         // choose tenant
-        console.log('tenant index:' + params.tenantIndex); 
+        console.log('tenant index:' + params.tenantIndex);
         tenants.selectTenantByIndex(params.tenantIndex);
-        //browser.waitForAngular();
-        //browser.driver.sleep(1000);
 
         // choose any model
         expect(element(by.css('.js-top-predictor-donut')).isPresent()).toBe(false);
         modelList.getAnyModel().click();
         browser.waitForAngular();
 
-        // default to predictor tab, check existence of donut         
+        // default to predictor tab, check existence of donut
         expect(element(by.css('.js-top-predictor-donut')).isDisplayed()).toBe(true);
         browser.driver.sleep(1000);
 
         // check existence of threshold explorer
-        expect(element(by.id('thresholdExplorerChart')).isDisplayed()).toBe(false); 
+        expect(element(by.id('thresholdExplorerChart')).isDisplayed()).toBe(false);
         modelTabs.getTabByIndex(1).click();
         browser.waitForAngular();
         browser.driver.sleep(1000);
-        expect(element(by.id('thresholdExplorerChart')).isDisplayed()).toBe(true);        
+        expect(element(by.id('thresholdExplorerChart')).isDisplayed()).toBe(true);
 
         // toggle details on, off, on again
-        modelDetails.toggleDetails();        
-        expect(element(by.id('moreDataPoints')).isDisplayed()).toBe(true); 
         modelDetails.toggleDetails();
-        expect(element(by.id('moreDataPoints')).isDisplayed()).toBe(false); 
+        expect(element(by.id('moreDataPoints')).isDisplayed()).toBe(true);
         modelDetails.toggleDetails();
-        expect(element(by.id('moreDataPoints')).isDisplayed()).toBe(true); 
+        expect(element(by.id('moreDataPoints')).isDisplayed()).toBe(false);
+        modelDetails.toggleDetails();
+        expect(element(by.id('moreDataPoints')).isDisplayed()).toBe(true);
 
         // check existence of sample leads tab
-        expect(element(by.id('topSampleTable')).isDisplayed()).toBe(false); 
+        expect(element(by.id('topSampleTable')).isDisplayed()).toBe(false);
         modelTabs.getTabByIndex(2).click();
         browser.waitForAngular();
         browser.driver.sleep(1000);
-        expect(element(by.id('topSampleTable')).isDisplayed()).toBe(true);  
+        expect(element(by.id('topSampleTable')).isDisplayed()).toBe(true);
 
         // check existence of hidden admin page
-        expect(element(by.id('adminInfoContainer')).isPresent()).toBe(false);  
+        expect(element(by.id('adminInfoContainer')).isPresent()).toBe(false);
         element(by.id('adminLink')).click();
         browser.waitForAngular();
-        browser.driver.sleep(1000);    
-        expect(element(by.id('adminInfoContainer')).isDisplayed()).toBe(true);  
-        
+        browser.driver.sleep(1000);
+        expect(element(by.id('adminInfoContainer')).isDisplayed()).toBe(true);
+
         // logout
-        logoutPage.logoutAsAdmin();     
+        logoutPage.logoutAsAdmin();
     }, 60000);
 
     it('should validate model back button', function () {
         // element(by.css('.back-button')).click();
-    });  
+    });
 
     it('should verify nonadmins do not see admin functionality', function () {    
-      //  loginPage.loginAsNonAdmin();
+        loginPage.loginAsNonAdmin();
+        tenants.selectTenantByIndex(params.tenantIndex);
 
-     //   logoutPage.logoutAsNonAdmin();
+        element.all(by.css('a.model')).first().click();
+        browser.driver.wait(function(){
+            return element(by.css('a.back-button')).isDisplayed();
+        }, 10000, 'tabs list should appear with in 10 sec.');
+
+        element(by.linkText('SAMPLE LEADS')).click();
+
+        expect(element(by.linkText('Admin')).isPresent()).toBe(false);
+
+        logoutPage.logoutAsNonAdmin();
     });  
 });
