dd5833f71b7ce3008394aeb052940ce33b5539e3
wbaumann <wbaumann@479f5059-62a3-4c39-9342-9852e3a68435>
2015-03-31 20:17:32 +0000

    Fix merge issues and remove the database destination, because there are no longer plans to integrate the Marketo Adapter with legacy PLS.

--- marketoadapter/src/main/java/com/latticeengines/marketoadapter/RecordDispatcher.java
+++ marketoadapter/src/main/java/com/latticeengines/marketoadapter/RecordDispatcher.java
@@ -11,48 +11,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class RecordDispatcher
-{
+public class RecordDispatcher {
     @PostConstruct
-    public void Initialize()
-    {
+    public void Initialize() {
         destinations = new HashMap<String, RecordDestination>();
         destinations.put("Console", consoleDestination);
-        destinations.put("Database", databaseDestination);
         destinations.put("Skald", skaldDestination);
     }
-    
-    public String receiveRecord(String key, Map<String, Object> record)
-    {
+
+    public String receiveRecord(String key, Map<String, Object> record) {
         // TODO: Add rate limiting.
-        
+
         CustomerSettings settings = manager.getCustomerSettingsByKey(key);
-        if (settings == null)
-        {
+        if (settings == null) {
             log.warn("Received a request with unknown key: " + key);
             throw new RuntimeException("Lattice Key does not match a known customer");
         }
 
         RecordDestination destination = destinations.get(settings.destination);
-        if (destination == null)
-        {
+        if (destination == null) {
             log.warn("Received a request with unknown destination: " + settings.destination);
             throw new RuntimeException("Encountered an internal configuration error");
         }
-        
+
         return destination.receiveRecord(settings.customerSpace, record);
     }
-    
+
     @Autowired
     private SettingsManager manager;
     @Autowired
     private ConsoleDestination consoleDestination;
     @Autowired
-    private DatabaseDestination databaseDestination;
-    @Autowired
     private SkaldDestination skaldDestination;
-    
+
     private Map<String, RecordDestination> destinations;
-    
+
     private static final Log log = LogFactory.getLog(RecordDispatcher.class);
 }
