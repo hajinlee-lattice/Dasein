93b079eb313177e539106ee013712c4e390db043
bross <bross@479f5059-62a3-4c39-9342-9852e3a68435>
2015-04-02 18:58:32 +0000

    Missing in previous commit.

---
+++ le-camille/src/main/java/com/latticeengines/camille/exposed/config/bootstrap/BootstrapStateUtil.java
@@ -0,0 +1,62 @@
+package com.latticeengines.camille.exposed.config.bootstrap;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+
+import com.latticeengines.camille.exposed.Camille;
+import com.latticeengines.camille.exposed.CamilleEnvironment;
+import com.latticeengines.camille.exposed.CamilleTransaction;
+import com.latticeengines.camille.exposed.paths.PathConstants;
+import com.latticeengines.camille.exposed.util.DocumentUtils;
+import com.latticeengines.domain.exposed.camille.Document;
+import com.latticeengines.domain.exposed.camille.Path;
+import com.latticeengines.domain.exposed.camille.bootstrap.BootstrapState;
+
+public class BootstrapStateUtil {
+
+    public static void initializeState(Path serviceDirectory, CamilleTransaction transaction, BootstrapState state) {
+        Path path = serviceDirectory.append(PathConstants.BOOTSTRAP_STATE_FILE);
+        transaction.create(path, DocumentUtils.toRawDocument(state), ZooDefs.Ids.OPEN_ACL_UNSAFE);
+    }
+
+    public static void initializeState(Path serviceDirectory, BootstrapState state) throws Exception {
+        Path path = serviceDirectory.append(PathConstants.BOOTSTRAP_STATE_FILE);
+        Camille camille = CamilleEnvironment.getCamille();
+        try {
+            camille.create(path, DocumentUtils.toRawDocument(state), ZooDefs.Ids.OPEN_ACL_UNSAFE);
+        } catch (Exception e) {
+            throw new Exception(String.format("Unexpected failure attempting to initialize state at path %s", path,
+                    state), e);
+        }
+    }
+
+    public static BootstrapState getState(Path serviceDirectory) throws Exception {
+        Path path = serviceDirectory.append(PathConstants.BOOTSTRAP_STATE_FILE);
+        try {
+            Camille camille = CamilleEnvironment.getCamille();
+            Document raw = camille.get(path);
+            return DocumentUtils.toTypesafeDocument(raw, BootstrapState.class);
+        } catch (KeeperException.NoNodeException e) {
+            return BootstrapState.createInitialState();
+        } catch (Exception e) {
+            throw new Exception(String.format("Unexpected failure attempting to retrieve bootstrap state at path %s",
+                    path), e);
+        }
+    }
+
+    public static void setState(Path serviceDirectory, BootstrapState state) throws Exception {
+        Path path = serviceDirectory.append(PathConstants.BOOTSTRAP_STATE_FILE);
+        Camille camille = CamilleEnvironment.getCamille();
+        try {
+            camille.upsert(path, DocumentUtils.toRawDocument(state), ZooDefs.Ids.OPEN_ACL_UNSAFE);
+        } catch (Exception e) {
+            throw new Exception(String.format("Unexpected failure attempting to set state at path %s to state %s",
+                    path, state), e);
+        }
+    }
+
+    public static void setState(Path serviceDirectory, CamilleTransaction transaction, BootstrapState state) {
+        Path path = serviceDirectory.append(PathConstants.BOOTSTRAP_STATE_FILE);
+        transaction.set(path, DocumentUtils.toRawDocument(state));
+    }
+}
---
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/camille/VersionedDocument.java
@@ -0,0 +1,34 @@
+package com.latticeengines.domain.exposed.camille;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public abstract class VersionedDocument {
+
+    public VersionedDocument() {
+        version = -1;
+    }
+
+    public VersionedDocument(int version) {
+        this.version = version;
+    }
+
+    public boolean documentVersionSpecified() {
+        return version >= 0;
+    }
+
+    @JsonIgnore
+    public int getDocumentVersion() {
+        return version;
+    }
+
+    @JsonIgnore
+    public void setDocumentVersion(int version) {
+        if (version < -1) {
+            throw new IllegalArgumentException("Version must either be -1 (unspecified), or greater or equal to 0");
+        }
+        this.version = version;
+    }
+
+    @JsonIgnore
+    private int version;
+}
