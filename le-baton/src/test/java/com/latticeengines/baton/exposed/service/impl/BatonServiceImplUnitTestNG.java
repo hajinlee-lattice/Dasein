7c7b9aa2887a56ac0663b0850f90bc60e76b77f4
bross <bross@479f5059-62a3-4c39-9342-9852e3a68435>
2015-04-07 16:17:46 +0000

    Fix unit test so that it uses a test zk server.

--- le-baton/src/test/java/com/latticeengines/baton/exposed/service/impl/BatonServiceImplUnitTestNG.java
+++ le-baton/src/test/java/com/latticeengines/baton/exposed/service/impl/BatonServiceImplUnitTestNG.java
@@ -2,6 +2,7 @@ package com.latticeengines.baton.exposed.service.impl;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNotNull;
 
 import java.util.AbstractMap;
 import java.util.List;
@@ -11,27 +12,20 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.latticeengines.baton.exposed.service.BatonService;
-import com.latticeengines.camille.exposed.CamilleConfiguration;
-import com.latticeengines.camille.exposed.CamilleEnvironment;
-import com.latticeengines.camille.exposed.CamilleEnvironment.Mode;
 import com.latticeengines.camille.exposed.lifecycle.ContractLifecycleManager;
+import com.latticeengines.camille.exposed.util.CamilleTestEnvironment;
 import com.latticeengines.domain.exposed.camille.CustomerSpace;
 import com.latticeengines.domain.exposed.camille.lifecycle.CustomerSpaceInfo;
 import com.latticeengines.domain.exposed.camille.lifecycle.CustomerSpaceProperties;
 import com.latticeengines.domain.exposed.camille.lifecycle.TenantInfo;
 
 public class BatonServiceImplUnitTestNG {
-    
+
     private BatonService batonService = new BatonServiceImpl();
 
-    
     @BeforeClass(groups = "unit")
     public void setup() throws Exception {
-        CamilleConfiguration config = new CamilleConfiguration();
-        config.setConnectionString("localhost:2181");
-        config.setPodId("ignored");
-
-        CamilleEnvironment.start(Mode.BOOTSTRAP, config);
+        CamilleTestEnvironment.start();
 
         if (ContractLifecycleManager.exists("CONTRACT1")) {
             ContractLifecycleManager.delete("CONTRACT1");
@@ -40,36 +34,41 @@ public class BatonServiceImplUnitTestNG {
         props.description = "Test tenant";
         props.displayName = "Tenant for testing";
         CustomerSpaceInfo info = new CustomerSpaceInfo(props, "");
-        
+
         batonService.createTenant("CONTRACT1", "TENANT1", CustomerSpace.BACKWARDS_COMPATIBLE_SPACE_ID, info);
     }
-    
+
     @AfterClass(groups = "unit")
     public void tearDown() throws Exception {
-        batonService.deleteTenant("CONTRACT1", "TENANT1");
-        List<AbstractMap.SimpleEntry<String, TenantInfo>> tenants = batonService.getTenants(null);
-        assertEquals(tenants.size(), 0);
+        CamilleTestEnvironment.stop();
     }
 
     @Test(groups = "unit")
     public void getTenantsWithContractId() {
-        assertSingleTenantProperties(batonService.getTenants("CONTRACT1"));
+        assertTenantProperties(batonService.getTenants("CONTRACT1"));
     }
 
     @Test(groups = "unit")
     public void getTenantsWithNoContractId() {
-        assertSingleTenantProperties(batonService.getTenants(null));
+        assertTenantProperties(batonService.getTenants(null));
     }
-    
+
     @Test(groups = "unit")
     public void deleteNonExistingTenant() {
         assertFalse(batonService.deleteTenant("CONTRACT1", "xyz"));
     }
-    
-    private void assertSingleTenantProperties(List<AbstractMap.SimpleEntry<String, TenantInfo>> tenants) {
-        assertEquals(tenants.size(), 1);
-        assertEquals(tenants.get(0).getKey(), "TENANT1");
-        assertEquals(tenants.get(0).getValue().properties.description, "Test tenant");
-        assertEquals(tenants.get(0).getValue().properties.displayName, "Tenant for testing");
+
+    private void assertTenantProperties(List<AbstractMap.SimpleEntry<String, TenantInfo>> tenants) {
+        AbstractMap.SimpleEntry<String, TenantInfo> tenant = null;
+        for (AbstractMap.SimpleEntry<String, TenantInfo> possible : tenants) {
+            if (possible.getKey().equals("TENANT1")) {
+                tenant = possible;
+                break;
+            }
+        }
+        assertNotNull(tenant);
+        assertEquals(tenant.getKey(), "TENANT1");
+        assertEquals(tenant.getValue().properties.description, "Test tenant");
+        assertEquals(tenant.getValue().properties.displayName, "Tenant for testing");
     }
 }
