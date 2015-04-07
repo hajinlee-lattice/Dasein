ce55d369eaf7dff15bb2c4498ee1e3239b48f1d4
bross <bross@479f5059-62a3-4c39-9342-9852e3a68435>
2015-04-07 15:50:35 +0000

    Update comments.

--- le-camille/src/main/java/com/latticeengines/camille/exposed/messaging/MessageConsumer.java
+++ le-camille/src/main/java/com/latticeengines/camille/exposed/messaging/MessageConsumer.java
@@ -1,6 +1,10 @@
 package com.latticeengines.camille.exposed.messaging;
 
 public abstract class MessageConsumer<T> {
-    // If throws, will log and continue
+    /**
+     * If throws, framework code will log and message will be consumed as usual.
+     * 
+     * @param message
+     */
     public abstract void consume(T message);
 }
