a800719d1928e99a1ad371ec46186c5314b35b1f
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-04-08 16:44:02 +0000

    update deleteDemoUser.sh

--- le-pls/bin/deleteDemoUser.sh
+++ le-pls/bin/deleteDemoUser.sh
@@ -1,30 +1,35 @@
 #!/usr/bin/python
 
 from optparse import OptionParser
-from urllib import urlencode
+import urllib
 import subprocess
 import sys
+import httplib
 
-request = '{"Tenant": "%s", "UserRegistration": {"User":{"Username":"%s","Email":"%s","FirstName":"%s","LastName":"%s","PhoneNumber":"650-555-5555","Active":false,"Title":"DemoUser"},"Credentials":{"Username":"%s","Password":"%s"},"Validation":false}}'
+def deleteUser(params):
+    print 'Connecting to the host ...'
+    conn = httplib.HTTPConnection(params["host"], int(params["port"]))
+    conn.request('GET', "/")
+    response = conn.getresponse()
+    print response.status, response.reason
+    response.read()
 
-class UserDeployer(object):
-    
-    def __init__(self,  hostPortWithProtocol, tenant, username):
-        self.url = "%s/pls/admin/users?namepattern=%s&tenants=[\"%s\"]" % (hostPortWithProtocol, urlencode(username), tenant)
-    
-    def deleteUser(self):
-        cmd = ["curl", "--insecure", "-H", "Content-Type: application/json", "-H", \
-               "MagicAuthentication: Security through obscurity!", "-X", "DELETE", \
-               self.url]
-        print(subprocess.check_output(cmd))
+    print 'Deleting the user ...'
+    headers = {"MagicAuthentication": "Security through obscurity!"}
+    qpars = 'tenants=["' + params["tenant"] + '"]&namepattern=' + urllib.quote_plus(params["username"])
+    conn.request('DELETE', "/pls/internal/users?" + qpars, headers=headers)
+    response = conn.getresponse()
+    print response.status, response.reason
+    print response.read()
+
+    conn.close()
 
 if __name__ == "__main__":
     parser = OptionParser()
-    parser.add_option("-o", "--hostPortWithProtocol", dest="hostPortWithProtocol")
+    parser.add_option("-o", "--host", dest="host", default="app.lattice-engines.com", help='default = app.lattice-engines.com')
+    parser.add_option("-p", "--port", dest="port", default=80, help="default = 80")
     parser.add_option("-t", "--tenant", dest="tenant")
-    parser.add_option("-u", "--user", dest="user")
+    parser.add_option("-u", "--username", dest="username")	
     (options, _) = parser.parse_args()
  
-    params = options.__dict__
-    ud = UserDeployer(params["hostPortWithProtocol"], params["tenant"], params["user"])
-    ud.deleteUser()
+    deleteUser(options.__dict__)
