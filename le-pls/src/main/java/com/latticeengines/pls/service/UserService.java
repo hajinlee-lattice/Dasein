e5389ae35394980be32d3b0ca1041c6b926437a9
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-04-10 17:39:15 +0000

    add back UserService

---
+++ le-pls/src/main/java/com/latticeengines/pls/service/UserService.java
@@ -0,0 +1,32 @@
+package com.latticeengines.pls.service;
+
+import java.util.List;
+
+import com.latticeengines.domain.exposed.security.User;
+import com.latticeengines.domain.exposed.security.UserRegistrationWithTenant;
+import com.latticeengines.pls.security.AccessLevel;
+
+public interface UserService {
+
+    boolean addAdminUser(UserRegistrationWithTenant userRegistrationWithTenant);
+
+    boolean assignAccessLevel(AccessLevel accessLevel, String tenantId, String username);
+
+    boolean resignAccessLevel(String tenantId, String username);
+
+    AccessLevel getAccessLevel(String tenantId, String username);
+
+    AccessLevel getAccessLevel(List<String> rights);
+
+    boolean softDelete(String tenantId, String username);
+
+    boolean deleteUser(String tenantId, String username);
+
+    List<User> getUsers(String tenantId);
+
+    boolean isVisible(AccessLevel loginLevel, AccessLevel targetLevel);
+
+    boolean isSuperior(AccessLevel loginLevel, AccessLevel targetLevel);
+
+    boolean inTenant(String tenantId, String username);
+}
