ada81014a582f9ea31941520fedfb9e977df7279
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2015-04-11 04:22:30 +0000

    Add Segment domain object.

---
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/pls/Segment.java
@@ -0,0 +1,123 @@
+package com.latticeengines.domain.exposed.pls;
+
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.latticeengines.domain.exposed.dataplatform.HasName;
+import com.latticeengines.domain.exposed.dataplatform.HasPid;
+import com.latticeengines.domain.exposed.security.HasTenant;
+import com.latticeengines.domain.exposed.security.HasTenantId;
+import com.latticeengines.domain.exposed.security.Tenant;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Entity
+@Table(name = "SEGMENT", uniqueConstraints = { @UniqueConstraint(columnNames = { "MODEL_ID" }),
+        @UniqueConstraint(columnNames = { "NAME", "TENANT_ID" }) })
+@Filter(name = "tenantFilter", condition = "TENANT_ID = :tenantFilterId")
+public class Segment implements HasName, HasPid, HasTenant, HasTenantId {
+    
+    private String name;
+    private Long pid;
+    private Tenant tenant;
+    private Long tenantId;
+    private String modelId;
+    private Integer priority;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    @Basic(optional = false)
+    @Column(name = "PID", unique = true, nullable = false)
+    @Override
+    public Long getPid() {
+        return pid;
+    }
+
+    @Override
+    public void setPid(Long pid) {
+        this.pid = pid;
+    }
+
+    @Override
+    @JsonIgnore
+    public void setTenantId(Long tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    @Override
+    @JsonIgnore
+    @Column(name = "TENANT_ID", nullable = false)
+    public Long getTenantId() {
+        return tenantId;
+    }
+
+    @JsonProperty("Tenant")
+    @Override
+    public void setTenant(Tenant tenant) {
+        this.tenant = tenant;
+        setTenantId(tenant.getPid());
+    }
+
+    @Override
+    @JsonProperty("Tenant")
+    @ManyToOne(cascade = { CascadeType.MERGE }, fetch = FetchType.EAGER)
+    @JoinColumn(name = "FK_TENANT_ID", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    public Tenant getTenant() {
+        return tenant;
+    }
+
+    @JsonProperty("Name")
+    @Override
+    @Column(name = "NAME", nullable = false)
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("Name")
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("ModelId")
+    @Column(name = "MODEL_ID", nullable = false)
+    public String getModelId() {
+        return modelId;
+    }
+
+    @JsonProperty("ModelId")
+    public void setModelId(String modelId) {
+        this.modelId = modelId;
+    }
+
+    @JsonProperty("Priority")
+    @Column(name = "PRIORITY", nullable = false)
+    public Integer getPriority() {
+        return priority;
+    }
+
+    @JsonProperty("Priority")
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+
+}
