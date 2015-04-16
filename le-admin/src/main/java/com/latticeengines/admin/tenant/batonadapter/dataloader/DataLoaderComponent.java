8b1ea74427483abc1e97c978aa7e41bac6fd481a
lming <lming@479f5059-62a3-4c39-9342-9852e3a68435>
2015-04-16 22:10:35 +0000

    bard jams adapter

--- le-admin/src/main/java/com/latticeengines/admin/tenant/batonadapter/dataloader/DataLoaderComponent.java
+++ le-admin/src/main/java/com/latticeengines/admin/tenant/batonadapter/dataloader/DataLoaderComponent.java
@@ -1,9 +1,12 @@
 package com.latticeengines.admin.tenant.batonadapter.dataloader;
 
+import org.springframework.stereotype.Component;
+
 import com.latticeengines.admin.tenant.batonadapter.LatticeComponent;
 import com.latticeengines.domain.exposed.camille.bootstrap.CustomerSpaceServiceInstaller;
 import com.latticeengines.domain.exposed.camille.bootstrap.CustomerSpaceServiceUpgrader;
 
+@Component
 public class DataLoaderComponent extends LatticeComponent {
     
     private CustomerSpaceServiceInstaller installer = new DataLoaderInstaller();
