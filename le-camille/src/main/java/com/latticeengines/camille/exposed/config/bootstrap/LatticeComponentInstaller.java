45c694b89b64a1ba43eb816350f7505fcdc9a736
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-04-21 00:49:43 +0000

    fix Installer

--- le-camille/src/main/java/com/latticeengines/camille/exposed/config/bootstrap/LatticeComponentInstaller.java
+++ le-camille/src/main/java/com/latticeengines/camille/exposed/config/bootstrap/LatticeComponentInstaller.java
@@ -10,6 +10,7 @@ import com.latticeengines.domain.exposed.camille.CustomerSpace;
 import com.latticeengines.domain.exposed.camille.DocumentDirectory;
 import com.latticeengines.domain.exposed.camille.Path;
 import com.latticeengines.domain.exposed.camille.bootstrap.CustomerSpaceServiceInstaller;
+import com.latticeengines.domain.exposed.camille.lifecycle.CustomerSpaceInfo;
 import com.latticeengines.domain.exposed.camille.lifecycle.CustomerSpaceProperties;
 
 public abstract class LatticeComponentInstaller implements CustomerSpaceServiceInstaller {
@@ -25,18 +26,17 @@ public abstract class LatticeComponentInstaller implements CustomerSpaceServiceI
     // the true installation steps other than writing to Camille
     protected abstract void installCore(
             CustomerSpace space, String serviceName, int dataVersion,
-            CustomerSpaceProperties spaceProps, DocumentDirectory configDir);
+            CustomerSpaceInfo spaceInfo, DocumentDirectory configDir);
 
     @Override
     public DocumentDirectory install(CustomerSpace space, String serviceName, int dataVersion, Map<String, String> properties) {
         SerializableDocumentDirectory sDir = new SerializableDocumentDirectory(properties);
-        Map<String, String> residualProps = sDir.getOtherProperties();
         DocumentDirectory dir = sDir.getDocumentDirectory();
         dir.makePathsLocal();
 
         if (!dryrun) {
             // do the true installation
-            installCore(space, serviceName, dataVersion, new CustomerSpaceProperties(), dir);
+            installCore(space, serviceName, dataVersion, new CustomerSpaceInfo(), dir);
         }
 
         return dir;
