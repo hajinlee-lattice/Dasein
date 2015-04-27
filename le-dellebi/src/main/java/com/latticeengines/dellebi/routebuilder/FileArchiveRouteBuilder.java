97ad64a9aab9fcd195eb4b2444e1e83cf138149b
llu <llu@479f5059-62a3-4c39-9342-9852e3a68435>
2015-04-27 04:40:11 +0000

    Add alerts sending.

--- le-dellebi/src/main/java/com/latticeengines/dellebi/routebuilder/FileArchiveRouteBuilder.java
+++ le-dellebi/src/main/java/com/latticeengines/dellebi/routebuilder/FileArchiveRouteBuilder.java
@@ -4,7 +4,10 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+
+import com.latticeengines.dellebi.util.MailSender;
 
 public class FileArchiveRouteBuilder extends RouteBuilder {
 
@@ -14,6 +17,12 @@ public class FileArchiveRouteBuilder extends RouteBuilder {
     private String camelDataIncomePath;
     @Value("${dellebi.cameldataarchivepath}")
     private String camelDataArchivePath;
+    
+    @Value("${dellebi.mailreceivelist}")
+    private String mailReceiveList;
+    
+    @Autowired
+    private MailSender mailSender;
 
     public void configure() {
 
@@ -23,6 +32,8 @@ public class FileArchiveRouteBuilder extends RouteBuilder {
             .process(new Processor() {
             public void process(Exchange exchange) throws Exception {
                 LOGGER.info("Received Dell EBI file: " + exchange.getIn().getHeader("CamelFileName"));
+
+                mailSender.sendEmail(mailReceiveList,"New Dell EBI files arrive!","Received Dell EBI file: " + exchange.getIn().getHeader("CamelFileName"));
                 }
             })
             .multicast().stopOnException().to(camelDataArchivePath, "direct:files").end();
