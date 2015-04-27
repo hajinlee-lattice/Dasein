d13d24e53a516e75f5c51cb18037f0cbb2ffb92b
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-04-27 17:46:58 +0000

    remove unnecessary fields from TenantInfo

--- le-domain/src/main/java/com/latticeengines/domain/exposed/camille/lifecycle/TenantInfo.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/camille/lifecycle/TenantInfo.java
@@ -1,9 +1,5 @@
 package com.latticeengines.domain.exposed.camille.lifecycle;
 
-import java.util.List;
-
-import com.latticeengines.domain.exposed.camille.bootstrap.BootstrapState;
-
 public class TenantInfo {
     public TenantInfo(TenantProperties properties) {
         this.properties = properties;
@@ -13,8 +9,4 @@ public class TenantInfo {
     public TenantInfo() {}
 
     public TenantProperties properties;
-    public String contractId;
-    public BootstrapState bootstrapState;
-    public List<CustomerSpaceInfo> spaceInfoList;
-    public ContractInfo contractInfo;
 }
