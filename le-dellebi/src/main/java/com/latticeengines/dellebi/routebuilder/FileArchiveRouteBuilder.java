0700d46ce5cd2c5fe627ba1aae6260e2cd7f3c2f
llu <llu@479f5059-62a3-4c39-9342-9852e3a68435>
2015-04-29 10:45:18 +0000

    Add Spring.

--- le-dellebi/src/main/java/com/latticeengines/dellebi/routebuilder/FileArchiveRouteBuilder.java
+++ le-dellebi/src/main/java/com/latticeengines/dellebi/routebuilder/FileArchiveRouteBuilder.java
@@ -3,7 +3,8 @@ package com.latticeengines.dellebi.routebuilder;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.log4j.Logger;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
@@ -11,7 +12,7 @@ import com.latticeengines.dellebi.util.MailSender;
 
 public class FileArchiveRouteBuilder extends RouteBuilder {
 
-    private final static Logger LOGGER = Logger.getLogger(FileArchiveRouteBuilder.class);
+    private static final Log log = LogFactory.getLog(FileArchiveRouteBuilder.class);
 
     @Value("${dellebi.cameldataincomepath}")
     private String camelDataIncomePath;
@@ -31,14 +32,14 @@ public class FileArchiveRouteBuilder extends RouteBuilder {
             .filter(header("CamelFileName").regex("tgt_(ship_to_addr_lattice|order_detail|lat_order_summary|warranty_global|quote_trans_global).+(.zip)"))
             .process(new Processor() {
             public void process(Exchange exchange) throws Exception {
-                LOGGER.info("Received Dell EBI file: " + exchange.getIn().getHeader("CamelFileName"));
+                log.info("Received Dell EBI file: " + exchange.getIn().getHeader("CamelFileName"));
 
                 mailSender.sendEmail(mailReceiveList,"New Dell EBI files arrive!","Received Dell EBI file: " + exchange.getIn().getHeader("CamelFileName"));
                 }
             })
             .multicast().stopOnException().to(camelDataArchivePath, "direct:files").end();
         }catch(Exception e){
-            LOGGER.info("File archiving failed!");
+            log.info("File archiving failed!");
         }
     }
 }
