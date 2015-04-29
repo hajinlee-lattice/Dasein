bdad7e04676e0689479e76f5e4cbc383d6ae6e0b
llu <llu@479f5059-62a3-4c39-9342-9852e3a68435>
2015-04-29 10:36:54 +0000

    Change DailyJob so it can be called by le-job Quartz.

--- le-dellebi/src/main/java/com/latticeengines/dellebi/dataprocess/DailyJob.java
+++ le-dellebi/src/main/java/com/latticeengines/dellebi/dataprocess/DailyJob.java
@@ -1,51 +1,45 @@
 package com.latticeengines.dellebi.dataprocess;
 
-import org.apache.log4j.Logger;
-import org.quartz.Job;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import com.latticeengines.dellebi.flowdef.DailyFlow;
-import com.latticeengines.dellebi.util.MailSender;
 
-public class DailyJob implements Job {
+public class DailyJob  extends QuartzJobBean  {
 	
-	@Value("${dellebi.mailreceivelist}")
-    private String mailReceiveList;
+	private DailyFlow dailyFlow;
 
-    public final static Logger LOGGER = Logger.getLogger(DailyJob.class);
+    private static final Log log = LogFactory.getLog(DailyJob.class);
 
     private void process() {
 
-        // Get files and unzip them.
-        LOGGER.info("Start to process files from inbox.");
-        ApplicationContext springContext = new ClassPathXmlApplicationContext("dellebi-properties-context.xml",
-                "dellebi-context.xml");       
+    	log.info("Start to process files from inbox.");      
         
         // Process data using Cascading 
         // Note Camel scans input folder intermittently so it archive and unzip data continuously if there's new data incomes.
         // But Cascading does it's job once when it be called.  So there's only time Casadading processes data and new data incomes after that,
         // Cascading processes it next day.
-        DailyFlow dailFlow = springContext.getBean("dailyFlow", DailyFlow.class);
-        dailFlow.doDailyFlow(springContext);
+    	dailyFlow.doDailyFlow();
         
         //Send notifications to inform Daily refresh is done.
-        LOGGER.info("EBI daily refresh just finished successfully.");
-        MailSender mailSender = springContext.getBean("mail", MailSender.class);
-        mailSender.sendEmail(mailReceiveList,"Dell EBI daily refresh just finished!","Dell EBI daily refresh just finished!");
+        log.info("EBI daily refresh just finished successfully.");
     }
 
     @Override
-    public void execute(JobExecutionContext context) throws JobExecutionException {
+    public void executeInternal(JobExecutionContext context) throws JobExecutionException {
 
         try {
             process();
         } catch (Exception e) {
-            LOGGER.error("Failed to execute daily job!", e);
+        	log.error("Failed to execute daily job!", e);
         }
 
+    }
+    
+    public void setDailyFlow(DailyFlow dailyFlow){
+        this.dailyFlow = dailyFlow;
     }
 }
