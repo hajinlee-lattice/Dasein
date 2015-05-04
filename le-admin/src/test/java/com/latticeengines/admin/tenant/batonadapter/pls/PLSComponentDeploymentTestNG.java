93b55abb7a82d0c375c079b0cd67c3758946f3bb
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-05-04 22:42:11 +0000

    create two test tenants in deployment tests

--- le-admin/src/test/java/com/latticeengines/admin/tenant/batonadapter/pls/PLSComponentDeploymentTestNG.java
+++ le-admin/src/test/java/com/latticeengines/admin/tenant/batonadapter/pls/PLSComponentDeploymentTestNG.java
@@ -1,3 +1,34 @@
 package com.latticeengines.admin.tenant.batonadapter.pls;
 
-public class PLSComponentDeploymentTestNG extends PLSComponentTestNG {}
+import org.testng.annotations.Test;
+
+import com.latticeengines.domain.exposed.camille.DocumentDirectory;
+import com.latticeengines.domain.exposed.camille.Path;
+
+public class PLSComponentDeploymentTestNG extends PLSComponentTestNG {
+
+    @Test(groups = "deployment", dependsOnMethods = "testInstallation")
+    public void installTestTenants() {
+        createTestTenant("Tenant1", "Tenant 1");
+        createTestTenant("Tenant2", "Tenant 2");
+    }
+
+    private void createTestTenant(String tenantId, String tenantName) {
+        String testAdminUsername = "bnguyen@lattice-engines.com";
+        String testAdminPassword = "admin";
+
+        DocumentDirectory confDir = batonService.getDefaultConfiguration(getServiceName());
+        confDir.makePathsLocal();
+
+        // modify the default config
+        DocumentDirectory.Node node = confDir.get(new Path("/AdminEmails"));
+        node.getDocument().setData("[\"" + testAdminUsername + "\"]");
+
+        node = confDir.get(new Path("/TenantName"));
+        node.getDocument().setData(TestContractId + " " + tenantName);
+
+        // send to bootstrapper message queue
+        super.bootstrap(confDir);
+    }
+
+}
