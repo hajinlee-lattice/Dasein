bb347994c06d864d76226e1c3afb73abb2a807ea
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-05-05 23:54:30 +0000

    provision a common testing tenant in deployment test

--- le-admin/src/test/java/com/latticeengines/admin/tenant/batonadapter/pls/PLSComponentDeploymentTestNG.java
+++ le-admin/src/test/java/com/latticeengines/admin/tenant/batonadapter/pls/PLSComponentDeploymentTestNG.java
@@ -11,6 +11,7 @@ public class PLSComponentDeploymentTestNG extends PLSComponentTestNG {
     public void installTestTenants() {
         createTestTenant("Tenant1", "Tenant 1");
         createTestTenant("Tenant2", "Tenant 2");
+        createCommonTenant();
     }
 
     private void createTestTenant(String tenantId, String tenantName) {
@@ -35,6 +36,36 @@ public class PLSComponentDeploymentTestNG extends PLSComponentTestNG {
 
         node = confDir.get(new Path("/TenantName"));
         node.getDocument().setData(TestContractId + " " + tenantName);
+
+        // send to bootstrapper message queue
+        super.bootstrap(contractId, tenantId, serviceName, confDir);
+    }
+
+    private void createCommonTenant() {
+
+        String contractId = "CommonTestContract";
+        String tenantId = "TestTenant";
+        loginAD();
+
+        try {
+            deleteTenant(contractId, tenantId);
+        } catch (Exception e) {
+            //ignore
+        }
+        createTenant(contractId, tenantId, false);
+
+        String testAdminUsername = "bnguyen@lattice-engines.com";
+        String testAdminPassword = "admin";
+
+        DocumentDirectory confDir = batonService.getDefaultConfiguration(getServiceName());
+        confDir.makePathsLocal();
+
+        // modify the default config
+        DocumentDirectory.Node node = confDir.get(new Path("/AdminEmails"));
+        node.getDocument().setData("[\"" + testAdminUsername + "\"]");
+
+        node = confDir.get(new Path("/TenantName"));
+        node.getDocument().setData("Lattice Internal Test Tenant");
 
         // send to bootstrapper message queue
         super.bootstrap(contractId, tenantId, serviceName, confDir);
