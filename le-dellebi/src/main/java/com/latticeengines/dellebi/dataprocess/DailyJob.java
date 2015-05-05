fa0d9c2829c7f04abd76ee4083111f7fce05794f
lyan <lyan@479f5059-62a3-4c39-9342-9852e3a68435>
2015-05-05 12:40:52 +0000

    Revert to 66902 since the Jekins build failure.

--- le-dellebi/src/main/java/com/latticeengines/dellebi/dataprocess/DailyJob.java
+++ le-dellebi/src/main/java/com/latticeengines/dellebi/dataprocess/DailyJob.java
@@ -7,27 +7,23 @@ import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import com.latticeengines.dellebi.flowdef.DailyFlow;
-import com.latticeengines.dellebi.util.SqoopDataService;
 
 public class DailyJob  extends QuartzJobBean  {
 	
 	private DailyFlow dailyFlow;
-	private SqoopDataService sqoopDataService;
 
     private static final Log log = LogFactory.getLog(DailyJob.class);
 
     private void process() {
 
     	log.info("Start to process files from inbox.");      
-    	        
+        
         // Process data using Cascading 
         // Note Camel scans input folder intermittently so it archive and unzip data continuously if there's new data incomes.
         // But Cascading does it's job once when it be called.  So there's only time Casadading processes data and new data incomes after that,
         // Cascading processes it next day.
     	dailyFlow.doDailyFlow();
-    	
-    	sqoopDataService.export();
-            	  	
+        
         //Send notifications to inform Daily refresh is done.
         log.info("EBI daily refresh just finished successfully.");
     }
@@ -45,9 +41,5 @@ public class DailyJob  extends QuartzJobBean  {
     
     public void setDailyFlow(DailyFlow dailyFlow){
         this.dailyFlow = dailyFlow;
-    }
-    
-    public void setSqoopDataService(SqoopDataService sqoopDataService){
-        this.sqoopDataService = sqoopDataService;
     }
 }
