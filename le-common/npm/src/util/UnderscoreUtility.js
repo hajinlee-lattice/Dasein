cd8b520944868b842aab0f3f39fd58ec91d21bf8
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-05-06 04:52:12 +0000

    put underscore in closure

--- le-common/npm/src/util/UnderscoreUtility.js
+++ le-common/npm/src/util/UnderscoreUtility.js
@@ -1,5 +1,8 @@
-angular.module('le.common.util.UnderscoreUtility', [
-])
-.factory('_', function() {
-    return window._; // assumes underscore has already been loaded on the page
-});
+(function(){
+    var app = angular.module('le.common.util.UnderscoreUtility', [
+    ]);
+
+    app.factory('_', function() {
+        return window._; // assumes underscore has already been loaded on the page
+    });
+}).call();
