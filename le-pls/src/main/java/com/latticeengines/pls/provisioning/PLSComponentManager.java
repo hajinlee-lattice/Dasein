ab86c5f8b6b5d5400501468a1b7338b5957c7dc1
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-05-07 16:38:28 +0000

    use tenant level displayname as tenant name

--- le-pls/src/main/java/com/latticeengines/pls/provisioning/PLSComponentManager.java
+++ le-pls/src/main/java/com/latticeengines/pls/provisioning/PLSComponentManager.java
@@ -1,16 +1,27 @@
 package com.latticeengines.pls.provisioning;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.latticeengines.domain.exposed.admin.TenantDocument;
+import com.latticeengines.domain.exposed.camille.CustomerSpace;
+import com.latticeengines.domain.exposed.camille.DocumentDirectory;
+import com.latticeengines.domain.exposed.exception.LedpCode;
+import com.latticeengines.domain.exposed.exception.LedpException;
 import com.latticeengines.domain.exposed.security.Credentials;
 import com.latticeengines.domain.exposed.security.Tenant;
 import com.latticeengines.domain.exposed.security.User;
 import com.latticeengines.domain.exposed.security.UserRegistration;
+import com.latticeengines.pls.service.TenantConfigService;
 import com.latticeengines.pls.service.TenantService;
 import com.latticeengines.security.exposed.AccessLevel;
 import com.latticeengines.security.exposed.service.UserService;
@@ -25,6 +36,9 @@ public class PLSComponentManager {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private TenantConfigService tenantConfigService;
 
     public void provisionTenant(Tenant tenant, List<String> adminEmails) {
 
@@ -52,6 +66,48 @@ public class PLSComponentManager {
             userService.assignAccessLevel(AccessLevel.SUPER_ADMIN, tenant.getId(), user.getUsername());
         }
 
+    }
+
+    public void provisionTenant(CustomerSpace space, DocumentDirectory configDir) {
+        // get tenant information
+        String camilleTenantId = space.getTenantId();
+        String camilleContractId = space.getContractId();
+        String camilleSpaceId = space.getSpaceId();
+
+        String PLSTenantId = String.format("%s.%s.%s", camilleContractId, camilleTenantId, camilleSpaceId);
+        LOGGER.info(String.format("Provisioning tenant %s", PLSTenantId));
+
+        TenantDocument tenantDocument = tenantConfigService.getTenantDocument(PLSTenantId);
+        String tenantName = tenantDocument.getTenantInfo().properties.displayName;
+
+        String emailListInJson;
+        List<String> adminEmails = new ArrayList<>();
+        try {
+            emailListInJson = configDir.get("/AdminEmails").getDocument().getData();
+        } catch (NullPointerException e) {
+            throw new LedpException(LedpCode.LEDP_18028, "Cannot parse input configuration", e);
+        }
+
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String unescaped = StringEscapeUtils.unescapeJava(emailListInJson);
+            JsonNode aNode = mapper.readTree(unescaped);
+            if (!aNode.isArray()) {
+                throw new IOException("AdminEmails suppose to be a list of strings");
+            }
+            for (JsonNode node : aNode) {
+                adminEmails.add(node.asText());
+            }
+        } catch (IOException e) {
+            throw new LedpException(LedpCode.LEDP_18028,
+                    "Cannot parse AdminEmails to a list of valid emails: " + emailListInJson, e);
+        }
+
+        Tenant tenant = new Tenant();
+        tenant.setId(PLSTenantId);
+        tenant.setName(tenantName);
+
+        provisionTenant(tenant, adminEmails);
     }
 
     public void discardTenant(Tenant tenant) {
--- le-pls/src/main/java/com/latticeengines/pls/provisioning/PLSInstaller.java
+++ le-pls/src/main/java/com/latticeengines/pls/provisioning/PLSInstaller.java
@@ -1,25 +1,10 @@
 package com.latticeengines.pls.provisioning;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.commons.lang3.StringEscapeUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.latticeengines.camille.exposed.config.bootstrap.LatticeComponentInstaller;
 import com.latticeengines.domain.exposed.camille.CustomerSpace;
 import com.latticeengines.domain.exposed.camille.DocumentDirectory;
-import com.latticeengines.domain.exposed.exception.LedpCode;
-import com.latticeengines.domain.exposed.exception.LedpException;
-import com.latticeengines.domain.exposed.security.Tenant;
 
 public class PLSInstaller extends LatticeComponentInstaller {
-
-    private static final Log LOGGER = LogFactory.getLog(PLSInstaller.class);
 
     private PLSComponentManager componentManager;
 
@@ -28,44 +13,7 @@ public class PLSInstaller extends LatticeComponentInstaller {
     @Override
     public void installCore(CustomerSpace space, String serviceName, int dataVersion, DocumentDirectory configDir) {
         if (!serviceName.equals(PLSComponent.componentName)) { return; }
-
-        // get tenant information
-        String camilleTenantId = space.getTenantId();
-        String camilleContractId = space.getContractId();
-        String camilleSpaceId = space.getSpaceId();
-
-        String PLSTenantId = String.format("%s.%s.%s", camilleContractId, camilleTenantId, camilleSpaceId);
-        LOGGER.info(String.format("Provisioning tenant %s", PLSTenantId));
-
-        String tenantName, emailListInJson;
-        List<String> adminEmails = new ArrayList<>();
-        try {
-            emailListInJson = configDir.get("/AdminEmails").getDocument().getData();
-            tenantName = configDir.get("/TenantName").getDocument().getData();
-        } catch (NullPointerException e) {
-            throw new LedpException(LedpCode.LEDP_18028, "Cannot parse input configuration", e);
-        }
-
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            String unescaped = StringEscapeUtils.unescapeJava(emailListInJson);
-            JsonNode aNode = mapper.readTree(unescaped);
-            if (!aNode.isArray()) {
-                throw new IOException("AdminEmails suppose to be a list of strings");
-            }
-            for (JsonNode node : aNode) {
-                adminEmails.add(node.asText());
-            }
-        } catch (IOException e) {
-            throw new LedpException(LedpCode.LEDP_18028,
-                    "Cannot parse AdminEmails to a list of valid emails: " + emailListInJson, e);
-        }
-
-        Tenant tenant = new Tenant();
-        tenant.setId(PLSTenantId);
-        tenant.setName(tenantName);
-
-        componentManager.provisionTenant(tenant, adminEmails);
+        componentManager.provisionTenant(space, configDir);
     }
 
     public void setComponentManager(PLSComponentManager manager) {
