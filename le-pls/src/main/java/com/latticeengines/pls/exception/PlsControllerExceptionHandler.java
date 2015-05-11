0953dfdc0ec9289224dc788dbb923cae17de373d
lming <lming@479f5059-62a3-4c39-9342-9852e3a68435>
2015-05-11 19:32:54 +0000

    merge pagerduty into le-monitor

--- le-pls/src/main/java/com/latticeengines/pls/exception/PlsControllerExceptionHandler.java
+++ le-pls/src/main/java/com/latticeengines/pls/exception/PlsControllerExceptionHandler.java
@@ -3,11 +3,12 @@ package com.latticeengines.pls.exception;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.Resource;
+
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.http.message.BasicNameValuePair;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,15 +19,16 @@ import org.springframework.web.servlet.view.json.MappingJacksonJsonView;
 import com.google.common.collect.ImmutableMap;
 import com.latticeengines.domain.exposed.exception.LedpCode;
 import com.latticeengines.domain.exposed.exception.LedpException;
-import com.latticeengines.pls.service.AlertService;
+import com.latticeengines.monitor.exposed.alerts.service.AlertService;
 import com.latticeengines.security.exposed.exception.SecurityControllerExceptionHandler;
 
 @ControllerAdvice
 @SuppressWarnings("deprecation")
 public class PlsControllerExceptionHandler extends SecurityControllerExceptionHandler {
     private static final Log log = LogFactory.getLog(PlsControllerExceptionHandler.class);
-    @Autowired
-    private AlertService alertService2;
+
+    @Resource(name = "plsAlertService")
+    private AlertService alertService;
 
     public PlsControllerExceptionHandler() {
     }
@@ -35,7 +37,8 @@ public class PlsControllerExceptionHandler extends SecurityControllerExceptionHa
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ModelAndView handleException(LedpException e) {
         MappingJacksonJsonView jsonView = new MappingJacksonJsonView();
-        String stackTrace = e.getCause() != null ? ExceptionUtils.getFullStackTrace(e.getCause()) : ExceptionUtils.getStackTrace(e);
+        String stackTrace = e.getCause() != null ? ExceptionUtils.getFullStackTrace(e.getCause()) : ExceptionUtils
+                .getStackTrace(e);
         log.error(e.getCode() + "\n" + stackTrace);
         return new ModelAndView(jsonView, ImmutableMap.of("errorCode", e.getCode().name(), //
                 "errorMsg", e.getMessage()));
@@ -50,7 +53,7 @@ public class PlsControllerExceptionHandler extends SecurityControllerExceptionHa
 
         List<BasicNameValuePair> details = new ArrayList<>();
         details.add(new BasicNameValuePair("stackTrace", stackTrace));
-        alertService2.triggerCriticalEvent(e.getMessage(), details);
+        alertService.triggerCriticalEvent(e.getMessage(), null, details);
 
         return new ModelAndView(jsonView, ImmutableMap.of("errorCode", LedpCode.LEDP_00002.name(), //
                 "errorMsg", stackTrace));
