59a37631548234a77f9ac4b27731c2939dde1af3
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-05-12 00:26:45 +0000

    use space configuration to get topology

--- le-pls/src/main/java/com/latticeengines/pls/service/impl/TenantConfigServiceImpl.java
+++ le-pls/src/main/java/com/latticeengines/pls/service/impl/TenantConfigServiceImpl.java
@@ -6,10 +6,8 @@ import org.springframework.stereotype.Component;
 
 import com.latticeengines.baton.exposed.service.BatonService;
 import com.latticeengines.baton.exposed.service.impl.BatonServiceImpl;
-import com.latticeengines.camille.exposed.lifecycle.SpaceLifecycleManager;
 import com.latticeengines.domain.exposed.admin.TenantDocument;
 import com.latticeengines.domain.exposed.camille.CustomerSpace;
-import com.latticeengines.domain.exposed.camille.lifecycle.CustomerSpaceInfo;
 import com.latticeengines.domain.exposed.exception.LedpCode;
 import com.latticeengines.domain.exposed.exception.LedpException;
 import com.latticeengines.pls.service.TenantConfigService;
@@ -23,12 +21,8 @@ public class TenantConfigServiceImpl implements TenantConfigService {
     @Override
     public String getTopology(String tenantId) {
         try {
-            CustomerSpace customerSpace = CustomerSpace.parse(tenantId);
-            CustomerSpaceInfo spaceInfo = SpaceLifecycleManager.getInfo(customerSpace.getContractId(),
-                    customerSpace.getTenantId(), customerSpace.getSpaceId());
-
-            return spaceInfo.properties.topology;
-
+            TenantDocument doc = getTenantDocument(tenantId);
+            return doc.getSpaceConfig().getTopology().getName().toUpperCase();
         } catch (Exception ex) {
             log.error("Can not get tenant's topology", ex);
             throw new LedpException(LedpCode.LEDP_18033, ex);
