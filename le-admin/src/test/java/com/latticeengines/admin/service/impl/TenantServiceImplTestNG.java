1670b333a16543b9dbf4b60b247559a8cbc5d1e8
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-05-14 22:07:23 +0000

    fix le-admin functional test

--- le-admin/src/test/java/com/latticeengines/admin/service/impl/TenantServiceImplTestNG.java
+++ le-admin/src/test/java/com/latticeengines/admin/service/impl/TenantServiceImplTestNG.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.latticeengines.admin.functionalframework.AdminFunctionalTestNGBase;
 import com.latticeengines.admin.service.TenantService;
 import com.latticeengines.domain.exposed.admin.CRMTopology;
 import com.latticeengines.domain.exposed.admin.LatticeProduct;
+import com.latticeengines.domain.exposed.admin.SerializableDocumentDirectory;
 import com.latticeengines.domain.exposed.admin.SpaceConfiguration;
 import com.latticeengines.domain.exposed.admin.TenantDocument;
 import com.latticeengines.domain.exposed.camille.DocumentDirectory;
@@ -32,7 +34,9 @@ public class TenantServiceImplTestNG extends AdminFunctionalTestNGBase {
     @Test(groups = "functional")
     public void testSetupSpaceConfigSchema() throws Exception {
         DocumentDirectory dir = tenantService.getSpaceConfigSchema();
-        Assert.assertEquals(dir.getChild("Topology").getDocument().getData(),
-                "[\"Marketo\",\"Eloqua\",\"SFDC\"]");
+        ObjectMapper mapper = new ObjectMapper();
+        SerializableDocumentDirectory.Metadata metadata = mapper.readValue(
+                dir.getChild("Topology").getDocument().getData(), SerializableDocumentDirectory.Metadata.class);
+        Assert.assertEquals(metadata.getOptions().size(), CRMTopology.values().length);
     }
 }
