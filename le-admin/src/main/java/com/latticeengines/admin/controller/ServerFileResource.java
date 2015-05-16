ff3065b7033ac62db23d23f73f8008790451e70a
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-05-16 00:14:10 +0000

    ServerFileUtils

--- le-admin/src/main/java/com/latticeengines/admin/controller/ServerFileResource.java
+++ le-admin/src/main/java/com/latticeengines/admin/controller/ServerFileResource.java
@@ -1,19 +1,10 @@
 package com.latticeengines.admin.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.latticeengines.admin.service.ServerFileService;
 import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
 
 @Api(value = "files on le-admin server", description = "REST resource for downloading local files from le-admin server")
 @RestController
@@ -21,20 +12,21 @@ import com.wordnik.swagger.annotations.ApiOperation;
 @PostAuthorize("hasRole('Platform Operations')")
 public class ServerFileResource {
 
-    @Autowired
-    private ServerFileService serverFileService;
+//    @Autowired
+//    private ServerFileService serverFileService;
 
-    @RequestMapping(value = "", method = RequestMethod.GET, headers = "Accept=application/json")
-    @ResponseBody
-    @ApiOperation(value = "Get list of services")
-    public void getServerFile(@RequestParam(value = "path") String path,
-                              @RequestParam(value = "filename", required = false) String filename,
-                              @RequestParam(value = "mimetype",
-                                      required = false, defaultValue = "text/plain") String mimeType,
-                              HttpServletRequest request, HttpServletResponse response) {
-        if (filename == null || filename.equals("")) filename = null;
-        serverFileService.downloadFile(request, response, path, filename, mimeType);
-    }
+//    @RequestMapping(value = "", method = RequestMethod.GET, headers = "Accept=application/json")
+//    @ResponseBody
+//    @ApiOperation(value = "Get list of services")
+//    public void getServerFile(@RequestParam(value = "path") String path,
+//                              @RequestParam(value = "filename", required = false) String filename,
+//                              @RequestParam(value = "mimetype",
+//                                      required = false, defaultValue = "text/plain") String mimeType,
+//                              HttpServletRequest request, HttpServletResponse response) {
+//        if (filename == null || filename.equals("")) filename = null;
+//        serverFileService.downloadFile(request, response, path, filename, mimeType);
+//    }
+
 
 
 }
