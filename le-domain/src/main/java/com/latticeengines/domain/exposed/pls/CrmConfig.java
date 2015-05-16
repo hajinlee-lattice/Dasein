003a2f03fd0cfa536591f5b9a3311be784fd210e
lming <lming@479f5059-62a3-4c39-9342-9852e3a68435>
2015-05-16 00:36:02 +0000

    config dataloader provider

---
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/pls/CrmConfig.java
@@ -0,0 +1,53 @@
+package com.latticeengines.domain.exposed.pls;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CrmConfig {
+
+    private CrmCredential crmCredential;
+    
+    private String dataProviderName;
+    private String entityType;
+    private String version;
+
+    @JsonProperty("CrmCredential")
+    public CrmCredential getCrmCredential() {
+        return crmCredential;
+    }
+
+    @JsonProperty("CrmCredential")
+    public void setCrmCredential(CrmCredential crmCredential) {
+        this.crmCredential = crmCredential;
+    }
+
+    @JsonProperty("EntityType")
+    public void setEntityType(String entityType) {
+        this.entityType = entityType;
+    }
+
+    @JsonProperty("EntityType")
+    public String getEntityType() {
+        return entityType;
+    }
+
+    @JsonProperty("DataProviderName")
+    public String getDataProviderName() {
+        return dataProviderName;
+    }
+
+    @JsonProperty("DataProviderName")
+    public void setDataProviderName(String dataProviderName) {
+        this.dataProviderName = dataProviderName;
+    }
+
+    @JsonProperty("Version")
+    public String getVersion() {
+        return this.version;
+    }
+
+    @JsonProperty("Version")
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+}
