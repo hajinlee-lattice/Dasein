fac7e14f61c0fb3441488803e3ec6d9970615511
llu <llu@479f5059-62a3-4c39-9342-9852e3a68435>
2015-05-19 03:26:47 +0000

    Add more methods to support adding conditions for Camel and Cascading

--- le-dellebi/src/main/java/com/latticeengines/dellebi/util/HadoopFileSystemOperations.java
+++ le-dellebi/src/main/java/com/latticeengines/dellebi/util/HadoopFileSystemOperations.java
@@ -10,13 +10,22 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.springframework.beans.factory.annotation.Value;
 
-public class HadoopFileSystemOperations implements FileSystemOperations {
+public class HadoopFileSystemOperations{
     @Value("${dellebi.datahadooprootpath}")
     private String dataHadoopRootPath;
+    
+    @Value("${dellebi.datahadoopinpath}")
+    private String dataHadoopInPath;
+    @Value("${dellebi.datahadoopworkingpath}")
+    private String dataHadoopWorkingPath;
+    
+    @Value("${dellebi.quotetrans}")
+    private String quoteTrans;
+    
+    private boolean ifFolderExist;
 
     private static final Log log = LogFactory.getLog(HadoopFileSystemOperations.class);
 
-    @Override
     public void cleanFolder(String folderName) {
         try {
             Configuration conf = new Configuration();
@@ -32,7 +41,6 @@ public class HadoopFileSystemOperations implements FileSystemOperations {
 
     }
 
-    @Override
     public int listFileNumber(String folderName) {
         int fileNumber = 0;
         try {
@@ -54,12 +62,91 @@ public class HadoopFileSystemOperations implements FileSystemOperations {
         return fileNumber;
     }
 
-    @Override
     public boolean isEmpty(String folderName) {
         if (listFileNumber(folderName) == 0) {
             return true;
         } else {
             return false;
         }
+    }
+
+    public void isExist(String folderName) {
+    	
+    	boolean isExist = false;
+        try {
+            Configuration conf = new Configuration();
+            FileSystem fs = FileSystem.get(URI.create(dataHadoopRootPath), conf);
+            Path path = new Path(folderName);
+            ifFolderExist = fs.exists(path);
+               
+        } catch (Exception e) {
+            log.warn("Failed to check if " + folderName + "exists or not.", e);
+        }
+
+        ifFolderExist = isExist;
+    }
+    
+public boolean isExistWithReturnValue(String folderName) {
+    	
+    	boolean isExist = false;
+        try {
+            Configuration conf = new Configuration();
+            FileSystem fs = FileSystem.get(URI.create(dataHadoopRootPath), conf);
+            Path path = new Path(folderName);
+            return fs.exists(path);
+               
+        } catch (Exception e) {
+            log.warn("Failed to check if " + folderName + "exists or not.", e);
+        }
+
+        return isExist;
+    }
+
+public boolean isExistWithOpenFileName(String folderName) {
+
+    try {
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(URI.create(dataHadoopRootPath), conf);
+        Path path = new Path(folderName);
+        if(fs.exists(path)){
+        	FileStatus[] fileStatus = fs.listStatus(path);
+            
+            for(FileStatus status:fileStatus ){
+            	if(status.getPath().getName().endsWith(".txt.opened")){
+            		return true;
+            	}
+            }
+        }          
+    } catch (Exception e) {
+        log.warn("Failed to check if " + folderName + "exists or not.", e);
+    }
+
+    return false;
+}
+
+    public void removeFile(String fileName) {
+
+        try {
+            Configuration conf = new Configuration();
+            FileSystem fs = FileSystem.get(URI.create(dataHadoopRootPath), conf);
+            Path path = new Path(fileName);
+            fs.deleteOnExit(path);
+        } catch (Exception e) {
+            log.warn("Failed!", e);
+        }
+    }
+    
+    public Boolean getIfNotExist(){
+    	return !ifFolderExist;
+    }
+    
+    public Boolean ifReadyToProcessData(){
+    	if(listFileNumber(dataHadoopInPath + "/" + quoteTrans) != 0
+    			&& isExistWithReturnValue(dataHadoopWorkingPath + "/" + quoteTrans) != true
+    			&& isExistWithOpenFileName(dataHadoopInPath + "/" + quoteTrans) != true){
+    		return true;
+    	}
+    	
+    	return false;
     }
 }
