5b9c61124ca5279b50403002630faeef2930ebe2
ygao <ygao@479f5059-62a3-4c39-9342-9852e3a68435>
2015-05-21 06:12:08 +0000

    DP-1101 Create test cases for scoring mapper - add testing files

---
+++ le-scoring/src/test/resources/com/latticeengines/scoring/models/supportedFiles/pipelinesteps.py
@@ -0,0 +1,64 @@
+from collections import defaultdict
+
+import encoder
+import numpy as np
+import pandas as pd
+from pipelinefwk import PipelineStep
+from pipelinefwk import get_logger
+
+logger = get_logger("pipeline")
+
+def dictfreq(doc):
+    freq = defaultdict(int)
+    freq[doc] += 1
+    return freq
+
+class EnumeratedColumnTransformStep(PipelineStep):
+    _enumMappings = {}
+
+    def __init__(self, enumMappings):
+        self._enumMappings = enumMappings
+        
+    def transform(self, dataFrame):
+        outputFrame = dataFrame
+        for column, encoder in self._enumMappings.iteritems():
+            if hasattr(encoder, 'classes_'):
+                classSet = set(encoder.classes_.flat)
+                outputFrame[column] = outputFrame[column].map(lambda s: 'NULL' if s not in classSet else s)
+                encoder.classes_ = np.append(encoder.classes_, 'NULL')
+            
+            if column in outputFrame:
+                logger.info("Transforming column %s." % column)
+                outputFrame[column] = encoder.transform(outputFrame[column])
+     
+        return outputFrame
+
+class ColumnTypeConversionStep(PipelineStep):
+    columnsToConvert_ = []
+    
+    def __init__(self, columnsToConvert=[]):
+        self.columnsToConvert_ = columnsToConvert
+
+    def transform(self, dataFrame):
+        if len(self.columnsToConvert_) > 0:
+            for column in self.columnsToConvert_:
+                if column in dataFrame and dataFrame[column].dtype == np.object_:
+                    dataFrame[column] = pd.Series(pd.lib.maybe_convert_numeric(dataFrame[column].as_matrix(), set(), coerce_numeric=True))
+                else:
+                    logger.info("Column %s cannot be transformed since it is not in the data frame." % column)
+        return dataFrame
+    
+class ImputationStep(PipelineStep):
+    _enumMappings = dict()
+    
+    def __init__(self, enumMappings):
+        self._enumMappings = enumMappings
+
+    def transform(self, dataFrame):
+        outputFrame = dataFrame
+        if len(self._enumMappings) > 0:
+            for column, value in self._enumMappings.iteritems():
+                if column in outputFrame:
+                    outputFrame[column] = outputFrame[column].fillna(value)
+        return outputFrame
+        
