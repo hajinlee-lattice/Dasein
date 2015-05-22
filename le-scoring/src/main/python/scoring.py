5b12e87c28b6723d98201bf86129ee418c8d4d26
ygao <ygao@479f5059-62a3-4c39-9342-9852e3a68435>
2015-05-22 18:05:26 +0000

    DP-1107 Ensure scores are equivalent for production leads between C# and YARN impls -- add the scoring.py for deployment

---
+++ le-scoring/src/main/python/scoring.py
@@ -0,0 +1,71 @@
+import sys
+import os
+import subprocess
+import shutil
+
+SCORING_SCRIPT_NAME = "scoringengine.py"
+PIPELINE_SCRIPT_NAME = "pipeline.py"
+PICKEL_FILE_NAME = "STPipelineBinary.p"
+PIPELINEFWK_SCRIPT_NAME = "pipelinefwk.py"
+ENCODER_SCRIPT_NAME = "encoder.py"
+PIPELINESTEPS_SCRIPT_NAME = "pipelinesteps.py"
+SCORING_INPUT_PREFIX = "scoringinputfile-"
+SCORING_OUTPUT_PREFIX = "scoringoutputfile-"
+
+def main(argv):
+    scoringFiles = [SCORING_SCRIPT_NAME, PIPELINE_SCRIPT_NAME, PICKEL_FILE_NAME, PIPELINEFWK_SCRIPT_NAME, ENCODER_SCRIPT_NAME, PIPELINESTEPS_SCRIPT_NAME]
+    print scoringFiles
+    for index in range(len(argv)):
+        leadFiles = []
+        if index == 0:
+            continue
+        #change the file name of supported files
+        modelID = argv[index]
+        manipulateSupportedFiles(modelID, scoringFiles, leadFiles)    
+        #do scoring
+        for leadFile in leadFiles:
+            modelEvaluate(modelID, leadFile)
+        #delete the supported files for the next round of scoring
+        deleteFiles(scoringFiles)
+        deleteFiles(leadFiles)
+
+def manipulateSupportedFiles(modelID, scoringFiles, leadFiles):
+    s = modelID + "-"
+    curFiles = os.listdir(os.getcwd())
+    targetedFiles = []
+    for f in scoringFiles:
+        targetedFiles.append(modelID+f)
+    # loop through all the files in the current directory
+    files = os.listdir('.')
+    for f in files:
+        if f.startswith(s):
+            updatedLeadName = SCORING_INPUT_PREFIX+f[len(s):]
+            os.rename(f, updatedLeadName)
+            leadFiles.append(updatedLeadName)
+            continue
+        for fileName in targetedFiles:
+            if f.startswith(fileName):
+                os.rename(f, fileName[len(modelID):])
+    print "after the renaming, the files are"
+    print os.listdir('.')   
+ 
+def modelEvaluate(modelID, leadFile):
+    #scoringoutputfile name
+    outputFile = modelID + SCORING_OUTPUT_PREFIX + leadFile[len(SCORING_INPUT_PREFIX):] + ".txt"
+    #print os.environ["PYTHONPATH"]
+    print sys.executable 
+    executable = "/usr/local/bin/python2.7"
+    popen = subprocess.Popen([executable, SCORING_SCRIPT_NAME, leadFile, outputFile], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    s, stderr = popen.communicate()
+    print s
+    print stderr
+
+def deleteFiles(files):
+    for f in files:
+        try:
+            os.remove(f)
+        except OSError, e:
+            print ("Error: %s - %s." % (e.filename, e.strerror))
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
