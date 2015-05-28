702c975e9fb6320518be45e84da1b799afb5f6a4
lming <lming@479f5059-62a3-4c39-9342-9852e3a68435>
2015-05-28 23:58:01 +0000

    add batch capability.

--- le-dellebi/src/main/java/com/latticeengines/dellebi/service/DellEbiFlowService.java
+++ le-dellebi/src/main/java/com/latticeengines/dellebi/service/DellEbiFlowService.java
@@ -1,20 +1,33 @@
 package com.latticeengines.dellebi.service;
 
+import com.latticeengines.domain.exposed.dataflow.DataFlowContext;
 
 public interface DellEbiFlowService {
 
-    String getFile();
+    public static final String FILE_SOURCE_LOCAL = "LOCAL";
+    public static final String FILE_SOURCE_SMB = "SMB";
+    public static final String FILE_SOURCE = "FILE_SOURCE";
+    public static final String TXT_FILE_NAME = "TXT_FILE_NAME";
+    public static final String ZIP_FILE_NAME = "ZIP_FILE_NAME";
 
-    String getOutputDir();
+    public static final String RESULT_KEY = "RESULT";
 
-    String getTxtDir();
+    public static final String START_TIME = "startTime";
 
-    String getZipDir();
+    DataFlowContext getFile();
 
-    FileType getFileType(String zipFileName);
+    String getOutputDir(DataFlowContext context);
 
-    boolean deleteSMBFile(String fileName);
+    void registerFailedFile(DataFlowContext context);
 
-    void registerFailedFile(String fileName);
+    boolean deleteFile(DataFlowContext context);
+
+    FileType getFileType(DataFlowContext context);
+
+    String getZipDir(DataFlowContext context);
+
+    String getTxtDir(DataFlowContext context);
+
+    String getTargetDB(DataFlowContext context);
 
 }
