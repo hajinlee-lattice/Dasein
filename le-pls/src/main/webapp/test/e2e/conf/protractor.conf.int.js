9af1b787523ea68ecc35ea6c5f30ebcce32097be
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-06-01 19:10:10 +0000

    fix e2e test tenantIds for env=int

--- le-pls/src/main/webapp/test/e2e/conf/protractor.conf.int.js
+++ le-pls/src/main/webapp/test/e2e/conf/protractor.conf.int.js
@@ -4,7 +4,7 @@ var common = require('./protractor.conf.common');
 
 var config = common.generateConfig('..');
 
-config.params.tenantId = "IntegrationTestPLSContract.Tenant2.Production";
-config.params.alternativeTenantId = "IntegrationTestPLSContract.Tenant1.Production";
+config.params.tenantId = "IntegrationTestPLSContract.IntegrationTestTenant2.Production";
+config.params.alternativeTenantId = "IntegrationTestPLSContract.IntegrationTestTenant1.Production";
 
 exports.config = config;
