05782aab14d606fbdc2da809920498754ed58efa
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2015-06-10 17:59:43 +0000

    Remove unused directory

---
+++ le-eai/src/test/java/com/latticeengines/eai/service/impl/file/converter/FileToAvroTypeConverterUnitTestNG.java
@@ -0,0 +1,31 @@
+package com.latticeengines.eai.service.impl.file.converter;
+
+import static org.testng.Assert.assertEquals;
+
+import org.apache.avro.Schema.Type;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class FileToAvroTypeConverterUnitTestNG {
+    
+    private FileToAvroTypeConverter fileToAvroTypeConverter = new FileToAvroTypeConverter();
+
+    @Test(groups = "unit", dataProvider = "fileTypeToAvroTypeProvider")
+    public void convertTypeToAvro(String fileType, Type avroType) {
+        assertEquals(fileToAvroTypeConverter.convertTypeToAvro(fileType), avroType);
+    }
+    
+    @DataProvider(name = "fileTypeToAvroTypeProvider")
+    public static Object[][] getDataFileProvider() {
+        return new Object[][] { { "Int", Type.INT }, //
+                { "Double", Type.DOUBLE }, //
+                { "Boolean", Type.BOOLEAN }, //
+                { "Float", Type.FLOAT }, //
+                { "String", Type.STRING }, //
+                { "Date", Type.LONG }, //
+                { "Timestamp", Type.LONG }, //
+                { "Long", Type.LONG }
+        };
+    }
+
+}
