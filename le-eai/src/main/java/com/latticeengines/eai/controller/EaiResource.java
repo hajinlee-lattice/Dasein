1b627ca4c61a3f54c4b7c6f8ba02236b0c9f1afe
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2015-06-10 05:08:31 +0000

    Make eai a microservice.

---
+++ le-eai/src/main/java/com/latticeengines/eai/controller/EaiResource.java
@@ -0,0 +1,46 @@
+package com.latticeengines.eai.controller;
+
+import java.util.Arrays;
+
+import org.apache.hadoop.yarn.api.records.ApplicationId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.latticeengines.dataplatform.exposed.service.ModelingService;
+import com.latticeengines.domain.exposed.api.AppSubmission;
+import com.latticeengines.domain.exposed.dataplatform.JobStatus;
+import com.latticeengines.domain.exposed.eai.ImportConfiguration;
+import com.latticeengines.eai.exposed.service.EaiService;
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+
+@Api(value = "importjobs", description = "REST resource for importing data into Lattice")
+@RestController
+@RequestMapping("/importjobs")
+public class EaiResource {
+    
+    @Autowired
+    private EaiService eaiService;
+    
+    @Autowired
+    private ModelingService modelingService;
+
+    @RequestMapping(value = "", method = RequestMethod.POST, headers = "Accept=application/json")
+    @ResponseBody
+    @ApiOperation(value = "Create an import data job")
+    public AppSubmission createImportDataJob(@RequestBody ImportConfiguration importConfig) {
+        return new AppSubmission(Arrays.<ApplicationId> asList(eaiService.extractAndImport(importConfig)));
+    }
+    
+    @RequestMapping(value = "/{applicationId}", method = RequestMethod.GET, headers = "Accept=application/json")
+    @ResponseBody
+    @ApiOperation(value = "Get status for submitted import job")
+    public JobStatus getImportDataJobStatus(@PathVariable String applicationId) {
+        return modelingService.getJobStatus(applicationId);
+    }
+}
