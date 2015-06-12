7a77c8795adede6aeeb05f95959f5c1071dc9da6
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-06-12 18:22:41 +0000

    hide most of protractor test to debug jenkins issue

--- le-pls/src/main/webapp/test/e2e/conf/protractor.conf.common.js
+++ le-pls/src/main/webapp/test/e2e/conf/protractor.conf.common.js
@@ -6,16 +6,16 @@ var Config = function() {
 
         return {
             specs: [
-                specBasePath + '/mainflow_spec.js',
-                specBasePath + '/modellist_spec.js',
-                specBasePath + '/modeldetail_spec.js',
-                specBasePath + '/predictors_spec.js',
-                specBasePath + '/thresholdexplorer_spec.js',
-                specBasePath + '/leadsample_spec.js',
-                specBasePath + '/usermgmt_spec.js',
-                specBasePath + '/multiplemodel_spec.js',
-                specBasePath + '/systemsetup_spec.js',
-                specBasePath + '/passwordchange_spec.js'
+                specBasePath + '/mainflow_spec.js'//,
+                //specBasePath + '/modellist_spec.js',
+                //specBasePath + '/modeldetail_spec.js',
+                //specBasePath + '/predictors_spec.js',
+                //specBasePath + '/thresholdexplorer_spec.js',
+                //specBasePath + '/leadsample_spec.js',
+                //specBasePath + '/usermgmt_spec.js',
+                //specBasePath + '/multiplemodel_spec.js',
+                //specBasePath + '/systemsetup_spec.js',
+                //specBasePath + '/passwordchange_spec.js'
             ],
 
             params: {
