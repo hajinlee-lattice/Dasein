f323b60fec8c009d485283ddcd2cb382dfa7f804
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-06-12 23:04:06 +0000

    PLS test tenant, contractID = tenantID

--- le-pls/src/test/java/com/latticeengines/pls/functionalframework/PlsFunctionalTestNGBase.java
+++ le-pls/src/test/java/com/latticeengines/pls/functionalframework/PlsFunctionalTestNGBase.java
@@ -583,10 +583,10 @@ public class PlsFunctionalTestNGBase extends AbstractTestNGSpringContextTests {
 
     private void setTestingTenants(){
         if (testingTenants == null || testingTenants.isEmpty()) {
-            List<String> subTenantIds = Arrays.asList(contractId + "Tenant1", contractId + "Tenant2");
+            List<String> subTenantIds = Arrays.asList(contractId + "PLSTenant1", contractId + "PLSTenant2");
             testingTenants = new ArrayList<>();
             for (String subTenantId: subTenantIds) {
-                String tenantId = String.format("%s.%s.%s", contractId + "PLS" + "Contract", subTenantId,
+                String tenantId = String.format("%s.%s.%s", subTenantId, subTenantId,
                         CustomerSpace.BACKWARDS_COMPATIBLE_SPACE_ID);
                 if (!tenantService.hasTenantId(tenantId)) {
                     Tenant tenant = new Tenant();
