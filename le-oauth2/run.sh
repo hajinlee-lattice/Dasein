a0df4270abd48df8d12a074b221b68d6539e1e2f
lming <lming@479f5059-62a3-4c39-9342-9852e3a68435>
2015-06-18 21:06:49 +0000

    add le-oauth2

--- le-oauth2/run.sh
+++
@@ -1,2 +0,0 @@
-#mvn -Pfunctional -DADMIN_PROPDIR=conf/env/dev jetty:run
-mvn -Pfunctional -DPLAYMAKER_PROPDIR=conf/env/dev -DargLine="" -Djavax.net.ssl.trustStore=certificates/ga209-ldap.dev.lattice.local.jks jetty:run
