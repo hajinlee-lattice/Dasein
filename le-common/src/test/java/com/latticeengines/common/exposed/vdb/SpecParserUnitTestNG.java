07b5c217dd3ea0c945d3333dda2206979f3962f3
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2015-06-18 20:20:00 +0000

    Fix warnings about unclosed resources.

--- le-common/src/test/java/com/latticeengines/common/exposed/vdb/SpecParserUnitTestNG.java
+++ le-common/src/test/java/com/latticeengines/common/exposed/vdb/SpecParserUnitTestNG.java
@@ -10,29 +10,44 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SpecParserUnitTestNG {
+
     @Test(groups = "unit")
     public void testGetSegmentMap() throws FileNotFoundException, SpecParseException {
         URL url = ClassLoader.getSystemResource("com/latticeengines/common/exposed/vdb/spec.maude");
         File specFile = new File(url.getFile());
-        String spec = new Scanner(specFile, "UTF-8").useDelimiter("\\A").next();
-        SpecParser sp = new SpecParser(spec);
-        Map<String, String> map = sp.getSegmentMap();
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(specFile, "UTF-8");
+            String spec = scanner.useDelimiter("\\A").next();
+            SpecParser sp = new SpecParser(spec);
+            Map<String, String> map = sp.getSegmentMap();
 
-        Assert.assertTrue(map.containsKey("default"));
-        Assert.assertTrue(map.containsKey("DefnSegment_Spain"));
-        Assert.assertTrue(map.containsKey("DefnSegment_US"));
-        Assert.assertEquals(map.get("default"), "Unknown");
-        Assert.assertEquals(map.get("DefnSegment_Spain"), "GUID-Spain");
-        Assert.assertEquals(map.get("DefnSegment_US"), "GUID-US");
+            Assert.assertTrue(map.containsKey("default"));
+            Assert.assertTrue(map.containsKey("DefnSegment_Spain"));
+            Assert.assertTrue(map.containsKey("DefnSegment_US"));
+            Assert.assertEquals(map.get("default"), "Unknown");
+            Assert.assertEquals(map.get("DefnSegment_Spain"), "GUID-Spain");
+            Assert.assertEquals(map.get("DefnSegment_US"), "GUID-US");
+            
+        } finally {
+            scanner.close();
+        }
     }
 
     @Test(groups = "unit")
     public void testGetTemplate() throws FileNotFoundException, SpecParseException {
         URL url = ClassLoader.getSystemResource("com/latticeengines/common/exposed/vdb/spec2.maude");
         File specFile = new File(url.getFile());
-        String spec = new Scanner(specFile, "UTF-8").useDelimiter("\\A").next();
-        SpecParser sp = new SpecParser(spec);
-        Assert.assertEquals(sp.getTemplate(), "PLS SFDC Template:1.4.1");
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(specFile, "UTF-8");
+            String spec = scanner.useDelimiter("\\A").next();
+            SpecParser sp = new SpecParser(spec);
+            Assert.assertEquals(sp.getTemplate(), "PLS SFDC Template:1.4.1");
+        } finally {
+            scanner.close();
+        }
+        
     }
 
 }
