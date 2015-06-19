8902cd73a447cc803251980ea0102c0f941e43ff
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2015-06-19 19:29:22 +0000

    Fix warnings.

--- le-common/src/main/java/com/latticeengines/common/exposed/vdb/SpecParseException.java
+++ le-common/src/main/java/com/latticeengines/common/exposed/vdb/SpecParseException.java
@@ -1,6 +1,9 @@
 package com.latticeengines.common.exposed.vdb;
 
 public class SpecParseException extends Exception {
+
+    private static final long serialVersionUID = -3698088874550104056L;
+
     public SpecParseException() {
     }
 
