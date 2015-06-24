11fa2c9c0a00cbb8ef78eeb7ce7fbe9f8fe36bc5
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2015-06-24 20:50:05 +0000

    add TenantModelInfo domain object

--- le-upgrade/src/main/java/com/latticeengines/upgrade/jdbc/TenantModelJdbcManager.java
+++ le-upgrade/src/main/java/com/latticeengines/upgrade/jdbc/TenantModelJdbcManager.java
@@ -2,12 +2,12 @@ package com.latticeengines.upgrade.jdbc;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import com.latticeengines.upgrade.domain.TenantModelInfo;
 import com.latticeengines.upgrade.yarn.YarnPathUtils;
 
 @Component("jdbcManager")
@@ -16,22 +16,27 @@ public class TenantModelJdbcManager {
     @Autowired
     private JdbcTemplate tenantModelInfoJdbcTemlate;
 
+    private static final String EXTERNAL_TENANT_MODEL_TABLE = "TenantModel_Info";
+
+    private static final String INTERNAL_TENANT_MODEL_TABLE = "TenantModel_Info_Internal";
+
     public List<String> getTenantsToUpgrade() {
-        List<Map<String, Object>> results = tenantModelInfoJdbcTemlate.queryForList("SELECT TenantName FROM TenantModel_Info");
+        List<TenantModelInfo> results = tenantModelInfoJdbcTemlate.query("SELECT TenantName FROM "
+                + EXTERNAL_TENANT_MODEL_TABLE, new BeanPropertyRowMapper<TenantModelInfo>(TenantModelInfo.class));
         List<String> tenants = new ArrayList<>();
-        for (Map<String, Object> entry: results) {
-            tenants.add(( String ) entry.get("TenantName"));
+        for (TenantModelInfo tenantModelInfo : results) {
+            tenants.add(tenantModelInfo.getTenant());
         }
         return tenants;
     }
 
     public List<String> getActiveModels(String dlTenantName) {
-        List<Map<String, Object>> results = tenantModelInfoJdbcTemlate.queryForList(
-                "SELECT ModelGUID FROM TenantModel_Info WHERE TenantName = \'" + dlTenantName + "\'"
-        );
+        List<TenantModelInfo> results = tenantModelInfoJdbcTemlate.query("SELECT ModelGUID FROM "
+                + EXTERNAL_TENANT_MODEL_TABLE + " WHERE TenantName = \'" + dlTenantName + "\'",
+                new BeanPropertyRowMapper<TenantModelInfo>(TenantModelInfo.class));
         List<String> models = new ArrayList<>();
-        for (Map<String, Object> entry: results) {
-            models.add(( String ) entry.get("ModelGUID"));
+        for(TenantModelInfo tenantModelInfo : results) {
+            models.add(tenantModelInfo.getModelGuid());
         }
         return models;
     }
@@ -39,20 +44,22 @@ public class TenantModelJdbcManager {
     public List<String> getActiveUuids(String dlTenantName) {
         List<String> modelGuids = getActiveModels(dlTenantName);
         List<String> uuids = new ArrayList<>();
-        for (String modelGuid: modelGuids) {
+        for (String modelGuid : modelGuids) {
             uuids.add(YarnPathUtils.extractUuid(modelGuid));
         }
         return uuids;
     }
 
     public void populateExternalTenantModelInfo(String dlTenantName, String modelGuid) {
-        tenantModelInfoJdbcTemlate.execute("IF NOT EXISTS (SELECT * FROM TenantModel_Info where TenantName = \'" + dlTenantName
-                + "\') insert into TenantModel_Info values (\'" + dlTenantName + "\', \'" + modelGuid + "\')");
+        tenantModelInfoJdbcTemlate.execute("IF NOT EXISTS (SELECT * FROM TenantModel_Info where TenantName = \'"
+                + dlTenantName + "\') insert into " + EXTERNAL_TENANT_MODEL_TABLE + " values (\'" + dlTenantName
+                + "\', \'" + modelGuid + "\')");
     }
-    
+
     public void populateInternalTenantModelInfo(String dlTenantName, String modelGuid) {
-        tenantModelInfoJdbcTemlate.execute("IF NOT EXISTS (SELECT * FROM TenantModel_Info where TenantName = \'" + dlTenantName
-                + "\') insert into TenantModel_Info_Internal values (\'" + dlTenantName + "\', \'" + modelGuid + "\')");
+        tenantModelInfoJdbcTemlate.execute("IF NOT EXISTS (SELECT * FROM TenantModel_Info where TenantName = \'"
+                + dlTenantName + "\') insert into " + INTERNAL_TENANT_MODEL_TABLE + " values (\'" + dlTenantName
+                + "\', \'" + modelGuid + "\')");
     }
 
     public boolean modelIsActive(String dlTenantName, String modelGuid) {
