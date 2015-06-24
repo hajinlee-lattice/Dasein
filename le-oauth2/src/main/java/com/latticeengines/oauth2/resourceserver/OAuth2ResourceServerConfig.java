a5bf41af1b69f986b7d9568b8634dae8dace194f
lming <lming@479f5059-62a3-4c39-9342-9852e3a68435>
2015-06-24 07:26:45 +0000

    add le-oauth2

--- le-oauth2/src/main/java/com/latticeengines/oauth2/resourceserver/OAuth2ResourceServerConfig.java
+++ le-oauth2/src/main/java/com/latticeengines/oauth2/resourceserver/OAuth2ResourceServerConfig.java
@@ -3,16 +3,17 @@ package com.latticeengines.oauth2.resourceserver;
 import javax.annotation.Resource;
 import javax.sql.DataSource;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 @Configuration
+@EnableAutoConfiguration
 @EnableResourceServer
 public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
@@ -21,14 +22,12 @@ public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter 
     @Resource(name = "dataSourceOauth2")
     private DataSource dataSource;
 
-    @Bean
-    public TokenStore tokenStore() {
-        return new JdbcTokenStore(dataSource);
-    }
+    @Autowired
+    private TokenStore tokenStore;
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) {
-        resources.resourceId(PLAYMAKER_REST_RESOURCE_ID).stateless(false);
+        resources.tokenStore(tokenStore).resourceId(PLAYMAKER_REST_RESOURCE_ID).stateless(false);
     }
 
     @Override
