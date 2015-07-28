5a209d8a6c8ecdc6ccb1de42ff6eb9e9a62f170d
smeng <smeng@479f5059-62a3-4c39-9342-9852e3a68435>
2015-07-28 18:11:56 +0000

    Change the DB with IP instead of server name

--- le-tools/PlaymakerEnd2End/tests/apitest.py
+++ le-tools/PlaymakerEnd2End/tests/apitest.py
@@ -41,8 +41,9 @@ class Test(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        tenant = 'PCM'
-        key = cls.getOneTimeKey(tenant, "jdbc:sqlserver://BODCDEVVSAS193\\SQL2008R2;databaseName=PCM")
+        #Paypal_Obfusc_DB
+        tenant = 'PCMTest'
+        key = cls.getOneTimeKey(tenant, "jdbc:sqlserver://10.41.1.193\\SQL2008R2;databaseName=PCM")
         cls.token = 'bearer ' + cls.getToken(key, tenant)
         #print "Key is: " + key
         #print  "token is: " + cls.token
