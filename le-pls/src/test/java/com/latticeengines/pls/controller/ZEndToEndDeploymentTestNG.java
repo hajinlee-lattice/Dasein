be161b07247dee8c9d5b652695e90b53ba08386a
mmeng <mmeng@479f5059-62a3-4c39-9342-9852e3a68435>
2015-08-05 20:27:29 +0000

    DP-1371 change EndToEndDeploymentTest to be called "ZEndToEndDeploymentTestNG" to get it run last

--- le-pls/src/test/java/com/latticeengines/pls/controller/ZEndToEndDeploymentTestNG.java
+++ le-pls/src/test/java/com/latticeengines/pls/controller/ZEndToEndDeploymentTestNG.java
@@ -26,9 +26,9 @@ import com.latticeengines.pls.functionalframework.ModelingServiceExecutor;
 import com.latticeengines.pls.functionalframework.PlsFunctionalTestNGBase;
 import com.latticeengines.security.exposed.AccessLevel;
 
-public class EndToEndDeploymentTestNG extends PlsFunctionalTestNGBase {
+public class ZEndToEndDeploymentTestNG extends PlsFunctionalTestNGBase {
 
-    private static final Log LOGGER = LogFactory.getLog(EndToEndDeploymentTestNG.class);
+    private static final Log LOGGER = LogFactory.getLog(ZEndToEndDeploymentTestNG.class);
 
     @Value("${pls.modelingservice.rest.endpoint.hostport}")
     private String modelingServiceHostPort;
@@ -68,56 +68,56 @@ public class EndToEndDeploymentTestNG extends PlsFunctionalTestNGBase {
         FileSystem fs = FileSystem.get(yarnConfiguration);
         fs.delete(new Path(String.format("%s/%s", modelingServiceHdfsBaseDir, tenant)), true);
     }
-    
-    private ModelingServiceExecutor buildModel(String tenant, String modelName, String metadata, String table) throws Exception {
-        InputStream modelSummaryFileAsStream = ClassLoader.getSystemResourceAsStream(
-                String.format("com/latticeengines/pls/controller/metadata-%s.avsc", metadata));
+
+    private ModelingServiceExecutor buildModel(String tenant, String modelName, String metadata, String table)
+            throws Exception {
+        InputStream modelSummaryFileAsStream = ClassLoader.getSystemResourceAsStream(String.format(
+                "com/latticeengines/pls/controller/metadata-%s.avsc", metadata));
         String metadataContents = new String(IOUtils.toByteArray(modelSummaryFileAsStream));
 
         ModelingServiceExecutor.Builder bldr = new ModelingServiceExecutor.Builder();
         bldr.modelingServiceHostPort(modelingServiceHostPort) //
-        .modelingServiceHdfsBaseDir(modelingServiceHdfsBaseDir) //
-        .yarnConfiguration(yarnConfiguration) //
-        .customer(tenant) //
-        .dataSourceDb(dataSourceDb) //
-        .dataSourceHost(dataSourceHost) //
-        .dataSourceUser(dataSourceUser) //
-        .dataSourcePassword(dataSourcePassword) //
-        .dataSourcePort(dataSourcePort) //
-        .dataSourceDbType("SQLServer") //
-        .table(table) //
-        .metadataTable("EventMetadata") //
-        .keyColumn("ModelingID") //
-        .profileExcludeList("LeadID", //
-                "Email", //
-                "ModelingID", //
-                "P1_Event", //
-                "CreationDate", //
-                "Company", //
-                "LastName", //
-                "FirstName") //
-        .targets("Event: P1_Event", //
-                 "Readouts: LeadID | Email | CreationDate", //
-                 "Company: Company", //
-                 "LastName: LastName", //
-                 "FirstName: FirstName", //
-                 "SpamIndicator: SpamIndicator") //
-        .metadataContents(metadataContents) //
-        .modelName(modelName);
+                .modelingServiceHdfsBaseDir(modelingServiceHdfsBaseDir) //
+                .yarnConfiguration(yarnConfiguration) //
+                .customer(tenant) //
+                .dataSourceDb(dataSourceDb) //
+                .dataSourceHost(dataSourceHost) //
+                .dataSourceUser(dataSourceUser) //
+                .dataSourcePassword(dataSourcePassword) //
+                .dataSourcePort(dataSourcePort) //
+                .dataSourceDbType("SQLServer") //
+                .table(table) //
+                .metadataTable("EventMetadata") //
+                .keyColumn("ModelingID") //
+                .profileExcludeList("LeadID", //
+                        "Email", //
+                        "ModelingID", //
+                        "P1_Event", //
+                        "CreationDate", //
+                        "Company", //
+                        "LastName", //
+                        "FirstName") //
+                .targets("Event: P1_Event", //
+                        "Readouts: LeadID | Email | CreationDate", //
+                        "Company: Company", //
+                        "LastName: LastName", //
+                        "FirstName: FirstName", //
+                        "SpamIndicator: SpamIndicator") //
+                .metadataContents(metadataContents) //
+                .modelName(modelName);
 
         return new ModelingServiceExecutor(bldr);
     }
 
     @Test(groups = "deployment", enabled = true, dataProvider = "modelMetadataProvider")
     public void runPipeline(String tenant, String modelName, String metadataSuffix, String tableName) throws Exception {
-        LOGGER.info(String.format(
-                "Running pipeline for model %s in tenant %s using table %s",
-                modelName, tenant, tableName));
+        LOGGER.info(String.format("Running pipeline for model %s in tenant %s using table %s", modelName, tenant,
+                tableName));
         ModelingServiceExecutor executor = buildModel(tenant, modelName, metadataSuffix, tableName);
         executor.init();
         executor.runPipeline();
     }
-    
+
     @SuppressWarnings({ "rawtypes", "unchecked" })
     @Test(groups = "deployment", enabled = true, dependsOnMethods = { "runPipeline" })
     public void checkModels() throws InterruptedException {
@@ -139,12 +139,11 @@ public class EndToEndDeploymentTestNG extends PlsFunctionalTestNGBase {
         assertTrue(summary.getName().startsWith("PLSModel-Eloqua"));
         assertNotNull(summary.getDetails());
     }
-    
+
     @DataProvider(name = "modelMetadataProvider")
     public static Object[][] getModelMetadataProvider() {
         return new Object[][] { //
-                { tenant, "PLSModel-Eloqua1", "eloqua1", "Q_PLS_Modeling_Tenant2" }, //
-                { tenant, "PLSModel-Eloqua2", "eloqua2", "Q_PLS_Modeling_Tenant2" }
-        };
+        { tenant, "PLSModel-Eloqua1", "eloqua1", "Q_PLS_Modeling_Tenant2" }, //
+                { tenant, "PLSModel-Eloqua2", "eloqua2", "Q_PLS_Modeling_Tenant2" } };
     }
 }
