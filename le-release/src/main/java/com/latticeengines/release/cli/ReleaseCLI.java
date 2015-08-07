6b23a52d60b3fbdbf408007e145601440c69b8fb
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2015-08-07 18:16:12 +0000

    organize imports

--- le-release/src/main/java/com/latticeengines/release/cli/ReleaseCLI.java
+++ le-release/src/main/java/com/latticeengines/release/cli/ReleaseCLI.java
@@ -1,34 +1,12 @@
 package com.latticeengines.release.cli;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.FileSystemResource;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.latticeengines.release.activities.StartReleaseNotificationActivity;
 import com.latticeengines.release.exposed.activities.Activity;
-import com.latticeengines.release.exposed.activities.BaseActivity;
-import com.latticeengines.release.exposed.domain.ReleaseProcessParameters;
-import com.latticeengines.release.exposed.domain.JiraParameters;
-import com.latticeengines.release.exposed.domain.ReleaseProcessParameters.NameValuePair;
-import com.latticeengines.release.exposed.domain.JiraParameters.JiraFields;
 import com.latticeengines.release.exposed.domain.ProcessContext;
-import com.latticeengines.release.hipchat.service.HipChatService;
-import com.latticeengines.release.hipchat.service.impl.HipChatServiceImpl;
-import com.latticeengines.release.jenkins.service.JenkinsService;
-import com.latticeengines.release.jenkins.service.impl.JenkinsServiceImpl;
-import com.latticeengines.release.jira.service.ChangeManagementJiraService;
-import com.latticeengines.release.jira.service.impl.ChangeManagementJiraServiceImpl;
-import com.latticeengines.release.jmx.service.JMXCheckService;
-import com.latticeengines.release.jmx.service.impl.JMXCheckServiceImpl;
-import com.latticeengines.release.nexus.service.NexusService;
-import com.latticeengines.release.nexus.service.impl.NexusServiceImpl;
 
 public class ReleaseCLI {
 
@@ -57,17 +35,6 @@ public class ReleaseCLI {
     }
 
     public static void invokeJenkins(ApplicationContext applicationContext) {
-        // JenkinsService js = (JenkinsServiceImpl)
-        // applicationContext.getBean("jenkinsService");
-
-        
-        //
-        // System.out.println(js.triggerJenkinsJobWithParameters("", par));
-        // JsonNode jn = js.getLastBuildStatus("");
-        // System.out.println(jn.get("building"));
-        // System.out.println(jn.get("result"));
-        // System.out.println(jn.get("number"));
-        // System.out.println(js.updateSVNBranchName("","release_2.0.5"));
 
         ProcessContext context = new ProcessContext();
         context.setUrl("http://bodcdevvldp117.lattice.local:8080/view/DeploymentTests/job/ledp_release_deploymenttests_prodcluster_api");
