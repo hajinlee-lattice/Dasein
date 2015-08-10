925464b474a90cdd522a4910ff1f9f0fb38e0fd6
mmeng <mmeng@479f5059-62a3-4c39-9342-9852e3a68435>
2015-08-10 21:38:27 +0000

    PLS-435 Add tenant name and dataloader tenant name to the Admin page

--- le-pls/src/main/webapp/app/AppCommon/widgets/leadsTabWidget/LeadsTabWidget.js
+++ le-pls/src/main/webapp/app/AppCommon/widgets/leadsTabWidget/LeadsTabWidget.js
@@ -1,16 +1,19 @@
 angular.module('mainApp.appCommon.widgets.LeadsTabWidget', [
     'mainApp.appCommon.utilities.ResourceUtility',
     'mainApp.appCommon.utilities.DateTimeFormatUtility',
+    'mainApp.appCommon.utilities.TenantIdParsingUtility',
     'mainApp.core.utilities.RightsUtility',
     'mainApp.core.utilities.BrowserStorageUtility',
     'mainApp.core.utilities.NavUtility'
 ])
-.controller('LeadsTabWidgetController', function ($scope, $rootScope, ResourceUtility, BrowserStorageUtility, RightsUtility, NavUtility) {
+.controller('LeadsTabWidgetController', function ($scope, $rootScope, ResourceUtility, TenantIdParsingUtility, BrowserStorageUtility, RightsUtility, NavUtility) {
     $scope.ResourceUtility = ResourceUtility;
 
     var clientSession = BrowserStorageUtility.getClientSession();
     $scope.showAdminLink = RightsUtility.maySeeAdminInfo();
     $scope.data.TenantId = clientSession.Tenant.Identifier;
+    $scope.data.TenantName = clientSession.Tenant.DisplayName;
+    $scope.data.DataLoaderTenantName = TenantIdParsingUtility.getDataLoaderTenantNameFromTenantId($scope.data.TenantId);
 
     $scope.adminLinkClick = function() {
        $rootScope.$broadcast(NavUtility.ADMIN_INFO_NAV_EVENT, $scope.data);
