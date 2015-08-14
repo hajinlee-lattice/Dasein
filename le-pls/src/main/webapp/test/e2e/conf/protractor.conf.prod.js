f7da82d78ab17fb000d89ee985523526acd8b305
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-08-14 00:38:34 +0000

    change tenantId for protractor tests on prod

--- le-pls/src/main/webapp/test/e2e/conf/protractor.conf.prod.js
+++ le-pls/src/main/webapp/test/e2e/conf/protractor.conf.prod.js
@@ -4,7 +4,7 @@ var common = require('./protractor.conf.common');
 
 var config = common.generateConfig('..');
 
-config.params.tenantId = "Tenant2";
-config.params.alternativeTenantId = "Tenant1";
+config.params.tenantId = "ProductionTestPLSTenant2.ProductionTestPLSTenant2.Production";
+config.params.alternativeTenantId = "ProductionTestPLSTenant1.ProductionTestPLSTenant1.Production";
 
 exports.config = config;
