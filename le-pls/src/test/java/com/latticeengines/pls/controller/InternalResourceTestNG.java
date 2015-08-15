1c298eac45f1adb32db903f5959167cc9456f25a
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-08-15 17:49:58 +0000

    1. revert InternalResourceDeploymentTest; 2. add apache httpclient to le-common's dependencies

--- le-pls/src/test/java/com/latticeengines/pls/controller/InternalResourceTestNG.java
+++ le-pls/src/test/java/com/latticeengines/pls/controller/InternalResourceTestNG.java
@@ -1,28 +1,15 @@
 package com.latticeengines.pls.controller;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.stereotype.Component;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-
 import com.latticeengines.camille.exposed.Camille;
 import com.latticeengines.camille.exposed.CamilleEnvironment;
 import com.latticeengines.camille.exposed.paths.PathBuilder;
 import com.latticeengines.domain.exposed.ResponseDocument;
+import com.latticeengines.domain.exposed.admin.CRMTopology;
 import com.latticeengines.domain.exposed.api.Status;
 import com.latticeengines.domain.exposed.camille.CustomerSpace;
 import com.latticeengines.domain.exposed.camille.Path;
+import com.latticeengines.domain.exposed.exception.LedpCode;
+import com.latticeengines.domain.exposed.exception.LedpException;
 import com.latticeengines.domain.exposed.pls.AttributeMap;
 import com.latticeengines.domain.exposed.pls.CrmCredential;
 import com.latticeengines.domain.exposed.pls.ModelSummary;
@@ -35,6 +22,22 @@ import com.latticeengines.pls.service.CrmCredentialService;
 import com.latticeengines.pls.service.TenantConfigService;
 import com.latticeengines.pls.service.TenantService;
 import com.latticeengines.security.exposed.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.stereotype.Component;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 @Component("internalResourceTestNG")
 public class InternalResourceTestNG extends PlsFunctionalTestNGBase {
@@ -166,5 +169,45 @@ public class InternalResourceTestNG extends PlsFunctionalTestNGBase {
         Assert.assertFalse(camille.exists(newPath));
     }
 
+    @SuppressWarnings("rawtypes")
+    @Test(groups = "deployment")
+    public void provisionThroughTenantConsole() throws Exception {
+        String tenantId = internalResource.getTestTenantIds().get(1);
+
+        final String SPACE_CONFIGURATION_ZNODE = "/SpaceConfiguration";
+        final String TOPOLOGY_ZNODE = "/Topology";
+
+        Camille camille = CamilleEnvironment.getCamille();
+        CustomerSpace customerSpace = CustomerSpace.parse(tenantId);
+        Path path = PathBuilder.buildCustomerSpacePath(CamilleEnvironment.getPodId(), customerSpace.getContractId(),
+                customerSpace.getTenantId(), customerSpace.getSpaceId()).append(
+                new Path(SPACE_CONFIGURATION_ZNODE + TOPOLOGY_ZNODE));
+        try {
+            camille.delete(path);
+        } catch (Exception e) {
+            // ignore
+        }
+
+        try {
+            tenantConfigService.getTopology(tenantId);
+        } catch (LedpException e) {
+            Assert.assertEquals(e.getCode(), LedpCode.LEDP_18033,
+                    "Should get 18033 (can not get tenant's topology) error.");
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        headers.add("Accept", "application/json");
+        headers.add("MagicAuthentication", "Security through obscurity!");
+        HttpEntity<String> requestEntity = new HttpEntity<>("", headers);
+        ResponseEntity<ResponseDocument> responseEntity = magicRestTemplate.exchange(getRestAPIHostPort()
+                + "/pls/internal/testtenants", HttpMethod.PUT, requestEntity, ResponseDocument.class);
+        ResponseDocument response = responseEntity.getBody();
+        Assert.assertTrue(response.isSuccess());
+
+        CRMTopology topology = tenantConfigService.getTopology(tenantId);
+        Assert.assertNotNull(topology);
+        Assert.assertEquals(topology, CRMTopology.ELOQUA);
+    }
 
 }
