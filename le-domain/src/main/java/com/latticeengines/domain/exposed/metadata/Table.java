0c914cdf0871f0962e421b0e4b4ab927ab042fc0
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2015-09-01 21:54:21 +0000

    Changes to metadata domain objects.

--- le-domain/src/main/java/com/latticeengines/domain/exposed/metadata/Table.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/metadata/Table.java
@@ -6,51 +6,100 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.UniqueConstraint;
+
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.latticeengines.common.exposed.graph.GraphNode;
 import com.latticeengines.common.exposed.util.JsonUtils;
 import com.latticeengines.domain.exposed.dataplatform.HasName;
+import com.latticeengines.domain.exposed.dataplatform.HasPid;
+import com.latticeengines.domain.exposed.security.HasTenantId;
+import com.latticeengines.domain.exposed.security.Tenant;
 
-public class Table extends AttributeOwner implements HasName {
 
+@Entity
+@javax.persistence.Table(name = "DATA_TABLE", //
+    uniqueConstraints = { @UniqueConstraint(columnNames = { "NAME", "TENANT_ID" }) })
+@Filter(name = "tenantFilter", condition = "TENANT_ID = :tenantFilterId")
+public class Table extends AttributeOwner implements HasName, HasPid, HasTenantId {
+
+    private Long pid;
     private String name;
     private String displayName;
     private List<Extract> extracts = new ArrayList<>();
     private PrimaryKey primaryKey;
+    private Tenant tenant;
+    private Long tenantId;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    @Basic(optional = false)
+    @Column(name = "PID", unique = true, nullable = false)
+    @Override
+    public Long getPid() {
+        return pid;
+    }
 
     @Override
+    @JsonIgnore
+    public void setPid(Long pid) {
+        this.pid = pid;
+    }
+
+    @Override
+    @JsonProperty("name")
     public String getName() {
         return name;
     }
 
     @Override
+    @JsonProperty("name")
     public void setName(String name) {
         this.name = name;
     }
 
-    @JsonProperty("display_name")
+    @JsonProperty("displayName")
     public void setDisplayName(String displayName) {
         this.displayName = displayName;
     }
 
-    @JsonProperty("display_name")
+    @JsonProperty("displayName")
     public String getDisplayName() {
         return displayName;
     }
 
+    @JsonProperty("extracts")
     public List<Extract> getExtracts() {
         return extracts;
     }
 
+    @JsonIgnore
     public void addExtract(Extract extract) {
         extracts.add(extract);
     }
 
+    @JsonProperty("primaryKey")
     public PrimaryKey getPrimaryKey() {
         return primaryKey;
     }
 
+    @JsonProperty("primaryKey")
     public void setPrimaryKey(PrimaryKey primaryKey) {
         this.primaryKey = primaryKey;
     }
@@ -72,6 +121,33 @@ public class Table extends AttributeOwner implements HasName {
         map.put("primaryKey", Arrays.<GraphNode>asList(new GraphNode[] { primaryKey }));
         map.put("extracts", extracts);
         return map;
+    }
+
+    @Override
+    @JsonIgnore
+    @Column(name = "TENANT_ID", nullable = false)
+    public Long getTenantId() {
+        return tenantId;
+    }
+
+    @Override
+    @JsonIgnore
+    public void setTenantId(Long tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    @JsonIgnore
+    public void setTenant(Tenant tenant) {
+        this.tenant = tenant;
+        setTenantId(tenant.getPid());
+    }
+
+    @JsonIgnore
+    @ManyToOne(cascade = { CascadeType.MERGE }, fetch = FetchType.EAGER)
+    @JoinColumn(name = "FK_TENANT_ID", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    public Tenant getTenant() {
+        return tenant;
     }
 
     @Override
---
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/metadata/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @author rgonzalez
+ *
+ */
+@FilterDef(name = "tenantFilter", //
+parameters = @ParamDef(name = "tenantFilterId", type = "java.lang.Long"))
+package com.latticeengines.domain.exposed.metadata;
+
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.ParamDef;
+
