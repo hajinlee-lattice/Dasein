a983baedad390abe5437da78e52fa7196e92ba64
yinjie.he <yinjie.he@479f5059-62a3-4c39-9342-9852e3a68435>
2015-09-02 15:01:51 +0000

    Designed MetadataField class

---
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/pls/MetadataField.java
@@ -0,0 +1,135 @@
+package com.latticeengines.domain.exposed.pls;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.latticeengines.common.exposed.util.JsonUtils;
+
+public class MetadataField {
+
+    private String columnName;
+    private String source; // source type
+    private String object; // table name
+    private String category;
+    private String displayName;
+    private String approvedUsage;
+    private String tags;
+    private String fundamentalType;
+    private String description;
+    private String displayDiscretization;
+    private String statisticalType;
+
+    @JsonProperty("ColumnName")
+    public String getColumnName() {
+        return columnName;
+    }
+
+    @JsonProperty("ColumnName")
+    public void setColumnName(String columnName) {
+        this.columnName = columnName;
+    }
+
+    @JsonProperty("Source")
+    public String getSource() {
+        return source;
+    }
+
+    @JsonProperty("Source")
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    @JsonProperty("Object")
+    public String getObject() {
+        return object;
+    }
+
+    @JsonProperty("Object")
+    public void setObject(String object) {
+        this.object = object;
+    }
+
+    @JsonProperty("Category")
+    public String getCategory() {
+        return category;
+    }
+
+    @JsonProperty("Category")
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @JsonProperty("DisplayName")
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @JsonProperty("DisplayName")
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    @JsonProperty("ApprovedUsage")
+    public String getApprovedUsage() {
+        return approvedUsage;
+    }
+
+    @JsonProperty("ApprovedUsage")
+    public void setApprovedUsage(String approvedUsage) {
+        this.approvedUsage = approvedUsage;
+    }
+
+    @JsonProperty("Tags")
+    public String getTags() {
+        return tags;
+    }
+
+    @JsonProperty("Tags")
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    @JsonProperty("FundamentalType")
+    public String getFundamentalType() {
+        return fundamentalType;
+    }
+
+    @JsonProperty("FundamentalType")
+    public void setFundamentalType(String fundamentalType) {
+        this.fundamentalType = fundamentalType;
+    }
+
+    @JsonProperty("Description")
+    public String getDescription() {
+        return description;
+    }
+
+    @JsonProperty("Description")
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @JsonProperty("DisplayDiscretization")
+    public String getDisplayDiscretization() {
+        return displayDiscretization;
+    }
+
+    @JsonProperty("DisplayDiscretization")
+    public void setDisplayDiscretization(String displayDiscretization) {
+        this.displayDiscretization = displayDiscretization;
+    }
+
+    @JsonProperty("StatisticalType")
+    public String getStatisticalType() {
+        return statisticalType;
+    }
+
+    @JsonProperty("StatisticalType")
+    public void setStatisticalType(String statisticalType) {
+        this.statisticalType = statisticalType;
+    }
+
+    @Override
+    public String toString() {
+        return JsonUtils.serialize(this);
+    }
+
+}
