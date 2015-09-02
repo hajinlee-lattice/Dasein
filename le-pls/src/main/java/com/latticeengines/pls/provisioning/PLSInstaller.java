b0a8586b7c175c43cc2bc720ae92e45d84115bf8
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2015-09-02 22:54:22 +0000

    Log any errors with the PLS installer before rethrowing to ZK so we have a copy of the error on the PLS side.

--- le-pls/src/main/java/com/latticeengines/pls/provisioning/PLSInstaller.java
+++ le-pls/src/main/java/com/latticeengines/pls/provisioning/PLSInstaller.java
@@ -1,10 +1,14 @@
 package com.latticeengines.pls.provisioning;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import com.latticeengines.baton.exposed.camille.LatticeComponentInstaller;
 import com.latticeengines.domain.exposed.camille.CustomerSpace;
 import com.latticeengines.domain.exposed.camille.DocumentDirectory;
 
 public class PLSInstaller extends LatticeComponentInstaller {
+    private static final Log log = LogFactory.getLog(PLSInstaller.class);
 
     private PLSComponentManager componentManager;
 
@@ -12,8 +16,13 @@ public class PLSInstaller extends LatticeComponentInstaller {
 
     @Override
     public DocumentDirectory installComponentAndModifyConfigDir(CustomerSpace space, String serviceName, int dataVersion, DocumentDirectory configDir) {
-        componentManager.provisionTenant(space, configDir);
-        return configDir;
+        try {
+            componentManager.provisionTenant(space, configDir);
+            return configDir;
+        } catch (Exception e) {
+            log.error(e);
+            throw e;
+        }
     }
 
     public void setComponentManager(PLSComponentManager manager) {
