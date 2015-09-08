f08d79368ba64e81901c3e005f3c934254b781e9
lming <lming@479f5059-62a3-4c39-9342-9852e3a68435>
2015-09-08 21:14:22 +0000

    change status code to 401

--- le-playmaker/src/main/java/com/latticeengines/playmaker/exception/ExceptionEncodingTranslator.java
+++ le-playmaker/src/main/java/com/latticeengines/playmaker/exception/ExceptionEncodingTranslator.java
@@ -1,6 +1,7 @@
 package com.latticeengines.playmaker.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
 import org.springframework.security.oauth2.provider.error.DefaultWebResponseExceptionTranslator;
 import org.springframework.web.util.HtmlUtils;
@@ -9,7 +10,7 @@ public class ExceptionEncodingTranslator extends DefaultWebResponseExceptionTran
 
     @Override
     public ResponseEntity<OAuth2Exception> translate(Exception e) throws Exception {
-        Exception ex = new Exception(HtmlUtils.htmlEscape(e.getMessage()));
+        Exception ex = new InsufficientAuthenticationException(HtmlUtils.htmlEscape(e.getMessage()));
         return super.translate(ex);
     }
 }
