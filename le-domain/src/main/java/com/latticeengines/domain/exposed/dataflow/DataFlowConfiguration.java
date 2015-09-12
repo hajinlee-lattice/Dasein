17ab688ef7abf3f807a185b8f17da61706f71268
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2015-09-12 23:39:22 +0000

    Change IMPORTCONFIG to EAICONFIG (eaiConfig) in order to support convention-based application integration.

---
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/dataflow/DataFlowConfiguration.java
@@ -0,0 +1,5 @@
+package com.latticeengines.domain.exposed.dataflow;
+
+public class DataFlowConfiguration {
+
+}
--- le-domain/src/main/java/com/latticeengines/domain/exposed/eai/ImportProperty.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/eai/ImportProperty.java
@@ -4,7 +4,7 @@ public class ImportProperty {
 
     public static final String TABLE = "table";
     public static final String HADOOPCONFIG = "hadoopConfig";
-    public static final String IMPORTCONFIG = "importConfig";
+    public static final String EAICONFIG = "eaiConfig";
     public static final String IMPORTCTX = "importContext";
     public static final String TARGETPATH = "targetPath";
     public static final String CUSTOMER = "customer";
