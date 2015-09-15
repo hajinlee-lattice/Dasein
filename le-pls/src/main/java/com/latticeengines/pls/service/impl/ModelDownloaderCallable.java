cd2bbf68ba5ed3d81465f6799ae6b434dc517923
ygao <ygao@479f5059-62a3-4c39-9342-9852e3a68435>
2015-09-15 18:14:24 +0000

    DP-1602 modeldownloader should not stop when there is an exception in downloading one model, delete the bad model.

--- le-pls/src/main/java/com/latticeengines/pls/service/impl/ModelDownloaderCallable.java
+++ le-pls/src/main/java/com/latticeengines/pls/service/impl/ModelDownloaderCallable.java
@@ -12,6 +12,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.hdfs.BlockMissingException;
 
 import com.latticeengines.common.exposed.util.HdfsUtils;
 import com.latticeengines.domain.exposed.camille.CustomerSpace;
@@ -88,12 +89,14 @@ public class ModelDownloaderCallable implements Callable<Boolean> {
                     modelSummaryEntityMgr.create(summary);
                     foundFilesToDownload = true;
                 }
+            } catch (BlockMissingException e) {
+                log.error(e);
+                // delete the bad model summary file
+                HdfsUtils.rmdir(yarnConfiguration, file);
+            } catch (IOException e) {
+                log.fatal(e); // will trigger pagerDuty
             } catch (Exception e) {
-                if (e instanceof IOException) {
-                    log.fatal(e); // will trigger pagerDuty
-                } else {
-                    log.error(e);
-                }
+                log.error(e);
             }
         }
 
