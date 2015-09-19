1b516d2e0aad0a5da2f2030e44fe26362cbc3434
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2015-09-19 01:28:52 +0000

    Complete the domain objects for the transform layer.

--- le-domain/src/main/java/com/latticeengines/domain/exposed/dataflow/DataFlowConfiguration.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/dataflow/DataFlowConfiguration.java
@@ -1,19 +1,43 @@
 package com.latticeengines.domain.exposed.dataflow;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.latticeengines.domain.exposed.BasePayloadConfiguration;
 
 public class DataFlowConfiguration extends BasePayloadConfiguration {
 
     private String dataFlowBeanName;
+    private List<DataFlowSource> dataSources;
+    private String targetPath;
 
-    @JsonProperty("dataflow_bean")
+    @JsonProperty("bean_name")
     public String getDataFlowBeanName() {
         return dataFlowBeanName;
     }
 
-    @JsonProperty("dataflow_bean")
+    @JsonProperty("bean_name")
     public void setDataFlowBeanName(String dataFlowBeanName) {
         this.dataFlowBeanName = dataFlowBeanName;
+    }
+
+    @JsonProperty("sources")
+    public List<DataFlowSource> getDataSources() {
+        return dataSources;
+    }
+
+    @JsonProperty("sources")
+    public void setDataSources(List<DataFlowSource> dataSources) {
+        this.dataSources = dataSources;
+    }
+
+    @JsonProperty("target_path")
+    public String getTargetPath() {
+        return targetPath;
+    }
+
+    @JsonProperty("target_path")
+    public void setTargetPath(String targetPath) {
+        this.targetPath = targetPath;
     }
 }
---
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/dataflow/DataFlowSource.java
@@ -0,0 +1,46 @@
+package com.latticeengines.domain.exposed.dataflow;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.latticeengines.domain.exposed.dataplatform.HasName;
+
+public class DataFlowSource implements HasName {
+
+    private String name;
+    private String table;
+    private String rawData;
+    
+    @Override
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+    @JsonProperty("table")
+    public String getTable() {
+        return table;
+    }
+    
+    @JsonProperty("table")
+    public void setTable(String table) {
+        this.table = table;
+    }
+
+    @JsonProperty("raw_data")
+    public String getRawData() {
+        return rawData;
+    }
+
+    @JsonProperty("raw_data")
+    public void setRawData(String rawData) {
+        this.rawData = rawData;
+    }
+
+
+}
