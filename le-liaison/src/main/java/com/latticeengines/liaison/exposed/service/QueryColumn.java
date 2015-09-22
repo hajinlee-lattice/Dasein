a71c9b2900c6e15c7143bcf19f7bd0b9dc56bb84
yinjie.he <yinjie.he@479f5059-62a3-4c39-9342-9852e3a68435>
2015-09-22 07:42:25 +0000

    1. Enlarge maxDisplayNameLength to 64

--- le-liaison/src/main/java/com/latticeengines/liaison/exposed/service/QueryColumn.java
+++ le-liaison/src/main/java/com/latticeengines/liaison/exposed/service/QueryColumn.java
@@ -14,7 +14,7 @@ public abstract class QueryColumn {
 	private String expression;
 	private Map<String,String> metadata;
 	
-	private static final int maxDisplayNameLength = 46;
+	private static final int maxDisplayNameLength = 64;
 	private static final int maxCategoryLength = 22;
 	private static final int maxDescriptionLength = 150;
 	private static final Set<String> approvedUsageValues = new HashSet<String>( Arrays.asList("None","Model","ModelAndModelInsights","ModelAndAllInsights") );
