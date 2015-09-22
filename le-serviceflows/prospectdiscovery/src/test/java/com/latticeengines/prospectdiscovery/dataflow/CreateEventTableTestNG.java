7cfc53de77e383c2b531bfa1c7244c8188e5ea65
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2015-09-22 07:34:03 +0000

    1. Change data flow to use Table instead of path 2. Make changes to the test to take advantage of the test framework

--- le-serviceflows/prospectdiscovery/src/test/java/com/latticeengines/prospectdiscovery/dataflow/CreateEventTableTestNG.java
+++ le-serviceflows/prospectdiscovery/src/test/java/com/latticeengines/prospectdiscovery/dataflow/CreateEventTableTestNG.java
@@ -3,14 +3,13 @@ package com.latticeengines.prospectdiscovery.dataflow;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.latticeengines.common.exposed.util.HdfsUtils;
-import com.latticeengines.dataflow.exposed.builder.CascadingDataFlowBuilder;
 import com.latticeengines.domain.exposed.dataflow.DataFlowContext;
+import com.latticeengines.domain.exposed.metadata.Table;
 import com.latticeengines.serviceflows.functionalframework.ServiceFlowsFunctionalTestNGBase;
 
 @ContextConfiguration(locations = { "classpath:serviceflows-prospectdiscovery-context.xml" })
@@ -20,12 +19,8 @@ public class CreateEventTableTestNG extends ServiceFlowsFunctionalTestNGBase {
     private String opportunity;
     private String contact;
     
-    @Autowired
-    private CascadingDataFlowBuilder createEventTable;
-    
     @BeforeClass(groups = "functional")
     public void setup() throws Exception {
-        createEventTable.setLocal(true);
         HdfsUtils.rmdir(yarnConfiguration, "/tmp/TmpEventTable");
         HdfsUtils.rmdir(yarnConfiguration, "/tmp/checkpoints");
         account = ClassLoader.getSystemResource("Account").getPath() + "/*.avro";
@@ -35,13 +30,13 @@ public class CreateEventTableTestNG extends ServiceFlowsFunctionalTestNGBase {
     
     @Test(groups = "functional")
     public void executeDataFlow() throws Exception {
-        Map<String, String> sources = new HashMap<>();
-        sources.put("Account", account);
-        sources.put("Opportunity", opportunity);
-        sources.put("Contact", contact);
+        Map<String, Table> sources = new HashMap<>();
+        sources.put("Account", createTableFromDir("Account", account, "CreatedDate"));
+        sources.put("Opportunity", createTableFromDir("Opportunity", opportunity, "LastModifiedDate"));
+        sources.put("Contact", createTableFromDir("Contact", contact, "LastModifiedDate"));
         
         DataFlowContext ctx = super.createDataFlowContext();
-        ctx.setProperty("SOURCES", sources);
+        ctx.setProperty("SOURCETABLES", sources);
         ctx.setProperty("CUSTOMER", "customer1");
         ctx.setProperty("TARGETPATH", "/tmp/TmpEventTable");
         ctx.setProperty("FLOWNAME", "CreateEventTable");
