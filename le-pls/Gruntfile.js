52ca7ec976ec72a366c117c1c5f0360fc54ad24d
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-09-23 01:59:47 +0000

    use downloaded css and js for kendo

--- le-pls/Gruntfile.js
+++ le-pls/Gruntfile.js
@@ -240,6 +240,7 @@ module.exports = function (grunt) {
                         '<%= pls.app %>/**/*.css',
                         '<%= pls.app %>/**/*.scss',
                         '!<%= pls.app %>/lib/js/*.js',
+                        '!<%= pls.app %>/lib/css/*.css',
                         '!<%= pls.app %>/app/production_<%= versionString %>.min.js',
                         '!<%= pls.app %>/assets/styles/production_<%= versionString %>.min.css'
                     ]
