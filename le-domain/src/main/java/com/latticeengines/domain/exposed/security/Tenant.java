a4e1d4ff362e9bc670349de8abb47187c7e78aee
bnguyen <bnguyen@479f5059-62a3-4c39-9342-9852e3a68435>
2015-09-30 22:40:26 +0000

    Fix tenant to only identity check against id.  Exclude name.

--- le-domain/src/main/java/com/latticeengines/domain/exposed/security/Tenant.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/security/Tenant.java
@@ -86,7 +86,6 @@ public class Tenant implements HasName, HasId<String>, HasPid {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
         return result;
     }
 
@@ -103,11 +102,6 @@ public class Tenant implements HasName, HasId<String>, HasPid {
             if (other.id != null)
                 return false;
         } else if (!id.equals(other.id))
-            return false;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
             return false;
         return true;
     }
