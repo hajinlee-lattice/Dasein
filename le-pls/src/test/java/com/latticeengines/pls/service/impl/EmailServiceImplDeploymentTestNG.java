59b1dd564a321be894a2798b5747a6717507266e
mmeng <mmeng@479f5059-62a3-4c39-9342-9852e3a68435>
2015-10-01 07:41:34 +0000

    add property to le-security buildmachine and print out to le-pls which will remove later in attempt to fix email deployment test.

--- le-pls/src/test/java/com/latticeengines/pls/service/impl/EmailServiceImplDeploymentTestNG.java
+++ le-pls/src/test/java/com/latticeengines/pls/service/impl/EmailServiceImplDeploymentTestNG.java
@@ -152,6 +152,7 @@ public class EmailServiceImplDeploymentTestNG extends PlsDeploymentTestNGBase {
 
                 if (message.getReceivedDate().after(registrationTimestamp)) {
                     String url = getHrefUrlFromMultiPart((Multipart) m.getContent());
+                    System.out.println("the message url is: " + url);
 
                     if (appUrl.equalsIgnoreCase(url)) {
                         // delete the message
