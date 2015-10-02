9ff194d498d2f3c478fa8582820fb7a59b64dc7a
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2015-10-02 01:51:12 +0000

    Refactor multi-tenant aspect framework.

--- le-security/src/main/java/com/latticeengines/security/exposed/entitymanager/impl/MultiTenantEntityMgrAspect.java
+++ le-security/src/main/java/com/latticeengines/security/exposed/entitymanager/impl/MultiTenantEntityMgrAspect.java
@@ -1,10 +1,7 @@
 package com.latticeengines.security.exposed.entitymanager.impl;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -12,47 +9,9 @@ import com.latticeengines.domain.exposed.security.Tenant;
 import com.latticeengines.security.exposed.TicketAuthenticationToken;
 import com.latticeengines.security.exposed.entitymanager.TenantEntityMgr;
 
-@Aspect
 public class MultiTenantEntityMgrAspect {
 
-    @Autowired
-    private SessionFactory sessionFactory;
-
-    @Autowired
-    private TenantEntityMgr tenantEntityMgr;
-
-
-    @Before("execution(* com.latticeengines.pls.entitymanager.impl.ModelSummaryEntityMgrImpl.find*(..))")
-    public void findModelSummary(JoinPoint joinPoint) {
-        enableMultiTenantFilter(joinPoint);
-    }
-
-    @Before("execution(* com.latticeengines.pls.entitymanager.impl.SegmentEntityMgrImpl.find*(..))")
-    public void findSegment(JoinPoint joinPoint) {
-        enableMultiTenantFilter(joinPoint);
-    }
-
-    @Before("execution(* com.latticeengines.pls.entitymanager.impl.ModelSummaryEntityMgrImpl.update*(..))")
-    public void updateModelSummary(JoinPoint joinPoint) {
-        enableMultiTenantFilter(joinPoint);
-    }
-
-    @Before("execution(* com.latticeengines.pls.entitymanager.impl.SegmentEntityMgrImpl.update*(..))")
-    public void updateSegment(JoinPoint joinPoint) {
-        enableMultiTenantFilter(joinPoint);
-    }
-
-    @Before("execution(* com.latticeengines.pls.entitymanager.impl.ModelSummaryEntityMgrImpl.delete*(..))")
-    public void deleteModelSummary(JoinPoint joinPoint) {
-        enableMultiTenantFilter(joinPoint);
-    }
-
-    @Before("execution(* com.latticeengines.pls.entitymanager.impl.SegmentEntityMgrImpl.delete*(..))")
-    public void deleteSegment(JoinPoint joinPoint) {
-        enableMultiTenantFilter(joinPoint);
-    }
-
-    public void enableMultiTenantFilter(JoinPoint joinPoint) {
+    public void enableMultiTenantFilter(JoinPoint joinPoint, SessionFactory sessionFactory, TenantEntityMgr tenantEntityMgr) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (!(auth instanceof TicketAuthenticationToken)) {
             throw new RuntimeException("Problem with multi-tenancy framework.");
