b8934f194629fa857367959e32578b34a32a365e
bross <bross@479f5059-62a3-4c39-9342-9852e3a68435>
2015-10-02 18:29:41 +0000

    Finishing up initial event table flow without fancy additions.  Refactoring poms.

--- le-serviceflows/prospectdiscovery/src/main/java/com/latticeengines/prospectdiscovery/dataflow/CreateEventTable.java
+++ le-serviceflows/prospectdiscovery/src/main/java/com/latticeengines/prospectdiscovery/dataflow/CreateEventTable.java
@@ -18,7 +18,7 @@ public class CreateEventTable extends CascadingDataFlowBuilder {
         setDataFlowCtx(dataFlowCtx);
         String account = addSource(sourceTables.get("Account"));
         String contact = addSource(sourceTables.get("Contact"));
-        //String oppty = addSource(sourceTables.get("Opportunity"));
+        String opportunity = addSource(sourceTables.get("Opportunity"));
         String removeNullEmailAddresses = addFilter(contact, //
                 "Email != null && !Email.trim().isEmpty()", //
                 new FieldList("Email"));
@@ -27,17 +27,17 @@ public class CreateEventTable extends CascadingDataFlowBuilder {
                 "AccountId != null && !AccountId.trim().isEmpty()",
                 new FieldList("AccountId"));
 
-        FieldMetadata domain = new FieldMetadata("Domain", String.class);
-        domain.setPropertyValue("length", "255");
-        domain.setPropertyValue("precision", "0");
-        domain.setPropertyValue("scale", "0");
-        domain.setPropertyValue("logicalType", "domain");
-        domain.setPropertyValue("displayName", "Domain");
+        FieldMetadata contactDomain = new FieldMetadata("ContactDomain", String.class);
+        contactDomain.setPropertyValue("length", "255");
+        contactDomain.setPropertyValue("precision", "0");
+        contactDomain.setPropertyValue("scale", "0");
+        contactDomain.setPropertyValue("logicalType", "domain");
+        contactDomain.setPropertyValue("displayName", "ContactDomain");
 
         String retrieveDomains = addFunction(removeNullAccountIds, //
                 "Email.substring(Email.indexOf('@') + 1)", //
                 new FieldList("Email"), //
-                domain);
+                contactDomain);
 
         // XXX Filter out public email address domains
 
@@ -48,7 +48,7 @@ public class CreateEventTable extends CascadingDataFlowBuilder {
         List<Aggregation> aggregations = new ArrayList<>();
         aggregations.add(new Aggregation("AccountId", "BucketSize", Aggregation.AggregationType.COUNT));
         String retrieveDomainBucketsForEachAccount = addGroupBy(retrieveDomains, //
-                new FieldList("Domain", "AccountId"), //
+                new FieldList("ContactDomain", "AccountId"), //
                 aggregations);
 
         aggregations = new ArrayList<>();
@@ -63,24 +63,56 @@ public class CreateEventTable extends CascadingDataFlowBuilder {
                 retrieveMaxDomainBucketSize, //
                 new FieldList("AccountId", "MaxBucketSize"));
 
-        String joinedWithAccounts = addLeftOuterJoin(retrieveBestDomain, //
-                new FieldList("AccountId"), //
-                account, //
+        String joinedWithAccounts = addLeftOuterJoin(account, //
+                new FieldList("Id"), //
+                retrieveBestDomain, //
                 new FieldList("AccountId"));
 
         // These domains may be null
-        String bestDomainsForEachAccount = addFunction(
+        FieldMetadata domain = new FieldMetadata("Domain", String.class);
+        domain.setPropertyValue("length", "255");
+        domain.setPropertyValue("precision", "0");
+        domain.setPropertyValue("scale", "0");
+        domain.setPropertyValue("logicalType", "domain");
+        domain.setPropertyValue("displayName", "Domain");
+        String domainsForEachAccount = addFunction(
                 joinedWithAccounts, //
-                "Website != null && Website.trim().isEmpty ? Website : Domain", //
-                new FieldList("Website", "Domain"), //
+                "Website != null && !Website.trim().isEmpty() ? Website : ContactDomain", //
+                new FieldList("Website", "ContactDomain"), //
                 domain);
 
-        // Fill out HasOpportunities and HasContacts fields
-
-        // Use event interpretation to set the event column using case when exists statement against opportunity
-
-
-        return retrieveBestDomain;
+        // XXX Remove "NULL"s
+        
+        // XXX Handle multiple different events/filters
+        
+    	// Get count of IsWon for each account
+        aggregations = new ArrayList<>();
+        aggregations.add(new Aggregation("IsWon", "WinCount", Aggregation.AggregationType.COUNT));
+        String wins = addGroupBy(opportunity, //
+        		new FieldList("AccountId"), //
+        		aggregations);       
+        		
+        // Left outer join with that
+        String joinedWithWins = addLeftOuterJoin(
+        		domainsForEachAccount, //
+        		new FieldList("Id"), //
+        		wins, //
+        		new FieldList("AccountId"));
+        
+        FieldMetadata event = new FieldMetadata("Event", Boolean.class);
+        event.setPropertyValue("logicalType", "event");
+        event.setPropertyValue("displayName", "Event");
+        String retrieveEventColumn = addFunction(
+        		joinedWithWins, //
+        		"WinCount != null && WinCount > 0 ? true : false",
+        		new FieldList("WinCount"),
+        		event);
+/*
+        String completed = addRetainFunction(
+        		retrieveEventColumn, 
+        		null);
+*/      
+        return retrieveEventColumn;      
     }
 
     public String constructFlowDefinition(DataFlowContext dataFlowContext, Map<String, String> sources) {
