3a16c1784542a16e89610d2ba1cc7e36eadd0d66
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2015-10-07 03:40:24 +0000

    Review request 1222: Move up implementation to superclass.

--- le-domain/src/main/java/com/latticeengines/domain/exposed/metadata/AttributeOwner.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/metadata/AttributeOwner.java
@@ -1,18 +1,68 @@
 package com.latticeengines.domain.exposed.metadata;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.latticeengines.common.exposed.graph.GraphNode;
+import com.latticeengines.common.exposed.visitor.Visitor;
+import com.latticeengines.common.exposed.visitor.VisitorContext;
 import com.latticeengines.domain.exposed.dataplatform.HasName;
 import com.latticeengines.domain.exposed.dataplatform.HasPid;
 
-public interface AttributeOwner extends HasPid, HasName, GraphNode {
+public abstract class AttributeOwner implements HasPid, HasName, GraphNode {
     
-    void addAttribute(String attribute);
+    protected Long pid;
+    protected String name;
+    protected String displayName;
+    private List<String> attributes = new ArrayList<>();
+    protected Table table;
 
-    List<String> getAttributes();
+    @JsonProperty("attributes")
+    @Transient
+    public List<String> getAttributes() {
+        return attributes;
+    }
 
-    void setAttributes(List<String> attributes);
+    @JsonProperty("attributes")
+    public void setAttributes(List<String> attributes) {
+        this.attributes = attributes;
+    }
 
-    String[] getAttributeNames();
+    @Override
+    public void accept(Visitor visitor, VisitorContext ctx) {
+        visitor.visit(this, ctx);
+    }
+
+    @Override
+    @JsonIgnore
+    @Transient
+    public Collection<? extends GraphNode> getChildren() {
+        return new ArrayList<>();
+    }
+
+    @JsonIgnore
+    @Transient
+    public Map<String, Collection<? extends GraphNode>> getChildMap() {
+        return new HashMap<>();
+    }
+
+    @JsonIgnore
+    @Transient
+    public String[] getAttributeNames() {
+        String[] attrs = new String[attributes.size()];
+        attributes.toArray(attrs);
+        return attrs;
+    }
+
+    public void addAttribute(String attribute) {
+        attributes.add(attribute);
+    }
+
 }
--- le-domain/src/main/java/com/latticeengines/domain/exposed/metadata/PrimaryKey.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/metadata/PrimaryKey.java
@@ -1,11 +1,5 @@
 package com.latticeengines.domain.exposed.metadata;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -15,7 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
-import javax.persistence.Transient;
 
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
@@ -23,21 +16,12 @@ import org.hibernate.annotations.OnDeleteAction;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.latticeengines.common.exposed.graph.GraphNode;
-import com.latticeengines.common.exposed.visitor.Visitor;
-import com.latticeengines.common.exposed.visitor.VisitorContext;
 
 
 @Entity
 @javax.persistence.Table(name = "METADATA_PRIMARY_KEY")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-public class PrimaryKey implements AttributeOwner {
-    
-    private Long pid;
-    private String name;
-    private String displayName;
-    private List<String> attributes = new ArrayList<>();
-    private Table table;
+public class PrimaryKey extends AttributeOwner {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -79,52 +63,6 @@ public class PrimaryKey implements AttributeOwner {
         this.displayName = displayName;
     }
 
-    @JsonProperty("attributes")
-    @Transient
-    @Override
-    public List<String> getAttributes() {
-        return attributes;
-    }
-
-    @JsonProperty("attributes")
-    @Override
-    public void setAttributes(List<String> attributes) {
-        this.attributes = attributes;
-    }
-
-    @Override
-    public void accept(Visitor visitor, VisitorContext ctx) {
-        visitor.visit(this, ctx);
-    }
-
-    @Override
-    @JsonIgnore
-    @Transient
-    public Collection<? extends GraphNode> getChildren() {
-        return new ArrayList<>();
-    }
-
-    @Override
-    @JsonIgnore
-    @Transient
-    public Map<String, Collection<? extends GraphNode>> getChildMap() {
-        return new HashMap<>();
-    }
-
-    @JsonIgnore
-    @Transient
-    @Override
-    public String[] getAttributeNames() {
-        String[] attrs = new String[attributes.size()];
-        attributes.toArray(attrs);
-        return attrs;
-    }
-
-    @Override
-    public void addAttribute(String attribute) {
-        attributes.add(attribute);
-    }
-
     @JsonIgnore
     @OneToOne(cascade = { CascadeType.MERGE, CascadeType.REMOVE })
     @JoinColumn(name = "FK_TABLE_ID", nullable = false)
@@ -138,4 +76,5 @@ public class PrimaryKey implements AttributeOwner {
         this.table = table;
     }
     
+
 }
