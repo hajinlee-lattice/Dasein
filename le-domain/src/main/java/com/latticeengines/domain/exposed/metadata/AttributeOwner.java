8aee060b8b59f43fa67b943b2433087793a0c370
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2015-10-13 02:26:35 +0000

    Review Request 1300: Fix attribute columns not getting persisted.

--- le-domain/src/main/java/com/latticeengines/domain/exposed/metadata/AttributeOwner.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/metadata/AttributeOwner.java
@@ -1,12 +1,25 @@
 package com.latticeengines.domain.exposed.metadata;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToOne;
 import javax.persistence.Transient;
+
+import org.apache.commons.lang.StringUtils;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -16,6 +29,7 @@ import com.latticeengines.common.exposed.visitor.VisitorContext;
 import com.latticeengines.domain.exposed.dataplatform.HasName;
 import com.latticeengines.domain.exposed.dataplatform.HasPid;
 
+@MappedSuperclass
 public abstract class AttributeOwner implements HasPid, HasName, GraphNode {
     
     protected Long pid;
@@ -24,6 +38,60 @@ public abstract class AttributeOwner implements HasPid, HasName, GraphNode {
     private List<String> attributes = new ArrayList<>();
     protected Table table;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    @Basic(optional = false)
+    @Column(name = "PID", unique = true, nullable = false)
+    @Override
+    public Long getPid() {
+        return pid;
+    }
+
+    @Override
+    @JsonIgnore
+    public void setPid(Long pid) {
+        this.pid = pid;
+    }
+
+    @Column(name = "NAME", unique = false, nullable = false)
+    @Override
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "DISPLAY_NAME", nullable = false)
+    @JsonProperty("display_name")
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @JsonProperty("display_name")
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    @JsonIgnore
+    @OneToOne
+    @JoinColumn(name = "FK_TABLE_ID", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    public Table getTable() {
+        return table;
+    }
+    
+    @JsonIgnore
+    public void setTable(Table table) {
+        this.table = table;
+    }
+
+    
     @JsonProperty("attributes")
     @Transient
     public List<String> getAttributes() {
@@ -63,6 +131,17 @@ public abstract class AttributeOwner implements HasPid, HasName, GraphNode {
 
     public void addAttribute(String attribute) {
         attributes.add(attribute);
+    }
+    
+    @Column(name = "ATTRIBUTES", nullable = false, length = 2048)
+    public String getAttributesAsStr() {
+        return StringUtils.join(attributes, ",");
+    }
+    
+    public void setAttributesAsStr(String attrStr) {
+        if (attrStr != null) {
+            attributes.addAll(Arrays.asList(attrStr.split(",")));
+        }
     }
 
 }
--- le-domain/src/main/java/com/latticeengines/domain/exposed/metadata/PrimaryKey.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/metadata/PrimaryKey.java
@@ -1,79 +1,13 @@
 package com.latticeengines.domain.exposed.metadata;
 
-import javax.persistence.Basic;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 
 @Entity
 @javax.persistence.Table(name = "METADATA_PRIMARY_KEY")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class PrimaryKey extends AttributeOwner {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @JsonIgnore
-    @Basic(optional = false)
-    @Column(name = "PID", unique = true, nullable = false)
-    @Override
-    public Long getPid() {
-        return pid;
-    }
-
-    @Override
-    @JsonIgnore
-    public void setPid(Long pid) {
-        this.pid = pid;
-    }
-
-    @Column(name = "NAME", unique = false, nullable = false)
-    @Override
-    @JsonProperty("name")
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    @JsonProperty("name")
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Column(name = "DISPLAY_NAME", nullable = false)
-    @JsonProperty("display_name")
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    @JsonProperty("display_name")
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
-
-    @JsonIgnore
-    @OneToOne
-    @JoinColumn(name = "FK_TABLE_ID", nullable = false)
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    public Table getTable() {
-        return table;
-    }
-    
-    @JsonIgnore
-    public void setTable(Table table) {
-        this.table = table;
-    }
-    
 
 }
