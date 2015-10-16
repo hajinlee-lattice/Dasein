873c28ceb3772d7e53c283f8aa7f3c37aa56476a
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2015-10-16 16:39:43 +0000

    Review Request 1318: Fix issues with FileSystem closed in production.

--- le-common/src/main/java/com/latticeengines/common/exposed/util/HdfsUtils.java
+++ le-common/src/main/java/com/latticeengines/common/exposed/util/HdfsUtils.java
@@ -53,53 +53,59 @@ public class HdfsUtils {
     };
 
     public static final void mkdir(Configuration configuration, String dir) throws Exception {
-        FileSystem fs = FileSystem.get(configuration);
-        fs.mkdirs(new Path(dir));
+        try (FileSystem fs = FileSystem.newInstance(configuration)) {
+            fs.mkdirs(new Path(dir));
+        }
     }
 
     public static final void rmdir(Configuration configuration, String dir) throws Exception {
-        FileSystem fs = FileSystem.get(configuration);
-        fs.delete(new Path(dir), true);
+        try (FileSystem fs = FileSystem.newInstance(configuration)) {
+            fs.delete(new Path(dir), true);
+        }
     }
 
     public static final String getHdfsFileContents(Configuration configuration, String hdfsPath) throws Exception {
-        FileSystem fs = FileSystem.get(configuration);
-        Path schemaPath = new Path(hdfsPath);
+        try (FileSystem fs = FileSystem.newInstance(configuration)) {
+            Path schemaPath = new Path(hdfsPath);
 
-        try (InputStream is = fs.open(schemaPath)) {
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
-            StreamUtils.copy(is, os);
-            return new String(os.toByteArray());
+            try (InputStream is = fs.open(schemaPath)) {
+                try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+                    StreamUtils.copy(is, os);
+                    return new String(os.toByteArray());
+                }
+            }
         }
-
     }
 
     public static final void copyLocalToHdfs(Configuration configuration, String localPath, String hdfsPath)
             throws Exception {
-        FileSystem fs = FileSystem.get(configuration);
-        fs.copyFromLocalFile(new Path(localPath), new Path(hdfsPath));
+        try (FileSystem fs = FileSystem.newInstance(configuration)) {
+            fs.copyFromLocalFile(new Path(localPath), new Path(hdfsPath));
+        }
     }
 
     public static final void copyHdfsToLocal(Configuration configuration, String hdfsPath, String localPath)
             throws Exception {
-        FileSystem fs = FileSystem.get(configuration);
-        fs.copyToLocalFile(new Path(hdfsPath), new Path(localPath));
+        try (FileSystem fs = FileSystem.newInstance(configuration)) {
+            fs.copyToLocalFile(new Path(hdfsPath), new Path(localPath));
+        }
     }
 
     public static final void writeToFile(Configuration configuration, String hdfsPath, String contents)
             throws Exception {
-        FileSystem fs = FileSystem.get(configuration);
-        Path filePath = new Path(hdfsPath);
+        try (FileSystem fs = FileSystem.newInstance(configuration)) {
+            Path filePath = new Path(hdfsPath);
 
-        try (BufferedWriter br = new BufferedWriter(new OutputStreamWriter(fs.create(filePath, true)))) {
-            br.write(contents);
+            try (BufferedWriter br = new BufferedWriter(new OutputStreamWriter(fs.create(filePath, true)))) {
+                br.write(contents);
+            }
         }
     }
 
     public static final boolean fileExists(Configuration configuration, String hdfsPath) throws Exception {
-        FileSystem fs = FileSystem.get(configuration);
-        return fs.exists(new Path(hdfsPath));
-
+        try (FileSystem fs = FileSystem.newInstance(configuration)) {
+            return fs.exists(new Path(hdfsPath));
+        }
     }
 
     public static final List<String> getFilesForDir(Configuration configuration, String hdfsDir) throws Exception {
@@ -123,42 +129,44 @@ public class HdfsUtils {
 
     public static final List<String> getFilesForDir(Configuration configuration, String hdfsDir,
             HdfsFilenameFilter filter) throws Exception {
-        FileSystem fs = FileSystem.get(configuration);
-        FileStatus[] statuses = fs.listStatus(new Path(hdfsDir));
-        List<String> filePaths = new ArrayList<String>();
-        for (FileStatus status : statuses) {
-            Path filePath = status.getPath();
-            boolean accept = true;
+        try (FileSystem fs = FileSystem.newInstance(configuration)) {
+            FileStatus[] statuses = fs.listStatus(new Path(hdfsDir));
+            List<String> filePaths = new ArrayList<String>();
+            for (FileStatus status : statuses) {
+                Path filePath = status.getPath();
+                boolean accept = true;
 
-            if (filter != null) {
-                accept = filter.accept(filePath.getName());
+                if (filter != null) {
+                    accept = filter.accept(filePath.getName());
+                }
+                if (accept) {
+                    filePaths.add(filePath.toString());
+                }
             }
-            if (accept) {
-                filePaths.add(filePath.toString());
-            }
+
+            return filePaths;
         }
-
-        return filePaths;
     }
 
     public static final List<String> getFilesForDir(Configuration configuration, String hdfsDir, HdfsFileFilter filter)
             throws Exception {
-        FileSystem fs = FileSystem.get(configuration);
-        FileStatus[] statuses = fs.listStatus(new Path(hdfsDir));
-        List<String> filePaths = new ArrayList<String>();
-        for (FileStatus status : statuses) {
-            Path filePath = status.getPath();
-            boolean accept = true;
+        try (FileSystem fs = FileSystem.newInstance(configuration)) {
+            FileStatus[] statuses = fs.listStatus(new Path(hdfsDir));
+            List<String> filePaths = new ArrayList<String>();
+            for (FileStatus status : statuses) {
+                Path filePath = status.getPath();
+                boolean accept = true;
 
-            if (filter != null) {
-                accept = filter.accept(status);
+                if (filter != null) {
+                    accept = filter.accept(status);
+                }
+                if (accept) {
+                    filePaths.add(filePath.toString());
+                }
             }
-            if (accept) {
-                filePaths.add(filePath.toString());
-            }
+
+            return filePaths;
         }
-
-        return filePaths;
     }
 
     public static final List<String> getFilesForDirRecursive(Configuration configuration, String hdfsDir,
@@ -168,19 +176,20 @@ public class HdfsUtils {
 
     public static final List<String> getFilesForDirRecursive(Configuration configuration, String hdfsDir,
             HdfsFileFilter filter, boolean returnFirstMatch) throws Exception {
-        FileSystem fs = FileSystem.get(configuration);
-        FileStatus[] statuses = fs.listStatus(new Path(hdfsDir));
-        Set<String> filePaths = new HashSet<String>();
-        for (FileStatus status : statuses) {
-            if (status.isDirectory()) {
-                filePaths.addAll(getFilesForDir(configuration, status.getPath().toString(), filter));
-                if (returnFirstMatch && filePaths.size() > 0) {
-                    break;
+        try (FileSystem fs = FileSystem.newInstance(configuration)) {
+            FileStatus[] statuses = fs.listStatus(new Path(hdfsDir));
+            Set<String> filePaths = new HashSet<String>();
+            for (FileStatus status : statuses) {
+                if (status.isDirectory()) {
+                    filePaths.addAll(getFilesForDir(configuration, status.getPath().toString(), filter));
+                    if (returnFirstMatch && filePaths.size() > 0) {
+                        break;
+                    }
+                    filePaths.addAll(getFilesForDirRecursive(configuration, status.getPath().toString(), filter));
                 }
-                filePaths.addAll(getFilesForDirRecursive(configuration, status.getPath().toString(), filter));
             }
+            return new ArrayList<>(filePaths);
         }
-        return new ArrayList<>(filePaths);
     }
 
     public static final String getApplicationLog(Configuration configuration, String user, String applicationId)
@@ -201,57 +210,61 @@ public class HdfsUtils {
 
     private static InputStream getInputStream(Configuration configuration, String user, String applicationId)
             throws IOException {
-        FileSystem fs = FileSystem.get(configuration);
-        String hdfsPath = configuration.get("yarn.nodemanager.remote-app-log-dir") + "/" + user + "/logs/"
-                + applicationId;
-        String encoding = configuration.get("yarn.nodemanager.log-aggregation.compression-type");
-        Path schemaPath = new Path(hdfsPath);
-        RemoteIterator<LocatedFileStatus> iterator = fs.listFiles(schemaPath, false);
-        InputStream is = null;
-        while (iterator.hasNext()) {
-            LocatedFileStatus file = iterator.next();
-            Path filePath = file.getPath();
-            switch (LogFileEncodingType.valueOf(encoding.toUpperCase())) {
-            case NONE:
-                is = fs.open(filePath);
-                break;
-            case GZ:
-                is = new GZIPInputStream(fs.open(filePath));
-                break;
+        try (FileSystem fs = FileSystem.newInstance(configuration)) {
+            String hdfsPath = configuration.get("yarn.nodemanager.remote-app-log-dir") + "/" + user + "/logs/"
+                    + applicationId;
+            String encoding = configuration.get("yarn.nodemanager.log-aggregation.compression-type");
+            Path schemaPath = new Path(hdfsPath);
+            RemoteIterator<LocatedFileStatus> iterator = fs.listFiles(schemaPath, false);
+            InputStream is = null;
+            while (iterator.hasNext()) {
+                LocatedFileStatus file = iterator.next();
+                Path filePath = file.getPath();
+                switch (LogFileEncodingType.valueOf(encoding.toUpperCase())) {
+                case NONE:
+                    is = fs.open(filePath);
+                    break;
+                case GZ:
+                    is = new GZIPInputStream(fs.open(filePath));
+                    break;
+                }
             }
+            return is;
         }
-        return is;
     }
 
     public static List<String> getFilesByGlob(Configuration configuration, String globPath) throws IOException {
-        FileSystem fs = FileSystem.get(configuration);
-        FileStatus[] statuses = fs.globStatus(new Path(globPath));
-        List<String> filePaths = new ArrayList<>();
-        if (statuses == null) {
+        try (FileSystem fs = FileSystem.newInstance(configuration)) {
+            FileStatus[] statuses = fs.globStatus(new Path(globPath));
+            List<String> filePaths = new ArrayList<>();
+            if (statuses == null) {
+                return filePaths;
+            }
+            for (FileStatus status : statuses) {
+                Path filePath = status.getPath();
+                filePaths.add(Path.getPathWithoutSchemeAndAuthority(filePath).toString());
+            }
             return filePaths;
         }
-        for (FileStatus status : statuses) {
-            Path filePath = status.getPath();
-            filePaths.add(Path.getPathWithoutSchemeAndAuthority(filePath).toString());
-        }
-        return filePaths;
-
     }
 
     public static boolean moveFile(Configuration configuration, String src, String dst) throws IOException {
-        FileSystem fs = FileSystem.get(configuration);
-        return fs.rename(new Path(src), new Path(dst));
+        try (FileSystem fs = FileSystem.newInstance(configuration)) {
+            return fs.rename(new Path(src), new Path(dst));
+        }
     }
 
     public static boolean copyFiles(Configuration configuration, String src, String dst)
             throws IllegalArgumentException, IOException {
-        return FileUtil.copy(FileSystem.get(configuration), new Path(src), FileSystem.get(configuration),
-                new Path(dst), false, false, configuration);
+        try (FileSystem fs = FileSystem.newInstance(configuration)) {
+            return FileUtil.copy(fs, new Path(src), fs, new Path(dst), false, false, configuration);
+        }
     }
 
     public static FileChecksum getCheckSum(Configuration configuration, String path) throws IOException {
-        FileSystem fs = FileSystem.get(configuration);
-        return fs.getFileChecksum(new Path(path));
+        try (FileSystem fs = FileSystem.newInstance(configuration)) {
+            return fs.getFileChecksum(new Path(path));
+        }
     }
 
 }
