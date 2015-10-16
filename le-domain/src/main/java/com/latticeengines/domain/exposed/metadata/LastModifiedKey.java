b9cc978bef172e3a7c6d7df6a3d54f6fd0112288
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2015-10-16 22:15:50 +0000

    review request 1341: Add time stamp column for LastModifiedKey Table

--- le-domain/src/main/java/com/latticeengines/domain/exposed/metadata/LastModifiedKey.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/metadata/LastModifiedKey.java
@@ -1,12 +1,37 @@
 package com.latticeengines.domain.exposed.metadata;
 
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
 @javax.persistence.Table(name = "METADATA_LASTMODIFIED_KEY")
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 public class LastModifiedKey extends AttributeOwner {
+
+    private Long lastModifiedTimeStemp;
+
+    public LastModifiedKey() {
+    }
+
+    public LastModifiedKey(List<String> attributes, long lastModifiedTimeStemp) {
+        this.setAttributes(attributes);
+        this.setLastModifiedTimestamp(lastModifiedTimeStemp);
+    }
+
+    @Column(name = "LAST_MODIFIED_TS", nullable = false)
+    @JsonProperty("last_modified_ts")
+    public Long getLastModifiedTimestamp() {
+        return lastModifiedTimeStemp;
+    }
+
+    @JsonProperty("last_modified_ts")
+    public void setLastModifiedTimestamp(Long lastModifiedTimeStemp) {
+        this.lastModifiedTimeStemp = lastModifiedTimeStemp;
+    }
 
 }
