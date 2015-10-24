0ac665b0f2b89c4c2ff5dbbd3a7b119b152a025b
lming <lming@479f5059-62a3-4c39-9342-9852e3a68435>
2015-10-24 01:39:26 +0000

    Review Request 1371: playMaker/Data platform integration

--- le-dataplatform/src/test/python/pd344trainingtest.py
+++ le-dataplatform/src/test/python/pd344trainingtest.py
@@ -29,5 +29,5 @@ class PD344TrainingTest(TrainingTestBase):
             if entry["Key"].find('STPipelineBinary') >= 0:
                 pipeline = pickle.load(open(fileName, "r"))
                 self.assertTrue(isinstance(pipeline.getPipeline()[3].getModel(), RandomForestClassifier), "clf not instance of sklearn RandomForestClassifier.")
-            elif entry["Key"].find('encoder') >= 0 or entry["Key"].find('pipelinesteps') >= 0: self.assertTrue(filecmp.cmp(fileName, './lepipeline.tar.gz/' + entry["Key"]))
+            elif entry["Key"].find('encoder') >= 0 or entry["Key"].find('pipelinesteps') >= 0 or entry["Key"].find('aggregatedmodel') >= 0: self.assertTrue(filecmp.cmp(fileName, './lepipeline.tar.gz/' + entry["Key"]))
             else: self.assertTrue(filecmp.cmp(fileName, './' + entry["Key"]))
