2948941548d9aa0c72d4c965b1a4375af67ed7d4
hmohsen <hmohsen@479f5059-62a3-4c39-9342-9852e3a68435>
2015-10-31 00:11:35 +0000

    Review Request 1486: Score Normalization

--- le-dataplatform/src/main/python/leframework/model/states/normalizationgenerator.py
+++ le-dataplatform/src/main/python/leframework/model/states/normalizationgenerator.py
@@ -23,21 +23,30 @@ class NormalizationGenerator(State, JsonGenBase):
             self.logger.info("Scores are not provided. Normalization buckets cannot be generated.")
 
     def buildMappingFunctionData(self, probabilityList):
-        bins = self.createIndexSequence(len(probabilityList), 100)
-        orderedProb = sorted(probabilityList, reverse = False)
-        cumulativePercent = [1.0 * bins[i + 1] / bins[len(bins) - 1] for i in range(len(bins) - 1)]
-
-        startProb = [orderedProb[bins[i]] for i in range(len(bins) - 1)]
-        endProb = startProb[:]
-        del endProb[0]
-        maxProb = orderedProb[len(probabilityList)-1]
-        endProb.append(maxProb)
+        bins = self.createIndexSequence(len(probabilityList), min((len(probabilityList)-1), 100))
+        
+        if len(bins) > 1:
+            orderedProb = sorted(probabilityList, reverse = False)
+            cumulativePercent = [1.0 * bins[i + 1] / bins[len(bins) - 1] for i in range(len(bins) - 1)]
+    
+            startProb = [orderedProb[bins[i]] for i in range(len(bins) - 1)]
+            endProb = startProb[:]
+            del endProb[0]
+            maxProb = orderedProb[len(probabilityList) - 1]
+            endProb.append(maxProb)
 
         return[{"StartProbability" : startProb[i], "EndProbability" : endProb[i], "CumulativePercentage" : cumulativePercent[i]} for i in range(len(startProb))]
 
     def createIndexSequence(self, number, rawSplits):
-        binSize = int(number / (rawSplits+1) / 1.0)
-        numBins=int(number / binSize)
+        if number == 0:
+            return []
+        
+        binSize = int(number / (rawSplits + 1) / 1.0)
+        
+        if binSize < 1:
+            binSize = 1
+            
+        numBins = int(number / binSize)
 
         sp=[binSize * i for i in range(numBins + 1)]
         sp[numBins] = number
