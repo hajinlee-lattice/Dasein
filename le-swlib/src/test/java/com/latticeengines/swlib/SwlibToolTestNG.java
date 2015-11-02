05f2fe78c38c22fab1977e76cf4db60b3718e84f
bross <bross@479f5059-62a3-4c39-9342-9852e3a68435>
2015-11-02 15:26:01 +0000

    Review Request 1524:  Use functional profile for SwLibToolUnitTestNG because it uses hadoop.

--- le-swlib/src/test/java/com/latticeengines/swlib/SwlibToolTestNG.java
+++ le-swlib/src/test/java/com/latticeengines/swlib/SwlibToolTestNG.java
@@ -8,25 +8,24 @@ import org.testng.annotations.Test;
 
 import com.latticeengines.common.exposed.util.HdfsUtils;
 
-public class SwlibToolUnitTestNG {
-    
-    @BeforeClass(groups = "unit")
+public class SwlibToolTestNG {
+
+    @BeforeClass(groups = "functional")
     public void setup() throws Exception {
         Configuration config = new Configuration();
         HdfsUtils.rmdir(config, "/app/swlib/dataflow");
     }
 
-    @Test(groups = "unit")
+    @Test(groups = "functional")
     public void main() throws Exception {
-        String jarFile = ClassLoader.getSystemResource("com/latticeengines/swlib/le-serviceflows-prospectdiscovery.jar").getPath();
-        SwlibTool.main(new String[] {
-                "-o", "install", //
+        String jarFile = ClassLoader
+                .getSystemResource("com/latticeengines/swlib/le-serviceflows-prospectdiscovery.jar").getPath();
+        SwlibTool.main(new String[] { "-o", "install", //
                 "-m", "dataflow", //
                 "-g", "com.latticeengines", //
                 "-a", "le-serviceflows", //
                 "-v", "1.0.0", //
-                "-f", jarFile 
-        });
+                "-f", jarFile });
         assertTrue(HdfsUtils.fileExists(new Configuration(), //
                 "/app/swlib/dataflow/com/latticeengines/le-serviceflows/1.0.0/le-serviceflows-1.0.0.jar"));
         assertTrue(HdfsUtils.fileExists(new Configuration(), //
