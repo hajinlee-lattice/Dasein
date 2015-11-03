83fb1a9b8da6266b69b5b7ce0abf3894b13eb6c6
smeng <smeng@479f5059-62a3-4c39-9342-9852e3a68435>
2015-11-03 02:05:14 +0000

    Modify Dante UI Test to support both prod servers review request 1539

--- le-tools/DanteTest/Property.py
+++ le-tools/DanteTest/Property.py
@@ -8,19 +8,21 @@ class DanteEnvironments(object):
     parser = SafeConfigParser()
     parser.read('config.ini')
 
-    #properties defination
-    #Configuration for Dante DB
-    Dante_DB_Server_Server=parser.get('Dante_DB', 'Dante_DB_Server')
-    Dante_DB_User=parser.get('Dante_DB', 'Dante_DB_User')
-    Dante_DB_PWD=parser.get('Dante_DB', 'Dante_DB_PWD')
-    Dante_DB_Name=parser.get('Dante_DB', 'Dante_DB_Name')
-    #Configuration for sales force
-    Sales_Force_URL=parser.get('Sales_Force', 'Sales_Force_URL')
-    Sales_Force_User=parser.get('Sales_Force', 'Sales_Force_User')
-    Sales_Force_PWD=parser.get('Sales_Force', 'Sales_Force_PWD')
-    Sales_Force_DT_service=parser.get('Sales_Force', 'Sales_Force_DT_service')
-    Browser_Type=parser.get('Sales_Force', 'Browser_Type')
+    Prod_Dante_Server = parser.get('Sales_Force', 'prod_dante_serer');
 
-    PROD_Dante_URL="https://bis.lattice-engines.com/buyerinsights/index.aspx?sin=DemoSession&serverurl=https%3A%2F%2Flbi.na31.visual.force.com%2Fservices%2FSoap%2Fu%2F34.0%2F00D37000000Pd2mEAC&Directory=salesforce&userlink=00537000000Rji5AAC&Recommendation=99&HasSalesprism=false&CustomSettings={%22SupportEmail%22%3A%22noreply%40salesforce.com%22%2C%22ShowScore%22%3A%20false%2C%22ShowLift%22%3A%20false%2C%22NoPlaysMessage%22%3A%22No%20Plays%20Found.%22%2C%22NoDataMessage%22%3A%22No%20Data%20Found.%22%2C%22hideNavigation%22%3A%20true%2C%22DefaultTab%22%3A%22TalkingPoints%22}"
-    Conf_file_Dante_Page='.\Config\DantePage.yml'
+    # properties defination
+    # Configuration for Dante DB
+    Dante_DB_Server_Server = parser.get('Dante_DB', 'Dante_DB_Server')
+    Dante_DB_User = parser.get('Dante_DB', 'Dante_DB_User')
+    Dante_DB_PWD = parser.get('Dante_DB', 'Dante_DB_PWD')
+    Dante_DB_Name = parser.get('Dante_DB', 'Dante_DB_Name')
+    # Configuration for sales force
+    Sales_Force_URL = parser.get('Sales_Force', 'Sales_Force_URL')
+    Sales_Force_User = parser.get('Sales_Force', 'Sales_Force_User')
+    Sales_Force_PWD = parser.get('Sales_Force', 'Sales_Force_PWD')
+    Sales_Force_DT_service = parser.get('Sales_Force', 'Sales_Force_DT_service')
+    Browser_Type = parser.get('Sales_Force', 'Browser_Type')
+
+    PROD_Dante_URL = 'https://' + Prod_Dante_Server + '?sin=DemoSession&serverurl=https%3A%2F%2Flbi.na31.visual.force.com%2Fservices%2FSoap%2Fu%2F34.0%2F00D37000000Pd2mEAC&Directory=salesforce&userlink=00537000000Rji5AAC&Recommendation=99&HasSalesprism=false&CustomSettings={%22SupportEmail%22%3A%22noreply%40salesforce.com%22%2C%22ShowScore%22%3A%20false%2C%22ShowLift%22%3A%20false%2C%22NoPlaysMessage%22%3A%22No%20Plays%20Found.%22%2C%22NoDataMessage%22%3A%22No%20Data%20Found.%22%2C%22hideNavigation%22%3A%20true%2C%22DefaultTab%22%3A%22TalkingPoints%22}'
+    Conf_file_Dante_Page = '.\Config\DantePage.yml'
 
--- le-tools/DanteTest/Tests/ProdDanteUISmoke.py
+++ le-tools/DanteTest/Tests/ProdDanteUISmoke.py
@@ -7,33 +7,33 @@ class Test(unittest.TestCase):
 
     def testcase_1_VerifyTalkingPoints(self):
         print '----PROD Dante test case for Talking Points----'
-        dp_TP=DantePageHelper()
+        dp_TP = DantePageHelper()
         dp_TP.OpenURL(DanteEnvironments.PROD_Dante_URL)
         dp_TP.WaitElement('Xpath=//a[text()="Talking Points"]')
         dp_TP.ClickLink('Xpath=//a[text()="Talking Points"]')
-        TP_infos_page=dp_TP.GetTalkingPoints()
-        data_TP=DanteDataHelper()
-        TP_infos_expected=data_TP.GetPRODExpectedTalkingPoints('.\Config\ExpectedResult.xml')
+        TP_infos_page = dp_TP.GetTalkingPoints()
+        data_TP = DanteDataHelper()
+        TP_infos_expected = data_TP.GetPRODExpectedTalkingPoints('.\Config\ExpectedResult.xml')
         dp_TP.Close_browser()
-        result=cmp(TP_infos_page,TP_infos_expected)
-        assert result==0, "Talking Points from Page is not same with expected Result, Actully Result is \n----------------------\n"+str(TP_infos_page)+"\n-------------------------\n But Expected Result is \n-----------------\n"+str(TP_infos_expected)+"\n-----------------"
+        result = cmp(TP_infos_page, TP_infos_expected)
+        assert result == 0, "Talking Points from Page is not same with expected Result, Actully Result is \n----------------------\n" + str(TP_infos_page) + "\n-------------------------\n But Expected Result is \n-----------------\n" + str(TP_infos_expected) + "\n-----------------"
 
 
     def testcase_2_VerifyBuyingSignal(self):
         print '----PROD Dante test case for Buying Signals----'
-        dp_BS=DantePageHelper()
+        dp_BS = DantePageHelper()
         dp_BS.OpenURL(DanteEnvironments.PROD_Dante_URL)
         dp_BS.WaitElement('Xpath=//a[text()="Buying Signals"]')
         dp_BS.ClickLink('Xpath=//a[text()="Buying Signals"]')
-        BS_infos_page=dp_BS.GetBuyingSignalsInfo()
-        data_BS=DanteDataHelper()
-        BS_infos_expected=data_BS.GetPRODExpectedBuyingSingal('.\Config\ExpectedResult.xml')
+        BS_infos_page = dp_BS.GetBuyingSignalsInfo()
+        data_BS = DanteDataHelper()
+        BS_infos_expected = data_BS.GetPRODExpectedBuyingSingal('.\Config\ExpectedResult.xml')
         dp_BS.Close_browser()
-        result=cmp(BS_infos_page,BS_infos_expected)
-        assert result==0, "Buying Signal from Page is not same with expected Result, Actully Result is \n----------------------\n"+str(BS_infos_page)+"\n-------------------------\n But Expected Result is \n-----------------\n"+str(BS_infos_expected)+"\n-----------------"
+        result = cmp(BS_infos_page, BS_infos_expected)
+        assert result == 0, "Buying Signal from Page is not same with expected Result, Actully Result is \n----------------------\n" + str(BS_infos_page) + "\n-------------------------\n But Expected Result is \n-----------------\n" + str(BS_infos_expected) + "\n-----------------"
 
 
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
+    # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
