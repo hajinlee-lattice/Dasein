6e9dd4ca0df1630b7843fcc32a5f3a3d50cefc14
bnguyen <bnguyen@479f5059-62a3-4c39-9342-9852e3a68435>
2015-11-06 20:46:27 +0000

    Review Request 1590: workflowapi microservice

--- le-workflowapi/src/test/java/com/latticeengines/workflowapi/flows/FitModelWorkflowTestNG.java
+++ le-workflowapi/src/test/java/com/latticeengines/workflowapi/flows/FitModelWorkflowTestNG.java
@@ -1,39 +1,9 @@
 package com.latticeengines.workflowapi.flows;
 
-import static org.testng.Assert.assertEquals;
-
-import org.springframework.batch.core.BatchStatus;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.testng.annotations.Test;
-
-import com.latticeengines.domain.exposed.workflow.WorkflowId;
 import com.latticeengines.workflowapi.functionalframework.WorkflowApiFunctionalTestNGBase;
-import com.latticeengines.workflowapi.steps.prospectdiscovery.BaseFitModelStepConfiguration;
 
 public class FitModelWorkflowTestNG extends WorkflowApiFunctionalTestNGBase {
 
-    private static final String DEMO_CUSTOMERSPACE = "DemoContract.DemoTenant.Production";
-    private static final String microServiceHostPort = "http://localhost:8080";
-    private static final String modelingServiceHdfsBaseDir = "/user/s-analytics/customers/";
 
-    @Autowired
-    private FitModelWorkflow fitModelWorkflow;
-
-    @Test(groups = "functional", enabled = true)
-    public void testWorkflow() throws Exception {
-        BaseFitModelStepConfiguration baseFitModelStepConfiguration = new BaseFitModelStepConfiguration();
-        baseFitModelStepConfiguration.setCustomerSpace(DEMO_CUSTOMERSPACE);
-        baseFitModelStepConfiguration.setMicroServiceHostPort(microServiceHostPort);
-        baseFitModelStepConfiguration.setModelingServiceHdfsBaseDir(modelingServiceHdfsBaseDir);
-
-        FitModelWorkflowConfiguration workflowConfig = new FitModelWorkflowConfiguration.Builder()
-                .setBaseFitModelStepConfiguration(baseFitModelStepConfiguration).build();
-//        workflowConfig.setContainerConfiguration(fitModelWorkflow.name(), DEMO_CUSTOMERSPACE,
-//                "FitModelWorkflowTest_submitWorkflow");
-
-        WorkflowId workflowId = workflowService.start(fitModelWorkflow.name(), workflowConfig);
-        BatchStatus status = workflowService.waitForCompletion(workflowId, MAX_MILLIS_TO_WAIT).getStatus();
-        assertEquals(status, BatchStatus.COMPLETED);
-    }
 
 }
