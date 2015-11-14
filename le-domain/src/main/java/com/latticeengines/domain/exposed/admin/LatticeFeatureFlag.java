374aabc32e564c97ce49c68094549a9766c63708
ygao <ygao@479f5059-62a3-4c39-9342-9852e3a68435>
2015-11-14 00:45:14 +0000

    review request 1664 DP-1453 & DP-1454 Mainly about back end work of feature flag and mapping from products to components; get rid of extra space

--- le-domain/src/main/java/com/latticeengines/domain/exposed/admin/LatticeFeatureFlag.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/admin/LatticeFeatureFlag.java
@@ -9,7 +9,7 @@ public enum LatticeFeatureFlag {
 
     DANTE("Dante", "Dante"), //
     QUOTA("Quota", "Quota"), //
-    TARGET_MARKET("Target Market", "Target Market"), //
+    TARGET_MARKET("TargetMarket", "Target Market"), //
     USE_EAI_VALIDATE_CREDENTIAL("ValidateCreds", "Use Eai to valiate source credentials");
 
     private String name;
