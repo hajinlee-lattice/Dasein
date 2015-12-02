c943c7bd662d2f49be09a47172d420644b201d2c
hmohsen <hmohsen@479f5059-62a3-4c39-9342-9852e3a68435>
2015-12-02 18:11:40 +0000

    Review Request 1806: Boolean cols changes

--- le-dataplatform/src/main/python/pipeline/pipeline.py
+++ le-dataplatform/src/main/python/pipeline/pipeline.py
@@ -30,7 +30,8 @@ def setupPipeline(metadata, stringColumns, targetColumn):
     # categoricalColumns refer to the columns that are categorical from the metadata
     # We need to transform the physical strings into numbers
     columnsToTransform = set(stringColumns - set(categoricalColumns.keys()))
-    steps = [EnumeratedColumnTransformStep(categoricalColumns), ColumnTypeConversionStep(columnsToTransform), ImputationStep(continuousColumns, {}, [], [], [], targetColumn)]
+    imputationValues = {}
+    steps = [EnumeratedColumnTransformStep(categoricalColumns), ColumnTypeConversionStep(columnsToTransform), ImputationStep(continuousColumns, imputationValues, targetColumn)]
     pipeline = Pipeline(steps)
     
     scoringSteps = steps + [ModelStep()]
