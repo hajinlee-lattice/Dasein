2cb4662c748b8d39548ec18a3967ff0975bb9fa5
mmeng <mmeng@479f5059-62a3-4c39-9342-9852e3a68435>
2015-12-03 08:06:46 +0000

    Review Request 1814: change job service to not use mocked json

--- le-ui/projects/prospectdiscovery/app/jobs/JobsService.js
+++ le-ui/projects/prospectdiscovery/app/jobs/JobsService.js
@@ -5,14 +5,13 @@ angular.module('services.jobs', [
     this.GetAllJobs = function() {
         var deferred = $q.defer();
         var result;
-/*
+        
         $http({
             method: 'GET',
             url: '/pls/jobs'
         }).then(
-            function onSuccess(response) { */
-                //var jobs = response.data;
-                var jobs = [{"id":1,"name":null,"description":null,"startTimestamp":1449106526700,"endTimestamp":null,"jobStatus":"Running","jobType":"Loadfile_import","user":"LatticeUser1","steps":null}];
+            function onSuccess(response) {
+                var jobs = response.data;
                 result = {
                     success: true,
                     resultObj: null
@@ -28,26 +27,24 @@ angular.module('services.jobs', [
                         user: rawObj.user
                     };
                 });
-                deferred.resolve(result);/*
+                deferred.resolve(result);
             }, function onError(response) {
 
             }
         );
-*/
         return deferred.promise;
     };
     
     this.getJobStatus = function(jobId) {
         var deferred = $q.defer();
         var result;
-/*
+
         $http({
             method: 'GET',
             url: '/pls/jobs/' + jobId
         }).then(
-            function onSuccess(response) { */
-                //var jobInfo = response.data;
-                var jobInfo = {"id":1,"name":null,"description":null,"startTimestamp":1448930225355,"endTimestamp":null,"jobStatus":"Running","jobType":"Loadfile_import","user":"LatticeUser1","steps":[{"name":null,"description":null,"startTimestamp":1448930225355,"endTimestamp":1448930225355,"jobStatus":"Completed","jobStepType":"Load_data"},{"name":null,"description":null,"startTimestamp":1448930225355,"endTimestamp":1448930225355,"jobStatus":"Completed","jobStepType":"Match_data"},{"name":null,"description":null,"startTimestamp":1448930225355,"endTimestamp":1448930225355,"jobStatus":"Completed","jobStepType":"Generate_insights"},{"name":null,"description":null,"startTimestamp":1448930225355,"endTimestamp":1448930225355,"jobStatus":"Completed","jobStepType":"Create_model"},{"name":null,"description":null,"startTimestamp":1448930225355,"endTimestamp":null,"jobStatus":"Running","jobStepType":"Create_global_target_market"}]};
+            function onSuccess(response) {
+                var jobInfo = response.data;
 
                 result = {
                     success: true,
@@ -59,12 +56,11 @@ angular.module('services.jobs', [
                             stepsCompleted: getStepsCompleted(jobInfo)
                         }
                 };
-                deferred.resolve(result);/*
+                deferred.resolve(result);
             }, function onError(resposne) {
                 
             }
         );
-*/
         return deferred.promise;
         
     };
