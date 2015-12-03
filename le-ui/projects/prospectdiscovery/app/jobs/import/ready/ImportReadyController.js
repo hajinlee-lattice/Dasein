9c63bbd96d8e30c639b6d03b24747465dc59fa5f
mmeng <mmeng@479f5059-62a3-4c39-9342-9852e3a68435>
2015-12-03 23:00:27 +0000

    Review Request 1872: modify styling on job status page and import summary page

--- le-ui/projects/prospectdiscovery/app/jobs/import/ready/ImportReadyController.js
+++ le-ui/projects/prospectdiscovery/app/jobs/import/ready/ImportReadyController.js
@@ -4,6 +4,10 @@ angular.module('controllers.jobs.import.ready', [
 .controller('ImportReadyController', function($scope, $rootScope, $stateParams) {
     $scope.jobId = $stateParams.jobId;
     
+    $scope.completionTimes = { "load_data": "1449132082493", "match_data": "1449132082493",
+            "generate_insights": "1449132082493", "create_model": "1449132082493", "create_global_target_market": "1449132082493" };
+            
+    
     $scope.tables = [
         {
             "name": "ACCOUNTS",
