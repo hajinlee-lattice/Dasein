0d7aca040602d3ad5452d9d4924408cf78d5845f
hmohsen <hmohsen@479f5059-62a3-4c39-9342-9852e3a68435>
2015-12-04 04:57:16 +0000

    Review Request 1806: Boolean Columns Generation and Processing (ENG-8159)

--- le-dataplatform/src/main/python/evpipeline/evpipelinesteps.py
+++ le-dataplatform/src/main/python/evpipeline/evpipelinesteps.py
@@ -3,11 +3,11 @@ import math
 import numpy as np
 import pandas as pd
 import random as rd
-import statsmodels.tsa.ar_model as am
- 
+
 from pipelinefwk import PipelineStep
 from pipelinefwk import get_logger
- 
+from sklearn.decomposition import PCA
+
 logger = get_logger("evpipeline")
   
 class EnumeratedColumnTransformStep(PipelineStep):
@@ -47,102 +47,185 @@ class ColumnTypeConversionStep(PipelineStep):
       
 class ImputationStep(PipelineStep):
     enumMappings_ = dict()
-    imputationValues_ = {}
-    targetColumn_ = ""
-     
-    def __init__(self, enumMappings, imputationValues, targetCol):
+    imputationValues = {}
+    targetColumn = ""    
+    scaling_array = []
+    nullColmean = []
+    component_matrix = []
+    
+    def __init__(self, enumMappings, imputationValues, scalingArray, meanColumn, componentMatrix, targetCol):
         self.enumMappings_ = enumMappings
-        self.imputationValues_ = imputationValues
-        self.targetColumn_ = targetCol
- 
+        self.imputationValues = imputationValues
+        self.targetColumn = targetCol
+        self.scalingArray = scalingArray
+        self.meanColumn = meanColumn
+        self.componentMatrix = componentMatrix
+   
     def transform(self, dataFrame):
+        outputFrame = dataFrame
+
         calculateImputationValues = True
-        if len(self.imputationValues_) != 0:
+        if len(self.imputationValues) != 0:
             calculateImputationValues = False
-               
-        outputFrame, nullValues = self.generateIsNullColumns(dataFrame)
-        if len(self.enumMappings_) > 0:
-            if self.targetColumn_ in outputFrame and calculateImputationValues == True:
-                expectedLabelValue = self.getExpectedLabelValue(outputFrame, self.targetColumn_) 
-             
-            for column, value in self.enumMappings_.iteritems():
-                if column in outputFrame:
-                    if nullValues[column] > 0 and calculateImputationValues == True:
-                        imputationBins = self.createBins(outputFrame[column], outputFrame[self.targetColumn_], 20)
-                        imputationValue = self.matchValue(expectedLabelValue, imputationBins)
-                        self.imputationValues_[column] = imputationValue
- 
-                    try:
-                        outputFrame[column] = outputFrame[column].fillna(self.imputationValues_[column])
-                    except KeyError:
-                        self.imputationValues_[column] = value
-                        outputFrame[column] = outputFrame[column].fillna(self.imputationValues_[column])
- 
+   
+        if dataFrame.isnull().any().any():
+            outputFrame, nullValues = self.generateTransformedBooleanColumns(dataFrame, calculateImputationValues)
+            outputFrame = self.imputeValues(outputFrame, nullValues, calculateImputationValues)
+        
         return outputFrame
-     
-    def getExpectedLabelValue(self, dataFrame, targetCol):
-        zeroLabels = (dataFrame[targetCol] == 0).sum()
-        oneLabels = (dataFrame[targetCol] == 1).sum()
-        expectedLabelValue = float(oneLabels) / (oneLabels + zeroLabels)
-        return expectedLabelValue
-         
+
+      
+    def generateTransformedBooleanColumns(self, dataFrame, calculateImputationValues):
+        outputFrame = dataFrame
+        nullCols, nullValues = self.generateIsNullColumns(outputFrame)
+        
+        if len(nullValues) > 0:
+            if calculateImputationValues:
+                (self.scalingArray, self.meanColumn, self.componentMatrix) = self.nullValuePCA(nullCols, outputFrame[self.targetColumn])
+    
+            nullCol_train_transformed = self.getTransformedMatrix(self.scalingArray, self.meanColumn, self.componentMatrix, nullCols)
+            if len(nullCol_train_transformed > 0):
+                nullCol_train_transformed = pd.DataFrame(nullCol_train_transformed)
+                nullCol_train_transformed.columns = ["Transformed_Boolean_"+str(nullCol_train_transformed.columns.values[i]) for i in range(len(nullCol_train_transformed.columns.values))]
+                outputFrame = pd.concat([outputFrame, nullCol_train_transformed], axis = 1)
+        del nullCols
+        return outputFrame, nullValues
+      
     def generateIsNullColumns(self, dataFrame):
         nullValues = {}
         outputFrame = dataFrame
+        nullColsFrame = pd.DataFrame()
+           
         if len(self.enumMappings_) > 0:
-            for column, _ in self.enumMappings_.iteritems():
+            for column, value in self.enumMappings_.iteritems():
                 if column in outputFrame:
-                    _, nullCount = self.getIsNullColumn(outputFrame[column])
-                    if nullCount > 0:
-                        nullValues[column] = nullCount
-                    else:
-                        nullValues[column] = 0
-        return outputFrame, nullValues
-             
+                    isNullColumn, nullCount = self.getIsNullColumn(outputFrame[column])
+                    nullValues[column] = nullCount
+                    nullColsFrame[column + "_isNull"] = pd.Series(isNullColumn)
+        return nullColsFrame, nullValues
+               
     def getIsNullColumn(self, dataColumn):
         nullCount = 0
         isNullColumn = []
         for i in range(len(dataColumn)):
             if pd.isnull(dataColumn[i]):
                 nullCount = nullCount + 1
-                isNullColumn.append(1)
+                isNullColumn.append(1.0)
             else:
-                isNullColumn.append(0)
+                isNullColumn.append(0.0)
         return isNullColumn, nullCount
-     
+      
+    def getScalingForPCA(self, dataFrame, eventColumn):
+        ratePopulation = np.mean(eventColumn)
+        scalarList = []
+        for col in dataFrame.columns:
+            rateColumn = np.mean(eventColumn[dataFrame[col] > 0])
+            if np.isnan(rateColumn):
+                rateColumn = 0
+                   
+            scalarList.append(rateColumn - ratePopulation)
+        return np.array(scalarList)
+       
+    def getindexofMaxVariance(self, explainedVarianceRatio, thresholdVariance):
+        sumVarianceRatio = 0
+        for i in range(len(explainedVarianceRatio)):
+            sumVarianceRatio += explainedVarianceRatio[i]
+            if sumVarianceRatio > thresholdVariance:
+                return i + 1
+       
+    def getPCAComponents(self, X):
+        pca = PCA()
+        pca.fit(X)
+        explainedVarianceRatio =  pca.explained_variance_ratio_
+        componentsMatrix = pca.components_
+        X_transformed = pca.transform(X)
+           
+        return (explainedVarianceRatio, componentsMatrix, X_transformed)  
+      
+    def nullValuePCA(self, inputDF, eventCol, thresholdVariance=0.98, numberOfColumnsThreshold=5):
+        if (len(inputDF.columns.values) < numberOfColumnsThreshold):
+            numberOfColumnsThreshold = len(inputDF.columns.values) - 1
+
+        scaling_array = self.getScalingForPCA(inputDF, eventCol)
+        inputScaled = np.multiply(inputDF.values, np.ones(inputDF.shape) * scaling_array.T)
+        np.nan_to_num(inputScaled)
+
+        (explainedVarianceRatio, componentsMatrix, inputTransformed) = self.getPCAComponents(inputScaled)
+        indexOfMaxVariance = self.getindexofMaxVariance(explainedVarianceRatio, thresholdVariance)
+        numberOfColumns = min(indexOfMaxVariance, numberOfColumnsThreshold)
+        means = np.mean(inputScaled, axis = 0)
+
+        return (scaling_array, np.mean(inputScaled, axis = 0), componentsMatrix[ : numberOfColumns, :])
+      
+    def imputeValues(self, dataFrame, nullValues, calculateImputationValues):
+        outputFrame = dataFrame
+        if len(self.enumMappings_) > 0:
+            expectedLabelValue = 0.0
+            if self.targetColumn in outputFrame and calculateImputationValues == True:
+                expectedLabelValue = self.getExpectedLabelValue(outputFrame, self.targetColumn) 
+               
+            for column, value in self.enumMappings_.iteritems():
+                if column in outputFrame:
+                    try:
+                        if nullValues[column] > 0 and calculateImputationValues == True:
+                            imputationBins = self.createBins(outputFrame[column], outputFrame[self.targetColumn])
+                            imputationValue = self.matchValue(expectedLabelValue, imputationBins)
+                            self.imputationValues[column] = imputationValue
+       
+                        outputFrame[column] = outputFrame[column].fillna(self.imputationValues[column])
+                    except KeyError:
+                        self.imputationValues[column] = value
+                        outputFrame[column] = outputFrame[column].fillna(self.imputationValues[column])
+        return outputFrame
+      
+    def getExpectedLabelValue(self, dataFrame, targetColumn):
+        zeroLabels = (dataFrame[targetColumn] == 0).sum()
+        oneLabels = (dataFrame[targetColumn] == 1).sum()
+        expectedLabelValue = float(oneLabels) / (oneLabels + zeroLabels)
+        return expectedLabelValue
+      
+    def getTransformedMatrix(self, scalingArray, meanColumn, componentMatrix, originalMatrix):
+        transformedMatrix = np.ndarray(shape = (0, 0)) 
+        
+        if (len(originalMatrix) > 0):
+            scaledMatrix = np.multiply(originalMatrix, np.ones(originalMatrix.shape) * scalingArray.T)
+            centeredMatrix = scaledMatrix - np.ones(originalMatrix.shape) * meanColumn.T
+            transformedMatrix = np.dot(centeredMatrix, componentMatrix.T)
+        return transformedMatrix
+      
     def createIndexSequence(self, number, rawSplits):
         if number == 0:
             return []
-          
+            
         binSize = int(number / (rawSplits + 1) / 1.0)
         numBins = int(number / binSize)
         sp = [binSize * i for i in range(numBins + 1)]
         sp[numBins] = number
         return tuple(sp)
-     
+       
     def meanValuePair(self, x,tupleSize=2):
         def mean(k):  return sum([float(y[k]) for y in x]) / len(x) / 1.0
         if tupleSize > 1: return [mean(k) for k in range(tupleSize)]
         return [sum(x) / 1.0 / len(x)]
- 
-    def createBins(self, x, y, numBins):
+   
+    def createBins(self, x, y, numBins=20):
         if len(x) != len(y):
-            logger.warn("Warning: Number of records and number of labels are different.")
- 
+             print "Warning: Number of records and number of labels are different."
+   
         pairs = []
         numberOfPoints = min(len(x), len(y))
         for i in range(numberOfPoints):
             if pd.isnull(x[i]) == False:
                 pairs.append([x[i], y[i]])
-         
-        if numBins > len(pairs) - 1 and len(pairs) != 0:
-            numBins = len(pairs)- 1
- 
+           
+        if numBins > len(pairs) -1 and len(pairs) != 0:
+            numBins = len(pairs)-1
+   
         indBins = self.createIndexSequence(len(pairs), numBins)
         def mvPair(k):
             return self.meanValuePair([pairs[i] for i in range(indBins[k], indBins[k+1])])
         return pd.Series([mvPair(b) for b in range(len(indBins)-1)])
- 
+   
     def matchValue(self, yvalue, binPairs):
         def absFn(x): return( x if x > 0 else (-x))
         def sgnFn(x):
@@ -150,25 +233,25 @@ class ImputationStep(PipelineStep):
                 return x
             else:
                 return (1 if x > 0 else (-1))
- 
+   
         numBins = len(binPairs)
-         
+           
         if numBins == 1:
             return binPairs[0][0]
         elif numBins == 0:
             return rd.randint(10, 1000)
-         
+           
         matches = [absFn(yvalue - p[1]) for p in binPairs]
         ind = [i for i,x in enumerate(matches) if x == min(matches)][0]
         valuePair = binPairs[ind]
-         
+           
         splitValue = binPairs[1][0] - binPairs[0][0]
         adjValue=0
         if valuePair == binPairs[0]:
             adjValue = -2.0 * splitValue
         if valuePair == binPairs[numBins - 1]:
             adjValue = 2.0 * splitValue
-        return valuePair[0] + adjValue 
+        return valuePair[0] + adjValue
  
 class EVModelStep(PipelineStep):
     model_ = None
--- le-dataplatform/src/main/python/pipeline/pipeline.py
+++ le-dataplatform/src/main/python/pipeline/pipeline.py
@@ -4,37 +4,36 @@ from pipelinesteps import ColumnTypeConversionStep
 from pipelinesteps import EnumeratedColumnTransformStep
 from pipelinesteps import ImputationStep
 from pipelinefwk import ModelStep
- 
+  
 def getDecoratedColumns(metadata):
     stringColumns = dict()
     continuousColumns = dict()
     transform = encoder.HashEncoder()
-         
+          
     for key, value in metadata.iteritems():
         if value[0]["Dtype"] == "STR":
             stringColumns[key] = transform
         else:
             continuousColumns[key] = value[0]["median"]
-         
+          
     return (stringColumns, continuousColumns)
- 
+  
 def encodeCategoricalColumnsForMetadata(metadata):
     for _, values in metadata.iteritems():
         for value in values:
             if value["Dtype"] == "STR" and value["hashValue"] is not None:
                 value["hashValue"] = encoder.transform(value["hashValue"])
- 
+  
 def setupPipeline(metadata, stringColumns, targetColumn):
     (categoricalColumns, continuousColumns) = getDecoratedColumns(metadata)
     # stringColumns refer to the columns that are categorical from the physical schema
     # categoricalColumns refer to the columns that are categorical from the metadata
     # We need to transform the physical strings into numbers
     columnsToTransform = set(stringColumns - set(categoricalColumns.keys()))
-    imputationValues = {}
-    steps = [EnumeratedColumnTransformStep(categoricalColumns), ColumnTypeConversionStep(columnsToTransform), ImputationStep(continuousColumns, imputationValues, targetColumn)]
+    steps = [EnumeratedColumnTransformStep(categoricalColumns), ColumnTypeConversionStep(columnsToTransform), ImputationStep(continuousColumns, {}, [], [], [], targetColumn)]
     pipeline = Pipeline(steps)
-    
+     
     scoringSteps = steps + [ModelStep()]
     scoringPipeline = Pipeline(scoringSteps)
-    
+     
     return pipeline, scoringPipeline
