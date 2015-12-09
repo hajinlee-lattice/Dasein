e71ff219d5bb931fab608e8a416305cb927bd1bb
nxu <nxu@479f5059-62a3-4c39-9342-9852e3a68435>
2015-12-09 09:55:03 +0000

    Review Request 1884

--- le-tools/PlaymakerEnd2End/Configuration/SetupPLME2EEnviroment.py
+++ le-tools/PlaymakerEnd2End/Configuration/SetupPLME2EEnviroment.py
@@ -3,20 +3,22 @@ from PlaymakerEnd2End.steps.updateAccount import updateTenantAccount
 from PlaymakerEnd2End.steps.configureDataloader import DataloaderDealer
 from PlaymakerEnd2End.steps.configureJAMS  import update247DB
 from PlaymakerEnd2End.steps.dealPlay  import DealPlay
-def setUpEnvironments():
-    playDealer=DealPlay()
-    dlDealer=DataloaderDealer()
-    #log=SalePrismEnvironments.logProvider.getLog("SetEnvironment",True)
-    print '########Start to set up E2E environment###########'
-    print 'set up playmaker configuration'
-    playDealer.setPlaymakerConfigurationByRest()
-    print 'update Tenant Account'
-    updateTenantAccount()
-    print 'start to set data loader'
-    dlDealer.setTenantDataProviderByREST()
-    print 'set up Jams'
-    update247DB()
-    print 'Complete!'
-if __name__ == "__main__":
-    setUpEnvironments()
+class setUpEnvironments(object):
+    @staticmethod
+    def setUp():
+        playDealer=DealPlay()
+        dlDealer=DataloaderDealer()
+        #log=SalePrismEnvironments.logProvider.getLog("SetEnvironment",True)
+        print '########Start to set up E2E environment###########'
+        print 'set up playmaker configuration'
+        playDealer.setPlaymakerConfigurationByRest()
+        print 'update Tenant Account'
+        updateTenantAccount()
+        print 'start to set data loader'
+        dlDealer.setTenantDataProviderByREST()
+        print 'set up Jams'
+        update247DB()
+        print '######Complete!#######'
+#if __name__ == "__main__":
+    #setUpEnvironments()
 
