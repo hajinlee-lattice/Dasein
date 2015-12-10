482b15838e3ef1cfddeea49c4016f2c50d9cbae7
hmohsen <hmohsen@479f5059-62a3-4c39-9342-9852e3a68435>
2015-12-10 18:45:38 +0000

    Review Request 1935: Revenue-based Models Normalization buckets

--- le-dataplatform/src/main/python/leframework/model/states/normalizationgenerator.py
+++ le-dataplatform/src/main/python/leframework/model/states/normalizationgenerator.py
@@ -1,64 +1,76 @@
 import logging
-
+ 
 from leframework.codestyle import overrides
 from leframework.model.jsongenbase import JsonGenBase
 from leframework.model.state import State
-
+ 
 class NormalizationGenerator(State, JsonGenBase):
     def __init__(self):
         State.__init__(self, "NormalizationGenerator")
         self.logger = logging.getLogger(name='normalizationgenerator')
-
+ 
     @overrides(State)
     def execute(self):
         mediator = self.mediator
         schema = mediator.schema
         score = mediator.allDataPreTransform[schema["reserved"]["score"]]
-        self.mediator.mappingData = []
-
+        revenue = mediator.data[schema["reserved"]["predictedrevenue"]]
+        self.mediator.probabilityMappingData = []
+        self.mediator.revenueMappingData = []
+ 
+        if mediator.revenueColumn is not None:
+            revenueMappingData = self.buildMappingFunctionData(revenue, True)
+            self.mediator.revenueMappingData = revenueMappingData 
+             
         if score is not None:
-            mappingData = self.buildMappingFunctionData(score)
-            self.mediator.mappingData = mappingData
+            mappingData = self.buildMappingFunctionData(score, False)
+            self.mediator.probabilityMappingData = mappingData
         else:
             self.logger.info("Scores are not provided. Normalization buckets cannot be generated.")
-
-    def buildMappingFunctionData(self, probabilityList):
-        bins = self.createIndexSequence(len(probabilityList), min((len(probabilityList)-1), 100))
-        
+ 
+    # score list contains probabilities (probability model) or revenue (revenue model) values
+    def buildMappingFunctionData(self, scoreList, isRevenueModel):
+        bins = self.createIndexSequence(len(scoreList), min((len(scoreList)-1), 100))
+         
         if len(bins) > 1:
-            orderedProb = sorted(probabilityList, reverse = False)
+            orderedScores = sorted(scoreList, reverse = False)
             cumulativePercent = [1.0 * bins[i + 1] / bins[len(bins) - 1] for i in range(len(bins) - 1)]
-    
-            startProb = [orderedProb[bins[i]] for i in range(len(bins) - 1)]
-            endProb = startProb[:]
-            del endProb[0]
-            maxProb = orderedProb[len(probabilityList) - 1]
-            endProb.append(maxProb)
-
-        return[{"StartProbability" : startProb[i], "EndProbability" : endProb[i], "CumulativePercentage" : cumulativePercent[i]} for i in range(len(startProb))]
-
+     
+            startScore = [orderedScores[bins[i]] for i in range(len(bins) - 1)]
+            endScore = startScore[:]
+            del endScore[0]
+            maxScore = orderedScores[len(scoreList) - 1]
+            endScore.append(maxScore)
+ 
+        return[{"Start" : startScore[i], "End" : endScore[i], "CumulativePercentage" : cumulativePercent[i]} for i in range(len(startScore))]
+ 
+ 
     def createIndexSequence(self, number, rawSplits):
         if number == 0:
             return []
-        
+         
         binSize = int(number / (rawSplits + 1) / 1.0)
-        
+         
         if binSize < 1:
             binSize = 1
-            
+             
         numBins = int(number / binSize)
-
+ 
         sp=[binSize * i for i in range(numBins + 1)]
         sp[numBins] = number
         minSize = min(sp[i + 1] - sp[i] for i in range(len(sp) - 1))
         if minSize < 2:
             sp = self.createIndexSequence(number, rawSplits - 2)
         return tuple(sp)
-
+ 
     @overrides(JsonGenBase)
     def getKey(self):
         return "NormalizationBuckets"
-    
+     
     @overrides(JsonGenBase)
     def getJsonProperty(self):
-        return self.mediator.mappingData
+        normalizationBuckets = {"Probability" + self.getKey() : self.mediator.probabilityMappingData}
+         
+        if self.mediator.revenueColumn is not None:
+            normalizationBuckets["Revenue" + self.getKey()] = self.mediator.revenueMappingData          
+        return normalizationBuckets 
