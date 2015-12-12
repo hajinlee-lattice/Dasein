8ee9e841de9384cb5b8dcad1524a7f0bf877d5dc
lming <lming@479f5059-62a3-4c39-9342-9852e3a68435>
2015-12-12 00:54:17 +0000

    Review Request 2011: Fix dell ebi test

--- le-domain/src/main/java/com/latticeengines/domain/exposed/dellebi/DellEbiConfig.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/dellebi/DellEbiConfig.java
@@ -38,10 +38,10 @@ public class DellEbiConfig implements HasPid, Serializable {
     @Column(name = "TargetColumns", nullable = false)
     private String targetColumns;
 
-    @Column(name = "TargetTable", nullable = false)
+//    @Column(name = "TargetTable", nullable = false)
     private String targetTable;
 
-//    @Column(name = "StartDate", nullable = true)
+    @Column(name = "StartDate", nullable = true)
     private Date startDate;
 
 //    @Column(name = "IsDeleted", nullable = true)
