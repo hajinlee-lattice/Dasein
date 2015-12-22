a79969be3ad3f2b5e81c6902ec5850127fe7f9a3
mmeng <mmeng@479f5059-62a3-4c39-9342-9852e3a68435>
2015-12-22 20:29:46 +0000

    Review Request 2184: fix eai compilation warning failure

--- le-eai/src/main/java/com/latticeengines/eai/routes/marketo/GenerateLeadActivitiesUrlProcessor.java
+++ le-eai/src/main/java/com/latticeengines/eai/routes/marketo/GenerateLeadActivitiesUrlProcessor.java
@@ -8,13 +8,18 @@ import org.apache.camel.Processor;
 
 public class GenerateLeadActivitiesUrlProcessor implements Processor {
 
-    @SuppressWarnings("unchecked")
     @Override
     public void process(Exchange exchange) throws Exception {
         String baseUrl = exchange.getProperty(MarketoImportProperty.BASEURL, String.class);
         String accessToken = exchange.getProperty(MarketoImportProperty.ACCESSTOKEN, String.class);
         String nextPageToken = exchange.getProperty(MarketoImportProperty.NEXTPAGETOKEN, String.class);
-        List<String> activityTypes = exchange.getProperty(MarketoImportProperty.ACTIVITYTYPES, List.class);
+
+        List<?> activityTypesUnchecked = exchange.getProperty(MarketoImportProperty.ACTIVITYTYPES, List.class);
+        List<String> activityTypes = new ArrayList<>();
+        for (Object activityTypeUnchecked : activityTypesUnchecked) {
+            activityTypes.add((String) activityTypeUnchecked);
+        }
+
         String url = new MarketoUrlGenerator().getActivitiesUrl(baseUrl, accessToken, nextPageToken, activityTypes);
         exchange.setProperty("activitiesUrl", url);
         exchange.setProperty(MarketoImportProperty.ACTIVITYRESULTLIST, new ArrayList<>());
