39384e80590f69d11643c3b24740d5b39eb362c5
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-12-22 19:25:45 +0000

    Review Request 2183 fix eai uncheck error

--- le-eai/src/main/java/com/latticeengines/eai/routes/marketo/GenerateLeadsUrlProcessor.java
+++ le-eai/src/main/java/com/latticeengines/eai/routes/marketo/GenerateLeadsUrlProcessor.java
@@ -1,5 +1,6 @@
 package com.latticeengines.eai.routes.marketo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.camel.Exchange;
@@ -7,14 +8,15 @@ import org.apache.camel.Processor;
 
 public class GenerateLeadsUrlProcessor implements Processor {
 
-    @SuppressWarnings("unchecked")
     @Override
     public void process(Exchange exchange) throws Exception {
         String baseUrl = exchange.getProperty(MarketoImportProperty.BASEURL, String.class);
         String accessToken = exchange.getProperty(MarketoImportProperty.ACCESSTOKEN, String.class);
         String nextPageToken = exchange.getProperty(MarketoImportProperty.NEXTPAGETOKEN, String.class);
         String filterType = exchange.getProperty(MarketoImportProperty.FILTERTYPE, String.class);
-        List<String> filterValues = exchange.getProperty(MarketoImportProperty.FILTERVALUES, List.class);
+        List<?> filterValuesUnchecked = exchange.getProperty(MarketoImportProperty.FILTERVALUES, List.class);
+        List<String> filterValues = new ArrayList<>();
+        for (Object object: filterValuesUnchecked) { filterValues.add((String) object); }
         exchange.setProperty("leadsUrl",
                 new MarketoUrlGenerator().getLeadsUrl(baseUrl, accessToken, nextPageToken, filterType, filterValues));
     }
