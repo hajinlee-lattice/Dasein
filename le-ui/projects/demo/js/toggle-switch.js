b03e24af186643f570a251b74afa25ed77b6635b
JHadden <JHadden@479f5059-62a3-4c39-9342-9852e3a68435>
2015-12-22 18:46:39 +0000

    Review Request 1024: Update Datatables & Select Menus

--- le-ui/projects/demo/js/toggle-switch.js
+++ le-ui/projects/demo/js/toggle-switch.js
@@ -1,14 +1,9 @@
 function toggleCheckFunctionality(){
-	console.log("toggle script function ready");
 	$(".toggle-check label").on("click", function(){
-		console.log("toggle");
 		$(".switch-tab").toggleClass("open");
 	});
 }
 
 $(document).ready(function(){
-
-	console.log("toggle script ready");
 	toggleCheckFunctionality();
-	
 });
