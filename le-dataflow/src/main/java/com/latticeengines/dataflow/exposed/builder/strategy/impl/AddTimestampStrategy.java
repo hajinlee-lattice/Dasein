1c23e2b1ea072de3d4697d4fba8d0ff7bf81cc6e
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2015-12-28 16:54:44 +0000

    Review Request 2227

--- le-dataflow/src/main/java/com/latticeengines/dataflow/exposed/builder/strategy/impl/AddTimestampStrategy.java
+++ le-dataflow/src/main/java/com/latticeengines/dataflow/exposed/builder/strategy/impl/AddTimestampStrategy.java
@@ -8,13 +8,31 @@ public class AddTimestampStrategy extends AddFieldStrategyBase {
 
     private static final long serialVersionUID = -2625708547865490421L;
 
+    public static int UNIFORM = 0;      // all records have the same timestamp
+    public static int SEQUENTIAL = 1;   // each record has a slightly different timestamp
+
+    private int mode;
+    private long timestamp;
+
     public AddTimestampStrategy(String fieldName) {
+        this(fieldName, UNIFORM);
+    }
+
+    public AddTimestampStrategy(String fieldName, int mode) {
         super(fieldName, Long.class);
+        this.timestamp = System.currentTimeMillis();
+        this.mode = mode;
     }
 
     @Override
     public Object compute(TupleEntry arguments) {
-        return System.currentTimeMillis();
+        if (mode == UNIFORM) {
+            return timestamp;
+        } else if (mode == SEQUENTIAL) {
+            return System.currentTimeMillis();
+        } else {
+            throw new UnsupportedOperationException("Unknown timestamp mode: " + this.mode);
+        }
     }
 
 }
