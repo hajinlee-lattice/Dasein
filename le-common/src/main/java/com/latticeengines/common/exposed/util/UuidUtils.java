bfce4c5e442b2a0661597d819d8ee5359747dd3c
bnguyen <bnguyen@479f5059-62a3-4c39-9342-9852e3a68435>
2015-12-29 22:51:19 +0000

    Review Request 2246: Explicitly state when model GUID is empty.

--- le-common/src/main/java/com/latticeengines/common/exposed/util/UuidUtils.java
+++ le-common/src/main/java/com/latticeengines/common/exposed/util/UuidUtils.java
@@ -3,9 +3,14 @@ package com.latticeengines.common.exposed.util;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.google.common.base.Strings;
+
 public class UuidUtils {
 
     public static String extractUuid(String modelGuid) {
+        if (Strings.isNullOrEmpty(modelGuid)) {
+            throw new IllegalArgumentException("The model GUID is empty");
+        }
         Pattern pattern = Pattern.compile("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}");
         Matcher matcher = pattern.matcher(modelGuid);
         if (matcher.find()) {
