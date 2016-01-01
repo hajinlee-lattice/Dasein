09bb37993989b333f882e8d5d9c6325a0b6bbefa
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-01-01 18:08:48 +0000

    Review Request 2286: Add report registration

--- le-domain/src/main/java/com/latticeengines/domain/exposed/pls/ReportPurpose.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/pls/ReportPurpose.java
@@ -1,6 +1,38 @@
 package com.latticeengines.domain.exposed.pls;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum ReportPurpose {
-    IMPORT_SUMMARY, //
-    MODEL_SUMMARY
+    IMPORT_SUMMARY("ImportSummary"), //
+    MODEL_SUMMARY("ModelSummary"), //
+    INDUSTRY_ATTR_LEVEL_SUMMARY("CreateAttributeLevelSummary_BusinessIndustry"), //
+    REVENUE_ATTR_LEVEL_SUMMARY("CreateAttributeLevelSummary_BusinessRevenueRange"), //
+    EMPLOYEE_ATTR_LEVEL_SUMMARY("CreateAttributeLevelSummary_BusinessEmployeesRange"), //
+    INDUSTRY_LIFT_ATTR_LEVEL_SUMMARY("CreateAttributeLevelSummary_BusinessIndustry_Probability"), //
+    REVENUE_LIFT_ATTR_LEVEL_SUMMARY("CreateAttributeLevelSummary_BusinessRevenueRange_Probability"), //
+    EMPLOYEE_LIFT_ATTR_LEVEL_SUMMARY("CreateAttributeLevelSummary_BusinessEmployeesRange_Probability");
+    
+    private String key;
+    
+    private static Map<String, ReportPurpose> map = new HashMap<>();
+    
+    static {
+        for (ReportPurpose reportPurpose : ReportPurpose.values()) {
+            map.put(reportPurpose.getKey(), reportPurpose);
+        }
+    }
+    
+    ReportPurpose(String key) {
+        this.key = key;
+    }
+    
+    public String getKey() {
+        return key;
+    }
+    
+    public static ReportPurpose getReportPurpose(String key) {
+        return map.get(key);
+    }
+    
 }
