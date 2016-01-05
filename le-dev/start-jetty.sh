4e77d941e60f6d81c9dde2f23b4fc4f03bc4502e
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-01-05 00:58:44 +0000

    Review Request 2303 fix microservice for start-jetty.sh

--- le-dev/start-jetty.sh
+++ le-dev/start-jetty.sh
@@ -12,6 +12,8 @@ export MONITOR_PROPDIR=$PWD/../le-monitor/conf/env/dev
 export WORKFLOWAPI_PROPDIR=$PWD/../le-workflowapi/conf/env/dev
 export PROXY_PROPDIR=$PWD/../le-proxy/conf/env/dev
 export SCORING_PROPDIR=$PWD/../le-scoring/conf/env/dev
+export CAMILLE_PROPDIR=$PWD/../le-camille/conf/env/dev
+export DATAFLOW_PROPDIR=$PWD/../le-dataflow/conf/env/dev
 export JAVA_OPTIONS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=4005,server=y,suspend=n -XX:MaxPermSize=1024m -Dsqoop.throwOnError=true -Djetty.class.path=$JAVA_HOME/lib/tools.jar:$HADOOP_HOME/share/hadoop/common/hadoop-common-2.6.0.2.2.0.0-2041.jar:$HADOOP_HOME/etc/hadoop"
 
 pushd $JETTY_HOME
