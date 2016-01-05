57929950c8ae1990260132529ca9b0d00c8ff50a
mmeng <mmeng@479f5059-62a3-4c39-9342-9852e3a68435>
2016-01-05 19:21:23 +0000

    Review Request 2288: use guava cache for jobs, move numthreads property to workflow.properties

--- le-workflow/src/main/java/com/latticeengines/workflow/core/WorkflowExecutionCache.java
+++ le-workflow/src/main/java/com/latticeengines/workflow/core/WorkflowExecutionCache.java
@@ -1,7 +1,6 @@
 package com.latticeengines.workflow.core;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -10,77 +9,97 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.TransformerUtils;
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobInstance;
 import org.springframework.batch.core.StepExecution;
-import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.explore.JobExplorer;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.launch.JobOperator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.Cache;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.stereotype.Component;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.latticeengines.domain.exposed.workflow.Job;
 import com.latticeengines.domain.exposed.workflow.JobStatus;
 import com.latticeengines.domain.exposed.workflow.JobStep;
 import com.latticeengines.domain.exposed.workflow.WorkflowExecutionId;
 import com.latticeengines.domain.exposed.workflow.WorkflowStatus;
 import com.latticeengines.workflow.exposed.service.WorkflowService;
+import com.latticeengines.workflow.service.impl.WorkflowServiceImpl;
 
 @Component("workflowExecutionCache")
 public class WorkflowExecutionCache {
 
-    @Value("${db.workflow.jobs.numthreads}")
+    private static final int MAX_CACHE_SIZE = 1000;
+    private static final Log log = LogFactory.getLog(WorkflowServiceImpl.class);
+
+    @Value("${workflow.jobs.numthreads}")
     private String numJobThreads;
 
-    private static final ConcurrentMapCache cache = new ConcurrentMapCache("workflowExecutionCache");
+    private Cache<Long, Job> cache;
     private ExecutorService executorService;
 
     @Autowired
     private JobExplorer jobExplorer;
-
-    @Autowired
-    private JobLauncher jobLauncher;
-
-    @Autowired
-    private JobRegistry jobRegistry;
-
-    @Autowired
-    private JobOperator jobOperator;
     
     @Autowired
     private WorkflowService workflowService;
 
+    @PostConstruct
+    public void init() {
+         cache = CacheBuilder.newBuilder()
+                     .maximumSize(MAX_CACHE_SIZE)
+                     .build();
+         executorService = Executors.newFixedThreadPool(Integer.parseInt(numJobThreads));
+    }
+    
     public List<Job> getJobs(List<WorkflowExecutionId> workflowIds) throws Exception {
         List<Job> jobs = new ArrayList<>();
 
         List<WorkflowExecutionId> missingJobIds = new ArrayList<>();
         for (WorkflowExecutionId workflowId : workflowIds) {
-            Cache.ValueWrapper jobValueWrapper = cache.get(workflowId);
-            
-            if (jobValueWrapper == null) {
+            if (cache.getIfPresent(workflowId.getId()) == null) {
                 missingJobIds.add(workflowId);
             } else {
-                jobs.add((Job) jobValueWrapper.get());
+                jobs.add(cache.getIfPresent(workflowId.getId()));
             }
         }
         
         jobs.addAll(loadMissingJobs(missingJobIds));
-
         return jobs;
     }
     
-    private List<Job> loadMissingJobs(List<WorkflowExecutionId> workflowIds) throws Exception {
-        if (executorService == null) {
-             executorService = Executors.newFixedThreadPool(Integer.parseInt(numJobThreads));
+    public Job getJob(WorkflowExecutionId workflowId) {
+        if (cache.getIfPresent(workflowId.getId()) != null) {
+            return cache.getIfPresent(workflowId.getId());
         }
 
+        log.info(String.format("Job with id: %s is not in the cache, reloading.", workflowId.getId()));
+        JobExecution jobExecution = jobExplorer.getJobExecution(workflowId.getId());
+        JobInstance jobInstance = jobExecution.getJobInstance();
+        WorkflowStatus workflowStatus = this.workflowService.getStatus(workflowId);
+
+        Job job = new Job();
+        job.setId(workflowId.getId());
+        job.setJobStatus(getJobStatusFromBatchStatus(workflowStatus.getStatus()));
+        job.setStartTimestamp(workflowStatus.getStartTime());
+        job.setJobType(jobInstance.getJobName());
+        job.setSteps(getJobSteps(jobExecution));
+        if (job.getJobStatus() == JobStatus.CANCELLED || job.getJobStatus() == JobStatus.COMPLETED
+                || job.getJobStatus() == JobStatus.FAILED) {
+            job.setEndTimestamp(workflowStatus.getEndTime());
+            cache.put(job.getId(), job);
+        }
+
+        return job;
+    }
+
+    private List<Job> loadMissingJobs(List<WorkflowExecutionId> workflowIds) throws Exception {
         List<Job> missingJobs = new ArrayList<>();
         Set<Callable<Job>> callables = new HashSet<>();
         
@@ -95,42 +114,12 @@ public class WorkflowExecutionCache {
         List<Future<Job>> futures = executorService.invokeAll(callables);
         for (Future<Job> future : futures) {
             Job job = future.get();
-            cache.put(job.getId(), job);
             missingJobs.add(job);
         }
 
         return missingJobs;
     }
     
-    public Job getJob(WorkflowExecutionId workflowId) {
-        Cache.ValueWrapper jobValueWrapper = cache.get(workflowId);
-        Job job = new Job();
-        
-        if (jobValueWrapper != null) {
-            job = (Job) jobValueWrapper.get();
-        } else {
-            JobExecution jobExecution = jobExplorer.getJobExecution(workflowId.getId());
-            JobInstance jobInstance = jobExecution.getJobInstance();
-            WorkflowStatus workflowStatus = this.workflowService.getStatus(workflowId);
-
-            job.setId(workflowId.getId());
-            job.setJobStatus(getJobStatusFromBatchStatus(workflowStatus.getStatus()));
-            job.setStartTimestamp(workflowStatus.getStartTime());
-            if (job.getJobStatus() == JobStatus.CANCELLED || job.getJobStatus() == JobStatus.COMPLETED
-                    || job.getJobStatus() == JobStatus.FAILED) {
-                job.setEndTimestamp(workflowStatus.getEndTime());
-            }
-
-            job.setJobType(jobInstance.getJobName());
-
-            job.setSteps(getJobSteps(jobExecution));
-
-            cache.put(workflowId, job);
-        }
-
-        return job;
-    }
-
     private List<JobStep> getJobSteps(JobExecution jobExecution) {
         List<JobStep> steps = new ArrayList<>();
 
