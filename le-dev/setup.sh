14b418cf989c629c24193f990652b53c1ca8ecea
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-01-07 00:38:22 +0000

    Review Request 0000: Add a setup.sh script for compiling everything

---
+++ le-dev/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+shopt -s expand_aliases
+source $HOME/.bash_aliases
+cd $HOME/workspace/ledp
+mvn -Pgenerate -DskipTests clean install
+cd le-dataplatform && dpdpl
+cd ../le-eai && eaidpl
+cd ../le-dataflow && dfdpl
+cd ../le-dataflowapi && dfapidpl
+cd ../le-workflowapi && wfapidpl
+cd ../le-microservice && microservicedpl
