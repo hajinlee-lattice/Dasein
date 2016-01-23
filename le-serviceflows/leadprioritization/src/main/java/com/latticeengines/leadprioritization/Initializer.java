4cbb5242ba83202f6ffd5a977b12189992a39155
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-01-23 17:55:42 +0000

    Review Request 2462: Basic code structure for LP service flows.

---
+++ le-serviceflows/leadprioritization/src/main/java/com/latticeengines/leadprioritization/Initializer.java
@@ -0,0 +1,16 @@
+package com.latticeengines.leadprioritization;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.latticeengines.domain.exposed.swlib.SoftwarePackageInitializer;
+
+public class Initializer implements SoftwarePackageInitializer {
+
+    @Override
+    public ApplicationContext initialize(ApplicationContext applicationContext) {
+        return new ClassPathXmlApplicationContext(new String[] { "serviceflows-leadprioritization-context.xml" }, // 
+                applicationContext);
+    }
+
+}
