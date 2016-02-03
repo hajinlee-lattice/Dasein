a16ce46b153bcc3018373470a4e684fee7336253
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-02-03 05:10:31 +0000

    Review Request 2586 remove potential Count field in the input of CountFlow

--- le-serviceflows/propdata/src/main/java/com/latticeengines/propdata/dataflow/common/CountFlow.java
+++ le-serviceflows/propdata/src/main/java/com/latticeengines/propdata/dataflow/common/CountFlow.java
@@ -1,5 +1,8 @@
 package com.latticeengines.propdata.dataflow.common;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import com.latticeengines.dataflow.exposed.builder.TypesafeDataFlowBuilder;
@@ -14,7 +17,26 @@ public class CountFlow extends TypesafeDataFlowBuilder<CountFlowParameters> {
     public Node construct(CountFlowParameters parameters) {
         Node source = addSource(parameters.getSourceTable());
         source = source.addRowID("ID");
+        source = removeCountField(source);
         source = source.aggregate(new Aggregation("ID", COUNT, Aggregation.AggregationType.COUNT));
+        return source;
+    }
+
+    private Node removeCountField(Node source) {
+        List<String> fieldNames = new ArrayList<>();
+        boolean hasCount = false;
+        for (FieldMetadata fm : source.getSchema()) {
+            if (fm.getFieldName().equals(COUNT)) {
+                hasCount = true;
+            } else {
+                fieldNames.add(fm.getFieldName());
+            }
+        }
+
+        if (hasCount) {
+            source = source.retain(new FieldList(fieldNames.toArray(new String[fieldNames.size()])));
+        }
+
         return source;
     }
 
