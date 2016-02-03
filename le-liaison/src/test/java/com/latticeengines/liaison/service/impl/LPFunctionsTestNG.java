5fd63cef93207101c25d6176b0013ac23d564077
mwilson <mwilson@479f5059-62a3-4c39-9342-9852e3a68435>
2016-02-03 00:27:07 +0000

    Review Request 2585: Update Functional Test To Validate Tech Attributes UI

--- le-liaison/src/test/java/com/latticeengines/liaison/service/impl/LPFunctionsTestNG.java
+++ le-liaison/src/test/java/com/latticeengines/liaison/service/impl/LPFunctionsTestNG.java
@@ -2,8 +2,10 @@ package com.latticeengines.liaison.service.impl;
 
 import java.io.IOException;
 import java.util.AbstractMap;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,27 +27,52 @@ public class LPFunctionsTestNG extends LiaisonTestNGBase {
     @Test(groups = "functional")
     public void testLPFunctions() throws IOException {
 
-        String tenantName = "Internal_LP_SFDC_220_2";
-        String dlURL = "https://data-pls2.prod.lattice.local/dataloader/";
+        System.out.println("\n\nHellow, World!\n\n");
+
+        //String tenantName = "Internal_LP_SFDC_220_2";
+        //String dlURL = "https://data-pls2.prod.lattice.local/dataloader/";
+        String tenantName = "TestSFDC_20160202";
+        String dlURL = "https://10.41.1.187:8080/";
         ConnectionMgr conn_mgr = connectionMgrFactory.getConnectionMgr("visiDB", tenantName, dlURL);
 
-        String source = "OrbIntelligence_Source";
-        Set<String> ldcCols = new HashSet<>();
-        ldcCols.add("linkedin_url");
-        ldcCols.add("BusinessName");
+        AbstractMap.SimpleImmutableEntry<String, String> typeAndVersions = lpfcn.getLPTemplateTypeAndVersion(conn_mgr);
+        String lp_template_type = typeAndVersions.getKey();
+        String lp_template_version = typeAndVersions.getValue();
 
-        AbstractMap.SimpleImmutableEntry<String, String> typeAndVersion = lpfcn.getLPTemplateTypeAndVersion(conn_mgr);
-        String lp_template_type = typeAndVersion.getKey();
-        String lp_template_version = typeAndVersion.getValue();
+        Map<String, Set<String>> map = new HashMap<String, Set<String>>();
+        Set<String> columns1 = new HashSet<String>();
+        map.put("OrbIntelligence_Source", columns1);
+        columns1.add("BusinessAddress1");
 
-        Map<String, String> existingAtts = lpfcn.getLDCWritebackAttributes(conn_mgr, source, lp_template_version);
-        for (Map.Entry<String, String> att : existingAtts.entrySet()) {
-            System.out.println(String.format("%s --> %s", att.getKey(), att.getValue()));
+        Set<String> columns2 = new HashSet<String>();
+        map.put("BuiltWith_Pivoted_Source", columns2);
+        columns2.add("TechIndicator_Akamai");
+
+        Set<String> columns3 = new HashSet<String>();
+        map.put("HGData_Pivoted_Source", columns3);
+        columns3.add("TechIndicator_ADP");
+
+        //String source = "OrbIntelligence_Source";
+        //Set<String> ldcCols = new HashSet<>();
+        //ldcCols.add("linkedin_url");
+        //ldcCols.add("BusinessName");
+        //ldcCols.add("BusinessAddress1");
+
+        for (Entry<String, Set<String>> entry : map.entrySet()) {
+            String source = entry.getKey();
+            lpfcn.addLDCMatch(conn_mgr, source, typeAndVersions.getValue());
+            lpfcn.setLDCWritebackAttributesDefaultName(conn_mgr, source, entry.getValue(),
+                    typeAndVersions.getKey(), typeAndVersions.getValue());
         }
 
-        lpfcn.removeLDCWritebackAttributes(conn_mgr, lp_template_version);
-        lpfcn.addLDCMatch(conn_mgr, source, lp_template_version);
-        lpfcn.setLDCWritebackAttributesDefaultName(conn_mgr, source, ldcCols, lp_template_type, lp_template_version);
+        //Map<String, String> existingAtts = lpfcn.getLDCWritebackAttributes(conn_mgr, source, lp_template_version);
+        //for (Map.Entry<String, String> att : existingAtts.entrySet()) {
+        //    System.out.println(String.format("%s --> %s", att.getKey(), att.getValue()));
+        // }
+
+        //lpfcn.removeLDCWritebackAttributes(conn_mgr, lp_template_version);
+        //lpfcn.addLDCMatch(conn_mgr, source, lp_template_version);
+        //lpfcn.setLDCWritebackAttributesDefaultName(conn_mgr, source, ldcCols, lp_template_type, lp_template_version);
         conn_mgr.getLoadGroupMgr().commit();
     }
 
