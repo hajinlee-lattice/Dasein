a0e68f50f7bcd5d59ee5d04b2697628fc7dbae41
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-02-03 19:53:03 +0000

    Review Request 2586 fix propdata scheduler

--- le-propdata/src/main/java/com/latticeengines/propdata/job/PropDataScheduler.java
+++ le-propdata/src/main/java/com/latticeengines/propdata/job/PropDataScheduler.java
@@ -1,6 +1,5 @@
 package com.latticeengines.propdata.job;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -38,10 +37,7 @@ public class PropDataScheduler {
     Boolean dryrun;
 
     @Autowired
-    List<RawSource> rawSourceList;
-
-    @Autowired
-    List<DerivedSource> derivedSourceList;
+    List<Source> sourceList;
 
     @Autowired
     ZkConfigurationService zkConfigurationService;
@@ -53,19 +49,13 @@ public class PropDataScheduler {
     private void registerJobs() throws SchedulerException {
         scheduler = new StdSchedulerFactory().getScheduler();
 
-        for(RawSource source: rawSourceList) {
-            try {
-                registerArchiveJob(source);
-            } catch (Exception e) {
-                log.error("Failed to register scheduler for source " + source.getSourceName());
-            }
-        }
-
-        for (DerivedSource source: derivedSourceList) {
-            try {
-                registerRefreshJob(source);
-            } catch (Exception e) {
-                log.error("Failed to register scheduler for source " + source.getSourceName());
+        for (Source source : sourceList) {
+            if (zkConfigurationService.refreshJobEnabled(source)) {
+                try {
+                    registerJob(source);
+                } catch (Exception e) {
+                    log.error("Failed to register scheduler for source " + source.getSourceName(), e);
+                }
             }
         }
 
@@ -73,24 +63,29 @@ public class PropDataScheduler {
     }
 
     public void reschedule() {
-        List<Source> sources = new ArrayList<Source>(rawSourceList);
-        sources.addAll(derivedSourceList);
-
-        for(Source source: sources) {
-            try {
-                rescheduleSource(source);
-            } catch (Exception e) {
-                log.error("Failed to reschedule scheduler for source " + source.getSourceName());
+        for (Source source : sourceList) {
+            if (zkConfigurationService.refreshJobEnabled(source)) {
+                try {
+                    rescheduleSource(source);
+                } catch (Exception e) {
+                    log.error("Failed to reschedule scheduler for source " + source.getSourceName(), e);
+                }
             }
+        }
+    }
+
+    private void registerJob(Source source) throws SchedulerException {
+        if (source instanceof RawSource) {
+            registerArchiveJob((RawSource) source);
+        } else if (source instanceof DerivedSource) {
+            registerRefreshJob((DerivedSource) source);
         }
     }
 
     private void registerArchiveJob(RawSource source) throws SchedulerException {
         ArchiveService service = progressOrchestrator.getArchiveService(source);
         if (service != null) {
-            JobDetail job = JobBuilder.newJob(ArchiveScheduler.class)
-                    .usingJobData("dryrun", dryrun)
-                    .build();
+            JobDetail job = JobBuilder.newJob(ArchiveScheduler.class).usingJobData("dryrun", dryrun).build();
             job.getJobDataMap().put("archiveService", service);
             job.getJobDataMap().put("zkConfigurationService", zkConfigurationService);
 
@@ -101,9 +96,7 @@ public class PropDataScheduler {
     private void registerRefreshJob(DerivedSource source) throws SchedulerException {
         RefreshService service = progressOrchestrator.getRefreshService(source);
         if (service != null) {
-            JobDetail job = JobBuilder.newJob(RefreshScheduler.class)
-                    .usingJobData("dryrun", dryrun)
-                    .build();
+            JobDetail job = JobBuilder.newJob(RefreshScheduler.class).usingJobData("dryrun", dryrun).build();
             job.getJobDataMap().put("refreshService", service);
             job.getJobDataMap().put("zkConfigurationService", zkConfigurationService);
 
@@ -113,19 +106,19 @@ public class PropDataScheduler {
 
     private CronTrigger cronTriggerForSource(Source source) {
         String cron = zkConfigurationService.refreshCronSchedule(source);
-        return TriggerBuilder.newTrigger()
-                .withIdentity(new TriggerKey(source.getSourceName()))
-                .withSchedule(CronScheduleBuilder.cronSchedule(cron))
-                .build();
+        return TriggerBuilder.newTrigger().withIdentity(new TriggerKey(source.getSourceName()))
+                .withSchedule(CronScheduleBuilder.cronSchedule(cron)).build();
     }
 
     private void rescheduleSource(Source source) throws SchedulerException {
         TriggerKey key = new TriggerKey(source.getSourceName());
         CronTrigger oldTrigger = (CronTrigger) scheduler.getTrigger(key);
-        if (!zkConfigurationService.refreshCronSchedule(source).equals(oldTrigger.getCronExpression())) {
+        if (oldTrigger == null) {
+            registerJob(source);
+        } else if (!zkConfigurationService.refreshCronSchedule(source).equals(oldTrigger.getCronExpression())) {
             CronTrigger newTrigger = cronTriggerForSource(source);
-            log.info("Reschedule " + source.getSourceName() + " from " + oldTrigger.getCronExpression()
-                    + " to " + newTrigger.getCronExpression());
+            log.info("Reschedule " + source.getSourceName() + " from " + oldTrigger.getCronExpression() + " to "
+                    + newTrigger.getCronExpression());
             scheduler.rescheduleJob(key, newTrigger);
         }
     }
