0478778b40fa750de2e034994570b93efe61f84c
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-02-03 05:22:20 +0000

    Review Request 2586 minor change in CountFlow

--- le-serviceflows/propdata/src/main/java/com/latticeengines/propdata/dataflow/common/CountFlow.java
+++ le-serviceflows/propdata/src/main/java/com/latticeengines/propdata/dataflow/common/CountFlow.java
@@ -12,29 +12,31 @@ import com.latticeengines.domain.exposed.propdata.dataflow.CountFlowParameters;
 public class CountFlow extends TypesafeDataFlowBuilder<CountFlowParameters> {
 
     public static final String COUNT = "Count";
+    public static final String ID = "CountID";
 
     @Override
     public Node construct(CountFlowParameters parameters) {
         Node source = addSource(parameters.getSourceTable());
-        source = source.addRowID("ID");
-        source = removeCountField(source);
-        source = source.aggregate(new Aggregation("ID", COUNT, Aggregation.AggregationType.COUNT));
+        source = removeCountIdField(source);
+        source = source.addRowID(ID);
+        source = source.retain(new FieldList(ID));
+        source = source.aggregate(new Aggregation(ID, COUNT, Aggregation.AggregationType.COUNT));
         source = source.retain(new FieldList(COUNT));
         return source;
     }
 
-    private Node removeCountField(Node source) {
+    private Node removeCountIdField(Node source) {
         List<String> fieldNames = new ArrayList<>();
-        boolean hasCount = false;
+        boolean hasCountId = false;
         for (FieldMetadata fm : source.getSchema()) {
-            if (fm.getFieldName().equals(COUNT)) {
-                hasCount = true;
+            if (fm.getFieldName().equals(ID)) {
+                hasCountId = true;
             } else {
                 fieldNames.add(fm.getFieldName());
             }
         }
 
-        if (hasCount) {
+        if (hasCountId) {
             source = source.retain(new FieldList(fieldNames.toArray(new String[fieldNames.size()])));
         }
 
