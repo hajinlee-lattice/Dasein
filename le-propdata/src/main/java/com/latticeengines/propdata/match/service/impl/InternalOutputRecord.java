f18cfd783968a8cf27fc1d35dd4c18fd68f186bd
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-02-04 01:15:40 +0000

    Review Request 2619 make InternalOutputRecord package private

--- le-propdata/src/main/java/com/latticeengines/propdata/match/service/impl/InternalOutputRecord.java
+++ le-propdata/src/main/java/com/latticeengines/propdata/match/service/impl/InternalOutputRecord.java
@@ -2,15 +2,15 @@ package com.latticeengines.propdata.match.service.impl;
 
 import com.latticeengines.domain.exposed.propdata.match.OutputRecord;
 
-public class InternalOutputRecord extends OutputRecord {
+class InternalOutputRecord extends OutputRecord {
 
     private String parsedDomain;
 
-    public String getParsedDomain() {
+    String getParsedDomain() {
         return parsedDomain;
     }
 
-    public void setParsedDomain(String parsedDomain) {
+    void setParsedDomain(String parsedDomain) {
         this.parsedDomain = parsedDomain;
     }
 }
