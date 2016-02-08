2d2ad788b65efdf1131847ef023cb06e020baa86
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-02-08 23:47:58 +0000

    Review Request 2662 add verification steps ot HG and BW pivoting

--- le-propdata/src/test/java/com/latticeengines/propdata/collection/service/impl/BuiltWithPivotServiceImplTestNG.java
+++ le-propdata/src/test/java/com/latticeengines/propdata/collection/service/impl/BuiltWithPivotServiceImplTestNG.java
@@ -1,8 +1,12 @@
 package com.latticeengines.propdata.collection.service.impl;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.testng.Assert;
 
+import com.latticeengines.domain.exposed.propdata.manage.RefreshProgress;
 import com.latticeengines.propdata.collection.entitymanager.RefreshProgressEntityMgr;
 import com.latticeengines.propdata.collection.service.PivotService;
 import com.latticeengines.propdata.core.source.PivotedSource;
@@ -26,12 +30,44 @@ public class BuiltWithPivotServiceImplTestNG extends PivotServiceImplTestNGBase 
     }
 
     @Override
-    RefreshProgressEntityMgr getProgressEntityMgr() { return progressEntityMgr; }
+    RefreshProgressEntityMgr getProgressEntityMgr() {
+        return progressEntityMgr;
+    }
 
     @Override
-    PivotedSource getSource() { return source; }
+    PivotedSource getSource() {
+        return source;
+    }
 
     @Override
-    Integer getExpectedRows() { return 48; }
+    Integer getExpectedRows() {
+        return 48;
+    }
+
+    @Override
+    protected void verifyResultTable(RefreshProgress progress) {
+        super.verifyResultTable(progress);
+
+        String domain = "bloomfw.com";
+        Map<String, Object> row = jdbcTemplateCollectionDB.queryForMap("SELECT [" + source.getDomainField()
+                + "], [BusinessTechnologiesDocinfo], [BusinessTechnologiesFramework], [BusinessTechnologiesWidgets], "
+                + "[TechIndicator_Apache] FROM [" + source.getSqlTableName() + "] WHERE [" + source.getDomainField()
+                + "] = '" + domain + "'");
+        Assert.assertEquals(row.get(source.getDomainField()), domain);
+        Assert.assertEquals(row.get("BusinessTechnologiesDocinfo"), 10);
+        Assert.assertEquals(row.get("BusinessTechnologiesFramework"), 5);
+        Assert.assertEquals(row.get("BusinessTechnologiesWidgets"), 6);
+        Assert.assertEquals(row.get("TechIndicator_Apache"), "No");
+
+        domain = "sandhuniforms.com";
+        row = jdbcTemplateCollectionDB.queryForMap("SELECT [" + source.getDomainField()
+                + "], [BusinessTechnologiesAnalytics], [BusinessTechnologiesFramework], [TechIndicator_Apache] FROM ["
+                + source.getSqlTableName() + "] WHERE [" + source.getDomainField() + "] = '" + domain + "'");
+
+        Assert.assertEquals(row.get(source.getDomainField()), domain);
+        Assert.assertEquals(row.get("BusinessTechnologiesAnalytics"), 2);
+        Assert.assertEquals(row.get("BusinessTechnologiesFramework"), 2);
+        Assert.assertEquals(row.get("TechIndicator_Apache"), "Yes");
+    }
 
 }
--- le-propdata/src/test/java/com/latticeengines/propdata/collection/service/impl/HGDataPivotServiceImplTestNG.java
+++ le-propdata/src/test/java/com/latticeengines/propdata/collection/service/impl/HGDataPivotServiceImplTestNG.java
@@ -1,8 +1,12 @@
 package com.latticeengines.propdata.collection.service.impl;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.testng.Assert;
 
+import com.latticeengines.domain.exposed.propdata.manage.RefreshProgress;
 import com.latticeengines.propdata.collection.entitymanager.RefreshProgressEntityMgr;
 import com.latticeengines.propdata.collection.service.PivotService;
 import com.latticeengines.propdata.core.source.PivotedSource;
@@ -26,12 +30,44 @@ public class HGDataPivotServiceImplTestNG extends PivotServiceImplTestNGBase {
     }
 
     @Override
-    RefreshProgressEntityMgr getProgressEntityMgr() { return progressEntityMgr; }
+    RefreshProgressEntityMgr getProgressEntityMgr() {
+        return progressEntityMgr;
+    }
 
     @Override
-    PivotedSource getSource() { return source; }
+    PivotedSource getSource() {
+        return source;
+    }
 
     @Override
-    Integer getExpectedRows() { return 142; }
+    Integer getExpectedRows() {
+        return 142;
+    }
+
+    @Override
+    protected void verifyResultTable(RefreshProgress progress) {
+        super.verifyResultTable(progress);
+
+        String domain = "rutgers.edu";
+        Map<String, Object> row = jdbcTemplateCollectionDB.queryForMap("SELECT [" + source.getDomainField()
+                + "], [CloudTechnologies_ApplicationDevelopmentMgmt], [CloudTechnologies_ATS], [CloudTechnologies_DataCenterSolutions_One], "
+                + "[TechIndicator_AutodeskAutoCAD] FROM [" + source.getSqlTableName() + "] WHERE ["
+                + source.getDomainField() + "] = '" + domain + "'");
+        Assert.assertEquals(row.get(source.getDomainField()), domain);
+        Assert.assertEquals(row.get("CloudTechnologies_ApplicationDevelopmentMgmt"), 2);
+        Assert.assertEquals(row.get("CloudTechnologies_ATS"), 1);
+        Assert.assertEquals(row.get("CloudTechnologies_DataCenterSolutions_One"), 5);
+        Assert.assertEquals(row.get("TechIndicator_AutodeskAutoCAD"), "No");
+
+        domain = "stewartsshops.com";
+        row = jdbcTemplateCollectionDB.queryForMap("SELECT [" + source.getDomainField()
+                + "], [CloudTechnologies_DatabaseManagementSoftware], [CloudTechnologies_DataCenterSolutions_One], [TechIndicator_AutodeskAutoCAD] FROM ["
+                + source.getSqlTableName() + "] WHERE [" + source.getDomainField() + "] = '" + domain + "'");
+
+        Assert.assertEquals(row.get(source.getDomainField()), domain);
+        Assert.assertEquals(row.get("CloudTechnologies_DatabaseManagementSoftware"), 2);
+        Assert.assertEquals(row.get("CloudTechnologies_DataCenterSolutions_One"), 5);
+        Assert.assertEquals(row.get("TechIndicator_AutodeskAutoCAD"), "Yes");
+    }
 
 }
