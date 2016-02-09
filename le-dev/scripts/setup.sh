f7d42db099c59e3804cc78b1e7151522c791d463
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-02-09 17:40:33 +0000

    Review Request 2668: Improve error management for setup.sh

--- le-dev/scripts/setup.sh
+++ le-dev/scripts/setup.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+
+function runCommand()
+{
+  $1 2> /dev/null
+  if [ $? -eq 0 ]
+  then
+      exit 0
+  fi
+}
+
 shopt -s expand_aliases
 source $HOME/.bash_aliases
 cd $HOME/workspace/ledp
-mvn -Pgenerate -DskipTests clean install
-cd le-dataplatform && dpdpl
-cd ../le-eai && eaidpl
-cd ../le-dataflow && dfdpl
-cd ../le-dataflowapi && dfapidpl
-cd ../le-workflowapi && wfapidpl
-cd ../le-swlib && swlibdpl
-cd ../le-microservice && microservicedpl
+runCommand "mvn -Pgenerate -DskipTests clean install"
+runCommand "cd le-dataplatform && dpdpl"
+runCommand "cd ../le-eai && eaidpl"
+runCommand "cd ../le-dataflow && dfdpl"
+runCommand "cd ../le-dataflowapi && dfapidpl"
+runCommand "cd ../le-workflowapi && wfapidpl"
+runCommand "cd ../le-swlib && swlibdpl"
+runCommand "cd ../le-microservice && microservicedpl"
+
+
