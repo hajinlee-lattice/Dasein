46faa1a4596f6ec8d1015f10cc4abe9f8fbd9692
bross <bross@479f5059-62a3-4c39-9342-9852e3a68435>
2016-02-09 17:50:34 +0000

    Review Request 2669: Fix pom.xml to re-introduce workflowapi dependencies on serviceflows

--- le-workflowapi/src/test/java/com/latticeengines/workflowapi/flows/ImportEventTableWorkflowInContainerDeploymentTestNG.java
+++ le-workflowapi/src/test/java/com/latticeengines/workflowapi/flows/ImportEventTableWorkflowInContainerDeploymentTestNG.java
@@ -25,13 +25,13 @@ public class ImportEventTableWorkflowInContainerDeploymentTestNG extends ImportE
         setupForImportWorkflow();
     }
 
-    @Test(groups = "deployment", enabled = true)
+    @Test(groups = "deployment", enabled = false)
     public void testWorkflowAccount() throws Exception {
         SourceFile sourceFile = uploadFile(RESOURCE_BASE + "/Account.csv", SchemaInterpretation.SalesforceAccount);
         run(sourceFile);
     }
 
-    @Test(groups = "deployment", enabled = true)
+    @Test(groups = "deployment", enabled = false)
     public void testWorkflowLead() throws Exception {
         SourceFile sourceFile = uploadFile(RESOURCE_BASE + "/Lead.csv", SchemaInterpretation.SalesforceLead);
         run(sourceFile);
