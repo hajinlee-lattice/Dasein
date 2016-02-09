375a8beb815db3131399d77e36ed0a39ee7d79db
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-02-09 19:54:05 +0000

    Review Request 2668: Improve error management for setup.sh

--- le-dev/scripts/setup.sh
+++ le-dev/scripts/setup.sh
@@ -2,23 +2,36 @@
 
 function runCommand()
 {
-  $1 2> /dev/null
+  $1
   if [ $? -eq 0 ]
   then
       exit 0
   fi
 }
 
+echo "Expanding aliases."
 shopt -s expand_aliases
+echo "Sourcing aliases file"
 source $HOME/.bash_aliases
+echo "Changing dir into workspace"
 cd $HOME/workspace/ledp
+echo "Top-level compile"
 runCommand "mvn -Pgenerate -DskipTests clean install"
+echo "Deploying dataplatform to local Hadoop"
 runCommand "cd le-dataplatform && dpdpl"
+echo "Deploying eai to local Hadoop"
 runCommand "cd ../le-eai && eaidpl"
+echo "Deploying dataflow to local Hadoop"
 runCommand "cd ../le-dataflow && dfdpl"
+echo "Deploying dataflowapi to local Hadoop"
 runCommand "cd ../le-dataflowapi && dfapidpl"
+echo "Deploying workflow to local Hadoop"
+runCommand "cd ../le-workflow && wfdpl"
+echo "Deploying workflowapi to local Hadoop"
 runCommand "cd ../le-workflowapi && wfapidpl"
+echo "Deploying swlib to local Hadoop"
 runCommand "cd ../le-swlib && swlibdpl"
+echo "Deploying microservices to local jetty"
 runCommand "cd ../le-microservice && microservicedpl"
 
 
