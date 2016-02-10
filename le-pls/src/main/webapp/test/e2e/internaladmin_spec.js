0c173b5d49d3e18d3398bc0a3a4e89b3ded4faa8
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-02-10 16:19:40 +0000

    Review Request 2683 fix pls protractor test - internal admin page

--- le-pls/src/main/webapp/test/e2e/internaladmin_spec.js
+++ le-pls/src/main/webapp/test/e2e/internaladmin_spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('internal admin page', function() {
+describe('internal admin page', function () {
     var loginPage = require('./po/login.po');
     var modelTabs = require('./po/modeltabs.po');
     var helper = require('./po/helper.po');
@@ -8,17 +8,18 @@ describe('internal admin page', function() {
     var modelNamesOnHDFS = ["PLSModel-Eloqua1", "PLSModel-Eloqua2"];
     var modelOnHDFSAndPage;
 
-    var expectedAttributesPresentOnInternalAdminPage = ["Model Health Score", "Tenant ID", "Tenant Name", "Dataloader Tenant Name",
-        "Internal ID", "Template Version", "Model Details", "Scored Leads", "Model Performance"];
+    var expectedAttributesPresentOnInternalAdminPage = ["Model Health Score", "Tenant ID", "Tenant Name",
+        "Dataloader URL", "Dataloader Tenant Name", "Internal ID", "Template Version", "Model Details",
+        "Scored Leads", "Model Performance"];
 
     var modelDetailFiles = ["modelsummary.json", "predictors.csv", "diagnostics.json", "metadata.avsc"];
     var scoredLeadFiles = ["readout.csv", "scores.csv"];
     var modelPerformanceFiles = ["performance.csv", "rfmodel.csv"];
 
-    it('should have at least one model with information on HDFS', function() {
+    it('should have at least one model with information on HDFS', function () {
         loginPage.loginAsSuperAdmin();
         var modelNamesElement = element.all(by.css('.model-title h2.editable'));
-        modelNamesElement.getText().then(function(modelNamesOnPage) {
+        modelNamesElement.getText().then(function (modelNamesOnPage) {
             for (var i = 0; i < modelNamesOnPage.length; i++) {
                 var modelNameOnPage = modelNamesOnPage[i];
                 if (modelNamesOnHDFS.indexOf(modelNameOnPage.substring(0, modelNamesOnHDFS[0].length)) >= 0) {
@@ -29,20 +30,20 @@ describe('internal admin page', function() {
         });
     });
 
-    it('should have all the necessary attributes on the table', function() {
+    it('should have all the necessary attributes on the table', function () {
         element(by.cssContainingText('.editable', modelOnHDFSAndPage)).click();
         modelTabs.getTabByIndex(2).click();
         browser.driver.sleep(1000);
         element(by.linkText('Admin')).click();
         browser.driver.sleep(6000);
 
-        var headers = element.all(by.css('table.table tbody tr th')).map(function(tableHeader) {
+        var headers = element.all(by.css('table.table tbody tr th')).map(function (tableHeader) {
             return tableHeader.getText();
         });
         expect(headers).toEqual(expectedAttributesPresentOnInternalAdminPage);
     });
 
-    it('all the links should be downloadable', function() {
+    it('all the links should be downloadable', function () {
         var modelDetails = element(by.cssContainingText('td', 'Model Summary JSON'));
         var scoredLeads = element(by.cssContainingText('td', 'Readout Sample CSV'));
         var modelPerformance = element(by.cssContainingText('td', 'Performance Chart CSV'));
