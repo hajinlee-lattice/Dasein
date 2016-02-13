af4e1ab42783aaf7b0217877f3d8c3bbeffb830d
bnguyen <bnguyen@479f5059-62a3-4c39-9342-9852e3a68435>
2016-02-13 01:04:16 +0000

    Review Request 2593 le-scoringapi stubbed data for SureShot

---
+++ le-scoringapi/src/main/java/com/latticeengines/scoringapi/unused/ScoreRequest.java
@@ -0,0 +1,29 @@
+package com.latticeengines.scoringapi.unused;
+
+import java.util.Map;
+
+import com.latticeengines.domain.exposed.camille.CustomerSpace;
+import com.latticeengines.domain.exposed.scoringapi.unused.ModelTags;
+
+public class ScoreRequest {
+    public ScoreRequest(CustomerSpace space, String combination, Map<String, Object> record) {
+        this.space = space;
+        this.combination = combination;
+        this.record = record;
+        this.tag = ModelTags.ACTIVE;
+    }
+
+    public ScoreRequest() {
+        this.tag = ModelTags.ACTIVE;
+    }
+
+    public CustomerSpace space;
+
+    // Combination of models that is the target of this score request.
+    public String combination;
+
+    // Which tagged version of the models to score against.
+    public String tag;
+
+    public Map<String, Object> record;
+}
