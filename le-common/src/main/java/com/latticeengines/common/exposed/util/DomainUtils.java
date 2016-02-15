7f2d0c39c53066ee8d151fe939b3c78cc3a8a920
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-02-15 00:06:01 +0000

    Review Request 2702 standard domain to lower case

--- le-common/src/main/java/com/latticeengines/common/exposed/util/DomainUtils.java
+++ le-common/src/main/java/com/latticeengines/common/exposed/util/DomainUtils.java
@@ -3,19 +3,24 @@ package com.latticeengines.common.exposed.util;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang.StringUtils;
+
 public class DomainUtils {
 
     private static Pattern pDomainNameOnly = Pattern.compile("((?!-)[A-Za-z0-9-]{1,63}(?<!-)\\.)+[A-Za-z]{2,6}");
 
     public static String parseDomain(String url) {
-        Matcher matcher = pDomainNameOnly.matcher(url);
-        if (matcher.find()) {
-            String domain = matcher.group(0);
-            domain =  domain.startsWith("www.") ? domain.substring(4) : domain;
-            if (domain.contains(".")) {
-                return domain;
-            } else {
-                return null;
+        if (StringUtils.isNotEmpty(url)) {
+            url = url.toLowerCase();
+            Matcher matcher = pDomainNameOnly.matcher(url);
+            if (matcher.find()) {
+                String domain = matcher.group(0);
+                domain = domain.startsWith("www.") ? domain.substring(4) : domain;
+                if (domain.contains(".")) {
+                    return domain;
+                } else {
+                    return null;
+                }
             }
         }
         return null;
