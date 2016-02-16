23c252e2d8a43867c45b45fe91db14119add7ef0
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-02-16 17:10:56 +0000

    Review Request 2702 monitor.alert.service.enabled

--- le-monitor/src/main/java/com/latticeengines/monitor/alerts/service/impl/AlertServiceImpl.java
+++ le-monitor/src/main/java/com/latticeengines/monitor/alerts/service/impl/AlertServiceImpl.java
@@ -20,7 +20,7 @@ public class AlertServiceImpl implements AlertService {
 
     private static final Log log = LogFactory.getLog(AlertServiceImpl.class);
 
-    @Value("${monitor.alertService.enabled}")
+    @Value("${monitor.alert.service.enabled}")
     private boolean alertServiceEnabled;
 
     @Autowired
