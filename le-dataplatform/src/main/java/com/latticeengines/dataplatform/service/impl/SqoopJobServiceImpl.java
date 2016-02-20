067497740d7ff28979a0af6eb3dd65fbe1739abc
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2016-02-20 21:05:22 +0000

    Review Request 2754: sqoop csv import row level validation

--- le-dataplatform/src/main/java/com/latticeengines/dataplatform/service/impl/SqoopJobServiceImpl.java
+++ le-dataplatform/src/main/java/com/latticeengines/dataplatform/service/impl/SqoopJobServiceImpl.java
@@ -15,7 +15,6 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.mapreduce.MRJobConfig;
 import org.apache.hadoop.mapreduce.filecache.DistributedCache;
 import org.apache.hadoop.mapreduce.v2.app.LedpMRAppMaster;
 import org.apache.hadoop.yarn.api.records.ApplicationId;
