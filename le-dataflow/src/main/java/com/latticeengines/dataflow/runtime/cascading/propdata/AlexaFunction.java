1c2ecfd95d731f3d965455f2002218e407971383
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-02-21 17:47:31 +0000

    Review Request 2767: Organize imports

--- le-dataflow/src/main/java/com/latticeengines/dataflow/runtime/cascading/propdata/AlexaFunction.java
+++ le-dataflow/src/main/java/com/latticeengines/dataflow/runtime/cascading/propdata/AlexaFunction.java
@@ -1,8 +1,6 @@
 package com.latticeengines.dataflow.runtime.cascading.propdata;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 import cascading.flow.FlowProcess;
 import cascading.operation.BaseOperation;
