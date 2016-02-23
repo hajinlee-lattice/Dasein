434fc1e7071addfbdf50ec987541027fd54b317a
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-02-23 19:44:07 +0000

    Review Request 2808: Scripts for local development to setup Hadoop-related stuff.

---
+++ le-dev/scripts/start-cluster.sh
@@ -0,0 +1,15 @@
+rm -f /tmp/*.out
+rm -Rf $HADOOP_HOME/logs/*.log*
+rm -Rf $HADOOP_HOME/logs/*.out*
+rm -Rf $HADOOP_HOME/logs/userlogs/*
+
+export YARN_RESOURCEMANAGER_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=5001,server=y,suspend=n"
+export YARN_NODEMANAGER_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=5002,server=y,suspend=n"
+
+$HADOOP_HOME/sbin/hadoop-daemon.sh start namenode
+$HADOOP_HOME/sbin/hadoop-daemon.sh start datanode
+$HADOOP_HOME/sbin/yarn-daemon.sh start resourcemanager
+$HADOOP_HOME/sbin/yarn-daemon.sh start nodemanager
+$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
+ZOO_LOG_DIR=$ZOOKEEPER_HOME/logs $ZOOKEEPER_HOME/bin/zkServer.sh start
+
