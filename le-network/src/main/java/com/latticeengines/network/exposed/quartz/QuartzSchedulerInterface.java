d2f94850ee7174bb3955c7015f9ca3637c21d063
jhe <jhe@479f5059-62a3-4c39-9342-9852e3a68435>
2016-02-24 07:32:59 +0000

    quartz scheduler(missing file) review request 2633

---
+++ le-network/src/main/java/com/latticeengines/network/exposed/quartz/QuartzSchedulerInterface.java
@@ -0,0 +1,21 @@
+package com.latticeengines.network.exposed.quartz;
+
+import java.util.List;
+
+import com.latticeengines.domain.exposed.quartz.JobConfig;
+import com.latticeengines.domain.exposed.quartz.JobInfo;
+import com.latticeengines.domain.exposed.quartz.JobInfoDetail;
+
+public interface QuartzSchedulerInterface {
+    Boolean setSchedulerStatus(String status);
+
+    Boolean addJob(String tenantId, JobConfig jobConfig);
+
+    Boolean deleteJob(String tenantId, String jobName);
+
+    List<JobInfo> listJobs(String tenantId);
+
+    List<JobInfo> listAllJobs();
+
+    JobInfoDetail getJobDetail(String tenantId, String jobName);
+}
