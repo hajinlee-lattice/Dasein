eb4b6510484b4838882d15d4ca25029c3491d524
ytian <ytian@479f5059-62a3-4c39-9342-9852e3a68435>
2016-03-02 06:47:32 +0000

    Review Request 2925 MissingLeadReport

---
+++ le-upgrade/src/main/python/LPMissingLeadReport.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import os, sys
+import appsequence
+import lpMissingLeadsReport
+
+PATCH_PATH = os.path.dirname(__file__)
+REVISION = '$Rev: 70953 $'
+
+print ''
+print 'PATH : {0}'.format(PATCH_PATH)
+print 'REV  : {0}'.format(REVISION)
+print ''
+
+(checkOnly, tenantFileName, resultsFileName) = appsequence.AppArgs.get(sys.argv)
+sequence = []
+sequence.append(appsequence.LPCheckVersion('2.3.0'))
+sequence.append(lpMissingLeadsReport.MissingLeadsReport())
+#sequence.append(appsequence.LPSetVersion('2.3.0'))
+app = appsequence.AppSequence(tenantFileName, resultsFileName, sequence, checkOnly)
+app.execute()
