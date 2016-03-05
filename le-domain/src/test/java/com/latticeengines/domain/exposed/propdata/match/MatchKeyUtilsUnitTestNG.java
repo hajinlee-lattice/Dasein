6c449658358bb49ebe71b0260019729158c842e7
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-03-05 00:26:52 +0000

    Review Request 3019 forgot to check in a unit test

---
+++ le-domain/src/test/java/com/latticeengines/domain/exposed/propdata/match/MatchKeyUtilsUnitTestNG.java
@@ -0,0 +1,43 @@
+package com.latticeengines.domain.exposed.propdata.match;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.latticeengines.common.exposed.util.JsonUtils;
+
+public class MatchKeyUtilsUnitTestNG {
+
+    @Test(groups = "unit", dataProvider = "resolveDomainDataProvider")
+    public void testResolveDomain(String[] fields, String[] expectedFields) throws IOException {
+        Map<MatchKey, List<String>> keyMap = MatchKeyUtils.resolveKeyMap(Arrays.asList(fields));
+        List<String> resolvedFields = keyMap.get(MatchKey.Domain);
+        Assert.assertEquals(resolvedFields.size(), expectedFields.length);
+        if (expectedFields.length > 0) {
+            for (int i = 0; i < resolvedFields.size(); i++) {
+                Assert.assertEquals(resolvedFields.get(i), expectedFields[i],
+                        String.format("Expected [%s] to be [%s]",
+                                StringUtils.join(resolvedFields, ", "),
+                                StringUtils.join(expectedFields, ",")));
+            }
+        }
+    }
+    
+    @DataProvider(name = "resolveDomainDataProvider")
+    private Object[][] resolveDomainDataProvider() {
+        return new Object[][] {
+                { new String[] { "Domain" }, new String[] { "Domain" } },
+                { new String[] { "Domain", "Website", "Email" }, new String[] { "Domain", "Website", "Email" } },
+                { new String[] { "Website", "Domain", "Email" }, new String[] { "Domain", "Website", "Email" } },
+                { new String[] { "DomainName", "Website", "EmailAddress" },
+                        new String[] { "DomainName", "Website", "EmailAddress" } },
+        };
+    }
+
+}
