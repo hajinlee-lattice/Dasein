03cd8c5bed2968bd7d01c6f2cb9ccadff24170e5
mwilson <mwilson@479f5059-62a3-4c39-9342-9852e3a68435>
2016-03-09 20:43:43 +0000

    Review Request 3076: Add Template Type; Reformat Code

--- le-liaison/src/main/python/GetVersionForTenants.py
+++ le-liaison/src/main/python/GetVersionForTenants.py
@@ -3,43 +3,46 @@
 import sys, datetime, time, requests, re
 from liaison import *
 
-def GetVersionForTenants( tenantFileName, versionFileName ):
-    
+def getVersionForTenants(tenantFileName, versionFileName):
+
     tenants = []
-    
-    with open( tenantFileName ) as tenantFile:
+
+    with open(tenantFileName) as tenantFile:
         for line in tenantFile:
             cols = line.strip().split(',')
             tenants.append(cols[0])
 
-    with open( versionFileName, mode = 'w' ) as versionFile:
+    with open(versionFileName, mode = 'w') as versionFile:
 
-        versionFile.write( 'TenantName,Version,CallTime\n' )
-        
+        versionFile.write('TenantName,Type,Version,CallTime\n')
+
         for t in tenants:
 
-            print '{0}...'.format( t )
+            print '{0}...'.format(t)
 
+            type = 'Unknown'
             version = 'Unknown'
             calltime = 0.0
             isLP = False
 
             try:
-                conn_mgr = ConnectionMgrFactory.Create( 'visiDB', tenant_name=t, verify=False, verbose=True )
+                conn_mgr = ConnectionMgrFactory.Create('visiDB', tenant_name=t, verify=False, verbose=True)
                 isLP = True
             except TenantNotMappedToURL:
                 version = 'Not on LP DataLoader'
-            
+
             t0 = time.time()
 
             if isLP:
                 try:
-                    exp_version = conn_mgr.getNamedExpression( 'Version' )
+                    exp_version = conn_mgr.getNamedExpression('Version')
                     defn = exp_version.Object().definition()
-                    c = re.search( 'LatticeFunctionExpressionConstant\(\"(.*?)\".*LatticeFunctionExpressionConstant\(\"(.*?)\"', defn )
+                    c = re.search('LatticeFunctionExpressionConstant\(\"PLS (.*?) Template:\".*LatticeFunctionExpressionConstant\(\"(.*?)\"', defn)
                     if c:
+                        type = c.group(1)
                         version = c.group(2)
                     else:
+                        type = 'Nonstandard type'
                         version = 'Nonstandard version'
                 except requests.exceptions.SSLError:
                     ## Not on a PROD DataLoader
@@ -48,38 +51,39 @@ def GetVersionForTenants( tenantFileName, versionFileName ):
                     ## Not on a PROD DataLoader
                     pass
                 except UnknownVisiDBSpec:
+                    type = 'No template type'
                     version = 'No template version'
 
             t1 = time.time()
 
             calltime = t1-t0
-            
-            versionFile.write( '{0},{1},{2}\n'.format(t,version,calltime) )
+
+            versionFile.write('{0},{1},{2},{3}\n'.format(t,type,version,calltime))
 
 
-def Usage( cmd, exit_code ):
+def usage(cmd, exit_code):
     path = ''
     i = cmd.rfind('\\')
-    if( i != -1 ):
+    if(i != -1):
         path = cmd[:i]
         cmd = cmd[i+1:]
-    
+
     print ''
-    print 'Usage: {0} <tenant_list.csv> <version_data.csv>'.format( cmd )
+    print 'Usage: {0} <tenant_list.csv> <version_data.csv>'.format(cmd)
     print ''
-    
-    exit( exit_code )
+
+    exit(exit_code)
 
 
 if __name__ == "__main__":
 
     if len(sys.argv) == 1:
-        Usage( sys.argv[0], 0 )
-    
+        usage(sys.argv[0], 0)
+
     if len(sys.argv) != 3:
-        Usage( sys.argv[0], 1 )
+        usage(sys.argv[0], 1)
 
     tenantFileName = sys.argv[1]
     versionFileName = sys.argv[2]
-    
-    GetVersionForTenants( tenantFileName, versionFileName )
+
+    getVersionForTenants(tenantFileName, versionFileName)
