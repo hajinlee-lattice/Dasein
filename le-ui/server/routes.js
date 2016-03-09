1b3d418b7f1b8191066ae3df4b1adbdecf632901
JLazarus <JLazarus@479f5059-62a3-4c39-9342-9852e3a68435>
2016-03-09 00:55:25 +0000

    attempt #5 to fix qa - Review Request 2987

--- le-ui/server/routes.js
+++ le-ui/server/routes.js
@@ -8,7 +8,6 @@ module.exports = [
             '/assets': '/assets',
             '/lib': '/lib',
             '/dist': '/dist',
-            '/bower_components': '/bower_components',
             '/common/assets': '/assets',
             '/common/dist': '/dist'
         }
