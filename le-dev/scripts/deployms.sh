b48162cd08decaed1bdb87bf679ef095a0623c53
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-03-15 23:14:16 +0000

    Review Request 3130 add some tomcat alias

--- le-dev/scripts/deployms.sh
+++ le-dev/scripts/deployms.sh
@@ -2,15 +2,15 @@
 
 VERSION=`cat ../le-parent/pom.xml | grep \<version\> | head -n 1 | cut -d \< -f 2 | cut -d \> -f 2`
 
-rm -rf $CATALINA_HOME/webapps/*.war
+rm -rf $CATALINA_HOME/webapps/ms/*.war
 
 for f in $(find . -name "*.war"); do
     fn=`echo $f | cut -d - -f 2`
     if [ $fn = "microservice" ]
     then
-        fn=doc
+        fn=ROOT
     fi
-    cp $f $CATALINA_HOME/webapps/$fn\#\#$VERSION.war;
+    cp $f $CATALINA_HOME/webapps/ms/$fn\#\#$VERSION.war;
 done
 
-ls $CATALINA_HOME/webapps/*.war
+ls $CATALINA_HOME/webapps/ms/*.war
--- le-dev/scripts/setupenv.sh
+++ le-dev/scripts/setupenv.sh
@@ -2,6 +2,7 @@
 
 printf "%s\n" "${HADOOP_CONF_DIR:?You must set HADOOP_CONF_DIR}"
 printf "%s\n" "${WSHOME:?You must set WSHOME}"
+printf "%s\n" "${CATALINA_HOME:?You must set CATALINA_HOME}"
 
 cp $WSHOME/le-dev/hadoop/dev/capacity-scheduler.xml $HADOOP_CONF_DIR
 cp $WSHOME/le-dev/hadoop/dev/mapred-site.xml $HADOOP_CONF_DIR
@@ -12,4 +13,7 @@ hadoop fs -mkdir -p /apps/tez || true
 hadoop fs -copyFromLocal $WSHOME/le-dev/hadoop/artifacts/tez-0.6.2.tar.gz /apps/tez || true
 
 pip install -r $WSHOME/le-dev/scripts/requirements.txt
+
+sudo mkdir -p /etc/ledp
+sudo cp $WSHOME/le-security/certificates/laca-ldap.dev.lattice.local.jks /etc/ledp
 
