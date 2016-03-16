121a6241ee44132f13e5b6b8dfa066d48a00041f
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-03-16 03:51:49 +0000

    Review Request 2571: Fix warnings

--- le-microservice/core/src/main/java/com/latticeengines/microservice/controller/StatusController.java
+++ le-microservice/core/src/main/java/com/latticeengines/microservice/controller/StatusController.java
@@ -1,20 +1,10 @@
 package com.latticeengines.microservice.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
 
 import com.wordnik.swagger.annotations.Api;
