6abad7b437510e75978a218c73baa52741725b4a
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-03-24 17:20:41 +0000

    Review Request 3333 by pass ssl check in customized yarn container

--- le-dataplatform/src/main/java/com/latticeengines/dataplatform/service/impl/YarnClientCustomizationServiceImpl.java
+++ le-dataplatform/src/main/java/com/latticeengines/dataplatform/service/impl/YarnClientCustomizationServiceImpl.java
@@ -4,10 +4,20 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.UUID;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
@@ -183,6 +193,34 @@ public class YarnClientCustomizationServiceImpl implements YarnClientCustomizati
         }
         YarnClientCustomization customization = YarnClientCustomization.getCustomization(clientName);
         customization.finalize(appMasterProperties, containerProperties);
+        try {
+            turnOffSslChecking();
+        } catch (Exception e) {
+            log.warn("Could not turn off ssl check.", e);
+        }
+    }
+
+    //TODO: remove this when enabling https on production cluster
+    private void turnOffSslChecking() throws NoSuchAlgorithmException, KeyManagementException {
+        final TrustManager[] UNQUESTIONING_TRUST_MANAGER = new TrustManager[] { new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+            }
+
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+            }
+        } };
+        final SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, UNQUESTIONING_TRUST_MANAGER, null);
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        });
     }
 
 }
