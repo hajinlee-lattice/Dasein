47fc496508a44e88b621e4a11f794abcf8c33156
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2016-03-28 23:17:20 +0000

    Review Request 3366: remove datatype.avsc from input directory,	instead generating the column type mapping in mr job

--- le-scoring/src/main/java/com/latticeengines/scoring/util/LocalizedFiles.java
+++ le-scoring/src/main/java/com/latticeengines/scoring/util/LocalizedFiles.java
@@ -1,18 +1,17 @@
 package com.latticeengines.scoring.util;
 
-import java.util.HashMap;
-
+import java.util.Map;
 import com.fasterxml.jackson.databind.JsonNode;
 
 public class LocalizedFiles {
     // key: modelGuid, value: model contents
-    private HashMap<String, JsonNode> models;
+    private Map<String, JsonNode> models;
 
-    public HashMap<String, JsonNode> getModels() {
+    public Map<String, JsonNode> getModels() {
         return this.models;
     }
 
-    public void setModels(HashMap<String, JsonNode> models) {
+    public void setModels(Map<String, JsonNode> models) {
         this.models = models;
     }
 
