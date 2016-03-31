05cfba2fa0b88a2105ef9e2ac56cd838c7f3275e
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2016-03-31 18:26:15 +0000

    Review Request 3409: PLS-1043 Combine score output with score input	data flow and data flow workflow step

--- le-domain/src/main/java/com/latticeengines/domain/exposed/dataflow/DataFlowParameters.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/dataflow/DataFlowParameters.java
@@ -3,6 +3,7 @@ package com.latticeengines.domain.exposed.dataflow;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.latticeengines.domain.exposed.dataflow.flows.AddStandardAttributesParameters;
+import com.latticeengines.domain.exposed.dataflow.flows.CombineInputTableWithScoreParameters;
 import com.latticeengines.domain.exposed.dataflow.flows.DedupEventTableParameters;
 import com.latticeengines.domain.exposed.dataflow.flows.CreateAttributeLevelSummaryParameters;
 import com.latticeengines.domain.exposed.dataflow.flows.CreateScoreTableParameters;
@@ -10,11 +11,12 @@ import com.latticeengines.domain.exposed.dataflow.flows.QuotaFlowParameters;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT, property = "property")
 @JsonSubTypes({ //
-@JsonSubTypes.Type(value = QuotaFlowParameters.class, name = "quotaFlowParameters"), //
-@JsonSubTypes.Type(value = CreateScoreTableParameters.class, name = "createScoreTableParameters"), //
-@JsonSubTypes.Type(value = CreateAttributeLevelSummaryParameters.class, name = "createAttributeLevelSummaryParameters"), //
-@JsonSubTypes.Type(value = DedupEventTableParameters.class, name = "dedupEventTableParameters"), //
-@JsonSubTypes.Type(value = AddStandardAttributesParameters.class, name = "addStandardAttributesParameters") //
+        @JsonSubTypes.Type(value = QuotaFlowParameters.class, name = "quotaFlowParameters"), //
+        @JsonSubTypes.Type(value = CreateScoreTableParameters.class, name = "createScoreTableParameters"), //
+        @JsonSubTypes.Type(value = CombineInputTableWithScoreParameters.class, name = "combineInputTableWithScoreParameters"), //
+        @JsonSubTypes.Type(value = CreateAttributeLevelSummaryParameters.class, name = "createAttributeLevelSummaryParameters"), //
+        @JsonSubTypes.Type(value = DedupEventTableParameters.class, name = "dedupEventTableParameters"), //
+        @JsonSubTypes.Type(value = AddStandardAttributesParameters.class, name = "addStandardAttributesParameters") //
 })
 public class DataFlowParameters {
 }
