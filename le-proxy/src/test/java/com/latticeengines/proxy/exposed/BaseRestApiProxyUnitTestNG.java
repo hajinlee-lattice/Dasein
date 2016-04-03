b22d47db86174f82ff12f208269d73f3dde947ed
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-04-03 03:23:40 +0000

    Review Request 2571: Fix warnings

--- le-proxy/src/test/java/com/latticeengines/proxy/exposed/BaseRestApiProxyUnitTestNG.java
+++ le-proxy/src/test/java/com/latticeengines/proxy/exposed/BaseRestApiProxyUnitTestNG.java
@@ -5,15 +5,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
-import com.latticeengines.proxy.exposed.workflowapi.WorkflowProxy;
-
 @ContextConfiguration(locations = { "classpath:test-proxy-context.xml" })
 public class BaseRestApiProxyUnitTestNG extends AbstractTestNGSpringContextTests {
     @Autowired
     private TestProxy testProxy;
-
-    @Autowired
-    private WorkflowProxy workflowProxy;
 
     @Test(groups = "unit")
     public void testUrlExpansion() {
