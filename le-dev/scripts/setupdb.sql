61605bfcd586df238b8053f25ff7b5ecd7b3e7d3
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-04-06 21:32:45 +0000

    Review Request 3312: Add db setup script.

--- le-dev/scripts/setupdb.sql
+++ le-dev/scripts/setupdb.sql
@@ -1,10 +1,20 @@
+DROP DATABASE PLS_MultiTenant;
+
 CREATE DATABASE IF NOT EXISTS PLS_MultiTenant;
+
+GRANT ALL on PLS_MultiTenant.* to root;
 
 USE PLS_MultiTenant;
 
 source WSHOME/le-db/ddl_pls_multitenant_mysql5innodb.sql
 
+DROP DATABASE LDC_ManageDB;
+
 CREATE DATABASE IF NOT EXISTS LDC_ManageDB;
+
+GRANT ALL on LDC_ManageDB.* to root;
+
+USE LDC_ManageDB;
 
 source WSHOME/le-db/ddl_ldc_managedb_mysql5innodb.sql
 
