cdf23cf4c36dd83c3e940ca76e30bca0fc49dfe0
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-04-08 22:52:33 +0000

    Review Request 3538: Don't format the generated script.

--- le-dev/scripts/setupdb.sh
+++ le-dev/scripts/setupdb.sh
@@ -3,4 +3,7 @@
 # Test for required env variables
 printf "%s\n" "${WSHOME:?You must set WSHOME}"
 
-sed "s|WSHOME|$WSHOME|g" setupdb.sql | mysql -u root -pwelcome
+sed -i 's/alter table .* drop foreign key .*;//g' $WSHOME/le-db/ddl_pls_multitenant_mysql5innodb.sql
+sed -i 's/alter table .* drop foreign key .*;//g' $WSHOME/le-db/ddl_ldc_managedb_mysql5innodb.sql
+
+sed "s|WSHOME|$WSHOME|g" $WSHOME/le-dev/scripts/setupdb.sql | mysql -u root -pwelcome
