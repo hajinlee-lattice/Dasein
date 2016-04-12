fc1db90f249a734cbe8d7d77f347d4e573e1efb4
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-04-12 16:36:34 +0000

    Review Request 3581 fix pls Segment EntityMgr test

--- le-pls/src/test/java/com/latticeengines/pls/entitymanager/impl/SegmentEntityMgrImplTestNG.java
+++ le-pls/src/test/java/com/latticeengines/pls/entitymanager/impl/SegmentEntityMgrImplTestNG.java
@@ -6,7 +6,6 @@ import static org.testng.Assert.assertNotNull;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -15,12 +14,11 @@ import com.latticeengines.domain.exposed.pls.Segment;
 import com.latticeengines.domain.exposed.security.Tenant;
 import com.latticeengines.pls.entitymanager.ModelSummaryEntityMgr;
 import com.latticeengines.pls.entitymanager.SegmentEntityMgr;
-import com.latticeengines.pls.functionalframework.PlsFunctionalTestNGBaseDeprecated;
-import com.latticeengines.security.exposed.entitymanager.TenantEntityMgr;
+import com.latticeengines.pls.functionalframework.PlsFunctionalTestNGBase;
 import com.latticeengines.security.exposed.service.TenantService;
 
-public class SegmentEntityMgrImplTestNG extends PlsFunctionalTestNGBaseDeprecated {
-    
+public class SegmentEntityMgrImplTestNG extends PlsFunctionalTestNGBase {
+
     @Autowired
     private ModelSummaryEntityMgr modelSummaryEntityMgr;
 
@@ -28,34 +26,30 @@ public class SegmentEntityMgrImplTestNG extends PlsFunctionalTestNGBaseDeprecate
     private SegmentEntityMgr segmentEntityMgr;
 
     @Autowired
-    private TenantEntityMgr tenantEntityMgr;
-
-    @Autowired
     private TenantService tenantService;
-    
+
     private Tenant tenant1;
     private Tenant tenant2;
-    
+
     @BeforeClass(groups = "functional")
     public void setup() throws Exception {
-        setupDbWithMarketoSMB("TENANT1", "TENANT1");
-        setupDbWithEloquaSMB("TENANT2", "TENANT2");
-        
-        tenant1 = tenantEntityMgr.findByTenantId("TENANT1");
-        tenant2 = tenantEntityMgr.findByTenantId("TENANT2");
-        
+        setupMarketoEloquaTestEnvironment();
+
+        tenant1 = marketoTenant;
+        tenant2 = eloquaTenant;
+
         setupSecurityContext(tenant1);
         List<ModelSummary> summariesForTenant1 = modelSummaryEntityMgr.findAll();
         setupSecurityContext(tenant2);
         List<ModelSummary> summariesForTenant2 = modelSummaryEntityMgr.findAll();
-        
+
         Segment segment1 = new Segment();
         segment1.setModelId(summariesForTenant1.get(0).getId());
         segment1.setName("SMB");
         segment1.setPriority(1);
         segment1.setTenant(tenant1);
         setupSecurityContext(tenant1);
-        for (Segment segment: segmentEntityMgr.findAll()) {
+        for (Segment segment : segmentEntityMgr.findAll()) {
             segmentEntityMgr.delete(segment);
         }
         segmentEntityMgr.create(segment1);
@@ -66,25 +60,19 @@ public class SegmentEntityMgrImplTestNG extends PlsFunctionalTestNGBaseDeprecate
         segment2.setPriority(2);
         segment2.setTenant(tenant2);
         setupSecurityContext(tenant2);
-        for (Segment segment: segmentEntityMgr.findAll()) {
+        for (Segment segment : segmentEntityMgr.findAll()) {
             segmentEntityMgr.delete(segment);
         }
         segmentEntityMgr.create(segment2);
     }
 
-    @AfterClass(groups = "functional")
-    public void teardown() throws Exception {
-        tenantService.discardTenant(tenant1);
-        tenantService.discardTenant(tenant2);
-    }
-    
     @Test(groups = "functional")
     public void findAll() {
         setupSecurityContext(tenant1);
         List<Segment> segments = segmentEntityMgr.findAll();
         assertEquals(segments.size(), 1);
     }
-    
+
     @Test(groups = "functional")
     public void getAll() {
         assertEquals(segmentEntityMgr.getAll().size(), 2);
