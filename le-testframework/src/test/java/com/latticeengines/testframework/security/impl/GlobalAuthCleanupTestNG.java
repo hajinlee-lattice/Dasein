12cf98d3862441aa21bca39aceb2b6fb25d65654
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-04-13 23:06:22 +0000

    Review Request 3627: clean up DL tenants

--- le-testframework/src/test/java/com/latticeengines/testframework/security/impl/GlobalAuthCleanupTestNG.java
+++ le-testframework/src/test/java/com/latticeengines/testframework/security/impl/GlobalAuthCleanupTestNG.java
@@ -1,32 +1,50 @@
 package com.latticeengines.testframework.security.impl;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.http.message.BasicNameValuePair;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.web.client.RestTemplate;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.latticeengines.camille.exposed.Camille;
 import com.latticeengines.camille.exposed.CamilleEnvironment;
 import com.latticeengines.camille.exposed.paths.PathBuilder;
 import com.latticeengines.common.exposed.util.HdfsUtils;
+import com.latticeengines.common.exposed.util.HttpClientWithOptionalRetryUtils;
+import com.latticeengines.common.exposed.util.JsonUtils;
+import com.latticeengines.domain.exposed.admin.DeleteVisiDBDLRequest;
+import com.latticeengines.domain.exposed.admin.TenantDocument;
 import com.latticeengines.domain.exposed.camille.CustomerSpace;
 import com.latticeengines.domain.exposed.camille.Path;
+import com.latticeengines.domain.exposed.dataloader.InstallResult;
+import com.latticeengines.domain.exposed.security.Credentials;
 import com.latticeengines.domain.exposed.security.Tenant;
+import com.latticeengines.remote.exposed.service.DataLoaderService;
+import com.latticeengines.security.exposed.Constants;
+import com.latticeengines.security.exposed.MagicAuthenticationHeaderHttpRequestInterceptor;
 import com.latticeengines.security.exposed.service.TenantService;
 import com.latticeengines.testframework.exposed.utils.TestFrameworkUtils;
+import com.latticeengines.testframework.rest.LedpResponseErrorHandler;
 
 @TestExecutionListeners({ DirtiesContextTestExecutionListener.class })
-@ContextConfiguration(locations = { "classpath:test-testframework-cleanup-context.xml",
-        "classpath:camille-runtime-context.xml" })
+@ContextConfiguration(locations = { "classpath:test-testframework-cleanup-context.xml" })
 public class GlobalAuthCleanupTestNG extends AbstractTestNGSpringContextTests {
 
     private static final Log log = LogFactory.getLog(GlobalAuthCleanupTestNG.class);
@@ -39,13 +57,28 @@ public class GlobalAuthCleanupTestNG extends AbstractTestNGSpringContextTests {
     @Autowired
     private Configuration yarnConfiguration;
 
+    @Autowired
+    private DataLoaderService dataLoaderService;
+
+    @Value("${admin.test.deployment.api:http://localhost:8085}")
+    private String adminApiHostPort;
+
     private Camille camille;
     private String podId;
+    private RestTemplate magicRestTemplate = new RestTemplate();
+    private LedpResponseErrorHandler errorHandler = new LedpResponseErrorHandler();
 
     @BeforeClass(groups = "cleanup")
     public void setup() {
         camille = CamilleEnvironment.getCamille();
         podId = CamilleEnvironment.getPodId();
+        if (adminApiHostPort.endsWith("/")) {
+            adminApiHostPort = adminApiHostPort.substring(0, adminApiHostPort.lastIndexOf("/"));
+        }
+        MagicAuthenticationHeaderHttpRequestInterceptor addMagicAuthHeader = new MagicAuthenticationHeaderHttpRequestInterceptor(
+                Constants.INTERNAL_SERVICE_HEADERVALUE);
+        magicRestTemplate.setInterceptors(Arrays.asList(new ClientHttpRequestInterceptor[] { addMagicAuthHeader }));
+        magicRestTemplate.setErrorHandler(errorHandler);
     }
 
     @Test(groups = "cleanup")
@@ -59,6 +92,7 @@ public class GlobalAuthCleanupTestNG extends AbstractTestNGSpringContextTests {
                 cleanupTenantInGA(tenant);
                 cleanupTenantInZK(tenant);
                 cleanupTenantInHdfs(tenant);
+                cleanupTenantsInDL(tenant);
             }
         }
         log.info("Finished cleaning up test tenants.");
@@ -96,6 +130,56 @@ public class GlobalAuthCleanupTestNG extends AbstractTestNGSpringContextTests {
         if (HdfsUtils.fileExists(yarnConfiguration, contractPath)) {
             HdfsUtils.rmdir(yarnConfiguration, contractPath);
         }
+    }
+
+    private void cleanupTenantsInDL(Tenant tenant) {
+        log.info("Clean up test tenant " + tenant.getId() + " from DL.");
+        CustomerSpace customerSpace =CustomerSpace.parse(tenant.getId());
+        String tenantName = customerSpace.getTenantId();
+
+        try {
+            String permStoreUrl = adminApiHostPort + "/admin/internal/BODCDEVVINT207/BODCDEVVINT187/" + tenantName;
+            magicRestTemplate.delete(permStoreUrl);
+            log.info("Cleanup VDB permstore for tenant " + tenantName);
+        } catch (Exception e) {
+            log.error("Failed to clean up permstore for vdb " + tenantName + " : "
+                    + errorHandler.getStatusCode() + ", " + errorHandler.getResponseString());
+        }
+
+        try {
+            List<BasicNameValuePair> adHeaders = loginAd();
+            String adminUrl = adminApiHostPort + "/admin/tenants/" + customerSpace.getTenantId() + "?contractId="
+                    + customerSpace.getContractId();
+            String response = HttpClientWithOptionalRetryUtils.sendGetRequest(adminUrl, false, adHeaders);
+            TenantDocument tenantDoc = JsonUtils.deserialize(response, TenantDocument.class);
+            String dlUrl = tenantDoc.getSpaceConfig().getDlAddress();
+            DeleteVisiDBDLRequest request = new DeleteVisiDBDLRequest(tenantName, "3");
+            InstallResult result = dataLoaderService.deleteDLTenant(request, dlUrl, true);
+            log.info("Delete DL tenant " + tenantName + " result=" + JsonUtils.serialize(result));
+        } catch (Exception e) {
+            log.error("Failed to clean up dl tenant " + tenantName + " : "
+                    + errorHandler.getStatusCode() + ", " + errorHandler.getResponseString());
+        }
+    }
+
+
+    private List<BasicNameValuePair> loginAd() throws IOException {
+        List<BasicNameValuePair> headers = new ArrayList<>();
+        headers.add(new BasicNameValuePair("Content-Type", "application/json"));
+        headers.add(new BasicNameValuePair("Accept", "application/json"));
+
+        Credentials credentials = new Credentials();
+        credentials.setUsername(TestFrameworkUtils.AD_USERNAME);
+        credentials.setPassword(TestFrameworkUtils.AD_PASSWORD);
+        String response = HttpClientWithOptionalRetryUtils.sendPostRequest(adminApiHostPort + "/admin/adlogin", false,
+                headers, JsonUtils.serialize(credentials));
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode json = mapper.readTree(response);
+        String token = json.get("Token").asText();
+
+        headers.add(new BasicNameValuePair("Authorization", token));
+        return headers;
     }
 
 }
