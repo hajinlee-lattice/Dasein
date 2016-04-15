2e1a3e3c8c4169354d2f7cc8e83055fbdf9c575e
bnguyen <bnguyen@479f5059-62a3-4c39-9342-9852e3a68435>
2016-04-15 16:53:52 +0000

    Review Request 3658 Enable CORS.

---
+++ le-scoringapi/src/main/java/com/latticeengines/scoringapi/web/CorsFilter.java
@@ -0,0 +1,34 @@
+package com.latticeengines.scoringapi.web;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class CorsFilter extends OncePerRequestFilter {
+    private static final Log log = LogFactory.getLog(CorsFilter.class);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        // Enables cross-origin-resource-sharing
+        response.addHeader("Access-Control-Allow-Origin", "*");
+
+        if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
+            // CORS "pre-flight" request
+            log.info("Enabling CORS in response header for pre-flight request.");
+            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+            response.addHeader("Access-Control-Allow-Headers", "Content-Type");
+            response.addHeader("Access-Control-Max-Age", "300"); // 5 min
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+}
