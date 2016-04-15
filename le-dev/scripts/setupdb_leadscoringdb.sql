61f54902c3058d9f51bfa8bbd57e03e8dc8a1480
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-04-15 16:39:56 +0000

    Review Request 3660 fix leadscoringdb script

--- le-dev/scripts/setupdb_leadscoringdb.sql
+++ le-dev/scripts/setupdb_leadscoringdb.sql
@@ -7,5 +7,4 @@ GRANT ALL ON LeadScoringDB.* TO root;
 USE `LeadScoringDB`;
 
 source WSHOME/le-dataplatform/ddl_leadscoringdb_mysql.sql
-source WSHOME/le-dev/testartifacts/sql/ddl_data_leadscoringdb_mysql5innodb.sql
 
