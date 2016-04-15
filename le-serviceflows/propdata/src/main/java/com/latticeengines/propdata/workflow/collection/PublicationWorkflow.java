0693f046c69c23da0af8abad9d718c9caca7aca4
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-04-15 18:46:46 +0000

    Review Request 3662 fill in PublicationWorkflow step

--- le-serviceflows/propdata/src/main/java/com/latticeengines/propdata/workflow/collection/PublicationWorkflow.java
+++ le-serviceflows/propdata/src/main/java/com/latticeengines/propdata/workflow/collection/PublicationWorkflow.java
@@ -1,7 +1,31 @@
 package com.latticeengines.propdata.workflow.collection;
 
+import org.springframework.batch.core.Job;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import com.latticeengines.propdata.workflow.collection.steps.Publish;
+import com.latticeengines.workflow.exposed.build.AbstractWorkflow;
+import com.latticeengines.workflow.exposed.build.Workflow;
+import com.latticeengines.workflow.exposed.build.WorkflowBuilder;
+
 @Component("publicationWorkflow")
-public class PublicationWorkflow {
+public class PublicationWorkflow  extends AbstractWorkflow<PublicationWorkflowConfiguration>  {
+
+    @Autowired
+    private Publish publish;
+
+    @Bean
+    public Job publicationWorkflow() throws Exception {
+        return publicationWorkflow();
+    }
+
+    @Override
+    public Workflow defineWorkflow() {
+        return new WorkflowBuilder() //
+                .next(publish) //
+                .build();
+    }
+
 }
