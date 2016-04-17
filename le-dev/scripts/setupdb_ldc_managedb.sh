02056634fe5001977485611ee6c568719c84c760
ygao <ygao@479f5059-62a3-4c39-9342-9852e3a68435>
2016-04-17 19:15:24 +0000

    review request 3685  modify db setup shell commands to meet with Mac OS

--- le-dev/scripts/setupdb_ldc_managedb.sh
+++ le-dev/scripts/setupdb_ldc_managedb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Remove alter table drop foreign key statements from the script
-sed -i 's/alter table .* drop foreign key .*;//g' $WSHOME/ddl_ldc_managedb_mysql5innodb.sql
+sed -i '' 's/alter table .* drop foreign key .*;//g' $WSHOME/ddl_ldc_managedb_mysql5innodb.sql
 
 sed "s|WSHOME|$WSHOME|g" $WSHOME/le-dev/scripts/setupdb_ldc_managedb.sql | mysql -u root -pwelcome
--- le-dev/scripts/setupdb_scoringdb.sh
+++ le-dev/scripts/setupdb_scoringdb.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Remove alter table drop foreign key statements from the script
-sed -i 's/alter table .* drop foreign key .*;//g' $WSHOME/ddl_scoringdb_mysql5innodb.sql
-sed -i 's/varchar(65535)/varchar(4000)/g' $WSHOME/ddl_scoringdb_mysql5innodb.sql
+sed -i '' 's/alter table .* drop foreign key .*;//g' $WSHOME/ddl_scoringdb_mysql5innodb.sql
+sed -i '' 's/varchar(65535)/varchar(4000)/g' $WSHOME/ddl_scoringdb_mysql5innodb.sql
 
 sed "s|WSHOME|$WSHOME|g" $WSHOME/le-dev/scripts/setupdb_scoringdb.sql | mysql -u root -pwelcome
