449ba709862fadcc6c232539203ca300da295dc5
ygao <ygao@479f5059-62a3-4c39-9342-9852e3a68435>
2016-04-18 21:36:39 +0000

    review request 3685  revert the change since it will break Linux OS

--- le-dev/scripts/setupdb_leadscoringdb.sh
+++ le-dev/scripts/setupdb_leadscoringdb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Remove alter table drop foreign key statements from the script
-sed -i '' 's/alter table .* drop foreign key .*;//g' $WSHOME/ddl_leadscoringdb_mysql.sql
+sed -i 's/alter table .* drop foreign key .*;//g' $WSHOME/ddl_leadscoringdb_mysql.sql
 
 sed "s|WSHOME|$WSHOME|g" $WSHOME/le-dev/scripts/setupdb_leadscoringdb.sql | mysql -u root -pwelcome
 sed "s|WSHOME|$WSHOME|g" $WSHOME/le-dev/testartifacts/sql/ddl_data_leadscoringdb_mysql5innodb.sql | mysql -u root -pwelcome
