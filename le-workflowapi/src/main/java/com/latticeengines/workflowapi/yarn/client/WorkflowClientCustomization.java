19e92925b040d3df189fae4f090e8420166034a3
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-04-19 06:12:48 +0000

    Review Request 3708 attempt to fix not find map by adding le-dataflow to le-workflowapi

--- le-workflowapi/src/main/java/com/latticeengines/workflowapi/yarn/client/WorkflowClientCustomization.java
+++ le-workflowapi/src/main/java/com/latticeengines/workflowapi/yarn/client/WorkflowClientCustomization.java
@@ -56,10 +56,6 @@ public class WorkflowClientCustomization extends SingleContainerClientCustomizat
         // there are propdata workflow steps need cascading, le-dataflow is the place to hold cascading dependency
         hdfsEntries.add(new LocalResourcesFactoryBean.TransferEntry(LocalResourceType.FILE, //
                 LocalResourceVisibility.PUBLIC, //
-                String.format("/app/%s/dataflow/lib/*", versionManager.getCurrentVersion()), //
-                false));
-        hdfsEntries.add(new LocalResourcesFactoryBean.TransferEntry(LocalResourceType.FILE, //
-                LocalResourceVisibility.PUBLIC, //
                 String.format("/app/%s/dataflow/dataflow.properties", versionManager.getCurrentVersion()),//
                 false));
 
