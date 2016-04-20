438d7c63a590c4b55990b01f8d720f3d186da8ce
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-04-20 17:55:58 +0000

    Review Request 3681 fix propdata test dataflow context

--- le-serviceflows/propdata/src/test/java/com/latticeengines/propdata/dataflow/testframework/PropdataServiceFlowsDataflowFunctionalTestNGBase.java
+++ le-serviceflows/propdata/src/test/java/com/latticeengines/propdata/dataflow/testframework/PropdataServiceFlowsDataflowFunctionalTestNGBase.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ContextConfiguration;
 
 import com.latticeengines.serviceflows.functionalframework.ServiceFlowsDataFlowFunctionalTestNGBase;
 
-@ContextConfiguration(locations = { "classpath:serviceflows-core-context.xml", "classpath:serviceflows-propdata-dataflow-context.xml" })
+@ContextConfiguration(locations = { "classpath:test-serviceflows-propdata-dataflow-context.xml" })
 public abstract class PropdataServiceFlowsDataflowFunctionalTestNGBase extends ServiceFlowsDataFlowFunctionalTestNGBase {
 
     protected static final String SHARED_AVRO_INPUT = "dataflow";
