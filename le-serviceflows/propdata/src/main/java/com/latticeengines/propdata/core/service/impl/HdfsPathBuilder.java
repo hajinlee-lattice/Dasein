0c4cc92bb13f1e13b412a0c0fde7f432c6e0c4a5
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-04-22 07:33:14 +0000

    Review Request 3769 fix publicatication workflow

--- le-serviceflows/propdata/src/main/java/com/latticeengines/propdata/core/service/impl/HdfsPathBuilder.java
+++ le-serviceflows/propdata/src/main/java/com/latticeengines/propdata/core/service/impl/HdfsPathBuilder.java
@@ -17,6 +17,8 @@ import com.latticeengines.propdata.core.source.Source;
 @Component("hdfsPathBuilder")
 public class HdfsPathBuilder {
 
+    public static final String SUCCESS_FILE = "_SUCCESS";
+
     private static final String MATCH_PREFIX = "match_";
     private static final String DATE_FORMAT_STRING = "yyyy-MM-dd_HH-mm-ss_z";
     private static final String UTC = "UTC";
--- le-serviceflows/propdata/src/main/java/com/latticeengines/propdata/engine/publication/service/impl/PublicationNewProgressValidatorImpl.java
+++ le-serviceflows/propdata/src/main/java/com/latticeengines/propdata/engine/publication/service/impl/PublicationNewProgressValidatorImpl.java
@@ -4,11 +4,19 @@ import java.text.ParseException;
 import java.util.Date;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.latticeengines.common.exposed.util.HdfsUtils;
 import com.latticeengines.domain.exposed.propdata.manage.Publication;
 import com.latticeengines.domain.exposed.propdata.manage.PublicationProgress;
+import com.latticeengines.propdata.core.service.SourceService;
+import com.latticeengines.propdata.core.service.impl.HdfsPathBuilder;
+import com.latticeengines.propdata.core.source.DerivedSource;
+import com.latticeengines.propdata.core.source.Source;
 import com.latticeengines.propdata.core.util.CronUtils;
 import com.latticeengines.propdata.engine.publication.entitymgr.PublicationProgressEntityMgr;
 import com.latticeengines.propdata.engine.publication.service.PublicationNewProgressValidator;
@@ -16,8 +24,19 @@ import com.latticeengines.propdata.engine.publication.service.PublicationNewProg
 @Component("publicationNewProgressValidator")
 public class PublicationNewProgressValidatorImpl implements PublicationNewProgressValidator {
 
+    private static final Log log = LogFactory.getLog(PublicationNewProgressValidatorImpl.class);
+
     @Autowired
     private PublicationProgressEntityMgr progressEntityMgr;
+
+    @Autowired
+    private HdfsPathBuilder hdfsPathBuilder;
+
+    @Autowired
+    private SourceService sourceService;
+
+    @Autowired
+    private Configuration yarnConfiguration;
 
     @Override
     public Boolean isValidToStartNewProgress(Publication publication, String currentVersion) {
@@ -25,12 +44,26 @@ public class PublicationNewProgressValidatorImpl implements PublicationNewProgre
             return false;
         }
 
-        PublicationProgress existingProgress = progressEntityMgr.findBySourceVersionUnderMaximumRetry(publication,
-                currentVersion);
-        if (existingProgress != null) {
+        if (hasExistingProgressAtVersion(publication, currentVersion)) {
             return false;
         }
 
+        if (thisCronAlreadyTriggered(publication)) {
+            return false;
+        }
+
+        if (noSourceAvro(publication.getSourceName(), currentVersion)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private Boolean hasExistingProgressAtVersion(Publication publication, String currentVersion) {
+        return progressEntityMgr.findBySourceVersionUnderMaximumRetry(publication, currentVersion) != null;
+    }
+
+    private Boolean thisCronAlreadyTriggered(Publication publication) {
         if (StringUtils.isNotEmpty(publication.getCronExpression())) {
             PublicationProgress lastRun = progressEntityMgr.findLatestUnderMaximumRetry(publication);
             Date lastTriggerTime = lastRun.getCreateTime();
@@ -42,11 +75,36 @@ public class PublicationNewProgressValidatorImpl implements PublicationNewProgre
             }
             if (!lastTriggerTime.before(lastScheduledTime)) {
                 // alread triggered
-                return false;
+                return true;
             }
         }
+        return false;
+    }
 
-        return true;
+    private Boolean noSourceAvro(String sourceName, String version) {
+        Source source = sourceService.findBySourceName(sourceName);
+        if (source instanceof DerivedSource) {
+            String avroDir = hdfsPathBuilder.constructSnapshotDir(source, version).toString();
+            try {
+                if (!HdfsUtils.fileExists(yarnConfiguration, avroDir)) {
+                    log.warn("The avro dir " + avroDir + " does not exists.");
+                    return true;
+                }
+                if (!HdfsUtils.fileExists(yarnConfiguration, avroDir + "/" + HdfsPathBuilder.SUCCESS_FILE)) {
+                    log.warn("Cannot find the _SUCCESS file in the avro dir " + avroDir);
+                    return true;
+                }
+                if (HdfsUtils.getFilesByGlob(yarnConfiguration, avroDir + "/*.avro").isEmpty()) {
+                    log.warn("Cannot find any avro file in the avro dir " + avroDir);
+                    return true;
+                }
+            } catch (Exception e) {
+                log.warn("Failed to verify source avro for sourcename=" + sourceName + ", version=" + version);
+            }
+        } else {
+            throw new UnsupportedOperationException("Only derived source can be published.");
+        }
+        return false;
     }
 
 }
