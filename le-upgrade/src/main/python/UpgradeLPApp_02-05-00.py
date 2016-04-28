e59d264b9849224af01f8bc1ac5ca4dd7effd83a
ytian <ytian@479f5059-62a3-4c39-9342-9852e3a68435>
2016-04-28 09:33:46 +0000

    Review Request 3896 Upgrade script for LP2.5.0 this scripts package including the following files: LP_ELQ_NewSpecs_2.5.0_from_2.4.3.maude LP_MKTO_NewSpecs_2.5.0_from_2.4.3.maude LP_SFDC_NewSpecs_2.5.0_from_2.4.3.maude LP_020500_RePDMatch_ReScore.py LP_020500_VDB_NewSpecs.py init.py UpgradeLPApp_02-05-00.py

--- le-upgrade/src/main/python/UpgradeLPApp_02-05-00.py
+++ le-upgrade/src/main/python/UpgradeLPApp_02-05-00.py
@@ -17,6 +17,6 @@ sequence = []
 sequence.append(appsequence.LPCheckVersion('2.4.3'))
 #sequence.append(lp020500.LP_020500_VDB_NewSpecs())
 sequence.append(lp020500.LP_020500_RePDMatch_ReScore())
-sequence.append(appsequence.LPSetVersion('2.4.3'))
+sequence.append(appsequence.LPSetVersion('2.5.0'))
 app = appsequence.AppSequence(tenantFileName, resultsFileName, sequence, checkOnly)
 app.execute()
