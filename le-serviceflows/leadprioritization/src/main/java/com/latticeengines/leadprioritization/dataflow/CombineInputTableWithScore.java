41d8e7053ae7007b7f010c16e4f1904512bf0522
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2016-04-29 20:34:56 +0000

    Review Request 3912: PLS-1168 scored training/testing files -- change	"percentile" column header to "score"

--- le-serviceflows/leadprioritization/src/main/java/com/latticeengines/leadprioritization/dataflow/CombineInputTableWithScore.java
+++ le-serviceflows/leadprioritization/src/main/java/com/latticeengines/leadprioritization/dataflow/CombineInputTableWithScore.java
@@ -23,6 +23,8 @@ public class CombineInputTableWithScore extends TypesafeDataFlowBuilder<CombineI
         Node combinedResultTable = inputTable.leftOuterJoin(InterfaceName.Id.name(), scoreTable,
                 InterfaceName.Id.name());
 
+        combinedResultTable = combinedResultTable.rename(new FieldList(ScoreResultField.Percentile.name()),
+                new FieldList("Score"));
         List<String> fieldsToDiscard = new ArrayList<>();
         if (!parameters.isDebuggingEnabled()) {
             fieldsToDiscard.add(ScoreResultField.RawScore.name());
