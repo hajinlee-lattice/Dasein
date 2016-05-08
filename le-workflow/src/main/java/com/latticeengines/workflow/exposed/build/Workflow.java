959d81ce1cb80294ea102f85689d6942bdbca0c7
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-05-08 04:37:27 +0000

    Review Request 2571: Fix warnings

--- le-workflow/src/main/java/com/latticeengines/workflow/exposed/build/Workflow.java
+++ le-workflow/src/main/java/com/latticeengines/workflow/exposed/build/Workflow.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.latticeengines.workflow.listener.LEJobListener;
-import org.springframework.batch.core.JobExecutionListener;
 
 public class Workflow {
 
