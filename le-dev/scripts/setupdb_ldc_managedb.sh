60d312e27b7261789146186912a3041f2bc2e664
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-05-11 21:45:24 +0000

    Review Request 4012 try to make script work on mac

--- le-dev/scripts/setupdb_ldc_managedb.sh
+++ le-dev/scripts/setupdb_ldc_managedb.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
 
-# Remove alter table drop foreign key statements from the script
-sed -i 's/alter table .* drop foreign key .*;//g' $WSHOME/le-propdata/ddl_ldc_managedb_mysql5innodb.sql
-
 function version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
-mysql_version=$(echo `mysqld --version` | sed 's/[[:alpha:]|(|[:space:]]//g' | cut -d \- -f 1) || 5.5
 
+UNAME=`uname`
 threshold_version=5.7
-if version_gt ${mysql_version} ${threshold_version}; then
-    echo "MySQL version $mysql_version is greater than $threshold_version, replacing DATA by DATA LOCAL"
-    sed "s|WSHOME|$WSHOME|g" $WSHOME/le-dev/scripts/setupdb_ldc_managedb.sql | sed "s|DATA|DATA LOCAL|g" | mysql -u root -pwelcome
+
+if [[ "${UNAME}" == 'Darwin' ]]; then
+    echo 'You are on Mac'
+
+    sed -i '' 's/alter table .* drop foreign key .*;//g' $WSHOME/le-propdata/ddl_ldc_managedb_mysql5innodb.sql
+    mysql_version=$(echo `mysqld --version` | sed 's/[[:alpha:]|(|[:space:]]//g' | cut -d \- -f 1 | cut -d \) -f 1) || 5.5
+
+    if version_gt ${mysql_version} ${threshold_version}; then
+        echo "MySQL version $mysql_version is greater than $threshold_version, replacing DATA by DATA LOCAL"
+        sed '' "s|WSHOME|$WSHOME|g" $WSHOME/le-dev/scripts/setupdb_ldc_managedb.sql | sed "s|DATA|DATA LOCAL|g" | mysql -u root -pwelcome
+    else
+        echo "MySQL version $mysql_version"
+        sed '' "s|WSHOME|$WSHOME|g" $WSHOME/le-dev/scripts/setupdb_ldc_managedb.sql | mysql -u root -pwelcome
+    fi
+
 else
-    echo "MySQL version $mysql_version"
-    sed "s|WSHOME|$WSHOME|g" $WSHOME/le-dev/scripts/setupdb_ldc_managedb.sql | mysql -u root -pwelcome
+
+    echo 'You are on ${UNAME}'
+
+    # Remove alter table drop foreign key statements from the script
+    sed -i 's/alter table .* drop foreign key .*;//g' $WSHOME/le-propdata/ddl_ldc_managedb_mysql5innodb.sql
+    mysql_version=$(echo `mysqld --version` | sed 's/[[:alpha:]|(|[:space:]]//g' | cut -d \- -f 1) || 5.5
+
+    if version_gt ${mysql_version} ${threshold_version}; then
+        echo "MySQL version $mysql_version is greater than $threshold_version, replacing DATA by DATA LOCAL"
+        sed "s|WSHOME|$WSHOME|g" $WSHOME/le-dev/scripts/setupdb_ldc_managedb.sql | sed "s|DATA|DATA LOCAL|g" | mysql -u root -pwelcome
+    else
+        echo "MySQL version $mysql_version"
+        sed "s|WSHOME|$WSHOME|g" $WSHOME/le-dev/scripts/setupdb_ldc_managedb.sql | mysql -u root -pwelcome
+    fi
+
 fi
+
+
+
 
 
