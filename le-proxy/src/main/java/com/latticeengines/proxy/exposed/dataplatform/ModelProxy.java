77910c3a1f9fc9f4ac804e35bece4c9187de9a9b
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-05-13 16:50:01 +0000

    Review Request 4049: Add more methods to ModelProxy

--- le-proxy/src/main/java/com/latticeengines/proxy/exposed/dataplatform/ModelProxy.java
+++ le-proxy/src/main/java/com/latticeengines/proxy/exposed/dataplatform/ModelProxy.java
@@ -3,9 +3,13 @@ package com.latticeengines.proxy.exposed.dataplatform;
 import org.springframework.stereotype.Component;
 
 import com.latticeengines.domain.exposed.api.AppSubmission;
+import com.latticeengines.domain.exposed.api.StringList;
 import com.latticeengines.domain.exposed.dataplatform.JobStatus;
+import com.latticeengines.domain.exposed.modeling.DataProfileConfiguration;
 import com.latticeengines.domain.exposed.modeling.ExportConfiguration;
 import com.latticeengines.domain.exposed.modeling.LoadConfiguration;
+import com.latticeengines.domain.exposed.modeling.Model;
+import com.latticeengines.domain.exposed.modeling.SamplingConfiguration;
 import com.latticeengines.network.exposed.dataplatform.ModelInterface;
 import com.latticeengines.proxy.exposed.BaseRestApiProxy;
 
@@ -32,5 +36,29 @@ public class ModelProxy extends BaseRestApiProxy implements ModelInterface {
     public AppSubmission exportData(ExportConfiguration config) {
         String url = constructUrl("/dataexports");
         return post("exportData", url, config, AppSubmission.class);
+    }
+
+    @Override
+    public StringList getFeatures(Model model) {
+        String url = constructUrl("/features");
+        return post("getFeatures", url, model, StringList.class);
+    }
+
+    @Override
+    public AppSubmission submit(Model model) {
+        String url = constructUrl("/models");
+        return post("submit", url, model, AppSubmission.class);
+    }
+
+    @Override
+    public AppSubmission profile(DataProfileConfiguration config) {
+        String url = constructUrl("/profiles");
+        return post("profile", url, config, AppSubmission.class);
+    }
+
+    @Override
+    public AppSubmission createSamples(SamplingConfiguration config) {
+        String url = constructUrl("/samples");
+        return post("createSamples", url, config, AppSubmission.class);
     }
 }
