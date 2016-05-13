61c073c34c53756c39f1bb2edb6df592bf9465bd
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-05-13 16:50:14 +0000

    Review Request 4049: Add more methods to ModelProxy

--- le-network/src/main/java/com/latticeengines/network/exposed/dataplatform/ModelInterface.java
+++ le-network/src/main/java/com/latticeengines/network/exposed/dataplatform/ModelInterface.java
@@ -1,9 +1,13 @@
 package com.latticeengines.network.exposed.dataplatform;
 
 import com.latticeengines.domain.exposed.api.AppSubmission;
+import com.latticeengines.domain.exposed.api.StringList;
 import com.latticeengines.domain.exposed.dataplatform.JobStatus;
+import com.latticeengines.domain.exposed.modeling.DataProfileConfiguration;
 import com.latticeengines.domain.exposed.modeling.ExportConfiguration;
 import com.latticeengines.domain.exposed.modeling.LoadConfiguration;
+import com.latticeengines.domain.exposed.modeling.Model;
+import com.latticeengines.domain.exposed.modeling.SamplingConfiguration;
 
 public interface ModelInterface {
 
@@ -12,4 +16,12 @@ public interface ModelInterface {
     AppSubmission loadData(LoadConfiguration config);
 
     AppSubmission exportData(ExportConfiguration config);
+    
+    StringList getFeatures(Model model);
+    
+    AppSubmission submit(Model model);
+    
+    AppSubmission profile(DataProfileConfiguration config);
+    
+    AppSubmission createSamples(SamplingConfiguration config);
 }
