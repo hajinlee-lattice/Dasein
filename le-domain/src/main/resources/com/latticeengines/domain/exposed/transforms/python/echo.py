523f0819707823f8cefa5f2274c6d75d5603b85f
bnguyen <bnguyen@479f5059-62a3-4c39-9342-9852e3a68435>
2016-05-17 23:04:25 +0000

    Review Request 4083 Remove deprecated unused transforms

--- le-domain/src/main/resources/com/latticeengines/domain/exposed/transforms/python/echo.py
+++
@@ -1,2 +0,0 @@
-def transform(args, record):
-    return args["value"]
--- le-domain/src/main/resources/com/latticeengines/domain/exposed/transforms/python/make_float.py
+++
@@ -1,9 +0,0 @@
-# Matches the behavior of ColumnTypeConversionStep.
-def transform(args, record):
-    column = args["column"]
-    value = record[column]
-
-    if value is None:
-        return None
-        
-    return float(value)
