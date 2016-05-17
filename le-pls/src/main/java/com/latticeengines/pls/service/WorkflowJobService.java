a4a485ab822d4d971018f83b97eddc92e026db82
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-05-17 17:58:32 +0000

    Review Request 2571: Fix warnings

--- le-pls/src/main/java/com/latticeengines/pls/service/WorkflowJobService.java
+++ le-pls/src/main/java/com/latticeengines/pls/service/WorkflowJobService.java
@@ -1,9 +1,9 @@
 package com.latticeengines.pls.service;
 
-import com.latticeengines.domain.exposed.workflow.JobStatus;
 import org.apache.hadoop.yarn.api.records.ApplicationId;
+
+import com.latticeengines.domain.exposed.workflow.JobStatus;
 import com.latticeengines.domain.exposed.workflow.WorkflowConfiguration;
-import com.latticeengines.domain.exposed.workflow.WorkflowStatus;
 
 public interface WorkflowJobService {
 
