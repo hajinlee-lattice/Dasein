f915f5ef8a89051b7457c6bfb23e3fb87101424e
mwilson <mwilson@479f5059-62a3-4c39-9342-9852e3a68435>
2016-05-23 20:15:55 +0000

    Review Request 4181: Fix logic for determining applicability

--- le-upgrade/src/main/python/MigrateToLP3ModelingApp.py
+++ le-upgrade/src/main/python/MigrateToLP3ModelingApp.py
@@ -23,15 +23,17 @@ sequence.append(lpmigration.LPMigration_ModelingUpdates())
 
 ## Modeling query for LP3
 sequence.append(lpmigration.LPMigration_1MoActivityForModeling())
+modelingQueryToBeCreated=True
 sequence.append(lpmigration.LPMigration_LP3ModelingQuery())
-sequence.append(lpmigration.LPMigration_LP3ModelingQuery1MoActivity())
-sequence.append(appsequence.WriteQueryToCSV('Q_LP3_ModelingLead_OneLeadPerDomain', timestamp))
+sequence.append(lpmigration.LPMigration_LP3ModelingQuery1MoActivity(forceApply=modelingQueryToBeCreated))
+sequence.append(appsequence.WriteQueryToCSV('Q_LP3_ModelingLead_OneLeadPerDomain', timestamp, forceApply=modelingQueryToBeCreated))
 
 ## Scoring queries for validating LP3 score distributions
 sequence.append(lpmigration.LPMigration_1MoActivityForScoring())
+scoringQueryToBeCreated=True
 sequence.append(lpmigration.LPMigration_LP3ScoringRecentAllRowsQuery())
-sequence.append(lpmigration.LPMigration_LP3ScoringQuery1MoActivity())
-sequence.append(appsequence.WriteQueryToCSV('Q_LP3_ScoringLead_RecentAllRows', timestamp))
+sequence.append(lpmigration.LPMigration_LP3ScoringQuery1MoActivity(forceApply=scoringQueryToBeCreated))
+sequence.append(appsequence.WriteQueryToCSV('Q_LP3_ScoringLead_RecentAllRows', timestamp, forceApply=scoringQueryToBeCreated))
 
 app = appsequence.AppSequence(tenantFileName, resultsFileName, sequence, checkOnly)
 app.execute()
