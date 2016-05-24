868ca4e18d83bcf5ef2762af1613def0b67dd7dc
JHadden <JHadden@479f5059-62a3-4c39-9342-9852e3a68435>
2016-05-24 18:30:55 +0000

    Review Request 1120: Adding in growing textarea

--- le-ui/projects/leadprioritization/app/create/controllers/CSVImportController.js
+++ le-ui/projects/leadprioritization/app/create/controllers/CSVImportController.js
@@ -570,5 +570,16 @@ angular.module('mainApp.create.csvImport', [
                 xhr.abort();
             }
         };
+
+        function expandTextarea(id) {
+            document.getElementById(id).addEventListener('keyup', function() {
+                this.style.overflow = 'hidden';
+                this.style.height = 0;
+                this.style.height = this.scrollHeight + 'px';
+            }, false);
+        }
+
+        expandTextarea('modelDescription');
+
     }
 ]);
