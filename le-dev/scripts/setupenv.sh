cc6c2dca6223641fe2da4393713e19a57def0166
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-05-25 22:46:05 +0000

    Review Request 4217 remove tez tarball from svn repo

--- le-dev/scripts/setupenv.sh
+++ le-dev/scripts/setupenv.sh
@@ -8,8 +8,16 @@ cp $WSHOME/le-dev/hadoop/dev/mapred-site.xml $HADOOP_CONF_DIR
 cp $WSHOME/le-dev/hadoop/dev/tez-site.xml $HADOOP_CONF_DIR
 cp $WSHOME/le-dev/hadoop/dev/yarn-site.xml $HADOOP_CONF_DIR
 
+
+export TEZ_TARBALL=$WSHOME/le-dev/hadoop/artifacts/tez-0.8.2.tar.gz
 hadoop fs -mkdir -p /apps/tez || true
-hadoop fs -copyFromLocal $WSHOME/le-dev/hadoop/artifacts/tez-0.8.2.tar.gz /apps/tez || true
+if [ -f "${TEZ_TARBALL}" ]; then
+    echo "tez tarball has already been downloaded"
+else
+    echo "downloading tez tarball from sftp"
+    scp -i $WSHOME/le-dev/sftpdevkey sftpdev@10.41.1.31:/artifactory/tez-0.8.2.tar.gz $TEZ_TARBALL
+fi
+hadoop fs -copyFromLocal $TEZ_TARBALL /apps/tez || true
 
 sudo pip install -r $WSHOME/le-dev/scripts/requirements.txt
 
