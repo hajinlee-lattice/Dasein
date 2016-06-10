c5a9259cebf9a62dfd2f9defa6332da9a9a7f958
JHadden <JHadden@479f5059-62a3-4c39-9342-9852e3a68435>
2016-06-10 19:25:46 +0000

    Review Request 1138: Updated Job status page for Create Model Workflow

--- le-ui/projects/common/assets/js/off-canvas.js
+++ le-ui/projects/common/assets/js/off-canvas.js
@@ -8,7 +8,11 @@ $(function() {
 
 });
 function checkBrowserWidth(){
-    if (window.matchMedia("(max-width: 1366px)").matches) {
+    if (window.matchMedia("(min-width: 800px)").matches) {
+    	console.log("remove");
         $("body").removeClass("open-nav");
+    } else {
+    	console.log("add");
+    	$("body").addClass("open-nav");
     }
 }
