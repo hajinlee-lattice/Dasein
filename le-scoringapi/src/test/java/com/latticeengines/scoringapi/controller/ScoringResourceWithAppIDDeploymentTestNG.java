4fbca9206c2bcb3c87ecd3f06d8aa81a3331cf59
akumar <akumar@479f5059-62a3-4c39-9342-9852e3a68435>
2016-06-19 20:07:04 +0000

    Review Request 4513 - minor bug and test fixes

---
+++ le-scoringapi/src/test/java/com/latticeengines/scoringapi/controller/ScoringResourceWithAppIDDeploymentTestNG.java
@@ -0,0 +1,35 @@
+package com.latticeengines.scoringapi.controller;
+
+import java.io.IOException;
+
+import org.springframework.http.ResponseEntity;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.latticeengines.domain.exposed.scoringapi.ScoreRequest;
+import com.latticeengines.domain.exposed.scoringapi.ScoreResponse;
+
+public class ScoringResourceWithAppIDDeploymentTestNG extends ScoringResourceDeploymentTestNGBase {
+
+	@Test(groups = "deployment", enabled = true)
+	public void scoreRecord() throws IOException {
+		String url = apiHostPort + "/score/record";
+		ScoreRequest scoreRequest = getScoreRequest();
+		scoreRequest.setModelId(MODEL_ID);
+		ResponseEntity<ScoreResponse> response = oAuth2RestTemplate.postForEntity(url, scoreRequest,
+				ScoreResponse.class);
+		ScoreResponse scoreResponse = response.getBody();
+		Assert.assertEquals(scoreResponse.getScore(), EXPECTED_SCORE_99);
+	}
+
+	@Override
+	protected boolean shouldUseAppId() {
+		return true;
+	}
+
+	@Override
+	protected String getAppIdForOauth2() {
+		return "DUMMY_APP";
+	}
+}
+
--- le-scoringapi/src/test/java/com/latticeengines/scoringapi/score/impl/CustomScoreRequestProcessorImpl.java
+++ le-scoringapi/src/test/java/com/latticeengines/scoringapi/score/impl/CustomScoreRequestProcessorImpl.java
@@ -12,20 +12,21 @@ import com.latticeengines.domain.exposed.scoringapi.WarningCode;
 
 @Component("customScoreRequestProcessor")
 public class CustomScoreRequestProcessorImpl extends ScoreRequestProcessorImpl {
-    private HashMap<String, List<Warning>> warningMap = new HashMap<>();
+	private HashMap<String, List<Warning>> warningMap = new HashMap<>();
 
-    @Override
-    List<Warning> getWarnings(String recordId) {
-        return warningMap.get(recordId);
-    }
+	@Override
+	List<Warning> getWarnings(String recordId) {
+		return warningMap.get(recordId);
+	}
 
-    @Override
-    void addWarning(String recordId, List<String> missingFields) {
-        List<Warning> warningList = warningMap.get(recordId);
-        if (warningList == null) {
-            warningList = new ArrayList<>();
-            warningMap.put(recordId, warningList);
-        }
-        warningList.add(new Warning(WarningCode.MISSING_COLUMN, new String[] { Joiner.on(",").join(missingFields) }));
-    }
+	@Override
+	void addWarning(WarningCode code, String recordId, List<String> fields, String modelId) {
+		List<Warning> warningList = warningMap.get(recordId);
+		if (warningList == null) {
+			warningList = new ArrayList<>();
+			warningMap.put(recordId, warningList);
+		}
+		warningList.add(new Warning(code,
+				new String[] { getWarningPrefix(modelId) + Joiner.on(",").join(fields) }));
+	}
 }
