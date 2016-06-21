8074afc94fade1ad0940719f018e47d9c58b535a
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-06-21 00:35:03 +0000

    Review Request 4518 some minor changes to docker scripts

--- le-docker/src/main/resources/kafka/bootstrap.sh
+++ le-docker/src/main/resources/kafka/bootstrap.sh
@@ -57,7 +57,7 @@ echo "provisioning ${KAFKA}-bkr1"
 docker run -d --name ${KAFKA}-bkr1 -h ${KAFKA}-bkr1 \
     --net ${KAFKA_NETWORK} \
     -e ZK_HOSTS=${ZK_HOSTS} \
-    -e DISCOVER_SERVICE=http://${KAFKA}-discover:5000 \
+    -e ADVERTISE_IP=${KAFKA}-bkr1  \
     -l cluster.name=${KAFKA} \
     -p ${BK_PORT}:9092 \
     latticeengines/kafka
@@ -68,7 +68,7 @@ do
     docker run -d --name ${KAFKA}-bkr${i} -h ${KAFKA}-bkr${i} \
         --net ${KAFKA_NETWORK} \
         -e ZK_HOSTS=${ZK_HOSTS} \
-        -e DISCOVER_SERVICE=http://${KAFKA}-discover:5000 \
+        -e ADVERTISE_IP=${KAFKA}-bkr${i}  \
         -l cluster.name=${KAFKA} \
         latticeengines/kafka
 done
@@ -81,7 +81,7 @@ do
     docker run -d --name ${KAFKA}-sr${i} -h ${KAFKA}-sr${i}\
         --net ${KAFKA_NETWORK} \
         -e ZK_HOSTS=${ZK_HOSTS} \
-        -e DISCOVER_SERVICE=http://${KAFKA}-discover:5000 \
+        -e ADVERTISE_IP=${KAFKA}-sr${i}  \
         -l cluster.name=${KAFKA} \
         latticeengines/kafka-schema-registry
 
