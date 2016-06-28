a6abdba98faaa1b278b0e51cfbf42274ed1d0846
jhe <jhe@479f5059-62a3-4c39-9342-9852e3a68435>
2016-06-28 07:46:43 +0000

    Review Request 4634: Don't load whole UserTenantRight table when query.

--- le-security/src/main/java/com/latticeengines/security/dao/impl/GlobalAuthUserTenantRightDaoImpl.java
+++ le-security/src/main/java/com/latticeengines/security/dao/impl/GlobalAuthUserTenantRightDaoImpl.java
@@ -3,6 +3,8 @@ package com.latticeengines.security.dao.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.springframework.stereotype.Component;
@@ -10,38 +12,45 @@ import org.springframework.stereotype.Component;
 import com.latticeengines.db.exposed.dao.impl.BaseDaoImpl;
 import com.latticeengines.domain.exposed.auth.GlobalAuthUserTenantRight;
 import com.latticeengines.security.dao.GlobalAuthUserTenantRightDao;
+import com.latticeengines.security.exposed.globalauth.impl.GlobalUserManagementServiceImpl;
 
 @Component("globalAuthUserTenantRightDao")
 public class GlobalAuthUserTenantRightDaoImpl extends BaseDaoImpl<GlobalAuthUserTenantRight>
         implements
         GlobalAuthUserTenantRightDao {
 
+    private static final Log log = LogFactory.getLog(GlobalAuthUserTenantRightDaoImpl.class);
+
     @SuppressWarnings("rawtypes")
     @Override
     public List<GlobalAuthUserTenantRight> findByUserIdAndTenantId(Long userId, Long tenantId) {
-        Session session = sessionFactory.getCurrentSession();
-        Class<GlobalAuthUserTenantRight> entityClz = getEntityClass();
-        String queryStr = String.format("from %s",
-                entityClz.getSimpleName(),
-                tenantId);
-        Query query = session.createQuery(queryStr);
-        List list = query.list();
-        List<GlobalAuthUserTenantRight> userTenantRightDataList = new ArrayList<GlobalAuthUserTenantRight>();
-        if (list.size() == 0) {
-            return null;
-        } else {
-            for (int i = 0; i < list.size(); i++) {
-                GlobalAuthUserTenantRight gaTenantRight = (GlobalAuthUserTenantRight) list.get(i);
-                if (gaTenantRight.getGlobalAuthUser() != null
-                        && gaTenantRight.getGlobalAuthUser().getPid().equals(userId)) {
-                    if (gaTenantRight.getGlobalAuthTenant() != null
-                            && gaTenantRight.getGlobalAuthTenant().getPid().equals(tenantId)) {
-                        userTenantRightDataList.add(gaTenantRight);
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            Class<GlobalAuthUserTenantRight> entityClz = getEntityClass();
+            String queryStr = String.format("from %s where User_ID = %d and Tenant_ID = %d", entityClz.getSimpleName(),
+                    userId, tenantId);
+            Query query = session.createQuery(queryStr);
+            List list = query.list();
+            List<GlobalAuthUserTenantRight> userTenantRightDataList = new ArrayList<GlobalAuthUserTenantRight>();
+            if (list.size() == 0) {
+                return null;
+            } else {
+                for (int i = 0; i < list.size(); i++) {
+                    GlobalAuthUserTenantRight gaTenantRight = (GlobalAuthUserTenantRight) list.get(i);
+                    if (gaTenantRight.getGlobalAuthUser() != null
+                            && gaTenantRight.getGlobalAuthUser().getPid().equals(userId)) {
+                        if (gaTenantRight.getGlobalAuthTenant() != null
+                                && gaTenantRight.getGlobalAuthTenant().getPid().equals(tenantId)) {
+                            userTenantRightDataList.add(gaTenantRight);
+                        }
                     }
                 }
             }
+            return userTenantRightDataList;
+        } catch (Exception e) {
+            log.error(String.format("Find tenant right by userId: %d, tenantId: %d failed.", userId, tenantId));
+            throw e;
         }
-        return userTenantRightDataList;
     }
 
     @Override
@@ -56,9 +65,9 @@ public class GlobalAuthUserTenantRightDaoImpl extends BaseDaoImpl<GlobalAuthUser
         Session session = sessionFactory.getCurrentSession();
         Class<GlobalAuthUserTenantRight> entityClz = getEntityClass();
         String queryStr = String.format(
-                "from %s where Operation_Name = '%s' and Tenant_ID = %d",
+                "from %s where Operation_Name = '%s' and User_ID = %d and Tenant_ID = %d",
                 entityClz.getSimpleName(),
-                operationName, tenantId);
+                operationName, userId, tenantId);
         Query query = session.createQuery(queryStr);
         List list = query.list();
         if (list.size() == 0) {
