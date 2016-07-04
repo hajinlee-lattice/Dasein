619afd2db6febfd91d50e735016b164a44662ab1
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-07-04 21:00:37 +0000

    Review Request 2571: Fix warnings

--- le-release/src/main/java/com/latticeengines/release/error/handler/DefaultErrorHandler.java
+++ le-release/src/main/java/com/latticeengines/release/error/handler/DefaultErrorHandler.java
@@ -3,15 +3,10 @@ package com.latticeengines.release.error.handler;
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import com.latticeengines.release.exposed.domain.ProcessContext;
 
 @Component("defaultErrorHandler")
 public class DefaultErrorHandler implements ErrorHandler {
-
-    @Autowired
-    private ProcessContext processContext;
 
     private static final Log log = LogFactory.getLog(DefaultErrorHandler.class);
 
