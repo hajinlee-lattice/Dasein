648916670c8e95646fce134643e79f485a0bc46c
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2016-07-05 18:48:15 +0000

    Review Request 4576: fix pls deployment test to clean up model summary

--- le-pls/src/test/java/com/latticeengines/pls/controller/ModelCopyResourceDeploymentTestNG.java
+++ le-pls/src/test/java/com/latticeengines/pls/controller/ModelCopyResourceDeploymentTestNG.java
@@ -88,6 +88,7 @@ public class ModelCopyResourceDeploymentTestNG extends PlsDeploymentTestNGBase {
                         .toString());
         HdfsUtils.rmdir(yarnConfiguration, customerBase + tenant1.getId());
         HdfsUtils.rmdir(yarnConfiguration, customerBase + tenant2.getId());
+        setupSecurityContext(tenant1);
         modelSummaryEntityMgr.deleteByModelId("ms__20a331e9-f18b-4358-8023-e44a36cb17d1-testWork");
     }
 
