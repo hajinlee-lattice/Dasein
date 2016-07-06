446e0c10ebed11244715004f1c4a50709471e8e4
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-07-06 00:33:54 +0000

    Review Request 4715 upload PublicDomain avro and csv

--- le-dev/scripts/setuptest.sh
+++ le-dev/scripts/setuptest.sh
@@ -3,10 +3,13 @@
 # Test for required env variables
 printf "%s\n" "${WSHOME:?You must set WSHOME}"
 
-hadoop fs -mkdir /tmp/Stoplist || true
-hadoop fs -mkdir /tmp/AccountMaster || true
-hadoop fs -copyFromLocal $WSHOME/le-dev/testartifacts/Stoplist/Stoplist.avro /tmp/Stoplist
-hadoop fs -copyFromLocal $WSHOME/le-dev/testartifacts/AccountMaster/AccountMaster.avro /tmp/AccountMaster
+hdfs dfs -mkdir -p /tmp/Stoplist || true
+hdfs dfs -mkdir -p /tmp/AccountMaster || true
+hdfs dfs -mkdir -p /Pods/Default/Services/PropData/MatchService/PublicDomain || true
+hdfs dfs -put -f $WSHOME/le-dev/testartifacts/Stoplist/Stoplist.avro /tmp/Stoplist
+hdfs dfs -put -f $WSHOME/le-dev/testartifacts/PublicDomain/PublicDomain.avro /Pods/Default/Services/PropData/MatchService/PublicDomain
+hdfs dfs -put -f $WSHOME/le-dev/testartifacts/PublicDomain/PublicDomain.csv /Pods/Default/Services/PropData/MatchService/PublicDomain
+hdfs dfs -put -f $WSHOME/le-dev/testartifacts/AccountMaster/AccountMaster.avro /tmp/AccountMaster
 
 # Expand aliases
 echo "Expanding aliases."
