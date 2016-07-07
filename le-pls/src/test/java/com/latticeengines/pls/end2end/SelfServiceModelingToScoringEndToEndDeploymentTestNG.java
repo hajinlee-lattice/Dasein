3028f395aa3caba3ad7ee4d280b3c1e6084f4bf5
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-07-07 18:47:46 +0000

    Review Request 4741: Rename test to be more consistent

--- le-pls/src/test/java/com/latticeengines/pls/end2end/SelfServiceModelingToScoringEndToEndDeploymentTestNG.java
+++ le-pls/src/test/java/com/latticeengines/pls/end2end/SelfServiceModelingToScoringEndToEndDeploymentTestNG.java
@@ -1,20 +1,22 @@
 package com.latticeengines.pls.end2end;
 
 import java.io.IOException;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
 import com.latticeengines.domain.exposed.pls.SchemaInterpretation;
 import com.latticeengines.domain.exposed.security.Tenant;
 import com.latticeengines.pls.functionalframework.PlsDeploymentTestNGBase;
 
 @Component
-public class SelfServeModelingToScoringEndToEndDeploymentTestNG extends PlsDeploymentTestNGBase {
+public class SelfServiceModelingToScoringEndToEndDeploymentTestNG extends PlsDeploymentTestNGBase {
 
-    private static final Log log = LogFactory.getLog(SelfServeModelingToScoringEndToEndDeploymentTestNG.class);
+    private static final Log log = LogFactory.getLog(SelfServiceModelingToScoringEndToEndDeploymentTestNG.class);
     private static final String RESOURCE_BASE = "com/latticeengines/pls/end2end/selfServiceModeling/csvfiles";
 
     @Autowired
@@ -45,30 +47,5 @@ public class SelfServeModelingToScoringEndToEndDeploymentTestNG extends PlsDeplo
         String modelId = selfServiceModeling.prepareModel(SchemaInterpretation.SalesforceAccount, fileName);
         scoreCompareService.analyzeScores(tenant.getId(), RESOURCE_BASE + "/" + fileName, modelId, 100);
     }
-
-//    Function<List<LinkedHashMap<String, String>>, Void> unknownColumnHandler = new Function<List<LinkedHashMap<String, String>>, Void>() {
-//        @Override
-//        public Void apply(List<LinkedHashMap<String, String>> unknownColumns) {
-//            Set<String> booleanSet = Sets.newHashSet(new String[] { "Interest_esb__c", "Interest_tcat__c",
-//                    "kickboxAcceptAll", "Free_Email_Address__c", "kickboxFree", "Unsubscribed", "kickboxDisposable",
-//                    "HasAnypointLogin", "HasCEDownload", "HasEEDownload" });
-//            Set<String> strSet = Sets.newHashSet(new String[] { "Lead_Source_Asset__c", "kickboxStatus", "SICCode",
-//                    "Source_Detail__c", "Cloud_Plan__c" });
-//            log.info(unknownColumns);
-//            for (LinkedHashMap<String, String> map : unknownColumns) {
-//                String columnName = map.get("columnName");
-//                if (booleanSet.contains(columnName)) {
-//                    map.put("columnType", Schema.Type.BOOLEAN.name());
-//                } else if (strSet.contains(columnName)) {
-//                    map.put("columnType", Schema.Type.STRING.name());
-//                } else if (columnName.startsWith("Activity_Count_")) {
-//                    map.put("columnType", Schema.Type.INT.name());
-//                }
-//            }
-//            log.info(unknownColumns);
-//
-//            return null;
-//        }
-//    };
 
 }
