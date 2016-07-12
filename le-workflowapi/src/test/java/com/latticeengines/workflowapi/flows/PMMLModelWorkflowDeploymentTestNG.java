1f6bd0934c44b65f11afa61d7941d0dab616b946
akumar <akumar@479f5059-62a3-4c39-9342-9852e3a68435>
2016-07-12 20:21:54 +0000

    Review Request 4761 - adding 15 sleep in testcase to allow model entry to be created in model summary table

--- le-workflowapi/src/test/java/com/latticeengines/workflowapi/flows/PMMLModelWorkflowDeploymentTestNG.java
+++ le-workflowapi/src/test/java/com/latticeengines/workflowapi/flows/PMMLModelWorkflowDeploymentTestNG.java
@@ -67,6 +67,10 @@ public class PMMLModelWorkflowDeploymentTestNG extends PMMLModelWorkflowTestNGBa
 
     @Test(groups = "deployment", enabled = true, dependsOnMethods = { "testWorkflow" })
     public void scoreRecords() throws IOException, InterruptedException {
+        // wait for 15 seconds to make sure that model entry is created in model
+        // summary
+        Thread.sleep(15 * 1000);
+
         Model model = testPMMLScoring.getMeodel(modelName, PMML_CUSTOMERSPACE, pmmlTenant);
         System.out.println(modelName + ", " + model.getModelId());
         Assert.assertNotNull(model.getModelId());
