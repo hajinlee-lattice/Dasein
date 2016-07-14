aa83396838fb08180aca5dfec66cbf5fb50de0f4
omukhtar <omukhtar@479f5059-62a3-4c39-9342-9852e3a68435>
2016-07-14 16:46:13 +0000

    Review Request 4820 Dataruleutilsds changes, fix bug and add debugging info

--- le-dataplatform/src/main/python/datarules/dataruleutilsds.py
+++ le-dataplatform/src/main/python/datarules/dataruleutilsds.py
@@ -2,80 +2,80 @@ import math
 import csv
 from itertools import groupby
 import pandas as pd
-        
-# given the length and conversion rate of sub-population and the length and conversion rate of overall-population, calculate the significance 
+
+# given the length and conversion rate of sub-population and the length and conversion rate of overall-population, calculate the significance
 def sigCalculation(sub, overall):
     subCnt, subRate = sub
     oaCnt, oaRate = overall
-    r = (subCnt*subRate + oaCnt*oaRate)/(subCnt+oaCnt)
-    return (subRate-oaRate)/(math.sqrt(r*(1.0-r)*(1.0/subCnt + 1.0/oaCnt)))
+    r = (subCnt * subRate + oaCnt * oaRate) / (subCnt + oaCnt)
+    return (subRate - oaRate) / (math.sqrt(r * (1.0 - r) * (1.0 / subCnt + 1.0 / oaCnt)))
 
-# given an array of events, calculate the conversion rate 
+# given an array of events, calculate the conversion rate
 def getRate(eventCol):
-    return sum(eventCol)*1.0/len(eventCol)
- 
-# given the length of data points and desirable number of buckets, return how many data points should be in each bucket 
+    return sum(eventCol) * 1.0 / len(eventCol)
+
+# given the length of data points and desirable number of buckets, return how many data points should be in each bucket
 def bucketing(lenData, numBucket):
-    numPtinBucket = int(lenData/float(numBucket))
+    numPtinBucket = int(lenData / float(numBucket))
     if numPtinBucket == 0:
         return [0, lenData]
-    leftover = lenData - numPtinBucket*numBucket    
-    cutPosition=[(numPtinBucket+1)*i if i <= leftover else numPtinBucket*i+leftover for i in range(numBucket+1)]
+    leftover = lenData - numPtinBucket * numBucket
+    cutPosition = [(numPtinBucket + 1) * i if i <= leftover else numPtinBucket * i + leftover for i in range(numBucket + 1)]
     return cutPosition
 
 
-def discretizeNumericVar(inputCol, numBucket = 20, emptyValList=[None,"Null","Empty",""]):
-        
-    idxNonEmpty = [i for i,x in enumerate(inputCol) if x not in emptyValList and not math.isnan(x)]
-    
+def discretizeNumericVar(inputCol, numBucket=20, emptyValList=[None, "Null", "Empty", ""]):
+
+    idxNonEmpty = [i for i, x in enumerate(inputCol) if x not in emptyValList and not math.isnan(x)]
+
     if len(idxNonEmpty) == 0:
         return [range(len(inputCol))]
-    
-    idxEmpty = [i for i,x in enumerate(inputCol) if x in emptyValList or math.isnan(x)]
-    
-    inputColNonEmpty=[inputCol[i] for i in idxNonEmpty]
-    
-    sortedInd = [y[0] for y in sorted(enumerate(inputColNonEmpty), key = lambda x: x[1])]
-    
-    idxNonEmptySorted = [idxNonEmpty[i] for i in sortedInd]   
+
+    idxEmpty = [i for i, x in enumerate(inputCol) if x in emptyValList or math.isnan(x)]
+
+    inputColNonEmpty = [inputCol[i] for i in idxNonEmpty]
+
+    sortedInd = [y[0] for y in sorted(enumerate(inputColNonEmpty), key=lambda x: x[1])]
+
+    idxNonEmptySorted = [idxNonEmpty[i] for i in sortedInd]
 
     idxCut = bucketing(len(idxNonEmpty), numBucket)
-        
-    return [idxEmpty] + [[idxNonEmptySorted[i] for i in range(idxCut[bucketIdx], idxCut[bucketIdx+1])] for bucketIdx in range(len(idxCut)-1)] 
-        
-    
+
+    return [idxEmpty] + [[idxNonEmptySorted[i] for i in range(idxCut[bucketIdx], idxCut[bucketIdx + 1])] for bucketIdx in range(len(idxCut) - 1)]
+
+
 # given a column of numerical data, discretize the data into categorical column. The null value are all in bucket 0, the non-null values are put in buckets ranging from 1 to numBucket
-def numricVarToCategVar(inputCol, numBucket = 20, emptyValList=[None,"Null","Empty",""]):
+def numricVarToCategVar(inputCol, numBucket=20, emptyValList=[None, "Null", "Empty", ""]):
 
     bucketIdxList = discretizeNumericVar(inputCol, numBucket, emptyValList)
-    
+
     categIdx = []
     idxList = []
     for i in range(len(bucketIdxList)):
         if len(bucketIdxList[i]) > 0:
             idxList = idxList + bucketIdxList[i]
-            categIdx = categIdx + [i]*len(bucketIdxList[i]) 
-            
-    sortedInd = [y[0] for y in sorted(enumerate(idxList), key = lambda x: x[1])]
-    
-    return [categIdx[i] for i in sortedInd]      
+            categIdx = categIdx + [i] * len(bucketIdxList[i])
 
-# get the column of feature value. if it's a numerical column, discretize it into buckets  
+    sortedInd = [y[0] for y in sorted(enumerate(idxList), key=lambda x: x[1])]
+
+    return [categIdx[i] for i in sortedInd]
+
+# get the column of feature value. if it's a numerical column, discretize it into buckets
 def getColVal(colVal, colType, numBucket):
     if colType == 'cat':
         return colVal
     elif colType == 'num':
-        return numricVarToCategVar(colVal, numBucket)    
-        
-# get the conversion rate for each feature value  
-def getGroupedRate(colVal, eventCol, cntRate_overall = None):
+        return numricVarToCategVar(colVal, numBucket)
 
-    dataGrouped = groupby(sorted(zip(colVal, eventCol), key = lambda y: y[0]), lambda z: z[0])
+# get the conversion rate for each feature value
+def getGroupedRate(colVal, eventCol, cntRate_overall=None):
+
+    dataGrouped = groupby(sorted(zip(colVal, eventCol), key=lambda y: y[0]), lambda z: z[0])
 
     dataGrouped = {key : [x for x in group] for key, group in dataGrouped}
-            
+
     cntRateGrouped = {key : (len(group), getRate([x[1] for x in group])) for (key, group) in dataGrouped.items()}
-    
+
     if cntRate_overall is None:
         cntRateGrouped = {key : (x[0], x[1]) for (key, x) in cntRateGrouped.items()}
     else:
@@ -83,31 +83,39 @@ def getGroupedRate(colVal, eventCol, cntRate_overall = None):
     return cntRateGrouped
 
 def strValueFix(x):
-    nullWordsPart = set(['missing','available', 'empty', 'bogus'])        
-    nullWordsFull = set(['nan','null'])
-    y=str(x)
-    if pd.isnull(x) or any(z in y.lower() for z in nullWordsPart) or any(z==y.lower() for z in nullWordsFull) :  return ''
+    nullWordsPart = set(['missing', 'available', 'empty', 'bogus'])
+    nullWordsFull = set(['nan', 'null'])
+    y = str(x)
+    if pd.isnull(x) or any(z in y.lower() for z in nullWordsPart) or any(z == y.lower() for z in nullWordsFull) :  return ''
     return y
 
 def convertCleanDataFrame(colNames, df, catColumnNamesSet, numColumnNamesSet, returnListofCols=True):
-    allCols=[]
+    allCols = []
 
     for c in colNames:
         if c in catColumnNamesSet:
             colType = 'cat'
         if c in numColumnNamesSet:
             colType = 'num'
-        col=df[c].tolist()
+        col = df[c].tolist()
         if colType == 'cat':
-            col=[strValueFix(x) for x in col]
+            col = [strValueFix(x) for x in col]
         if colType == 'num':
-            col=[float(x) for x in col]
+            try:
+                col = [float(str(x)) for x in col]
+		col1 = [float(x) for x in col] # exception will be thrown in SelfServeModeling that will be caught
+            except Exceptin as e:
+		print "Exception while converting to float: %s" % e
+                print c, "columnName that caused an exception"
+                print "column values and types"
+                for x in col:
+                    print col, type(col)
         allCols.append(col)
     if returnListofCols: return allCols
-    numRows=len(allCols[0])
-    allRows=[[allCols[j][i] for j in range(len(allCols))] for i in range(numRows)]
+    numRows = len(allCols[0])
+    allRows = [[allCols[j][i] for j in range(len(allCols))] for i in range(numRows)]
     return allRows
-    
+
 def selectIdColumn(dataFrame):
     if "Id" in dataFrame.columns:
         return "Id"
