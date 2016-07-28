7df488bd45120e83fb61814a5c57074f54631bf2
ygao <ygao@479f5059-62a3-4c39-9342-9852e3a68435>
2016-07-28 22:25:25 +0000

    Review Request 5079: Change the cfgbld script to make it work for Mac

--- le-dev/scripts/cfgbld.sh
+++ le-dev/scripts/cfgbld.sh
@@ -4,4 +4,11 @@ pushd $WSHOME/le-config
 mvn -DskipTests clean compile
 popd
 
-sed -i "s|\${LE_STACK}|${LE_STACK}|g" $WSHOME/le-config/conf/env/dev/latticeengines.properties
+UNAME=`uname`
+if [[ "${UNAME}" == 'Darwin' ]]; then
+    echo "You are on Mac"
+    sed -i '' "s|\${LE_STACK}|${LE_STACK}|g" $WSHOME/le-config/conf/env/dev/latticeengines.properties
+else
+    echo "You are on ${UNAME}"
+    sed -i "s|\${LE_STACK}|${LE_STACK}|g" $WSHOME/le-config/conf/env/dev/latticeengines.properties
+fi
