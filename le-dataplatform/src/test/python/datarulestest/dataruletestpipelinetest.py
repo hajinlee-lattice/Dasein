61d432f7fb0bebb72b57b649cb44c35a22514ac3
omukhtar <omukhtar@479f5059-62a3-4c39-9342-9852e3a68435>
2016-08-02 02:01:03 +0000

    Review Request 5114 Update datarule test pipeline json and test

--- le-dataplatform/src/test/python/datarulestest/dataruletestpipelinetest.py
+++ le-dataplatform/src/test/python/datarulestest/dataruletestpipelinetest.py
@@ -14,7 +14,7 @@ class DataRuleTestPipelineTest(DataRuleTestBase):
         launcher = Launcher("model-datarule-test.json")
         launcher.execute(False)
 
-        self.assertRuleOutputCount(9)
+        self.assertRuleOutputCount(7)
         self.assertColumnRuleOutput("./results/datarules/TestColumnRule_ColumnRule.avro", ["BW_mapping", "BW_seo_title"])
         testRowRule = TestRowRule()
         self.assertRowRuleOutput("./results/datarules/TestRowRule_RowRule.avro", testRowRule.getRowsToRemove())
