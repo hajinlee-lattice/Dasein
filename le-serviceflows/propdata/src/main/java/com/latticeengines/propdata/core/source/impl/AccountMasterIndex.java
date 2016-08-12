5f058324a499b388e94f1748c8814d8cbe364fa7
dzheng <dzheng@479f5059-62a3-4c39-9342-9852e3a68435>
2016-08-12 22:04:36 +0000

    Review Request 5303

---
+++ le-serviceflows/propdata/src/main/java/com/latticeengines/propdata/core/source/impl/AccountMasterIndex.java
@@ -0,0 +1,73 @@
+package com.latticeengines.propdata.core.source.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.latticeengines.propdata.core.source.FixedIntervalSource;
+import com.latticeengines.propdata.core.source.PurgeStrategy;
+import com.latticeengines.propdata.core.source.Source;
+
+@Component("accountMasterIndex")
+public class AccountMasterIndex implements FixedIntervalSource {
+    @Autowired
+    AccountMasterSeed baseSource;
+
+    private long cutoffLimitInSeconds;
+
+    private String cronExpression;
+
+    @Override
+    public String getSourceName() {
+        return "AccountMasterIndex";
+    }
+
+    @Override
+    public Source[] getBaseSources() {
+        return new Source[] { baseSource };
+    }
+
+    @Override
+    public String[] getPrimaryKey() {
+        return new String[] { "LatticeID", "Domain", "DUNS" };
+    }
+
+    @Override
+    public String getTransformationServiceBeanName() {
+        return "accountMasterIndexRefreshService";
+    }
+
+    @Override
+    public String getDirForBaseVersionLookup() {
+        return "Snapshot";
+    }
+
+    @Override
+    public String getTimestampField() {
+        return "LE_Last_Upload_Date";
+    }
+
+    @Override
+    public String getDefaultCronExpression() {
+        return cronExpression;
+    }
+
+    @Override
+    public Integer getNumberOfVersionsToKeep() {
+        return 3;
+    }
+
+    @Override
+    public Integer getNumberOfDaysToKeep() {
+        return 7;
+    }
+
+    @Override
+    public PurgeStrategy getPurgeStrategy() {
+        return PurgeStrategy.NUM_VERSIONS;
+    }
+
+    @Override
+    public Long getCutoffDuration() {
+        return cutoffLimitInSeconds;
+    }
+}
