2626d25e577480a0ef6bf5512d44fdb06588164d
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-08-13 01:21:14 +0000

    Review Request 5311 use nexus docker

--- le-awsenvironment/src/main/python/latticeengines/ecr/docker.py
+++ le-awsenvironment/src/main/python/latticeengines/ecr/docker.py
@@ -5,6 +5,8 @@ import subprocess
 
 from ..conf import AwsEnvironment
 
+NEXUS_DOCKER_REGISTRY="bodcdevnexus75.dev.lattice.local:18555"
+NAMESPACE="latticeengines"
 
 def main():
     args = parse_args()
@@ -15,7 +17,10 @@ def push(args):
     config = AwsEnvironment(args.environment)
     print "pushing image %s:%s to repo ..." % (args.image, args.remotetag)
     login_cmd = login(args.environment)
-    destination = config.ecr_registry() + "/latticeengines/" + args.image + ":" + args.remotetag
+    reg_url = config.ecr_registry()
+    if args.environment == 'dev':
+        reg_url = NEXUS_DOCKER_REGISTRY
+    destination = reg_url + "/" + NAMESPACE + "/" + args.image + ":" + args.remotetag
     subprocess.call(login_cmd + "; docker push %s" % destination, shell=True)
     subprocess.call("docker rmi " + destination, shell=True)
 
@@ -25,7 +30,9 @@ def pull(args):
 
 def pull_internal(environment, image, remotetag, localtag):
     registry = AwsEnvironment(environment).ecr_registry()
-    source = registry + "/latticeengines/" + image + ":" + remotetag
+    if environment == 'dev':
+        registry = NEXUS_DOCKER_REGISTRY
+    source = registry + "/" + NAMESPACE + "/" + image + ":" + remotetag
     login_cmd = login(environment)
     subprocess.call(login_cmd + "; docker pull %s" % source, shell=True)
     tag_for_local(registry, image, remotetag, localtag)
@@ -33,18 +40,32 @@ def pull_internal(environment, image, remotetag, localtag):
 
 def tag_for_remote(args):
     config = AwsEnvironment(args.environment)
-    print "tagging image %s:%s as %s ..." % (args.image, args.localtag, args.remotetag)
-    source = "latticeengines/" +  args.image + ":" + args.localtag
-    destination = config.ecr_registry() + "/latticeengines/" + args.image + ":" + args.remotetag
+    source = NAMESPACE + "/" +  args.image + ":" + args.localtag
+    reg_url = config.ecr_registry()
+    if args.environment == 'dev':
+        reg_url = NEXUS_DOCKER_REGISTRY
+    destination = reg_url + "/" + NAMESPACE + "/" + args.image + ":" + args.remotetag
+    print "tagging image %s as %s ..." % (source, destination)
     subprocess.call(["docker", "tag", source, destination])
 
 def tag_for_local(registry, image, remotetag, localtag):
-    source = registry + "/latticeengines/" + image + ":" + remotetag
+    source = registry + "/" + NAMESPACE + "/" + image + ":" + remotetag
     print "tagging image %s as %s:%s ..." % (source, image, localtag)
-    destination = "latticeengines/" +  image + ":" + localtag
+    destination = "" + NAMESPACE + "/" +  image + ":" + localtag
     subprocess.call(["docker", "tag", source, destination])
 
 def login(environment):
+    if environment == 'dev':
+        return login_nexus()
+    else:
+        return login_aws(environment)
+
+def login_nexus():
+    username, password, url = 'ysong', 'welcome', 'bodcdevnexus75.dev.lattice.local:18555'
+    print "logging in docker registry %s ..." % url
+    return 'docker login -u %s -p %s %s' % (username, password, url)
+
+def login_aws(environment):
     config = AwsEnvironment(environment)
     account_id = config.aws_account_id()
     print "logging in docker registry for account %s ..." % account_id
@@ -59,14 +80,14 @@ def parse_args():
     commands = parser.add_subparsers(help="commands")
 
     subparser = commands.add_parser("push")
-    subparser.add_argument('image', metavar='IMAGE', type=str, help='local docker image name. you can ignore the namespace latticeengines')
+    subparser.add_argument('image', metavar='IMAGE', type=str, help='local docker image name. you can ignore the namespace ' + NAMESPACE)
     subparser.add_argument('-e', dest='environment', type=str, default='dev', choices=['dev', 'qacluster','prodcluster'], help='environment')
     subparser.add_argument('-t', dest='remotetag', type=str, default="latest", help='remote tag (default=latest)')
     subparser.add_argument('--local-tag', dest='localtag', type=str, default="latest", help='local tag (default=latest)')
     subparser.set_defaults(func=push)
 
     subparser = commands.add_parser("pull")
-    subparser.add_argument('image', metavar='IMAGE', type=str, help='local docker image name. you can ignore the namespace latticeengines')
+    subparser.add_argument('image', metavar='IMAGE', type=str, help='local docker image name. you can ignore the namespace ' + NAMESPACE)
     subparser.add_argument('-e', dest='environment', type=str, default='dev', choices=['dev', 'qacluster','prodcluster'], help='environment')
     subparser.add_argument('-t', dest='remotetag', type=str, default="latest", help='remote tag (default=latest)')
     subparser.add_argument('--local-tag', dest='localtag', type=str, default="latest", help='local tag (default=latest)')
