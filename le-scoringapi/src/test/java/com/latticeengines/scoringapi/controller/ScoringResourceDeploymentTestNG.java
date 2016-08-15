351cd46a17924bcde2c0505a6d6a579b7b4e4b0f
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-08-15 15:41:44 +0000

    Review Request 5165: Cleanup code

--- le-scoringapi/src/test/java/com/latticeengines/scoringapi/controller/ScoringResourceDeploymentTestNG.java
+++ le-scoringapi/src/test/java/com/latticeengines/scoringapi/controller/ScoringResourceDeploymentTestNG.java
@@ -184,6 +184,7 @@ public class ScoringResourceDeploymentTestNG extends ScoringResourceDeploymentTe
         }
     }
 
+    @SuppressWarnings("rawtypes")
     @Test(groups = "deployment", enabled = true, dependsOnMethods = { "scoreRecords" })
     public void testScoreCorrectness() throws IOException {
         String url = apiHostPort + "/score/record/debug";
