ba0144872bff76fdbb1604e03a28942bf84ce11c
mmeng <mmeng@479f5059-62a3-4c39-9342-9852e3a68435>
2016-08-16 22:41:05 +0000

    Review Request 5299: modify sending bis (sfdc setting) access token pls controller backend

--- le-pls/src/main/java/com/latticeengines/pls/controller/BISAccessTokenResource.java
+++ le-pls/src/main/java/com/latticeengines/pls/controller/BISAccessTokenResource.java
@@ -1,21 +1,24 @@
 package com.latticeengines.pls.controller;
 
-import com.latticeengines.domain.exposed.ResponseDocument;
-import com.latticeengines.domain.exposed.security.User;
-import com.latticeengines.security.exposed.service.EmailService;
-import com.latticeengines.security.exposed.service.UserService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.latticeengines.domain.exposed.ResponseDocument;
+import com.latticeengines.domain.exposed.playmaker.PlaymakerTenant;
+import com.latticeengines.domain.exposed.security.User;
+import com.latticeengines.security.exposed.service.EmailService;
+import com.latticeengines.security.exposed.service.UserService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Api(value = "bisaccesstoken", description = "REST resource for getting the one-time bis access token")
 @RestController
@@ -39,25 +42,29 @@ public class BISAccessTokenResource {
     @ResponseBody
     @SuppressWarnings("unchecked")
     @ApiOperation(value = "Get a one-time bis access token and email it to user")
-    public ResponseDocument<Boolean> getOneTimeTokenAndEmail(@RequestParam String username, @RequestParam String tenantId) {
-        Map<String, String> requestBody = new HashMap<>();
-        requestBody.put("TenantName", tenantId);
-        requestBody.put("TenantPassword", "null");
-        requestBody.put("ExternalId", "null");
-        requestBody.put("JdbcDriver", "null");
-        requestBody.put("JdbcUrl", "null");
+    public ResponseDocument<Boolean> getOneTimeTokenAndEmail(@RequestParam String username,
+            @RequestParam String tenantId) {
+        PlaymakerTenant playmakerTenant = new PlaymakerTenant();
+        playmakerTenant.setTenantName(tenantId);
+        playmakerTenant.setTenantPassword("null");
+        playmakerTenant.setExternalId("null");
+        playmakerTenant.setJdbcDriver("null");
+        playmakerTenant.setJdbcUrl("null");
 
         try {
             User user = userService.findByUsername(username);
-            Map<String, String> response = restTemplate.postForObject(String.format("%s/tenants?TenantName=%s",
-                    playmakerEndpoint, tenantId), requestBody, Map.class);
+            playmakerTenant = restTemplate.postForObject(
+                    String.format("%s/tenants", playmakerEndpoint), playmakerTenant,
+                    PlaymakerTenant.class);
 
             log.info(String.format("The user is: %s", user.toString()));
-            log.info(response);
+            log.info(playmakerTenant);
 
-            emailService.sendPlsOnetimeSfdcAccessTokenEmail(user, tenantId, response.get("TenantPassword"));
+            emailService.sendPlsOnetimeSfdcAccessTokenEmail(user, tenantId,
+                    playmakerTenant.getTenantPassword());
         } catch (Exception e) {
-            log.warn(String.format("Generate bis access token failed for user: %s and tenant: %s", username, tenantId), e);
+            log.warn(String.format("Generate bis access token failed for user: %s and tenant: %s",
+                    username, tenantId), e);
             return ResponseDocument.failedResponse(e);
         }
         return ResponseDocument.successResponse(true);
