bf3f67de4e0d4536079cbc77d80731af36d8db34
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-08-22 09:51:38 +0000

    Review Request 5395 fix the logic about homogeneous bulk match input

--- le-propdata/src/main/java/com/latticeengines/propdata/match/service/impl/BulkMatchPlanner.java
+++ le-propdata/src/main/java/com/latticeengines/propdata/match/service/impl/BulkMatchPlanner.java
@@ -1,7 +1,10 @@
 package com.latticeengines.propdata.match.service.impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
+import com.latticeengines.domain.exposed.metadata.ColumnMetadata;
 import com.latticeengines.domain.exposed.propdata.match.MatchInput;
 import com.latticeengines.domain.exposed.propdata.match.MatchOutput;
 import com.latticeengines.propdata.match.service.MatchPlanner;
@@ -9,6 +12,10 @@ import com.newrelic.api.agent.Trace;
 
 @Component("bulkMatchPlanner")
 public class BulkMatchPlanner extends MatchPlannerBase implements MatchPlanner {
+
+    public MatchContext plan(MatchInput input, List<ColumnMetadata> metadatas) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
 
     @Trace
     public MatchContext plan(MatchInput input) {
