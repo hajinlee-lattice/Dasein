cc49fd29e37d2102a45faecf88cde6ef233ad619
ygao <ygao@479f5059-62a3-4c39-9342-9852e3a68435>
2016-08-27 18:26:59 +0000

    review request 5476 -- Provide rawScore in the rts bulk scoring and add some tests, address Ron's comment

--- le-scoring/src/main/java/com/latticeengines/scoring/orchestration/service/ScoringDaemonService.java
+++ le-scoring/src/main/java/com/latticeengines/scoring/orchestration/service/ScoringDaemonService.java
@@ -42,7 +42,7 @@ public interface ScoringDaemonService {
     public static final String SCORING_JOB_TYPE = "scoringJob";
     public static final String UNIQUE_KEY_COLUMN = "LeadID";
 
-    public static final String Import_Error_File_Name = "error.csv";
+    public static final String IMPORT_ERROR_FILE_NAME = "error.csv";
     public static final String MODEL_ID = "ModelId";
 
 }
