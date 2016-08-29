642a99f709576b8a2571b17659fe0d67c5992a16
lyan <lyan@479f5059-62a3-4c39-9342-9852e3a68435>
2016-08-29 10:00:24 +0000

    Review Request 5522: change org.slf4j.Logger to org.apache.commons.logging.Log

--- le-propdata/src/test/java/com/latticeengines/propdata/match/service/impl/MatchAcceptanceServiceImplTestNG.java
+++ le-propdata/src/test/java/com/latticeengines/propdata/match/service/impl/MatchAcceptanceServiceImplTestNG.java
@@ -10,8 +10,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.CallableStatementCreator;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -32,7 +32,7 @@ import com.latticeengines.propdata.match.testframework.PropDataMatchFunctionalTe
 
 public class MatchAcceptanceServiceImplTestNG extends PropDataMatchFunctionalTestNGBase {
 
-    private static final Logger log = LoggerFactory.getLogger(MatchAcceptanceServiceImplTestNG.class);
+    private static final Log log = LogFactory.getLog(MatchAcceptanceServiceImplTestNG.class);
 
     @Autowired
     private MatchCommandsService matchCommandsService;
