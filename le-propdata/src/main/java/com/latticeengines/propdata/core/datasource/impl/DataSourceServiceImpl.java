aeb5e982879adc97d3bd3767cea158f2a92d48c8
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-09-01 03:56:36 +0000

    Review Request 5569

--- le-propdata/src/main/java/com/latticeengines/propdata/core/datasource/impl/DataSourceServiceImpl.java
+++ le-propdata/src/main/java/com/latticeengines/propdata/core/datasource/impl/DataSourceServiceImpl.java
@@ -2,11 +2,13 @@ package com.latticeengines.propdata.core.datasource.impl;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -21,6 +23,9 @@ import com.latticeengines.propdata.core.service.ZkConfigurationService;
 
 @Component
 public class DataSourceServiceImpl implements DataSourceService {
+
+    private static final Log log  = LogFactory.getLog(DataSourceServiceImpl.class);
+    private final AtomicInteger roundRobinPos = new AtomicInteger(0);
 
     @Autowired
     private ZkConfigurationService zkConfigurationService;
@@ -48,11 +53,17 @@ public class DataSourceServiceImpl implements DataSourceService {
     public List<JdbcTemplate> getJdbcTemplatesFromDbPool(DataSourcePool pool, Integer num) {
         List<DataSourceConnection> connectionList = zkConfigurationService.getConnectionsInPool(pool);
         List<JdbcTemplate> jdbcTemplates = new ArrayList<>();
-        Integer roundRobinPos = new Random(System.currentTimeMillis()).nextInt(connectionList.size());
         for (int i = 0; i < num; i++) {
-            DataSourceConnection connection = connectionList.get(roundRobinPos);
-            jdbcTemplates.add(DataSourceUtils.getJdbcTemplate(connection));
-            roundRobinPos = (roundRobinPos + 1) % connectionList.size();
+            try {
+                DataSourceConnection connection = connectionList.get(roundRobinPos.get() % connectionList.size());
+                jdbcTemplates.add(DataSourceUtils.getJdbcTemplate(connection));
+            } catch (Exception e) {
+                log.error("Failed to retrieve a jdbcTemplate from datasource pool", e);
+            }
+            synchronized (roundRobinPos) {
+                Integer nextPos = (roundRobinPos.get() + 1) % connectionList.size();
+                roundRobinPos.set(nextPos);
+            }
         }
         return jdbcTemplates;
     }
