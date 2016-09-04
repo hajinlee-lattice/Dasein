a38d2186e094a5de9f1dc6a602648149146015ca
fmeng <fmeng@479f5059-62a3-4c39-9342-9852e3a68435>
2016-09-04 00:04:02 +0000

    Temp fix to PLS-1945. Martin needs to investigate further. Review Request 0000 - just add null checking.

--- le-pls/src/main/java/com/latticeengines/pls/service/impl/WorkflowJobServiceImpl.java
+++ le-pls/src/main/java/com/latticeengines/pls/service/impl/WorkflowJobServiceImpl.java
@@ -96,7 +96,7 @@ public class WorkflowJobServiceImpl implements WorkflowJobService {
         }
 
         for (Job job : jobs) {
-            String modelId = job.getInputs() != null && job.getOutputs() != null && job.getInputs().get(WorkflowContextConstants.Inputs.MODEL_ID) != null
+            String modelId = job != null && job.getInputs() != null && job.getOutputs() != null && job.getInputs().get(WorkflowContextConstants.Inputs.MODEL_ID) != null
                     ? job.getInputs().get(WorkflowContextConstants.Inputs.MODEL_ID)
                     : job.getOutputs().get(WorkflowContextConstants.Inputs.MODEL_ID);
             if (modelId == null || !modelIdToModelSummaries.containsKey(modelId)) {
