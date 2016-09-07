b63118490af56c0667471cfbc39c9e97dbda07fa
bross <bross@479f5059-62a3-4c39-9342-9852e3a68435>
2016-09-07 18:02:08 +0000

    Review Request 0000: Parallel utilities

---
+++ le-common/src/main/java/com/latticeengines/common/exposed/parallel/Parallel.java
@@ -0,0 +1,41 @@
+package com.latticeengines.common.exposed.parallel;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Parallel {
+    private static final int NUM_CORES = Runtime.getRuntime().availableProcessors();
+
+    public static <T> void foreach(final Iterable<T> elements, final Operation<T> operation) {
+        ExecutorService forPool = Executors.newFixedThreadPool(NUM_CORES * 2);
+
+        try {
+            forPool.invokeAll(createCallables(elements, operation));
+        } catch (InterruptedException e) {
+            // pass
+        }
+    }
+
+    private static <T> Collection<Callable<Void>> createCallables(final Iterable<T> elements, final Operation<T> operation) {
+        List<Callable<Void>> callables = new LinkedList<>();
+        for (final T elem : elements) {
+            callables.add(new Callable<Void>() {
+                @Override
+                public Void call() {
+                    operation.perform(elem);
+                    return null;
+                }
+            });
+        }
+
+        return callables;
+    }
+
+    public interface Operation<T> {
+        void perform(T parameter);
+    }
+}
