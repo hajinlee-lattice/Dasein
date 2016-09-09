94af22f0274f0767722f1164ceb61eac977e9573
omukhtar <omukhtar@479f5059-62a3-4c39-9342-9852e3a68435>
2016-09-09 00:13:16 +0000

    Review Request 5679 ModelTesting UI upload files and create folders

--- le-modelquality/src/main/ui/modeltestingui/routes/runexperiment.js
+++ le-modelquality/src/main/ui/modeltestingui/routes/runexperiment.js
@@ -1,16 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var util = require('util')
 var config = require('../public/resources/modelqualitybasic.json');
 var request = require("request")
 
+var WebHDFS = require('webhdfs');
+var hdfs = WebHDFS.createClient({ user: process.env.USER, host: "10.41.1.185", port: 50070 });
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
   str = config
   //obj = JSON.parse(config)
- console.log(str.tenant)
- console.log(str)
-
+ console.log("req.files" + util.inspect(req.body))
+ console.log("req.files" + util.inspect(req.body))
+ console.log("req.files" + util.inspect(req.files["datasetCSV"]))
+ basePath = "/app/a/2.0.38/experiments/"
+var date = new Date()
+ randomID = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" +  date.getDate() + "-" + parseInt(Math.random() * 10000000000)
+ hdfsDatasetFile= basePath + randomID + "/dataset.csv"
+ hdfsPipelineFile= basePath + randomID + "/pipeline.json"
+ hdfsPipelineTarFile= basePath + randomID + "/lepipeline.tar.gz"
  
+ console.log(hdfs)
+ console.dir(hdfs)
+ hdfs.mkdir(basePath + randomID, mode="0777", function(err, success) {
+ hdfs.writeFile(hdfsDatasetFile, req.files["datasetCSV"].data, function (err, success) {
+        if (err instanceof Error) { console.log(err); }
+        console.log(success)
+      }); 
+hdfs.writeFile(hdfsPipelineFile, req.files["pipelineJSON"].data, function (err, success) {
+        if (err instanceof Error) { console.log(err); }
+        console.log(success)
+      }); 
+hdfs.writeFile(hdfsPipelineTarFile, req.files["pipelineTARGZ"].data, function (err, success) {
+        if (err instanceof Error) { console.log(err); }
+        console.log(success)
+      }); 
+})
+  res.send("Done")
+ return
   // Send post request!
   var url = "https://admin-qa.lattice.local:8080/modelquality/runmodel/?tenant=ModelQualityExperiments.ModelQualityExperiments.Production&username=bnguyen@lattice-engines.com&password=tahoe&apiHostPort=https://bodcdevtca18.lattice.local:8081"
   var tenant="ModelQualityExperiments.ModelQualityExperiments.Production"
