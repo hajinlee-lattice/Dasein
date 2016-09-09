040102414be4a20e8b227824b0df32f831517b34
ygao <ygao@479f5059-62a3-4c39-9342-9852e3a68435>
2016-09-09 21:23:24 +0000

    Review Request 5689: address Haitao comment on logging import

--- le-scoring/src/main/java/com/latticeengines/scoring/orchestration/service/impl/ScoringStepYarnProcessorImpl.java
+++ le-scoring/src/main/java/com/latticeengines/scoring/orchestration/service/impl/ScoringStepYarnProcessorImpl.java
@@ -6,11 +6,11 @@ import java.util.List;
 import java.util.Properties;
 import java.util.UUID;
 
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.yarn.api.records.ApplicationId;
-import org.codehaus.plexus.util.ExceptionUtils;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.springframework.beans.factory.annotation.Autowired;
