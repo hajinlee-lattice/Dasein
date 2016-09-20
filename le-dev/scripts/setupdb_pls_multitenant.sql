9f682d715afc2dcb4051357b1186a5d9ae606309
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-09-20 01:02:53 +0000

    Review Request 5779 migrate propdata engine triggers to quartz framework

--- le-dev/scripts/setupdb_pls_multitenant.sql
+++ le-dev/scripts/setupdb_pls_multitenant.sql
@@ -8,4 +8,5 @@ USE `PLS_MultiTenant`;
 
 source WSHOME/le-db/ddl_pls_multitenant_mysql5innodb.sql
 
-INSERT INTO QUARTZ_JOBACTIVE(IsActive, JobName, TenantId) VALUES (1, 'modelSummaryDownload', 'PredefinedJobs'); 
+INSERT INTO QUARTZ_JOBACTIVE(IsActive, JobName, TenantId) VALUES (1, 'modelSummaryDownload', 'PredefinedJobs');
+INSERT INTO QUARTZ_JOBACTIVE(IsActive, JobName, TenantId) VALUES (0, 'dataCloudRefresh', 'PredefinedJobs');
