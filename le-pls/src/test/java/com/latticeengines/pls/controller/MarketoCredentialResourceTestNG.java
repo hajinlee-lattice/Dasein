6c3c851a1e0bdd0356509c0407bb72a9221ea2ba
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-09-30 17:44:57 +0000

    Review Request 5165: Cleanup code

--- le-pls/src/test/java/com/latticeengines/pls/controller/MarketoCredentialResourceTestNG.java
+++ le-pls/src/test/java/com/latticeengines/pls/controller/MarketoCredentialResourceTestNG.java
@@ -1,9 +1,7 @@
 package com.latticeengines.pls.controller;
 
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertNotNull;
-import static org.testng.Assert.assertNull;
 import static org.testng.Assert.assertTrue;
 
 import java.util.List;
