5027d1f7838262420f14ba0aabc04b028b746925
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-10-01 16:36:50 +0000

    Review Request 5940 enhance local GA db population

--- le-dev/scripts/setupdb_globalauth.sql
+++ le-dev/scripts/setupdb_globalauth.sql
@@ -46,6 +46,17 @@ INSERT INTO GlobalUser (
   NOW(),
   0,
   0
+), (
+  'pls-super-admin-tester@test.lattice-engines.com',
+  'Super',
+  'User',
+  '000-000-0000',
+  'Super User',
+  1,
+  NOW(),
+  NOW(),
+  0,
+  0
 );
 
 INSERT INTO GlobalAuthentication (
@@ -57,16 +68,38 @@ INSERT INTO GlobalAuthentication (
   Last_Modification_Date,
   Created_By,
   Last_Modified_by
-) VALUES (
-  1,
-  'bnguyen@lattice-engines.com',
+) SELECT
+ GlobalUser_ID,
+ 'bnguyen@lattice-engines.com',
   'mE2oR2b7hmeO1DpsoKuxhzx/7ODE9at6um7wFqa7udg=',
   0,
   NOW(),
   NOW(),
   0,
   0
-);
+FROM GlobalUser
+WHERE Email = 'bnguyen@lattice-engines.com';
+
+INSERT INTO GlobalAuthentication (
+  User_ID,
+  Username,
+  Password,
+  MustChangePassword,
+  Creation_Date,
+  Last_Modification_Date,
+  Created_By,
+  Last_Modified_by
+) SELECT
+ GlobalUser_ID,
+ 'pls-super-admin-tester@test.lattice-engines.com',
+  'EETAlfvFzCdm6/t3Ro8g89vzZo6EDCbucJMTPhYgWiE=',
+  0,
+  NOW(),
+  NOW(),
+  0,
+  0
+FROM GlobalUser
+WHERE Email = 'pls-super-admin-tester@test.lattice-engines.com';
 
 
 INSERT INTO GlobalUserTenantRight (
@@ -77,12 +110,36 @@ INSERT INTO GlobalUserTenantRight (
   Last_Modification_Date,
   Created_By,
   Last_Modified_by
-) VALUES (
-  1,
-  1,
+) SELECT
+  u.GlobalUser_ID,
+  t.GlobalTenant_ID,
   'SUPER_ADMIN',
   NOW(),
   NOW(),
   0,
   0
-);
+FROM GlobalUser u, GlobalTenant t
+WHERE u.Email = 'bnguyen@lattice-engines.com'
+AND t.Deployment_ID = 'LocalTest.LocalTest.Production'
+LIMIT 1;
+
+INSERT INTO GlobalUserTenantRight (
+  Tenant_ID,
+  User_ID,
+  Operation_Name,
+  Creation_Date,
+  Last_Modification_Date,
+  Created_By,
+  Last_Modified_by
+) SELECT
+  u.GlobalUser_ID,
+  t.GlobalTenant_ID,
+  'SUPER_ADMIN',
+  NOW(),
+  NOW(),
+  0,
+  0
+FROM GlobalUser u, GlobalTenant t
+WHERE u.Email = 'pls-super-admin-tester@test.lattice-engines.com'
+AND t.Deployment_ID = 'LocalTest.LocalTest.Production'
+LIMIT 1;
