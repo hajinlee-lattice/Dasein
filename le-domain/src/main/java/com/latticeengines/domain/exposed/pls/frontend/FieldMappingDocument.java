45740fca4e0434424ab86711586f2f13d2891413
mmeng <mmeng@479f5059-62a3-4c39-9342-9852e3a68435>
2016-10-03 18:49:37 +0000

    Review Request 5921: add required fields for field mapping document

--- le-domain/src/main/java/com/latticeengines/domain/exposed/pls/frontend/FieldMappingDocument.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/pls/frontend/FieldMappingDocument.java
@@ -1,5 +1,6 @@
 package com.latticeengines.domain.exposed.pls.frontend;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -10,6 +11,9 @@ public class FieldMappingDocument {
 
     @JsonProperty
     private List<String> ignoredFields;
+
+    @JsonProperty
+    private List<String> requiredFields = new ArrayList<>();
 
     public void setFieldMappings(List<FieldMapping> fieldMappings) {
         this.fieldMappings = fieldMappings;
@@ -26,4 +30,13 @@ public class FieldMappingDocument {
     public List<String> getIgnoredFields() {
         return this.ignoredFields;
     }
+
+    public void setRequiredFields(List<String> requiredFields) {
+        this.requiredFields = requiredFields;
+    }
+
+    public List<String> getRequiredFields() {
+        return requiredFields;
+    }
+
 }
