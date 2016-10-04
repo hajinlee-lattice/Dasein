b9161349dd6326cdc391bdd17f9535d2a2ceb401
dhom <dhom@479f5059-62a3-4c39-9342-9852e3a68435>
2016-10-04 23:03:28 +0000

    Review Request 6005 - fix node server port env var

--- le-ui/server/configs/config_leadmin.js
+++ le-ui/server/configs/config_leadmin.js
@@ -1,24 +1,22 @@
 'use strict';
 
-const path = require('path');
-
 module.exports = {
     protocols: {
-        http: process.env.ADMIN_HTTP_PORT,
-        https: process.env.ADMIN_HTTPS_PORT
+        http: parseInt(process.env.ADMIN_HTTP_PORT) || undefined,
+        https: parseInt(process.env.ADMIN_HTTPS_PORT) || undefined
     },
     proxies: {
         '/admin': {
             'local_path': '/admin',
             'remote_host': process.env.API_ADMIN_URL || 'http://localhost:8085',
             'remote_path': '/admin',
-          'type': 'pipe'
+            'type': 'pipe'
         },
         '/modelquality': {
             'local_path': '/modelquality',
             'remote_host': process.env.API_MCSVC_URL || 'http://localhost:8080',
             'remote_path': '/modelquality',
-          'type': 'pipe'
+            'type': 'pipe'
         },
         '/influxdb' : {
             'local_path': '/influxdb',
--- le-ui/server/configs/config_leui.js
+++ le-ui/server/configs/config_leui.js
@@ -1,30 +1,28 @@
 'use strict';
 
-const path = require('path');
-
 module.exports = {
     protocols: {
-        http: process.env.HTTP_PORT,
-        https: process.env.HTTPS_PORT,
+        http: parseInt(process.env.HTTP_PORT) || undefined,
+        https: parseInt(process.env.HTTPS_PORT) || undefined
     },
     proxies: {
         '/pls': {
             'local_path': '/pls',
             'remote_host': process.env.API_URL || 'http://localhost:8081',
             'remote_path': '/pls',
-          'type': 'pipe'
+            'type': 'pipe'
         },
         '/score': {
             'local_path': '/score',
             'remote_host': process.env.API_CON_URL || 'http://localhost:8073',
             'remote_path': '/score',
-          'type': 'pipe'
+            'type': 'pipe'
         },
         '/files': {
             'local_path': '/files',
             'remote_host': process.env.API_URL || 'http://localhost:8080',
             'remote_path': '/pls',
-          'type': 'file_pipe'
+            'type': 'file_pipe'
         }
     }
 };
