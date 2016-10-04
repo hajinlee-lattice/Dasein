22dc1f79db6bac224505b2828189e19eb9b1cd3a
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-10-04 23:37:00 +0000

    Review Request 5854 resume cadvisor

--- le-awsenvironment/src/main/python/latticeengines/cf/module/ec2.py
+++ le-awsenvironment/src/main/python/latticeengines/cf/module/ec2.py
@@ -91,6 +91,17 @@ def ecs_metadata(ec2, ecscluster):
                             "echo ECS_AVAILABLE_LOGGING_DRIVERS=[\\\"json-file\\\", \\\"awslogs\\\"] >> /etc/ecs/ecs.config\n",
                             "echo ECS_RESERVED_PORTS=[22] >> /etc/ecs/ecs.config\n"
                         ] ] }
+                    },
+                    "20_start_cadvisor" : {
+                        "command" : { "Fn::Join": [ "", [
+                            "#!/bin/bash\n",
+                            "start ecs\n"
+                            "yum install -y aws-cli jq\n"
+                            "instance_arn=$(curl -s http://localhost:51678/v1/metadata | jq -r '. | .ContainerInstanceArn' | awk -F/ '{print $NF}' )\n",
+                            "az=$(curl -s http://instance-data/latest/meta-data/placement/availability-zone)\n",
+                            "region=", { "Ref" : "AWS::Region" }, "\n",
+                            "aws ecs start-task --cluster ", ecscluster.ref(), " --task-definition cadvisor --container-instances $instance_arn --region $region\n"
+                        ] ] }
                     }
                 },
                 "services" : {
