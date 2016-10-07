c432214bcd733808c84d0eff6093e1b1f5b99deb
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2016-10-07 19:28:54 +0000

    Review Request 6068: second round of cleaning up dataplatform functional test

--- le-dataplatform/src/test/java/com/latticeengines/dataplatform/service/impl/dlorchestration/ModelStepYarnProcessorImplTestNG.java
+++ le-dataplatform/src/test/java/com/latticeengines/dataplatform/service/impl/dlorchestration/ModelStepYarnProcessorImplTestNG.java
@@ -37,7 +37,7 @@ public class ModelStepYarnProcessorImplTestNG extends DataPlatformFunctionalTest
         return false;
     }
 
-    @Test(groups = "functional")
+    @Test(groups = "functional", enabled = false)
     public void testExecuteYarnSteps() throws Exception {
         cleanUpHdfs("Nutanix");
         cleanUpHdfs(CustomerSpace.parse("Nutanix").toString());
@@ -109,28 +109,6 @@ public class ModelStepYarnProcessorImplTestNG extends DataPlatformFunctionalTest
         } catch (Exception e) {
             log.error("Failed to create FeaturesThreshold document in Camille. " + e.toString());
         }
-
-        cleanUpHdfs("Nutanix");
-        cleanUpHdfs(CustomerSpace.parse("Nutanix").toString());
-        setupDBConfig();
-        ModelCommand command = ModelingServiceTestUtils.createModelCommandWithCommandParameters(1L, featureThreshold);
-        ModelCommandParameters commandParameters = new ModelCommandParameters(command.getCommandParameters());
-
-        List<ApplicationId> appIds = modelStepYarnProcessor.executeYarnStep("Nutanix", ModelCommandStep.LOAD_DATA,
-                command, commandParameters);
-        waitForSuccess(1 * NUM_ALGORITHMS, appIds, ModelCommandStep.LOAD_DATA);
-
-        appIds = modelStepYarnProcessor.executeYarnStep("Nutanix", ModelCommandStep.GENERATE_SAMPLES, command,
-                commandParameters);
-        waitForSuccess(1 * NUM_ALGORITHMS, appIds, ModelCommandStep.GENERATE_SAMPLES);
-
-        appIds = modelStepYarnProcessor.executeYarnStep("Nutanix", ModelCommandStep.PROFILE_DATA, command,
-                commandParameters);
-        waitForSuccess(1 * NUM_ALGORITHMS, appIds, ModelCommandStep.PROFILE_DATA);
-
-        appIds = modelStepYarnProcessor.executeYarnStep("Nutanix", ModelCommandStep.SUBMIT_MODELS, command,
-                commandParameters);
-        waitForSuccess(ModelingServiceTestUtils.NUM_SAMPLES * NUM_ALGORITHMS, appIds, ModelCommandStep.SUBMIT_MODELS);
     }
 
     private void setupDBConfig() {
