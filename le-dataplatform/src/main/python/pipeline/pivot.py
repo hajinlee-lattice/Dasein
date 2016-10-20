9abd97f237a71991dea7d04e9dfa8679272a9a8e
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-10-20 18:45:00 +0000

    Review Request 6268: RecordTransformer changes

--- le-dataplatform/src/main/python/pipeline/pivot.py
+++ le-dataplatform/src/main/python/pipeline/pivot.py
@@ -4,9 +4,18 @@ def transform(args, record):
     column = args["column1"]
     targetColumn = args["column2"]
     
+    recordValue = record[column]
+    if targetColumn.endswith("___ISNULL___"):
+        if recordValue is None:
+            return 1.0
+        return 0.0
+    
     values = pivotValues[targetColumn][1]
     
-    for value in values:
-        if record[column] == value:
-            return 1
-    return 0
+    if values is not None and len(values) > 0:
+        for value in values:
+            if record[column] == value:
+                return 1.0
+        return 0.0
+    
+    return 0.0
