3ed209bf1027d762bfa228a7be3fe68d73072c78
dzheng <dzheng@479f5059-62a3-4c39-9342-9852e3a68435>
2016-10-25 23:49:53 +0000

    Review Request 6338

---
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/datacloud/match/DnBMatchEntry.java
@@ -0,0 +1,136 @@
+package com.latticeengines.domain.exposed.datacloud.match;
+
+import java.util.List;
+
+public class DnBMatchEntry {
+    private String transactionId;
+
+    private Boolean success;
+
+    private List<String> messages;
+
+    private String name;
+
+    private String city;
+
+    private String state;
+
+    private String country;
+
+    private String countryCode;
+
+    private String domain;
+
+    private String duns;
+
+    private String email;
+
+    private String zipcode;
+
+    private String street;
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public List<String> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<String> messages) {
+        this.messages = messages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getDuns() {
+        return duns;
+    }
+
+    public void setDuns(String duns) {
+        this.duns = duns;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+}
