1538d984df56165280497c8e522fd27c599f36a1
ygao <ygao@479f5059-62a3-4c39-9342-9852e3a68435>
2016-10-25 00:14:46 +0000

    review request 6319 Change the setupzk.sh to meet the case where the Contract node is empty

--- le-dev/scripts/setupzk.sh
+++ le-dev/scripts/setupzk.sh
@@ -8,9 +8,11 @@ fi
 PYTHON=${PYTHON:=python}
 ZK_HOST="localhost:2181"
 LOCAL_TEST_ROOT="/Pods/Default/Contracts/LocalTest"
+LOCAL_TEST_POD="/Pods/Default/Contracts"
 
-$PYTHON -c "import zc.zk; zk = zc.zk.ZooKeeper('${ZK_HOST}'); print zk.delete_recursive('${LOCAL_TEST_ROOT}') if zk.exists('${LOCAL_TEST_ROOT}') else '${LOCAL_TEST_ROOT} is empty'"
+$PYTHON -c "import zc.zk; zk = zc.zk.ZooKeeper('${ZK_HOST}'); print zk.delete_recursive('${LOCAL_TEST_ROOT}') if zk.exists('${LOCAL_TEST_ROOT}') else '${LOCAL_TEST_ROOT} is empty'; 
+print 'LOCAL_TEST_POD exists' if zk.exists('${LOCAL_TEST_POD}') else zk.create('${LOCAL_TEST_POD}', '', zc.zk.OPEN_ACL_UNSAFE)"
 
-zookeeper_import ${ZK_HOST} $WSHOME/le-dev/testartifacts/zookeeper/LocalTest.txt /Pods/Default/Contracts
+zookeeper_import ${ZK_HOST} $WSHOME/le-dev/testartifacts/zookeeper/LocalTest.txt ${LOCAL_TEST_POD}
 
 $PYTHON -c "import zc.zk; zk = zc.zk.ZooKeeper('${ZK_HOST}'); print zk.export_tree('${LOCAL_TEST_ROOT}')"
