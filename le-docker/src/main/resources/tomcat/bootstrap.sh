b31972651d2db475e7ae01dbdbc3e7e60415486f
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-10-27 17:56:23 +0000

    Review Request 6236 fix tomcat docker https

--- le-docker/src/main/resources/tomcat/bootstrap.sh
+++ le-docker/src/main/resources/tomcat/bootstrap.sh
@@ -9,23 +9,28 @@ NETWORK="${NETWORK:=lenet}"
 SERVICE="matchapi"
 
 source ../functions.sh
-teardown_simple_service ${SERVICE} ${CLUSTER}
+# teardown_simple_service ${SERVICE} ${CLUSTER}
 
-cat ${WSHOME}/le-config/conf/env/dev/latticeengines.properties | head -n 10
-rm -rf /tmp/latticeengines.properties
-cp ${WSHOME}/le-config/conf/env/dev/latticeengines.properties /tmp/latticeengines.properties
-cat /tmp/latticeengines.properties | head -n 10
+#docker run -d \
+#    --name ${CLUSTER}_matchapi \
+#    -h ${CLUSTER}-matchapi \
+#    --net host \
+#    -e LE_ENVIRONMENT=dev \
+#    -e LE_STACK=${LE_STACK} \
+#    -v ${WSHOME}/le-config/conf/env/dev/latticeengines.properties:/etc/ledp/latticeengines.properties \
+#    -l ${SERVICE}.group=${CLUSTER} \
+#    -l cluster=${CLUSTER} \
+#    -p 80:8080 \
+#    -p 443:8443 \
+#    latticeengines/matchapi
 
 docker run -d \
-    --name ${CLUSTER}_matchapi \
-    -h ${CLUSTER}-matchapi \
-    --net host \
-    -e LE_ENVIRONMENT=dev \
-    -e LE_STACK=${LE_STACK} \
-    -v /tmp/latticeengines.properties:/etc/ledp/dev/latticeengines.properties \
+    --name ${CLUSTER}_tomcat \
+    -h ${CLUSTER}-tomcat \
     -l ${SERVICE}.group=${CLUSTER} \
     -l cluster=${CLUSTER} \
-    -p 8076:8080 \
-    latticeengines/matchapi
+    -p 80:8080 \
+    -p 443:8443 \
+    latticeengines/tomcat
 
 docker_ps
