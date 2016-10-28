1f92c5c14f60a3eca5dc25702d87baca7fef6d1a
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-10-28 04:00:03 +0000

    Review Request 6382: Rename WorkflowFunctionalTestNGBase to WorkflowTestNGBase

---
+++ le-serviceflows/core/src/test/java/com/latticeengines/serviceflows/functionalframework/ServiceFlowsWorkflowDeploymentTestNGBase.java
@@ -0,0 +1,117 @@
+package com.latticeengines.serviceflows.functionalframework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.util.LinkedMultiValueMap;
+import org.testng.annotations.Listeners;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.latticeengines.domain.exposed.ResponseDocument;
+import com.latticeengines.domain.exposed.admin.LatticeProduct;
+import com.latticeengines.domain.exposed.camille.CustomerSpace;
+import com.latticeengines.domain.exposed.metadata.Table;
+import com.latticeengines.domain.exposed.pls.ModelingParameters;
+import com.latticeengines.domain.exposed.pls.SchemaInterpretation;
+import com.latticeengines.domain.exposed.pls.SourceFile;
+import com.latticeengines.domain.exposed.pls.frontend.FieldMapping;
+import com.latticeengines.domain.exposed.pls.frontend.FieldMappingDocument;
+import com.latticeengines.domain.exposed.security.Tenant;
+import com.latticeengines.testframework.security.GlobalAuthTestBed;
+import com.latticeengines.testframework.security.impl.GlobalAuthCleanupTestListener;
+import com.latticeengines.workflow.functionalframework.WorkflowTestNGBase;
+
+@Listeners({ GlobalAuthCleanupTestListener.class })
+@TestExecutionListeners({ DirtiesContextTestExecutionListener.class })
+@ContextConfiguration(locations = { "classpath:test-serviceflows-context.xml" })
+public abstract class ServiceFlowsWorkflowDeploymentTestNGBase extends WorkflowTestNGBase {
+
+    private static final Log log = LogFactory.getLog(ServiceFlowsWorkflowDeploymentTestNGBase.class);
+    
+    protected static final long WORKFLOW_WAIT_TIME_IN_MILLIS = 1000L * 60 * 90;
+    
+    @Autowired
+    protected GlobalAuthTestBed deploymentTestBed;
+
+    @Value("${common.test.microservice.url}")
+    protected String microServiceUrl;
+    
+    protected String microServiceHostPort;
+    
+    @Value("${common.test.pls.url}")
+    protected String plsUrl;
+
+    protected CustomerSpace setupTenant() {
+        microServiceHostPort = microServiceUrl.split("//")[1];
+        deploymentTestBed.bootstrapForProduct(LatticeProduct.LPA3);
+        restTemplate = deploymentTestBed.getRestTemplate();
+        magicRestTemplate = deploymentTestBed.getMagicRestTemplate();
+        Tenant tenant = deploymentTestBed.getMainTestTenant();
+        deploymentTestBed.switchToSuperAdmin();
+        return CustomerSpace.parse(tenant.getId());
+    }
+
+    @SuppressWarnings("rawtypes")
+    public SourceFile uploadFile(String resourceBase, String fileName) {
+        log.info("Uploading file " + fileName);
+
+        LinkedMultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+        map.add("file", new ClassPathResource(String.format("%s/%s", resourceBase, fileName)));
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = new HttpEntity<>(map, headers);
+        ResponseDocument response = restTemplate.postForObject( //
+                String.format("%s/pls/models/uploadfile/unnamed?displayName=%s", plsUrl, fileName), //
+                requestEntity, ResponseDocument.class);
+        SourceFile sourceFile = new ObjectMapper().convertValue(response.getResult(), SourceFile.class);
+        log.info(sourceFile.getName());
+        return sourceFile;
+    }
+
+    @SuppressWarnings("rawtypes")
+    public void resolveMetadata(SourceFile sourceFile, SchemaInterpretation schemaInterpretation) {
+        log.info("Resolving metadata for modeling ...");
+        ModelingParameters parameters = new ModelingParameters();
+        parameters.setDescription("Test");
+        parameters.setFilename(sourceFile.getName());
+
+        sourceFile.setSchemaInterpretation(schemaInterpretation);
+        ResponseDocument response = restTemplate.postForObject(
+                String.format("%s/pls/models/uploadfile/%s/fieldmappings?schema=%s", plsUrl,
+                        sourceFile.getName(), schemaInterpretation.name()),
+                parameters, ResponseDocument.class);
+        FieldMappingDocument mappings = new ObjectMapper().convertValue(response.getResult(),
+                FieldMappingDocument.class);
+
+        for (FieldMapping mapping : mappings.getFieldMappings()) {
+            if (mapping.getMappedField() == null) {
+                mapping.setMappedToLatticeField(false);
+                mapping.setMappedField(mapping.getUserField().replace(' ', '_'));
+            }
+        }
+
+        List<String> ignored = new ArrayList<>();
+        mappings.setIgnoredFields(ignored);
+
+        log.info("The fieldmappings are: " + mappings.getFieldMappings());
+        log.info("The ignored fields are: " + mappings.getIgnoredFields());
+        restTemplate.postForObject(String.format("%s/pls/models/uploadfile/fieldmappings?displayName=%s",
+                plsUrl, sourceFile.getName()), mappings, Void.class);
+        
+        Table table = restTemplate.getForObject(String.format("%s/pls/fileuploads/%s/metadata",
+                plsUrl, sourceFile.getName()), Table.class);
+        sourceFile.setTableName(table.getName());
+    }
+}
