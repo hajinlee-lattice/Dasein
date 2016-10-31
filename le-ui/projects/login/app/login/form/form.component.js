b5525fe8d741bbc61c452db1864b568516dabcb3
JLazarus <JLazarus@479f5059-62a3-4c39-9342-9852e3a68435>
2016-10-31 13:34:10 +0000

    Review Request 0000 - login store now in it's own service file

--- le-ui/projects/login/app/login/form/form.component.js
+++ le-ui/projects/login/app/login/form/form.component.js
@@ -5,41 +5,6 @@ angular.module('login.form', [
     'mainApp.core.utilities.BrowserStorageUtility',
     'mainApp.login.services.LoginService'
 ])
-.service('LoginStore', function(TimestampIntervalUtility, BrowserStorageUtility) {
-    this.login = {
-        username: '',
-        expires: null,
-        expireDays: 0,
-        tenant: ''
-    };
-
-    this.set = function(LoginDocument, ClientSession) {
-        this.login.username = LoginDocument.UserName;
-        this.login.expires = LoginDocument.PasswordLastModified;
-        this.login.expireDays = Math.abs(TimestampIntervalUtility.getDays(LoginDocument.PasswordLastModified) - 90);
-
-        if (ClientSession && ClientSession.Tenant) {
-            this.login.tenant = ClientSession.Tenant.DisplayName;
-        }
-    }
-
-    this.redirectToLP = function (Tenant) {
-        if (!Tenant) {
-            var ClientSession = BrowserStorageUtility.getClientSession();
-            var Tenant = ClientSession.Tenant;
-        }
-
-        var UIVersion = Tenant.UIVersion || "2.0";
-        var pathMap = {
-            "3.0": "/lp/",
-            "2.0": "/lp2/"
-        };
-
-        var previousSession = BrowserStorageUtility.getClientSession();
-
-        return window.open(pathMap[UIVersion] || "/lp2", "_self");
-    }
-})
 .component('loginForm', {
     templateUrl: 'app/login/form/form.component.html',
     controller: function (
