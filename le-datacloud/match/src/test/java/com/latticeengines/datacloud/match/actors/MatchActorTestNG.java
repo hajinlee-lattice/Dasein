345d261d819844f2c4762002d2895ce2e6a41861
akumar <akumar@479f5059-62a3-4c39-9342-9852e3a68435>
2016-11-03 23:27:38 +0000

    Review request 6549 - enhanced actor test

--- le-datacloud/match/src/test/java/com/latticeengines/datacloud/match/actors/MatchActorTestNG.java
+++ le-datacloud/match/src/test/java/com/latticeengines/datacloud/match/actors/MatchActorTestNG.java
@@ -1,35 +1,55 @@
 package com.latticeengines.datacloud.match.actors;
 
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.latticeengines.actors.ActorTemplate;
+import com.latticeengines.datacloud.match.actors.framework.MatchActorSystem;
 import com.latticeengines.datacloud.match.actors.visitor.DataSourceLookupRequest;
 import com.latticeengines.datacloud.match.actors.visitor.MatchTraveler;
 import com.latticeengines.datacloud.match.actors.visitor.impl.DnbLookupActor;
 import com.latticeengines.datacloud.match.testframework.DataCloudMatchFunctionalTestNGBase;
 
 import akka.actor.ActorRef;
+import akka.pattern.Patterns;
+import akka.util.Timeout;
+import scala.concurrent.Await;
+import scala.concurrent.Future;
+import scala.concurrent.duration.FiniteDuration;
 
 public class MatchActorTestNG extends DataCloudMatchFunctionalTestNGBase {
     @Autowired
-    private SampleActorSystem sampleActorSystem;
+    private MatchActorSystem actorSystem;
 
     @Test
-    public void testDnBActor() {
-        LogManager.getLogger("com.latticeengines.datacloud.match.actors.visitor").setLevel(Level.DEBUG);
-        LogManager.getLogger("com.latticeengines.actors.visitor").setLevel(Level.DEBUG);
-
-        ActorRef actorRef = sampleActorSystem.getActorRef(DnbLookupActor.class);
+    public void testDnBActor() throws Exception {
         DataSourceLookupRequest msg = new DataSourceLookupRequest();
         msg.setCallerMicroEngineReference(null);
         msg.setInputData("");
         String rootOperationUid = UUID.randomUUID().toString();
         MatchTraveler matchTravelerContext = new MatchTraveler(rootOperationUid);
         msg.setMatchTravelerContext(matchTravelerContext);
-        actorRef.tell(msg, null);
+
+        Object result = sendMessageToActor(msg, DnbLookupActor.class);
+        Assert.assertNotNull(result);
+    }
+
+    private Object sendMessageToActor(Object msg, Class<? extends ActorTemplate> actorClazz) throws Exception {
+        LogManager.getLogger("com.latticeengines.datacloud.match.actors.visitor").setLevel(Level.DEBUG);
+        LogManager.getLogger("com.latticeengines.actors.visitor").setLevel(Level.DEBUG);
+
+        ActorRef actorRef = actorSystem.getActorRef(actorClazz);
+
+        Timeout timeout = new Timeout(new FiniteDuration(10, TimeUnit.MINUTES));
+        Future<Object> future = Patterns.ask(actorRef, msg, timeout);
+
+        Object result = Await.result(future, timeout.duration());
+        return result;
     }
 }
