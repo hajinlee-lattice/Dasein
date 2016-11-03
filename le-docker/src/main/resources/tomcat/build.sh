0fdd1a1691477b151d1f2f5f001310bb51362b6d
bnguyen <bnguyen@479f5059-62a3-4c39-9342-9852e3a68435>
2016-11-03 19:32:46 +0000

    Review Request 6545 Create oauth2, playmaker, scoringapi definitions

--- le-docker/src/main/resources/tomcat/build.sh
+++ le-docker/src/main/resources/tomcat/build.sh
@@ -12,4 +12,16 @@ pushd le-matchapi
 build_docker latticeengines/matchapi
 popd
 
+pushd le-oauth2
+build_docker latticeengines/oauth2
+popd
+
+pushd le-playmaker
+build_docker latticeengines/playmaker
+popd
+
+pushd le-scoringapi
+build_docker latticeengines/scoringapi
+popd
+
 docker rmi -f $(docker images -a --filter "dangling=true" -q --no-trunc) 2> /dev/null
--- le-docker/src/main/resources/tomcat/teardown.sh
+++ le-docker/src/main/resources/tomcat/teardown.sh
@@ -6,3 +6,6 @@ CLUSTER="${CLUSTER:=tomcat}"
 
 source ../functions.sh
 teardown_simple_service "matchapi" $CLUSTER
+teardown_simple_service "oauth2" $CLUSTER
+teardown_simple_service "playmaker" $CLUSTER
+teardown_simple_service "scoringapi" $CLUSTER
