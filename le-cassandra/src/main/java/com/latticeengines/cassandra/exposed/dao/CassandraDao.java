92dc56b74e86aeaebe8d1ab08493f57625e2dab7
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-11-05 16:51:37 +0000

    Review Request 6597: Remove defunct projects

--- le-cassandra/src/main/java/com/latticeengines/cassandra/exposed/dao/CassandraDao.java
+++
@@ -1,9 +0,0 @@
-package com.latticeengines.cassandra.exposed.dao;
-
-import java.io.Serializable;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CassandraDao<T, ID extends Serializable> extends CrudRepository<T, ID> {
-
-}
--- le-cassandra/src/main/java/com/latticeengines/cassandra/exposed/dao/data/ColumnType.java
+++
@@ -1,5 +0,0 @@
-package com.latticeengines.cassandra.exposed.dao.data;
-
-public enum ColumnType {
-    ASCII, BIGINT, BLOB, BOOLEAN, COUNTER, DECIMAL, DOUBLE, FLOAT, INET, INT, LIST, MAP, SET, TEXT, TIMESTAMP, TIMEUUID, UUID, VARCHAR, VARINT
-}
