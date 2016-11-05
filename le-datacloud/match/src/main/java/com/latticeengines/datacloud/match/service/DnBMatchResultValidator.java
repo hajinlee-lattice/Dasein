dd86284ee75180033eb16b4d1588c37fed135102
dzheng <dzheng@479f5059-62a3-4c39-9342-9852e3a68435>
2016-11-05 00:36:45 +0000

    Review Request 6589

--- le-datacloud/match/src/main/java/com/latticeengines/datacloud/match/service/DnBMatchResultValidator.java
+++ le-datacloud/match/src/main/java/com/latticeengines/datacloud/match/service/DnBMatchResultValidator.java
@@ -3,5 +3,5 @@ package com.latticeengines.datacloud.match.service;
 import com.latticeengines.domain.exposed.datacloud.dnb.DnBMatchOutput;
 
 public interface DnBMatchResultValidator {
-    public boolean validate(DnBMatchOutput res);
+    boolean validate(DnBMatchOutput res);
 }
