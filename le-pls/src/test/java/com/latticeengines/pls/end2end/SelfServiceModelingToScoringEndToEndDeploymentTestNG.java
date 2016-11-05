0651d9cfc59eecb0ea595191003654bd5bd74542
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-11-05 23:39:17 +0000

    Review Request 6600: Make trunk health more strict.

--- le-pls/src/test/java/com/latticeengines/pls/end2end/SelfServiceModelingToScoringEndToEndDeploymentTestNG.java
+++ le-pls/src/test/java/com/latticeengines/pls/end2end/SelfServiceModelingToScoringEndToEndDeploymentTestNG.java
@@ -1,6 +1,9 @@
 package com.latticeengines.pls.end2end;
 
+import static org.testng.Assert.assertEquals;
+
 import java.io.IOException;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -38,7 +41,15 @@ public class SelfServiceModelingToScoringEndToEndDeploymentTestNG extends PlsDep
     public void testLeadModelToScoreCorrectness() throws InterruptedException, IOException {
         String fileName = "Mulesoft_MKTO_LP3_ScoringLead_20160316_170113.csv";
         String modelId = selfServiceModeling.prepareModel(SchemaInterpretation.SalesforceLead, fileName);
-        scoreCompareService.analyzeScores(tenant.getId(), RESOURCE_BASE + "/" + fileName, modelId, 1000);
+        Map<String, ComparedRecord> diffRecords = scoreCompareService.analyzeScores(tenant.getId(), 
+                RESOURCE_BASE + "/" + fileName, modelId, 1000);
+        
+        String expectedDiffCountStr = System.getProperty("DIFFCOUNT");
+        
+        if (expectedDiffCountStr != null) {
+            int expectedDiffCount = Integer.valueOf(expectedDiffCountStr);
+            assertEquals(diffRecords.size(), expectedDiffCount);
+        }
     }
 
     @Test(groups = "deployment.lp", enabled = false)
