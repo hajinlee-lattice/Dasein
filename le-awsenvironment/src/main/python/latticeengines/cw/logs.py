bb849b72f11a50e681cab201024334fea8306a43
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-11-08 21:34:52 +0000

    Review Request 6526 add create log group api

--- le-awsenvironment/src/main/python/latticeengines/cw/logs.py
+++ le-awsenvironment/src/main/python/latticeengines/cw/logs.py
@@ -18,9 +18,32 @@ def clean_internal(group):
             logStreamName=stream['logStreamName']
         )
 
+
+def create(args):
+    create_internal(args.group)
+
+def create_internal(group):
+    client = boto3.client('logs')
+    response = client.describe_log_groups(
+        logGroupNamePrefix=group
+    )
+    already_exists = False
+    for group in response['logGroups']:
+        if group['logGroupName'] == group:
+            already_exists = True
+    if already_exists:
+        clean_internal(group)
+    else:
+        client.create_log_group(logGroupName=group)
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Docker image management')
     commands = parser.add_subparsers(help="commands")
+
+    subparser = commands.add_parser("create", description="Create a log group. If already exists, clean it up.")
+    subparser.add_argument('group', metavar='GROUP', type=str, help='log group to be created.')
+    subparser.set_defaults(func=clean)
 
     subparser = commands.add_parser("clean", description="Clean up a log group")
     subparser.add_argument('group', metavar='GROUP', type=str, help='log group to be cleaned up.')
