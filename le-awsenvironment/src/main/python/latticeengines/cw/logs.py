43938033c28906481936d9b88e4d00df7bd6970c
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-11-14 16:50:57 +0000

    Review Request 6694 add clean up log streams with prefix

--- le-awsenvironment/src/main/python/latticeengines/cw/logs.py
+++ le-awsenvironment/src/main/python/latticeengines/cw/logs.py
@@ -6,18 +6,20 @@ def main():
     args.func(args)
 
 def clean(args):
-    clean_internal(args.group)
+    clean_internal(args.group, args.prefix)
 
-def clean_internal(group):
+def clean_internal(group, prefix=None):
     client = boto3.client('logs')
-    response = client.describe_log_streams(logGroupName=group)
+    if prefix is None:
+        response = client.describe_log_streams(logGroupName=group)
+    else:
+        response = client.describe_log_streams(logGroupName=group, logStreamNamePrefix=prefix)
     streams = response['logStreams']
     for stream in streams:
         client.delete_log_stream(
             logGroupName=group,
             logStreamName=stream['logStreamName']
         )
-
 
 def create(args):
     create_internal(args.group)
@@ -42,11 +44,12 @@ def parse_args():
     commands = parser.add_subparsers(help="commands")
 
     subparser = commands.add_parser("create", description="Create a log group. If already exists, clean it up.")
-    subparser.add_argument('group', metavar='GROUP', type=str, help='log group to be created.')
+    subparser.add_argument('-g', dest='group', type=str,required=True, help='log group to be created.')
     subparser.set_defaults(func=create)
 
     subparser = commands.add_parser("clean", description="Clean up a log group")
-    subparser.add_argument('group', metavar='GROUP', type=str, help='log group to be cleaned up.')
+    subparser.add_argument('-g', dest='group', type=str,required=True, help='log group to be cleaned up.')
+    subparser.add_argument('-p', dest='prefix', type=str, help='log stream prefix to be deleted.')
     subparser.set_defaults(func=clean)
 
     args = parser.parse_args()
