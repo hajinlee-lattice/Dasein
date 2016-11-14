b0828350f22065fb17443f67f68bf5f20e10d3d4
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-11-14 07:41:53 +0000

    Review Request 6694 ignore json truncate error in rest error handling

--- le-security/src/main/java/com/latticeengines/security/exposed/serviceruntime/exception/GetResponseErrorHandler.java
+++ le-security/src/main/java/com/latticeengines/security/exposed/serviceruntime/exception/GetResponseErrorHandler.java
@@ -52,7 +52,9 @@ public class GetResponseErrorHandler implements ResponseErrorHandler {
 
             //TODO: temporary workaround. After finding out the root cause of json truncation, we should remove this.
             if (message.contains("Could not read JSON: Unexpected end-of-input in VALUE_STRING")
-                    || message.contains("Could not read JSON: Unexpected end-of-input in character escape sequence")) {
+                    || message.contains("Could not read JSON: Unexpected end-of-input in character escape sequence")
+                    || stackTraceString.contains("Could not read JSON: Unexpected end-of-input in VALUE_STRING")
+                    || stackTraceString.contains("Could not read JSON: Unexpected end-of-input in character escape sequence")) {
                 throw new RuntimeException("Seems JSON IO was truncated: " + body);
             }
             exception = new RemoteLedpException(stackTraceString, status, code, message);
