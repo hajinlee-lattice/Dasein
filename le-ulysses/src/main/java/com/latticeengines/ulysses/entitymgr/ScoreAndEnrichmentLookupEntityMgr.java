efca70012b668cd65d4bb7c98e889c1911dedfb6
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2016-11-15 17:28:00 +0000

    Review Request 6786: Ulysses entity managers

--- le-ulysses/src/main/java/com/latticeengines/ulysses/entitymgr/ScoreAndEnrichmentLookupEntityMgr.java
+++
@@ -1,8 +0,0 @@
-package com.latticeengines.ulysses.entitymgr;
-
-import com.latticeengines.datafabric.entitymanager.BaseFabricEntityMgr;
-import com.latticeengines.domain.exposed.ulysses.ScoreAndEnrichmentLookupRecord;
-
-public interface ScoreAndEnrichmentLookupEntityMgr extends BaseFabricEntityMgr<ScoreAndEnrichmentLookupRecord> {
-
-}
