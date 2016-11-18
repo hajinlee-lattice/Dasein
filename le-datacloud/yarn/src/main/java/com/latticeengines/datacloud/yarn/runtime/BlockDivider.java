65341c4cc0f9ad189603d48f05d6431d5d9a5503
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-11-18 20:51:56 +0000

    Review Request 6859 fix bulk match

--- le-datacloud/yarn/src/main/java/com/latticeengines/datacloud/yarn/runtime/BlockDivider.java
+++ le-datacloud/yarn/src/main/java/com/latticeengines/datacloud/yarn/runtime/BlockDivider.java
@@ -9,7 +9,6 @@ import org.apache.avro.generic.GenericRecord;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
 
 import com.latticeengines.common.exposed.util.AvroUtils;
 
@@ -37,7 +36,7 @@ class BlockDivider {
     }
 
     private void readSchema() {
-        Schema schema = AvroUtils.getSchema(yarnConfiguration, new Path(avroPath));
+        Schema schema = AvroUtils.getSchemaFromGlob(yarnConfiguration, avroPath);
         log.info("Got input schema: " + schema.toString());
         fields = new ArrayList<>();
         fieldTypes = new ArrayList<>();
