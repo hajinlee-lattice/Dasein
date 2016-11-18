767dfcdbc2e2ef258a067ec31c6a81e1d136aac7
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-11-18 16:39:56 +0000

    Review Request 6859 change workflow rest template to the one in http-clientutils

--- le-serviceflows/core/src/main/java/com/latticeengines/serviceflows/workflow/core/BaseWorkflowStep.java
+++ le-serviceflows/core/src/main/java/com/latticeengines/serviceflows/workflow/core/BaseWorkflowStep.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.latticeengines.common.exposed.util.HttpClientUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -93,7 +94,7 @@ public abstract class BaseWorkflowStep<T extends BaseStepConfiguration> extends 
     protected MagicAuthenticationHeaderHttpRequestInterceptor addMagicAuthHeader = new MagicAuthenticationHeaderHttpRequestInterceptor();
     protected List<ClientHttpRequestInterceptor> addMagicAuthHeaders = Arrays
             .asList(new ClientHttpRequestInterceptor[] { addMagicAuthHeader });
-    protected RestTemplate restTemplate = new RestTemplate();
+    protected RestTemplate restTemplate = HttpClientUtils.newRestTemplate();
 
     @Override
     public boolean setup() {
