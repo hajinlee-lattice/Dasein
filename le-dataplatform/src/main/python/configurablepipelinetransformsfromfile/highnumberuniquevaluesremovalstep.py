9f261147915989396f97f9c6c7d3138a9dbdd158
mwilson <mwilson@479f5059-62a3-4c39-9342-9852e3a68435>
2016-11-21 21:28:06 +0000

    Review Request 6927: Exclude key columns from removal by HighNumberUniqueValuesRemovalStep

--- le-dataplatform/src/main/python/configurablepipelinetransformsfromfile/highnumberuniquevaluesremovalstep.py
+++ le-dataplatform/src/main/python/configurablepipelinetransformsfromfile/highnumberuniquevaluesremovalstep.py
@@ -16,15 +16,17 @@ class HighNumberUniqueValuesRemovalStep(PipelineStep):
     ## the algorithm to ensure that maxNumberUniqueValues
     ## is no more than N.
 
-    def __init__(self, maxNumberUniqueValues, categoricalColumns):
+    def __init__(self, maxNumberUniqueValues, categoricalColumns, params):
         self.maxNumberUniqueValues = maxNumberUniqueValues
         self.categoricalColumns = categoricalColumns
+        self.params = params
 
     def transform(self, dataFrame, configMetadata, test):
         columnsToRemove = set()
+        keys = self.params['schema']['keys']
 
         for colname in self.categoricalColumns:
-            if colname in dataFrame.columns:
+            if colname in dataFrame.columns and colname not in keys:
                 if len(dataFrame[colname].unique()) > self.maxNumberUniqueValues:
                     logger.info('Removing categorical column with more than {0} distinct values: "{1}".'.format(self.maxNumberUniqueValues, colname))
                     columnsToRemove.add(colname)
