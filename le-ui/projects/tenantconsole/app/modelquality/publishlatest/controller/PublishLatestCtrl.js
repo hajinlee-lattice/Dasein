b3ce7883bc09b4ad5317a38d9d2403a6d1761dd5
dhom <dhom@479f5059-62a3-4c39-9342-9852e3a68435>
2016-11-29 00:23:50 +0000

    Review Request 7009 - model quality publish latest reduce to only analytic pipeline

--- le-ui/projects/tenantconsole/app/modelquality/publishlatest/controller/PublishLatestCtrl.js
+++ le-ui/projects/tenantconsole/app/modelquality/publishlatest/controller/PublishLatestCtrl.js
@@ -5,13 +5,7 @@ angular.module('app.modelquality.controller.PublishLatestCtrl', [
     var vm = this;
     angular.extend(vm, {
         labels: {
-            ALGORITHMS: 'Publish latest algorithm',
-            ANALYTICPIPELINES: 'Publish latest analytic pipeline',
-            PIPELINES: 'Publish latest pipeline',
-            SAMPLINGCONFIGS: 'Publish latest sampling config',
-            DATAFLOWS: 'Publish latest dataflow',
-            PROPDATACONFIGS: 'Publish latest prop data config',
-            ALL: 'Publish All'
+            ANALYTICPIPELINES: 'Publish latest analytic pipeline'
         },
         error: false,
         message: null,
@@ -19,26 +13,6 @@ angular.module('app.modelquality.controller.PublishLatestCtrl', [
     });
 
     vm.urls = [];
-    vm.urls.push({
-        url: '/modelquality/algorithms/latest',
-        labelKey: 'ALGORITHMS'
-    });
-    vm.urls.push({
-        url: '/modelquality/samplingconfigs/latest',
-        labelKey: 'SAMPLINGCONFIGS'
-    });
-    vm.urls.push({
-        url: '/modelquality/dataflows/latest',
-        labelKey: 'DATAFLOWS'
-    });
-    vm.urls.push({
-        url: '/modelquality/propdataconfigs/latest',
-        labelKey: 'PROPDATACONFIGS'
-    });
-    vm.urls.push({
-        url: '/modelquality/pipelines/latest',
-        labelKey: 'PIPELINES'
-    });
     vm.urls.push({
         url: '/modelquality/analyticpipelines/latest',
         labelKey: 'ANALYTICPIPELINES'
@@ -53,25 +27,6 @@ angular.module('app.modelquality.controller.PublishLatestCtrl', [
         $http.post(url).then(function (result) {
             vm.error = false;
             vm.message = 'POST ' + url + ' success';
-        }).catch(function (error) {
-            vm.error = true;
-            vm.message = error.data.errorCode + ': ' + error.data.errorMsg;
-        }).finally(function () {
-            vm.loading = false;
-        });
-    };
-
-    vm.publishAll = function () {
-        vm.loading = true;
-        vm.message = null;
-
-        var promises = vm.urls.map(function (item) {
-          return $http.post(item.url);
-        });
-
-        $q.all(promises).then(function (result) {
-            vm.error = false;
-            vm.message = 'POST all latest success';
         }).catch(function (error) {
             vm.error = true;
             vm.message = error.data.errorCode + ': ' + error.data.errorMsg;
