ac666d6e653676b2818aba10189dc391d9bd5539
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-12-07 02:57:18 +0000

    Review Request 7023 template of sql upgrade scripts

---
+++ le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
@@ -0,0 +1,15 @@
+USE `PLS_MultiTenant`;
+
+DROP PROCEDURE IF EXISTS `UpdateSchema`;
+
+DELIMITER //
+CREATE PROCEDURE `UpdateSchema` ()
+  BEGIN
+
+  END //
+DELIMITER ;
+
+CALL `UpdateSchema`() ;
+
+
+
