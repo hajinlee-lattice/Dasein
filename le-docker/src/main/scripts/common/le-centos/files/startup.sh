3e753f9801c8ffe41b9de4f3e837e6da3d3c8f2d
svnsync <svnsync@479f5059-62a3-4c39-9342-9852e3a68435>
2016-12-15 02:13:59 +0000

    DEVOPS-2112 Review Request 7229 Removed uneeded services

--- le-docker/src/main/scripts/common/le-centos/files/startup.sh
+++ le-docker/src/main/scripts/common/le-centos/files/startup.sh
@@ -1,4 +1,4 @@
-service rngd restart 
-service snmpd restart 
-service ntpd restart 
-service sshd restart
+#service rngd restart 
+#service snmpd restart 
+#service ntpd restart 
+#service sshd restart
