93580e9e56cf2ee333cbbbf24388b57219a6567d
dzheng <dzheng@479f5059-62a3-4c39-9342-9852e3a68435>
2016-12-20 18:27:00 +0000

    Review Request 7334

---
+++ le-network/src/main/java/com/latticeengines/network/exposed/propdata/AMStatsInterface.java
@@ -0,0 +1,11 @@
+package com.latticeengines.network.exposed.propdata;
+
+import com.latticeengines.domain.exposed.datacloud.manage.AccountMasterFactQuery;
+import com.latticeengines.domain.exposed.datacloud.statistics.AccountMasterCube;
+import com.latticeengines.domain.exposed.datacloud.statistics.TopNAttributeTree;
+
+public interface AMStatsInterface {
+    TopNAttributeTree getTopAttrTree();
+
+    public AccountMasterCube getCube(AccountMasterFactQuery query);
+}
