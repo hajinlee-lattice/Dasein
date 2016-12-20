dd4ed32ef6ec2198862701330e11977c1ef22395
dzheng <dzheng@479f5059-62a3-4c39-9342-9852e3a68435>
2016-12-20 18:26:45 +0000

    Review Request 7334

---
+++ le-matchapi/src/main/java/com/latticeengines/matchapi/controller/AMStatsResource.java
@@ -0,0 +1,36 @@
+package com.latticeengines.matchapi.controller;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.latticeengines.datacloud.core.service.AccountMasterStatisticsService;
+import com.latticeengines.domain.exposed.datacloud.manage.AccountMasterFactQuery;
+import com.latticeengines.domain.exposed.datacloud.statistics.AccountMasterCube;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(value = "match", description = "REST resource for account master statistics")
+@RestController
+@RequestMapping("/amstats")
+public class AMStatsResource {
+    private static final Log log = LogFactory.getLog(AMStatsResource.class);
+
+    @Autowired
+    private AccountMasterStatisticsService accountMasterStatisticsService;
+
+    @RequestMapping(value = "/cubes", method = RequestMethod.POST, headers = "Accept=application/json")
+    @ResponseBody
+    @ApiOperation(value = "Get account master statistics cube")
+    private AccountMasterCube getCube(@RequestBody AccountMasterFactQuery query, HttpServletResponse response) {
+        return accountMasterStatisticsService.query(query);
+    }
+}
