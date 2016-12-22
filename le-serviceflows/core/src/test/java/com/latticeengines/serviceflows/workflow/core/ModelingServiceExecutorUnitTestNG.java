3589b2a4c83c5248fcad87d8c9ff2a1c358926a5
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2016-12-22 06:06:36 +0000

    Review Request 7390 fix serviceflow unit test

--- le-serviceflows/core/src/test/java/com/latticeengines/serviceflows/workflow/core/ModelingServiceExecutorUnitTestNG.java
+++ le-serviceflows/core/src/test/java/com/latticeengines/serviceflows/workflow/core/ModelingServiceExecutorUnitTestNG.java
@@ -1,13 +1,17 @@
 package com.latticeengines.serviceflows.workflow.core;
 
-import static org.testng.Assert.assertEquals;
-
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
 import com.latticeengines.domain.exposed.modelreview.DataRule;
 
@@ -35,8 +39,31 @@ public class ModelingServiceExecutorUnitTestNG {
         dataRules.add(ruleC);
 
         String result = modelingServiceExecutor.getEnabledRulesAsPipelineProp(dataRules);
-        assertEquals(result,
-                "remediatedatarulesstep.enabledRules={\"RuleA\":[\"ColA\",\"ColB\",\"ColC\"],\"RuleC\":[]}");
+        verifyEnabledRules(result);
+    }
+
+    private void verifyEnabledRules(String result) {
+        // verify result is "remediatedatarulesstep.enabledRules={\"RuleA\":[\"ColA\",\"ColB\",\"ColC\"],\"RuleC\":[]}"
+        Assert.assertTrue(result.startsWith("remediatedatarulesstep.enabledRules="));
+        String jsonStr = result.replace("remediatedatarulesstep.enabledRules=", "");
+        ObjectMapper objectMapper = new ObjectMapper();
+        TypeReference<Map<String, List<String>>> typeRef = new TypeReference<Map<String, List<String>>>(){};
+        Map<String, List<String>> map = new HashMap<>();
+        try {
+            map = objectMapper.readValue(jsonStr, typeRef);
+        } catch (IOException e) {
+            Assert.fail("Failed to deserialize json string [" + jsonStr + "]", e);
+        }
+
+        Assert.assertEquals(map.size(), 2);
+        Assert.assertTrue(map.containsKey("RuleA"));
+        Assert.assertTrue(map.containsKey("RuleC"));
+
+        List<String> cols = map.get("RuleA");
+        Assert.assertEquals(cols.size(), 3);
+        Assert.assertTrue(cols.contains("ColA"));
+        Assert.assertTrue(cols.contains("ColB"));
+        Assert.assertTrue(cols.contains("ColC"));
     }
 
 }
