39798b8f6cadc6828fb538ed1746765ae5abb7ed
fmeng <fmeng@479f5059-62a3-4c39-9342-9852e3a68435>
2016-12-24 04:26:56 +0000

    Review Request 0000 - fix test

--- le-scoringapi/src/test/java/com/latticeengines/scoringapi/controller/EnrichmentResourceDeploymentTestNG.java
+++ le-scoringapi/src/test/java/com/latticeengines/scoringapi/controller/EnrichmentResourceDeploymentTestNG.java
@@ -360,7 +360,7 @@ public class EnrichmentResourceDeploymentTestNG extends ScoringApiControllerDepl
             Assert.assertTrue(attr.getDisplayName().toUpperCase().contains(CORRECT_ORDER_SEARCH_DISPLAY_NAME_STR3.toUpperCase()));
         }
 
-        Assert.assertEquals(combinedAttributeList.size(), 1);
+        Assert.assertEquals(combinedAttributeList.size(), 2);
 
         combinedAttributeList = getLeadEnrichmentAttributeList(true, SEARCH_DISPLAY_NAME_STR2,
                 Category.TECHNOLOGY_PROFILE);
