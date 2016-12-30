3118e3c55502c254b609da6ba7fed13f6d89e558
dhom <dhom@479f5059-62a3-4c39-9342-9852e3a68435>
2016-12-30 22:07:37 +0000

    Review Request 7493 PLS-2714 another update to lookup form validation

--- le-ui/projects/leadprioritization/app/lookup/form/FormController.js
+++ le-ui/projects/leadprioritization/app/lookup/form/FormController.js
@@ -5,13 +5,25 @@ angular
 .controller('LookupFormController', function(
     $state, LookupStore, ResourceUtility, FeatureFlagService
 ) {
+
+    var validFormStates = [
+        ['Website'],
+        ['CompanyName', 'City', 'State', 'Country']
+    ];
+
     var vm = this;
 
     angular.extend(vm, {
         request: LookupStore.get('request'),
         params: LookupStore.get('params'),
         ResourceUtility: ResourceUtility,
-        requiredMissingField: {},
+        requiredMissingField: {
+            Website: true,
+            CompanyName: true,
+            City: true,
+            State: true,
+            Country: true
+        },
         formIsValid: false
     });
 
@@ -27,11 +39,6 @@ angular
     }
 
     vm.next = function() {
-        vm.formIsValid = vm.validate();
-        if (!vm.formIsValid) {
-            return;
-        }
-
         var timestamp = new Date().getTime();
 
         LookupStore.add('timestamp', timestamp);
@@ -42,26 +49,34 @@ angular
     }
 
     vm.validate = function() {
-        var validFormStates = [
-            ['Website'],
-            ['CompanyName', 'City', 'State', 'Country']
-        ];
+
+        vm.requiredMissingField = {};
 
         for (var i = 0; i < validFormStates.length; i++) {
             var state = validFormStates[i];
             var valid = true;
-            vm.requiredMissingField = {}
+            var dirty = false;
+            var stateMissingField = {};
 
             for (var j = 0; j < state.length; j++) {
                 var field = state[j];
+
                 if (!vm.request.record[field]) {
                     vm.requiredMissingField[field] = true;
+                    stateMissingField[field] = true;
                     valid = false;
+                } else {
+                    dirty = true;
                 }
             }
 
             if (valid) {
                 return true;
+            }
+
+            if (dirty) {
+                vm.requiredMissingField = stateMissingField;
+                return false;
             }
         }
 
