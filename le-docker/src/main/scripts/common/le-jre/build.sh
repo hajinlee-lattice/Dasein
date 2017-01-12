8fd75d31ea6e5ad91329f9ad1ec1ad9d897c9a22
svnsync <svnsync@479f5059-62a3-4c39-9342-9852e3a68435>
2017-01-12 19:45:04 +0000

    Review Request 7472 DEVOPS-2123 JRE layer like is done in the main docker repo

---
+++ le-docker/src/main/scripts/common/le-jre/build.sh
@@ -0,0 +1,3 @@
+source ../../functions.sh
+build_docker latticeengines/jre
+docker tag latticeengines/jre:latest latticeengines/jre:1.8
--- le-docker/src/main/scripts/common/le-tomcat/build.sh
+++ le-docker/src/main/scripts/common/le-tomcat/build.sh
@@ -1,33 +1,3 @@
-mkdir -p binaries
-
-pushd binaries
-if [ `basename $PWD`  != 'binaries' ] ; then
-    echo $PWD
-    echo "Not where we are supposed to be"
-    exit -1
-fi
-
-if [ ! -f "apache-tomcat-8.5.8-deployer.tar.gz" ]; then
-    wget http://10.41.1.10/tars/apache-tomcat-8.5.8-deployer.tar.gz
-fi
-
-if [ ! -f "apache-tomcat-8.5.8.tar.gz" ]; then
-    wget http://10.41.1.10/tars/apache-tomcat-8.5.8.tar.gz
-fi
-
-if [ ! -f "catalina-jmx-remote.jar" ]; then
-    wget http://10.41.1.10/tars/catalina-jmx-remote.jar
-fi
-
-if [ ! -f "catalina-ws.jar" ]; then
-    wget http://10.41.1.10/tars/catalina-ws.jar
-fi
-
-if [ ! -f "jdk-8u101-linux-x64.rpm" ]; then
-    wget http://10.41.1.10/tars/jdk-8u101-linux-x64.rpm
-fi
-
-popd
 
 source ../../functions.sh
 build_docker latticeengines/tomcat
