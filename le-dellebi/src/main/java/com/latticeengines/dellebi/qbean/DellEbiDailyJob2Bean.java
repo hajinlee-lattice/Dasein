ba775744e2f44b4caae72ef107a246ef484ab5df
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2017-01-13 00:25:22 +0000

    Review Request 7621: Organize imports and fix warnings

--- le-dellebi/src/main/java/com/latticeengines/dellebi/qbean/DellEbiDailyJob2Bean.java
+++ le-dellebi/src/main/java/com/latticeengines/dellebi/qbean/DellEbiDailyJob2Bean.java
@@ -2,14 +2,7 @@ package com.latticeengines.dellebi.qbean;
 
 import java.util.concurrent.Callable;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import com.latticeengines.dellebi.flowdef.DailyFlow;
-import com.latticeengines.dellebi.service.impl.DellEbiDailyJobCallable;
-import com.latticeengines.dellebi.util.ExportAndReportService;
-import com.latticeengines.quartzclient.qbean.QuartzJobBean;
 
 @Component("dellEbiDailyJob2")
 public class DellEbiDailyJob2Bean extends DellEbiDailyJobBean {
