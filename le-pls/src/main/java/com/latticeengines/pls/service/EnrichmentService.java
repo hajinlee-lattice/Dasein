f6c0191786d77e12183e104929c1b18a8ace5605
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2017-01-13 00:25:52 +0000

    Review Request 7621: Organize imports and fix warnings

--- le-pls/src/main/java/com/latticeengines/pls/service/EnrichmentService.java
+++ le-pls/src/main/java/com/latticeengines/pls/service/EnrichmentService.java
@@ -1,13 +1,12 @@
 package com.latticeengines.pls.service;
 
+import java.util.List;
+
 import com.latticeengines.domain.exposed.datacloud.manage.AccountMasterFactQuery;
 import com.latticeengines.domain.exposed.datacloud.statistics.AccountMasterCube;
-import com.latticeengines.domain.exposed.datacloud.statistics.TopNAttributeTree;
 import com.latticeengines.domain.exposed.datacloud.statistics.TopNAttributes;
 import com.latticeengines.domain.exposed.metadata.Category;
 import com.latticeengines.domain.exposed.pls.MarketoMatchField;
-
-import java.util.List;
 
 public interface EnrichmentService {
 
