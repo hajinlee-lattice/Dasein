d7e34a128be2e44c99c20b189937addddbbe960d
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-01-17 18:06:59 +0000

    Review Request 7660 change decision graphy: PLS-2694 PD-1462

--- le-dev/scripts/setupdb_ldc_managedb.sql
+++ le-dev/scripts/setupdb_ldc_managedb.sql
@@ -83,7 +83,7 @@ VALUES
 INSERT `DecisionGraph` (GraphName, Vertices, StartingVertices, Edges)
 VALUES
   ('Trilogy', 'DunsDomainBased,DomainBased,DunsBased', '0', '0:1|1:2'),
-  ('DragonClaw', 'DunsDomainBased,DomainBased,DunsBased,LocationToDuns', '0', '0:2,3|3:0,1');
+  ('DragonClaw', 'DunsDomainBased,DomainBased,DunsBased,LocationToDuns', '0', '0:1,2,3|3:0');
 
 
 LOAD DATA INFILE 'WSHOME/le-dev/testartifacts/LDC_ManageDB/CountryCode.csv' INTO TABLE `CountryCode`
