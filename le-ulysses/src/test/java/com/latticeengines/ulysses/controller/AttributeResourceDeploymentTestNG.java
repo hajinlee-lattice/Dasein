ee21af741de4bb1fcc1a0544c42beab80dcadf05
jadusumalli <jadusumalli@479f5059-62a3-4c39-9342-9852e3a68435>
2017-01-19 00:04:40 +0000

    Review Request 7641, added deployment test cases for Ulysses

---
+++ le-ulysses/src/test/java/com/latticeengines/ulysses/controller/AttributeResourceDeploymentTestNG.java
@@ -0,0 +1,61 @@
+package com.latticeengines.ulysses.controller;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.latticeengines.domain.exposed.attribute.PrimaryField;
+import com.latticeengines.domain.exposed.scoringapi.FieldInterpretation;
+import com.latticeengines.domain.exposed.scoringapi.FieldInterpretationCollections;
+import com.latticeengines.domain.exposed.validation.ValidationExpression;
+import com.latticeengines.ulysses.testframework.UlyssesDeploymentTestNGBase;
+
+public class AttributeResourceDeploymentTestNG extends UlyssesDeploymentTestNGBase {
+
+    private static final Logger log = Logger.getLogger(AttributeResourceDeploymentTestNG.class);
+    
+    private static final ObjectMapper OM = new ObjectMapper();
+
+    private String getAttributeResourceUrl() {
+        return ulyssesHostPort + "/ulysses/attributes";
+    }
+
+    @Test(groups = "deployment")
+    public void testGetPrimaryAttributes() {
+        String url = getAttributeResourceUrl() + "/primary";
+        List<?> genericList = getOAuth2RestTemplate().getForObject(url, List.class);
+        List<PrimaryField> primaryFields = OM.convertValue(genericList, new TypeReference<List<PrimaryField>>() {
+        });
+        log.info("Converted Primary Fields: " + primaryFields);
+        Assert.assertNotNull(primaryFields);
+        Assert.assertTrue(primaryFields.size() > 0);
+        Assert.assertEquals(FieldInterpretationCollections.PrimaryMatchingFields.size(), primaryFields.size());
+        // Create a temporary set with Primary Field Names
+        Set<String> fieldNames = new HashSet<>();
+        for (PrimaryField field : primaryFields) {
+            fieldNames.add(field.getFieldName());
+        }
+        // Make sure that all the values from EnumSet are returned in response
+        for (FieldInterpretation field : FieldInterpretationCollections.PrimaryMatchingFields) {
+            Assert.assertTrue(fieldNames.contains(field.getFieldName()));
+        }
+    }
+
+    @Test(groups = "deployment")
+    public void testGetPrimaryAttributesValidationExpression() {
+        String url = getAttributeResourceUrl() + "/primary/validation-expression";
+        ValidationExpression expression = getOAuth2RestTemplate().getForObject(url, ValidationExpression.class);
+        log.info("Primary Fields Expression: " + expression);
+        Assert.assertNotNull(expression);
+        Assert.assertNotNull(expression.getCondition());
+        Assert.assertTrue(CollectionUtils.isNotEmpty(expression.getExpressions())
+                || CollectionUtils.isNotEmpty(expression.getFields()));
+    }
+}
