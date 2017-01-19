883a751ec108a7be1a223093715eca93dab7f8de
dhom <dhom@479f5059-62a3-4c39-9342-9852e3a68435>
2017-01-19 01:16:23 +0000

    Review Request 7683 PLS-2799 update create model field mapping for fuzzy match requirement

--- le-ui/projects/leadprioritization/app/create/customfields/CustomFieldsController.js
+++ le-ui/projects/leadprioritization/app/create/customfields/CustomFieldsController.js
@@ -1,7 +1,7 @@
 angular
 .module('lp.create.import')
 .controller('CustomFieldsController', function(
-    $scope, $state, $stateParams, $timeout, ResourceUtility, 
+    $scope, $state, $stateParams, $timeout, $anchorScroll, ResourceUtility,
     ImportService, ImportStore, FieldDocument, UnmappedFields, CancelJobModal
 ) {
     var vm = this;
@@ -17,8 +17,18 @@ angular
         ],
         ignoredFields: FieldDocument.ignoredFields = [],
         fieldMappings: FieldDocument.fieldMappings,
-        RequiredFields: [],
-        initialized: false
+        initialized: false,
+        NextClicked: false,
+        standardFieldsList: ['Event', 'Id', null, 'CompanyName', 'State', 'Zip', 'Country', 'PhoneNumber'],
+        requiredFieldsMissing: {
+            'Event': true,
+            'Id': true,
+            'CompanyName': true
+        },
+        ignoredFieldLabel: '-- Unmapped Field --',
+        UnmappedFieldsMap: {},
+        standardFieldMappings: {},
+        AvailableFields: []
     });
 
     vm.init = function() {
@@ -26,22 +36,44 @@ angular
         vm.csvMetadata = ImportStore.Get($stateParams.csvFileName) || {};
         vm.schema = vm.csvMetadata.schemaInterpretation || 'SalesforceLead';
         vm.UnmappedFields = UnmappedFields[vm.schema] || [];
+        vm.standardFieldsList[2] = (vm.schema === 'SalesforceLead') ? 'Email' : 'Website';
+        vm.requiredFieldsMissing[vm.standardFieldsList[2]] = true;
 
-        vm.UnmappedFields.forEach(function(field) {
-            if (field.requiredType == 'Required') {
-                vm.RequiredFields.push(field.name);
-            }
-        });
-
-        vm.UnmappedFieldsMap = {};
         vm.UnmappedFields.forEach(function(UnmappedField) {
             vm.UnmappedFieldsMap[UnmappedField.name] = UnmappedField;
         });
 
+        var fieldMappingsMap = {};
+        vm.fieldMappings.forEach(function(fieldMapping) {
+            fieldMappingsMap[fieldMapping.mappedField] = fieldMapping;
+        });
+
+        vm.standardFieldsList.forEach(function(field) {
+            // create a copy of mapping object to preserve FieldDocument
+            // FieldDocument updated when NextClicked
+            if (fieldMappingsMap[field]) {
+                vm.standardFieldMappings[field] = angular.copy(fieldMappingsMap[field]);
+                vm.standardFieldMappings[field].mappedToLatticeField = true;
+            } else {
+                vm.standardFieldMappings[field] = {
+                    fieldType: vm.UnmappedFieldsMap[field] ? vm.UnmappedFieldsMap[field].fieldType : null,
+                    mappedField: field,
+                    mappedToLatticeField: true,
+                    userField: vm.ignoredFieldLabel
+                };
+            }
+        });
+
         vm.refreshLatticeFields();
-    }
+    };
 
     vm.changeMappingOption = function(mapping, selectedOption) {
+        vm.setMappingOption(mapping, selectedOption);
+
+        vm.refreshLatticeFields();
+    };
+
+    vm.setMappingOption = function(mapping, selectedOption) {
         mapping.mappedToLatticeField = mapping.mappedToLatticeField || false;
         delete mapping.ignored;
 
@@ -51,8 +83,8 @@ angular
                 mapping.mappedToLatticeField = false;
                 break;
             case 1: // map to lattice cloud
-                mapping.mappedField = vm.UnmappedFieldsMap[mapping.mappedField] 
-                    ? mapping.mappedField 
+                mapping.mappedField = vm.UnmappedFieldsMap[mapping.mappedField]
+                    ? mapping.mappedField
                     : '';
 
                 mapping.mappedToLatticeField = true;
@@ -63,40 +95,59 @@ angular
                 mapping.ignored = true;
                 break;
         }
-
-        vm.refreshLatticeFields();
-
-        $timeout(vm.validateForm, 0);
-    }
+    };
 
     vm.changeLatticeField = function(mapping) {
         mapping.mappedToLatticeField = true;
-        mapping.fieldType = vm.UnmappedFieldsMap[mapping.mappedField].fieldType;
+        mapping.fieldType = vm.UnmappedFieldsMap[mapping.mappedField] ? vm.UnmappedFieldsMap[mapping.mappedField].fieldType : null;
 
         vm.refreshLatticeFields();
-
-        $timeout(vm.validateForm, 0);
-    }
+    };
 
     vm.refreshLatticeFields = function() {
         vm.fieldMappingsMapped = {};
 
         vm.fieldMappings.forEach(function(fieldMapping) {
             if (fieldMapping.mappedField && !fieldMapping.ignored &&
-                !(vm.RequiredFields.indexOf(fieldMapping.mappedField) > -1 &&
-                !fieldMapping.mappedToLatticeField)) {
+                fieldMapping.mappedToLatticeField) {
 
                 vm.fieldMappingsMapped[fieldMapping.mappedField] = fieldMapping;
             }
         });
 
-    }
+        if (!vm.NextClicked) {
+            vm.AvailableFields = [];
+
+            var usedUserField = {};
+
+            for (var standardFieldMap in vm.standardFieldMappings) {
+                var fieldMapping = vm.standardFieldMappings[standardFieldMap];
+
+                var userField = fieldMapping.userField;
+                usedUserField[userField] = true;
+            }
+
+            vm.fieldMappings.forEach(function(fieldMapping) {
+                var userField = fieldMapping.userField;
+                if (!usedUserField[userField]) {
+                    vm.AvailableFields.push(userField);
+                }
+            });
+        }
+
+        $timeout(vm.validateForm, 0);
+    };
+
+    $scope.filterStandardList = function(input) {
+        for (var stdField in vm.standardFieldMappings) {
+            if (vm.standardFieldMappings[stdField].userField === input.userField) {
+                return false;
+            }
+        }
+        return true;
+    };
 
     vm.resetClicked = function($event) {
-        /*
-        ImportStore.ResetAdvancedSettings();
-        $state.go('home.models.import');
-        */
         if ($event != null) {
             $event.stopPropagation();
         }
@@ -104,6 +155,52 @@ angular
     };
 
     vm.clickNext = function() {
+        vm.NextClicked = true;
+
+        var userFieldMappingsMap = {},
+            mappedFieldMappingsMap = {};
+
+        vm.fieldMappings.forEach(function(fieldMapping) {
+            userFieldMappingsMap[fieldMapping.userField] = fieldMapping;
+
+            if (fieldMapping.mappedField) {
+                mappedFieldMappingsMap[fieldMapping.mappedField] = fieldMapping;
+            }
+        });
+
+        for (var standardField in vm.standardFieldMappings) {
+            var stdFieldMapping = vm.standardFieldMappings[standardField];
+
+            if (stdFieldMapping.userField && stdFieldMapping.userField !== vm.ignoredFieldLabel) {
+
+                // clear any lattice field that has been remapped
+                if (stdFieldMapping.mappedField) {
+                    var mappedMapping = mappedFieldMappingsMap[stdFieldMapping.mappedField];
+                    if (mappedMapping) {
+                        mappedMapping.mappedField = null;
+                        mappedMapping.mappedToLatticeField = false;
+                    }
+                }
+
+                // update user fields that has been mapped
+                var userMapping = userFieldMappingsMap[stdFieldMapping.userField];
+                if (userMapping) {
+                    userMapping.mappedField = stdFieldMapping.mappedField;
+                    userMapping.fieldType = stdFieldMapping.fieldType;
+                    userMapping.mappedToLatticeField = true;
+                }
+            }
+        }
+    };
+
+    vm.clickRemap = function() {
+        $anchorScroll();
+
+        vm.NextClicked = false;
+    };
+
+    vm.clickNextModel = function() {
+
         ShowSpinner('Saving Field Mappings...');
 
         // build ignoredFields list from temp 'ignored' fieldMapping property
@@ -126,20 +223,20 @@ angular
                 }
             });
         });
-    }
+    };
 
     vm.validateMappingSelect = function(mapping) {
         var name = 'mapping_lattice_field_select_';
 
         vm.validateIsDuplicate(name, mapping);
-    }
+    };
 
     vm.validateMappingInput = function(mapping) {
         var name = 'mapping_custom_field_input_';
 
         vm.validateIsReserved(name, mapping);
         vm.validateIsDuplicate(name, mapping);
-    }
+    };
 
     vm.validateIsReserved = function(name, mapping) {
         var isReserved = !!vm.UnmappedFieldsMap[mapping.mappedField] && !mapping.ignored;
@@ -147,7 +244,7 @@ angular
         if ($scope.fieldMappingForm[name + mapping.userField]) {
             $scope.fieldMappingForm[name + mapping.userField].$setValidity("Reserved", !isReserved);
         }
-    }
+    };
 
     vm.validateIsDuplicate = function(name, mapping) {
         var value = mapping.mappedField;
@@ -166,32 +263,59 @@ angular
         if ($scope.fieldMappingForm[name + mapping.userField]) {
             $scope.fieldMappingForm[name + mapping.userField].$setValidity("Duplicate", !isDuplicate);
         }
-    }
+    };
+
+    vm.validateRequiredFields = function() {
+        for (var requiredField in vm.requiredFieldsMissing) {
+            var fieldMapping = vm.standardFieldMappings[requiredField];
+
+            if (fieldMapping && fieldMapping.userField && fieldMapping.userField !== vm.ignoredFieldLabel) {
+                vm.requiredFieldsMissing[requiredField] = false;
+            } else {
+                vm.requiredFieldsMissing[requiredField] = true;
+            }
+        }
+
+        if (vm.schema === 'SalesforceLead') {
+            if (!vm.requiredFieldsMissing['Email']) {
+                vm.requiredFieldsMissing['CompanyName'] = false;
+            } else if (!vm.requiredFieldsMissing['CompanyName']) {
+                vm.requiredFieldsMissing['Email'] = false;
+            }
+        } else if (vm.schema === 'SalesforceAccount') {
+            if (!vm.requiredFieldsMissing['Website']) {
+                vm.requiredFieldsMissing['CompanyName'] = false;
+            } else if (!vm.requiredFieldsMissing['CompanyName']) {
+                vm.requiredFieldsMissing['Website'] = false;
+            }
+        }
+
+        for (var field in vm.requiredFieldsMissing) {
+             vm.FormValidated = vm.FormValidated && !vm.requiredFieldsMissing[field];
+        }
+    };
 
     // here are additional checks not covered by angular's built in form validation
     vm.validateForm = function() {
         vm.FormValidated = true;
 
-        // make sure there are no empty drop-down selection
-        vm.fieldMappings.forEach(function(fieldMapping) {
-            if (!fieldMapping.mappedField && fieldMapping.mappedToLatticeField) {
-                vm.FormValidated = false;
-            }
+        if (!vm.NextClicked) {
+            vm.validateRequiredFields();
+        } else {
+            // make sure there are no empty drop-down selection
+            vm.fieldMappings.forEach(function(fieldMapping) {
+                if (!fieldMapping.mappedField && fieldMapping.mappedToLatticeField) {
+                    vm.FormValidated = false;
+                }
 
-            if (fieldMapping.mappedToLatticeField) {
-                vm.validateMappingSelect(fieldMapping);
-            } else {
-                vm.validateMappingInput(fieldMapping);
-            }
-        });
-
-        // make sure all lattice required fields are mapped
-        vm.RequiredFields.forEach(function(requiredField) {
-            if (!vm.fieldMappingsMapped[requiredField]) {
-                vm.FormValidated = false;
-            }
-        });
-    }
+                if (fieldMapping.mappedToLatticeField) {
+                    vm.validateMappingSelect(fieldMapping);
+                } else {
+                    vm.validateMappingInput(fieldMapping);
+                }
+            });
+        }
+    };
 
     vm.init();
 });
