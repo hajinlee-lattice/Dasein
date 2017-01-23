cdfbc4724ccfc78970d05b0a7bf1454704e66c98
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-01-23 22:12:15 +0000

    Review Request 7682 hard code catalina home

--- le-matchapi/src/main/scripts/preinstall.sh
+++ le-matchapi/src/main/scripts/preinstall.sh
@@ -3,14 +3,14 @@
 id -u tomcat &>/dev/null || useradd tomcat
 getent group tomcat || groupadd tomcat
 
-APP_DIR="${CATALINA_HOME}/webapps/matchapi"
+APP_DIR="/usr/share/tomcat/webapps/matchapi"
 
 if [ ! -d "${APP_DIR}" ]; then
     mkdir -p ${APP_DIR}
 fi
 
 if [ ! -d "${APP_DIR}/manager" ]; then
-    cp -r ${CATALINA_HOME}/webapps/manager ${APP_DIR}
+    cp -r /usr/share/tomcat/webapps/manager ${APP_DIR}
 fi
 
 source /etc/profile
