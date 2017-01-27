3f554a87c92fc0ff4fac5e388efc45b0437ed444
yyang <yyang@479f5059-62a3-4c39-9342-9852e3a68435>
2017-01-27 23:13:46 +0000

    Review request 7909 Match grade tuning and mock am seed transformer

---
+++ le-datacloud/etl/src/main/java/com/latticeengines/datacloud/etl/transformation/transformer/impl/MegatronTransformer.java
@@ -0,0 +1,514 @@
+package com.latticeengines.datacloud.etl.transformation.transformer.impl;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import java.util.Random;
+
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.latticeengines.common.exposed.util.AvroUtils;
+import com.latticeengines.datacloud.core.source.Source;
+import com.latticeengines.datacloud.core.util.HdfsPathBuilder;
+import com.latticeengines.domain.exposed.datacloud.manage.TransformationProgress;
+import com.latticeengines.domain.exposed.datacloud.transformation.configuration.impl.TransformerConfig;
+import com.latticeengines.domain.exposed.datacloud.transformation.configuration.impl.MegatronConfig;
+
+@Component("megatron")
+class Megatron extends AbstractTransformer<MegatronConfig> {
+
+    private static final Log log = LogFactory.getLog(Megatron.class);
+
+    @Autowired
+    protected Configuration yarnConfiguration;
+
+    private static final String avroName = "/seed.avro";
+
+    private static int ORPHAN = 1;
+    private static int WRONGDU = 2;
+    private static int DUPPD = 4;
+    private static int MISSINGPD = 8;
+    private static int OOB = 16;
+
+    private Random random = new Random();
+
+    @Autowired
+    protected HdfsPathBuilder hdfsPathBuilder;
+
+    @Override
+    public boolean validateConfig(MegatronConfig config, List<String> baseSources) {
+        int numAccounts = config.getNumAccounts();
+        if ((numAccounts < 4 * 1024) || (numAccounts > 80 * 1024)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected Class<? extends TransformerConfig> getConfigurationClass() {
+        return MegatronConfig.class;
+    }
+
+    @Override
+    public String getName() {
+         return "megatron";
+    }
+
+    @SuppressWarnings("finally")
+	@Override
+    protected boolean transform(TransformationProgress progress, String workflowDir, Source[] baseSources, List<String> baseSourceVersions,
+                                Source[] baseTemplates, Source targetTemplate, MegatronConfig config, String confStr) {
+
+        Schema seedSchema;
+
+        try {
+            InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("schema/MockAmSeed.avsc");
+            Schema.Parser parser = new Schema.Parser();
+            seedSchema = parser.parse(is);
+        } catch (IOException e) {
+            log.error("Message key schema avsc file not found", e);
+            seedSchema = null;
+        }
+
+        MegatronContext context = new MegatronContext(config, seedSchema);
+
+        try {
+            createAccounts(context);
+        } catch (Exception e) {
+             log.error("Failed to create accounts", e);
+        } finally {
+            if (context.getNumAccounts() == config.getNumAccounts()) {
+                try {
+
+                    List<GenericData.Record> accounts = context.getAllAccounts();
+                    List<GenericRecord> finalAccounts = new ArrayList<GenericRecord>();
+                    for (GenericData.Record account : accounts) {
+                        String domain = (String)account.get("Domain");
+                        if (domain != null) {
+                            account.put("Domain", domain + ".com");
+                        }
+                        finalAccounts.add(account);
+                    }
+                    log.info("flushing " + finalAccounts.size() + " acounts");
+                    AvroUtils.writeToHdfsFile(yarnConfiguration, context.getSchema(), workflowDir + avroName, finalAccounts);
+                } catch (Exception e) {
+                    log.error("Failed to write to avro file", e);
+                }
+                return true;
+            } else {
+                log.error("Not all accounts are created" + context.getNumAccounts() + " " + config.getNumAccounts());
+                return false;
+            }
+        }
+    }
+
+    private void createAccounts(MegatronContext context) throws Exception {
+        MegatronConfig config = context.getConfig();
+        while (context.getNumAccounts() < config.getNumAccounts()) {
+            createOneSet(context);
+        }
+
+        messupSomePDs(context);
+        oobSomeAccounts(context);
+
+    }
+
+    private void createOneSet(MegatronContext context) throws Exception {
+        GenericData.Record dmHQ = createDmHQ(context);
+
+        if (domainOnly(context, dmHQ)) {
+            return;
+        }
+
+        if (!dunsOnly(context, dmHQ)) {
+            addSomeDomains(context, dmHQ, 0);
+        }
+
+        addSomeSubs(context, dmHQ);
+
+        messupFirmo(context, dmHQ);
+
+        addSomeOrphans(context, dmHQ);
+
+    }
+
+    private void oobSomeAccounts(MegatronContext context) {
+        if (!context.checkFlag(OOB)) {
+            return;
+        }
+        List<GenericData.Record> accounts = context.getAllAccounts();
+        for (GenericData.Record account : accounts) {
+            if (random.nextInt(16) == 1) {
+                tagAccount(account, OOB);
+            }
+        }
+    }
+
+    private void messupSomePDs(MegatronContext context) {
+        List<GenericData.Record> accounts = context.getAllAccounts();
+        for (GenericData.Record account : accounts) {
+             List<GenericData.Record> domains = context.getDomains(account);
+             if (domains == null) {
+                 if (!context.checkFlag(MISSINGPD)) {
+                     continue;
+                 }
+                 if (random.nextInt(16) == 8) {
+                     account.put("Domain", null);
+                     tagAccount(account, MISSINGPD);
+                 }
+                 continue;
+             }
+             if (!context.checkFlag(DUPPD)) {
+                 continue;
+             }
+             if (random.nextInt(16) != 8) {
+                 continue;
+             }
+             for (GenericData.Record domain : domains) {
+                 domain.put("LE_IS_PRIMARY_DOMAIN", "Y");
+                 tagAccount(domain, DUPPD);
+             }
+        }
+    }
+
+    private void addSomeOrphans(MegatronContext context, GenericData.Record dmHQ) throws Exception {
+        if (!context.checkFlag(ORPHAN)) {
+            return;
+        }
+        int orphans = random.nextInt(6);
+        orphans = (orphans < 3) ? 0 : orphans - 3;
+        for (int i = 0; i < orphans; i++) {
+            GenericData.Record orphan  = context.cloneAccountWithNewDUNS(dmHQ);
+            orphan.put("LE_NUMBER_OF_LOCATIONS", 0);
+            tagAccount(orphan, ORPHAN);
+            context.addAccount(null, orphan);
+        }
+    }
+
+    private void messupFirmo(MegatronContext context, GenericData.Record dmHQ) {
+        if (!context.checkFlag(WRONGDU)) {
+            return;
+        }
+        List<GenericData.Record> accounts = context.getChildren(dmHQ);
+
+        if (accounts == null) {
+            return;
+        }
+
+        if (random.nextInt(16) != 1) {
+            return;
+        }
+
+        String employeeStr = (String)dmHQ.get("LE_EMPLOYEE_RANGE");
+        Integer location = (Integer)dmHQ.get("LE_NUMBER_OF_LOCATIONS");
+        String revenueStr =  (String)dmHQ.get("LE_REVENUE_RANGE");
+        String industryStr =  (String)dmHQ.get("LE_INDUSTRY");
+
+
+        for (GenericData.Record account : accounts) {
+             int employees =  random.nextInt(12);
+             account.put("LE_EMPLOYEE_RANGE", employees + "");
+             int locations = (employees == 1) ? 1 : random.nextInt(16);
+             account.put("LE_NUMBER_OF_LOCATIONS", ((locations < 4) ? 1 : locations - 3));
+             account.put("LE_REVENUE_RANGE", random.nextInt(16) + "");
+             account.put("LE_INDUSTRY", random.nextInt(16) + "");
+             if (!(employeeStr.equals((String)account.get("LE_EMPLOYEE_RANGE"))) ||
+                 !(location == ((Integer)account.get("LE_NUMBER_OF_LOCATIONS"))) ||
+                 !(revenueStr.equals((String)account.get("LE_REVENUE_RANGE"))) ||
+                 !(industryStr.equals((String)account.get("LE_INDUSTRY")))) {
+                 tagAccount(account, WRONGDU);
+             }
+        }
+    }
+
+    private GenericData.Record createDmHQ(MegatronContext context) throws Exception {
+        GenericData.Record dmHQ = context.createAccount();
+        context.addAccount(null, dmHQ);
+        return dmHQ;
+    }
+
+    private boolean domainOnly(MegatronContext context, GenericData.Record account) {
+        if (random.nextInt(5) == 1)  {
+            account.put("DUNS", null);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean dunsOnly(MegatronContext context, GenericData.Record account) {
+        if (random.nextInt(5) == 1)  {
+            account.put("Domain", null);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void addSomeDomains(MegatronContext context, GenericData.Record account, int sub) throws Exception {
+        int numDomains = random.nextInt(12);
+
+        numDomains = (numDomains <= 4 ? 0 : numDomains - 4);
+
+        String domain = (String)account.get("Domain");
+
+        for (int i = 0; i < numDomains; i++) {
+            GenericData.Record child  = context.cloneAccount(account);
+            child.put("Domain", domain + "_" +  sub + "_" + i);
+            child.put("LE_IS_PRIMARY_DOMAIN", "N");
+            context.addDomain(account, child);
+        }
+    }
+
+    public void addSomeSubs(MegatronContext context, GenericData.Record dmHQ) throws Exception {
+
+        if ((((String)dmHQ.get("LE_EMPLOYEE_RANGE")).equals("0")) || (((String)dmHQ.get("LE_EMPLOYEE_RANGE")).equals("1"))) {
+            return;
+        }
+
+        int numSubs = random.nextInt(16);
+
+        List<GenericData.Record> domains = context.getDomains(dmHQ);
+
+        for (int i = 0; i < numSubs; i++) {
+            GenericData.Record sub  = context.cloneAccountWithNewDUNS(dmHQ);
+            sub.put("LE_IS_PRIMARY_LOCATION", "N");
+            context.addAccount(dmHQ, sub);
+
+            if (dunsOnly(context, sub)) {
+                continue;
+            }
+
+            if (domains != null) {
+                for (int j = 0; j < domains.size(); j++) {
+                    GenericData.Record domain = context.cloneAccount(sub);
+                    domain.put("Domain", (domains.get(j)).get("Domain"));
+                    domain.put("LE_IS_PRIMARY_DOMAIN", "N");
+                    context.addDomain(sub, domain);
+                }
+            }
+
+            if (random.nextInt(3) == 1) {
+                addSomeDomains(context, sub, i + 1);
+            }
+        }
+    }
+
+    private void tagAccount(GenericData.Record account, int flag) {
+        int origFlag = (Integer)account.get("MockFlag");
+        account.put("MockFlag", flag | origFlag);
+    }
+
+    class MegatronContext {
+
+        private MegatronConfig config;
+        private Schema schema;
+
+        private long currentAccountId;
+
+        private int currentDuns;
+
+        private int currentDomain;
+
+        private int currentName;
+
+        private int currentGDuns;
+
+        private int flags;
+        private Random random = new Random();
+
+        Map<Long, List<GenericData.Record>> domainMap;
+
+        Map<Long, List<GenericData.Record>> hqMap;
+
+        List<GenericData.Record> accounts;
+
+        public MegatronContext(MegatronConfig config, Schema schema) {
+            this.config = config;
+            this.schema = schema;
+
+            Integer flags = config.getFlags();
+
+            if (flags == null) {
+                this.flags = ORPHAN | WRONGDU | MISSINGPD | DUPPD;
+            } else {
+                this.flags = flags;
+            }
+
+            hqMap = new HashMap<Long, List<GenericData.Record>>();
+            domainMap = new HashMap<Long, List<GenericData.Record>>();
+            accounts = new ArrayList<GenericData.Record>();
+
+            currentAccountId = 0;
+            currentDuns = 0;
+            currentDomain = 0;
+            currentGDuns = 0;
+            currentName = 0;
+        }
+
+        public boolean checkFlag(int flag) {
+            return ((flags & flag) != 0);
+        }
+
+        public Schema getSchema() {
+            return schema;
+        }
+
+        public MegatronConfig getConfig() {
+            return config;
+        }
+
+        public int getNumAccounts() {
+            return accounts.size();
+        }
+
+        public void addAccount(GenericData.Record parent, GenericData.Record child) {
+
+            if (parent != null) {
+                Long pid = (Long)parent.get("LatticeID");
+                List<GenericData.Record> childList = hqMap.get(parent);
+                if (childList == null) {
+                    childList = new ArrayList<GenericData.Record>();
+                    hqMap.put(pid, childList);
+                }
+                childList.add(child);
+            }
+
+            accounts.add(child);
+        }
+
+        public void addDomain(GenericData.Record parent, GenericData.Record child) {
+
+            Long pid = (Long)parent.get("LatticeID");
+            List<GenericData.Record> childList = domainMap.get(pid);
+            if (childList == null) {
+                childList = new ArrayList<GenericData.Record>();
+                domainMap.put(pid, childList);
+            }
+            childList.add(child);
+
+            accounts.add(child);
+        }
+
+        public List<GenericData.Record>  getAllAccounts() {
+            return accounts;
+
+        }
+
+        public Map<Long, List<GenericData.Record>> getAllDomain() {
+            return domainMap;
+        }
+
+        public List<GenericData.Record> getChildren(GenericData.Record dmHQ) {
+            Long pid = (Long)dmHQ.get("LatticeID");
+            List<GenericData.Record> childList = domainMap.get(pid);
+            return childList;
+        }
+
+        public List<GenericData.Record> getDomains(GenericData.Record account) {
+            Long pid = (Long)account.get("LatticeID");
+            return domainMap.get(pid);
+        }
+
+        public GenericData.Record createAccount() throws Exception {
+            if (getNumAccounts() >= config.getNumAccounts()) {
+                throw new Exception("Enough accounts");
+            }
+
+            GenericData.Record account = new GenericData.Record(schema);
+
+            boolean sameGlobalDuns = (currentDuns == 0) ? false : ((random.nextInt(2) == 0) ? true : false); 
+
+
+            if (!sameGlobalDuns) {
+                currentGDuns = currentDuns;
+            }
+ 
+            account.put("LatticeID", currentAccountId);
+
+            account.put("LE_PRIMARY_DUNS", currentDuns + "");
+            account.put("DUNS", currentDuns + "");
+            account.put("GLOBAL_ULTIMATE_DUNS_NUMBER", currentGDuns +"");
+
+            account.put("Domain", "Domain" + currentDomain);
+            account.put("LE_IS_PRIMARY_DOMAIN", "Y");
+
+            account.put("LE_IS_PRIMARY_LOCATION", "Y");
+            account.put("Name", currentName + "");
+
+            account.put("LE_COUNTRY", "USA");
+            account.put("ZipCode", random.nextInt(4096) + "");
+            account.put("Street", random.nextInt(4096) + "");
+            account.put("City", random.nextInt(4096) + "");
+            String state;
+            switch (random.nextInt(6)) {
+                case 0:
+                    state = "NY";
+                    break;
+                case 1:
+                    state = "NH";
+                    break;
+                default:
+                    state = "CA";
+            }
+            account.put("State", state);
+            account.put("Country", "USA");
+            account.put("LE_COMPANY_PHONE", random.nextInt(4096) + "");
+
+            int employees = random.nextInt(8);
+            employees = (employees < 4) ? 1 : (employees - 3);
+            account.put("LE_EMPLOYEE_RANGE", employees + "");
+            int locations = (employees == 1) ? 1 : random.nextInt(16);
+            account.put("LE_NUMBER_OF_LOCATIONS", ((locations < 4) ? 1 : (locations - 3)));
+            account.put("LE_REVENUE_RANGE", random.nextInt(16) + ""); 
+
+            account.put("LE_INDUSTRY", random.nextInt(16) + "");
+
+            account.put("LE_COMPANY_DESCRIPTION", "What So ever");
+            account.put("LE_SIC_CODE", random.nextInt(4096) + "");
+            account.put("LE_NAICS_CODE", + random.nextInt(4096) + "");
+            account.put("LE_Last_Upload_Date", System.currentTimeMillis());
+
+            account.put("MockFlag", 0);
+
+            currentAccountId++;
+            currentDomain++;
+            currentDuns++;
+            currentName++;
+
+            return account;
+        }
+
+       public GenericData.Record cloneAccount(GenericData.Record other) throws Exception {
+            if (getNumAccounts() >= config.getNumAccounts()) {
+                throw new Exception("Enough accounts");
+            }
+
+            GenericData.Record account = new GenericData.Record(other, false);
+            account.put("LatticeID", currentAccountId++);
+            return account;
+       }
+
+       public GenericData.Record cloneAccountWithNewDUNS(GenericData.Record other) throws Exception  {
+            if (getNumAccounts() >= config.getNumAccounts()) {
+                throw new Exception("Enough accounts");
+            }
+
+            GenericData.Record account = new GenericData.Record(other, false);
+            account.put("LatticeID", currentAccountId++);
+            account.put("DUNS", currentDuns + "");
+            currentDuns++;
+            return account;
+       }
+   }
+}
--- le-datacloud/match/src/main/java/com/latticeengines/datacloud/match/service/impl/DnBMatchResultValidatorImpl.java
+++ le-datacloud/match/src/main/java/com/latticeengines/datacloud/match/service/impl/DnBMatchResultValidatorImpl.java
@@ -1,8 +1,20 @@
 package com.latticeengines.datacloud.match.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import com.latticeengines.common.exposed.util.JsonUtils;
+import com.latticeengines.datacloud.match.dnb.DnBMatchContext;
+import com.latticeengines.datacloud.match.dnb.DnBMatchGrade;
+import com.latticeengines.datacloud.match.dnb.DnBReturnCode;
 import com.latticeengines.datacloud.match.service.DnBMatchResultValidator;
 import com.latticeengines.domain.exposed.datacloud.dnb.DnBMatchContext;
 import com.latticeengines.domain.exposed.datacloud.dnb.DnBMatchGrade;
@@ -11,35 +23,231 @@ import com.latticeengines.domain.exposed.datacloud.dnb.DnBReturnCode;
 @Component("dnbMatchResultValidatorImpl")
 public class DnBMatchResultValidatorImpl implements DnBMatchResultValidator {
 
-    @Value("${datacloud.dnb.confidencecode.threshold}")
-    private int confidenceCodeThreshold;
+    private static final Log log = LogFactory.getLog(DnBMatchResultValidatorImpl.class);
+
+    @Value("${datacloud.dnb.matchcriteria:default}")
+    private String matchCriteriaJson;
+
+    private DnBMatchCriteria matchCriteria;
+
+    @PostConstruct
+    public void loadMatchCriteria() {
+
+        try {
+            if (!matchCriteriaJson.equals("default")) {
+                matchCriteria = JsonUtils.deserialize(matchCriteriaJson, DnBMatchCriteria.class);
+            }
+        } catch (Exception e) {
+            matchCriteria = null;
+        }
+
+        if (matchCriteria == null) {
+
+            matchCriteria = new DnBMatchCriteria();
+
+            DnBMatchBucket bucket;
+
+            bucket  = new DnBMatchBucket();
+            bucket.setLowerConfidence(8);
+            bucket.setUpperConfidence(10);
+            bucket.addRule("*", "*", "*", "*");
+            matchCriteria.addBucket(bucket);
+
+            bucket  = new DnBMatchBucket();
+            bucket.setLowerConfidence(6);
+            bucket.setUpperConfidence(7);
+            bucket.addRule("A", "*", "*", "A");
+            bucket.addRule("A", "ABZ", "ABZ", "BZ");
+            matchCriteria.addBucket(bucket);
+
+            bucket  = new DnBMatchBucket();
+            bucket.setLowerConfidence(4);
+            bucket.setUpperConfidence(5);
+            bucket.addRule("A", "A", "AB", "ABZ");
+            bucket.addRule("A", "AB", "A", "ABZ");
+            bucket.addRule("B", "A", "A", "ABZ");
+            matchCriteria.addBucket(bucket);
+        }
+
+        log.info("Dnb Match Criteria: " + JsonUtils.serialize(matchCriteria));
+    }
 
     public boolean validate(DnBMatchContext res) {
-        if (discardConfidenceCode(res.getConfidenceCode()) && discardMatchGrade(res.getMatchGrade())) {
+
+        if (res.getDnbCode() != DnBReturnCode.OK) {
+            return true;
+        }
+
+        if (!matchCriteria.accept(res.getConfidenceCode(), res.getMatchGrade())) {
             res.setDnbCode(DnBReturnCode.DISCARD);
             return false;
         }
         return true;
     }
 
-    private boolean discardConfidenceCode(Integer confidenceCode) {
-        return confidenceCode != null && confidenceCode < confidenceCodeThreshold;
+    public static class DnBMatchCriteria {
+
+        @JsonProperty("Buckets")
+        List<DnBMatchBucket> buckets;
+
+        public boolean accept(Integer confidence, DnBMatchGrade matchGrade) {
+            if ((confidence == null) || (matchGrade == null) || (buckets == null)) {
+                return false;
+            }
+            for (DnBMatchBucket bucket : buckets) {
+                if (bucket.accept(confidence, matchGrade)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public List<DnBMatchBucket> getBuckets() {
+            return buckets;
+        }
+
+        public void setBuckets(List<DnBMatchBucket> buckets) {
+            this.buckets = buckets;
+        }
+
+        public void addBucket(DnBMatchBucket bucket) {
+            if (buckets == null) {
+                buckets = new ArrayList<DnBMatchBucket>();
+            }
+            buckets.add(bucket);
+        }
     }
 
-    // There are 2 kinds of DnBMatchGrade: length = 7 and length = 11
-    // DnBMatchGrade with length = 7 does not involve zipcode
-    private boolean discardMatchGrade(DnBMatchGrade matchGrade) {
-        if (matchGrade == null) {
+    public static class DnBMatchBucket {
+
+        @JsonProperty("LowerConfidence")
+        private Integer lowerConfidence;
+
+        @JsonProperty("UpperConfidence")
+        private Integer upperConfidence;
+
+        @JsonProperty("Rules")
+        private List<DnBMatchRule> rules;
+
+        public void setUpperConfidence(Integer upperConfidence) {
+            this.upperConfidence = upperConfidence;
+        }
+
+        public Integer getUpperConfidence() {
+            return upperConfidence;
+        }
+
+        public void setLowerConfidence(Integer lowerConfidence) {
+            this.lowerConfidence = lowerConfidence;
+        }
+
+        public Integer getLowerConfidence() {
+            return lowerConfidence;
+        }
+
+        public List<DnBMatchRule> getRules() {
+            return rules;
+        }
+
+        public void setRules(List<DnBMatchRule> rules) {
+            this.rules = rules;
+        }
+
+        public void addRule(String name, String city, String state, String zip) {
+            DnBMatchRule rule = new DnBMatchRule();
+            rule.setName(name);
+            rule.setCity(city);
+            rule.setState(state);
+            rule.setZip(zip);
+            if (rules == null) {
+                rules = new ArrayList<DnBMatchRule>();
+            }
+            rules.add(rule);
+        }
+
+        public boolean accept(Integer confidence, DnBMatchGrade matchGrade) {
+            if ((confidence < lowerConfidence) || (confidence > upperConfidence)) {
+                return false;
+            } else {
+                if (rules == null) {
+                    return false;
+                }
+                for (DnBMatchRule rule : rules) {
+                    if (rule.accept(matchGrade)) {
+                        return true;
+                    }
+                }
+            }
             return false;
         }
-        if ((matchGrade.getNameCode() != null && "A".equals(matchGrade.getNameCode())) //
-                && ("A".equals(matchGrade.getCityCode()) || "Z".equals(matchGrade.getCityCode())) //
-                && ("A".equals(matchGrade.getStateCode()) || "Z".equals(matchGrade.getStateCode())) //
-                && ("A".equals(matchGrade.getPhoneCode()) || "Z".equals(matchGrade.getPhoneCode())) //
-                && (matchGrade.getZipCodeCode() == null || "A".equals(matchGrade.getZipCodeCode())
-                        || "Z".equals(matchGrade.getZipCodeCode()))) {
-            return false;
-        }
-        return true;
     }
+
+    public static class DnBMatchRule {
+
+        @JsonProperty("Name")
+        private String name;
+
+        @JsonProperty("City")
+        private String city;
+
+        @JsonProperty("State")
+        private String state;
+
+        @JsonProperty("Zip")
+        private String zip;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public String getState() {
+            return state;
+        }
+
+        public void setState(String state) {
+            this.state = state;
+        }
+
+        public String getZip() {
+            return zip;
+        }
+
+        public void setZip(String zip) {
+            this.zip = zip;
+        }
+
+
+        public boolean accept(DnBMatchGrade matchGrade) {
+            return (accept(name, matchGrade.getNameCode()) &&
+                    accept(city, matchGrade.getCityCode()) &&
+                    accept(state, matchGrade.getStateCode()) &&
+                    accept(zip, matchGrade.getZipCodeCode()));
+        }
+
+        private boolean accept(String rule, String grade) {
+             if (rule.equals("*")) {
+                 return true;
+             } else {
+                 grade = (grade == null) ? "Z" : grade;
+                 if (rule.contains(grade)) {
+                     return true;
+
+                 }
+             }
+             return false;
+        }
+    }
+
 }
