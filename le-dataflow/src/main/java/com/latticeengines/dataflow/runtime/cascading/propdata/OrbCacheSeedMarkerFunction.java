58c1020a17e6f026e7243e95dc047d1f18db2c2e
akumar <akumar@479f5059-62a3-4c39-9342-9852e3a68435>
2017-01-28 01:15:06 +0000

    Review request 7915 - refactor OrbCacheSeedMarkerFunction to simplify logic

--- le-dataflow/src/main/java/com/latticeengines/dataflow/runtime/cascading/propdata/OrbCacheSeedMarkerFunction.java
+++ le-dataflow/src/main/java/com/latticeengines/dataflow/runtime/cascading/propdata/OrbCacheSeedMarkerFunction.java
@@ -1,6 +1,5 @@
 package com.latticeengines.dataflow.runtime.cascading.propdata;
 
-import java.util.Iterator;
 import java.util.List;
 
 import cascading.flow.FlowProcess;
@@ -26,28 +25,17 @@ public class OrbCacheSeedMarkerFunction extends BaseOperation implements Functio
     @Override
     public void operate(FlowProcess flowProcess, FunctionCall functionCall) {
         TupleEntry arguments = functionCall.getArguments();
-        Tuple tuple = arguments.getTupleCopy();
-        Fields fields = arguments.getFields();
         Tuple result = Tuple.size(1);
-
-        @SuppressWarnings("unchecked")
-        Iterator<Object> itr = fields.iterator();
-        int pos = 0;
-
         boolean isSecondaryOrEmail = false;
 
-        while (itr.hasNext() && !isSecondaryOrEmail) {
-            String field = (String) itr.next();
-            Boolean value = tuple.getBoolean(pos++);
-            for (String fieldToCheck : fieldsToCheck) {
-                if (field.equals(fieldToCheck) //
-                        && value != null //
-                        && value == Boolean.TRUE) {
-                    isSecondaryOrEmail = true;
-                    break;
-                }
+        for (String field : fieldsToCheck) {
+            Boolean value = arguments.getBoolean(field);
+            if (value == Boolean.TRUE) {
+                isSecondaryOrEmail = true;
+                break;
             }
         }
+
         result.setBoolean(0, isSecondaryOrEmail);
         functionCall.getOutputCollector().add(result);
     }
