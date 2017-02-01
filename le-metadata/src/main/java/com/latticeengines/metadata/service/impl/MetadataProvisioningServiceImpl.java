f27609200d22965a03feaf15f3d4f8456ee1d47d
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2017-02-01 20:04:51 +0000

    Review Request 7957: Add a registry to le-metadata for import tables

--- le-metadata/src/main/java/com/latticeengines/metadata/service/impl/MetadataProvisioningServiceImpl.java
+++ le-metadata/src/main/java/com/latticeengines/metadata/service/impl/MetadataProvisioningServiceImpl.java
@@ -1,17 +1,19 @@
 package com.latticeengines.metadata.service.impl;
 
-import java.io.File;
-import java.net.URL;
-import java.net.URLDecoder;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.util.List;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.latticeengines.common.exposed.util.JsonUtils;
 import com.latticeengines.domain.exposed.camille.CustomerSpace;
 import com.latticeengines.domain.exposed.metadata.Table;
@@ -34,20 +36,49 @@ public class MetadataProvisioningServiceImpl implements MetadataProvisioningServ
 
     @Override
     public void provisionImportTables(CustomerSpace space) {
-        try {
-            URL url = getClass().getClassLoader().getResource("Tables");
-            File tablesDir = new File(URLDecoder.decode(url.getPath(), "UTF-8"));
-            File[] files = tablesDir.listFiles();
-            for (File file : files) {
-                String str = FileUtils.readFileToString(file);
-                Table table = JsonUtils.deserialize(str, Table.class);
-                Tenant tenant = tenantService.findByTenantId(space.toString());
-                table.setTenant(tenant);
-                table.setTableType(TableType.IMPORTTABLE);
 
-                DateTime date = new DateTime();
-                table.getLastModifiedKey().setLastModifiedTimestamp(date.minusYears(2).getMillis());
-                mdService.createTable(space, table);
+        try {
+            ClassLoader classLoader = getClass().getClassLoader();
+            InputStream tableRegistryStream = classLoader.getResourceAsStream("Tables/tables.json");
+            
+            String fileContents = StreamUtils.copyToString(tableRegistryStream, Charset.defaultCharset());
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode json;
+            try {
+                json = mapper.readValue(fileContents, JsonNode.class);
+            } catch (IOException e) {
+                // ignore
+                log.error("Error loading import tables.", e);
+                return;
+            }
+
+            JsonNode sourceTypes = json.get("source_types");
+            if (!sourceTypes.isArray()) {
+                throw new RuntimeException("source_types element must be an array.");
+            }
+
+            for (JsonNode sourceType : sourceTypes) {
+                String s = sourceType.get("name").textValue();
+                
+                JsonNode tables = sourceType.get("tables");
+                
+                if (!tables.isArray()) {
+                    throw new RuntimeException("tables element must be an array.");
+                }
+                
+                for (JsonNode jsonTable : tables) {
+                    String path = String.format("Tables/%s/%s.json", s, jsonTable.get("name").textValue());
+                    InputStream is = classLoader.getResourceAsStream(path);
+                    String tableContents = StreamUtils.copyToString(is, Charset.defaultCharset());
+                    Table table = JsonUtils.deserialize(tableContents, Table.class);
+                    Tenant tenant = tenantService.findByTenantId(space.toString());
+                    table.setTenant(tenant);
+                    table.setTableType(TableType.IMPORTTABLE);
+
+                    DateTime date = new DateTime();
+                    table.getLastModifiedKey().setLastModifiedTimestamp(date.minusYears(2).getMillis());
+                    mdService.createTable(space, table);
+                }
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
