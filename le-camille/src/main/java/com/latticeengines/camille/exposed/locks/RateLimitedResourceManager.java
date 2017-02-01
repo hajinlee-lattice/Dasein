a82d5754924073792f493e108d29abcb05e55d48
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-02-01 18:11:56 +0000

    Review Request 7836 a minor change to rate limited resource manager

--- le-camille/src/main/java/com/latticeengines/camille/exposed/locks/RateLimitedResourceManager.java
+++ le-camille/src/main/java/com/latticeengines/camille/exposed/locks/RateLimitedResourceManager.java
@@ -55,7 +55,7 @@ public class RateLimitedResourceManager {
     public static RateLimitedAcquisition acquire(String resourceName, Map<String, Long> quantities, long duration,
             TimeUnit timeUnit) {
         localMode.set(false);
-        
+
         if (!definitions.containsKey(resourceName)) {
             log.warn("The resource " + resourceName + " is not registered.");
             return RateLimitedAcquisition.disallowed()
