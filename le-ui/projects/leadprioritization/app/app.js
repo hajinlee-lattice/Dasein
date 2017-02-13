a77a88fdfeb2c41a605b25fc5871dad575df1915
JHadden <JHadden@479f5059-62a3-4c39-9342-9852e3a68435>
2017-02-13 04:46:27 +0000

    Review Request 8175 - Updates to ABCD Bucketing

--- le-ui/projects/leadprioritization/app/app.js
+++ le-ui/projects/leadprioritization/app/app.js
@@ -56,6 +56,50 @@ var mainApp = angular.module('mainApp', [
       $scope.tenantName = window.escape(Tenant.DisplayName);
     }
 
+    // Get IE or Edge browser version
+    var version = detectIE();
+
+    if (version === false) {
+      document.getElementById('body').classList.add('not-ie');
+    } else if (version >= 12) {
+      document.getElementById('body').classList.add('edge-' + version);
+    } else {
+      document.getElementById('body').classList.add('ie-' + version);
+    }
+
+    // add details to debug result
+    console.log(window.navigator.userAgent);
+
+    /**
+     * detect IE
+     * returns version of IE or false, if browser is not Internet Explorer
+     */
+    function detectIE() {
+      var ua = window.navigator.userAgent;
+
+      var msie = ua.indexOf('MSIE ');
+      if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+      }
+
+      var trident = ua.indexOf('Trident/');
+      if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+      }
+
+      var edge = ua.indexOf('Edge/');
+      if (edge > 0) {
+        // Edge (IE 12+) => return version number
+        return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+      }
+
+      // other browser
+      return false;
+    }
+
 })
 // adds Authorization token to $http requests to access API
 .factory('authInterceptor', function ($rootScope, $q, BrowserStorageUtility) {
--- le-ui/projects/leadprioritization/app/models/controllers/ModelRatingsController.js
+++ le-ui/projects/leadprioritization/app/models/controllers/ModelRatingsController.js
@@ -50,32 +50,41 @@ angular.module('lp.models.ratings', [
         });
     }
 
-    // Render Chart
     function renderChart(){
         
-        // Define height of chart
-        vm.chartContainerHeight = Math.round(12*vm.summary.bar_lifts[0] + 10);
+        var verticalAxis = document.getElementById("verticalAxis");
+
+        // Get tallest bar in set
+        vm.largestLiftInSet = Math.max.apply(Math,vm.summary.bar_lifts.map(function(o){return o;}));
+
+        // Set height of chart components based off tallest bar
+        vm.relativeHeightOfTallest = Math.round((12*vm.largestLiftInSet) + 10);
+        if(vm.relativeHeightOfTallest < 150){
+            vm.chartContainerHeight = Math.round((25*vm.largestLiftInSet) + 10);
+            vm.barMultiplier = 25;
+            console.log("small");
+        } else {
+            vm.chartContainerHeight = Math.round((15*vm.largestLiftInSet) + 10);
+            vm.barMultiplier = 15;
+            console.log("large");
+        }
 
         // Define height of dugout
         vm.dugoutHeight = vm.chartContainerHeight - 8;
-
-        // Get highest lift value and round to next integer up
-        function getMaxLift(arr, prop) {
-            var max;
-            for (var i=0 ; i<arr.length ; i++) {
-                if (!max || parseInt(arr[i][prop]) > parseInt(max[prop]))
-                    max = arr[i];
-            }
-            return max;
-        }
-        var maxLift = getMaxLift(vm.currentConfiguration, "lift");
         
         // Create vertical axis based on maxLift
-        vm.yAxisNumber = Math.round(maxLift.lift);
+        vm.yAxisNumber = Math.round(vm.largestLiftInSet);
+        if(vm.yAxisNumber >= 3 && vm.yAxisNumber <= 10) {
+            verticalAxis.classList.add('reduceSmall');
+        } else if (vm.yAxisNumber > 10) {
+            verticalAxis.classList.add('reduceBig');
+        }
+
         vm.getNumber = function(num) {return new Array(num);}
         vm.axisItemHeight = vm.chartContainerHeight / vm.yAxisNumber;
 
-            
+        console.log(vm.largestLiftInSet, vm.yAxisNumber, 25*vm.largestLiftInSet+10, vm.chartContainerHeight); 
+ 
     }
 
     function initSliders(){
@@ -161,12 +170,28 @@ angular.module('lp.models.ratings', [
         $rootScope.$broadcast('model-details', { displayName: Model.ModelDetails.DisplayName });
         $scope.Math = window.Math;
 
-        console.log(vm.historicalBuckets);
+        vm.getModelJobNumber = vm.model.ModelDetails.ModelSummaryProvenanceProperties[5].ModelSummaryProvenanceProperty.value;
+        // console.log(vm.model.ModelDetails.ModelSummaryProvenanceProperties[5].ModelSummaryProvenanceProperty);
+        console.log($stateParams);
 
         if(vm.model.EventTableProvenance.SourceSchemaInterpretation === "SalesforceLead"){
             vm.modelType = "Leads";
         } else {
             vm.modelType = "Accounts";
+        }
+
+        // Set value for total leads in set
+        // This will need to get changed when we're saving configurations
+        vm.historyTotalLeads = pluckDeepKey("num_leads", vm.historicalBuckets);
+
+        // Add values for a specific key in object
+        function pluckDeepKey(key, obj) {
+          if (_.has(obj, key)) {
+            return obj[key];
+          }
+          return _.reduce(_.flatten(_.map(obj, function(v) {
+            return _.isObject(v) ? pluckDeepKey(key, v) : [];
+          }), false), function(a,b) { return a + b });
         }
 
     };
