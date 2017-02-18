b13e41703d9c505928b1c69360d57899106f2f9c
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2017-02-18 05:34:39 +0000

    Review Request 8251: add some logging

--- le-serviceflows/leadprioritization/src/main/java/com/latticeengines/leadprioritization/dataflow/AddStandardAttributes.java
+++ le-serviceflows/leadprioritization/src/main/java/com/latticeengines/leadprioritization/dataflow/AddStandardAttributes.java
@@ -48,21 +48,26 @@ public class AddStandardAttributes extends TypesafeDataFlowBuilder<AddStandardAt
 
     private void fixTransformArgumentsAndMetadata(Node eventTable, List<TransformDefinition> definitions) {
         fixStdLengthDomainArgs(eventTable, definitions.stream()
-                .filter(a -> a.output == TransformationPipeline.stdLengthDomain.output).findFirst().orElse(null));
+                .filter(a -> a.output.equals(TransformationPipeline.stdLengthDomain.output)).findFirst().orElse(null));
         fixStdVisidbDsIndustryGroupArgs(eventTable,
-                definitions.stream().filter(a -> a.output == TransformationPipeline.stdVisidbDsIndustryGroup.output)
+                definitions.stream()
+                        .filter(a -> a.output.equals(TransformationPipeline.stdVisidbDsIndustryGroup.output))
                         .findFirst().orElse(null));
     }
 
     private void fixStdLengthDomainArgs(Node eventTable, TransformDefinition domainLength) {
+        log.info("Fixing Domain Length");
         if (domainLength == null) {
+            log.info("Domain Length is null");
             return;
         }
+
         Attribute websiteOrEmail = eventTable.getSourceAttribute(InterfaceName.Website);
 
         if (websiteOrEmail == null) {
             websiteOrEmail = eventTable.getSourceAttribute(InterfaceName.Email);
         }
+        log.info("websiteOrEmail is: " + websiteOrEmail != null ? websiteOrEmail.getName() : "null");
         if (websiteOrEmail != null && !domainLength.arguments.isEmpty()) {
             domainLength.arguments.put("column", websiteOrEmail.getName());
             log.info("set domain_length arguments to: " + domainLength.arguments);
