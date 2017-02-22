692996a7c681cd72b7d280ff195f32959fc6f043
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2017-02-22 23:58:53 +0000

    Review Request 8334: Add anaconda setup of legacy environment to setupenv.sh

--- le-dev/scripts/setupenv.sh
+++ le-dev/scripts/setupenv.sh
@@ -6,6 +6,7 @@ printf "%s\n" "${LE_STACK:?You must set LE_STACK to a unique value among develop
 printf "%s\n" "${HADOOP_NAMENODE_DATA_DIR:?You must set HADOOP_NAMENODE_DATA_DIR}"
 printf "%s\n" "${HADOOP_DATANODE_DATA_DIR:?You must set HADOOP_DATANODE_DATA_DIR}"
 printf "%s\n" "${DYNAMO_HOME:?You must set DYNAMO_HOME}"
+printf "%s\n" "${ANACONDA_HOME:?You must set ANACONDA_HOME}"
 
 bash $WSHOME/le-dev/start-cluster.sh || true
 
@@ -73,3 +74,26 @@ mkdir -p $DYNAMO_HOME
 pushd $DYNAMO_HOME
 echo "Installing DynamoDB to $DYNAMO_HOME"
 tar xzf $DYNAMO_ARTIFACT_DIR/dynamodb_local_latest.tar.gz 
+
+CONDA_ARTIFACT_DIR=$WSHOME/le-dev/conda/artifacts
+mkdir -p $CONDA_ARTIFACT_DIR
+if [ -f $CONDA_ARTIFACT_DIR/Anaconda2-4.3.0-Linux-x86_64.sh ]; then
+    echo "Skipping download of Anaconda"
+elif [ -f $ANACONDA_HOME ]; then
+    echo "Skipping installation of Anaconda"
+else
+    echo "Downloading Anaconda"
+    pushd $CONDA_ARTIFACT_DIR
+    wget https://repo.continuum.io/archive/Anaconda2-4.3.0-Linux-x86_64.sh
+    bash $CONDA_ARTIFACT_DIR/Anaconda2-4.3.0-Linux-x86_64.sh -b -p $ANACONDA_HOME
+    popd
+fi
+$ANACONDA_HOME/bin/conda create -n lattice -y python=2.7.13 pip
+source $ANACONDA_HOME/bin/activate lattice
+pip install avro==1.7.7 fastavro==0.7.7 kazoo==2.2.1 patsy==0.3.0 pexpect==4.0.1 psutil==2.2.1 ptyprocess==0.5.1 python-dateutil==2.4.1
+$ANACONDA_HOME/bin/conda install -y pandas=0.13.1=np18py27_0
+pip install statsmodels==0.5.0
+$ANACONDA_HOME/bin/conda install -y libiconv=1.14=0 libxml2=2.9.4=0 libxslt=1.1.28=3 lxml=3.4.0=py27_0 numpy=1.8.2=py27_1 openssl=1.0.2j=0 py=1.4.31=py27_0 pytest=2.9.2=py27_0 pytz=2016.10=py27_0 readline=6.2=2 scikit-learn=0.14.1=np18py27_1 setuptools=27.2.0=py27_0 sqlite=3.13.0=0 tk=8.5.18=0 wheel=0.29.0=py27_0 zlib=1.2.8=3
+
+ 
+
