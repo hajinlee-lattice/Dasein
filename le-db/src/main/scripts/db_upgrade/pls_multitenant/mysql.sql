12fc4b2fad1531c67a52928181d7e15be6f00ec0
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2017-02-24 04:10:23 +0000

    Review Request 8354: Create upgrade script for aws

--- le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
+++ le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
@@ -12,6 +12,11 @@ CREATE PROCEDURE `UpdateSchema`()
         alter table `METADATA_DATA_COLLECTION` add index FKF69DFD4336865BC (FK_TENANT_ID), add constraint FKF69DFD4336865BC foreign key (FK_TENANT_ID) references `TENANT` (`TENANT_PID`) on delete cascade;
         alter table `METADATA_SEGMENT` add index FK90C89E03382C8AE3 (FK_QUERY_SOURCE_ID), add constraint FK90C89E03382C8AE3 foreign key (FK_QUERY_SOURCE_ID) references `METADATA_DATA_COLLECTION` (`PID`) on delete cascade;
         alter table `SOURCE_FILE` add `ENTITY_EXTERNAL_TYPE` varchar(255);
+        
+        create table `METADATA_HIERARCHY` (`PID` bigint not null auto_increment unique, `NAME` varchar(255) not null, FK_TABLE_ID bigint not null, primary key (`PID`));
+        create table `METADATA_LEVEL` (`PID` bigint not null auto_increment unique, `ATTRIBUTES` varchar(2048) not null, `DISPLAY_NAME` varchar(255) not null, `NAME` varchar(255) not null, `ORDER_IN_HIERARCHY` integer not null, FK_HIERARCHY_ID bigint not null, primary key (`PID`));
+        alter table `METADATA_HIERARCHY` add index FK603219055FC50F27 (FK_TABLE_ID), add constraint FK603219055FC50F27 foreign key (FK_TABLE_ID) references `METADATA_TABLE` (`PID`) on delete cascade;
+        alter table `METADATA_LEVEL` add index FK41081254F80C45B8 (FK_HIERARCHY_ID), add constraint FK41081254F80C45B8 foreign key (FK_HIERARCHY_ID) references `METADATA_HIERARCHY` (`PID`) on delete cascade;
      COMMIT;
   END;
 
