6eb1c65abbde0228f52e22f8149dfdea3640e6c3
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-02-25 16:36:07 +0000

    Review Request 8382 simplify retain once more, enforce retain and sort

--- le-dataflow/src/main/java/com/latticeengines/dataflow/exposed/builder/operations/RetainOperation.java
+++ le-dataflow/src/main/java/com/latticeengines/dataflow/exposed/builder/operations/RetainOperation.java
@@ -5,9 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import com.latticeengines.dataflow.exposed.builder.common.FieldList;
 import com.latticeengines.dataflow.exposed.builder.util.DataFlowUtils;
 import com.latticeengines.domain.exposed.dataflow.FieldMetadata;
@@ -19,26 +16,10 @@ import cascading.pipe.assembly.Retain;
 
 public class RetainOperation extends Operation {
 
-    private static final Log log = LogFactory.getLog(RetainOperation.class);
-
     public RetainOperation(Input input, FieldList retainedFields) {
         Pipe prior = input.pipe;
         Map<String, FieldMetadata> fmMap = new HashMap<>();
-
-        boolean needRetain = false;
-        for (String field: retainedFields.getFields()) {
-            if (!fmMap.containsKey(field)) {
-                needRetain = true;
-                break;
-            }
-        }
-        Pipe retain = prior;
-        if (needRetain) {
-            retain = new Retain(retain, DataFlowUtils.convertToFields(retainedFields.getFields()));
-        } else {
-            log.info("The fields in the node is already the same as declared fields. No need to retain.");
-        }
-
+        Pipe retain = new Retain(prior, DataFlowUtils.convertToFields(retainedFields.getFields()));
         for (FieldMetadata fm: input.metadata) {
             fmMap.put(fm.getFieldName(), fm);
         }
