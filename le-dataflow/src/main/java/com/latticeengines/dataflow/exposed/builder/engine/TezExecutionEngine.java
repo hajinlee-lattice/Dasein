2afe206cbc7930037334c6ce5721f7f1b3159271
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-02-28 01:08:35 +0000

    Review Request 8417 minor touch on prod hive config

--- le-dataflow/src/main/java/com/latticeengines/dataflow/exposed/builder/engine/TezExecutionEngine.java
+++ le-dataflow/src/main/java/com/latticeengines/dataflow/exposed/builder/engine/TezExecutionEngine.java
@@ -2,6 +2,7 @@ package com.latticeengines.dataflow.exposed.builder.engine;
 
 import java.util.Properties;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -32,12 +33,12 @@ public class TezExecutionEngine extends ExecutionEngine {
     }
 
     private Properties updateTezRuntime(Properties properties) {
-        if (properties.containsKey("tez.task.resource.memory.mb")
-                && !properties.containsKey("tez.runtime.io.sort.mb")) {
+        if (StringUtils.isNotBlank(properties.getProperty("tez.task.resource.memory.mb"))
+                && StringUtils.isBlank(properties.getProperty("tez.runtime.io.sort.mb"))) {
             long taskmb = Long.valueOf(properties.getProperty("tez.task.resource.memory.mb"));
             long sortmb = Math.min(Math.round(taskmb * 0.5), 2048);
             properties.put("tez.runtime.io.sort.mb", String.valueOf(sortmb));
-            log.info("Automatically set tez.runtime.io.sort.mb=" + properties.get("tez.runtime.io.sort.mb"));
+            log.info("Automatically set tez.runtime.io.sort.mb = " + properties.get("tez.runtime.io.sort.mb"));
         }
         return properties;
     }
