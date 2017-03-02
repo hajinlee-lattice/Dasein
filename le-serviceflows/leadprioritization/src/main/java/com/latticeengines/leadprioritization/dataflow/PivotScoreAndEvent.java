1fb8af3c26e764390a9434ed10e6f17a8ce77711
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2017-03-02 21:05:12 +0000

    Review Request 8512: update to remove deprecation

--- le-serviceflows/leadprioritization/src/main/java/com/latticeengines/leadprioritization/dataflow/PivotScoreAndEvent.java
+++ le-serviceflows/leadprioritization/src/main/java/com/latticeengines/leadprioritization/dataflow/PivotScoreAndEvent.java
@@ -21,8 +21,8 @@ public class PivotScoreAndEvent extends TypesafeDataFlowBuilder<PivotScoreAndEve
     @Override
     public Node construct(PivotScoreAndEventParameters parameters) {
         Node inputTable = addSource(parameters.getScoreOutputTableName());
-        inputTable = inputTable.addFunction(String.format("%s ? 1 : 0", InterfaceName.Event.name()), new FieldList(
-                InterfaceName.Event.name()), new FieldMetadata("IsPositiveEvent", Integer.class));
+        inputTable = inputTable.apply(String.format("%s ? 1 : 0", InterfaceName.Event.name()),
+                new FieldList(InterfaceName.Event.name()), new FieldMetadata("IsPositiveEvent", Integer.class));
 
         List<Aggregation> aggregations = new ArrayList<>();
         aggregations.add(new Aggregation("IsPositiveEvent", "TotalPositiveEvents", AggregationType.SUM));
@@ -30,14 +30,14 @@ public class PivotScoreAndEvent extends TypesafeDataFlowBuilder<PivotScoreAndEve
                 .add(new Aggregation(ScoreResultField.Percentile.displayName, "TotalEvents", AggregationType.COUNT));
         Node aggregatedNode = inputTable.groupBy(new FieldList(ScoreResultField.Percentile.displayName), aggregations);
 
-        aggregatedNode = aggregatedNode.addFunction("TotalEvents == 0 ? 0 : TotalPositiveEvents / TotalEvents",
+        aggregatedNode = aggregatedNode.apply("TotalEvents == 0 ? 0 : TotalPositiveEvents / TotalEvents",
                 new FieldList("TotalPositiveEvents", "TotalEvents"), new FieldMetadata("ConversionRate", Double.class));
 
         double modelAvgProbability = parameters.getModelAvgProbability();
 
         String expression = String.format("%s / %f", "ConversionRate", modelAvgProbability);
-        aggregatedNode = aggregatedNode.addFunction(expression, new FieldList("ConversionRate"), new FieldMetadata(
-                "Lift", Double.class));
+        aggregatedNode = aggregatedNode.apply(expression, new FieldList("ConversionRate"),
+                new FieldMetadata("Lift", Double.class));
         return aggregatedNode;
 
     }
