16aa0b513d2b75036917f70adc0063855adc2df1
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-03-03 15:54:34 +0000

    Review Request 8420 add create datafabric pod into setup zk.sh

--- le-dev/scripts/setupzk.sh
+++ le-dev/scripts/setupzk.sh
@@ -9,9 +9,12 @@ PYTHON=${PYTHON:=python}
 ZK_HOST="localhost:2181"
 LOCAL_TEST_ROOT="/Pods/Default/Contracts/LocalTest"
 LOCAL_TEST_POD="/Pods/Default/Contracts"
+DATA_FABRIC_POD="/Pods/FabricConnectors"
 
 $PYTHON -c "import zc.zk; zk = zc.zk.ZooKeeper('${ZK_HOST}'); print zk.delete_recursive('${LOCAL_TEST_ROOT}') if zk.exists('${LOCAL_TEST_ROOT}') else '${LOCAL_TEST_ROOT} is empty'; 
 print 'LOCAL_TEST_POD exists' if zk.exists('${LOCAL_TEST_POD}') else zk.create_recursive('${LOCAL_TEST_POD}', '', zc.zk.OPEN_ACL_UNSAFE)"
+
+$PYTHON -c "import zc.zk; zk = zc.zk.ZooKeeper('${ZK_HOST}');  print '{DATA_FABRIC_POD} exists' if zk.exists('${DATA_FABRIC_POD}') else zk.create_recursive('${DATA_FABRIC_POD}', '', zc.zk.OPEN_ACL_UNSAFE)"
 
 zookeeper_import ${ZK_HOST} $WSHOME/le-dev/testartifacts/zookeeper/LocalTest.txt ${LOCAL_TEST_POD}
 
