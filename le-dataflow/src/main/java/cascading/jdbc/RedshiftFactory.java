830c7149decec4266d6bbe126399f073451ed156
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2017-03-03 20:37:00 +0000

    Review Request 8522: export flow using cascading

---
+++ le-dataflow/src/main/java/cascading/jdbc/RedshiftFactory.java
@@ -0,0 +1,269 @@
+/*
+ * Copyright (c) 2007-2015 Concurrent, Inc. All Rights Reserved.
+ *
+ * Project and contact information: http://www.cascading.org/
+ *
+ * This file is part of the Cascading project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cascading.jdbc;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import cascading.scheme.Scheme;
+import cascading.tap.SinkMode;
+import cascading.tap.Tap;
+import cascading.tuple.Fields;
+import cascading.util.Util;
+
+/**
+ * The {@link RedshiftFactory} is a factory class to create {@link RedshiftTap}s
+ * and {@link RedshiftScheme}s. The class is meant to be used by <a
+ * href="http://www.cascading.org/lingual/">lingual</a> for dynamically creating
+ * Taps and Schemes, so that redshift can be used as a <a
+ * href="http://docs.cascading.org/lingual/1.0/#_creating_a_data_provider"
+ * >provider</a> within lingual.
+ */
+public class RedshiftFactory extends JDBCFactory
+  {
+
+  private static final Logger LOG = LoggerFactory.getLogger( RedshiftFactory.class );
+
+  /** environment variable for the aws access key */
+  private static final String SYSTEM_AWS_ACCESS_KEY = "AWS_ACCESS_KEY";
+
+  /** environment variable for the aws secret key */
+  private static final String SYSTEM_AWS_SECRET_KEY = "AWS_SECRET_KEY";
+
+  public static final String PROTOCOL_S3_OUTPUT_PATH = "s3outputpath";
+  public static final String PROTOCOL_AWS_ACCESS_KEY = "awsacceskey";
+  public static final String PROTOCOL_AWS_SECRET_KEY = "awssecretkey";
+  ;
+  public static final String PROTOCOL_KEEP_DEBUG_HFS_DATA = "keepdebughfsdata";
+  public static final String PROTOCOL_USE_DIRECT_INSERT = "usedirectinsert";
+
+  public static final String FORMAT_DISTRIBUTION_KEY = "distributionkey";
+  public static final String FORMAT_SORT_KEYS = "sortkeys";
+  public static final String FORMAT_COPY_OPTIONS_PREFIX = "copyoptions.";
+  public static final String FORMAT_FIELD_DELIMITER = "fielddelimiter";
+  public static final String FORMAT_QUOTE_CHARACTER = "quotecharacter";
+
+  @SuppressWarnings("unused")
+  public String getDescription()
+    {
+    return getClass().getSimpleName();
+    }
+
+  @SuppressWarnings("rawtypes")
+  public Scheme createScheme( String format, Fields fields, Properties formatProperties )
+    {
+    LOG.info( String.format("creating RedshiftScheme for format %s with fields %s and properties %s", format, fields, formatProperties ));
+
+    String delimiter = formatProperties.getProperty( FORMAT_FIELD_DELIMITER, RedshiftScheme.DEFAULT_DELIMITER );
+    String quoteCharacter = formatProperties.getProperty( FORMAT_QUOTE_CHARACTER, RedshiftScheme.DEFAULT_QUOTE );
+
+    RedshiftTableDesc redshiftTableDesc = createTableDescFromProperties( fields, formatProperties, true );
+
+    Map<CopyOption, String> copyOptions = extractCopyOptions( formatProperties, FORMAT_COPY_OPTIONS_PREFIX );
+
+    boolean tableAlias = getTableAlias( formatProperties );
+
+    return new RedshiftScheme( fields, redshiftTableDesc, delimiter, quoteCharacter, copyOptions, tableAlias );
+    }
+
+  @SuppressWarnings("rawtypes")
+  public Tap createTap( String protocol, Scheme scheme, String identifier, SinkMode sinkMode, Properties protocolProperties )
+    {
+    LOG.info( "creating RedshiftTap with properties {} in mode {}", protocolProperties, sinkMode );
+
+    String jdbcUserProperty = protocolProperties.getProperty( PROTOCOL_JDBC_USER );
+    String jdbcPasswordProperty = protocolProperties.getProperty( PROTOCOL_JDBC_PASSWORD );
+
+    String jdbcUser = null;
+    if( !Util.isEmpty( jdbcUserProperty ) )
+      jdbcUser = jdbcUserProperty;
+
+    String jdbcPassword = null;
+    if( !Util.isEmpty( jdbcPasswordProperty ) )
+      jdbcPassword = jdbcPasswordProperty;
+
+    String hfsStagingDir = protocolProperties.getProperty( PROTOCOL_S3_OUTPUT_PATH, "/tmp" );
+
+    AWSCredentials credentials = determineAwsCredentials( protocolProperties );
+
+    boolean keepDebugHdfsData = Boolean.parseBoolean( protocolProperties.getProperty( PROTOCOL_KEEP_DEBUG_HFS_DATA ) );
+    boolean useDirectInsert = Boolean.parseBoolean( protocolProperties.getProperty( PROTOCOL_USE_DIRECT_INSERT, "true" ) );
+
+    // source fields will be the JDBC-typed fields so use them as defaults.
+    RedshiftTableDesc redshiftTableDesc = createTableDescFromProperties( scheme.getSourceFields(), protocolProperties, false );
+
+    Fields sinkFields = scheme.getSinkFields();
+    if( !redshiftTableDesc.hasRequiredTableInformation() && sinkFields != Fields.UNKNOWN && sinkFields != Fields.ALL && sinkFields != null
+      && sinkFields.getTypes() != null )
+      {
+      LOG.debug( "tabledesc information incomplete, falling back to sink-fields {}", scheme.getSinkFields() );
+      redshiftTableDesc.completeFromFields( scheme.getSinkFields() );
+      ( (JDBCScheme) scheme ).setColumns( redshiftTableDesc.getColumnNames() );
+      }
+
+    // users can overwrite the sink mode.
+    String sinkModeProperty = protocolProperties.getProperty( PROTOCOL_SINK_MODE );
+    if( !Util.isEmpty( sinkModeProperty ) )
+      sinkMode = SinkMode.valueOf( sinkModeProperty );
+
+    return new RedshiftTap( identifier, jdbcUser, jdbcPassword, null, hfsStagingDir, credentials, redshiftTableDesc, (RedshiftScheme) scheme, sinkMode, keepDebugHdfsData, useDirectInsert );
+    }
+
+  private RedshiftTableDesc createTableDescFromProperties( Fields fields, Properties properties, boolean allowNullName )
+    {
+    String tableName = properties.getProperty( PROTOCOL_TABLE_NAME, null );
+
+    if( !allowNullName )
+      if( Util.isEmpty( tableName ) )
+        throw new IllegalArgumentException( "no tablename given" );
+
+    String separator = properties.getProperty( PROTOCOL_FIELD_SEPARATOR, DEFAULT_SEPARATOR );
+
+    String[] columnNames = getColumnNames( fields, properties, separator );
+
+    String[] columnDefs = null;
+    String columnDefsProperty = properties.getProperty( PROTOCOL_COLUMN_DEFS, null );
+    if( !Util.isEmpty( columnDefsProperty ) )
+      columnDefs = columnDefsProperty.split( separator );
+
+    String distributionKey = properties.getProperty( FORMAT_DISTRIBUTION_KEY );
+
+    String[] sortKeys = null;
+    if( properties.containsKey( FORMAT_SORT_KEYS ) )
+      sortKeys = properties.getProperty( FORMAT_SORT_KEYS ).split( DEFAULT_SEPARATOR );
+
+    RedshiftTableDesc desc = new RedshiftTableDesc( tableName, columnNames, columnDefs, distributionKey, sortKeys );
+    return desc;
+    }
+
+  /**
+   * Helper method that tries to determine the AWS credentials. It first tries
+   * the {@link Properties} passed in, next it checks for the environment
+   * variables <code>AWS_ACCESS_KEY</code> and <code>AWS_SECRET_KEY</code>. If
+   * none of the above contains the credentials, the method returns
+   * {@link AWSCredentials}.
+   *
+   * @param properties a {@link Properties} object, which can contain the AWS
+   *                   credentials.
+   * @return an {@link AWSCredentials} installed.
+   */
+  private AWSCredentials determineAwsCredentials( Properties properties )
+    {
+    // try to determine the aws credentials starting with the assumption
+    // that they are available from the AWS environment
+    AWSCredentials awsCredentials = AWSCredentials.RUNTIME_DETERMINED;
+
+    // first try the properties
+    String awsAccessKey = properties.getProperty( PROTOCOL_AWS_ACCESS_KEY );
+    String awsSecretKey = properties.getProperty( PROTOCOL_AWS_SECRET_KEY );
+
+    if( !Util.isEmpty( awsAccessKey ) && !Util.isEmpty( awsSecretKey ) )
+      awsCredentials = new AWSCredentials( awsAccessKey, awsSecretKey );
+
+    // next try environment variables
+    if( awsCredentials == AWSCredentials.RUNTIME_DETERMINED )
+      {
+      awsAccessKey = System.getenv( SYSTEM_AWS_ACCESS_KEY );
+      awsSecretKey = System.getenv( SYSTEM_AWS_SECRET_KEY );
+      if( !Util.isEmpty( awsAccessKey ) && !Util.isEmpty( awsSecretKey ) )
+        awsCredentials = new AWSCredentials( awsAccessKey, awsSecretKey );
+      }
+    return awsCredentials;
+    }
+
+  public static Map<CopyOption, String> extractCopyOptions( Properties properties, String copyOptionsPrefix )
+    {
+    Map<CopyOption, String> copyOptions = new HashMap<CopyOption, String>();
+    for( CopyOption curOption : CopyOption.values() )
+      {
+      String propConfName = copyOptionsPrefix + curOption.toString();
+      if( properties.containsKey( propConfName ) )
+        {
+        String propValue = properties.get( propConfName ) != null ? properties.get( propConfName ).toString() : null;
+        copyOptions.put( curOption, propValue );
+        }
+      }
+    return copyOptions;
+    }
+
+  /** Enum of all the COPY options supported by the Redshift load command and information about how to covert them to SQL commands. */
+  public static enum CopyOption
+    {
+      FIXEDWIDTH( "\'%s\'" ),
+      DELIMITER( "\'%s\'" ),
+      CSV( " QUOTE \'%s\' " ),
+      ENCRYPTED,
+      GZIP,
+      LZOP,
+      FORMAT("%s"),
+      REMOVEQUOTES,
+      EXPLICIT_IDS,
+      ACCEPTINVCHARS( "\'%s\'" ),
+      MAXERROR( "%s" ),
+      DATEFORMAT( "\'%s\'" ),
+      TIMEFORMAT( "\'%s\'" ),
+      IGNOREHEADER( "%s" ),
+      ACCEPTANYDATE,
+      IGNOREBLANKLINES,
+      TRUNCATECOLUMNS,
+      FILLRECORD,
+      TRIMBLANKS,
+      NOLOAD,
+      NULL( "\'%s\'" ),
+      EMPTYASNULL,
+      BLANKSASNULL,
+      COMPROWS( "%s" ),
+      COMPUPDATE( "%s" ),
+      STATUPDATE( "%s" ),
+      ESCAPE,
+      ROUNDEC;
+
+    private String formattableCommandString;
+
+    CopyOption( String formattableCommandString )
+      {
+      this.formattableCommandString = formattableCommandString;
+      }
+
+    CopyOption()
+      {
+      this.formattableCommandString = "";
+      }
+
+    public String getArguments( String argument )
+      {
+      if( this.equals( CSV ) && argument == null )
+        return " CSV ";
+
+      if( formattableCommandString.length() == 0 || argument == null )
+        return formattableCommandString;
+
+      return String.format( formattableCommandString + " ", argument );
+      }
+
+    }
+
+
+  }
---
+++ le-dataflow/src/main/java/cascading/jdbc/RedshiftTap.java
@@ -0,0 +1,258 @@
+/*
+ * Copyright (c) 2007-2015 Concurrent, Inc. All Rights Reserved.
+ *
+ * Project and contact information: http://www.cascading.org/
+ *
+ * This file is part of the Cascading project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cascading.jdbc;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.RecordReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import cascading.flow.FlowProcess;
+import cascading.jdbc.db.DBConfiguration;
+import cascading.scheme.Scheme;
+import cascading.tap.SinkMode;
+import cascading.tap.Tap;
+import cascading.tap.hadoop.Hfs;
+import cascading.tuple.TupleEntryCollector;
+
+/**
+ * This class and {@link RedshiftScheme} manage the ability to read and write
+ * data to Amazon's Redshift via EMR. Because Redshift data is loaded into
+ * Redshift via S3 but read from it via JDBC both these classes wrap the pairing
+ * of an HFS {@link Tap} (for writing) and a JDBC {@link Tap} for reading behind
+ * one {@link cascading.scheme.Scheme} object.
+ */
+public class RedshiftTap extends JDBCTap {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RedshiftTap.class);
+
+    public static final String DB_DRIVER = "org.postgresql.Driver";
+
+    private AWSCredentials awsCredentials;
+    private RedshiftScheme redshiftScheme;
+    private Hfs hfsStagingDir;
+    private String s3WorkingDir;
+    private boolean keepDebugHfsData;
+    private boolean useDirectInsert;
+
+    /**
+     * Redshift tap to stage data to S3 and then issue a JDBC COPY command to
+     * specified Redshift table
+     *
+     * @param sinkMode
+     *            use {@link SinkMode#REPLACE} to drop Redshift table before
+     *            loading; {@link SinkMode#UPDATE} to not drop table for
+     *            incremental loading
+     */
+    public RedshiftTap(String connectionUrl, String username, String password, String hfsStagingDir,
+            AWSCredentials awsCredentials, RedshiftTableDesc redshiftTableDesc, RedshiftScheme redshiftScheme,
+            SinkMode sinkMode, boolean keepDebugHfsData, boolean useDirectInsert) {
+        super(connectionUrl, username, password, DB_DRIVER, redshiftTableDesc, redshiftScheme, sinkMode);
+        this.redshiftScheme = redshiftScheme;
+        String workingDirPath = hfsStagingDir + "/" + UUID.randomUUID();
+        this.s3WorkingDir = workingDirPath.replaceAll("s3n://", "s3://");
+        this.hfsStagingDir = new Hfs(redshiftScheme.getTextDelimited(), workingDirPath);
+        this.awsCredentials = awsCredentials;
+        this.keepDebugHfsData = keepDebugHfsData;
+        this.useDirectInsert = useDirectInsert;
+        LOG.info("created {} ", toString());
+    }
+
+    public RedshiftTap(String connectionUrl, String username, String password,
+            Scheme<Configuration, RecordReader, OutputCollector, ?, ?> scheme, String hfsStagingDir,
+            AWSCredentials awsCredentials, RedshiftTableDesc redshiftTableDesc,
+            RedshiftScheme redshiftScheme, SinkMode sinkMode, boolean keepDebugHfsData, boolean useDirectInsert) {
+        this(connectionUrl, username, password, hfsStagingDir, awsCredentials, redshiftTableDesc, redshiftScheme,
+                sinkMode, keepDebugHfsData, useDirectInsert);
+
+        String workingDirPath = hfsStagingDir + "/" + UUID.randomUUID();
+        this.s3WorkingDir = workingDirPath.replaceAll("s3n://", "s3://");
+        this.hfsStagingDir = new Hfs(scheme, workingDirPath);
+    }
+
+    /**
+     * Redshift tap to stage data to S3 and then issue a JDBC COPY command to
+     * specified Redshift table
+     *
+     * @param sinkMode
+     *            use {@link SinkMode#REPLACE} to drop Redshift table before
+     *            loading; {@link SinkMode#UPDATE} to not drop table for
+     *            incremental loading
+     */
+    public RedshiftTap(String connectionUrl, String username, String password, String hfsStagingDir,
+            AWSCredentials awsCredentials, RedshiftTableDesc redshiftTableDesc, RedshiftScheme redshiftScheme,
+            SinkMode sinkMode) {
+        this(connectionUrl, username, password, hfsStagingDir, awsCredentials, redshiftTableDesc, redshiftScheme,
+                sinkMode, false, true);
+    }
+
+    /**
+     * Simplified constructor for testing
+     */
+    protected RedshiftTap(String connectionUrl, RedshiftTableDesc redshiftTableDesc, RedshiftScheme redshiftScheme,
+            SinkMode sinkMode) {
+        this(connectionUrl, null, null, null, null, redshiftTableDesc, redshiftScheme, sinkMode, false, true);
+    }
+
+    /**
+     * Simplified constructor for testing
+     */
+    protected RedshiftTap(String connectionUrl, RedshiftScheme redshiftScheme) {
+        this(connectionUrl, null, null, null, null, null, redshiftScheme, null, false, true);
+    }
+
+    @Override
+    public void sourceConfInit(FlowProcess<? extends Configuration> process, Configuration configuration) {
+        if (username == null)
+            DBConfiguration.configureDB(configuration, driverClassName, connectionUrl);
+        else
+            DBConfiguration.configureDB(configuration, driverClassName, connectionUrl, username, password);
+
+        super.sourceConfInit(process, configuration);
+    }
+
+    @Override
+    public void sinkConfInit(FlowProcess<? extends Configuration> process, Configuration conf) {
+        if (!useDirectInsert) {
+            // if we haven't set the credentials beforehand try to set them from
+            // the job conf
+            if (awsCredentials.equals(AWSCredentials.RUNTIME_DETERMINED)) {
+                String accessKey = conf.get("fs.s3n.awsAccessKeyId", null);
+                String secretKey = conf.get("fs.s3n.awsSecretAccessKey", null);
+                awsCredentials = new AWSCredentials(accessKey, secretKey);
+            }
+            // make the credentials to be used available to the JobConf if they
+            // were set differently
+            conf.set("fs.s3n.awsAccessKeyId", awsCredentials.getAwsAccessKey());
+            conf.set("fs.s3n.awsSecretAccessKey", awsCredentials.getAwsSecretKey());
+        }
+        super.sinkConfInit(process, conf);
+    }
+
+    @Override
+    public TupleEntryCollector openForWrite(FlowProcess<? extends Configuration> flowProcess,
+            OutputCollector outputCollector) throws IOException {
+        // force a table creation if one does not exist
+        LOG.info("creating db table: " + getTableName());
+        super.createResource(flowProcess);
+        if (useDirectInsert) {
+            return super.openForWrite(flowProcess, outputCollector);
+        } else {
+            LOG.info("Creating scratch dir: " + hfsStagingDir.getIdentifier());
+            hfsStagingDir.createResource(flowProcess);
+            return hfsStagingDir.openForWrite(flowProcess);
+        }
+    }
+
+    @Override
+    public boolean createResource(Configuration configuration) throws IOException {
+        LOG.info("creating resources");
+        boolean createSuccess = true;
+        if (!useDirectInsert) {
+            LOG.info("creating hfs scratch space: {}", hfsStagingDir.getIdentifier());
+            createSuccess = hfsStagingDir.createResource(configuration);
+        }
+        if (createSuccess) {
+            LOG.info("creating DB table: {}", super.getIdentifier());
+            createSuccess = super.createResource(configuration);
+        }
+        return createSuccess;
+    }
+
+    @Override
+    public boolean deleteResource(Configuration configuration) throws IOException {
+        LOG.info("deleting resources");
+        boolean deleteSuccsess;
+        LOG.info("deleting DB table: {}", super.getIdentifier());
+        deleteSuccsess = super.deleteResource(configuration);
+        if (deleteSuccsess && hfsStagingDir.resourceExists(configuration)) {
+            LOG.info("deleting hfs scratch space: {}", hfsStagingDir.getIdentifier());
+            deleteSuccsess = hfsStagingDir.deleteResource(configuration);
+        }
+        return deleteSuccsess;
+    }
+
+    @Override
+    public boolean commitResource(Configuration configuration) throws IOException {
+        if (!useDirectInsert) {
+            String copyCommand = buildCopyFromS3Command();
+            try {
+                int results = super.executeUpdate(copyCommand);
+                if (results != 0)
+                    LOG.info("Copy return code: {} ( expected: 0 )", results);
+            } finally {
+                // clean scratch resources even if load failed.
+                if (!keepDebugHfsData && hfsStagingDir.resourceExists(configuration))
+                    hfsStagingDir.deleteResource(configuration);
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public long getModifiedTime(Configuration configuration) throws IOException {
+        if (hfsStagingDir.resourceExists(configuration))
+            return hfsStagingDir.getModifiedTime(configuration);
+        return super.getModifiedTime(configuration);
+    }
+
+    public boolean isUseDirectInsert() {
+        return useDirectInsert;
+    }
+
+    public String buildCopyFromS3Command() {
+        return String.format("COPY %s from '%s' %s %s ;", redshiftScheme.getRedshiftTableDesc().getTableName(),
+                s3WorkingDir, buildAuthenticationOptions(), buildCopyOptions());
+    }
+
+    protected String buildAuthenticationOptions() {
+        return String.format(" CREDENTIALS 'aws_access_key_id=%s;aws_secret_access_key=%s' ",
+                awsCredentials.getAwsAccessKey(), awsCredentials.getAwsSecretKey());
+    }
+
+    private String buildCopyOptions() {
+        StringBuilder builder = new StringBuilder();
+        for (Map.Entry<RedshiftFactory.CopyOption, String> copyOption : redshiftScheme.getCopyOptions().entrySet()) {
+            builder.append(" ");
+            if (copyOption.getValue() == null)
+                builder.append(copyOption.getKey().toString());
+            else
+                builder.append(copyOption.getKey().toString()).append(" ")
+                        .append(copyOption.getKey().getArguments(copyOption.getValue()));
+        }
+        return builder.toString();
+    }
+
+    @Override
+    public String toString() {
+        if (getIdentifier() != null)
+            return getClass().getSimpleName() + "[\"" + getScheme() + "\"]" + "[->\"" + hfsStagingDir.getIdentifier()
+                    + "\"->\"" + super.getIdentifier() + "\"]"; // sanitize
+        else
+            return getClass().getSimpleName() + "[\"" + getScheme() + "\"]" + "[no more info]";
+    }
+
+}
