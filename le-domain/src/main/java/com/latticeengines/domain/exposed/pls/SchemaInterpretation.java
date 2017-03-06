cd4c979393f6842a5acfad760382958486711140
bross <bross@479f5059-62a3-4c39-9342-9852e3a68435>
2017-03-06 20:22:50 +0000

    Review Request 8576:  Add BucketedAccountMaster to SchemaInterpretation

--- le-domain/src/main/java/com/latticeengines/domain/exposed/pls/SchemaInterpretation.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/pls/SchemaInterpretation.java
@@ -1,11 +1,12 @@
 package com.latticeengines.domain.exposed.pls;
 
 public enum SchemaInterpretation {
-    SalesforceAccount, 
-    SalesforceLead, 
-    TestingData, 
-    Account, 
-    Contact, 
-    TimeSeries, 
-    Category
+    SalesforceAccount,
+    SalesforceLead,
+    TestingData,
+    Account,
+    Contact,
+    TimeSeries,
+    Category,
+    BucketedAccountMaster
 }
