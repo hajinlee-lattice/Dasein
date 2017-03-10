Review Request 8666: Fix precision calculation for floats between -1.0 and 1.0

le-dataplatform/src/main/python/leframework/util/precisionutil.py
@@ -14,8 +14,9 @@ class PrecisionUtil(object):
             return x
         if type(x) == int:
             return x
-        e = np.int64(np.log10(np.fabs(x)))
-        n = precision if e < 0 else precision - 1
+        l = np.log10(np.fabs(x))
+        e = np.int64(l)
+        n = precision if l < 0.0 else precision - 1
         m = round(float(x) / 10**e, n)
         return m*10**e
 

le-dataplatform/src/test/python/precisiontest.py
@@ -32,10 +32,13 @@ class PrecisionTest(TestCase):
             np.float64(-2.71828182846e15), \
             np.float64(2.71828182846e-15), \
             0.00057615364, \
-            24756 \
+            24756, \
+            0.3333333333333, \
+            3.33e-1 \
         ]
 
-        valuesAtPrecision3 = [3.14, -3.14, 2.72e15, -2.72e15, 2.72e-15, 5.76e-4, 2.48e4]
+        valuesAtPrecision3 = [3.14, -3.14, 2.72e15, -2.72e15, 2.72e-15, 5.76e-4, 2.48e4, 0.333, 0.333]
+        print valuesAtPrecision3
 
         vs = pd.DataFrame(data=[{'value':v} for v in valuesToTest])
         vss = vs[['value']].apply(lambda x : PrecisionUtil.setPrecision(x, 3))