faa4c332f8a1c9ceb6eb19fb3d9d9fbdde6b3588
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2017-03-13 18:41:48 +0000

    Review Request 8725: PLS-2390: Update outdated null value imputation code

--- le-dataplatform/src/main/python/configurablepipelinetransformsfromfile/imputationstep.py
+++ le-dataplatform/src/main/python/configurablepipelinetransformsfromfile/imputationstep.py
@@ -1,13 +1,13 @@
 from collections import OrderedDict
-
 import json
+from math import sqrt, fabs
 import os
-import pandas as pd
-import random as rd
 
+import pandas as pd
 from pipelinefwk import PipelineStep
-from pipelinefwk import get_logger
 from pipelinefwk import create_column
+from pipelinefwk import get_logger
+
 
 logger = get_logger("pipeline")
 
@@ -34,7 +34,14 @@ class ImputationStep(PipelineStep):
     def getRTSArtifacts(self):
         return [("imputations.json", self.imputationFilePath)]
 
+    def getDebugArtifacts(self):
+        return [{"imputationstep-imputationvalues.json": self.imputationValues}]
+
     def transform(self, dataFrame, configMetadata, test):
+        if self.targetColumn not in dataFrame.columns.values:
+            logger.error('ImputationStep: targetCol does not exist in the dataFrame: {}'.format(str(self.targetColumn)))
+            return dataFrame
+    
         if len(self.imputationValues) == 0:
             self.imputationValues = self.__computeImputationValues(dataFrame)
             self.__writeRTSArtifacts()
@@ -51,112 +58,176 @@ class ImputationStep(PipelineStep):
             json.dump(self.imputationValues, fp)
             self.imputationFilePath = os.path.abspath(fp.name)
 
-    def __getNullValues(self, dataFrame):
-        nullValues = {}
-        outputFrame = dataFrame
-
-        if len(self.columns) > 0:
-            for column in self.columns:
-                if column in outputFrame:
-                    nullCount = self.__getIsNullColumn(outputFrame[column])
-                    nullValues[column] = nullCount
-        return nullValues
-
-    def __getIsNullColumn(self, dataColumn):
-        nullCount = 0
-
-        for i in range(len(dataColumn)):
-            if pd.isnull(dataColumn[i]):
-                nullCount = nullCount + 1
-        return nullCount
-
     def __computeImputationValues(self, dataFrame):
-        nullValues = self.__getNullValues(dataFrame)
         outputFrame = dataFrame
         imputationValues = {}
+        eventVector = outputFrame[self.targetColumn].tolist()
         if len(self.columns) > 0:
-            expectedLabelValue = 0.0
-            if self.targetColumn in outputFrame:
-                expectedLabelValue = self.__getExpectedLabelValue(outputFrame, self.targetColumn)
-
             for column, value in self.columns.iteritems():
                 if column in outputFrame:
                     try:
-                        if nullValues[column] > 0:
-                            imputationBins = self.__createBins(outputFrame[column], outputFrame[self.targetColumn])
-                            imputationValue = self.__matchValue(expectedLabelValue, imputationBins)
-                            imputationValues[column] = imputationValue
-                        else:
-                            imputationValues[column] = value
+                        columnData = outputFrame[column].tolist()
+                        imputationValues[column] = self.__imputeMissingNumValue(columnData, eventVector)
                     except KeyError:
                         imputationValues[column] = value
         return imputationValues
 
-    def __getExpectedLabelValue(self, dataFrame, targetColumn):
-        zeroLabels = (dataFrame[targetColumn] == 0).sum()
-        oneLabels = (dataFrame[targetColumn] == 1).sum()
-        expectedLabelValue = float(oneLabels) / (oneLabels + zeroLabels)
-        return expectedLabelValue
+    # get list of ordered indices for x
+    def __orderedIndicies(self, x, descending=False):
+        return [y[0] for y in sorted(enumerate(x), key=lambda x: x[1], reverse=descending)]
 
-    def __createIndexSequence(self, number, rawSplits):
-        if number == 0:
-            return []
+    # this creates and populates mean values in each bin
+    def __meanBin(self, a, idxbin):
+        return sum(a[idxbin[0]:idxbin[1]]) * 1.0 / (idxbin[1] - idxbin[0])
+        
+    def __sgnFn(self, x):
+        if x == 0:
+            return x
+        else:
+            return (1 if x > 0 else (-1))
+        
+    # this returns the matched x value for the bin corresponding to the input y value
+    def __matchValue(self, xList_notNull, eventList_notNull, binIndex, matchingRate):
 
-        binSize = int(number / (rawSplits + 1) / 1.0)
-        numBins = int(number / binSize)
-        sp = [binSize * i for i in range(numBins + 1)]
-        sp[numBins] = number
-        return tuple(sp)
+        xListMean = [self.__meanBin(xList_notNull, (binIndex[i - 1], binIndex[i])) for i in range(1, len(binIndex))]
+        eventListMean = [self.__meanBin(eventList_notNull, (binIndex[i - 1], binIndex[i])) for i in range(1, len(binIndex))]
 
-    def __meanValuePair(self, x, tupleSize=2):
-        def mean(k):  return sum([float(y[k]) for y in x]) / len(x) / 1.0
-        if tupleSize > 1: return [mean(k) for k in range(tupleSize)]
-        return [sum(x) / 1.0 / len(x)]
-
-    def __createBins(self, x, y, numBins=20):
-        if len(x) != len(y):
-            print "Warning: Number of records and number of labels are different."
-
-        pairs = []
-        numberOfPoints = min(len(x), len(y))
-        for i in range(numberOfPoints):
-            if pd.isnull(x[i]) == False:
-                pairs.append([x[i], y[i]])
-
-        if numBins > len(pairs) -1 and len(pairs) != 0:
-            numBins = len(pairs)-1
-
-        indBins = self.__createIndexSequence(len(pairs), numBins)
-        def mvPair(k):
-            return self.__meanValuePair([pairs[i] for i in range(indBins[k], indBins[k+1])])
-        return pd.Series([mvPair(b) for b in range(len(indBins)-1)])
-
-    def __matchValue(self, yvalue, binPairs):
-        def absFn(x): return( x if x > 0 else (-x))
-        def sgnFn(x):
-            if x == 0:
-                return x
+        if len(xListMean) == 1:
+            if eventListMean[0] > matchingRate:
+                return min(xList_notNull) - 1
             else:
-                return (1 if x > 0 else (-1))
+                return max(xList_notNull) + 1
+        
+        direction = self.__sgnFn(eventListMean[-1] - eventListMean[0])
+        if min(eventListMean) > matchingRate:
+            if direction == 1:
+                return min(xList_notNull[binIndex[0]:binIndex[1]]) - 1
+            else:
+                return max(xList_notNull[binIndex[-2]:binIndex[-1]]) + 1
+        elif max(eventListMean) < matchingRate:
+            if direction == 1:
+                return max(xList_notNull[binIndex[-2]:binIndex[-1]]) + 1
+            else:
+                return min(xList_notNull[binIndex[0]:binIndex[1]]) - 1
+        else:
+            matches = [fabs(matchingRate - p) for p in eventListMean]
+            ind = [i for i, p in enumerate(matches) if p == min(matches)]
+            return xListMean[ind[0]]
 
-        numBins = len(binPairs)
+    def __getSig(self, sub, overall):
+        sub_cnt, sub_rate = sub
+        oa_cnt, oa_rate = overall
+        r = (sub_cnt * sub_rate + oa_cnt * oa_rate) / (sub_cnt + oa_cnt)
+        if r in [0, 1]:
+            return 0
+        else:
+            return (sub_rate - oa_rate) / (sqrt(r * (1.0 - r) * (1.0 / sub_cnt + 1.0 / oa_cnt)))
 
-        if numBins == 1:
-            return binPairs[0][0]
-        elif numBins == 0:
-            return rd.randint(10, 1000)
+    def __getTuple(self, eventCol):
+        try:
+            return (len(eventCol), sum(eventCol) * 1.0 / len(eventCol))
+        except:
+            return (len(eventCol), sum(float(x) for x in eventCol) / len(eventCol))
+           
+    def __createIndexSequenceByValue(self, xList, eventList, rawSplits=10):
+        sp_0 = [0]
+        xList_len = len(xList)
+        posCurr = 0
+        valCurr = xList[0]
+        numPtsCurr = 0    
+        divider = 3  
 
-        matches = [absFn(yvalue - p[1]) for p in binPairs]
-        ind = [i for i,x in enumerate(matches) if x == min(matches)][0]
-        valuePair = binPairs[ind]
+        stepSize = int(len(xList) / (rawSplits * divider))
+        numPtsInBin = stepSize * divider
+        
+        if stepSize == 0:
+            return [0, len(xList)]
+            
+        while posCurr < xList_len:
+            posCurr = min(posCurr + stepSize, xList_len)
+            if xList[posCurr - 1] == valCurr:
+                numPtsCurr += stepSize
+                continue
+            elif xList[posCurr - 1] > valCurr:
+                pos_temp = len([x for x in xList[posCurr - stepSize:posCurr] if x == valCurr])
+                if numPtsCurr + pos_temp >= numPtsInBin:
+                    sp_0.append(posCurr - stepSize + pos_temp)
+                    numPtsCurr = stepSize - pos_temp
+                    valCurr = xList[posCurr - stepSize + pos_temp]
+                else:
+                    numPtsCurr += stepSize
+        sp_0.append(xList_len)
+        
+        sp_0_length = len(sp_0)
+        bucket = 1
+        while bucket < sp_0_length - 1:
+            if xList[sp_0[bucket] - 1] == xList[sp_0[bucket]]:
+                xListInBucket = xList[sp_0[bucket - 1]:sp_0[bucket]]
+                sp_0[bucket] = sp_0[bucket] - len([x for x in xListInBucket if x == xList[sp_0[bucket]]])
+            bucket = bucket + 1
+        if sp_0[-1] < len(xList):
+            sp_0.append(len(xList))
 
-        splitValue = binPairs[1][0] - binPairs[0][0]
-        adjValue=0
-        if valuePair == binPairs[0]:
-            adjValue = -2.0 * splitValue
-        if valuePair == binPairs[numBins - 1]:
-            adjValue = 2.0 * splitValue
-        return valuePair[0] + adjValue
-    
-    def getDebugArtifacts(self):
-        return [{"imputationstep-imputationvalues.json": self.imputationValues}]
+        return sp_0
+
+        
+    def __createIndexSequence(self, xList, eventList, rawSplits=10, sigThreshold=2):
+
+        sp = self.__createIndexSequenceByValue(xList, eventList, rawSplits)
+
+        bucket = 0
+        while bucket < len(sp) - 2:
+            eventList1 = eventList[sp[bucket]:sp[bucket + 1]]
+            eventList2 = eventList[sp[bucket + 1]:sp[bucket + 2]]
+            if fabs(self.__getSig(self.__getTuple(eventList1), self.__getTuple(eventList2))) < sigThreshold:
+                del sp[bucket + 1]
+            else:
+                bucket += 1
+        
+        return tuple(sp)    
+
+        
+    '''
+    input: 
+        columnData: list of the numerical column
+        eventVector: list of the events
+
+    output:
+       imputedValue: the imputed value that needs to be written to the Artifact for the scoring process, when there is no missing value, this output is None
+    '''
+
+    def __imputeMissingNumValue(self, columnData, eventVector, rawBinNumber=10, sigThreshold=2):
+        
+        nullInd = [pd.isnull(x) for x in columnData]
+        nullLength = len([x for x in nullInd if x])
+        
+        xList_notmissing = [float(x[0]) for x in zip(columnData, nullInd) if not x[1]]
+        if (len(columnData) - nullLength) / rawBinNumber < 20:
+            rawBinNumber = int((len(columnData) - nullLength) / 20)
+        
+        if rawBinNumber <= 1:
+            try:
+                imputedValue = sum(xList_notmissing) * 1.0 / len(xList_notmissing)
+            except ZeroDivisionError:
+                imputedValue = 0.0
+        else:
+            idxOrdered = self.__orderedIndicies(xList_notmissing)
+            
+            xList_notmissing_ord = [xList_notmissing[i] for i in idxOrdered]
+            
+            eventList_notmissing = [x[0] for x in zip(eventVector, nullInd) if not x[1]]
+            
+            eventList_notmissing_ord = [eventList_notmissing[i] for i in idxOrdered]
+                                    
+            binIndex = self.__createIndexSequence(xList_notmissing_ord, eventList_notmissing_ord, rawBinNumber, sigThreshold)
+            
+            eventList_missing = [x[0] for x in zip(eventVector, nullInd) if x[1]]
+
+            try:
+                rate_null = sum(eventList_missing) * 1.0 / len(eventList_missing)
+            except ZeroDivisionError:
+                rate_null = sum(eventList_notmissing) * 1.0 / len(eventList_notmissing)
+
+            imputedValue = self.__matchValue(xList_notmissing_ord, eventList_notmissing_ord, binIndex, rate_null)
+            
+        return imputedValue
