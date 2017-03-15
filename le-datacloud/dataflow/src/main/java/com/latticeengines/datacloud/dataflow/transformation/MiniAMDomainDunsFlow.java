13f93ea289ea47a65c387489b864ceec079f6104
ssakhalkar <ssakhalkar@479f5059-62a3-4c39-9342-9852e3a68435>
2017-03-15 21:25:29 +0000

    Review Request 8776

---
+++ le-datacloud/dataflow/src/main/java/com/latticeengines/datacloud/dataflow/transformation/MiniAMDomainDunsFlow.java
@@ -0,0 +1,181 @@
+package com.latticeengines.datacloud.dataflow.transformation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.latticeengines.dataflow.exposed.builder.Node;
+import com.latticeengines.dataflow.exposed.builder.common.FieldList;
+import com.latticeengines.dataflow.exposed.builder.common.JoinType;
+import com.latticeengines.domain.exposed.datacloud.dataflow.TransformationFlowParameters;
+import com.latticeengines.domain.exposed.datacloud.transformation.configuration.impl.MiniAMDomainDunsConfig;
+import com.latticeengines.domain.exposed.datacloud.transformation.configuration.impl.TransformerConfig;
+import com.latticeengines.domain.exposed.dataflow.FieldMetadata;
+
+@Component("miniAMDomainDunsFlow")
+public class MiniAMDomainDunsFlow extends ConfigurableFlowBase<MiniAMDomainDunsConfig> {
+    private final static String DNB_GU_VALUE = "GuDnbValue";
+    private final static String DNB_DU_VALUE = "DuDnbValue";
+    @Override
+    public Node construct(TransformationFlowParameters parameters) {
+        List<String> miniTargetTableAttr = getMiniTargetTableAttr(parameters);
+        MiniAMDomainDunsConfig config = getTransformerConfig(parameters);
+        Node miniDomainDunsList = addSource(parameters.getBaseTables().get(0));
+        Node dnbSeedDataSet = addSource(parameters.getBaseTables().get(1));
+        List<Node> storeList = new ArrayList<Node>();
+        storeList.add(dnbSeedDataSet);
+        for (int i = 2; i < parameters.getBaseTables().size(); i++) {
+            storeList.add(addSource(parameters.getBaseTables().get(i)));
+        }
+
+        // Adding DUNS by matching with domains
+        List<Node> storeDunsValues = new ArrayList<Node>();
+        for (Node seedDataSet : storeList) {
+            Node storeDomainsMatched = miniDomainDunsList
+                    .join(config.getOutputDataSetValue(), seedDataSet, "Domain", JoinType.INNER) //
+                    .retain("DUNS") //
+                    .rename(new FieldList("DUNS"), new FieldList(config.getOutputDataSetValue())) //
+                    .addColumnWithFixedValue(config.getOutputDataSetType(), "DUNS", String.class) //
+                    .retain(new FieldList(miniDomainDunsList.getFieldNames())) //
+                    .renamePipe(seedDataSet + "duns");
+            storeDunsValues.add(storeDomainsMatched);
+        }
+
+        // Merge into target table
+        miniDomainDunsList = miniDomainDunsList.merge(storeDunsValues);
+
+        // Remove Nulls
+        String checkNullExpression = config.getOutputDataSetValue() + " != null ";
+        miniDomainDunsList = miniDomainDunsList.filter(checkNullExpression,
+                new FieldList(config.getOutputDataSetValue()));
+        // De-duplication
+        miniDomainDunsList = miniDomainDunsList.groupByAndLimit(new FieldList(miniTargetTableAttr), 1);
+
+        /*
+         * Adding DUNS from DnbDataSeed by matching with GU, DU hierarchy and
+         * storing the result into Node (Type{GU, DU}, Value{DUNS associated to
+         * GU, DU hierarchy})
+         */
+        String checkGuExpression = config.getDnbInputDataSetGU() + " != null";
+        String checkDuExpression = config.getDnbInputDataSetGU() + " == null";
+        Node storeDnbHierarchy = miniDomainDunsList
+                .join(config.getOutputDataSetValue(), dnbSeedDataSet, config.getDnbInputDataSetDuns(), JoinType.INNER)
+                .renamePipe("storeDnbGuDu");
+
+        // Dnb GU data set node
+        Node storeDnbGu = storeDnbHierarchy.retain(config.getDnbInputDataSetGU()) //
+                .apply(String.format("%s ? %s : null", checkGuExpression,
+                        config.getDnbInputDataSetGU()), new FieldList(config.getDnbInputDataSetGU()),
+                        new FieldMetadata(DNB_GU_VALUE, String.class)) //
+                .retain(DNB_GU_VALUE) //
+                .renamePipe("storeDnbGu");
+
+        // Removing nulls from Dnb GU data set node
+        String checkNullGuDuns = DNB_GU_VALUE + " != null ";
+        storeDnbGu = storeDnbGu.filter(checkNullGuDuns, new FieldList(DNB_GU_VALUE));
+
+        // Deduplication
+        storeDnbGu = storeDnbGu.groupByAndLimit(new FieldList(DNB_GU_VALUE), 1);
+
+        /*
+         * Update DU DataSet : Left Join DU dataSet with GU DataSet so that
+         * hierarchy is maintained (whatever DUNS exist under DU, if same one
+         * exist in GU then DU ones should be discarded, since GU takes
+         * precedence over DU)
+         */
+        Node storeDnbDu = storeDnbHierarchy.retain(config.getDnbInputDataSetGU(), config.getDnbInputDataSetDU()) //
+                .apply(String.format("%s ? %s : null", checkDuExpression, config.getDnbInputDataSetDU()), //
+                        new FieldList(config.getDnbInputDataSetGU(), config.getDnbInputDataSetDU()),
+                        new FieldMetadata(DNB_DU_VALUE, String.class)) //
+                .retain(DNB_DU_VALUE) //
+                .renamePipe("storeDnbDu");
+
+        // Removing nulls from Dnb DU data set node
+        String checkNullDuDuns = DNB_DU_VALUE + " != null ";
+        storeDnbDu = storeDnbDu.filter(checkNullDuDuns, new FieldList(DNB_DU_VALUE));
+
+        // Deduplication
+        storeDnbDu = storeDnbDu.groupByAndLimit(new FieldList(DNB_DU_VALUE), 1);
+
+        // Removing DUNS from DU which are already present in GU
+        String checkValInGU = DNB_GU_VALUE + " == null";
+        storeDnbDu = storeDnbDu.join(DNB_DU_VALUE, storeDnbGu, DNB_GU_VALUE, JoinType.LEFT) //
+                .filter(checkValInGU, new FieldList(DNB_GU_VALUE, DNB_DU_VALUE));
+        /*
+         * Joining the DU and GU nodes DunsValue with Dnb Seed to get the
+         * respective DUNS under DU and GU
+         */
+        List<Node> dunsFromGuDu = new ArrayList<Node>();
+        // DUNS from GuNode
+        Node guDuns = storeDnbGu.join(DNB_GU_VALUE, dnbSeedDataSet, config.getDnbInputDataSetGU(), JoinType.INNER) //
+                .retain("DUNS") //
+                .rename(new FieldList("DUNS"), new FieldList(config.getOutputDataSetValue())) //
+                .addColumnWithFixedValue(config.getOutputDataSetType(), "DUNS", String.class) //
+                .retain(new FieldList(miniDomainDunsList.getFieldNames())) //
+                .renamePipe("GuDuns");
+        dunsFromGuDu.add(guDuns);
+
+        // DUNS from DuNode
+        Node duDuns = storeDnbDu.join(DNB_DU_VALUE, dnbSeedDataSet, config.getDnbInputDataSetDU(), JoinType.INNER) //
+                .retain("DUNS") //
+                .rename(new FieldList("DUNS"), new FieldList(config.getOutputDataSetValue())) //
+                .addColumnWithFixedValue(config.getOutputDataSetType(), "DUNS", String.class) //
+                .retain(new FieldList(miniDomainDunsList.getFieldNames())) //
+                .renamePipe("DuDuns");
+        dunsFromGuDu.add(duDuns);
+
+        // Merge into target table
+        miniDomainDunsList = miniDomainDunsList.merge(dunsFromGuDu);
+
+        // Remove Nulls
+        miniDomainDunsList = miniDomainDunsList.filter(checkNullExpression,
+                new FieldList(config.getOutputDataSetValue()));
+        // De-duplication
+        miniDomainDunsList = miniDomainDunsList.groupByAndLimit(new FieldList(miniTargetTableAttr), 1);
+
+        // Adding domains by matching with DUNS
+        List<Node> storeDomainValues = new ArrayList<Node>();
+        for (Node seedDataSet : storeList) {
+            Node storeDunsMatched = miniDomainDunsList
+                    .join(config.getOutputDataSetValue(), seedDataSet, "DUNS", JoinType.INNER) //
+                    .retain("Domain") //
+                    .rename(new FieldList("Domain"), new FieldList(config.getOutputDataSetValue())) //
+                    .addColumnWithFixedValue(config.getOutputDataSetType(), "DOMAIN", String.class) //
+                    .retain(new FieldList(miniDomainDunsList.getFieldNames())) //
+                    .renamePipe(seedDataSet + "domain");
+            storeDomainValues.add(storeDunsMatched);
+        }
+
+        // Merge into target table
+        miniDomainDunsList = miniDomainDunsList.merge(storeDomainValues);
+        // De-duplication
+        miniDomainDunsList = miniDomainDunsList.groupByAndLimit(new FieldList(miniTargetTableAttr), 1);
+
+        return miniDomainDunsList;
+    }
+
+    private List<String> getMiniTargetTableAttr(TransformationFlowParameters parameters) {
+        List<String> miniTargetTableAttr = new ArrayList<String>();
+        MiniAMDomainDunsConfig config = getTransformerConfig(parameters);
+        miniTargetTableAttr.add(config.getOutputDataSetType());
+        miniTargetTableAttr.add(config.getOutputDataSetValue());
+        return miniTargetTableAttr;
+    }
+
+    @Override
+    public String getDataFlowBeanName() {
+        return "miniAMDomainDunsFlow";
+    }
+
+    @Override
+    public String getTransformerName() {
+        return "miniAMDomainDunsTransformer";
+    }
+
+    @Override
+    public Class<? extends TransformerConfig> getTransformerConfigClass() {
+        return MiniAMDomainDunsConfig.class;
+    }
+
+}
