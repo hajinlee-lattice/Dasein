e9eaced786fadee7ebd9987b5d3d4b797228abd7
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2017-03-15 19:39:11 +0000

    Review Request 8170: CDL import and stage without Lattice Account Ids

--- le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/dataflow/resolve/ResolveStagingAndRuntimeTable.java
+++ le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/dataflow/resolve/ResolveStagingAndRuntimeTable.java
@@ -16,9 +16,9 @@ import org.apache.commons.lang3.tuple.ImmutableTriple;
 import org.apache.commons.lang3.tuple.Triple;
 import org.springframework.stereotype.Component;
 
-import com.latticeengines.cdl.dataflow.TypesafeDataFlowWithResolutionBuilder;
 import com.latticeengines.common.exposed.util.AvroUtils;
 import com.latticeengines.dataflow.exposed.builder.Node;
+import com.latticeengines.dataflow.exposed.builder.TypesafeDataFlowBuilder;
 import com.latticeengines.dataflow.exposed.builder.common.DataFlowProperty;
 import com.latticeengines.dataflow.exposed.builder.common.FieldList;
 import com.latticeengines.dataflow.exposed.builder.strategy.KVAttrPicker;
@@ -32,7 +32,8 @@ import com.latticeengines.domain.exposed.metadata.Attribute;
 import com.latticeengines.domain.exposed.metadata.Table;
 
 @Component("resolveStagingAndRuntimeTable")
-public class ResolveStagingAndRuntimeTable extends TypesafeDataFlowWithResolutionBuilder<ResolveStagingAndRuntimeTableParameters> {
+public class ResolveStagingAndRuntimeTable
+        extends TypesafeDataFlowBuilder<ResolveStagingAndRuntimeTableParameters> {
 
     @SuppressWarnings("unchecked")
     @Override
@@ -41,46 +42,46 @@ public class ResolveStagingAndRuntimeTable extends TypesafeDataFlowWithResolutio
         KeyLoadStrategy keyLoadStrategy = getKeyLoadStrategy(parameters);
         String stageTableName = parameters.stageTableName;
         String runtimeTableName = parameters.runtimeTableName;
-        
+
         DataFlowContext ctx = getDataFlowCtx();
         Map<String, Table> sourceTables = ctx.getProperty(DataFlowProperty.SOURCETABLES, Map.class);
         Table stageTable = sourceTables.get(stageTableName);
         Table runtimeTable = sourceTables.get(runtimeTableName);
-        
+
         if (keyLoadStrategy == KeyLoadStrategy.Full || runtimeTable == null) {
             return addSource(stageTableName);
         }
-        
+
         Node extract = addSource(stageTableName);
         Node runtime = addSource(runtimeTableName);
-        
+
         List<String> idColumns = stageTable.getPrimaryKey().getAttributes();
-        
+
         extract = extract.kvDepivot(new FieldList(idColumns), new FieldList(idColumns));
         runtime = runtime.kvDepivot(new FieldList(idColumns), new FieldList(idColumns));
-        
+
         extract = extract.addColumnWithFixedValue("TableType", "Extract", String.class);
         runtime = runtime.addColumnWithFixedValue("TableType", "Runtime", String.class);
-        
-        
+
         Node kv = extract.merge(runtime);
         List<Triple<Node, Class<?>, Set<String>>> nodes = getFilteredAttributes(kv, stageTable, runtimeTable);
-        
+
         List<Node> picked = new ArrayList<>();
         Node firstPicked = null;
         int i = 0;
         Iterator<Triple<Node, Class<?>, Set<String>>> it = nodes.iterator();
-        
+
         Map<String, FieldMetadata> fm = new HashMap<>();
         while (it.hasNext()) {
             Triple<Node, Class<?>, Set<String>> t = it.next();
-            Node n = t.getLeft().kvPickAttr(idColumns.get(0), new AttributePicker(t.getMiddle(), fieldLoadStrategy)).discard("TableType");
+            Node n = t.getLeft().kvPickAttr(idColumns.get(0), new AttributePicker(t.getMiddle(), fieldLoadStrategy))
+                    .discard("TableType");
             if (i == 0) {
                 firstPicked = n;
             } else {
                 picked.add(n);
             }
-            
+
             for (String name : t.getRight()) {
                 if (name.equals(idColumns.get(0))) {
                     continue;
@@ -88,21 +89,22 @@ public class ResolveStagingAndRuntimeTable extends TypesafeDataFlowWithResolutio
                 if (!fm.containsKey(name)) {
                     fm.put(name, new FieldMetadata(name, t.getMiddle()));
                 }
-                
+
             }
             i++;
         }
-        
+
         kv = firstPicked.merge(picked);
         return kv.kvReconstruct(idColumns.get(0), new ArrayList<>(fm.values()));
     }
-    
-    private List<Triple<Node, Class<?>, Set<String>>> getFilteredAttributes(Node kv, Table stageTable, Table runtimeTable) {
+
+    private List<Triple<Node, Class<?>, Set<String>>> getFilteredAttributes(Node kv, Table stageTable,
+            Table runtimeTable) {
         List<Triple<Node, Class<?>, Set<String>>> filteredAttributes = new ArrayList<>();
-        
+
         Map<Class<?>, Set<String>> allFieldsPerType = getTypeToFieldNamesMap(stageTable);
         Map<Class<?>, Set<String>> runtimeFieldsPerType = getTypeToFieldNamesMap(runtimeTable);
-        
+
         for (Map.Entry<Class<?>, Set<String>> entry : runtimeFieldsPerType.entrySet()) {
             Class<?> key = entry.getKey();
             if (allFieldsPerType.containsKey(key)) {
@@ -112,7 +114,6 @@ public class ResolveStagingAndRuntimeTable extends TypesafeDataFlowWithResolutio
             }
         }
 
-        
         int i = 1;
         for (Map.Entry<Class<?>, Set<String>> entry : allFieldsPerType.entrySet()) {
             String expression = getJavaExpression(entry.getValue());
@@ -121,29 +122,29 @@ public class ResolveStagingAndRuntimeTable extends TypesafeDataFlowWithResolutio
             filteredAttributes.add(triple);
             i++;
         }
-        
+
         return filteredAttributes;
     }
-    
+
     private String getJavaExpression(Set<String> fieldNames) {
         List<String> expr = new ArrayList<>();
-        
+
         for (String fieldName : fieldNames) {
             expr.add(String.format("%s.equals(\"%s\")", KVDepivotStrategy.KEY_ATTR, fieldName));
         }
-        
+
         return StringUtils.join(expr, " || ");
     }
-    
+
     private Map<Class<?>, Set<String>> getTypeToFieldNamesMap(Table table) {
         Map<Class<?>, Set<String>> map = new HashMap<>();
-        
+
         for (Attribute field : table.getAttributes()) {
             String type = field.getPhysicalDataType();
             Class<?> javaType = AvroUtils.getJavaType(Type.valueOf(type.toUpperCase()));
-            
+
             Set<String> fieldNames = map.get(javaType);
-            
+
             if (fieldNames == null) {
                 fieldNames = new HashSet<>();
             }
@@ -152,25 +153,25 @@ public class ResolveStagingAndRuntimeTable extends TypesafeDataFlowWithResolutio
         }
         return map;
     }
-    
+
     private FieldLoadStrategy getFieldLoadStrategy(ResolveStagingAndRuntimeTableParameters parameters) {
         FieldLoadStrategy fieldLoadStrategy = parameters.fieldLoadStrategy;
-        
+
         if (fieldLoadStrategy == null) {
             return FieldLoadStrategy.Update;
         }
         return fieldLoadStrategy;
     }
-    
+
     private KeyLoadStrategy getKeyLoadStrategy(ResolveStagingAndRuntimeTableParameters parameters) {
         KeyLoadStrategy keyLoadStrategy = parameters.keyLoadStrategy;
-        
+
         if (keyLoadStrategy == null) {
             return KeyLoadStrategy.Incremental;
         }
         return keyLoadStrategy;
     }
-    
+
     private static class AttributePicker implements KVAttrPicker {
         private static final long serialVersionUID = 1L;
 
@@ -179,7 +180,6 @@ public class ResolveStagingAndRuntimeTable extends TypesafeDataFlowWithResolutio
         private String valClzName;
 
         private FieldLoadStrategy fieldLoadStrategy;
-
 
         public AttributePicker(Class<?> valClz, FieldLoadStrategy fieldLoadStrategy) {
             valClzName = valClz.getSimpleName();
@@ -198,11 +198,11 @@ public class ResolveStagingAndRuntimeTable extends TypesafeDataFlowWithResolutio
 
         @Override
         public Object updateHelpAndReturnValue(Object oldValue, Map<String, Object> oldHelp, Object newValue,
-                                        Map<String, Object> newHelp) {
-            
+                Map<String, Object> newHelp) {
+
             String oldSource = (String) oldHelp.get("TableType");
             String newSource = (String) newHelp.get("TableType");
-            
+
             boolean updateWithNewValue = false;
             if (oldSource == null) {
                 updateWithNewValue = true;
@@ -227,5 +227,4 @@ public class ResolveStagingAndRuntimeTable extends TypesafeDataFlowWithResolutio
             }
         }
     }
-    }
-
+}
