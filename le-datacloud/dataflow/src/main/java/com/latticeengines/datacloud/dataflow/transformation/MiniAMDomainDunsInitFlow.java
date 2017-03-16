741eb5a34a0634ae38378af2a2eaa84ea7b00fc2
ssakhalkar <ssakhalkar@479f5059-62a3-4c39-9342-9852e3a68435>
2017-03-16 23:15:06 +0000

    Review Request 8776

--- le-datacloud/dataflow/src/main/java/com/latticeengines/datacloud/dataflow/transformation/MiniAMDomainDunsInitFlow.java
+++ le-datacloud/dataflow/src/main/java/com/latticeengines/datacloud/dataflow/transformation/MiniAMDomainDunsInitFlow.java
@@ -7,31 +7,53 @@ import org.springframework.stereotype.Component;
 
 import com.latticeengines.dataflow.exposed.builder.Node;
 import com.latticeengines.dataflow.exposed.builder.common.FieldList;
-import com.latticeengines.dataflow.runtime.cascading.propdata.MiniAMSeedMergeDomainDuns;
 import com.latticeengines.domain.exposed.datacloud.dataflow.TransformationFlowParameters;
 import com.latticeengines.domain.exposed.datacloud.transformation.configuration.impl.MiniAMDomainDunsInitConfig;
 import com.latticeengines.domain.exposed.datacloud.transformation.configuration.impl.TransformerConfig;
 import com.latticeengines.domain.exposed.dataflow.FieldMetadata;
 
-import cascading.tuple.Fields;
-
 @Component("miniAMDomainDunsInitFlow")
 public class MiniAMDomainDunsInitFlow extends ConfigurableFlowBase<MiniAMDomainDunsInitConfig> {
-
+    private final static String DOMAIN_TYPE = "Domain";
+    private final static String DUNS_TYPE = "Duns";
+    private final static String GOLDEN_SET_DOMAIN = "Golden_Domain";
+    private final static String GOLDEN_SET_DUNS = "Golden_Duns";
     @Override
     public Node construct(TransformationFlowParameters parameters) {
         List<String> miniAMAttr = getMiniAMAttr(parameters);
         List<FieldMetadata> fieldMetadataTmp = prepareMiniAMFieldMetadata(parameters);
         MiniAMDomainDunsInitConfig config = getTransformerConfig(parameters);
         Node goldenDataSet = addSource(parameters.getBaseTables().get(0));
-        goldenDataSet = goldenDataSet.apply(
-                new MiniAMSeedMergeDomainDuns(new Fields(miniAMAttr.toArray(new String[miniAMAttr.size()])),
-                        config.getGoldenInputDataSetDomain(), config.getGoldenInputDataSetDuns(),
-                        config.getOutputDataSetType(), config.getOutputDataSetValue()),
-                new FieldList(goldenDataSet.getFieldNames()), fieldMetadataTmp, new FieldList(miniAMAttr));
+        String checkGoldenSetDomain = config.getGoldenInputDataSetDomain() + " != null";
+        String checkGoldenSetDuns = config.getGoldenInputDataSetDuns() + " != null";
+        // Mapping GoldenSet domains from schema (Domain, Duns) to (Type, Value)
+        Node goldenDataDomainSet = goldenDataSet
+                .apply(String.format("%s ? %s : null", checkGoldenSetDomain, config.getGoldenInputDataSetDomain()), 
+                        new FieldList(config.getGoldenInputDataSetDomain()), new FieldMetadata(GOLDEN_SET_DOMAIN, String.class)) //
+                .retain(new FieldList(config.getGoldenInputDataSetDomain())) //
+                .rename(new FieldList(config.getGoldenInputDataSetDomain()), new FieldList(config.getOutputDataSetValue())) //
+                .addColumnWithFixedValue(config.getOutputDataSetType(), DOMAIN_TYPE, String.class) //
+                .retain(new FieldList(miniAMAttr)) //
+                .renamePipe("GoldenDomain");
+        // Mapping GoldenSet duns from schema (Domain, Duns) to (Type, Value)
+        Node goldenDataDunsSet = goldenDataSet
+                .apply(String.format("%s ? %s : null", checkGoldenSetDuns, config.getGoldenInputDataSetDuns()),
+                        new FieldList(config.getGoldenInputDataSetDuns()),
+                        new FieldMetadata(GOLDEN_SET_DUNS, String.class)) //
+                .retain(new FieldList(config.getGoldenInputDataSetDuns())) //
+                .rename(new FieldList(config.getGoldenInputDataSetDuns()), new FieldList(config.getOutputDataSetValue())) //
+                .addColumnWithFixedValue(config.getOutputDataSetType(), DUNS_TYPE, String.class) //
+                .retain(new FieldList(miniAMAttr)) //
+                .renamePipe("GoldenDuns");
+        // Merge
+        goldenDataDomainSet = goldenDataDomainSet.merge(goldenDataDunsSet);
+        // Remove nulls
+        String checkNullExpression = config.getOutputDataSetValue() + " != null ";
+        goldenDataDomainSet = goldenDataDomainSet.filter(checkNullExpression,
+                new FieldList(config.getOutputDataSetType(), config.getOutputDataSetValue()));
         // De-duplication
-        goldenDataSet = goldenDataSet.groupByAndLimit(new FieldList(miniAMAttr), 1);
-        return goldenDataSet;
+        goldenDataDomainSet = goldenDataDomainSet.groupByAndLimit(new FieldList(miniAMAttr), 1);
+        return goldenDataDomainSet;
     }
 
     private List<FieldMetadata> prepareMiniAMFieldMetadata(TransformationFlowParameters parameters) {
