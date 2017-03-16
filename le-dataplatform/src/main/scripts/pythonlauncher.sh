6a28066b60d2dd56ab53d7097c28102e93826b26
mwilson <mwilson@479f5059-62a3-4c39-9342-9852e3a68435>
2017-03-16 19:38:16 +0000

    Review Request 8829: Added debugging output to pythonlauncher.sh to check which Python executable is being run

--- le-dataplatform/src/main/scripts/pythonlauncher.sh
+++ le-dataplatform/src/main/scripts/pythonlauncher.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+echo "pythonlauncher.sh:"
+
 if [ "${ANACONDA_HOME}" = "" ]; then
     ANACONDA_HOME=/opt/conda
 fi
+echo "  * ANACONDA_HOME: ${ANACONDA_HOME}"
 
-
+echo "  * Activating Anaconda Env: $1"
 source $ANACONDA_HOME/bin/activate $1
-python2 "${@:2}"
+
+echo "  * Excuting Python Script: ${@:2}"
+python "${@:2}"
