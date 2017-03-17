c6d0ee53dccfade4b90ae96111cf0c9047602f6a
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2017-03-17 18:58:17 +0000

    Review Request 8850: fix servicesflow unit test

--- le-datacloud/etl/src/main/java/com/latticeengines/datacloud/etl/transformation/service/impl/SimpleTransformationServiceBase.java
+++ le-datacloud/etl/src/main/java/com/latticeengines/datacloud/etl/transformation/service/impl/SimpleTransformationServiceBase.java
@@ -10,8 +10,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.latticeengines.domain.exposed.metadata.Table;
-
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
