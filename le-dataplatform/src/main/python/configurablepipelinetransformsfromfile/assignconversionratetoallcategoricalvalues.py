Review Request 8860: Fix inconsistent precision in AssignConversionRateToAllCategoricalValues and CategoricalGroupingStep (DP-4062)

le-dataplatform/src/main/python/configurablepipelinetransformsfromfile/assignconversionratetoallcategoricalvalues.py
@@ -1,40 +1,35 @@
 '''
 Description:
-If the choice is made to assign a categorical variable to numeric values, the choice should be the conversion rate for that specitic categorical value.
+If the choice is made to assign a categorical variable to numeric values, the choice should be the conversion rate for that specific categorical value.
 For example, category A has 0% conversion, B has 1% conversion, and C has 8% conversion in the training set.  Reassign A,B, and C to 0,.01 and .08, respectively
 '''
 from collections import Counter
 from json import encoder
 import json
+import numbers
+import numpy as np
 import os
 import pandas as pd
 from pipelinefwk import PipelineStep
 from pipelinefwk import create_column
 from pipelinefwk import get_logger
-
+try:
+    from precisionutil import PrecisionUtil
+except ImportError as e:
+    from leframework.util.precisionutil import PrecisionUtil
 
 logger = get_logger("pipeline")
 
 class AssignConversionRateToAllCategoricalValues(PipelineStep):
 
-    columnList = []
-    categoricalColumnMapping = None
-    categoricalColumnMappingFilePath = None
-    totalPositiveThreshold = 12
-    categoricalColumns = {}
-    targetColumn = ""
-
-    def __init__(self, categoricalColumns, targetColumn, totalPositiveThreshold=12):
+    def __init__(self, categoricalColumns, targetColumn, totalPositiveThreshold, categoricalColumnMapping):
         self.categoricalColumns = categoricalColumns
         self.targetColumn = targetColumn
         self.totalPositiveThreshold = totalPositiveThreshold
 
-        if self.categoricalColumnMapping is None:
-            self.categoricalColumnMapping = {}
-        if categoricalColumns:
-            self.columnList = categoricalColumns.keys()
-        else:
-            self.columnList = []
+        self.categoricalColumnMapping = categoricalColumnMapping
+        self.categoricalColumnMappingFilePath = None
+
         logger.info("Initialized AssignConversionRate with categoricalColumns " + str(categoricalColumns)
                     + ", targetColumn=" + str(targetColumn)
                     + ", positiveThreshold=" + str(self.totalPositiveThreshold))
@@ -68,7 +63,7 @@ class AssignConversionRateToAllCategoricalValues(PipelineStep):
     def __assignConversionRateToCategoricalColumns(self, column, dataFrame, meanConversionRate):
         if self.targetColumn in dataFrame and len(dataFrame[column]) == len(dataFrame[self.targetColumn]):
             logger.info("AssignConversionRate training phase. Converting column: " + column)
-            workingColumn = list(dataFrame[column])
+            workingColumn = list(dataFrame[column].apply(self.__convertFloatToString))
             cCount = Counter(workingColumn)
             targetColumn = list(dataFrame[self.targetColumn])
             posCount = Counter([workingColumn[i] for i in range(len(workingColumn)) if targetColumn[i] == 1])
@@ -79,24 +74,25 @@ class AssignConversionRateToAllCategoricalValues(PipelineStep):
                 if yy[k][0] == 0 or yy[k][1] < self.totalPositiveThreshold:
                     return 1.0
                 return (yy[k][1] / yy[k][0]) / meanConversionRate
-            keyConversionRate = { k:conversionRateCalc(k) for k in yy.keys() }
+            keyConversionRate = { k:PrecisionUtil.setPrecision(conversionRateCalc(k), 6) for k in yy.keys() }
             self.categoricalColumnMapping[column] = keyConversionRate
 
     def __applyConversionRate(self, categoryValue):
         if self.currentColumn in self.categoricalColumnMapping:
             if pd.isnull(categoryValue):
                 return 1.0
-            if categoryValue in self.categoricalColumnMapping[self.currentColumn]:
-                return self.categoricalColumnMapping[self.currentColumn][categoryValue]
+            categoryValueStr = self.__convertFloatToString(categoryValue)
+            if categoryValueStr in self.categoricalColumnMapping[self.currentColumn]:
+                return self.categoricalColumnMapping[self.currentColumn][categoryValueStr]
         return 1.0
 
+    def __convertFloatToString(self, x):
+        return '{0:.2f}'.format(x) if (isinstance(x, numbers.Real) and not np.isnan(x)) else x
+
     def __writeRTSArtifacts(self):
-        e = encoder.FLOAT_REPR
-        encoder.FLOAT_REPR = lambda o: format(o, ".2f")
         with open("conversionratemapping.json", "wb") as fp:
             json.dump(self.categoricalColumnMapping, fp)
             self.categoricalColumnMappingFilePath = os.path.abspath(fp.name)
-        encoder.FLOAT_REPR = e
 
     def getRTSArtifacts(self):
         return [("conversionratemapping.json", self.categoricalColumnMappingFilePath)]

le-dataplatform/src/main/python/configurablepipelinetransformsfromfile/categoricalgroupingstep.py
@@ -2,6 +2,8 @@ from collections import Counter
 from json import encoder
 import json
 from math import sqrt
+import numbers
+import numpy as np
 import os
 
 import pandas as pd
@@ -83,7 +85,7 @@ class CategoricalGroupingStep(PipelineStep):
         featureValueDict = {}
 
         if not test:
-            
+
             eventList = dataFrame[self.targetColumn].tolist()
             popCount = len(eventList)
             popRate = sum(eventList) * 1.0 / len(eventList)
@@ -105,7 +107,7 @@ class CategoricalGroupingStep(PipelineStep):
 
                 if any([pd.isnull(x) for x in xList]):
                     xList = ['LE-missing' if pd.isnull(x) else x for x in xList]
-                
+
                 featValMapping = self.__getGroupMapping(xList, eventList, popCount, popRate, self.thresholdError, self.thresholdSigSize, self.thresholdBinSize)
 
                 if len(featValMapping) > 0 :
@@ -117,7 +119,7 @@ class CategoricalGroupingStep(PipelineStep):
 
                     # code below that can be deleted in the future 
                     dd = self.__conversionRateEncoding(xListNew, eventList)
-                    featValMapping = {k:round(dd[v] / popRate, 2) for k, v in featValMapping.iteritems()}
+                    featValMapping = {self.__convertFloatToString(k):round(dd[v] / popRate, 2) for k, v in featValMapping.iteritems()}
                     xListNew = [round(dd[x] / popRate, 2) for x in xListNew]
                     # code above that can be deleted in the future
 
@@ -140,16 +142,16 @@ class CategoricalGroupingStep(PipelineStep):
 
                 if origFeatName not in self.dsCategVarGroupingInfo:
                     logger.error('{} not found in dsCategVarGroupingInfo'.format(origFeatName))
-            
-                origColValue = dataFrame[origFeatName].tolist()
+
+                origColValue = dataFrame[origFeatName].apply(self.__convertFloatToString).tolist()
                 if any([pd.isnull(x) for x in origColValue]):
                     origColValue = ['LE-missing' if pd.isnull(x) else x for x in origColValue]
 
                 valmap = self.dsCategVarGroupingInfo[origFeatName]
                 dsColVal = [valmap[x] if x in valmap else 1.0 for x in origColValue]
-                    
+
                 featureValueDict.update({newFeatName : dsColVal})
-        
+
         featureValueDict = {k: featureValueDict[k] for k in featureValueDict if k in self.groupedFeatures}
         if len(featureValueDict) > 0:
             logger.info('Columns that are being added to the event table: {}'.format(str(featureValueDict.keys())))
@@ -163,12 +165,15 @@ class CategoricalGroupingStep(PipelineStep):
 
         return dataFrame
 
+    def __convertFloatToString(self, x):
+        return '{0:.2f}'.format(x) if (x is not None and isinstance(x, numbers.Real) and not np.isnan(x)) else x
+
     def __findFeatures(self, dataFrame):
         featureList = [featureName for featureName in self.categoricalColumns if featureName in dataFrame.columns \
                 and featureName not in self.keys and featureName not in self.samples and featureName not in self.readouts \
                 and featureName not in self.target and featureName not in self.featuresToExclude]
         return featureList
-        
+
     def __conversionRateEncoding(self, columnList, eventList):
         cCount = Counter(columnList)
         posCount = Counter([columnList[i] for i in range(len(columnList)) if eventList[i] == 1])
@@ -178,15 +183,10 @@ class CategoricalGroupingStep(PipelineStep):
         return {k:yy[k][1] / yy[k][0] for k in yy.keys()}
 
     def __writeRTSArtifacts(self):
-        e = encoder.FLOAT_REPR
-        encoder.FLOAT_REPR = lambda o: format(o, ".2f")
-
         with open("dsCategVarGroupingInfo.json", "wb") as fp:
             logger.info('Writing RTS artifacts: {}'.format(json.dumps(self.dsCategVarGroupingInfo)))
             json.dump(self.dsCategVarGroupingInfo, fp)
             self.dsCategVarGroupingInfoFilePath = os.path.abspath(fp.name)
-            
-        encoder.FLOAT_REPR = e
 
     def __appendMetadataEntryInBatches(self, configMetadata):
         for featureName in self.groupedFeatures:
@@ -222,12 +222,12 @@ class CategoricalGroupingStep(PipelineStep):
             return 0
         else:
             return (subRate - oaRate) / (sqrt(r * (1.0 - r) * (1.0 / subCnt + 1.0 / oaCnt)))
-        
+
     def __getTuple(self, xList, eventList, k):
         """
         input:  xList : feature column
                 eventList: event column
-                k : special value to look 
+                k : special value to look
         output: return (sub-population size, sub-population rate) with respect to the value of k
         """
         if pd.isnull(k):
@@ -254,7 +254,7 @@ class CategoricalGroupingStep(PipelineStep):
             sig = self.__getSig((count, rate), (popCount, popRate))
             output.update({k: (count, rate, sig)})
         return output
-        
+
     def __groupCategoricalVar(self, xList, eventList, popCount, popRate, thresholdError=3, thresholdSigSize=0.01, threshBinSize=0.05):
         '''
         input:  xList : feature column
@@ -269,18 +269,18 @@ class CategoricalGroupingStep(PipelineStep):
             4. don't belong to any of the 3 above sets
         '''
         groupingResults = self.__getGroupingResults(xList, eventList, popCount, popRate)
-        
+
         featureValBigsize = dict([(x, y) for x, y in groupingResults.items() if y[0] >= popCount * threshBinSize])
         featureValSignificant = dict([(x, y) for x, y in groupingResults.items() if abs(y[2]) >= thresholdError * 0.05 * sqrt(popCount * popRate) and y[0] >= popCount * thresholdSigSize and x not in featureValBigsize.keys()])
         featureValClosetomean = dict([(x, y) for x, y in groupingResults.items() if abs(y[2]) <= thresholdError * 0.025 * sqrt(popCount * popRate) and x not in featureValBigsize.keys()])
         featureValLeftOver = dict([(x, y) for x, y in groupingResults.items() if x not in featureValBigsize.keys() and x not in featureValSignificant and x not in featureValClosetomean])
-        
+
         return (featureValBigsize, featureValSignificant, featureValClosetomean, featureValLeftOver)
-        
+
     def __getGroupMapping(self, xList, eventList, popCount, popRate, thresholdError=3, thresholdSigSize=0.01, threshBinSize=0.05):
 
         featValMapping = {}
-        
+
         featureValBigsize, featureValSignificant, featureValClosetomean, featureValLeftOver = self.__groupCategoricalVar(xList, eventList, popCount, popRate, thresholdError, thresholdSigSize, threshBinSize)
 
         logger.info('featureValBigsize={}'.format(featureValBigsize))
@@ -322,7 +322,7 @@ class CategoricalGroupingStep(PipelineStep):
                         featValMapping = {}
                 else:
                     featValMapping = {}
-                    
+
                 if len(featValMapping) == 0:
                     logger.info('feature values in left_over are too significant; original column is to be deleted and not grouped')
 

le-dataplatform/src/main/python/leframework/util/precisionutil.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numbers
 
 class PrecisionUtil(object):
 
@@ -10,11 +11,7 @@ class PrecisionUtil(object):
 
     @classmethod
     def setPrecisionOfValue(cls, x, precision):
-        if x is np.isnan(x):
-            return x
-        if type(x) == int:
-            return x
-        if x == 0.0:
+        if x is None or not isinstance(x, numbers.Real) or isinstance(x, numbers.Integral) or np.isnan(x) or x == 0.0:
             return x
         l = np.log10(np.fabs(x))
         e = np.int64(l)