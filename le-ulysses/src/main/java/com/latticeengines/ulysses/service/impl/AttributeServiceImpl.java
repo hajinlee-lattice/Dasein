920bfd57d565a7a6b43f1e031e5da582aeb6c206
rgonzalez <rgonzalez@479f5059-62a3-4c39-9342-9852e3a68435>
2017-03-27 17:38:54 +0000

    Review Request 6244: Organize imports

--- le-ulysses/src/main/java/com/latticeengines/ulysses/service/impl/AttributeServiceImpl.java
+++ le-ulysses/src/main/java/com/latticeengines/ulysses/service/impl/AttributeServiceImpl.java
@@ -19,8 +19,8 @@ import com.latticeengines.ulysses.service.AttributeService;
 @Component("attributeService")
 public class AttributeServiceImpl implements AttributeService {
 
-	private static final Logger log = Logger.getLogger(AttributeServiceImpl.class);
-	 
+    private static final Logger log = Logger.getLogger(AttributeServiceImpl.class);
+
     @Override
     public List<PrimaryField> getPrimaryFields() {
         EnumSet<FieldInterpretation> primaryMatchingFields = FieldInterpretationCollections.PrimaryMatchingFields;
@@ -33,14 +33,14 @@ public class AttributeServiceImpl implements AttributeService {
 
     @Override
     public PrimaryFieldValidationExpression getPrimaryFieldValidationExpression(CustomerSpace customerSpace) {
-    	boolean fuzzyMatchEnabled = FeatureFlagClient.isEnabled(customerSpace,
-				LatticeFeatureFlag.ENABLE_FUZZY_MATCH.getName());
-    	log.debug("Fuzzy Matching Enabled for client: " + fuzzyMatchEnabled);
-		String expression = fuzzyMatchEnabled ? FieldInterpretationCollections.FUZZY_MATCH_VALIDATION_EXPRESSION
-				: FieldInterpretationCollections.NON_FUZZY_MATCH_VALIDATION_EXPRESSION;
-		
-		PrimaryFieldValidationExpression validationExp = new PrimaryFieldValidationExpression();
-		validationExp.setExpression(expression);
+        boolean fuzzyMatchEnabled = FeatureFlagClient.isEnabled(customerSpace,
+                LatticeFeatureFlag.ENABLE_FUZZY_MATCH.getName());
+        log.debug("Fuzzy Matching Enabled for client: " + fuzzyMatchEnabled);
+        String expression = fuzzyMatchEnabled ? FieldInterpretationCollections.FUZZY_MATCH_VALIDATION_EXPRESSION
+                : FieldInterpretationCollections.NON_FUZZY_MATCH_VALIDATION_EXPRESSION;
+
+        PrimaryFieldValidationExpression validationExp = new PrimaryFieldValidationExpression();
+        validationExp.setExpression(expression);
         return validationExp;
     }
 
