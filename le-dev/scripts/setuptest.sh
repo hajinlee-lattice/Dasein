f6de8e754d0cb124470ef44b5d215ac4fcc8bd16
mwilson <mwilson@479f5059-62a3-4c39-9342-9852e3a68435>
2017-03-29 17:34:47 +0000

    Review Request 9068: Add modelquality datasets when running setuptest.sh

--- le-dev/scripts/setuptest.sh
+++ le-dev/scripts/setuptest.sh
@@ -6,10 +6,13 @@ printf "%s\n" "${WSHOME:?You must set WSHOME}"
 hdfs dfs -mkdir -p /tmp/Stoplist || true
 hdfs dfs -mkdir -p /tmp/AccountMaster || true
 hdfs dfs -mkdir -p /Pods/Default/Services/PropData/MatchService/PublicDomain || true
+hdfs dfs -mkdir -p /Pods/Default/Services/ModelQuality/datasets || true
 hdfs dfs -put -f $WSHOME/le-dev/testartifacts/Stoplist/Stoplist.avro /tmp/Stoplist
 hdfs dfs -put -f $WSHOME/le-dev/testartifacts/PublicDomain/PublicDomain.avro /Pods/Default/Services/PropData/MatchService/PublicDomain
 hdfs dfs -put -f $WSHOME/le-dev/testartifacts/PublicDomain/PublicDomain.csv /Pods/Default/Services/PropData/MatchService/PublicDomain
 hdfs dfs -put -f $WSHOME/le-dev/testartifacts/AccountMaster/AccountMaster.avro /tmp/AccountMaster
+hdfs dfs -put -f $WSHOME/le-modelquality/src/test/resources/com/latticeengines/modelquality/csvfiles/Mulesoft_NA_domain_enhanced21k.csv /Pods/Default/Services/ModelQuality/datasets
+hdfs dfs -put -f $WSHOME/le-modelquality/src/test/resources/com/latticeengines/modelquality/csvfiles/NGINXReducedRowsEnhanced_20160712.csv /Pods/Default/Services/ModelQuality/datasets
 
 
 # Expand aliases
