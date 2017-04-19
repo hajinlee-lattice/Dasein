3f97c89ba1564601da738e18770c839c8a008c46
penglong.liu <penglong.liu@479f5059-62a3-4c39-9342-9852e3a68435>
2017-04-19 01:48:57 +0000

    review request 9245 UI part of "Add capability to remove LPI users from all LPI tenants"

--- le-ui/projects/tenantconsole/app/core/directive/MainNavDirective.js
+++ le-ui/projects/tenantconsole/app/core/directive/MainNavDirective.js
@@ -1,9 +1,10 @@
 var app = angular.module("app.core.directive.MainNavDirective", [
     'ui.router',
-    'le.common.util.BrowserStorageUtility'
+    'le.common.util.BrowserStorageUtility',
+    'app.core.util.SessionUtility',
 ]);
 
-app.service('MainNavService', function(){
+app.service('MainNavService', function($q, $http, SessionUtility){
     this.parseNavState = function (stateName) {
         if (stateName.indexOf("TENANT") === 0) {
             return "Tenants";
@@ -19,6 +20,32 @@ app.service('MainNavService', function(){
         }
         return "unknown";
     };
+    this.deactiveUserStatus = function(emails) {
+        var defer = $q.defer();
+
+        var result = {
+            success: false,
+            resultObj: [],
+            errMsg: null
+        };
+
+        $http({
+            method: 'PUT',
+            url: '/admin/internal/services/deactiveUserStatus',
+            headers: {
+                'MagicAuthentication': "Security through obscurity!"
+            },
+            data: emails
+        }).success(function(data) {
+            result.success = (data === "true" || data === true);
+            defer.resolve(result);
+        }).error(function(err, status){
+            SessionUtility.handleAJAXError(err, status);
+            result.errMsg = err;
+        });
+
+        return defer.promise;
+    };
 });
 
 app.directive('mainNav', function(){
@@ -26,7 +53,7 @@ app.directive('mainNav', function(){
         restrict: 'AE',
         templateUrl: 'app/core/view/MainNavView.html',
         scope: {activeNav: '='},
-        controller: function ($scope, $rootScope, $state,
+        controller: function ($scope, $rootScope, $state, $uibModal,
                               MainNavService, BrowserStorageUtility) {
             routeToCorrectState();
 
@@ -35,6 +62,50 @@ app.directive('mainNav', function(){
             $scope.onSignOutClick = function() {
                 BrowserStorageUtility.clear();
                 $state.go('LOGIN');
+            };
+
+            $scope.onDeactiveUserClick = function(){
+                var modalInstance = $uibModal.open({
+                    templateUrl: 'addNewPopModal.html',
+                    controller: function($scope, $uibModalInstance, _, $window ){
+                        $scope.emails = "";
+                        $scope.isValid = true;
+
+                        $scope.validateEmailInfo = function(){
+                            if ($scope.addform.emailId.$error.required || $scope.emails === '') {
+                                $scope.EmailIdErrorMsg = "Emails is required.";
+                                $scope.showEmailIdError = true;
+                                $scope.isValid = false;
+                                return false;
+                            }
+                            else {
+                                $scope.showEmailIdError = false;
+                                $scope.isValid = true;
+                            }
+                            return true;
+                        };
+
+                        $scope.ok = function () {
+                            if ($scope.validateEmailInfo()) {
+                                if ($window.confirm("Please confirm deactive users?")) {
+                                    $uibModalInstance.close($scope.emails);
+                                }
+                            }
+                        };
+
+                        $scope.cancel = function () {
+                            $uibModalInstance.dismiss('cancel');
+                        };
+                    }
+                });
+
+                modalInstance.result.then(function (emails) {
+                    MainNavService.deactiveUserStatus(emails);
+                    $state.go('TENANT.LIST');
+                }, function () {
+                    $state.go('TENANT.LIST');
+                });
+
             };
 
             function routeToCorrectState(){
