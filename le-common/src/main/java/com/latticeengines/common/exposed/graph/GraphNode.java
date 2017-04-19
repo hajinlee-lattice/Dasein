3cde379dd760663378838f30f33fd46560e46033
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2017-04-19 22:19:59 +0000

    Review Request 9311: move accept to default method and fix bfs

--- le-common/src/main/java/com/latticeengines/common/exposed/graph/GraphNode.java
+++ le-common/src/main/java/com/latticeengines/common/exposed/graph/GraphNode.java
@@ -4,10 +4,16 @@ import java.util.Collection;
 import java.util.Map;
 
 import com.latticeengines.common.exposed.visitor.Visitable;
+import com.latticeengines.common.exposed.visitor.Visitor;
+import com.latticeengines.common.exposed.visitor.VisitorContext;
 
 public interface GraphNode extends Visitable {
 
     Collection<? extends GraphNode> getChildren();
-    
+
     Map<String, Collection<? extends GraphNode>> getChildMap();
+
+    default void accept(Visitor visitor, VisitorContext ctx) {
+        visitor.visit(this, ctx);
+    }
 }
--- le-common/src/main/java/com/latticeengines/common/exposed/graph/traversal/impl/BreadthFirstSearch.java
+++ le-common/src/main/java/com/latticeengines/common/exposed/graph/traversal/impl/BreadthFirstSearch.java
@@ -6,6 +6,8 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Set;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 import com.latticeengines.common.exposed.graph.GraphNode;
 import com.latticeengines.common.exposed.graph.traversal.AbstractTraversalAlgorithm;
 import com.latticeengines.common.exposed.visitor.Visitor;
@@ -13,38 +15,35 @@ import com.latticeengines.common.exposed.visitor.VisitorContext;
 
 public class BreadthFirstSearch extends AbstractTraversalAlgorithm {
 
-    private final Queue<GraphNode> queue = new LinkedList<>();
+    private final Queue<Pair<GraphNode, GraphNode>> queue = new LinkedList<>();
     private final Set<GraphNode> seenNodes = new HashSet<GraphNode>();
 
     @Override
     public void run(GraphNode node, Visitor visitor) {
-        queue.offer(node);
         runSearch(node, visitor);
     }
 
     @Override
     public void runSearch(GraphNode node, Visitor visitor) {
-        VisitorContext ctx = new VisitorContext();
-        ctx.setProperty("parent", node);
+        queue.offer(Pair.<GraphNode, GraphNode> of(node, null));
         while (!queue.isEmpty()) {
-            GraphNode n = queue.remove();
+            Pair<GraphNode, GraphNode> pair = queue.poll();
+            GraphNode n = pair.getLeft();
+            GraphNode parent = pair.getRight();
             seenNodes.add(n);
+            VisitorContext ctx = new VisitorContext();
+            ctx.setProperty("parent", parent);
             preRun(visitor);
             n.accept(visitor, ctx);
             postRun(visitor);
             Collection<? extends GraphNode> children = n.getChildren();
-
-            ctx = new VisitorContext();
-            ctx.setProperty("parent", n);
-
             for (GraphNode child : children) {
                 if (!seenNodes.contains(child)) {
-                    queue.offer(child);
+                    queue.offer(Pair.<GraphNode, GraphNode> of(child, node));
                 }
             }
         }
     }
-
 
     @Override
     public void preRun(Visitor visitor) {
