2abfe12953abbd2588084509f69c5b5328901325
bross <bross@479f5059-62a3-4c39-9342-9852e3a68435>
2017-04-20 21:01:03 +0000

    Review Request 9332: Fixes to generate demo json code

--- le-pls/src/test/java/com/latticeengines/pls/manual/GenerateDemoJsonTestNG.java
+++ le-pls/src/test/java/com/latticeengines/pls/manual/GenerateDemoJsonTestNG.java
@@ -6,6 +6,7 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import org.apache.commons.csv.CSVFormat;
@@ -20,6 +21,8 @@ import com.latticeengines.domain.exposed.datacloud.statistics.AttributeStatistic
 import com.latticeengines.domain.exposed.datacloud.statistics.AttributeStatsDetails;
 import com.latticeengines.domain.exposed.datacloud.statistics.Bucket;
 import com.latticeengines.domain.exposed.datacloud.statistics.Buckets;
+import com.latticeengines.domain.exposed.datacloud.statistics.TopNAttributes;
+import com.latticeengines.domain.exposed.metadata.FundamentalType;
 import com.latticeengines.domain.exposed.pls.LeadEnrichmentAttribute;
 
 public class GenerateDemoJsonTestNG {
@@ -33,22 +36,41 @@ public class GenerateDemoJsonTestNG {
         List<LeadEnrichmentAttribute> attributes = new ArrayList<>();
         AccountMasterCube cube = new AccountMasterCube();
         cube.setStatistics(new HashMap<>());
+        Map<String, List<TopNAttributes.TopAttribute>> subcategories = new HashMap<>();
         for (CSVRecord record : parser) {
             String category = record.get(0);
             String subcategory = record.get(1);
             String attribute = record.get(2);
             String value = record.get(3);
-
-            attributes.add(createLeadEnrichmentAttribute(category, subcategory, attribute));
-            AttributeStatistics statistics = cube.getStatistics().get(attribute);
+            String fieldName = "Demo_" + attribute.replace(" ", "_");
+            if (!attributes.stream().anyMatch(a -> a.getFieldName().equals(fieldName))) {
+                attributes.add(createLeadEnrichmentAttribute(category, subcategory, attribute, fieldName));
+            }
+            AttributeStatistics statistics = cube.getStatistics().get(fieldName);
             if (statistics == null) {
                 statistics = new AttributeStatistics();
-                cube.getStatistics().put(attribute, statistics);
+                cube.getStatistics().put(fieldName, statistics);
             }
             addBucket(statistics, value);
+
+            List<TopNAttributes.TopAttribute> list = subcategories.get(subcategory);
+            if (list == null) {
+                list = new ArrayList<>();
+                subcategories.put(subcategory, list);
+            }
+            if (!list.stream().anyMatch(a -> a.getAttribute().equals(fieldName))) {
+                list.add(createTopAttribute(fieldName));
+            }
         }
         FileUtils.write(new File("/tmp/buckets.json"), JsonUtils.serialize(cube));
         FileUtils.write(new File("/tmp/attributes.json"), JsonUtils.serialize(attributes));
+        FileUtils.write(new File("/tmp/topn.json"), JsonUtils.serialize(subcategories));
+    }
+
+    private TopNAttributes.TopAttribute createTopAttribute(String fieldName) {
+        TopNAttributes.TopAttribute topAttribute = new TopNAttributes.TopAttribute(fieldName,
+                (long) random.nextInt(15000));
+        return topAttribute;
     }
 
     private void addBucket(AttributeStatistics statistics, String value) {
@@ -68,11 +90,14 @@ public class GenerateDemoJsonTestNG {
         buckets.getBucketList().add(bucket);
     }
 
-    private LeadEnrichmentAttribute createLeadEnrichmentAttribute(String category, String subcategory, String name) {
+    private LeadEnrichmentAttribute createLeadEnrichmentAttribute(String category, String subcategory,
+            String displayName, String fieldName) {
         LeadEnrichmentAttribute attribute = new LeadEnrichmentAttribute();
-        attribute.setFieldName(name);
+        attribute.setDisplayName(displayName);
+        attribute.setFieldName(fieldName);
         attribute.setCategory(category);
         attribute.setSubcategory(subcategory);
+        attribute.setFundamentalType(FundamentalType.BOOLEAN);
         return attribute;
     }
 }
