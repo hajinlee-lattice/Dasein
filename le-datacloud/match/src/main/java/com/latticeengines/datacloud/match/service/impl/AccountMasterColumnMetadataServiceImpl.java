c08bc2f5b3088674a846688649be543490d34742
ssakhalkar <ssakhalkar@479f5059-62a3-4c39-9342-9852e3a68435>
2017-04-21 22:43:18 +0000

    Review Request 9291

--- le-datacloud/match/src/main/java/com/latticeengines/datacloud/match/service/impl/AccountMasterColumnMetadataServiceImpl.java
+++ le-datacloud/match/src/main/java/com/latticeengines/datacloud/match/service/impl/AccountMasterColumnMetadataServiceImpl.java
@@ -1,7 +1,13 @@
 package com.latticeengines.datacloud.match.service.impl;
 
+import java.util.Date;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
 import javax.annotation.Resource;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -9,12 +15,16 @@ import com.latticeengines.datacloud.core.entitymgr.DataCloudVersionEntityMgr;
 import com.latticeengines.datacloud.match.exposed.service.MetadataColumnService;
 import com.latticeengines.datacloud.match.exposed.util.MatchUtils;
 import com.latticeengines.domain.exposed.datacloud.manage.AccountMasterColumn;
+import com.latticeengines.domain.exposed.datacloud.manage.DataCloudVersion;
 
 @Component("accountMasterColumnMetadataService")
 public class AccountMasterColumnMetadataServiceImpl extends BaseColumnMetadataServiceImpl<AccountMasterColumn> {
+    private static final Log log = LogFactory.getLog(AccountMasterColumnMetadataServiceImpl.class);
 
     @Resource(name = "accountMasterColumnService")
     private MetadataColumnService<AccountMasterColumn> accountMasterColumnService;
+
+    private ConcurrentMap<String, Date> cachedRefreshDate = new ConcurrentHashMap<>();
 
     @Autowired
     private DataCloudVersionEntityMgr versionEntityMgr;
@@ -37,6 +47,24 @@ public class AccountMasterColumnMetadataServiceImpl extends BaseColumnMetadataSe
     @Override
     protected String getLatestVersion() {
         return versionEntityMgr.currentApprovedVersion().getVersion();
+    }
+
+    @Override
+    protected boolean refreshCacheNeeded() {
+        DataCloudVersion approvedVersion = versionEntityMgr.currentApprovedVersion();
+        String approvedVersionValue = approvedVersion.getVersion();
+        Date approvedVersionDate = approvedVersion.getMetadataRefreshDate();
+        if (cachedRefreshDate.get(approvedVersionValue) != null
+                && approvedVersionDate
+                        .compareTo(cachedRefreshDate.get(approvedVersionValue)) <= 0) {
+            log.info("Metadata Refresh Date : " + approvedVersionDate);
+            log.info("Cache is already update-to-date as per the metadata refresh date so not refreshing it again");
+            return false;
+        }
+        cachedRefreshDate.put(approvedVersionValue, approvedVersionDate);
+        log.info("Metadata Refresh Date : " + approvedVersionDate);
+        log.info("Cache refreshed successfully");
+        return true;
     }
 
 }
