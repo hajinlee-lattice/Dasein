04077e6bc52bc74a69ac11dc097201060957bf53
jlmehta <jlmehta@479f5059-62a3-4c39-9342-9852e3a68435>
2017-04-26 22:16:11 +0000

    DP-4092: Ensure pipelineService looks into the correct hdfs location (aws or non aws folders) for pipeline.json artifact Review Request 9419

---
+++ le-modelquality/src/test/java/com/latticeengines/modelquality/service/impl/PipelineServiceDeploymentTestNG.java
@@ -0,0 +1,40 @@
+package com.latticeengines.modelquality.service.impl;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.latticeengines.common.exposed.util.HdfsUtils;
+import com.latticeengines.modelquality.functionalframework.ModelQualityTestNGBase;
+import com.latticeengines.modelquality.service.PipelineService;
+
+public class PipelineServiceDeploymentTestNG extends ModelQualityTestNGBase {
+
+    @Autowired
+    private PipelineService pipelineService;
+
+    @Test(groups = "deployment", enabled = true)
+    public void testValidPipelineJsonPath() throws Exception {
+
+        String pipelineJson = getPrivateFieldValue(pipelineService, "pipelineJson");
+        String stackName = getPrivateFieldValue(pipelineService, "stackName");
+        Method method = pipelineService.getClass().getSuperclass().getDeclaredMethod("getActiveStack");
+
+        @SuppressWarnings("unchecked")
+        String pipelineJsonPath = String.format(pipelineJson, stackName, //
+                ((Map<String, String>) method.invoke(pipelineService, null)).get("ArtifactVersion"));
+
+        Assert.assertTrue(HdfsUtils.fileExists(yarnConfiguration, pipelineJsonPath), "pipeline.json not found in path : " + pipelineJsonPath);
+    }
+
+    private String getPrivateFieldValue(Object obj, String fieldName) throws NoSuchFieldException, IllegalAccessException {
+        Field f = obj.getClass().getDeclaredField(fieldName); // NoSuchFieldException
+        f.setAccessible(true);
+        String value = (String) f.get(obj); // IllegalAccessException
+        return value;
+    }
+}
