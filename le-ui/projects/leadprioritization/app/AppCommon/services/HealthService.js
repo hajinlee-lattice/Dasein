553ef3cf64f76008667928155a1b43d658bf229d
dhom <dhom@479f5059-62a3-4c39-9342-9852e3a68435>
2017-05-05 19:31:48 +0000

    Review Request 9567 pls-3399 temporarily disabled ui system status check

--- le-ui/projects/leadprioritization/app/AppCommon/services/HealthService.js
+++ le-ui/projects/leadprioritization/app/AppCommon/services/HealthService.js
@@ -6,39 +6,7 @@ angular.module('mainApp.appCommon.services.HealthService', [
 
     this.checkSystemStatus = function() {
         var deferred = $q.defer();
-        var cancelled = false;
-
-        var http = $http({
-            method: 'GET',
-            url: '/pls/health/systemstatus',
-            timeout: $timeout(function() {
-                deferred.resolve();
-                cancelled = true;
-            }, CHECK_SYSTEM_STATUS_TIMEOUT)
-        }).then(function(response) {
-            if (response.data.status === 'OK') {
-                deferred.resolve();
-            } else {
-                ServiceErrorUtility.showBanner({
-                    data: {
-                        errorMsg: response.data.message || ResourceUtility.getString('UNEXPECTED_SERVICE_ERROR')
-                    }
-                });
-                deferred.reject();
-            }
-        }).catch(function() {
-            if (cancelled) {
-                return;
-            }
-
-            ServiceErrorUtility.showBanner({
-                data: {
-                    errorMsg: ResourceUtility.getString('UNEXPECTED_SERVICE_ERROR')
-                }
-            });
-            deferred.reject();
-        });
-
+        deferred.resolve();
         return deferred.promise;
     };
 });
