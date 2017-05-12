1734145de3b2cb81186358d4592c4a2a86797f30
jlmehta <jlmehta@479f5059-62a3-4c39-9342-9852e3a68435>
2017-05-12 04:51:04 +0000

    Fix doc resource URLs and add healthcheck for modelquality Review Request 9676

--- le-microservice/core/src/main/java/com/latticeengines/microservice/doc/DocketProviderImpl.java
+++ le-microservice/core/src/main/java/com/latticeengines/microservice/doc/DocketProviderImpl.java
@@ -2,12 +2,12 @@ package com.latticeengines.microservice.doc;
 
 import org.springframework.stereotype.Component;
 
-import springfox.documentation.RequestHandler;
-import springfox.documentation.builders.RequestHandlerSelectors;
-
 import com.google.common.base.Predicate;
 import com.latticeengines.microservice.exposed.DocketProvider;
 import com.latticeengines.microservice.exposed.DocketProviderBase;
+
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.RequestHandlerSelectors;
 
 @Component("docketProvider")
 public class DocketProviderImpl extends DocketProviderBase implements DocketProvider {
@@ -20,8 +20,8 @@ public class DocketProviderImpl extends DocketProviderBase implements DocketProv
         return RequestHandlerSelectors.basePackage("com.latticeengines.microservice.controller");
     }
 
-    protected String contextPath()  {
-        return "/doc";
+    protected String contextPath() {
+        return "";
     }
 
 }
