ee136d885d7b84214d5a55ac30a871c269e6e1ed
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-05-18 08:07:38 +0000

    Review Request 9726 fix bit codec utils

--- le-common/src/main/java/com/latticeengines/common/exposed/util/BitCodecUtils.java
+++ le-common/src/main/java/com/latticeengines/common/exposed/util/BitCodecUtils.java
@@ -68,14 +68,9 @@ public class BitCodecUtils {
     }
 
     public static int getBits(long result, int lowestBitPos, int numBits) {
-        int value = 0;
-        for (int i = 0; i < numBits; i++) {
-            int offset = lowestBitPos + i;
-            if ((result >> offset & 1) == 1) {
-                value = value | (1 << i);
-            }
-        }
-        return value;
+        long mask = bitMask(0, lowestBitPos, numBits);
+        long masked = result & mask;
+        return (int) masked >> lowestBitPos;
     }
 
 
