f5a2b25b6e9d821a80471f051be5739cfff236ec
ygao <ygao@479f5059-62a3-4c39-9342-9852e3a68435>
2017-05-18 17:44:02 +0000

    review request 9735: Schema Change for the Play

--- le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
+++ le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
@@ -7,7 +7,7 @@ CREATE PROCEDURE `UpdateSchema`()
   BEGIN
      START TRANSACTION;
 	create table `LAUNCH` (`PID` bigint not null auto_increment unique, `STATE` integer not null, `NAME` varchar(255) not null, `TABLE_NAME` varchar(255), `TIMESTAMP` datetime not null, FK_PLAY_ID bigint, primary key (`PID`)) ENGINE=InnoDB;
-	create table `PLAY` (`PID` bigint not null auto_increment unique, `DESCRIPTION` varchar(255), `DISPLAY_NAME` varchar(255) not null, `IS_VISIBLE` boolean not null, `NAME` varchar(255) not null, `SEGMENT_NAME` varchar(255), `TENANT_ID` bigint not null, FK_CALL_PREP_ID bigint, FK_TENANT_ID bigint not null, primary key (`PID`)) ENGINE=InnoDB;
+	create table `PLAY` (`PID` bigint not null auto_increment unique, `DESCRIPTION` varchar(255), `DISPLAY_NAME` varchar(255) not null, `NAME` varchar(255) not null, `SEGMENT_NAME` varchar(255), `TENANT_ID` bigint not null, FK_CALL_PREP_ID bigint, FK_TENANT_ID bigint not null, primary key (`PID`)) ENGINE=InnoDB;
 	create table `CALL_PREP` (`PID` bigint not null auto_increment unique, primary key (`PID`)) ENGINE=InnoDB;
 
 	alter table `LAUNCH` add index FK856C17B3E57F1489 (FK_PLAY_ID), add constraint FK856C17B3E57F1489 foreign key (FK_PLAY_ID) references `PLAY` (`PID`) on delete cascade;
