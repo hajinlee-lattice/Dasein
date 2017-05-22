9e770468e9ba01bffe821737694ab8db5c34497b
bholland <bholland@479f5059-62a3-4c39-9342-9852e3a68435>
2017-05-22 22:25:03 +0000

    Review Request 9782 see PLS-4015 - in datacloud explorer added new boolean display for insights tab

--- le-ui/projects/common/components/datacloud/explorer/attributetile/attributetile.component.js
+++ le-ui/projects/common/components/datacloud/explorer/attributetile/attributetile.component.js
@@ -10,11 +10,21 @@ angular
         },
         controllerAs: 'vm',
         templateUrl: '/components/datacloud/explorer/attributetile/attributetile.component.html',
-        controller: function ($scope, $document, $timeout, $interval, DataCloudStore) {
+        controller: function ($scope, $document, $timeout, $interval, DataCloudStore, NumberUtility) {
             var vm = $scope.vm;
             
             angular.extend(vm, { });
 
+            vm.booleanStats = function(stats) {
+                var booleans = {};
+                for(var i in stats) {
+                    var stat = stats[i];
+                    booleans[stat.Lbl] = stat;
+                }
+                return booleans;
+            }
+
+            vm.NumberUtility = NumberUtility;
         }
     };
 });
