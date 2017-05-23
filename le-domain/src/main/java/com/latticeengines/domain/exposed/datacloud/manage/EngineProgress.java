4ef5f91b821900e24021444dbd8c9ff0213a6716
dzheng <dzheng@479f5059-62a3-4c39-9342-9852e3a68435>
2017-05-23 18:15:32 +0000

    Review Request 9791: Progress entity class for orchestration framework

---
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/datacloud/manage/EngineProgress.java
@@ -0,0 +1,77 @@
+package com.latticeengines.domain.exposed.datacloud.manage;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class EngineProgress {
+    private Engine engine;
+    private String name;
+    private String version;
+    private ProgressStatus status;
+    private Float progress;
+    private String message;
+
+    public enum Engine {
+        INGESTION, TRANSFORMATION, PUBLICATION
+    }
+
+    @JsonProperty("Engine")
+    public Engine getEngine() {
+        return engine;
+    }
+
+    @JsonProperty("Engine")
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @JsonProperty("Name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("Name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("Version")
+    public String getVersion() {
+        return version;
+    }
+
+    @JsonProperty("Version")
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    @JsonProperty("Status")
+    public ProgressStatus getStatus() {
+        return status;
+    }
+
+    @JsonProperty("Status")
+    public void setStatus(ProgressStatus status) {
+        this.status = status;
+    }
+
+    @JsonProperty("Progress")
+    public Float getProgress() {
+        return progress;
+    }
+
+    @JsonProperty("Progress")
+    public void setProgress(Float progress) {
+        this.progress = progress;
+    }
+
+    @JsonProperty("Message")
+    public String getMessage() {
+        return message;
+    }
+
+    @JsonProperty("Message")
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
