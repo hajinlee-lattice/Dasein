8f93f5a5e654472b05dd79d85aab2982758a930e
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-05-30 07:09:19 +0000

    Review Request 9806 docker common containers build with no cache

--- le-docker/src/main/scripts/common/build.sh
+++ le-docker/src/main/scripts/common/build.sh
@@ -5,7 +5,7 @@ docker rmi -f $(docker images -a --filter "dangling=true" -q --no-trunc) 2> /dev
 build_docker() {
 	IMAGE=$1
 	sed -i.bak "s|{{TIMESTAMP}}|$(date +%s)|g" Dockerfile
-	docker build -t $IMAGE . || true
+	docker build --no-cache -t $IMAGE . || true
 	mv Dockerfile.bak Dockerfile
 }
 
