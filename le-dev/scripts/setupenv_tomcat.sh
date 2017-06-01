659c865d366ea16d0dedb2a40361e554a7c05ae1
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-06-01 09:43:51 +0000

    Review Request 9862 fix tomcat setup

--- le-dev/scripts/setupenv_tomcat.sh
+++ le-dev/scripts/setupenv_tomcat.sh
@@ -16,7 +16,7 @@ if [ "${BOOTSTRAP_MODE}" = "bootstrap" ]; then
     TOMCAT_TGZ_URL=https://www.apache.org/dyn/closer.cgi?action=download&filename=tomcat/tomcat-${TOMCAT_MAJOR}/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz
 
     if [ ! -f "${ARTIFACT_DIR}/apache-tomcat-${TOMCAT_VERSION}.tar.gz" ]; then
-        wget TOMCAT_TGZ_URL -O $ARTIFACT_DIR/apache-tomcat-${TOMCAT_VERSION}.tar.gz
+        wget "${TOMCAT_TGZ_URL}" -O $ARTIFACT_DIR/apache-tomcat-${TOMCAT_VERSION}.tar.gz
     fi
     rm -rf $ARTIFACT_DIR/apache-tomcat-${TOMCAT_VERSION} || true
     tar xzf $ARTIFACT_DIR/apache-tomcat-${TOMCAT_VERSION}.tar.gz -C $ARTIFACT_DIR
