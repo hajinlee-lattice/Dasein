953473111ffd5f4a90badf345afb9ef004a48c48
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-06-01 08:28:18 +0000

    Review Request 9862 fix tomcat build

--- le-docker/src/main/scripts/tomcat/build.sh
+++ le-docker/src/main/scripts/tomcat/build.sh
@@ -6,30 +6,26 @@ docker rmi -f $(docker images -a --filter "dangling=true" -q --no-trunc) 2> /dev
 
 source ../functions.sh
 
-pushd le-tomcatbase &&
-build_docker latticeengines/tomcatbase &&
+for img in 'haproxy' 'swagger' 'tomcatbase'; do
+
+    pushd le-${img} &&
+    build_docker latticeengines/${img} &&
+    popd &
+
+done
+wait
+
+pushd le-microservice
+bash build.sh $MICROSERVICES
 popd
 
-#for img in 'haproxy' 'swagger' 'tomcatbase'; do
-#
-#    pushd le-${img} &&
-#    build_docker latticeengines/${img} &&
-#    popd &
-#
-#done
-#wait
-#
-#pushd le-microservice
-#bash build.sh $MICROSERVICES
-#popd
-#
-#for img in 'config' 'playmaker' 'oauth2'; do
-#
-#    pushd le-${img} &&
-#    build_docker latticeengines/${img} &&
-#    popd &
-#
-#done
-#wait
+for img in 'config' 'playmaker' 'oauth2'; do
+
+    pushd le-${img} &&
+    build_docker latticeengines/${img} &&
+    popd &
+
+done
+wait
 
 docker rmi -f $(docker images -a --filter "dangling=true" -q --no-trunc) 2> /dev/null || true
