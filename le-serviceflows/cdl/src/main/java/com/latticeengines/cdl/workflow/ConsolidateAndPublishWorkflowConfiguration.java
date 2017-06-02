84c217b63b7387b5f85f011cf28b5ce88a3a2c81
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2017-06-02 16:42:46 +0000

    Review Request 9923: redshift publish deployment test

--- le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/workflow/ConsolidateAndPublishWorkflowConfiguration.java
+++ le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/workflow/ConsolidateAndPublishWorkflowConfiguration.java
@@ -1,6 +1,7 @@
 package com.latticeengines.cdl.workflow;
 
 import com.latticeengines.cdl.workflow.steps.StartExecutionConfiguration;
+import com.latticeengines.domain.exposed.camille.CustomerSpace;
 import com.latticeengines.domain.exposed.eai.HdfsToRedshiftConfiguration;
 import com.latticeengines.domain.exposed.workflow.WorkflowConfiguration;
 
@@ -14,6 +15,20 @@ public class ConsolidateAndPublishWorkflowConfiguration extends WorkflowConfigur
         public ConsolidateAndPublishWorkflowConfiguration configuration = new ConsolidateAndPublishWorkflowConfiguration();
         public StartExecutionConfiguration startExecutionConfiguration = new StartExecutionConfiguration();
         public RedshiftPublishWorkflowConfiguration.Builder redshiftPublishWorkflowConfigurationBuilder = new RedshiftPublishWorkflowConfiguration.Builder();
+
+        public Builder customer(CustomerSpace customerSpace) {
+            configuration.setContainerConfiguration("consolidateAndPublishWorkflow", customerSpace,
+                    "consolidateAndPublishWorkflow");
+            startExecutionConfiguration.setCustomerSpace(customerSpace);
+            redshiftPublishWorkflowConfigurationBuilder.customer(customerSpace);
+            return this;
+        }
+
+        public Builder microServiceHostPort(String microServiceHostPort) {
+            startExecutionConfiguration.setMicroServiceHostPort(microServiceHostPort);
+            redshiftPublishWorkflowConfigurationBuilder.microServiceHostPort(microServiceHostPort);
+            return this;
+        }
 
         public Builder datafeedName(String datafeedName) {
             startExecutionConfiguration.setDataFeedName(datafeedName);
