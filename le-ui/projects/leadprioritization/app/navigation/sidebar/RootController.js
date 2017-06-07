0fe2ff14acc9981ca7d1132628a7c0c502638551
dhom <dhom@479f5059-62a3-4c39-9342-9852e3a68435>
2017-06-07 15:38:08 +0000

    Review Request 9992 PLS-4097 playbook behind cdl feature flag

--- le-ui/projects/leadprioritization/app/navigation/sidebar/RootController.js
+++ le-ui/projects/leadprioritization/app/navigation/sidebar/RootController.js
@@ -34,6 +34,7 @@ angular
             $scope.showSalesforceSettings = FeatureFlagService.FlagIsEnabled(flags.USE_SALESFORCE_SETTINGS);
             $scope.showCampaignsPage = FeatureFlagService.FlagIsEnabled(flags.CAMPAIGNS_PAGE);
             $scope.showAnalysisPage = FeatureFlagService.FlagIsEnabled(flags.ENABLE_CDL);
+            $scope.showPlayBook = FeatureFlagService.FlagIsEnabled(flags.ENABLE_CDL);
             $scope.showSegmentationPage = FeatureFlagService.FlagIsEnabled(flags.ENABLE_CDL);
             $scope.showLatticeInsightsPage = FeatureFlagService.FlagIsEnabled(flags.LATTICE_INSIGHTS);
             $scope.showContactUs = false;
