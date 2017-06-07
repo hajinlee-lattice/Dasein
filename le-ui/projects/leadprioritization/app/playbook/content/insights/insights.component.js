7cdb99b14fae31081cdb8db8cd51638f1a196f5f
dhom <dhom@479f5059-62a3-4c39-9342-9852e3a68435>
2017-06-07 20:07:39 +0000

    Review Request 10004 playbook wizard validation talking points

--- le-ui/projects/leadprioritization/app/playbook/content/insights/insights.component.js
+++ le-ui/projects/leadprioritization/app/playbook/content/insights/insights.component.js
@@ -1,41 +1,45 @@
 angular.module('lp.playbook.wizard.insights', [])
 .controller('PlaybookWizardInsights', function(
-    $state, $stateParams, $scope, ResourceUtility, CgTalkingPointStore, TalkingPointAttributes, TalkingPoints
+    $scope, PlaybookWizardStore, CgTalkingPointStore, TalkingPointAttributes, TalkingPoints
 ) {
     var vm = this;
 
     angular.extend(vm, {
-       attributes: TalkingPointAttributes,
-       talkingPoints: TalkingPoints
+        attributes: TalkingPointAttributes,
+        talkingPoints: TalkingPoints
     });
 
     vm.addTalkingPoint = function() {
-       vm.talkingPoints.push({timestamp: new Date().getTime(), Title: null, Content: null, Offset: vm.talkingPoints.length, IsNew: true});
+        vm.talkingPoints.push({timestamp: new Date().getTime(), Title: null, Content: null, Offset: vm.talkingPoints.length, IsNew: true});
     };
 
     vm.onDelete = function(pos) {
-       vm.talkingPoints.splice(pos, 1);
-       for (var i = pos; i < vm.talkingPoints.length; i++) {
-           vm.talkingPoints[i].Offset--;
-       }
+    vm.talkingPoints.splice(pos, 1);
+        for (var i = pos; i < vm.talkingPoints.length; i++) {
+            vm.talkingPoints[i].Offset--;
+        }
     };
 
     vm.reorder = function(from, to) {
-       var tmp = vm.talkingPoints[from];
-       vm.talkingPoints[from] = vm.talkingPoints[to];
-       vm.talkingPoints[to] = tmp;
-       vm.talkingPoints.forEach(function(tp, i) {
-           tp.Offset = i;
-       });
+        var tmp = vm.talkingPoints[from];
+        vm.talkingPoints[from] = vm.talkingPoints[to];
+        vm.talkingPoints[to] = tmp;
+        vm.talkingPoints.forEach(function(tp, i) {
+            tp.Offset = i;
+        });
     };
 
-    vm.validTalkingPoints = function() {
-       for (var i = 0; i < vm.talkingPoints.length; i++) {
-           if (!vm.talkingPoints[i].Content || !vm.talkingPoints[i].Title) {
-               return false;
-           }
-       }
+    function validateTalkingPoints() {
+        for (var i = 0; i < vm.talkingPoints.length; i++) {
+            if (!vm.talkingPoints[i].Content || !vm.talkingPoints[i].Title) {
+                PlaybookWizardStore.setValidation('insights', false);
+                return false;
+            }
+        }
 
-       return true;
+        PlaybookWizardStore.setValidation('insights', true);
+        return true;
     };
+
+    $scope.$watch(validateTalkingPoints);
 });
