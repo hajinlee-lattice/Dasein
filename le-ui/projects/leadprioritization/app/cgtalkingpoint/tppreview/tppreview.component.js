4b9a5d2a43c5bb58c4207e377822bd7451cfe41c
dhom <dhom@479f5059-62a3-4c39-9342-9852e3a68435>
2017-06-08 17:35:14 +0000

    Review Request 10024 fix dante iframe for talking point

--- le-ui/projects/leadprioritization/app/cgtalkingpoint/tppreview/tppreview.component.js
+++ le-ui/projects/leadprioritization/app/cgtalkingpoint/tppreview/tppreview.component.js
@@ -5,50 +5,65 @@ angular.module('lp.cg.talkingpoint.preview', [])
         replace: true,
         scope: {},
         templateUrl: 'app/cgtalkingpoint/tppreview/tppreview.component.html',
-        controller: 'cgTalkingPointPreviewCtrl'
+        controller: 'cgTalkingPointPreviewCtrl',
+        controllerAs: 'vm'
     };
 })
 .controller('cgTalkingPointPreviewCtrl', function ($scope, $sce, $element, CgTalkingPointStore) {
+    var talkingPoints = CgTalkingPointStore.getTalkingPoints();
+    var iframe = null;
 
-    var iframe = $element.find('#tppreview_iframe')[0];
-
-    $scope.leadPreviewObject = CgTalkingPointStore.generateLeadPreviewObject();
-    CgTalkingPointStore.getAccounts().then(function(accounts) {
-        $scope.accounts = accounts;
-
-        return CgTalkingPointStore.getDanteUrl();
-    }).then(function(danteUrl) {
-        $scope.sceIframeSrc = $sce.trustAsResourceUrl(danteUrl);
-    }).then(function() {
-        $scope.selected = $scope.accounts[0];
-        $scope.playName = 'Foo play';
-        $scope.playDescription = 'This is bar play';
-        $scope.leadPreviewObject.notionObject.SalesforceAccountID = $scope.selected.id;
-        $scope.leadPreviewObject.notionObject.PlayDisplayName = $scope.playName;
-        $scope.leadPreviewObject.notionObject.PlayDescription = $scope.playDescription;
-        $scope.leadPreviewObject.notionObject.TalkingPoints = CgTalkingPointStore.getTalkingPoints();
-        $scope.hasTalkingPoints = $scope.leadPreviewObject.notionObject.TalkingPoints.length > 0;
+    var vm = this;
+    angular.extend(vm, {
+        hasTalkingPoints: talkingPoints.length > 0,
+        leadPreviewObject: null,
+        accounts: null,
+        selected: null,
+        sceIframeSrc: null
     });
 
-    window.addEventListener('message', handleLpiPreviewInit);
+    vm.init = function() {
+        iframe = $element.find('#tppreview_iframe')[0];
+        window.addEventListener('message', handleLpiPreviewInit);
+
+        vm.leadPreviewObject = CgTalkingPointStore.generateLeadPreviewObject();
+        CgTalkingPointStore.getAccounts().then(function(accounts) {
+            vm.accounts = accounts;
+
+            return CgTalkingPointStore.getDanteUrl();
+        }).then(function(danteUrl) {
+            vm.sceIframeSrc = $sce.trustAsResourceUrl(danteUrl);
+        }).then(function() {
+            vm.selected = vm.accounts[0];
+            vm.leadPreviewObject.notionObject.SalesforceAccountID = vm.selected.id;
+            vm.leadPreviewObject.notionObject.PlayDisplayName = 'Foo';
+            vm.leadPreviewObject.notionObject.PlayDescription = 'Bar';
+            vm.leadPreviewObject.notionObject.TalkingPoints = talkingPoints;
+        });
+    };
 
     function handleLpiPreviewInit(evt) {
         if (evt.data === 'initLpiPreview') {
-            emitLeadObject();
             window.removeEventListener('message', handleLpiPreviewInit);
+            emitLeadObject();
         }
     }
 
-    $scope.onAccountChange = function() {
-        $scope.leadPreviewObject.notionObject.SalesforceAccountID = $scope.selected.id;
+    function emitLeadObject() {
+        iframe.contentWindow.postMessage(vm.leadPreviewObject,'*')
+    }
+
+    vm.onAccountChange = function() {
+        vm.leadPreviewObject.notionObject.SalesforceAccountID = vm.selected.id;
         emitLeadObject();
     };
 
-    function emitLeadObject() {
-        iframe.contentWindow.postMessage($scope.leadPreviewObject,'*')
-    }
 
     $scope.$on('$destroy', function () {
         window.removeEventListener('message', handleLpiPreviewInit);
     });
+
+    if (vm.hasTalkingPoints) {
+        vm.init();
+    }
 });
