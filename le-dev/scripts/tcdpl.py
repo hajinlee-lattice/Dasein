51d64e5a3591ce2737bdb76789719736acec2bed
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-06-10 00:12:47 +0000

    Review Request 9999 remove objectapi from local ms module list

--- le-dev/scripts/tcdpl.py
+++ le-dev/scripts/tcdpl.py
@@ -30,8 +30,7 @@ MS_MODULES = ['dataflowapi', 'eai', 'metadata', 'modeling', 'propdata', 'scoring
               'modelquality', 'sqoop', 'datacloudapi', 'objectapi', 'dante']
 
 # ms modules for pre check in
-COMMON_MODULES = ['dataflowapi', 'eai', 'metadata', 'modeling', 'scoring', 'workflowapi', 'quartz', 'sqoop',
-                  'objectapi']
+COMMON_MODULES = ['dataflowapi', 'eai', 'metadata', 'modeling', 'scoring', 'workflowapi', 'quartz', 'sqoop']
 
 # apps for pre check in
 COMMON_APPS = ['admin', 'pls', 'microservice', 'playmaker', 'oauth2', 'scoringapi', 'matchapi']
