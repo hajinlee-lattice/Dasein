28b9a938faa4b650a1b6cd35477469366c0998ef
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-07-04 20:37:22 +0000

    Review Request 10338 fix dbdiff

--- le-dev/scripts/dbdiff.sh
+++ le-dev/scripts/dbdiff.sh
@@ -19,7 +19,7 @@ pushd $WSHOME/le-db &&
 mvn -DskipTests clean install &&
 popd
 
-echo 'using mysqldiff to find db diff ...'
+echo 'using mysqldiff to find db diff in ${DB} ...'
 mysqldiff \
     --server1=root:${PASSWORD}@${AURORA_URL} \
     --server2=root:welcome@127.0.0.1 \
@@ -29,6 +29,6 @@ mysqldiff \
     --difftype=sql \
     ${DB}:${DB} > ${ENV}_diff.sql
 
-python combine_sql.py -d ${ENV}_diff.sql -g ${WSHOME}/le-db/ddl_pls_multitenant_mysql5innodb.sql -o ${ENV}_upgrade.sql
+python ${WSHOME}/le-dev/scripts/combine_sql.py -b ${DB} -d ${ENV}_diff.sql -g ${WSHOME}/le-db/ddl_pls_multitenant_mysql5innodb.sql -o ${ENV}_upgrade.sql
 
 echo "upgrade script is generated at ${PWD}/${ENV}_upgrade.sql and ${PWD}/${ENV}_upgrade.sql.2"
