c0dc37cfbb0a95afd9c811384d6d83055c4f8e24
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2017-07-07 07:25:56 +0000

    Review Request 10420: add data feed profile table

--- le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
+++ le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
@@ -7,3 +7,17 @@ ALTER TABLE `PLS_MultiTenant`.`DATAFEED_TASK`
 
 ALTER TABLE `PLS_MultiTenant`.`PLAY_LAUNCH`
   ADD COLUMN `APPLICATION_ID` varchar(255) DEFAULT NULL;
+
+alter table `DATAFEED` add (`ACTIVE_PROFILE` bigint);
+
+create table `DATAFEED_PROFILE` (
+`PID` bigint not null auto_increment unique, 
+`FEED_EXEC_ID` bigint not null, 
+`WORKFLOW_ID` bigint, 
+FK_FEED_ID bigint not null, 
+primary key (`PID`)) ENGINE=InnoDB;
+
+alter table `DATAFEED_PROFILE` add index FK8299D49299B68AE3 (FK_FEED_ID), 
+add constraint FK8299D49299B68AE3 foreign key (FK_FEED_ID) references `DATAFEED` (`PID`) on delete cascade;
+
+
