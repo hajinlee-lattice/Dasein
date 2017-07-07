03c1cf8a527ab0074cdc989abd4a4999fd0d32fd
dzheng <dzheng@479f5059-62a3-4c39-9342-9852e3a68435>
2017-07-07 05:15:33 +0000

    Review Request 10425: Try to fix compilation error in ConsolidateAccountData and CalculateStatsStep

--- le-serviceflows/core/src/main/java/com/latticeengines/serviceflows/workflow/etl/BaseTransformWrapperStep.java
+++ le-serviceflows/core/src/main/java/com/latticeengines/serviceflows/workflow/etl/BaseTransformWrapperStep.java
@@ -32,6 +32,12 @@ public abstract class BaseTransformWrapperStep<T extends BaseWrapperStepConfigur
     @Autowired
     protected YarnClient yarnClient;
 
+    @Value("${pls.cdl.transform.workflow.mem.mb}")
+    protected int workflowMemMb;
+
+    @Value("${pls.cdl.transform.workflow.mem.mb.max}")
+    protected int workflowMemMbMax;
+
     @Value("${pls.cdl.transform.cascading.partitions}")
     protected int cascadingPartitions;
 
