4ce8fc17b065edf6405a1c2456fdcee074a2635b
akumar <akumar@479f5059-62a3-4c39-9342-9852e3a68435>
2017-07-10 21:21:00 +0000

    Review request 10452 - DEVOPS-2725 - adding stack trace for ZK feature flag issue

--- le-camille/src/main/java/com/latticeengines/camille/exposed/featureflags/CamilleFeatureFlagProvider.java
+++ le-camille/src/main/java/com/latticeengines/camille/exposed/featureflags/CamilleFeatureFlagProvider.java
@@ -206,7 +206,7 @@ public class CamilleFeatureFlagProvider implements FeatureFlagProvider {
         try {
             doc = definitionCache.get();
         } catch (Exception e) {
-            log.warn("Exception occurred attempting to retrieve feature flag definitions");
+            log.warn("Exception occurred attempting to retrieve feature flag definitions", e);
             doc = null;
         }
 
@@ -228,7 +228,7 @@ public class CamilleFeatureFlagProvider implements FeatureFlagProvider {
             toReturn = valueCache.get(new CustomerSpaceScope(space), new Path("/" + PathConstants.FEATURE_FLAGS_FILE),
                     FeatureFlagValueMap.class);
         } catch (Exception e) {
-            log.warn("Exception occurred attempting to retrieve feature flags");
+            log.warn("Exception occurred attempting to retrieve feature flags", e);
             toReturn = null;
         }
         if (toReturn == null) {
