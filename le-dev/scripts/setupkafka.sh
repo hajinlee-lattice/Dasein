8bc78a644b483d344a34ed05d17c99bd98cb714a
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-07-14 16:54:19 +0000

    Review Request 10498 add setupkafka.sh

---
+++ le-dev/scripts/setupkafka.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Test for required env variables
+printf "%s\n" "${CONFLUENT_HOME:?Must set CONFLUENT_HOME to your root dir for confluent platform}"
+printf "%s\n" "${WSHOME:?You must set WSHOME}"
+
+echo "Compile datafabric connect shaded jar"
+pushd ${WSHOME}/le-datafabric && \
+mvn -f pom-connect-shaded.xml -DskipTests package && \
+popd
+
+echo "Copy datafabric connect shaded jar to confluent home"
+cp ${WSHOME}/le-datafabric/target/le-datafabric-connect-*-shaded.jar ${CONFLUENT_HOME}/connectors
+ls -al ${CONFLUENT_HOME}/connectors
