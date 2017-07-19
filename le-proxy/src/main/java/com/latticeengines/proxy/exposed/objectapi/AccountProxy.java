438a44155ad2589644bcf386b1a4d5f588daed4f
bnguyen <bnguyen@479f5059-62a3-4c39-9342-9852e3a68435>
2017-07-19 05:10:17 +0000

    Review Request 10577 PLS-4306 Implement API in AccountProxy for reading account data for given segment/materialized query

--- le-proxy/src/main/java/com/latticeengines/proxy/exposed/objectapi/AccountProxy.java
+++ le-proxy/src/main/java/com/latticeengines/proxy/exposed/objectapi/AccountProxy.java
@@ -1,21 +1,35 @@
 package com.latticeengines.proxy.exposed.objectapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.latticeengines.domain.exposed.camille.CustomerSpace;
+import com.latticeengines.domain.exposed.query.BusinessEntity;
 import com.latticeengines.domain.exposed.query.DataPage;
 import com.latticeengines.domain.exposed.query.DataRequest;
+import com.latticeengines.domain.exposed.query.EntityLookup;
+import com.latticeengines.domain.exposed.query.PageFilter;
+import com.latticeengines.domain.exposed.query.Query;
 import com.latticeengines.domain.exposed.query.Restriction;
+import com.latticeengines.domain.exposed.query.frontend.FrontEndQuery;
+import com.latticeengines.domain.exposed.util.QueryTranslator;
+import com.latticeengines.domain.exposed.util.ReverseQueryTranslator;
 import com.latticeengines.network.exposed.objectapi.AccountInterface;
 import com.latticeengines.proxy.exposed.MicroserviceRestApiProxy;
 
 @Component("accountProxy")
 public class AccountProxy extends MicroserviceRestApiProxy implements AccountInterface {
+
+    @Autowired
+    private EntityProxy entityProxy;
+
     public AccountProxy() {
         super("objectapi/customerspaces");
     }
 
     @Override
     public long getAccountsCount(String customerSpace, String start, DataRequest dataRequest) {
+        customerSpace = CustomerSpace.parse(customerSpace).toString();
         String url = constructUrl("/{customerSpace}/accounts/count?start={start}", customerSpace, start);
         return post("get Count", url, dataRequest, Integer.class);
     }
@@ -23,17 +37,29 @@ public class AccountProxy extends MicroserviceRestApiProxy implements AccountInt
     @Override
     public DataPage getAccounts(String customerSpace, String start, Integer offset, Integer pageSize,
             DataRequest dataRequest) {
+        customerSpace = CustomerSpace.parse(customerSpace).toString();
         String url = constructUrl("/{customerSpace}/accounts/data?start={start}&offset={offset}&pageSize={pagesize}",
                 customerSpace, start, offset, pageSize);
         return post("get Data", url, dataRequest, DataPage.class);
     }
 
-    public int getSegmentAccountsCount(String customerSpace, Restriction segmentRestrictionQuery) {
-        return 0;
+    public long getAccountsCount(String customerSpace, Restriction restriction) {
+        customerSpace = CustomerSpace.parse(customerSpace).toString();
+        FrontEndQuery frontEndQuery = new FrontEndQuery();
+        frontEndQuery.setFrontEndRestriction(ReverseQueryTranslator.translateRestriction(restriction));
+        Query query = QueryTranslator.translate(frontEndQuery, null);
+        query.addLookup(new EntityLookup(BusinessEntity.Account));
+        return entityProxy.getCount(customerSpace, query);
     }
 
-    public DataPage getSegmentAccounts(String customerSpace, Restriction segmentRestrictionQuery, Integer offset,
-            Integer pageSize, DataRequest dataRequest) {
-        return null;
+    public DataPage getAccounts(String customerSpace, Restriction restriction, Integer offset, Integer pageSize,
+            DataRequest dataRequest) {
+        customerSpace = CustomerSpace.parse(customerSpace).toString();
+        PageFilter pageFilter = new PageFilter(offset, pageSize);
+        FrontEndQuery frontEndQuery = new FrontEndQuery();
+        frontEndQuery.setFrontEndRestriction(ReverseQueryTranslator.translateRestriction(restriction));
+        frontEndQuery.setPageFilter(pageFilter);
+        Query query = QueryTranslator.translate(frontEndQuery, null);
+        return entityProxy.getData(customerSpace, query);
     }
 }
