107b083ef54a483f07e1fdc6002e8c80482fbdc0
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-07-20 19:57:32 +0000

    Review Request 10498 get rid of qacluster_aws and prodcluster_aws

--- le-config/src/main/python/aggregate.py
+++ le-config/src/main/python/aggregate.py
@@ -5,7 +5,6 @@ PROPERTY_DIR = "/conf/env/"
 PROPERTY_FILE_SUFFIX = "*.properties"
 LINE_SEPERATOR = "\n=============================\n"
 ENVIRONMENTS=('dev', 'devcluster', 'qacluster','prodcluster')
-ENV_WITH_AWS=('qacluster','prodcluster')
 
 WSHOME=os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
 
@@ -31,32 +30,6 @@ def main():
                 aggregated += aggregate_props(dir_name, keys)
 
         target_path=os.path.join(confdir(environment), 'latticeengines.properties')
-        if os.path.isfile(target_path):
-            os.remove(target_path)
-        with open(target_path, 'w') as f:
-            print 'Writing to ' + target_path
-            f.write(aggregated)
-
-    for environment in ENV_WITH_AWS:
-        aggregated=""
-        keys={}
-        for dir_name, _, _ in os.walk(WSHOME):
-            end_with = PROPERTY_DIR + environment + "_aws"
-            if dir_name[-len(end_with):] == end_with \
-                    and 'le-config' not in dir_name \
-                    and 'le-docker' not in dir_name \
-                    and 'le-awsenvironment' not in dir_name:
-                aggregated += aggregate_props(dir_name, keys)
-
-        for dir_name, _, _ in os.walk(WSHOME):
-            end_with = PROPERTY_DIR + environment
-            if dir_name[-len(end_with):] == end_with \
-                    and 'le-config' not in dir_name \
-                    and 'le-docker' not in dir_name \
-                    and 'le-awsenvironment' not in dir_name:
-                aggregated += aggregate_props(dir_name, keys, quiet=True)
-
-        target_path=os.path.join(confdir(environment + "_aws"), 'latticeengines.properties')
         if os.path.isfile(target_path):
             os.remove(target_path)
         with open(target_path, 'w') as f:
