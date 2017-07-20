ae8595176034f89e54cdb2b8d94940fca0bd6848
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-07-20 18:43:16 +0000

    Review Request 10498 try to fix ui docker

--- le-docker/src/main/scripts/nodejs/le-express/start.sh
+++ le-docker/src/main/scripts/nodejs/le-express/start.sh
@@ -1,28 +1,30 @@
 #!/usr/bin/env bash
 
+APP_ROOT=/opt/node/le-ui
+
 if [ -z "${LE_ENVIRONMENT}" ]; then
     echo "must specify LE_ENVIRONMENT"
     exit 1
 fi;
 
-if [ ! -f "/opt/node/app/ENV_VARS" ]; then
+if [ ! -f "${APP_ROOT}/ENV_VARS" ]; then
     echo "copying ENV_VARS file for LE_ENVIRONMENT=${LE_ENVIRONMENT}"
-    cp /opt/node/app/conf/env/${LE_ENVIRONMENT}/ENV_VARS /opt/node/app
+    cp ${APP_ROOT}/conf/env/${LE_ENVIRONMENT}/ENV_VARS ${APP_ROOT}
 fi
 
 if [ "${INSTALL_MODE}" == "EXTERNAL" ]; then
-    sed -i "s|{{NODE_APPS}}|leui|g" /opt/node/app/ENV_VARS
-    sed -i "s/export ADMIN_HTTPS_PORT=/export ADMIN_HTTPS_PORT=false/g" /opt/node/app/ENV_VARS
+    sed -i "s|{{NODE_APPS}}|leui|g" ${APP_ROOT}/ENV_VARS
+    sed -i "s/export ADMIN_HTTPS_PORT=/export ADMIN_HTTPS_PORT=false/g" ${APP_ROOT}/ENV_VARS
 elif [ "${INSTALL_MODE}" == "INTERNAL" ]; then
-    sed -i "s|{{NODE_APPS}}|leadmin|g" /opt/node/app/ENV_VARS
-    sed -i "s/export HTTPS_PORT=/export HTTPS_PORT=false/g" /opt/node/app/ENV_VARS
+    sed -i "s|{{NODE_APPS}}|leadmin|g" ${APP_ROOT}/ENV_VARS
+    sed -i "s/export HTTPS_PORT=/export HTTPS_PORT=false/g" ${APP_ROOT}/ENV_VARS
 else
-    sed -i "s|{{NODE_APPS}}|leui,leadmin|g" /opt/node/app/ENV_VARS
+    sed -i "s|{{NODE_APPS}}|leui,leadmin|g" ${APP_ROOT}/ENV_VARS
 fi
 
-sed -i "/export LOGGING=/d" /opt/node/app/ENV_VARS
+sed -i "/export LOGGING=/d" ${APP_ROOT}/ENV_VARS
 
-source /opt/node/app/ENV_VARS
+source ${APP_ROOT}/ENV_VARS
 
 npm install debug
-node /opt/node/app/app.js
+node ${APP_ROOT}/app.js
