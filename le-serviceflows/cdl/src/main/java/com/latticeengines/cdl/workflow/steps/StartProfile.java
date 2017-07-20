fb17d30e1b2c42a7e07356b34d3ef309b16c1fed
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2017-07-20 18:03:40 +0000

    Review Request 10618: skip sort contact if master table can't be found

--- le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/workflow/steps/StartProfile.java
+++ le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/workflow/steps/StartProfile.java
@@ -3,8 +3,12 @@ package com.latticeengines.cdl.workflow.steps;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.latticeengines.domain.exposed.metadata.Table;
+import com.latticeengines.domain.exposed.metadata.TableRoleInCollection;
 import com.latticeengines.domain.exposed.metadata.datafeed.DataFeedProfile;
 import com.latticeengines.domain.exposed.serviceflows.cdl.steps.CalculateStatsStepConfiguration;
+import com.latticeengines.domain.exposed.serviceflows.cdl.steps.SortContactStepConfiguration;
+import com.latticeengines.proxy.exposed.metadata.DataCollectionProxy;
 import com.latticeengines.proxy.exposed.metadata.DataFeedProxy;
 import com.latticeengines.serviceflows.workflow.core.BaseWorkflowStep;
 
@@ -13,6 +17,9 @@ public class StartProfile extends BaseWorkflowStep<CalculateStatsStepConfigurati
 
     @Autowired
     private DataFeedProxy dataFeedProxy;
+
+    @Autowired
+    private DataCollectionProxy dataCollectionProxy;
 
     @Override
     public void execute() {
@@ -24,6 +31,16 @@ public class StartProfile extends BaseWorkflowStep<CalculateStatsStepConfigurati
             throw new RuntimeException(
                     String.format("current active profile has a workflow id %s, which is different from %s ",
                             profile.getWorkflowId(), jobId));
+        }
+
+        Table contactMasterTable = dataCollectionProxy.getTable(configuration.getCustomerSpace().toString(),
+                TableRoleInCollection.ConsolidatedContact);
+        if (contactMasterTable == null) {
+            log.info("Cannot find the contact master table in default collection");
+            SortContactStepConfiguration sortContactStepConfig = getConfigurationFromJobParameters(
+                    SortContactStepConfiguration.class);
+            sortContactStepConfig.setSkipStep(true);
+            putObjectInContext(SortContactStepConfiguration.class.getName(), sortContactStepConfig);
         }
     }
 
