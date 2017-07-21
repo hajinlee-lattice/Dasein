f4a479e676e8fcc2ec9d9480c28eea6e7327ddb0
akumar <akumar@479f5059-62a3-4c39-9342-9852e3a68435>
2017-07-21 04:29:05 +0000

    Review request 10625 - adding a temporary workaround to avoid time conversion issue in AccountProxy

--- le-objectapi/src/main/java/com/latticeengines/objectapi/controller/AccountResource.java
+++ le-objectapi/src/main/java/com/latticeengines/objectapi/controller/AccountResource.java
@@ -1,5 +1,6 @@
 package com.latticeengines.objectapi.controller;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,14 +46,15 @@ public class AccountResource implements AccountInterface {
             @ApiParam(value = "The UTC timestamp of last modification in ISO8601 format", required = false) @RequestParam(value = "start", required = false) String start,
             @RequestBody DataRequest dataRequest) {
         customerSpace = CustomerSpace.parse(customerSpace).toString();
+        start = fixDateStringIfNeeded(start);
         Query query = accountQueryService.generateAccountQuery(start, dataRequest);
         return queryEvaluatorService.getCount(customerSpace, query);
     }
 
     /*
      * Based on
-     * https://confluence.lattice-engines.com/display/ENG/PlayMakerAPI+-+Datastore+proposal+for+Recommendation%
-     * 2C+Account+Extension+and+Plays
+     * https://confluence.lattice-engines.com/display/ENG/PlayMakerAPI+-+
+     * Datastore+proposal+for+Recommendation% 2C+Account+Extension+and+Plays
      * 
      */
     @RequestMapping(value = "/data", method = RequestMethod.POST)
@@ -65,8 +67,22 @@ public class AccountResource implements AccountInterface {
             @ApiParam(value = "Number of records returned above offset (max is 250 records per request)", required = true) @RequestParam(value = "pageSize", required = true) Integer pageSize,
             @RequestBody DataRequest dataRequest) {
         customerSpace = CustomerSpace.parse(customerSpace).toString();
+        start = fixDateStringIfNeeded(start);
         Query query = accountQueryService.generateAccountQuery(start, offset, pageSize, dataRequest);
         return queryEvaluatorService.getData(customerSpace, query);
     }
 
+    // TODO - remove it once AccountProxy/Resource is fixed. For some reason it
+    // is not able to handle date of format: java.text.ParseException:
+    // Unparseable date: 1970-01-01T00:00:00%2B0000
+    //
+    // added following code to unblock demo and integration
+    private String fixDateStringIfNeeded(String start) {
+        String HTTP_PLUS_ENCODING = "%2B";
+        String PLUS = "+";
+        if (StringUtils.isNotEmpty(start) && StringUtils.contains(start, HTTP_PLUS_ENCODING)) {
+            start = StringUtils.replace(start, HTTP_PLUS_ENCODING, PLUS);
+        }
+        return start;
+    }
 }
