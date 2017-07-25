47c57b7bff1376261e281a5240404f26412adf28
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-07-25 21:41:35 +0000

    Review Request 10667 not checking existence of env var jacoco agent, in case jacocoagent jar is not localized

--- le-common/src/main/java/com/latticeengines/common/exposed/util/JacocoUtils.java
+++ le-common/src/main/java/com/latticeengines/common/exposed/util/JacocoUtils.java
@@ -4,20 +4,26 @@ import java.io.File;
 import java.util.Properties;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class JacocoUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(JacocoUtils.class);
 
     public static void setJacoco(Properties containerProperties, String execFile) {
         String jacocoDataDir = PropertyUtils.getProperty("yarn.jacoco.data.dir");
         boolean jacocoEnabled = Boolean.valueOf(PropertyUtils.getProperty("yarn.jacoco.enabled"));
         if (jacocoEnabled) {
             String jacocoAgentFile = System.getenv("JACOCO_AGENT_FILE");
-            if (StringUtils.isBlank(jacocoAgentFile) || !new File(jacocoAgentFile).exists()) {
+            if (StringUtils.isBlank(jacocoAgentFile)) {
                 jacocoAgentFile = "jacocoagent.jar";
             }
             String jacocoDestFile = String.format("%s/%s.exec", jacocoDataDir, execFile);
             containerProperties.put("JACOCO_AGENT_FILE", jacocoAgentFile);
             containerProperties.put("JACOCO_DEST_FILE", jacocoDestFile);
+            log.info("Using jacoco agent " + jacocoAgentFile);
+            log.info("Using jacoco dest file " + jacocoDestFile);
         }
     }
 
