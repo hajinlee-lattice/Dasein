a9d5c83b5cd2c23c56626682226575624eab682e
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-07-27 22:17:45 +0000

    Review Request 10667 add products api

--- le-app/src/main/java/com/latticeengines/app/exposed/controller/CommonTenantConfigResource.java
+++ le-app/src/main/java/com/latticeengines/app/exposed/controller/CommonTenantConfigResource.java
@@ -1,5 +1,9 @@
 package com.latticeengines.app.exposed.controller;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.latticeengines.app.exposed.service.CommonTenantConfigService;
+import com.latticeengines.domain.exposed.admin.LatticeProduct;
 import com.latticeengines.domain.exposed.camille.featureflags.FeatureFlagValueMap;
 import com.latticeengines.security.exposed.util.MultiTenantContext;
 
@@ -26,5 +31,17 @@ public class CommonTenantConfigResource {
     @ApiOperation(value = "Get tenant's feature flags")
     public FeatureFlagValueMap getFeatureFlags() {
         return configService.getFeatureFlags(MultiTenantContext.getTenant().getId());
+    }
+
+    @RequestMapping(value = "/products", method = RequestMethod.GET, headers = "Accept=application/json")
+    @ResponseBody
+    @ApiOperation(value = "Get tenant's feature flags")
+    public List<String> getProducts() {
+        List<LatticeProduct> products = configService.getProducts(MultiTenantContext.getTenant().getId());
+        if (products != null) {
+            return products.stream().map(LatticeProduct::getName).collect(Collectors.toList());
+        } else {
+            return Collections.emptyList();
+        }
     }
 }
