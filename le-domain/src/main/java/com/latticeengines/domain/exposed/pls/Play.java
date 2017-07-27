e655903857ad7654d32ea12d7e3a1b9f25640bd7
ygao <ygao@479f5059-62a3-4c39-9342-9852e3a68435>
2017-07-27 20:26:20 +0000

    review request 10693 Increase the the length of description of Play object

--- le-domain/src/main/java/com/latticeengines/domain/exposed/pls/Play.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/pls/Play.java
@@ -65,7 +65,7 @@ public class Play implements HasName, HasPid, HasTenantId {
     private String displayName;
 
     @JsonProperty("description")
-    @Column(name = "DESCRIPTION", length = 10000, nullable = true)
+    @Column(name = "DESCRIPTION", length = 8192, nullable = true)
     private String description;
 
     @JsonProperty("segment")
