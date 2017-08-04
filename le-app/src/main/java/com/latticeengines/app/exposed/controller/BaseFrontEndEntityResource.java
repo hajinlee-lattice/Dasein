a71d81aa5573b9ef69fe8c2d75d413cc23a6d02d
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-08-04 18:32:39 +0000

    Review Request 10842 cache entity queries

--- le-app/src/main/java/com/latticeengines/app/exposed/controller/BaseFrontEndEntityResource.java
+++ le-app/src/main/java/com/latticeengines/app/exposed/controller/BaseFrontEndEntityResource.java
@@ -1,7 +1,12 @@
 package com.latticeengines.app.exposed.controller;
 
+import java.util.concurrent.TimeUnit;
+
 import org.apache.commons.lang3.StringUtils;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import com.latticeengines.common.exposed.util.JsonUtils;
 import com.latticeengines.domain.exposed.metadata.MetadataSegment;
 import com.latticeengines.domain.exposed.query.BusinessEntity;
 import com.latticeengines.domain.exposed.query.DataPage;
@@ -18,14 +23,36 @@ public abstract class BaseFrontEndEntityResource {
 
     private final SegmentProxy segmentProxy;
 
+    private final LoadingCache<String, Long> countCache;
+
+    private final LoadingCache<String, DataPage> dataCache;
+
     BaseFrontEndEntityResource(EntityProxy entityProxy, SegmentProxy segmentProxy) {
         this.entityProxy = entityProxy;
         this.segmentProxy = segmentProxy;
+        countCache = Caffeine.newBuilder() //
+                .maximumSize(1000) //
+                .expireAfterWrite(10, TimeUnit.MINUTES) //
+                .refreshAfterWrite(1, TimeUnit.MINUTES) //
+                .build(this::getCountFromObjectApi);
+        dataCache = Caffeine.newBuilder() //
+                .maximumSize(100) //
+                .expireAfterWrite(10, TimeUnit.MINUTES) //
+                .refreshAfterWrite(1, TimeUnit.MINUTES) //
+                .build(this::getDataFromObjectApi);
     }
 
     public long getCount(FrontEndQuery frontEndQuery, String segment) {
         appendSegmentRestriction(frontEndQuery, segment);
-        return entityProxy.getCount(MultiTenantContext.getCustomerSpace().toString(), getMainEntity(), frontEndQuery);
+        String tenantId = MultiTenantContext.getCustomerSpace().getTenantId();
+        return countCache.get(String.format("%s:%s", tenantId, JsonUtils.serialize(frontEndQuery)));
+    }
+
+    private long getCountFromObjectApi(String serializedKey) {
+        String tenantId = serializedKey.substring(0, serializedKey.indexOf(":"));
+        String serializedQuery = serializedKey.substring(tenantId.length() + 1);
+        FrontEndQuery frontEndQuery = JsonUtils.deserialize(serializedQuery, FrontEndQuery.class);
+        return entityProxy.getCount(tenantId, getMainEntity(), frontEndQuery);
     }
 
     public long getCountForRestriction(FrontEndRestriction restriction) {
@@ -38,7 +65,15 @@ public abstract class BaseFrontEndEntityResource {
 
     public DataPage getData(FrontEndQuery frontEndQuery, String segment) {
         appendSegmentRestriction(frontEndQuery, segment);
-        return entityProxy.getData(MultiTenantContext.getCustomerSpace().toString(), getMainEntity(), frontEndQuery);
+        String tenantId = MultiTenantContext.getCustomerSpace().getTenantId();
+        return dataCache.get(String.format("%s:%s", tenantId, JsonUtils.serialize(frontEndQuery)));
+    }
+
+    private DataPage getDataFromObjectApi(String serializedKey) {
+        String tenantId = serializedKey.substring(0, serializedKey.indexOf(":"));
+        String serializedQuery = serializedKey.substring(tenantId.length() + 1);
+        FrontEndQuery frontEndQuery = JsonUtils.deserialize(serializedQuery, FrontEndQuery.class);
+        return entityProxy.getData(tenantId, getMainEntity(), frontEndQuery);
     }
 
     private void appendSegmentRestriction(FrontEndQuery frontEndQuery, String segment) {
