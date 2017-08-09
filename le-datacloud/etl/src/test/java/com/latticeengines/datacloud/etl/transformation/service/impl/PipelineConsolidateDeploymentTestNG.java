ca2adf05ac469332687ff175914d290ac3e6df46
lming <lming@479f5059-62a3-4c39-9342-9852e3a68435>
2017-08-09 04:41:44 +0000

    Review Request 10905: CDL consolidate - improve matching performance

--- le-datacloud/etl/src/test/java/com/latticeengines/datacloud/etl/transformation/service/impl/PipelineConsolidateDeploymentTestNG.java
+++ le-datacloud/etl/src/test/java/com/latticeengines/datacloud/etl/transformation/service/impl/PipelineConsolidateDeploymentTestNG.java
@@ -16,6 +16,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
