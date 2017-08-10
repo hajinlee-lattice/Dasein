ed957dce1d17b913947b548863c3f21e6bf2b6a6
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-08-10 22:45:33 +0000

    Review Request 10931 fix lp health check

--- le-serviceapps/lp/src/main/java/com/latticeengines/apps/lp/controller/HealthResource.java
+++ le-serviceapps/lp/src/main/java/com/latticeengines/apps/lp/controller/HealthResource.java
@@ -1,5 +1,6 @@
 package com.latticeengines.apps.lp.controller;
 
+import com.latticeengines.apps.core.annotation.NoCustomerSpace;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,12 +15,13 @@ import io.swagger.annotations.ApiOperation;
 @Api(value = "health", description = "REST resource for checking health of service app")
 @RestController
 @RequestMapping("/health")
-public class HealthCheckResource {
+public class HealthResource {
 
     @RequestMapping(value = "", method = RequestMethod.GET, headers = "Accept=application/json")
     @ResponseBody
     @ApiOperation(value = "Health check")
     @NoMetricsLog
+    @NoCustomerSpace
     public StatusDocument healthCheck() {
         return StatusDocument.online();
     }
