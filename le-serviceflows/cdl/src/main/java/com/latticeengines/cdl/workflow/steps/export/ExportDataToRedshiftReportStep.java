f8f54104e21586db00d3b5e4d92116df9d66294b
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-08-12 19:29:18 +0000

    Review Request 10960 revert change to skip report to redshift

--- le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/workflow/steps/export/ExportDataToRedshiftReportStep.java
+++ le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/workflow/steps/export/ExportDataToRedshiftReportStep.java
@@ -1,9 +1,8 @@
 package com.latticeengines.cdl.workflow.steps.export;
 
 import java.util.Map;
+import java.util.Optional;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.latticeengines.domain.exposed.metadata.Table;
@@ -15,19 +14,14 @@ import com.latticeengines.serviceflows.workflow.report.BaseReportStep;
 @Component("exportDataToRedshiftReportStep")
 public class ExportDataToRedshiftReportStep extends BaseReportStep<BaseReportStepConfiguration> {
 
-    private static final Logger log = LoggerFactory.getLogger(ExportDataToRedshiftReportStep.class);
-
     @Override
     public void execute() {
         Map<BusinessEntity, Table> entityTableMap = getMapObjectFromContext(TABLE_GOING_TO_REDSHIFT,
                 BusinessEntity.class, Table.class);
-        if (entityTableMap != null && !entityTableMap.isEmpty()) {
-            entityTableMap.forEach((k, v) -> //
-                    getJson().put(k.getServingStore().name(), v.getExtracts().get(0).getProcessedRecords()));
-            super.execute();
-        } else {
-            log.info("No table is going to redshift, skip report.");
-        }
+        Optional.ofNullable(entityTableMap).ifPresent(map -> map.forEach((k, v) -> {
+            getJson().put(k.getServingStore().name(), v.getExtracts().get(0).getProcessedRecords());
+        }));
+        super.execute();
     }
 
     @Override
