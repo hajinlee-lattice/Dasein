f02087e7728bd24c02673c1cbc9546d7be8bff3f
ssakhalkar <ssakhalkar@479f5059-62a3-4c39-9342-9852e3a68435>
2017-08-14 22:50:58 +0000

    Review Request 10850

---
+++ le-datacloud/dataflow/src/main/java/com/latticeengines/datacloud/dataflow/transformation/ManualSeedEnrichDunsFlow.java
@@ -0,0 +1,102 @@
+package com.latticeengines.datacloud.dataflow.transformation;
+
+import java.util.Arrays;
+
+import org.springframework.stereotype.Component;
+
+import com.latticeengines.dataflow.exposed.builder.Node;
+import com.latticeengines.dataflow.exposed.builder.common.FieldList;
+import com.latticeengines.dataflow.exposed.builder.common.JoinType;
+import com.latticeengines.domain.exposed.datacloud.dataflow.TransformationFlowParameters;
+import com.latticeengines.domain.exposed.datacloud.transformation.configuration.impl.ManualSeedEnrichDunsConfig;
+import com.latticeengines.domain.exposed.datacloud.transformation.configuration.impl.TransformerConfig;
+
+import cascading.tuple.Fields;
+
+@Component(ManualSeedEnrichDunsFlow.DATAFLOW_BEAN_NAME)
+public class ManualSeedEnrichDunsFlow extends ConfigurableFlowBase<ManualSeedEnrichDunsConfig> {
+
+    public final static String DATAFLOW_BEAN_NAME = "ManSeedEnrichDunsFlow";
+    public final static String TRANSFORMER_NAME = "ManSeedEnrichDunsTransformer";
+
+    @Override
+    public String getDataFlowBeanName() {
+        return DATAFLOW_BEAN_NAME;
+    }
+
+    @Override
+    public String getTransformerName() {
+        return TRANSFORMER_NAME;
+    }
+
+    @Override
+    public Class<? extends TransformerConfig> getTransformerConfigClass() {
+        return ManualSeedEnrichDunsConfig.class;
+    }
+
+    @Override
+    public Node construct(TransformationFlowParameters parameters) {
+        ManualSeedEnrichDunsConfig config = getTransformerConfig(parameters);
+        /*
+         * 1-manualSeedData : ManualSeedCompanyName, ManualSeedPostalCode,
+         * ManualSeedCity, ManualSeedState, ManualSeedCountry
+         * 2-manualSeedWithoutZip : removing zipCode field 3 -
+         * 3-manualSeedWithoutZipCity : removing zipCode and city fields
+         */
+        // Set 1
+        Node manualSeedData = addSource(parameters.getBaseTables().get(0));
+        // Set 2
+        Node manualSeedWithoutZip = addSource(parameters.getBaseTables().get(1));
+        // Set 3
+        Node manualSeedWithoutZipCity = addSource(parameters.getBaseTables().get(2));
+
+        // rename & retain sets 2 and 3
+        manualSeedWithoutZip = manualSeedWithoutZip //
+                .rename(new FieldList(config.getManSeedDuns(), config.getManSeedId()),
+                        new FieldList(renameZip(config.getManSeedDuns()),
+                                renameZip(config.getManSeedId())));
+
+        // retain the field names to avoid mismatch in the column names
+        manualSeedWithoutZip = manualSeedWithoutZip //
+                .retain(new FieldList(renameZip(config.getManSeedDuns()), renameZip(config.getManSeedId())));
+        manualSeedWithoutZipCity = manualSeedWithoutZipCity //
+                .rename(new FieldList(config.getManSeedDuns(), config.getManSeedId()),
+                        new FieldList(renameZipCity(config.getManSeedDuns()),
+                                renameZipCity(config.getManSeedId())));
+
+        // retain the field names to avoid mismatch in the column names
+        manualSeedWithoutZipCity = manualSeedWithoutZipCity //
+                .retain(new FieldList(renameZipCity(config.getManSeedDuns()), renameZipCity(config.getManSeedId())));
+
+        // inner join all the 3 tables based on Id
+        Node combinedResultSet = manualSeedData //
+                .coGroup(new FieldList(config.getManSeedId()), //
+                        Arrays.asList(manualSeedWithoutZip, manualSeedWithoutZipCity), //
+                        Arrays.asList(
+                                new FieldList(renameZip(config.getManSeedId())),
+                                new FieldList(renameZipCity(config.getManSeedId()))),
+                        JoinType.INNER);
+
+        ManualSeedEnrichDunsFunction enrichDunsFunction = new ManualSeedEnrichDunsFunction(
+                new Fields(combinedResultSet.getFieldNamesArray()), config.getManSeedDuns(),
+                renameZip(config.getManSeedDuns()), renameZipCity(config.getManSeedDuns()));
+
+        combinedResultSet = combinedResultSet.apply(enrichDunsFunction,
+                new FieldList(combinedResultSet.getFieldNames()), combinedResultSet.getSchema(),
+                new FieldList(combinedResultSet.getFieldNames()), Fields.REPLACE);
+
+        // retain manual Seed fields
+        combinedResultSet = combinedResultSet //
+                .retain(new FieldList(manualSeedData.getFieldNames()));
+        return combinedResultSet;
+    }
+
+    private String renameZip(String field) {
+        return "Zip_" + field;
+    }
+
+    private String renameZipCity(String field) {
+        return "ZipCity_" + field;
+    }
+
+}
---
+++ le-datacloud/etl/src/test/java/com/latticeengines/datacloud/etl/transformation/service/impl/ManualSeedDunsEnrichTestNG.java
@@ -0,0 +1,173 @@
+package com.latticeengines.datacloud.etl.transformation.service.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.avro.generic.GenericRecord;
+import org.apache.commons.lang3.tuple.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.latticeengines.common.exposed.util.JsonUtils;
+import com.latticeengines.datacloud.core.source.Source;
+import com.latticeengines.datacloud.core.source.impl.GeneralSource;
+import com.latticeengines.datacloud.dataflow.transformation.ManualSeedEnrichDunsFlow;
+import com.latticeengines.datacloud.etl.transformation.service.TransformationService;
+import com.latticeengines.domain.exposed.datacloud.manage.TransformationProgress;
+import com.latticeengines.domain.exposed.datacloud.transformation.configuration.impl.ManualSeedEnrichDunsConfig;
+import com.latticeengines.domain.exposed.datacloud.transformation.configuration.impl.PipelineTransformationConfiguration;
+import com.latticeengines.domain.exposed.datacloud.transformation.step.TransformationStepConfig;
+
+public class ManualSeedDunsEnrichTestNG
+        extends TransformationServiceImplTestNGBase<PipelineTransformationConfiguration> {
+    private static final Logger log = LoggerFactory.getLogger(ManualSeedDunsEnrichTestNG.class);
+
+    GeneralSource source = new GeneralSource("ManualSeedEnrichedDuns");
+
+    GeneralSource baseSource1 = new GeneralSource("ManualSeedData");
+    GeneralSource baseSource2 = new GeneralSource("ManualSeedWithoutZip");
+    GeneralSource baseSource3 = new GeneralSource("ManualSeedWithoutZipAndCity");
+
+    @Test(groups = "pipeline1", enabled = true)
+    public void testTransformation() {
+        prepareManualSeedDuns();
+        prepareManSeedNoZipDuns();
+        prepareManSeedNoZipCityDuns();
+        TransformationProgress progress = createNewProgress();
+        progress = transformData(progress);
+        finish(progress);
+        confirmResultFile(progress);
+        cleanupProgressTables();
+    }
+
+    @Override
+    protected TransformationService<PipelineTransformationConfiguration> getTransformationService() {
+        return pipelineTransformationService;
+    }
+
+    @Override
+    protected Source getSource() {
+        return source;
+    }
+
+    @Override
+    protected String getPathToUploadBaseData() {
+        return hdfsPathBuilder.constructSnapshotDir(source.getSourceName(), targetVersion).toString();
+    }
+
+    @Override
+    protected PipelineTransformationConfiguration createTransformationConfiguration() {
+        try {
+            PipelineTransformationConfiguration configuration = new PipelineTransformationConfiguration();
+            configuration.setName("ManualSeedEnrichDuns");
+            configuration.setVersion(targetVersion);
+
+            // Initialize manualSeed Data Set
+            TransformationStepConfig step1 = new TransformationStepConfig();
+            List<String> baseSourceStep1 = new ArrayList<String>();
+            baseSourceStep1.add(baseSource1.getSourceName());
+            baseSourceStep1.add(baseSource2.getSourceName());
+            baseSourceStep1.add(baseSource3.getSourceName());
+            step1.setBaseSources(baseSourceStep1);
+            step1.setTargetSource(source.getSourceName());
+            step1.setTransformer(ManualSeedEnrichDunsFlow.TRANSFORMER_NAME);
+            String confParamStr = getManualSeedDataConfig();
+            step1.setConfiguration(confParamStr);
+
+            // -----------
+            List<TransformationStepConfig> steps = new ArrayList<TransformationStepConfig>();
+            steps.add(step1);
+
+            // -----------
+            configuration.setSteps(steps);
+            return configuration;
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void prepareManualSeedDuns() {
+        List<Pair<String, Class<?>>> columns = new ArrayList<>();
+        columns.add(Pair.of("MAN_Id", String.class));
+        columns.add(Pair.of("MAN_Domain", String.class));
+        columns.add(Pair.of("MAN_DUNS", String.class));
+        Object[][] data = new Object[][] {
+                { "1", "sbi.com", "DUNS12" }, { "2", "adobe.com", "DUNS22" }, { "3", "netapp.com", "DUNS14" },
+                { "4", "cisco.com", null }, { "5", "data.com", null }, { "6", "yp.com", null },
+                { "7", "conviva.com", null }, { "8", "matrix.com", null }, { "9", "yahoo.com", null },
+                { "10", "aptus.com", null } };
+        uploadBaseSourceData(baseSource1.getSourceName(), baseSourceVersion, columns, data);
+    }
+
+    private void prepareManSeedNoZipDuns() {
+        List<Pair<String, Class<?>>> columns = new ArrayList<>();
+        columns.add(Pair.of("MAN_Id", String.class));
+        columns.add(Pair.of("MAN_Domain", String.class));
+        columns.add(Pair.of("MAN_DUNS", String.class));
+        Object[][] data = new Object[][] {
+                { "1", "sbi.com", "DUNS33" }, { "2", "adobe.com", "DUNS22" }, { "3", "netapp.com", "DUNS44" },
+                { "4", "cisco.com", "DUNS67" }, { "5", "data.com", "DUNS21" }, { "6", "yp.com", null },
+                { "7", "conviva.com", "DUNS89" }, { "8", "matrix.com", null }, { "9", "yahoo.com", "DUNS50" },
+                { "10", "aptus.com", "DUNS11" }
+        };
+        uploadBaseSourceData(baseSource2.getSourceName(), baseSourceVersion, columns, data);
+    }
+
+    private void prepareManSeedNoZipCityDuns() {
+        List<Pair<String, Class<?>>> columns = new ArrayList<>();
+        columns.add(Pair.of("MAN_Id", String.class));
+        columns.add(Pair.of("MAN_Domain", String.class));
+        columns.add(Pair.of("MAN_DUNS", String.class));
+        Object[][] data = new Object[][] {
+                { "1", "sbi.com", "DUNS33" }, { "2", "adobe.com", "DUNS22" }, { "3", "netapp.com", "DUNS45" },
+                { "4", "cisco.com", "DUNS67" }, { "5", "data.com", "DUNS21" }, { "6", "yp.com", "DUNS77" },
+                { "7", "conviva.com", "DUNS89" }, { "8", "matrix.com", null }, { "9", "yahoo.com", "DUNS99" },
+                { "10", "aptus.com", "DUNS12" }
+        };
+        uploadBaseSourceData(baseSource3.getSourceName(), baseSourceVersion, columns, data);
+    }
+
+    private String getManualSeedDataConfig() throws JsonProcessingException {
+        ManualSeedEnrichDunsConfig conf = new ManualSeedEnrichDunsConfig();
+        conf.setManSeedDuns("MAN_DUNS");
+        conf.setManSeedId("MAN_Id");
+        return JsonUtils.serialize(conf);
+    }
+
+    @Override
+    protected String getPathForResult() {
+        Source targetSource = sourceService.findBySourceName(source.getSourceName());
+        String targetVersion = hdfsSourceEntityMgr.getCurrentVersion(targetSource);
+        return hdfsPathBuilder.constructSnapshotDir(source.getSourceName(), targetVersion).toString();
+    }
+
+    private Object[][] expectedDataValues = {
+            { "7", "conviva.com", "DUNS89" }, { "6", "yp.com", "DUNS77" }, { "4", "cisco.com", "DUNS67" },
+            { "8", "matrix.com", null }, { "1", "sbi.com", "DUNS12" }, { "2", "adobe.com", "DUNS22" },
+            { "3", "netapp.com", "DUNS14" }, { "5", "data.com", "DUNS21" }, { "9", "yahoo.com", "DUNS50" },
+            { "10", "aptus.com", "DUNS11" } };
+
+    @Override
+    protected void verifyResultAvroRecords(Iterator<GenericRecord> records) {
+        int rowCount = 0;
+        Map<String, Object[]> expectedData = new HashMap<>();
+        for (Object[] data : expectedDataValues) {
+            expectedData.put(String.valueOf(data[0]), data);
+        }
+        while (records.hasNext()) {
+            GenericRecord record = records.next();
+            String id = String.valueOf(record.get("MAN_Id"));
+            Object[] expected = expectedData.get(id);
+            Assert.assertTrue(equals(record.get("MAN_Domain"), expected[1]));
+            Assert.assertTrue(equals(record.get("MAN_DUNS"), expected[2]));
+            rowCount++;
+        }
+        Assert.assertEquals(rowCount, 10);
+    }
+}
