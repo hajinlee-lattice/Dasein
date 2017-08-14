5fc63b997a7b5a8e2fdd04e80b4c2dcdc9f79538
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2017-08-14 19:08:26 +0000

    Review Request 10917: adding publish report to test

--- le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/workflow/steps/StartExecution.java
+++ le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/workflow/steps/StartExecution.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.latticeengines.cdl.workflow.steps.export.ExportDataToRedshiftReportStep;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
