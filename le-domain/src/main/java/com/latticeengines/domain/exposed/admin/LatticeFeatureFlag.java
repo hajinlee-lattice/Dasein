78b8dad835791a3b6f172e3243d03c672da4ec06
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-08-21 03:26:06 +0000

    Review Request 11000 deprecate enable cdl

--- le-domain/src/main/java/com/latticeengines/domain/exposed/admin/LatticeFeatureFlag.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/admin/LatticeFeatureFlag.java
@@ -7,7 +7,16 @@ import com.latticeengines.domain.exposed.pls.PlsFeatureFlag;
 
 public enum LatticeFeatureFlag {
 
+
     DANTE("Dante", "Dante"), //
+    ALLOW_PIVOT_FILE("AllowPivotFile", "Allow pivot file"), //
+    ENABLE_DATA_ENCRYPTION("EnableDataEncryption", "Enable data encryption"), //
+    ENABLE_INTERNAL_ENRICHMENT_ATTRIBUTES("EnableInternalEnrichmentAttributes",
+            "Enable Internal Enrichment Attributes"), //
+    LATTICE_INSIGHTS("LatticeInsights", "Lattice Insights"), //
+    ENABLE_MATCH_DEBUG("EnableMatchDebug", "Enable Match Debug"), //
+    ENABLE_LPI_PLAYMAKER("EnableLpiPlaymaker", "Enable LPI Playmaker"), //
+
     @Deprecated
     QUOTA(true, "Quota", "Quota"), //
     @Deprecated
@@ -22,27 +31,20 @@ public enum LatticeFeatureFlag {
     USE_MARKETO_SETTINGS(true, "UseMarketoSettings", "Use Marketo settings"), //
     @Deprecated
     USE_ELOQUA_SETTINGS(true, "UseEloquaSettings", "Use Eloqua settings"), //
-    ALLOW_PIVOT_FILE("AllowPivotFile", "Allow pivot file"), //
     @Deprecated
     ENABLE_CAMPAIGN_UI(true, "EnableCampaignUI", "Enable Campaign UI"), //
     @Deprecated
     USE_DNB_RTS_AND_MODELING(true, "UseDnbRtsAndModeling", "User DNB RTS and Modeling"), //
     @Deprecated
     ENABLE_LATTICE_MARKETO_CREDENTIAL_PAGE(true, "EnableLatticeMarketoCredentialPage", "Enable Lattice Marketo Credential Page"), //
-    ENABLE_DATA_ENCRYPTION("EnableDataEncryption", "Enable data encryption"), //
-    ENABLE_INTERNAL_ENRICHMENT_ATTRIBUTES("EnableInternalEnrichmentAttributes",
-            "Enable Internal Enrichment Attributes"), //
     @Deprecated
     ENABLE_DATA_PROFILING_V2(true, "EnableDataProfilingV2", "Enable Data Profiling Version 2"), //
     @Deprecated
     ENABLE_FUZZY_MATCH(true, "EnableFuzzyMatch", "Enable Fuzzy Match"), //
-    LATTICE_INSIGHTS("LatticeInsights", "Lattice Insights"), //
     @Deprecated
     BYPASS_DNB_CACHE(true, "BypassDnbCache", "Bypass DnB Cache"), //
     @Deprecated
-    ENABLE_CDL("EnableCdl", "Enable Customer Data Lake"), //
-    ENABLE_MATCH_DEBUG("EnableMatchDebug", "Enable Match Debug"), //
-    ENABLE_LPI_PLAYMAKER("EnableLpiPlaymaker", "Enable LPI Playmaker"); //
+    ENABLE_CDL(true, "EnableCdl", "Enable Customer Data Lake"); //
 
     private String name;
     private String documentation;
