fae63dba45f130c45f7005e9d10f3667e0c75b25
akumar <akumar@479f5059-62a3-4c39-9342-9852e3a68435>
2017-09-12 08:16:35 +0000

    Review request 11381 -  bug fix in preview

--- le-pls/src/main/java/com/latticeengines/pls/service/impl/RatingEntityPreviewServiceImpl.java
+++ le-pls/src/main/java/com/latticeengines/pls/service/impl/RatingEntityPreviewServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 
 import com.latticeengines.common.exposed.util.JsonUtils;
 import com.latticeengines.domain.exposed.metadata.InterfaceName;
+import com.latticeengines.domain.exposed.metadata.MetadataSegment;
 import com.latticeengines.domain.exposed.pls.RatingEngine;
 import com.latticeengines.domain.exposed.pls.RatingModel;
 import com.latticeengines.domain.exposed.query.AttributeLookup;
@@ -73,6 +74,11 @@ public class RatingEntityPreviewServiceImpl implements RatingEntityPreviewServic
         RatingModel model = setModelInfo(ratingEngine, entityFrontEndQuery);
 
         setLookups(entityType, entityFrontEndQuery, model, lookupFieldNames);
+
+        MetadataSegment segment = ratingEngine.getSegment();
+
+        entityFrontEndQuery.setAccountRestriction(segment.getAccountFrontEndRestriction());
+        entityFrontEndQuery.setContactRestriction(segment.getContactFrontEndRestriction());
 
         log.info(String.format("Entity query => %s", JsonUtils.serialize(entityFrontEndQuery)));
 
