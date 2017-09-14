1fcfd1658541c7a2114167f60baa4d0434a4c1e0
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-09-14 07:08:19 +0000

    Review Request 11417 simplify oauth2 build

--- le-dev/scripts/setup.sh
+++ le-dev/scripts/setup.sh
@@ -31,10 +31,6 @@ echo "Top-level compile"
 mvn -T8 -DskipTests clean install 2> /tmp/errors.txt
 processErrors
 
-echo "Top-level playmaker compile"
-mvn -T6 -f playmaker-pom.xml -DskipTests clean install 2> /tmp/errors.txt
-processErrors
-
 echo "Top-level shaded yarn compile"
 mvn -T6 -f shaded-pom.xml -Pshaded-yarn -DskipTests clean package 2> /tmp/errors.txt
 processErrors
