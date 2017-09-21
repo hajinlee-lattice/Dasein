8ec6226b7a367b8969eb68de3b3731e6ffc789e2
jhe <jhe@479f5059-62a3-4c39-9342-9852e3a68435>
2017-09-21 07:31:59 +0000

    Review Request 11469: Remove code that already moved to CDL package.

--- le-pls/src/main/java/com/latticeengines/pls/dao/DLTenantMappingDao.java
+++
@@ -1,9 +0,0 @@
-package com.latticeengines.pls.dao;
-
-import com.latticeengines.db.exposed.dao.BaseDao;
-import com.latticeengines.domain.exposed.dataloader.DLTenantMapping;
-
-public interface DLTenantMappingDao extends BaseDao<DLTenantMapping> {
-
-    DLTenantMapping getDLTenantMapping(String dlTenantId, String dlLoadGroup);
-}
