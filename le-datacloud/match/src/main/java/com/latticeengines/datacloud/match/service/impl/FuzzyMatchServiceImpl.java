a483cd1dd1f0a8b37cce48dc32eede2f59612afe
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-10-04 18:46:23 +0000

    Review Request 11651 fix NPE in bulk match

--- le-datacloud/match/src/main/java/com/latticeengines/datacloud/match/service/impl/FuzzyMatchServiceImpl.java
+++ le-datacloud/match/src/main/java/com/latticeengines/datacloud/match/service/impl/FuzzyMatchServiceImpl.java
@@ -269,14 +269,14 @@ public class FuzzyMatchServiceImpl implements FuzzyMatchService {
                 } else {
                     record.log(logEntry.getMessage() + "\n"
                             + StringEscapeUtils.escapeJson(ExceptionUtils.getStackTrace(logEntry.getThrowable())));
-                    record.getErrorMessages().add(logEntry.getMessage() + " : " + logEntry.getThrowable().getMessage());
+                    record.addErrorMessages(logEntry.getMessage() + " : " + logEntry.getThrowable().getMessage());
                 }
             }
             if (logEntry.getLevel().isGreaterOrEqual(Level.ERROR)) {
                 if (logEntry.getThrowable() == null) {
-                    record.getErrorMessages().add(logEntry.getMessage());
+                    record.addErrorMessages(logEntry.getMessage());
                 } else {
-                    record.getErrorMessages().add(logEntry.getMessage() + " : " + logEntry.getThrowable().getMessage());
+                    record.addErrorMessages(logEntry.getMessage() + " : " + logEntry.getThrowable().getMessage());
                 }
             }
         }
