58ef3c68e59914985f93740fa08d6bd65743a49d
lming <lming@479f5059-62a3-4c39-9342-9852e3a68435>
2017-10-06 07:25:14 +0000

    Review Request 11671: AnalyticPurchaseState generation - revert this docker for now

--- le-docker/src/main/scripts/common/build.sh
+++ le-docker/src/main/scripts/common/build.sh
@@ -24,10 +24,6 @@ pushd le-ubuntu
 build_docker latticeengines/ubuntu ${NO_CACHE}
 popd
 
-pushd le-python
-build_docker latticeengines/python ${NO_CACHE}
-popd
-
 pushd le-httpd
 build_docker latticeengines/httpd ${NO_CACHE}
 popd
