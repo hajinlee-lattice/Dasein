2dcd7a3dd0b401aebc0a586b085fc615db2762a1
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-10-06 23:34:15 +0000

    Review Request 11692 update LocalTest data

--- le-metadata/src/test/java/com/latticeengines/metadata/service/impl/RegisterLocalTestBucketedAccountTableTestNG.java
+++ le-metadata/src/test/java/com/latticeengines/metadata/service/impl/RegisterLocalTestBucketedAccountTableTestNG.java
@@ -2,9 +2,11 @@ package com.latticeengines.metadata.service.impl;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+
+import javax.inject.Inject;
 
 import org.junit.Assert;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.testng.annotations.Test;
 
@@ -21,28 +23,35 @@ import com.latticeengines.metadata.service.DataCollectionService;
 import com.latticeengines.metadata.service.MetadataService;
 import com.latticeengines.security.exposed.entitymanager.TenantEntityMgr;
 import com.latticeengines.security.exposed.util.MultiTenantContext;
+import com.latticeengines.testframework.exposed.service.TestArtifactService;
 
 @Component
 public class RegisterLocalTestBucketedAccountTableTestNG extends MetadataFunctionalTestNGBase {
 
     private static final String LOCALTEST_TENANT = "LocalTest";
+    private static final String S3_DIR = "le-dev/LocalTest";
+    private static final String S3_VERSION = "1";
 
-    @Autowired
+    @Inject
     private MetadataService mdService;
 
-    @Autowired
+    @Inject
     private TenantEntityMgr tenantEntityMgr;
 
-    @Autowired
+    @Inject
     private DataCollectionService dataCollectionService;
+
+    @Inject
+    private TestArtifactService testArtifactService;
 
     private String customerSpace;
 
+    @SuppressWarnings("groupsTestNG")
     @Test(groups = "registertable")
     public void registerMetadataTable() throws Exception {
         createLocalTestTenant();
         bootstrapDataCollection();
-        registerBucketedAccount();
+        registerTables();
     }
 
     private void createLocalTestTenant() {
@@ -56,26 +65,34 @@ public class RegisterLocalTestBucketedAccountTableTestNG extends MetadataFunctio
 
     private void bootstrapDataCollection() throws IOException {
         DataCollection collection = dataCollectionService.getDataCollection(customerSpace, "");
-        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("dev/stats.json");
+        InputStream is = testArtifactService.readTestArtifactAsStream(S3_DIR, S3_VERSION, "stats_container.json");
         ObjectMapper om = new ObjectMapper();
         StatisticsContainer container = om.readValue(is, StatisticsContainer.class);
         container.setVersion(collection.getVersion());
         dataCollectionService.addStats(customerSpace, collection.getName(), container);
     }
 
-    private void registerBucketedAccount() throws IOException {
-        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("dev/BucketedAccount.json");
-        ObjectMapper om = new ObjectMapper();
-        Table bucketedTable = om.readValue(is, Table.class);
-        String tableName = bucketedTable.getName();
-        bucketedTable.setTableType(TableType.DATATABLE);
-        if (mdService.getTable(CustomerSpace.parse(LOCALTEST_TENANT), tableName) == null) {
-            mdService.createTable(CustomerSpace.parse(LOCALTEST_TENANT), bucketedTable);
-        } else {
-            mdService.updateTable(CustomerSpace.parse(LOCALTEST_TENANT), bucketedTable);
+    private void registerTables() throws IOException {
+        for (TableRoleInCollection role: Arrays.asList(
+                TableRoleInCollection.BucketedAccount, //
+                TableRoleInCollection.SortedContact, //
+                TableRoleInCollection.SortedProduct, //
+                TableRoleInCollection.AggregatedTransaction, //
+                TableRoleInCollection.CalculatedPurchaseHistory
+        )) {
+            InputStream is = testArtifactService.readTestArtifactAsStream(S3_DIR, S3_VERSION, role.name() + ".json");
+            ObjectMapper om = new ObjectMapper();
+            Table table = om.readValue(is, Table.class);
+            String tableName = table.getName();
+            table.setTableType(TableType.DATATABLE);
+            if (mdService.getTable(CustomerSpace.parse(LOCALTEST_TENANT), tableName) == null) {
+                mdService.createTable(CustomerSpace.parse(LOCALTEST_TENANT), table);
+            } else {
+                mdService.updateTable(CustomerSpace.parse(LOCALTEST_TENANT), table);
+            }
+            DataCollection.Version version = dataCollectionService.getDataCollection(customerSpace, "").getVersion();
+            dataCollectionService.upsertTable(customerSpace, "", tableName, role, version);
         }
-        DataCollection.Version version = dataCollectionService.getDataCollection(customerSpace, "").getVersion();
-        dataCollectionService.upsertTable(customerSpace, "", tableName, TableRoleInCollection.BucketedAccount, version);
     }
 
 }
