dbbc0d63da21198207816a1f50c353fc7fb50651
dzheng <dzheng@479f5059-62a3-4c39-9342-9852e3a68435>
2017-10-09 23:29:30 +0000

    Review Request 11694 Fix metadata upload

--- le-dev/scripts/setupdb_ldc_managedb.sh
+++ le-dev/scripts/setupdb_ldc_managedb.sh
@@ -36,6 +36,9 @@ gunzip -c $WSHOME/le-dev/testartifacts/LDC_ManageDB/AccountMasterColumn205.csv.g
 rm -rf $WSHOME/le-dev/testartifacts/LDC_ManageDB/AccountMasterColumn206.csv || true
 gunzip -c $WSHOME/le-dev/testartifacts/LDC_ManageDB/AccountMasterColumn206.csv.gz > $WSHOME/le-dev/testartifacts/LDC_ManageDB/AccountMasterColumn206.csv
 
+rm -rf $WSHOME/le-dev/testartifacts/LDC_ManageDB/AccountMasterColumn207.csv || true
+gunzip -c $WSHOME/le-dev/testartifacts/LDC_ManageDB/AccountMasterColumn207.csv.gz > $WSHOME/le-dev/testartifacts/LDC_ManageDB/AccountMasterColumn207.csv
+
 rm -rf $WSHOME/le-dev/testartifacts/LDC_ManageDB/SourceColumn.csv || true
 gunzip -c $WSHOME/le-dev/testartifacts/LDC_ManageDB/SourceColumn.csv.gz > $WSHOME/le-dev/testartifacts/LDC_ManageDB/SourceColumn.csv
 
