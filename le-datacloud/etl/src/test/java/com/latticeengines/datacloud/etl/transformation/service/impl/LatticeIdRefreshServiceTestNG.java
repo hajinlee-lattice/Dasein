b8da4e3d058324e25fe8852d2220f77564d68781
dzheng <dzheng@479f5059-62a3-4c39-9342-9852e3a68435>
2017-10-10 21:17:58 +0000

    Review Request 11714: Improve LatticeIdRefreshServiceTestNG

--- le-datacloud/etl/src/test/java/com/latticeengines/datacloud/etl/transformation/service/impl/LatticeIdRefreshServiceTestNG.java
+++ le-datacloud/etl/src/test/java/com/latticeengines/datacloud/etl/transformation/service/impl/LatticeIdRefreshServiceTestNG.java
@@ -31,23 +31,27 @@ public class LatticeIdRefreshServiceTestNG
         extends TransformationServiceImplTestNGBase<PipelineTransformationConfiguration> {
     private static final Logger log = LoggerFactory.getLogger(LatticeIdRefreshServiceTestNG.class);
 
-    private GeneralSource source = new GeneralSource("AccountMasterId");
-    private GeneralSource amsInit = new GeneralSource("AccountMasterSeedInit");
-    private GeneralSource amsRefresh = new GeneralSource("AccountMasterSeedRefresh");
+    private GeneralSource source = new GeneralSource("AMID");
+    private GeneralSource amsInit = new GeneralSource("AMSeedInit");
+    private GeneralSource amsRefresh = new GeneralSource("AMSeedRefresh");
+    private GeneralSource amsSecondRefresh = new GeneralSource("AMSeedSecondRefresh");
 
     private static final String STRATEGY = "AccountMasterSeedRebuild";
 
     private static final String AMID_VERSION_EMPTY = "2017-06-01_00-00-00_UTC";
     private static final String AMID_VERSION_INIT = "2017-07-01_00-00-00_UTC";
+    private static final String AMID_VERSION_REFRESH = "2017-08-01_00-00-00_UTC";
 
     @Test(groups = "pipeline1")
     public void testTransformation() {
         prepareAMIDEmpty();
         prepareAMSeedInit();
         prepareAMSeedRefresh();
+        prepareSecondAMSeedRefresh();
         TransformationProgress progress = createNewProgress();
         progress = transformData(progress);
         finish(progress);
+        confirmIntermediateSource(source, AMID_VERSION_REFRESH);
         confirmResultFile(progress);
         cleanupProgressTables();
     }
@@ -77,15 +81,29 @@ public class LatticeIdRefreshServiceTestNG
             baseSources = new ArrayList<>();
             baseSources.add(amsRefresh.getSourceName());
             step2.setBaseSources(baseSources);
+            step2.setTargetVersion(AMID_VERSION_REFRESH);
             step2.setTransformer(LatticeIdRefreshTransformer.TRANSFORMER_NAME);
             step2.setTargetSource(source.getSourceName());
             String confParamStr2 = getTransformerConfigForRefresh();
             step2.setConfiguration(confParamStr2);
 
+            TransformationStepConfig step3 = new TransformationStepConfig();
+            inputSteps = new ArrayList<>();
+            inputSteps.add(1);
+            step3.setInputSteps(inputSteps);
+            baseSources = new ArrayList<>();
+            baseSources.add(amsSecondRefresh.getSourceName());
+            step3.setBaseSources(baseSources);
+            step3.setTransformer(LatticeIdRefreshTransformer.TRANSFORMER_NAME);
+            step3.setTargetSource(source.getSourceName());
+            String confParamStr3 = getTransformerConfigForRefresh();
+            step3.setConfiguration(confParamStr3);
+
             // -----------
             List<TransformationStepConfig> steps = new ArrayList<TransformationStepConfig>();
             steps.add(step1);
             steps.add(step2);
+            steps.add(step3);
 
             // -----------
             configuration.setSteps(steps);
@@ -147,26 +165,26 @@ public class LatticeIdRefreshServiceTestNG
     }
 
     private Object[][] amsInitData = new Object[][] { //
-            { "dom1.com", "DUNS1" }, // Not refreshed
-            { "dom1.com", "DUNS2" }, // Not refreshed
-            { "dom2.com", "DUNS1" }, // Not refreshed
-            { "dom2.com", "DUNS2" }, // Not refreshed
-            { null, "DUNS1" }, // Not refreshed
-            { null, "DUNS3" }, // Not refreshed
-            { "dom1.com", null }, // Not refreshed
-            { "dom3.com", null }, // Not refreshed
-            { "dom11.com", "DUNS11" }, // Retired
-            { "dom11.com", "DUNS22" }, // Retired
-            { "dom22.com", "DUNS11" }, // Retired
-            { "dom22.com", "DUNS22" }, // Retired
-            { null, "DUNS11" }, // Retired
-            { null, "DUNS33" }, // Retired
-            { "dom11.com", null }, // Retired
-            { "dom33.com", null }, // Retired
-            { "dom1111.com", "DUNS1111" }, // Redirected
-            { "dom2222.com", "DUNS2222" }, // Redirected
-            { "dom3333.com", "DUNS3333" }, // Redirected
-            { "dom11111.com", "DUNS11111" }, // Obsolete
+            { "dom1.com", "DUNS1" }, // Not to refresh
+            { "dom1.com", "DUNS2" }, // Not to refresh
+            { "dom2.com", "DUNS1" }, // Not to refresh
+            { "dom2.com", "DUNS2" }, // Not to refresh
+            { null, "DUNS1" }, // Not to refresh
+            { null, "DUNS3" }, // Not to refresh
+            { "dom1.com", null }, // Not to refresh
+            { "dom3.com", null }, // Not to refresh
+            { "dom11.com", "DUNS11" }, // To retire
+            { "dom11.com", "DUNS22" }, // To retire
+            { "dom22.com", "DUNS11" }, // To retire
+            { "dom22.com", "DUNS22" }, // To retire
+            { null, "DUNS11" }, // To retire
+            { null, "DUNS33" }, // To retire
+            { "dom11.com", null }, // To retire
+            { "dom33.com", null }, // To retire
+            { "dom1111.com", "DUNS1111" }, // To redirect
+            { "dom2222.com", "DUNS2222" }, // To redirect
+            { "dom3333.com", "DUNS3333" }, // To redirect
+            { "dom11111.com", "DUNS11111" }, // To obsolete
     };
 
     private void prepareAMSeedInit() {
@@ -177,28 +195,28 @@ public class LatticeIdRefreshServiceTestNG
     }
     
     private Object[][] amsRefreshData = new Object[][] { //
-            { "dom1.com", "DUNS1" }, // Not refreshed
-            { "dom1.com", "DUNS2" }, // Not refreshed
-            { "dom2.com", "DUNS1" }, // Not refreshed
-            { "dom2.com", "DUNS2" }, // Not refreshed
-            { null, "DUNS1" }, // Not refreshed
-            { null, "DUNS3" }, // Not refreshed
-            { "dom1.com", null }, // Not refreshed
-            { "dom3.com", null }, // Not refreshed
-            { "dom111.com", "DUNS111" }, // New
-            { "dom111.com", "DUNS222" }, // New
-            { "dom222.com", "DUNS111" }, // New
-            { "dom222.com", "DUNS222" }, // New
-            { null, "DUNS111" }, // New
-            { null, "DUNS333" }, // New
-            { "dom111.com", null }, // New
-            { "dom333.com", null }, // New
-            { "dom1111.com", null }, // New
-            { null, "DUNS1111" }, // New
-            { "dom2222.com", null }, // New
-            { null, "DUNS3333" }, // New
-            { "dom11111.comNew", "DUNS11111" }, // New
-            { "dom11111.com", "DUNS11111New" }, // New
+            { "dom1.com", "DUNS1" }, // Not refreshed compared to init version
+            { "dom1.com", "DUNS2" }, // Not refreshed compared to init version
+            { "dom2.com", "DUNS1" }, // Not refreshed compared to init version
+            { "dom2.com", "DUNS2" }, // Not refreshed compared to init version
+            { null, "DUNS1" }, // Not refreshed compared to init version
+            { null, "DUNS3" }, // Not refreshed compared to init version
+            { "dom1.com", null }, // Not refreshed compared to init version
+            { "dom3.com", null }, // Not refreshed compared to init version
+            { "dom111.com", "DUNS111" }, // New compared to init version
+            { "dom111.com", "DUNS222" }, // New compared to init version
+            { "dom222.com", "DUNS111" }, // New compared to init version
+            { "dom222.com", "DUNS222" }, // New compared to init version
+            { null, "DUNS111" }, // New compared to init version
+            { null, "DUNS333" }, // New compared to init version
+            { "dom111.com", null }, // New compared to init version
+            { "dom333.com", null }, // New compared to init version
+            { "dom1111.com", null }, // New compared to init version
+            { null, "DUNS1111" }, // New compared to init version
+            { "dom2222.com", null }, // New compared to init version
+            { null, "DUNS3333" }, // New compared to init version
+            { "dom11111.comNew", "DUNS11111" }, // New compared to init version
+            { "dom11111.com", "DUNS11111New" }, // New compared to init version
     };
 
     private void prepareAMSeedRefresh() {
@@ -208,9 +226,53 @@ public class LatticeIdRefreshServiceTestNG
         uploadBaseSourceData(amsRefresh.getSourceName(), baseSourceVersion, columns, amsRefreshData);
     }
 
+    private Object[][] amsSecondRefreshData = new Object[][] { //
+            { "dom1.com", "DUNS1" }, // Not refreshed compared to refresh version
+            { "dom1.com", "DUNS2" }, // Not refreshed compared to refresh version
+            { "dom2.com", "DUNS1" }, // Not refreshed compared to refresh version
+            { "dom2.com", "DUNS2" }, // Not refreshed compared to refresh version
+            { null, "DUNS1" }, // Not refreshed compared to refresh version
+            { null, "DUNS3" }, // Not refreshed compared to refresh version
+            { "dom1.com", null }, // Not refreshed compared to refresh version
+            { "dom3.com", null }, // Not refreshed compared to refresh version
+            { "dom111.com", "DUNS111" }, // Not refreshed compared to refresh version
+            { "dom111.com", "DUNS222" }, // Not refreshed compared to refresh version
+            { "dom222.com", "DUNS111" }, // Not refreshed compared to refresh version
+            { "dom222.com", "DUNS222" }, // Not refreshed compared to refresh version
+            { null, "DUNS111" }, // Not refreshed compared to refresh version
+            { null, "DUNS333" }, // Not refreshed compared to refresh version
+            { "dom111.com", null }, // Not refreshed compared to refresh version
+            { "dom333.com", null }, // Not refreshed compared to refresh version
+            { "dom1111.com", null }, // Not refreshed compared to refresh version
+            { null, "DUNS1111" }, // Not refreshed compared to refresh version
+            { "dom2222.com", null }, // Not refreshed compared to refresh version
+            { null, "DUNS3333" }, // Not refreshed compared to refresh version
+            { "dom11111.comNew", "DUNS11111" }, // Not refreshed compared to refresh version
+            { "dom11111.com", "DUNS11111New" }, // Not refreshed compared to refresh version
+            { "dom33.com", null }, // OBSOLETE -> ACTIVE compared to refresh version
+            { "dom1111.com", "DUNS1111" }, // UPDATED -> ACTIVE compared to refresh version
+    };
+
+    private void prepareSecondAMSeedRefresh() {
+        List<Pair<String, Class<?>>> columns = new ArrayList<>();
+        columns.add(Pair.of("Domain", String.class));
+        columns.add(Pair.of("DUNS", String.class));
+        uploadBaseSourceData(amsSecondRefresh.getSourceName(), baseSourceVersion, columns, amsSecondRefreshData);
+    }
+
     @Override
-    protected void verifyResultAvroRecords(Iterator<GenericRecord> records) {
-        log.info("Start to verify records one by one.");
+    protected void verifyIntermediateResult(String source, String version, Iterator<GenericRecord> records) {
+        log.info("Start to verify " + source + " @" + version);
+        switch (version) {
+        case AMID_VERSION_REFRESH:
+            verifyAMID2ndRefresh(records);
+            break;
+        default:
+            break;
+        }
+    }
+
+    private void verifyAMID2ndRefresh(Iterator<GenericRecord> records) {
         Object[][] expectedData = new Object[][] { //
                 { "dom1.com", "DUNS1", "ACTIVE" }, //
                 { "dom1.com", "DUNS2", "ACTIVE" }, //
@@ -247,6 +309,51 @@ public class LatticeIdRefreshServiceTestNG
                 { "dom11111.com", "DUNS11111New", "ACTIVE" }, //
                 { "dom11111.comNew", "DUNS11111", "ACTIVE" }, //
         };
+        checkResult(records, expectedData, 34L);
+    }
+
+    @Override
+    protected void verifyResultAvroRecords(Iterator<GenericRecord> records) {
+        Object[][] expectedData = new Object[][] { //
+                { "dom1.com", "DUNS1", "ACTIVE" }, //
+                { "dom1.com", "DUNS2", "ACTIVE" }, //
+                { "dom2.com", "DUNS1", "ACTIVE" }, //
+                { "dom2.com", "DUNS2", "ACTIVE" }, //
+                { null, "DUNS1", "ACTIVE" }, //
+                { null, "DUNS3", "ACTIVE" }, //
+                { "dom1.com", null, "ACTIVE" }, //
+                { "dom3.com", null, "ACTIVE" }, //
+                { "dom111.com", "DUNS111", "ACTIVE" }, //
+                { "dom111.com", "DUNS222", "ACTIVE" }, //
+                { "dom222.com", "DUNS111", "ACTIVE" }, //
+                { "dom222.com", "DUNS222", "ACTIVE" }, //
+                { null, "DUNS111", "ACTIVE" }, //
+                { null, "DUNS333", "ACTIVE" }, //
+                { "dom111.com", null, "ACTIVE" }, //
+                { "dom333.com", null, "ACTIVE" }, //
+                { "dom11.com", "DUNS11", "OBSOLETE" }, //
+                { "dom11.com", "DUNS22", "OBSOLETE" }, //
+                { "dom22.com", "DUNS11", "OBSOLETE" }, //
+                { "dom22.com", "DUNS22", "OBSOLETE" }, //
+                { null, "DUNS11", "OBSOLETE" }, //
+                { null, "DUNS33", "OBSOLETE" }, //
+                { "dom11.com", null, "OBSOLETE" }, //
+                { "dom33.com", null, "ACTIVE" }, //
+                { "dom1111.com", "DUNS1111", "ACTIVE" }, //
+                { "dom2222.com", "DUNS2222", "UPDATED" }, //
+                { "dom3333.com", "DUNS3333", "UPDATED" }, //
+                { "dom1111.com", null, "ACTIVE" }, //
+                { null, "DUNS1111", "ACTIVE" }, //
+                { "dom2222.com", null, "ACTIVE" }, //
+                { null, "DUNS3333", "ACTIVE" }, //
+                { "dom11111.com", "DUNS11111", "OBSOLETE" }, //
+                { "dom11111.com", "DUNS11111New", "ACTIVE" }, //
+                { "dom11111.comNew", "DUNS11111", "ACTIVE" }, //
+        };
+        checkResult(records, expectedData, 34L);
+    }
+
+    private void checkResult(Iterator<GenericRecord> records, Object[][] expectedData, long expectedRows) {
         Map<String, String> expected = new HashMap<>();
         for (Object[] data : expectedData) {
             expected.put(String.valueOf(data[0] + String.valueOf(data[1])), String.valueOf(data[2]));
@@ -275,7 +382,7 @@ public class LatticeIdRefreshServiceTestNG
             }
             rowNum++;
         }
-        Assert.assertEquals(34L, rowNum);
+        Assert.assertEquals(expectedRows, rowNum);
     }
 
 }
