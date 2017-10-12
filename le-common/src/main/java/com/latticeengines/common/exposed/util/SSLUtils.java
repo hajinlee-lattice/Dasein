9bd9023bc4521dd157988e1bc7d78946f122c316
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-10-12 22:00:17 +0000

    Review Request 11747 PLS-5053 revert trust manager change for now

--- le-common/src/main/java/com/latticeengines/common/exposed/util/SSLUtils.java
+++ le-common/src/main/java/com/latticeengines/common/exposed/util/SSLUtils.java
@@ -2,11 +2,15 @@ package com.latticeengines.common.exposed.util;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.security.cert.X509Certificate;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.slf4j.Logger;
@@ -17,9 +21,21 @@ public class SSLUtils {
     private static final Logger log = LoggerFactory.getLogger(SSLUtils.class);
     private static ThreadLocal<Boolean> verifySSLHostName = new ThreadLocal<>();
 
-    public static final SSLConnectionSocketFactory SSL_BLIND_SOCKET_FACTORY = newSslBlindSocketFactory();
+    public static final SSLConnectionSocketFactory SSL_BLIND_SOCKET_FACTORY = newBlindSslConnectionSocketFactory();
+
+    private static final SSLSocketFactory DEFAULT_SSL_FACTORY = (SSLSocketFactory) SSLSocketFactory.getDefault();
     private static final HostnameVerifier DEFAULT_HOST_NAME_VERIFIER = HttpsURLConnection.getDefaultHostnameVerifier();
     private static final HostnameVerifier BLIND_HOST_NAME_VERIFIER = (hostname, session) -> true;
+
+    private static final TrustManager[] UNQUESTIONING_TRUST_MANAGER = new TrustManager[]{new X509TrustManager() {
+        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+            return null;
+        }
+        public void checkClientTrusted(X509Certificate[] certs, String authType) {
+        }
+        public void checkServerTrusted(X509Certificate[] certs, String authType) {
+        }
+    }};
 
     public static void turnOffSSLNameVerification() {
         switchSSLNameVerification(false);
@@ -35,6 +51,8 @@ public class SSLUtils {
             try {
                 HostnameVerifier verifier = on ? DEFAULT_HOST_NAME_VERIFIER : BLIND_HOST_NAME_VERIFIER;
                 HttpsURLConnection.setDefaultHostnameVerifier(verifier);
+                SSLSocketFactory sf = on ? DEFAULT_SSL_FACTORY : newBlindSslSocketFactory();
+                HttpsURLConnection.setDefaultSSLSocketFactory(sf);
                 verifySSLHostName.set(on);
                 log.info("Turned " + action + " ssl for current thread: " + Thread.currentThread().getName());
             } catch (Exception e) {
@@ -43,10 +61,20 @@ public class SSLUtils {
         }
     }
 
-    private static SSLConnectionSocketFactory newSslBlindSocketFactory() {
+    private static SSLConnectionSocketFactory newBlindSslConnectionSocketFactory() {
         try {
-            SSLSocketFactory sf = (SSLSocketFactory) SSLSocketFactory.getDefault();
+            SSLSocketFactory sf = newBlindSslSocketFactory();
             return new SSLConnectionSocketFactory(sf, BLIND_HOST_NAME_VERIFIER);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create a trust-everything connection manager ", e);
+        }
+    }
+
+    private static SSLSocketFactory newBlindSslSocketFactory() {
+        try {
+            final SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, UNQUESTIONING_TRUST_MANAGER, null);
+            return sc.getSocketFactory();
         } catch (Exception e) {
             throw new RuntimeException("Failed to create a trust-everything connection manager ", e);
         }
