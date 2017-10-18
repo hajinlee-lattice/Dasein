26481766b07b6d6e6dd231fa9ca7eaeb449b6fc5
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-10-18 21:17:13 +0000

    Review Request 11817 try to fix dataplatform functional test

--- le-dataplatform/src/main/python/leframework/executor.py
+++ le-dataplatform/src/main/python/leframework/executor.py
@@ -102,18 +102,21 @@ class Executor(object):
         pipelineMediator = params['pipelineMediator']
         if pipelineMediator == None or ('REMOVEDCOLUMNS' not in pipelineMediator and 'ADDEDCOLUMNS' not in pipelineMediator):
             return
+
         with open(diagnostics, "r") as fp:
             fp_content = fp.read()
-            if len(fp_content) > 0:
-                diagnosticsJson = json.load(fp_content)
-                if 'REMOVEDCOLUMNS' in pipelineMediator:
-                    diagnosticsJson['Summary']['RemovedColumnsInPipeline'] = pipelineMediator["REMOVEDCOLUMNS"]
-                if 'ADDEDCOLUMNS' in pipelineMediator:
-                    diagnosticsJson['Summary']['AddedColumnsInPipeline'] = pipelineMediator["ADDEDCOLUMNS"]
-                with open(diagnostics, "wb") as fout:
-                        json.dump(diagnosticsJson, fout)
-            else:
-                logger.info("There is not diagnostics file to merge.")
+
+        if len(fp_content) > 0:
+            diagnosticsJson = json.load(fp_content)
+            if 'REMOVEDCOLUMNS' in pipelineMediator:
+                diagnosticsJson['Summary']['RemovedColumnsInPipeline'] = pipelineMediator["REMOVEDCOLUMNS"]
+            if 'ADDEDCOLUMNS' in pipelineMediator:
+                diagnosticsJson['Summary']['AddedColumnsInPipeline'] = pipelineMediator["ADDEDCOLUMNS"]
+            with open(diagnostics, "wb") as fout:
+                json.dump(diagnosticsJson, fout)
+        else:
+            logger.info("There is not diagnostics file to merge.")
+
 
     @abstractmethod
     def getModelDirPath(self, schema): pass
