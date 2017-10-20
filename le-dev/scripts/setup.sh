8229d2ca10ee8e92bdfad2864845f28ae4734d86
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-10-20 21:57:08 +0000

    Review Request 11839 ignore all the INFO log

--- le-dev/scripts/setup.sh
+++ le-dev/scripts/setup.sh
@@ -62,12 +62,12 @@ wait
 UNAME=`uname`
 if [[ "${UNAME}" == 'Darwin' ]]; then
     echo "You are on Mac"
-    sed -i '' "/INFO fs.TrashPolicyDefault/d" /tmp/errors.txt
+    sed -i '' "/INFO /d" /tmp/errors.txt
     sed -i '' "/WARN util.NativeCodeLoader/d" /tmp/errors.txt
     sed -i '' "/\/app\/swlib/d" /tmp/errors.txt
 else
     echo "You are on ${UNAME}"
-    sed -i "/INFO fs.TrashPolicyDefault/d" /tmp/errors.txt
+    sed -i "/INFO /d" /tmp/errors.txt
     sed -i "/WARN util.NativeCodeLoader/d" /tmp/errors.txt
     sed -i "/\/app\/swlib/d" /tmp/errors.txt
 fi
