4b954d7cfa5e7ae5d5069686269c95d26cf89b76
yinpeng.li <yinpeng.li@479f5059-62a3-4c39-9342-9852e3a68435>
2017-10-24 02:56:43 +0000

    Review Request 11860 PLS-4857 Improve code coverage - metadata controller - StatisticsContainerResource

---
+++ le-metadata/src/test/java/com/latticeengines/metadata/controller/StatisticsContainerResourceDeploymentTestNG.java
@@ -0,0 +1,72 @@
+package com.latticeengines.metadata.controller;
+
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
+import static org.testng.AssertJUnit.assertEquals;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.latticeengines.domain.exposed.metadata.Category;
+import com.latticeengines.domain.exposed.metadata.DataCollection;
+import com.latticeengines.domain.exposed.metadata.StatisticsContainer;
+import com.latticeengines.domain.exposed.metadata.statistics.CategoryStatistics;
+import com.latticeengines.domain.exposed.metadata.statistics.Statistics;
+import com.latticeengines.metadata.functionalframework.MetadataDeploymentTestNGBase;
+import com.latticeengines.proxy.exposed.metadata.DataCollectionProxy;
+import com.latticeengines.proxy.exposed.metadata.StatisticsContainerProxy;
+
+public class StatisticsContainerResourceDeploymentTestNG extends MetadataDeploymentTestNGBase {
+    private static final Logger log = LoggerFactory.getLogger(StatisticsContainerResourceDeploymentTestNG.class);
+
+    @Autowired
+    private StatisticsContainerProxy statisticsContainerProxy;
+
+    @Autowired
+    private DataCollectionProxy dataCollectionProxy;
+
+    private StatisticsContainer container;
+
+    @Override
+    @BeforeClass(groups = "deployment")
+    public void setup() {
+        super.setup();
+    }
+
+    @Test(groups = "deployment")
+    public void testCreate() {
+        container = new StatisticsContainer();
+        Statistics statistics = new Statistics();
+        statistics.getCategories().put(Category.ACCOUNT_INFORMATION, new CategoryStatistics());
+        container.setStatistics(statistics);
+        DataCollection.Version version = dataCollectionProxy.getDefaultDataCollection(customerSpace1).getVersion();
+        container.setVersion(version);
+        dataCollectionProxy.upsertStats(customerSpace1, container);
+        container = dataCollectionProxy.getStats(customerSpace1);
+        assertNotNull(container);
+        log.info("stats name = " + container.getName());
+    }
+
+    @Test(groups = "deployment", dependsOnMethods = "testCreate")
+    public void testRetrieve() {
+        // from collection
+        StatisticsContainer retrieved = dataCollectionProxy.getStats(customerSpace1);
+        assertEquals(retrieved.getStatistics().getCategories().size(), container.getStatistics().getCategories().size());
+        assertTrue(retrieved.getStatistics().getCategories().containsKey(Category.ACCOUNT_INFORMATION));
+
+        // from stats directly
+        retrieved = statisticsContainerProxy.getStatistics(customerSpace1, container.getName());
+        assertEquals(retrieved.getStatistics().getCategories().size(), container.getStatistics().getCategories().size());
+        assertTrue(retrieved.getStatistics().getCategories().containsKey(Category.ACCOUNT_INFORMATION));
+    }
+
+    @Test(groups = "deployment", dependsOnMethods = "testRetrieve")
+    public void testDelete() {
+        statisticsContainerProxy.deleteStatistics(customerSpace1, container.getName());
+        StatisticsContainer retrieved = statisticsContainerProxy.getStatistics(customerSpace1, container.getName());
+        assertEquals(retrieved, null);
+    }
+}
