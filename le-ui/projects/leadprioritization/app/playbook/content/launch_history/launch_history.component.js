4896e989721b2e85f2209aa6261860f0c2ff88e9
jhadden <jhadden@479f5059-62a3-4c39-9342-9852e3a68435>
2017-11-03 00:36:43 +0000

    Review Request 12019: 1) Play Tabs & Launch History

--- le-ui/projects/leadprioritization/app/playbook/content/launch_history/launch_history.component.js
+++ le-ui/projects/leadprioritization/app/playbook/content/launch_history/launch_history.component.js
@@ -56,6 +56,10 @@ angular.module('lp.playbook.dashboard.launch_history', [])
 
     };
 
+    vm.playSelectChange = function(play){
+        console.log(play);
+    };
+
     vm.play_name_required = function(){
         return !vm.stored.play_display_name;
     };
@@ -88,4 +92,42 @@ angular.module('lp.playbook.dashboard.launch_history', [])
     }
 
     vm.init();
+})
+.filter('unique', function () {
+
+    return function (items, filterOn) {
+
+        if (filterOn === false) {
+            return items;
+        }
+
+        if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
+            var hashCheck = {}, newItems = [];
+
+            var extractValueToCompare = function (item) {
+                if (angular.isObject(item) && angular.isString(filterOn)) {
+                    return item[filterOn];
+                } else {
+                    return item;
+                }
+            };
+
+            angular.forEach(items, function (item) {
+                var valueToCheck, isDuplicate = false;
+
+                for (var i = 0; i < newItems.length; i++) {
+                    if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                if (!isDuplicate) {
+                    newItems.push(item);
+                }
+
+            });
+            items = newItems;
+        }
+        return items;
+    };
 });
