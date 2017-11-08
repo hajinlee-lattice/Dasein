c5f16f05b613d4ed82e7e3433a198af9878e3082
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2017-11-08 06:56:49 +0000

    Review Request 12081: fix syntax refresh key syntax

--- le-domain/src/main/java/com/latticeengines/domain/exposed/camille/Path.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/camille/Path.java
@@ -38,7 +38,7 @@ public class Path implements Serializable {
 
     public Path(List<String> parts) throws IllegalArgumentException {
         for (String part : parts) {
-            if (!part.matches("^[\\w\\-.]+$")) {
+            if (!part.matches("^[\\w\\-|.]+$")) {
                 throw new IllegalArgumentException("Provided path array part " + part + " is invalid.");
             }
         }
@@ -48,7 +48,7 @@ public class Path implements Serializable {
 
     public Path(String... parts) throws IllegalArgumentException {
         for (String part : parts) {
-            if (!part.matches("^[\\w\\-.]+$")) {
+            if (!part.matches("^[\\w\\-|.]+$")) {
                 throw new IllegalArgumentException("Provided path array part " + part + " is invalid.");
             }
         }
