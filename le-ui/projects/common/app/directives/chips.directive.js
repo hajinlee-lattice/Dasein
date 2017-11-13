beffe92c75305cddbb2f843f7898037949103cab
lgigli <lgigli@479f5059-62a3-4c39-9342-9852e3a68435>
2017-11-13 18:12:32 +0000

    Review Request 12143

---
+++ le-ui/projects/common/app/directives/chips.directive.js
@@ -0,0 +1,134 @@
+angular.module('mainApp.appCommon.directives.chips', [])
+.directive('chips', function () {
+    return {
+        restrict: 'E',
+        template: `<div class="chips-query-container">
+                        <div class="chips-container">
+                            <div class="chip" ng-repeat="(key, value) in chips">
+                                <div class="chip-container">
+                                    <span>{{value.name}}</span>
+                                    <i class="chip-remove fa fa-times" aria-hidden="true" ng-click="removeItem(value.id)"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="text-container dropdown" ng-class="(showQueryList) ?'show-dropdown' : ''">
+                            <input class="query" type="text" placeholder="{{placeholder}}"  ng-keydown="queryKeyPressed($event)" ng-model="query" ng-blur="blurText()">
+                            <div class="dropdown-content" ng-class="(showQueryList) ?'show-dropdown' : ''">
+                                <div class="query-result-row" ng-repeat="item in (filteredItems = (queryItems | filter:query))" 
+                                    ng-class="(isRowSelected($index)) ?'query-result-row-selected-test' : ''"
+                                    ng-click="chooseItem(item)" ng-mouseover="hoverIn()" ng-mouseleave="hoverOut()">
+                                    {{item.name}}
+                                </div>
+                            </div>
+                        </div>
+                    </div>`,
+        scope: { placeholder: '@', datasource: '=', callback: '&callbackFunction' },
+        link: function (scope, element, attrs, ctrl) {
+
+            scope.showClass = ''
+            scope.chips = {};
+            scope.positionInQueryList = 0;
+            scope.query = '';
+            scope.filteredItems = [];
+            scope.queryItems = scope.datasource;
+            scope.mouseOut = true;
+            scope.blur = true;
+            scope.showQueryList = false;
+
+
+            scope.isRowSelected = function (index) {
+
+                if (index == scope.positionInQueryList) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            scope.blurText = function () {
+                if (scope.mouseOut && scope.blur) {
+                    scope.showQueryList = false;
+                }
+                scope.blur = true;
+            }
+
+            scope.showQueryResult = function () {
+
+                if (scope.query.length > 0) {
+                    scope.showQueryList = true;
+                } else {
+                    scope.positionInQueryList = 0;
+                    scope.showQueryList = false;
+                }
+                return scope.showQueryList;
+            }
+            scope.queryKeyPressed = function (keyEvent) {
+                scope.blur = false;
+                if (scope.query.length >= 0) {
+                    scope.showQueryList = true;
+                }
+
+                if ('ArrowDown' === keyEvent.key) {
+                    keyEvent.preventDefault()
+                    var items = scope.filteredItems;
+                    var l = items.length;
+                    if (scope.positionInQueryList < scope.filteredItems.length - 1) {
+                        scope.positionInQueryList = scope.positionInQueryList + 1;
+                    }
+                }
+                if ('ArrowUp' === keyEvent.key) {
+                    keyEvent.preventDefault()
+                    if (scope.positionInQueryList > 0) {
+                        scope.positionInQueryList = scope.positionInQueryList - 1;
+                    }
+                }
+                if (keyEvent.key === 'Enter') {
+                    scope.chooseItem(scope.filteredItems[scope.positionInQueryList]);
+                    scope.positionInQueryList = -1;
+
+                }
+            }
+            scope.chooseItem = function (item) {
+                if (item) {
+                    if (scope.chips[item.id] === undefined) {
+                        scope.chips[item.id] = item;
+                    }
+                    scope.callCallback();
+                }
+            }
+            scope.removeItem = function (id) {
+                delete scope.chips[id];
+                scope.callCallback();
+            }
+
+            scope.callCallback = function () {
+                if (typeof (scope.callback) != undefined) {
+                    scope.callback({ args: scope.chips });
+                }
+            }
+
+            scope.hoverIn = function () {
+                scope.showQueryList = true;
+                scope.mouseOverRow = true;
+                scope.clearPositionInQueryList();
+            }
+            scope.hoverOut = function () {
+                scope.mouseOverRow = false;
+                if (scope.blur) {
+                    scope.clearPositionInQueryList();
+                    scope.showQueryList = false;
+                }
+            }
+
+            scope.clearPositionInQueryList = function () {
+                scope.positionInQueryList = -1;
+            }
+            scope.clearQuery = function () {
+                scope.query = '';
+            }
+
+            scope.setListVisibility = function (visible) {
+                scope.showQueryList = visible;
+            }
+        }
+    }
+});
--- le-ui/projects/leadprioritization/app/ratingsengine/content/ai/refine/ai-refine-target.component.js
+++ le-ui/projects/leadprioritization/app/ratingsengine/content/ai/refine/ai-refine-target.component.js
@@ -1,4 +1,4 @@
-angular.module('lp.ratingsengine.ai.refine', [])
+angular.module('lp.ratingsengine.ai.refine', ['mainApp.appCommon.directives.chips'])
     .controller('RatingsEngineAIRefineTarget', function ($scope, RefineService) {
         var vm = this;
 
@@ -6,7 +6,7 @@ angular.module('lp.ratingsengine.ai.refine', [])
             refine: RefineService.refineModel,
             sellOption: '',
             notyet: false,
-            resell: false,
+            resell: false
 
         });
 
@@ -16,18 +16,19 @@ angular.module('lp.ratingsengine.ai.refine', [])
             $scope.$watch(function () {
                 return RefineService.refineModel;
             },
-            function (newVal, oldVal) {
-                // alert("Inside watch");
-                vm.refine = newVal;
-                console.log('NEW ' + newVal + ' - OLD ' + oldVal);
-            }, true);
+                function (newVal, oldVal) {
+                    // alert("Inside watch");
+                    vm.refine = newVal;
+                    console.log('NEW ' + newVal + ' - OLD ' + oldVal);
+                }, true);
+
+
         }
 
         vm.showRefineModel = function () {
             console.log('Change view');
             RefineService.changeValue();
         }
-
         vm.init();
     })
     .controller('RatingsEngineAIRefineModel', function ($scope, RefineService) {
@@ -38,7 +39,10 @@ angular.module('lp.ratingsengine.ai.refine', [])
             bought: false,
             historical: false,
             similarProducts: false,
-            similarSegments: false
+            similarSegments: false,
+            productDataSource: [],
+            datasource: []
+
         });
 
         vm.init = function () {
@@ -46,11 +50,45 @@ angular.module('lp.ratingsengine.ai.refine', [])
             $scope.$watch(function () {
                 return RefineService.refineModel;
             },
-            function (newVal, oldVal) {
-                // alert("Inside watch");
-                vm.refine =newVal;
-                console.log('NEW ' + newVal + ' - OLD ' + oldVal);
-            }, true);
+                function (newVal, oldVal) {
+                    // alert("Inside watch");
+                    vm.refine = newVal;
+                    console.log('NEW ' + newVal + ' - OLD ' + oldVal);
+                }, true);
+            vm.productsDataSource =
+                [
+                    { 'id': 1, 'name': 'iPhone6' },
+                    { 'id': 2, 'name': 'iPhone6+' },
+                    { 'id': 3, 'name': 'iPhone7' },
+                    { 'id': 4, 'name': 'iPhone7+' },
+                    { 'id': 5, 'name': 'iPhoneX' },
+                    { 'id': 6, 'name': 'Nokya' },
+                    { 'id': 7, 'name': 'Samsung7' },
+                    { 'id': 8, 'name': 'Google Phone' }
+                ];
+            vm.datasource =
+                [
+                    { 'id': 1, 'name': 'New Yourk' },
+                    { 'id': 2, 'name': 'Russian' },
+                    { 'id': 3, 'name': 'Italy' },
+                    { 'id': 4, 'name': 'Florence' },
+                    { 'id': 5, 'name': 'Rome' },
+                    { 'id': 6, 'name': 'London' },
+                    { 'id': 7, 'name': 'Paris' },
+                    { 'id': 8, 'name': 'Madrid' },
+                    { 'id': 9, 'name': 'Instambul' },
+                    { 'id': 10, 'name': 'Tokyo' },
+                    { 'id': 11, 'name': 'San Jose' },
+                    { 'id': 12, 'name': 'San Francisco' }
+                ];
+        }
+
+        vm.callbackSegments = function (element) {
+            console.log(element);
+            // console.log(element.args);
+        }
+        vm.productsCallback = function (elements) {
+            console.log(elements);
         }
 
         vm.init();
@@ -58,7 +96,7 @@ angular.module('lp.ratingsengine.ai.refine', [])
     .service('RefineService', function () {
         this.refineModel = false;
 
-        this.reset = function(){
+        this.reset = function () {
             this.refineModel = false;
         }
 
