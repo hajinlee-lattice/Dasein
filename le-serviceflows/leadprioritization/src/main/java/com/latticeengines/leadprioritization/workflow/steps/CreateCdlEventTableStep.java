0e7ccc9e5148bd3eef43d4711e2f7e96a0a02acd
lming <lming@479f5059-62a3-4c39-9342-9852e3a68435>
2017-11-20 20:03:39 +0000

    Review Request 12270: Add production Id and Category into the metadata

--- le-serviceflows/leadprioritization/src/main/java/com/latticeengines/leadprioritization/workflow/steps/CreateCdlEventTableStep.java
+++ le-serviceflows/leadprioritization/src/main/java/com/latticeengines/leadprioritization/workflow/steps/CreateCdlEventTableStep.java
@@ -2,6 +2,8 @@ package com.latticeengines.leadprioritization.workflow.steps;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.collections4.CollectionUtils;
 import org.slf4j.Logger;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Component;
 import com.latticeengines.domain.exposed.dataflow.DataFlowParameters;
 import com.latticeengines.domain.exposed.metadata.ApprovedUsage;
 import com.latticeengines.domain.exposed.metadata.Attribute;
+import com.latticeengines.domain.exposed.metadata.Category;
 import com.latticeengines.domain.exposed.metadata.FundamentalType;
 import com.latticeengines.domain.exposed.metadata.InterfaceName;
 import com.latticeengines.domain.exposed.metadata.LogicalDataType;
@@ -85,11 +88,13 @@ public class CreateCdlEventTableStep extends RunDataFlow<CreateCdlEventTableConf
                     || InterfaceName.AnalyticPurchaseState_ID.name().equalsIgnoreCase(name)) {
                 attribute.setApprovedUsage(ApprovedUsage.NONE);
                 attribute.setTags(ModelingMetadata.EXTERNAL_TAG);
+                attribute.setCategory(Category.ACCOUNT_INFORMATION);
                 if (InterfaceName.AnalyticPurchaseState_ID.name().equalsIgnoreCase(name)) {
                     attribute.setLogicalDataType(LogicalDataType.InternalId);
                 }
             } else {
                 attribute.setApprovedUsage(ApprovedUsage.MODEL_ALLINSIGHTS);
+                attribute.setCategory(Category.ACCOUNT_INFORMATION);
                 attribute.setDisplayDiscretizationStrategy("{\"unified\": {}}");
                 attribute.setTags(ModelingMetadata.INTERNAL_TAG);
                 setDisplayNameAndOthers(attribute, name);
@@ -121,30 +126,45 @@ public class CreateCdlEventTableStep extends RunDataFlow<CreateCdlEventTableConf
 
     protected void setDisplayNameAndOthers(Attribute attribute, String name) {
         if (name.matches("Product_.*_Revenue")) {
-            attribute.setDisplayName("Last Period Spend");
+            Pattern pattern = Pattern.compile("Product_(.*)_Revenue");
+            Matcher matcher = pattern.matcher(name);
+            matcher.matches();
+            attribute.setDisplayName("Last Period Spend for " + matcher.group(1));
             attribute.setDescription("Product spend in last period");
             attribute.setFundamentalType(FundamentalType.CURRENCY);
             return;
         }
         if (name.matches("Product_.*_RevenueRollingSum6")) {
-            attribute.setDisplayName("6-Period Spend");
+            Pattern pattern = Pattern.compile("Product_(.*)_RevenueRollingSum6");
+            Matcher matcher = pattern.matcher(name);
+            matcher.matches();
+            attribute.setDisplayName("6-Period Spend for " + matcher.group(1));
             attribute.setDescription("Product spend for last 6 periods");
             attribute.setFundamentalType(FundamentalType.CURRENCY);
             return;
         }
         if (name.matches("Product_.*_RevenueMomentum3")) {
-            attribute.setDisplayName("Rate of Change of 3-Period Spend");
+            Pattern pattern = Pattern.compile("Product_(.*)_RevenueMomentum3");
+            Matcher matcher = pattern.matcher(name);
+            matcher.matches();
+            attribute.setDisplayName("Rate of Change of 3-Period Spend for " + matcher.group(1));
             attribute.setDescription(
                     "Percent change in the 3 period spend, where values > 0 show increasing spend and < 0 indicate decreasing spend");
             return;
         }
         if (name.matches("Product_.*_Units")) {
-            attribute.setDisplayName("Last Period Units");
+            Pattern pattern = Pattern.compile("Product_(.*)_Units");
+            Matcher matcher = pattern.matcher(name);
+            matcher.matches();
+            attribute.setDisplayName("Last Period Units for " + matcher.group(1));
             attribute.setDescription("Units purchased in last period");
             return;
         }
         if (name.matches("Product_.*_Span")) {
-            attribute.setDisplayName("Purchase Recency");
+            Pattern pattern = Pattern.compile("Product_(.*)_Span");
+            Matcher matcher = pattern.matcher(name);
+            matcher.matches();
+            attribute.setDisplayName("Purchase Recency for " + matcher.group(1));
             attribute.setDescription(
                     "Indicator of how recently a customer purchased, where higher values indicate more recent purchase (e.g. 1 = Last Period  and 0 = Never)");
             return;
