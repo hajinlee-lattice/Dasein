5a2228bbe1ec78a8f031f75ae7a098dc977fa759
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-12-07 16:48:21 +0000

    Review Request 12430 clone data table

--- le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/workflow/steps/update/BaseCloneEntityStep.java
+++ le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/workflow/steps/update/BaseCloneEntityStep.java
@@ -1,34 +1,25 @@
 package com.latticeengines.cdl.workflow.steps.update;
 
-import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 import javax.inject.Inject;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 
-import com.latticeengines.camille.exposed.CamilleEnvironment;
-import com.latticeengines.camille.exposed.paths.PathBuilder;
-import com.latticeengines.common.exposed.util.HdfsUtils;
-import com.latticeengines.common.exposed.util.JsonUtils;
 import com.latticeengines.common.exposed.util.NamingUtils;
 import com.latticeengines.domain.exposed.camille.CustomerSpace;
 import com.latticeengines.domain.exposed.metadata.DataCollection;
-import com.latticeengines.domain.exposed.metadata.Extract;
 import com.latticeengines.domain.exposed.metadata.Table;
 import com.latticeengines.domain.exposed.metadata.TableRoleInCollection;
-import com.latticeengines.domain.exposed.metadata.TableType;
 import com.latticeengines.domain.exposed.query.BusinessEntity;
 import com.latticeengines.domain.exposed.serviceflows.cdl.steps.process.ProcessStepConfiguration;
 import com.latticeengines.proxy.exposed.metadata.DataCollectionProxy;
 import com.latticeengines.proxy.exposed.metadata.MetadataProxy;
 import com.latticeengines.redshiftdb.exposed.service.RedshiftService;
 import com.latticeengines.scheduler.exposed.LedpQueueAssigner;
-import com.latticeengines.security.exposed.util.MultiTenantContext;
 import com.latticeengines.serviceflows.workflow.core.BaseWorkflowStep;
+import com.latticeengines.serviceflows.workflow.util.TableCloneUtils;
 
 public abstract class BaseCloneEntityStep<T extends ProcessStepConfiguration> extends BaseWorkflowStep<T> {
 
@@ -67,11 +58,10 @@ public abstract class BaseCloneEntityStep<T extends ProcessStepConfiguration> ex
         String activeTableName = dataCollectionProxy.getTableName(customerSpace.toString(), role, active);
         Table activeTable = dataCollectionProxy.getTable(customerSpace.toString(), role, active);
         String cloneName = NamingUtils.timestamp(role.name());
-        Table inactiveTable = JsonUtils.clone(activeTable);
-        inactiveTable.setExtracts(null);
-        inactiveTable.setTableType(TableType.DATATABLE);
-        inactiveTable.setName(cloneName);
-        inactiveTable = cloneDataTable(activeTable, inactiveTable);
+        String queue = LedpQueueAssigner.getPropDataQueueNameForSubmission();
+        queue = LedpQueueAssigner.overwriteQueueAssignment(queue, queueScheme);
+        Table inactiveTable = TableCloneUtils //
+                .cloneDataTable(yarnConfiguration, customerSpace, cloneName, activeTable, queue);
         if (isServingStore(role)) {
             copyRedshiftTable(activeTableName, cloneName);
         }
@@ -82,68 +72,6 @@ public abstract class BaseCloneEntityStep<T extends ProcessStepConfiguration> ex
     private void copyRedshiftTable(String original, String clone) {
         redshiftService.dropTable(clone);
         redshiftService.cloneTable(original, clone);
-    }
-
-    private Table cloneDataTable(Table original, Table clone) {
-        String cloneDataPath = PathBuilder.buildDataTablePath(CamilleEnvironment.getPodId(),
-                customerSpace, original.getNamespace()).append(clone.getName()).toString();
-
-        Extract newExtract = new Extract();
-        newExtract.setPath(cloneDataPath + "/*.avro");
-        newExtract.setName(NamingUtils.uuid("Extract"));
-        AtomicLong count = new AtomicLong(0);
-        if (original.getExtracts() != null && original.getExtracts().size() > 0) {
-            try {
-                if (HdfsUtils.fileExists(yarnConfiguration, cloneDataPath)) {
-                    HdfsUtils.rmdir(yarnConfiguration, cloneDataPath);
-                }
-                HdfsUtils.mkdir(yarnConfiguration, cloneDataPath);
-            } catch (IOException e) {
-                throw new RuntimeException("Failed to create table dir at " + cloneDataPath);
-            }
-
-            original.getExtracts().forEach(extract -> {
-                String srcPath = extract.getPath();
-                if (!srcPath.endsWith(".avro") && !srcPath.endsWith("*.avro")) {
-                    srcPath = srcPath.endsWith("/") ? srcPath : srcPath + "/";
-                    srcPath += "*.avro";
-                }
-                String srcDir = srcPath.substring(0, srcPath.lastIndexOf("/"));
-                try {
-                    String queue = LedpQueueAssigner.getPropDataQueueNameForSubmission();
-                    queue = LedpQueueAssigner.overwriteQueueAssignment(queue, queueScheme);
-                    log.info(String.format("Copying table data from %s to %s", srcDir, cloneDataPath));
-                    HdfsUtils.distcp(yarnConfiguration, srcDir, cloneDataPath, queue);
-                } catch (Exception e) {
-                    throw new RuntimeException(String.format("Failed to copy in HDFS from %s to %s", srcPath,
-                            cloneDataPath), e);
-                }
-
-                if (extract.getProcessedRecords() != null && extract.getProcessedRecords() > 0) {
-                    count.addAndGet(extract.getProcessedRecords());
-                }
-            });
-        }
-        newExtract.setProcessedRecords(count.get());
-        newExtract.setExtractionTimestamp(System.currentTimeMillis());
-        clone.setExtracts(Collections.singletonList(newExtract));
-
-        String oldTableSchema = PathBuilder.buildDataTableSchemaPath(CamilleEnvironment.getPodId(),
-                MultiTenantContext.getCustomerSpace(), original.getNamespace()).append(original.getName()).toString();
-        String cloneTableSchema = oldTableSchema.replace(original.getName(), clone.getName());
-
-        try {
-            if (HdfsUtils.fileExists(yarnConfiguration, oldTableSchema)) {
-                HdfsUtils.copyFiles(yarnConfiguration, oldTableSchema, cloneTableSchema);
-                log.info(String.format("Copying table schema from %s to %s", oldTableSchema, cloneTableSchema));
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(String.format("Failed to copy schema in HDFS from %s to %s", oldTableSchema,
-                    cloneTableSchema), e);
-        }
-
-        metadataProxy.createTable(customerSpace.toString(), clone.getName(), clone);
-        return metadataProxy.getTable(customerSpace.toString(), clone.getName());
     }
 
     private boolean isServingStore(TableRoleInCollection role) {
---
+++ le-serviceflows/core/src/main/java/com/latticeengines/serviceflows/workflow/util/TableCloneUtils.java
@@ -0,0 +1,93 @@
+package com.latticeengines.serviceflows.workflow.util;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.apache.hadoop.conf.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.latticeengines.camille.exposed.CamilleEnvironment;
+import com.latticeengines.camille.exposed.paths.PathBuilder;
+import com.latticeengines.common.exposed.util.HdfsUtils;
+import com.latticeengines.common.exposed.util.JsonUtils;
+import com.latticeengines.common.exposed.util.NamingUtils;
+import com.latticeengines.domain.exposed.camille.CustomerSpace;
+import com.latticeengines.domain.exposed.metadata.Extract;
+import com.latticeengines.domain.exposed.metadata.Table;
+import com.latticeengines.domain.exposed.metadata.TableType;
+import com.latticeengines.security.exposed.util.MultiTenantContext;
+
+public class TableCloneUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(TableCloneUtils.class);
+
+    public static Table cloneDataTable(Configuration yarnConfiguration, //
+                                       CustomerSpace customerSpace, //
+                                       String cloneName, //
+                                       Table original, //
+                                       String distCpQueue) {
+        Table clone = JsonUtils.clone(original);
+        clone.setTableType(TableType.DATATABLE);
+        clone.setName(cloneName);
+
+        String cloneDataPath = PathBuilder.buildDataTablePath(CamilleEnvironment.getPodId(),
+                customerSpace, original.getNamespace()).append(clone.getName()).toString();
+
+        Extract newExtract = new Extract();
+        newExtract.setPath(cloneDataPath + "/*.avro");
+        newExtract.setName(NamingUtils.uuid("Extract"));
+        AtomicLong count = new AtomicLong(0);
+        if (original.getExtracts() != null && original.getExtracts().size() > 0) {
+            try {
+                if (HdfsUtils.fileExists(yarnConfiguration, cloneDataPath)) {
+                    HdfsUtils.rmdir(yarnConfiguration, cloneDataPath);
+                }
+                HdfsUtils.mkdir(yarnConfiguration, cloneDataPath);
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to create table dir at " + cloneDataPath);
+            }
+
+            original.getExtracts().forEach(extract -> {
+                String srcPath = extract.getPath();
+                if (!srcPath.endsWith(".avro") && !srcPath.endsWith("*.avro")) {
+                    srcPath = srcPath.endsWith("/") ? srcPath : srcPath + "/";
+                    srcPath += "*.avro";
+                }
+                String srcDir = srcPath.substring(0, srcPath.lastIndexOf("/"));
+                try {
+                    log.info(String.format("Copying table data from %s to %s", srcDir, cloneDataPath));
+                    HdfsUtils.distcp(yarnConfiguration, srcDir, cloneDataPath, distCpQueue);
+                } catch (Exception e) {
+                    throw new RuntimeException(String.format("Failed to copy in HDFS from %s to %s", srcPath,
+                            cloneDataPath), e);
+                }
+
+                if (extract.getProcessedRecords() != null && extract.getProcessedRecords() > 0) {
+                    count.addAndGet(extract.getProcessedRecords());
+                }
+            });
+        }
+        newExtract.setProcessedRecords(count.get());
+        newExtract.setExtractionTimestamp(System.currentTimeMillis());
+        clone.setExtracts(Collections.singletonList(newExtract));
+
+        String oldTableSchema = PathBuilder.buildDataTableSchemaPath(CamilleEnvironment.getPodId(),
+                MultiTenantContext.getCustomerSpace(), original.getNamespace()).append(original.getName()).toString();
+        String cloneTableSchema = oldTableSchema.replace(original.getName(), clone.getName());
+
+        try {
+            if (HdfsUtils.fileExists(yarnConfiguration, oldTableSchema)) {
+                HdfsUtils.copyFiles(yarnConfiguration, oldTableSchema, cloneTableSchema);
+                log.info(String.format("Copying table schema from %s to %s", oldTableSchema, cloneTableSchema));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(String.format("Failed to copy schema in HDFS from %s to %s", oldTableSchema,
+                    cloneTableSchema), e);
+        }
+
+        return clone;
+    }
+
+}
