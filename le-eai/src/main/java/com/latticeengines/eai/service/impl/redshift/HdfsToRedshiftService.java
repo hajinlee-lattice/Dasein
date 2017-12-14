46f53f8f5371d48e00d25a2d7a914d7f29e70599
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2017-12-14 23:43:37 +0000

    Review Request 12430 fix hdfs to redshift service

--- le-eai/src/main/java/com/latticeengines/eai/service/impl/redshift/HdfsToRedshiftService.java
+++ le-eai/src/main/java/com/latticeengines/eai/service/impl/redshift/HdfsToRedshiftService.java
@@ -113,13 +113,11 @@ public class HdfsToRedshiftService extends EaiRuntimeService<HdfsToRedshiftConfi
             createRedshiftTableIfNotExist(configuration);
             redshiftService.loadTableFromAvroInS3(stagingTableName, redshiftTableConfig.getS3Bucket(),
                     s3Prefix(tableName), redshiftTableConfig.getJsonPathPrefix());
-            Long countStage = redshiftService.countTable(tableName);
-            log.info("After staging, there are " + countStage + " rows in " + stagingTableName);
             redshiftService.dropTable(stagingTableName);
             redshiftService.analyzeTable(stagingTableName);
             redshiftService.replaceTable(stagingTableName, tableName);
             Long countAfter = redshiftService.countTable(tableName);
-            log.info("After creation, there are " + countAfter + " rows in " + tableName);
+            log.info("Created a table with " + countAfter + " rows: " + tableName);
             redshiftService.dropTable(stagingTableName);
         } else {
             redshiftService.loadTableFromAvroInS3(tableName, redshiftTableConfig.getS3Bucket(), s3Prefix(tableName),
