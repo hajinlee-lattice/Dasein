0943293c6a9e34408cd0cf42852033cb4dc7f011
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2017-12-21 01:16:39 +0000

    Review Request 12626: try to use utf8 charset for serialize and deserialize context

--- le-workflow/src/main/java/com/latticeengines/workflow/core/Uft8JdbcExecutionContextDao.java
+++ le-workflow/src/main/java/com/latticeengines/workflow/core/Uft8JdbcExecutionContextDao.java
@@ -3,19 +3,29 @@ package com.latticeengines.workflow.core;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.hsqldb.types.Charset;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.repository.ExecutionContextSerializer;
 import org.springframework.batch.core.repository.dao.JdbcExecutionContextDao;
 import org.springframework.batch.item.ExecutionContext;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.util.Assert;
 
 public class Uft8JdbcExecutionContextDao extends JdbcExecutionContextDao {
+
+    private static final String FIND_JOB_EXECUTION_CONTEXT = "SELECT SHORT_CONTEXT, SERIALIZED_CONTEXT "
+            + "FROM %PREFIX%JOB_EXECUTION_CONTEXT WHERE JOB_EXECUTION_ID = ?";
+
+    private static final String FIND_STEP_EXECUTION_CONTEXT = "SELECT SHORT_CONTEXT, SERIALIZED_CONTEXT "
+            + "FROM %PREFIX%STEP_EXECUTION_CONTEXT WHERE STEP_EXECUTION_ID = ?";
 
     private ExecutionContextSerializer serializer;
 
@@ -34,12 +44,40 @@ public class Uft8JdbcExecutionContextDao extends JdbcExecutionContextDao {
 
         try {
             serializer.serialize(m, out);
-            results = new String(out.toByteArray(), Charset.UTF8.toString());
+            results = new String(out.toByteArray(), Charset.forName("UTF-8"));
         } catch (IOException ioe) {
             throw new IllegalArgumentException("Could not serialize the execution context", ioe);
         }
 
         return results;
+    }
+
+    @Override
+    public ExecutionContext getExecutionContext(JobExecution jobExecution) {
+        Long executionId = jobExecution.getId();
+        Assert.notNull(executionId, "ExecutionId must not be null.");
+
+        List<ExecutionContext> results = getJdbcTemplate().query(getQuery(FIND_JOB_EXECUTION_CONTEXT),
+                new ExecutionContextRowMapper(), executionId);
+        if (results.size() > 0) {
+            return results.get(0);
+        } else {
+            return new ExecutionContext();
+        }
+    }
+
+    @Override
+    public ExecutionContext getExecutionContext(StepExecution stepExecution) {
+        Long executionId = stepExecution.getId();
+        Assert.notNull(executionId, "ExecutionId must not be null.");
+
+        List<ExecutionContext> results = getJdbcTemplate().query(getQuery(FIND_STEP_EXECUTION_CONTEXT),
+                new ExecutionContextRowMapper(), executionId);
+        if (results.size() > 0) {
+            return results.get(0);
+        } else {
+            return new ExecutionContext();
+        }
     }
 
     private class ExecutionContextRowMapper implements RowMapper<ExecutionContext> {
@@ -54,7 +92,7 @@ public class Uft8JdbcExecutionContextDao extends JdbcExecutionContextDao {
 
             Map<String, Object> map;
             try {
-                ByteArrayInputStream in = new ByteArrayInputStream(serializedContext.getBytes(Charset.UTF8.toString()));
+                ByteArrayInputStream in = new ByteArrayInputStream(serializedContext.getBytes(Charset.forName("UTF-8")));
                 map = serializer.deserialize(in);
             } catch (IOException ioe) {
                 throw new IllegalArgumentException("Unable to deserialize the execution context", ioe);
