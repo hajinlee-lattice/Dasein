26e6d5a87a439b4915885cdd8a94210ece404eed
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-01-03 02:48:19 +0000

    Review Request 12753 add logging to express docker

--- le-docker/src/main/scripts/nodejs/le-express/start.sh
+++ le-docker/src/main/scripts/nodejs/le-express/start.sh
@@ -3,11 +3,15 @@
 echo "APP_ROOT=${APP_ROOT}"
 
 if [ -f "/etc/ledp/lattice.crt" ]; then
+    echo "Copying /etc/ledp/lattice.crt to /etc/pki/tls/star.lattice.local.crt"
     cp -f /etc/ledp/lattice.crt /etc/pki/tls/star.lattice.local.crt
+    chmod 600 /etc/pki/tls/star.lattice.local.crt
 fi
 
 if [ -f "/etc/ledp/lattice.key" ]; then
-    cp -f /etc/ledp/lattice.key /etc/pki/tls/private/private.key
+    echo "Copying /etc/ledp/lattice.key /etc/pki/tls/private/private.key"
+    cp -f /etc/ledp/lattice.pem /etc/pki/tls/private/private.key
+    chmod 600 /etc/pki/tls/private/private.key
 fi
 
 if [ -z "${LE_ENVIRONMENT}" ]; then
