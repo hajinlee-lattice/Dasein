db446963a8152d9c5416e57c2c68c00a7bb605b7
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-01-08 03:47:37 +0000

    Review Request 12817 PLS-6318 fix display name for bit-encoded attributes

--- le-domain/src/main/java/com/latticeengines/domain/exposed/util/MetadataConverter.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/util/MetadataConverter.java
@@ -240,7 +240,11 @@ public class MetadataConverter {
             for (JsonNode bucketedAttr : (ArrayNode) bucketedAttrs) {
                 Attribute attr = new Attribute();
                 attr.setName(bucketedAttr.get("nominal_attr").asText());
-                attr.setDisplayName(attr.getName());
+                if (StringUtils.isNotBlank(attr.getDisplayName())) {
+                    attr.setDisplayName(attr.getDisplayName());
+                } else {
+                    attr.setDisplayName(attr.getName());
+                }
                 attr.setPhysicalDataType(Schema.Type.STRING.getName());
                 attr.setBitOffset(bucketedAttr.get("lowest_bit").asInt());
                 attr.setNumOfBits(bucketedAttr.get("num_bits").asInt());
