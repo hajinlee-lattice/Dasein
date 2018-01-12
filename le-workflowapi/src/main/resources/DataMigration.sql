b5a0ccb8096aedd26c8ad5821262c65c264944d7
kliu <kliu@479f5059-62a3-4c39-9342-9852e3a68435>
2018-01-12 23:10:07 +0000

    Review Request 12882: data migration script.

---
+++ le-workflowapi/src/main/resources/DataMigration.sql
@@ -0,0 +1,20 @@
+# update status which are not enum of JobStatus (PENDING, RUNNING, COMPLETED, FAILED, CANCELLED, SKIPPED)
+# UPDATE WORKFLOW_JOB
+# SET STATUS = CASE
+#              WHEN 'SUCCEEDED' THEN 'COMPLETED'
+#              WHEN 'STOPPED' THEN 'CANCELLED'
+#              WHEN 'ABANDONED' OR 'KILLED' OR 'UNKNOWN' THEN 'FAILED'
+#              WHEN 'STARTING' OR 'UNDEFINED' THEN 'PENDING'
+#              WHEN 'STARTED' OR 'STOPPING' THEN 'RUNNING'
+#              END;
+
+# update null status
+UPDATE WORKFLOW_JOB job
+    JOIN WORKFLOW_JOB_EXECUTION jobExecution ON job.WORKFLOW_ID = jobExecution.JOB_EXECUTION_ID
+SET job.STATUS = CASE
+                 WHEN jobExecution.STATUS = 'ABANDONED' OR jobExecution.STATUS = 'FAILED' OR jobExecution.STATUS = 'UNKNOWN' THEN 'FAILED'
+                 WHEN jobExecution.STATUS = 'STOPPED' THEN 'CANCELLED'
+                 WHEN jobExecution.STATUS = 'STARTED' OR jobExecution.STATUS = 'STOPPING' OR jobExecution.STATUS = 'STARTING' THEN 'RUNNING'
+                 WHEN jobExecution.STATUS = 'COMPLETED' THEN 'COMPLETED'
+                 END
+WHERE job.STATUS IS NULL;
