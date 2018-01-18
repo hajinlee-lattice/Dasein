3b5f4d7fdb8e590958884172bbda8ed3af7ae0e8
arahim <arahim@479f5059-62a3-4c39-9342-9852e3a68435>
2018-01-18 08:02:10 +0000

    Review Request 12925: PLS-5429 Simplify operators for numerical attributes in the advanced query builder.

--- le-ui/projects/common/components/datacloud/query/advanced/tree/tree.service.js
+++ le-ui/projects/common/components/datacloud/query/advanced/tree/tree.service.js
@@ -17,7 +17,7 @@ angular.module('common.datacloud.query.builder.tree.service', [])
             'GREATER_OR_EQUAL': 'is greater than or equal to',
             'LESS_THAN': 'is less than',
             'LESS_OR_EQUAL': 'is less than or equal to',
-            'GTE_AND_LTE': 'is greater or equal and lesser or equal',
+            'GTE_AND_LTE': 'is between',
             'GTE_AND_LT': 'is greater or equal and less than',
             'GT_AND_LTE': "is greater than and lesser or equal",
             'GT_AND_LT': "is greater than and less than",
@@ -27,18 +27,13 @@ angular.module('common.datacloud.query.builder.tree.service', [])
             'STARTS_WITH': 'starts with'
         };
         this.numerical_operations = {
-            'IS_NULL': 'Empty',
-            'IS_NOT_NULL': 'Not Empty',
             'EQUAL': 'Equal',
             'NOT_EQUAL': 'Not Equal',
             'GREATER_THAN': 'Greater Than',
             'GREATER_OR_EQUAL': 'Greater or Equal',
             'LESS_THAN': 'Less Than',
             'LESS_OR_EQUAL': 'Lesser or Equal',
-            'GTE_AND_LTE': '>= and <=',
-            'GTE_AND_LT': '>= and <',
-            'GT_AND_LTE': "> and <=",
-            'GT_AND_LT': "> and <"
+            'GTE_AND_LTE': 'Between'
         },
             this.enum_operations = {
                 'EQUAL': 'Is Equal To',
@@ -49,10 +44,7 @@ angular.module('common.datacloud.query.builder.tree.service', [])
             'IS_NOT_NULL'
         ];
         this.two_inputs = [
-            'GTE_AND_LTE',
-            'GTE_AND_LT',
-            'GT_AND_LTE',
-            'GT_AND_LT'
+            'GTE_AND_LTE'
         ];
 
         /**
