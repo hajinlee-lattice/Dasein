2c2d69d8a62e9a9e763edd3daeb342a7f5e345c3
ygao <ygao@479f5059-62a3-4c39-9342-9852e3a68435>
2018-01-19 23:21:07 +0000

    review request 2333 change the update scripts

---
+++ le-db/src/main/scripts/db_upgrade/pls_multitenant/previous/4.2.2/mysql.sql
@@ -0,0 +1,75 @@
+CREATE PROCEDURE `UpdateCDLTables`()
+    BEGIN
+        CREATE TABLE `CDL_EXTERNAL_SYSTEM` ( 
+	     `PID`          BIGINT NOT NULL auto_increment, 
+	     `CRM_IDS`      VARCHAR(4000), 
+	     `ERP_IDS`      VARCHAR(4000), 
+	     `MAP_IDS`      VARCHAR(4000), 
+	     `OTHER_IDS`    VARCHAR(4000), 
+	     `TENANT_ID`    BIGINT NOT NULL, 
+	     `FK_TENANT_ID` BIGINT NOT NULL, 
+	     PRIMARY KEY (`PID`) 
+	  ) 
+	ENGINE = InnoDB; 
+
+	ALTER TABLE `CDL_EXTERNAL_SYSTEM` 
+	  ADD CONSTRAINT `UKqbbh3ppo2juf81q2jm0pbws5t` UNIQUE (`TENANT_ID`); 
+
+	ALTER TABLE `CDL_EXTERNAL_SYSTEM` 
+	  ADD CONSTRAINT `FKpohehcwu5bgh0wp5gyhvl9sm` FOREIGN KEY (`FK_TENANT_ID`) 
+	  REFERENCES `TENANT` (`TENANT_PID`) ON DELETE CASCADE; 
+
+        ALTER TABLE PLS_MultiTenant.DATAFEED
+          ADD COLUMN `MAINTENANCE_MODE` bit;
+            
+        ALTER TABLE `PLS_MultiTenant`.`AI_MODEL`
+		    ADD COLUMN `FK_MODEL_SUMMARY_ID` bigint;
+		    
+		ALTER TABLE `PLS_MultiTenant`.`AI_MODEL` ADD CONSTRAINT `FKgenp90xodrrj475g7g7xcxoti` foreign key (`FK_MODEL_SUMMARY_ID`) references `PLS_MultiTenant`.`MODEL_SUMMARY` (`PID`);
+    
+    END;
+//
+DELIMITER ;
+
+CREATE PROCEDURE `UpdateRatingEngineTable`()
+    BEGIN
+        ALTER TABLE `PLS_MultiTenant`.`RATING_ENGINE`
+            ADD COLUMN `ACTIVE_MODEL_PID` bigint;    
+    END;
+//
+DELIMITER ;
+
+CREATE PROCEDURE `MigrateRatingModelTable`()
+    BEGIN
+        update RATING_ENGINE inner join RATING_MODEL
+        on RATING_ENGINE.pid = RATING_MODEL.FK_RATING_ENGINE_ID
+        set RATING_ENGINE.ACTIVE_MODEL_PID = RATING_MODEL.PID
+        where RATING_ENGINE.pid != 0
+    End;
+//
+DELIMITER ;
+
+CREATE PROCEDURE `AddActionTable`()
+    BEGIN
+        create table `ACTION` (`PID` bigint not null auto_increment, `ACTION_INITIATOR` varchar(255), `CREATED` datetime not null, `DESCRIPTION` longtext, `OWNER_ID` bigint, `TRACKING_ID` bigint, `TYPE` varchar(255) not null, `UPDATED` datetime not null, `FK_TENANT_ID` bigint not null, primary key (`PID`)) engine=InnoDB;
+        alter table `ACTION` add constraint `FK7oxur5w3ha4rj4rofg1fgekc3` foreign key (`FK_TENANT_ID`) references `TENANT` (`TENANT_PID`) on delete cascade;
+    End;
+//
+DELIMITER ;
+
+
+
+DELIMITER //
+CREATE PROCEDURE `UpdateSchema`()
+    BEGIN
+        START TRANSACTION;
+        CALL `UpdateCDLTables`();
+        CALL `UpdateRatingEngineTable`();
+        CALL `MigrateRatingModelTable`();
+        CALL `AddActionTable`();
+        COMMIT;
+    END;
+//
+DELIMITER ;
+
+CALL `UpdateSchema`();
