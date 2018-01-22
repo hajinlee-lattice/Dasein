784263dcf6d5efdc7248079e8a3a7fcef1beb369
kliu <kliu@479f5059-62a3-4c39-9342-9852e3a68435>
2018-01-22 20:10:58 +0000

    Review Request 12950: Add procedure to create WorkflowJobUpdate table.

--- le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
+++ le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
@@ -5,6 +5,18 @@ CREATE PROCEDURE `UpdateCDLTables`()
 DELIMITER ;
 
 
+CREATE PROCEDURE `AddWorkflowJobUpdate`()
+    BEGIN
+        CREATE TABLE `WORKFLOW_JOB_UPDATE` (
+            `PID` bigint(20) NOT NULL AUTO_INCREMENT,
+            `LAST_UPDATE_TIME` bigint(20) NOT NULL,
+            `WORKFLOW_PID` bigint(20) NOT NULL,
+            PRIMARY KEY (`PID`),
+            KEY `IX_WORKFLOW_PID` (`WORKFLOW_PID`)
+        ) ENGINE=InnoDB;
+    END
+//
+DELIMITER ;
 
 
 DELIMITER //
@@ -12,6 +24,7 @@ CREATE PROCEDURE `UpdateSchema`()
     BEGIN
         START TRANSACTION;
         CALL `UpdateCDLTables`();
+        CALL `AddWorkflowJobUpdate`();
         COMMIT;
     END;
 //
