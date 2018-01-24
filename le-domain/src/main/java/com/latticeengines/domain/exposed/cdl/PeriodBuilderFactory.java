b26da989239120a6e111dd732c9c4e693c7d78b5
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-01-24 18:24:47 +0000

    Review Request 12981 DP-4862 get period strategies api

--- le-domain/src/main/java/com/latticeengines/domain/exposed/cdl/PeriodBuilderFactory.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/cdl/PeriodBuilderFactory.java
@@ -2,19 +2,29 @@ package com.latticeengines.domain.exposed.cdl;
 
 import com.latticeengines.common.exposed.period.CalendarMonthPeriodBuilder;
 import com.latticeengines.common.exposed.period.CalendarQuarterPeriodBuilder;
+import com.latticeengines.common.exposed.period.CalendarWeekPeriodBuilder;
+import com.latticeengines.common.exposed.period.CalendarYearPeriodBuilder;
 import com.latticeengines.common.exposed.period.PeriodBuilder;
 
 public final class PeriodBuilderFactory {
 
     public static PeriodBuilder build(PeriodStrategy strategy) {
         switch (strategy.getTemplate()) {
+            case Week:
+                return buildWeekPeriod(strategy);
             case Month:
                 return buildMonthPeriod(strategy);
             case Quarter:
                 return buildQuarterPeriod(strategy);
+            case Year:
+                return buildYearPeriod(strategy);
             default:
                 throw new UnsupportedOperationException("Unknown strategy template " + strategy.getTemplate());
         }
+    }
+
+    private static PeriodBuilder buildWeekPeriod(PeriodStrategy strategy) {
+        return new CalendarWeekPeriodBuilder(strategy.getStartTimeStr());
     }
 
     private static PeriodBuilder buildMonthPeriod(PeriodStrategy strategy) {
@@ -23,6 +33,10 @@ public final class PeriodBuilderFactory {
 
     private static PeriodBuilder buildQuarterPeriod(PeriodStrategy strategy) {
         return new CalendarQuarterPeriodBuilder(strategy.getStartTimeStr());
+    }
+
+    private static PeriodBuilder buildYearPeriod(PeriodStrategy strategy) {
+        return new CalendarYearPeriodBuilder(strategy.getStartTimeStr());
     }
 
 }
