1a75cabb9696e56ac6ef3493af676d100db67ebe
JLazarus <JLazarus@479f5059-62a3-4c39-9342-9852e3a68435>
2018-01-26 21:17:36 +0000

    Review Request 0000 - Added ability to define a custom header template to wizard in order to aid Amaan's work

---
+++ le-ui/projects/common/components/wizard/header/header.component.js
@@ -0,0 +1,17 @@
+angular.module('common.wizard.header', [])
+.controller('WizardHeader', function(
+    $state, $stateParams, $scope, $timeout, $rootScope, ResourceUtility, WizardProgressItems,
+    WizardProgressContext, WizardControlsOptions, WizardValidationStore/*, ImportWizardService, ImportWizardStore*/
+) {
+    var vm = this;
+
+    angular.extend(vm, {
+
+    });
+
+    vm.init = function() {
+     
+    }
+
+    vm.init();
+});
