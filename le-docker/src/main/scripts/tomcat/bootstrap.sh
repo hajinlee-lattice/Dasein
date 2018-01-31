e8b1ca24b4653fa0539d1b86c324ce71d4420213
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-01-31 00:10:46 +0000

    Review Request 13043 make http2 mode toggleble

--- le-docker/src/main/scripts/tomcat/bootstrap.sh
+++ le-docker/src/main/scripts/tomcat/bootstrap.sh
@@ -11,7 +11,7 @@ SERVICE="lp"
 source ../functions.sh
 # teardown_simple_service ${SERVICE} ${CLUSTER}
 
-docker run -d \
+docker run -d --net host \
     --name ${CLUSTER}_${SERVICE} \
     -h ${HOSTNAME} \
     -e LE_ENVIRONMENT=dev \
@@ -22,9 +22,8 @@ docker run -d \
     -v ${WSHOME}/le-docker/src/main/scripts/tomcat/logs:/opt/apache-tomcat-8.5.15/logs:rw \
     -l ${SERVICE}.group=${CLUSTER} \
     -l cluster=${CLUSTER} \
-    -p 8080:8080 \
-    -p 8443:8443 \
-    -p 1099:1099 \
+    -p 80:8080 \
+    -p 443:8443 \
     latticeengines/${SERVICE}
 
 #docker run -d \
