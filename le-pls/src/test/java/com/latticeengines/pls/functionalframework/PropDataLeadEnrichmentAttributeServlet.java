ce57e6bdfabb8a693b1a5decb7cc01832cc2e551
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-02-05 20:29:36 +0000

    Review Request 13043 fix mocked column metadata proxy in pls functional test

--- le-pls/src/test/java/com/latticeengines/pls/functionalframework/PropDataLeadEnrichmentAttributeServlet.java
+++ le-pls/src/test/java/com/latticeengines/pls/functionalframework/PropDataLeadEnrichmentAttributeServlet.java
@@ -3,15 +3,19 @@ package com.latticeengines.pls.functionalframework;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.http.HttpHeaders;
 import org.apache.http.HttpStatus;
+import org.springframework.http.MediaType;
 
+import com.latticeengines.common.exposed.converter.KryoHttpMessageConverter;
 import com.latticeengines.common.exposed.util.JsonUtils;
+import com.latticeengines.common.exposed.util.KryoUtils;
 import com.latticeengines.domain.exposed.metadata.ApprovedUsage;
 import com.latticeengines.domain.exposed.metadata.Category;
 import com.latticeengines.domain.exposed.metadata.ColumnMetadata;
@@ -24,10 +28,35 @@ public class PropDataLeadEnrichmentAttributeServlet extends HttpServlet {
     private static final long serialVersionUID = -6068276954643550403L;
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("application/json");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String header = req.getHeader(HttpHeaders.ACCEPT);
+        if (KryoHttpMessageConverter.KRYO_VALUE.equals(header)) {
+            returnKryoResponse(resp);
+        } else {
+            returnJsonResponse(resp);
+        }
+    }
 
-        ColumnMetadata[] columns = new ColumnMetadata[2];
+    private void returnKryoResponse(HttpServletResponse resp) throws IOException {
+        resp.setContentType(KryoHttpMessageConverter.KRYO_VALUE);
+
+        List<ColumnMetadata> columns = Arrays.asList(getFirstColumn(), getSecondColumn());
+        KryoUtils.write(resp.getOutputStream(), columns);
+
+        resp.setStatus(HttpStatus.SC_OK);
+    }
+
+    private void returnJsonResponse(HttpServletResponse resp) throws IOException {
+        resp.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        List<ColumnMetadata> columns = Arrays.asList(getFirstColumn(), getSecondColumn());
+        String json = JsonUtils.serialize(columns);
+        resp.getWriter().write(json);
+
+        resp.setStatus(HttpStatus.SC_OK);
+    }
+
+    private ColumnMetadata getFirstColumn() {
         ColumnMetadata column = new ColumnMetadata();
         column.setColumnName("TechIndicator_AddThis");
         column.setDisplayName("Add This");
@@ -39,7 +68,11 @@ public class PropDataLeadEnrichmentAttributeServlet extends HttpServlet {
         column.setDescription("Tech Indicator Add This");
         column.setApprovedUsageList(Arrays.asList(ApprovedUsage.MODEL));
         column.setCategory(Category.WEBSITE_PROFILE);
-        columns[0] = column;
+        return column;
+    }
+
+    private ColumnMetadata getSecondColumn() {
+        ColumnMetadata column = new ColumnMetadata();
         column = new ColumnMetadata();
         column.setColumnName("TechIndicator_RemoveThis");
         column.setDisplayName("Remove This");
@@ -51,10 +84,6 @@ public class PropDataLeadEnrichmentAttributeServlet extends HttpServlet {
         column.setDescription("Tech Indicator Remove This");
         column.setApprovedUsageList(Collections.singletonList(ApprovedUsage.MODEL_MODELINSIGHTS));
         column.setCategory(Category.WEBSITE_PROFILE);
-        columns[1] = column;
-        String json = JsonUtils.serialize(columns);
-        resp.getWriter().write(json);
-
-        resp.setStatus(HttpStatus.SC_OK);
+        return column;
     }
 }
