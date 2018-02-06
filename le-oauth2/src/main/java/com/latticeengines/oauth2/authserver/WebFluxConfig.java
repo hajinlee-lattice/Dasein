18e4f6f7acf0b2465ab9ad6ad2e0b42bd153af81
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-02-06 17:06:03 +0000

    Review Request 13043 add webflux dependency to oauth2

---
+++ le-oauth2/src/main/java/com/latticeengines/oauth2/authserver/WebFluxConfig.java
@@ -0,0 +1,11 @@
+package com.latticeengines.oauth2.authserver;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class WebFluxConfig implements WebFluxConfigurer {
+
+}
