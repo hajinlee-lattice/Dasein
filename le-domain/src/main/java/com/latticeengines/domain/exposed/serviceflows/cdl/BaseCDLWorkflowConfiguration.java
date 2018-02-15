34f7921ca14311bbdbf078a0d35b34c0a4d8c4a9
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2018-02-15 16:24:20 +0000

    Review Request 13307: create serviceflows modeling and scoring modules

--- le-domain/src/main/java/com/latticeengines/domain/exposed/serviceflows/cdl/BaseCDLWorkflowConfiguration.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/serviceflows/cdl/BaseCDLWorkflowConfiguration.java
@@ -1,16 +1,23 @@
 package com.latticeengines.domain.exposed.serviceflows.cdl;
 
 import java.util.Collection;
-import java.util.Collections;
 
+import com.google.common.collect.ImmutableSet;
 import com.latticeengines.domain.exposed.swlib.SoftwareLibrary;
 import com.latticeengines.domain.exposed.workflow.WorkflowConfiguration;
 
 public class BaseCDLWorkflowConfiguration extends WorkflowConfiguration {
 
+    // @Override
+    // public Collection<String> getSwpkgNames() {
+    // return Collections.singleton(SoftwareLibrary.CDL.getName());
+    // }
     @Override
     public Collection<String> getSwpkgNames() {
-        return Collections.singleton(SoftwareLibrary.CDL.getName());
+        return ImmutableSet.<String> builder() //
+                .add(SoftwareLibrary.Modeling.getName())//
+                .addAll(super.getSwpkgNames()) //
+                .build();
     }
 
 }
