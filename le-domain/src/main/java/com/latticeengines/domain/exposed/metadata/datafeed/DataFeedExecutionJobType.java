3347d920b97898f2beb46a2650e8f575d7bb3219
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2018-02-17 06:27:25 +0000

    Review Request 13339: add lock api and retry api

--- le-domain/src/main/java/com/latticeengines/domain/exposed/metadata/datafeed/DataFeedExecutionJobType.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/metadata/datafeed/DataFeedExecutionJobType.java
@@ -3,17 +3,24 @@ package com.latticeengines.domain.exposed.metadata.datafeed;
 import com.latticeengines.domain.exposed.metadata.datafeed.DataFeed.Status;
 
 public enum DataFeedExecutionJobType {
-    CDLOperation(Status.Deleting), //
-    Import(Status.InitialLoaded), //
-    PA(Status.ProcessAnalyzing); //
+    CDLOperation {
+        @Override
+        public Status getRunningStatus() {
+            return Status.Deleting;
+        }
+    }, //
+    Import {
+        @Override
+        public Status getRunningStatus() {
+            return Status.InitialLoaded;
+        }
+    }, //
+    PA {
+        @Override
+        public Status getRunningStatus() {
+            return Status.ProcessAnalyzing;
+        }
+    }; //
 
-    private final Status status;
-
-    private DataFeedExecutionJobType(Status status) {
-        this.status = status;
-    }
-
-    public Status getRunningStatus() {
-        return status;
-    }
+    public abstract Status getRunningStatus();
 }
