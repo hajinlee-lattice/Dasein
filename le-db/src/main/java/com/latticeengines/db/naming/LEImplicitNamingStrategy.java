270a7b1320d5298a8019c9880c3229519886d319
penglong.liu <penglong.liu@479f5059-62a3-4c39-9342-9852e3a68435>
2018-02-21 04:40:48 +0000

    Review Request 13179 PLS-6052 Change all the foreign keys in the domain object

--- le-db/src/main/java/com/latticeengines/db/naming/LEImplicitNamingStrategy.java
+++ le-db/src/main/java/com/latticeengines/db/naming/LEImplicitNamingStrategy.java
@@ -13,36 +13,48 @@ public class LEImplicitNamingStrategy extends ImplicitNamingStrategyJpaCompliant
     /**
      *
      */
-    private static Integer num = 0;
     private static final long serialVersionUID = 4391821679953137277L;
     public static final LEImplicitNamingStrategy INSTANCE = new LEImplicitNamingStrategy();
 
     @Override
     public Identifier determineForeignKeyName(ImplicitForeignKeyNameSource source) {
         Identifier userProvidedIdentifier = source.getUserProvidedIdentifier();
+        Identifier tableName = source.getTableName();
         Identifier referencedTableName = source.getReferencedTableName();
         List<Identifier> columnNames = source.getColumnNames();
         List<Identifier> referencedColumnNames = source.getReferencedColumnNames();
+        StringBuilder sb = new StringBuilder("FK_");
+        sb.append(tableName.getText().replace("_", ""));
 
-        StringBuilder sb = new StringBuilder();
+        final Identifier[] columnNamesArray, referencedColumnNamesArray;
+        if (columnNames == null || columnNames.isEmpty()) {
+            columnNamesArray = new Identifier[0];
+        } else {
+            columnNamesArray = columnNames.toArray(new Identifier[columnNames.size()]);
+        }
+        if (referencedColumnNames == null || referencedColumnNames.isEmpty()) {
+            referencedColumnNamesArray = new Identifier[0];
+        } else {
+            referencedColumnNamesArray = referencedColumnNames.toArray(new Identifier[referencedColumnNames.size()]);
+        }
 
-        sb.append(referencedTableName.getText().replace("_", ""));
         if (columnNames != null && columnNames.size() != 0) {
             sb.append("_");
         }
-        for (Identifier columnName : columnNames) {
+        for (Identifier columnName : columnNamesArray) {
             sb.append(columnName.getText().replace("_", ""));
         }
 
-        for (Identifier columnName : referencedColumnNames) {
-            sb.append(columnName.getText().replace("_", ""));
-        }
-        if (referencedColumnNames != null && referencedColumnNames.size() != 0) {
-            sb.append("_");
-        }
+        sb.append("_").append(referencedTableName.getText().replace("_", ""));
+        // if (referencedColumnNames != null && referencedColumnNames.size() !=
+        // 0) {
+        // sb.append("_");
+        // }
+        // for (Identifier columnName : referencedColumnNamesArray) {
+        // sb.append(columnName.getText().replace("_", ""));
+        // }
         String result = sb.toString();
         return userProvidedIdentifier != null ? userProvidedIdentifier
-                : toIdentifier(result.substring(0, Math.min(result.length(), 28)) + (num++) % 100,
-                        source.getBuildingContext());
+                : toIdentifier(result.substring(0, Math.min(result.length(), 64)), source.getBuildingContext());
     }
 }
