16265e7264ee12ceff9b3df14889f0b75ba36632
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2018-02-21 21:48:06 +0000

    review request 0000: fix class not found

--- le-domain/src/main/java/com/latticeengines/domain/exposed/serviceflows/cdl/BaseCDLWorkflowConfiguration.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/serviceflows/cdl/BaseCDLWorkflowConfiguration.java
@@ -2,22 +2,18 @@ package com.latticeengines.domain.exposed.serviceflows.cdl;
 
 import java.util.Collection;
 
-import org.apache.commons.collections4.CollectionUtils;
-
 import com.google.common.collect.ImmutableSet;
 import com.latticeengines.domain.exposed.swlib.SoftwareLibrary;
 import com.latticeengines.domain.exposed.workflow.WorkflowConfiguration;
 
 public class BaseCDLWorkflowConfiguration extends WorkflowConfiguration {
 
-    @Override
+	@Override
     public Collection<String> getSwpkgNames() {
-        ImmutableSet.Builder<String> builder = ImmutableSet.<String>builder() //
-                .add(SoftwareLibrary.CDL.getName());
-        if (CollectionUtils.isNotEmpty(super.getSwpkgNames())) {
-            builder.addAll(super.getSwpkgNames());
-        }
-        return builder.build();
+        return ImmutableSet.<String> builder() //
+                .add(SoftwareLibrary.Modeling.getName())//
+                .add(SoftwareLibrary.CDL.getName()) //
+                .build();
     }
 
 }
