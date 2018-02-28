6f963136bc6b68f3cd4e4837fa63c0a3a4706f9a
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2018-02-28 00:25:49 +0000

    Review Request 13445: add upgrade script

--- le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
+++ le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
@@ -1,6 +1,9 @@
 CREATE PROCEDURE `UpdateCDLTables`()
     BEGIN
-    
+	    alter table DATAFEED DROP COLUMN `ACTIVE_PROFILE`;
+        drop table DATAFEED_PROFILE;
+        alter table DATAFEED_EXECUTION ADD COLUMN `JOB_TYPE` varchar(255) not null DEFAULT 'PA';
+        UPDATE DATAFEED_EXECUTION SET STATUS = 'Completed' where STATUS = 'ProcessAnalyzed';
     END;
 //
 DELIMITER ;
