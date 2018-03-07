8e1756285bd6d22853ab43d250b7d3567964cc7f
ygao <ygao@479f5059-62a3-4c39-9342-9852e3a68435>
2018-03-07 23:04:36 +0000

    Review Request 13562: Initial check in for PLS-7207

--- le-pls/src/main/java/com/latticeengines/pls/controller/JobResource.java
+++ le-pls/src/main/java/com/latticeengines/pls/controller/JobResource.java
@@ -2,8 +2,11 @@ package com.latticeengines.pls.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,14 +26,17 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping("/jobs")
 @PreAuthorize("hasRole('View_PLS_Jobs')")
 public class JobResource {
+    private static final Logger log = LoggerFactory.getLogger(JobResource.class);
 
     @Autowired
     private WorkflowJobService workflowJobService;
 
-    @RequestMapping(value = "/{jobId}", method = RequestMethod.GET, headers = "Accept=application/json")
+    @GetMapping(value = "/{jobId}")
     @ResponseBody
     @ApiOperation(value = "Get a job by id")
-    public Job find(@PathVariable String jobId) {
+    public Job find(@PathVariable String jobId, //
+            @RequestParam(value = "type", required = false) String type) {
+        log.info(String.format("Type of the job is ", type));
         return workflowJobService.find(jobId);
     }
 
