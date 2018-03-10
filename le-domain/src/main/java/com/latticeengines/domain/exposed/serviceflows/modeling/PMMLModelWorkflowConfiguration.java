49b2d3851b9a980e8752d0d6593cd005f74e087c
hliu <hliu@479f5059-62a3-4c39-9342-9852e3a68435>
2018-03-10 00:28:24 +0000

    Review Request 13563: add name space to each step to enable step configuration from different

--- le-domain/src/main/java/com/latticeengines/domain/exposed/serviceflows/modeling/PMMLModelWorkflowConfiguration.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/serviceflows/modeling/PMMLModelWorkflowConfiguration.java
@@ -4,62 +4,66 @@ import java.util.Map;
 
 import com.latticeengines.domain.exposed.camille.CustomerSpace;
 import com.latticeengines.domain.exposed.serviceflows.modeling.steps.CreatePMMLModelConfiguration;
+import com.latticeengines.domain.exposed.serviceflows.modeling.steps.ModelStepConfiguration;
 
 public class PMMLModelWorkflowConfiguration extends BaseModelingWorkflowConfiguration {
 
     public static class Builder {
         private PMMLModelWorkflowConfiguration configuration = new PMMLModelWorkflowConfiguration();
-        private CreatePMMLModelConfiguration model = new CreatePMMLModelConfiguration();
+        private ModelStepConfiguration model = new ModelStepConfiguration();
+        private CreatePMMLModelConfiguration pmml = new CreatePMMLModelConfiguration();
 
         public Builder podId(String podId) {
+            pmml.setPodId(podId);
             model.setPodId(podId);
             return this;
         }
 
         public Builder microServiceHostPort(String microServiceHostPort) {
+            pmml.setMicroServiceHostPort(microServiceHostPort);
             model.setMicroServiceHostPort(microServiceHostPort);
             return this;
         }
 
         public Builder customer(CustomerSpace customerSpace) {
-            configuration.setContainerConfiguration("pmmlModelWorkflow", customerSpace, "pmmlModelWorkflow");
+            configuration.setCustomerSpace(customerSpace);
             model.setCustomerSpace(customerSpace);
-            return this;
-        }
-
-        public Builder workflow(String workflowName) {
-            configuration.setWorkflowName(workflowName);
-            configuration.setName(configuration.getClass().getSimpleName());
+            pmml.setCustomerSpace(customerSpace);
             return this;
         }
 
         public Builder modelingServiceHdfsBaseDir(String modelingServiceHdfsBaseDir) {
             model.setModelingServiceHdfsBaseDir(modelingServiceHdfsBaseDir);
+            pmml.setModelingServiceHdfsBaseDir(modelingServiceHdfsBaseDir);
             return this;
         }
 
         public Builder modelName(String modelName) {
             model.setModelName(modelName);
+            pmml.setModelName(modelName);
             return this;
         }
 
         public Builder displayName(String displayName) {
             model.setDisplayName(displayName);
+            pmml.setDisplayName(displayName);
             return this;
         }
 
         public Builder module(String moduleName) {
             model.setModuleName(moduleName);
+            pmml.setModuleName(moduleName);
             return this;
         }
 
         public Builder pmmlArtifactPath(String pmmlArtifactPath) {
-            model.setPmmlArtifactPath(pmmlArtifactPath);
+            pmml.setPmmlArtifactPath(pmmlArtifactPath);
             return this;
         }
 
         public Builder pivotArtifactPath(String pivotArtifactPath) {
             model.setPivotArtifactPath(pivotArtifactPath);
+            pmml.setPivotArtifactPath(pivotArtifactPath);
             return this;
         }
 
@@ -69,6 +73,7 @@ public class PMMLModelWorkflowConfiguration extends BaseModelingWorkflowConfigur
         }
 
         public Builder internalResourceHostPort(String internalResourceHostPort) {
+            pmml.setInternalResourceHostPort(internalResourceHostPort);
             model.setInternalResourceHostPort(internalResourceHostPort);
             configuration.setInternalResourceHostPort(internalResourceHostPort);
             return this;
@@ -76,15 +81,20 @@ public class PMMLModelWorkflowConfiguration extends BaseModelingWorkflowConfigur
 
         public Builder sourceSchemaInterpretation(String sourceSchemaInterpretation) {
             model.setSourceSchemaInterpretation(sourceSchemaInterpretation);
+            pmml.setSourceSchemaInterpretation(sourceSchemaInterpretation);
             return this;
         }
 
         public Builder moduleName(String moduleName) {
             model.setModuleName(moduleName);
+            pmml.setModuleName(moduleName);
             return this;
         }
 
         public PMMLModelWorkflowConfiguration build() {
+            configuration.setContainerConfiguration("pmmlModelWorkflow", configuration.getCustomerSpace(),
+                    configuration.getClass().getSimpleName());
+            configuration.add(pmml);
             configuration.add(model);
             return configuration;
         }
