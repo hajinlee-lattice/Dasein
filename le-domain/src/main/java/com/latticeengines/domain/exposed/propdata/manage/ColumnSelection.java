9266d8a03888242241c31626779681f50fe66aa0
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-03-14 00:11:22 +0000

    Review Request 13584 checkin a missed file

--- le-domain/src/main/java/com/latticeengines/domain/exposed/propdata/manage/ColumnSelection.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/propdata/manage/ColumnSelection.java
@@ -132,6 +132,7 @@ public class ColumnSelection {
         RTS("RTS"),
 
         // CDL
+        LookupId("LookupId"), //
         TalkingPoint("TalkingPoint"), //
         CompanyProfile("CompanyProfile");
 
