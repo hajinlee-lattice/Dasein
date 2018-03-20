1cd1b91b160002e73e180862a6de5018ef42517a
penglong.liu <penglong.liu@479f5059-62a3-4c39-9342-9852e3a68435>
2018-03-20 01:24:14 +0000

    Review Request 13742 PLS-7249 LPI - User Managment: "There is already a user in this tenant using the same email address." displayed when add a new user to the tenant

---
+++ le-auth/src/main/java/com/latticeengines/auth/aspect/GlobalAuthRetryAspect.java
@@ -0,0 +1,62 @@
+package com.latticeengines.auth.aspect;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.time.DurationFormatUtils;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Aspect
+public class GlobalAuthRetryAspect {
+
+    public static Logger log = LoggerFactory.getLogger(GlobalAuthRetryAspect.class);
+
+    @Around("execution(public * com.latticeengines.auth.exposed.entitymanager.impl.GlobalAuthAuthenticationEntityMgrImpl.find*(..)) "
+            + "|| execution(public * com.latticeengines.auth.exposed.entitymanager.impl.GlobalAuthUserEntityMgrImpl.find*(..))")
+    public Object globalAuthApiRetry(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        long startTime = System.currentTimeMillis();
+        Object retVal = null;
+        int retry = 2;
+        try {
+            while (retry > 0) {
+                try {
+                    retVal = joinPoint.proceed();
+                    if (retVal != null) {
+                        return retVal;
+                    }
+                } catch (Throwable ex) {
+                    log.warn(String.format("There's exception happening! retries=%s", retry), ex);
+                }
+                Thread.sleep(200);
+                retry--;
+            }
+        } catch (Exception e) {
+            log.warn(String.format("exception from retry"), e);
+        } finally {
+            long endTime = System.currentTimeMillis();
+            String queryParameters = getQueryParameters();
+            log.info(String.format("global auth method=%s threadName=%s ElapsedTime=%s queryParameters=%s",
+                    joinPoint.getSignature().toShortString(), Thread.currentThread().getName(),
+                    DurationFormatUtils.formatDuration(endTime - startTime, "HH:mm:ss:SS"), queryParameters));
+        }
+        return retVal;
+
+
+    }
+
+    private String getQueryParameters() {
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder
+                .getRequestAttributes();
+        if (requestAttributes != null) {
+            HttpServletRequest request = requestAttributes.getRequest();
+            return request.getQueryString();
+        }
+        return "";
+    }
+}
