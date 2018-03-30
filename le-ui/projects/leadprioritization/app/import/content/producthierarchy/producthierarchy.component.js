3460e7ba3bdb8fab7a2fb2af88df0f4c3bba2618
bholland <bholland@479f5059-62a3-4c39-9342-9852e3a68435>
2018-03-30 02:13:57 +0000

    Review Request 13982 see PLS-7529 - toggled product hierarchy to update instead of append

--- le-ui/projects/leadprioritization/app/import/content/producthierarchy/producthierarchy.component.js
+++ le-ui/projects/leadprioritization/app/import/content/producthierarchy/producthierarchy.component.js
@@ -65,7 +65,7 @@ angular.module('lp.import.wizard.producthierarchy', [])
                     mappedField: vm.mappedFieldMap[key],
                     originalUserField: (vm.saveMap[vm.mappedFieldMap[key]] ? vm.saveMap[vm.mappedFieldMap[key]].originalUserField : vm.keyMap[vm.mappedFieldMap[key]]),
                     originalMappedField: (vm.saveMap[vm.mappedFieldMap[key]] ? vm.saveMap[vm.mappedFieldMap[key]].originalMappedField : vm.mappedFieldMap[key]),
-                    append: true
+                    append: false
                 };
             mapped.push(map);
             if(userField) {
