be8d39dc76a2b1292203c9e022248bede2b6cb59
jlmehta <jlmehta@479f5059-62a3-4c39-9342-9852e3a68435>
2018-04-12 17:58:04 +0000

    PLS-7705: Remove Modelsummary as foreign key from AIModel. Only persist the model summary id in aimodel Review Request 14143

--- le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
+++ le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
@@ -1,11 +1,11 @@
 CREATE PROCEDURE `UpdateCDLTables`()
     BEGIN
 
-    ALTER TABLE `PLS_MultiTenant`.`AI_MODEL` add column `MODEL_SUMMARY` varchar(255);
+    ALTER TABLE `PLS_MultiTenant`.`AI_MODEL` add column `MODEL_SUMMARY_ID` varchar(255);
 
     UPDATE `PLS_MultiTenant`.`AI_MODEL` a
     INNER JOIN `PLS_MultiTenant`.`MODEL_SUMMARY` m ON m.`PID` = a.`FK_MODEL_SUMMARY_ID`
-    SET a.`MODEL_SUMMARY` = m.`ID`
+    SET a.`MODEL_SUMMARY_ID` = m.`ID`
     WHERE a.`FK_MODEL_SUMMARY_ID` IS NOT NULL;
 
     END;
