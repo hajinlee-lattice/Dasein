44b74785ab6e0fc5148c3cadaae01da60a154f44
dzheng <dzheng@479f5059-62a3-4c39-9342-9852e3a68435>
2018-04-16 18:43:52 +0000

    Review Request 14179 Add ReportConstants

---
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/serviceapps/cdl/ReportConstants.java
@@ -0,0 +1,7 @@
+package com.latticeengines.domain.exposed.serviceapps.cdl;
+
+public class ReportConstants {
+    public static final String NEW = "NEW";
+    public static final String UPDATE = "UPDATE";
+    public static final String DELETE = "DELETE";
+}
