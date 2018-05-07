7e8d19ebffd521dbc81b624790810ab10c8d7b1e
jhadden <jhadden@479f5059-62a3-4c39-9342-9852e3a68435>
2018-05-07 19:07:41 +0000

    Review Request 0000: Fix play creation wizard bug for Crm selection

--- le-ui/projects/leadprioritization/app/playbook/content/crmselection/crmselection.component.js
+++ le-ui/projects/leadprioritization/app/playbook/content/crmselection/crmselection.component.js
@@ -5,20 +5,34 @@ angular.module('lp.playbook.wizard.crmselection', [])
         orgs: '<'
     },
     controller: function(
-        $scope, $state, $timeout, 
-        ResourceUtility, BrowserStorageUtility, SfdcService, ModalStore
+        $scope, $state, $timeout, $stateParams,
+        ResourceUtility, BrowserStorageUtility, PlaybookWizardStore, SfdcService
     ) {
-        var vm = this;
+        var vm = this            
 
         vm.$onInit = function() {
-            console.log(vm.orgs);
+            vm.stored = PlaybookWizardStore.crmselection_form;
+            // console.log(vm.orgs);
+            PlaybookWizardStore.setValidation('crmselection', false);
+            if($stateParams.play_name) {
+                // PlaybookWizardStore.setValidation('settings', true);
+                PlaybookWizardStore.getPlay($stateParams.play_name).then(function(play){
+
+                    console.log(play);
+                    vm.savedSegment = play.crmselection;
+                    vm.stored.crm_selection = play.crmselection;
+                    if(play.crmselection) {
+                        PlaybookWizardStore.setValidation('crmselection', true);
+                    }
+                });
+            }
         }
 
         vm.checkValid = function(form) {
             PlaybookWizardStore.setValidation('crmselection', form.$valid);
-            if(vm.stored.segment_selection) {
+            if(vm.stored.crm_selection) {
                 PlaybookWizardStore.setSettings({
-                    segment: vm.stored.segment_selection
+                    crm_selection: vm.stored.crm_selection
                 });
             }
         }
