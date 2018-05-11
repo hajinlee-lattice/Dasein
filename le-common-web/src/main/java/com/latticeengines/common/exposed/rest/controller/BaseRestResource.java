5907e3d6759bbac381f510912c132fb72948a8fe
jadusumalli <jadusumalli@479f5059-62a3-4c39-9342-9852e3a68435>
2018-05-11 03:00:50 +0000

    Review Request 14508. Fix for getTable. dp-6175

---
+++ le-common-web/src/main/java/com/latticeengines/common/exposed/rest/controller/BaseRestResource.java
@@ -0,0 +1,64 @@
+package com.latticeengines.common.exposed.rest.controller;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+
+
+/**
+ * Base Resource that will inherit some common functionality like
+ * 1. Creating Pageable Requests
+ * 2. Add common resources ex: POST ../logging (Dynamically controls log level) 
+ * 3. Adding healthCheck
+ *
+ */
+public class BaseRestResource {
+
+    private static final Logger log = Logger.getLogger(BaseRestResource.class);
+    
+    /**
+     * @param pageIndex
+     * @return zero-based page index
+     */
+    protected int getZeroBasedPage(int pageIndex) {
+        pageIndex = (pageIndex < 1 ) ? 0 : (pageIndex - 1);
+        return pageIndex;
+    }
+    
+    protected Pageable getPageRequest(Integer pageIndex, Integer size) {
+        return getPageRequest(pageIndex, size, null, null);
+    }
+    
+    protected Pageable getPageRequest(Integer pageIndex, Integer size, String... sortColumns) {
+        return getPageRequest(pageIndex, size, null, sortColumns);
+    }
+    
+    protected Pageable getPageRequest(Integer pageIndex, Integer size, Direction sortDirection, String... sortColumns) {
+        if (pageIndex == null || size == null || pageIndex <= 0 || size <= 0) {
+            return null;
+        }
+        
+        if (sortColumns != null && sortColumns.length > 0) {
+            sortColumns = Arrays.stream(sortColumns).filter(col -> StringUtils.isNotBlank(col)).toArray(String[]::new);
+        }
+        
+        Sort sortCriteria = null;
+        if (sortColumns != null && sortColumns.length > 0) {
+            sortCriteria = sortDirection != null ? Sort.by(sortDirection, sortColumns) : Sort.by(sortColumns);
+        } else {
+            sortCriteria = Sort.unsorted();
+        }
+        PageRequest pageRequest = PageRequest.of(getZeroBasedPage(pageIndex), size, sortCriteria);
+        if (log.isDebugEnabled()) {
+            log.debug("PageRequest " + pageRequest);
+        }
+        return pageRequest;
+    }
+
+}
