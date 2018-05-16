45ff0a29a95e309c8ecf74bd3fa7f159e2bdec9a
ssakhalkar <ssakhalkar@479f5059-62a3-4c39-9342-9852e3a68435>
2018-05-16 23:43:19 +0000

    Review Request 14604

--- le-dev/scripts/setupdb_ldc_managedb.sql
+++ le-dev/scripts/setupdb_ldc_managedb.sql
@@ -195,7 +195,8 @@ VALUES
   ('2.0.8', '2017-11-17', '2.0', 'APPROVED', 'FULL', NOW(), '0'),
   ('2.0.9', '2018-01-28', '2.0', 'APPROVED', 'FULL', NOW(), '0'),
   ('2.0.10', '2018-03-02', '2.0', 'APPROVED', 'FULL', NOW(), '0'),
-  ('2.0.11', '2018-04-24', '2.0', 'APPROVED', 'FULL', NOW(), '0');
+  ('2.0.11', '2018-04-24', '2.0', 'APPROVED', 'FULL', NOW(), '0'),
+  ('2.0.12', '2018-05-15', '2.0', 'APPROVED', 'FULL', NOW(), '0');
 
 UPDATE `DataCloudVersion`
 SET
@@ -288,5 +289,12 @@ SET
   `DynamoTableSignature_Lookup` = '20180420',
   `EnrichmentStatsVersion`      = '2017-08-30_16-45-58_UTC'
 WHERE `Version` = '2.0.11';
+
+UPDATE `DataCloudVersion`
+SET
+  `AccountMasterHdfsVersion` = '2018-05-10_00-00-00_UTC',
+  `AccountLookupHdfsVersion` = '2018-05-10_00-00-00_UTC',
+  `EnrichmentStatsVersion`      = '2017-08-30_16-45-58_UTC'
+WHERE `Version` = '2.0.12';
 
 SET SQL_SAFE_UPDATES = 1;
