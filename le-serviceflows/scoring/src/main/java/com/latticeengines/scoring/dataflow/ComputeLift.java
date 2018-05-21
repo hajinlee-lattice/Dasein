450fcb89d4718bdfb8b748bdc013b889581eed46
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-05-21 21:59:57 +0000

    Review Request 14467 reduce compute lift DAG size

--- le-serviceflows/scoring/src/main/java/com/latticeengines/scoring/dataflow/ComputeLift.java
+++ le-serviceflows/scoring/src/main/java/com/latticeengines/scoring/dataflow/ComputeLift.java
@@ -1,19 +1,24 @@
 package com.latticeengines.scoring.dataflow;
 
+import java.io.Serializable;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.springframework.stereotype.Component;
 
 import com.latticeengines.dataflow.exposed.builder.Node;
 import com.latticeengines.dataflow.exposed.builder.TypesafeDataFlowBuilder;
 import com.latticeengines.dataflow.exposed.builder.common.FieldList;
+import com.latticeengines.dataflow.runtime.cascading.MappingFunction;
 import com.latticeengines.domain.exposed.dataflow.FieldMetadata;
 import com.latticeengines.domain.exposed.metadata.InterfaceName;
 import com.latticeengines.domain.exposed.serviceflows.scoring.dataflow.ComputeLiftParameters;
 
+import cascading.operation.Function;
 import cascading.operation.aggregator.Average;
 import cascading.operation.aggregator.Count;
 import cascading.tuple.Fields;
@@ -25,6 +30,7 @@ public class ComputeLift extends TypesafeDataFlowBuilder<ComputeLiftParameters> 
     private String liftField;
     private String ratingField;
 
+    private static final String SCORE_FIELD = "ScoreField";
     public static final String MODEL_AVG = "ModelAvg";
     public static final String RATING_COUNT = "RatingCount";
 
@@ -36,14 +42,19 @@ public class ComputeLift extends TypesafeDataFlowBuilder<ComputeLiftParameters> 
         liftField = parameters.getLiftField();
         ratingField = parameters.getRatingField();
         Map<String, String> scoreFieldMap = parameters.getScoreFieldMap();
-        Map<String, Node> nodes = splitNodes(inputTable, scoreFieldMap);
+
+        Map<Serializable, Serializable> valueMap = new HashMap<>();
+        scoreFieldMap.forEach(valueMap::put);
+        Function scoreFieldMapping = new MappingFunction(modelGuidField, SCORE_FIELD, valueMap);
+        inputTable = inputTable.apply(scoreFieldMapping, new FieldList(modelGuidField), new FieldMetadata(SCORE_FIELD, String.class));
+
+        Map<String, Node> nodes = splitNodes(inputTable, new HashSet<>(scoreFieldMap.values()));
 
         Node merged = null;
         for (Map.Entry<String, Node> entry : nodes.entrySet()) {
-            String modelGuid = entry.getKey();
+            String scoreField = entry.getKey();
             Node node = entry.getValue();
-            String scoreField = scoreFieldMap.get(modelGuid);
-            Node lift = computeLift(node, modelGuid, scoreField);
+            Node lift = computeLift(node, scoreField);
             if (merged == null) {
                 merged = lift;
             } else {
@@ -54,23 +65,25 @@ public class ComputeLift extends TypesafeDataFlowBuilder<ComputeLiftParameters> 
         return merged;
     }
 
-    private Map<String, Node> splitNodes(Node input, Map<String, String> scoreFieldMap) {
+    private Map<String, Node> splitNodes(Node input, Set<String> scoreFields) {
         Map<String, Node> nodes = new HashMap<>();
-        scoreFieldMap.forEach((modelGuid, scoreField) -> {
-            Node model = input.filter(String.format("\"%s\".equals(%s)", modelGuid, modelGuidField),
-                    new FieldList(modelGuidField));
-            model = model.renamePipe(modelGuid);
-            nodes.put(modelGuid, model);
+        scoreFields.forEach(scoreField -> {
+            Node model = input.filter(String.format("\"%s\".equals(%s)", scoreField, SCORE_FIELD),
+                    new FieldList(SCORE_FIELD));
+            model = model.renamePipe(scoreField);
+            nodes.put(scoreField, model);
         });
         return nodes;
     }
 
-    private Node computeLift(Node node, String modelGuid, String scoreField) {
+    private Node computeLift(Node node, String scoreField) {
         if (InterfaceName.Event.name().equals(scoreField)) {
             node = node.apply(String.format("Boolean.TRUE.equals(%s) ? 1.0 : 0.0", scoreField),
                     new FieldList(scoreField), new FieldMetadata("EventScore", Double.class));
             scoreField = "EventScore";
         }
+
+        // model average
         List<FieldMetadata> fms = Arrays.asList( //
                 new FieldMetadata(modelGuidField, String.class), //
                 new FieldMetadata(scoreField, Double.class), //
@@ -79,13 +92,16 @@ public class ComputeLift extends TypesafeDataFlowBuilder<ComputeLiftParameters> 
         Node avg = node.retain(scoreField, modelGuidField) //
                 .groupByAndAggregate(new FieldList(modelGuidField), new Average(new Fields(MODEL_AVG)), fms,
                         Fields.ALL);
-        avg = avg.retain(modelGuidField, MODEL_AVG).renamePipe(modelGuid + "_avg");
+        avg = avg.retain(modelGuidField, MODEL_AVG).renamePipe(scoreField + "_avg");
+
+        // rating
         Node rating = node.retain(scoreField, modelGuidField, ratingField);
         List<FieldMetadata> ratingCountFms = Arrays.asList( //
+                new FieldMetadata(modelGuidField, String.class), //
                 new FieldMetadata(ratingField, String.class), //
                 new FieldMetadata(RATING_COUNT, Long.class) //
         );
-        Node ratingCount = rating.groupByAndAggregate(new FieldList(ratingField),
+        Node ratingCount = rating.groupByAndAggregate(new FieldList(modelGuidField, ratingField),
                 new Count(new Fields(RATING_COUNT)), ratingCountFms, Fields.ALL);
         List<FieldMetadata> ratingAvgFms = Arrays.asList( //
                 new FieldMetadata(modelGuidField, String.class), //
@@ -95,8 +111,10 @@ public class ComputeLift extends TypesafeDataFlowBuilder<ComputeLiftParameters> 
         );
         Node ratingAvg = rating.groupByAndAggregate(new FieldList(modelGuidField, ratingField),
                 new Average(new Fields("RatingAvg")), ratingAvgFms, Fields.ALL);
-        ratingAvg = ratingAvg.retain(modelGuidField, ratingField, "RatingAvg").renamePipe(modelGuid + "_rating_avg");
-        Node output = ratingAvg.innerJoin(ratingField, ratingCount, ratingField).innerJoin(modelGuidField, avg, modelGuidField);
+        ratingAvg = ratingAvg.retain(modelGuidField, ratingField, "RatingAvg").renamePipe(scoreField + "_rating_avg");
+
+        FieldList joinFields = new FieldList(modelGuidField, ratingField);
+        Node output = ratingAvg.innerJoin(joinFields, ratingCount, joinFields).innerJoin(modelGuidField, avg, modelGuidField);
         output = output.apply(String.format("%s > 0 ? RatingAvg / %s : 0.0", MODEL_AVG, MODEL_AVG),
                 new FieldList("RatingAvg", MODEL_AVG), new FieldMetadata(liftField, Double.class));
         return output.retain(modelGuidField, ratingField, liftField, RATING_COUNT);
