1c131009410183552aa0a2486c6917ce79240136
jhe <jhe@479f5059-62a3-4c39-9342-9852e3a68435>
2018-05-28 09:39:36 +0000

    Review Request 14756: Try fix FileUploadServiceImplDeploymentTestNG and remove unused imports.

--- le-pls/src/test/java/com/latticeengines/pls/service/impl/FileUploadServiceImplDeploymentTestNG.java
+++ le-pls/src/test/java/com/latticeengines/pls/service/impl/FileUploadServiceImplDeploymentTestNG.java
@@ -10,22 +10,19 @@ import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.latticeengines.camille.exposed.CamilleEnvironment;
 import com.latticeengines.camille.exposed.paths.PathBuilder;
 import com.latticeengines.common.exposed.util.HdfsUtils;
+import com.latticeengines.db.exposed.entitymgr.TenantEntityMgr;
 import com.latticeengines.db.exposed.util.MultiTenantContext;
 import com.latticeengines.domain.exposed.camille.CustomerSpace;
 import com.latticeengines.domain.exposed.pls.SchemaInterpretation;
 import com.latticeengines.domain.exposed.pls.SourceFile;
-import com.latticeengines.domain.exposed.security.Tenant;
 import com.latticeengines.pls.functionalframework.PlsDeploymentTestNGBase;
-import com.latticeengines.pls.functionalframework.PlsFunctionalTestNGBaseDeprecated;
 import com.latticeengines.pls.service.FileUploadService;
-import com.latticeengines.db.exposed.entitymgr.TenantEntityMgr;
 import com.latticeengines.security.exposed.service.TenantService;
 
 public class FileUploadServiceImplDeploymentTestNG extends PlsDeploymentTestNGBase {
