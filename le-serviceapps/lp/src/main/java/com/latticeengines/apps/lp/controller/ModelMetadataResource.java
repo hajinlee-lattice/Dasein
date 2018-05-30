a49a9ecea0bf9592660637a93573ad1348bfcd0c
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-05-30 21:35:17 +0000

    Review Request 14729 fix model metadata resource

--- le-serviceapps/lp/src/main/java/com/latticeengines/apps/lp/controller/ModelMetadataResource.java
+++ le-serviceapps/lp/src/main/java/com/latticeengines/apps/lp/controller/ModelMetadataResource.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -72,7 +73,7 @@ public class ModelMetadataResource {
         return modelMetadataService.getLatticeAttributeNames(modelId);
     }
 
-    @GetMapping("/attributes-from-fields")
+    @PostMapping("/attributes-from-fields")
     @ResponseBody
     @ApiOperation(value = "Get customized attributes from fields")
     public List<Attribute> getAttributesFromFields(@PathVariable String customerSpace,
