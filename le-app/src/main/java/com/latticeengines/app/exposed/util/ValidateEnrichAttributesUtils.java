16189e7be9a0d354e8b22e6a53bbf1e330da3e11
penglong.liu <penglong.liu@479f5059-62a3-4c39-9342-9852e3a68435>
2018-05-30 02:09:26 +0000

    Review Request 14776 PLS-8670 PagerDuty #84715 PremiumEnrichAttributes: 12000 is out of the range of 0 and 1000

--- le-app/src/main/java/com/latticeengines/app/exposed/util/ValidateEnrichAttributesUtils.java
+++ le-app/src/main/java/com/latticeengines/app/exposed/util/ValidateEnrichAttributesUtils.java
@@ -3,16 +3,14 @@ package com.latticeengines.app.exposed.util;
 public class ValidateEnrichAttributesUtils {
 
     public static final int MIN_PREMIUM_ENRICHMENT_ATTRIBUTES = 0;
-    public static final int DEFAULT_PREMIUM_ENRICHMENT_ATTRIBUTES = 20;
-    public static final int MAX_PREMIUM_ENRICHMENT_ATTRIBUTES = 1000;
+    public static final int DEFAULT_PREMIUM_ENRICHMENT_ATTRIBUTES = 32;
 
     public static int validateEnrichAttributes(String maxPremiumEnrichAttributesStr) {
         maxPremiumEnrichAttributesStr = maxPremiumEnrichAttributesStr.replaceAll("\"", "");
         int premiumEnrichAttributes = Integer.parseInt(maxPremiumEnrichAttributesStr);
-        if (premiumEnrichAttributes < MIN_PREMIUM_ENRICHMENT_ATTRIBUTES
-                || premiumEnrichAttributes > MAX_PREMIUM_ENRICHMENT_ATTRIBUTES) {
-            throw new RuntimeException(String.format("PremiumEnrichAttributes: %d is out of the range of %d and %d.",
-                    premiumEnrichAttributes, MIN_PREMIUM_ENRICHMENT_ATTRIBUTES, MAX_PREMIUM_ENRICHMENT_ATTRIBUTES));
+        if (premiumEnrichAttributes < MIN_PREMIUM_ENRICHMENT_ATTRIBUTES) {
+            throw new RuntimeException(String.format("PremiumEnrichAttributes: %d is less than %d.",
+                    premiumEnrichAttributes, MIN_PREMIUM_ENRICHMENT_ATTRIBUTES));
         }
         return premiumEnrichAttributes;
     }
