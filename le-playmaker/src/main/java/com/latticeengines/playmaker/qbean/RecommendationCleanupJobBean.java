5b31635599b68898c3bf53ac4857eeb8d0e7a067
akumar <akumar@479f5059-62a3-4c39-9342-9852e3a68435>
2018-06-01 06:17:24 +0000

    Review request 14804 - PLS-5409, PLS-8658, PLS-8704 - added soft delete feature for play, play launch and recommendation domain objects. Also implemented efficient and lightweight recommendation cleanup methods...

---
+++ le-playmaker/src/main/java/com/latticeengines/playmaker/qbean/RecommendationCleanupJobBean.java
@@ -0,0 +1,31 @@
+package com.latticeengines.playmaker.qbean;
+
+import java.util.concurrent.Callable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.latticeengines.playmaker.service.RecommendationCleanupService;
+import com.latticeengines.playmaker.service.impl.RecommendationCleanupCallable;
+import com.latticeengines.quartzclient.qbean.QuartzJobBean;
+
+@Component("recommendationCleanupJob")
+public class RecommendationCleanupJobBean implements QuartzJobBean {
+
+    private static final Logger log = LoggerFactory.getLogger(RecommendationCleanupJobBean.class);
+
+    @Autowired
+    private RecommendationCleanupService recommendationCleanupService;
+
+    @Override
+    public Callable<Boolean> getCallable(String jobArguments) {
+        log.info(String.format("Got callback with job arguments = %s", jobArguments));
+
+        RecommendationCleanupCallable.Builder builder = new RecommendationCleanupCallable.Builder();
+        builder.recommendationCleanupService(recommendationCleanupService);
+        return new RecommendationCleanupCallable(builder);
+    }
+
+}
