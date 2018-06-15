ac01d9c2e8def72bf774cbf8ae7ea7a4fb97a8e5
dzheng <dzheng@479f5059-62a3-4c39-9342-9852e3a68435>
2018-06-15 17:33:10 +0000

    Review Request 15040 Fix UpdateContactDeploymentTestNG

--- le-serviceapps/cdl/src/test/java/com/latticeengines/apps/cdl/end2end/UpdateContactDeploymentTestNG.java
+++ le-serviceapps/cdl/src/test/java/com/latticeengines/apps/cdl/end2end/UpdateContactDeploymentTestNG.java
@@ -17,7 +17,7 @@ public class UpdateContactDeploymentTestNG extends CDLEnd2EndDeploymentTestNGBas
     @Test(groups = "end2end")
     public void runTest() throws Exception {
         resumeCheckpoint(UpdateAccountDeploymentTestNG.CHECK_POINT);
-        Assert.assertEquals(countInRedshift(BusinessEntity.Contact), 500);
+        Assert.assertEquals(countInRedshift(BusinessEntity.Contact), 1100);
 
         new Thread(this::createTestSegments).start();
 
