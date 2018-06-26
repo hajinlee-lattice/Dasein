3b5220042d2619ca9420c3e61c4b38393c3c82b6
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-06-26 19:18:29 +0000

    Review Request 15227 PLS-6541 fix displayname for external ids

--- le-serviceapps/cdl/src/main/java/com/latticeengines/apps/cdl/mds/impl/ExternalSystemMetadataStoreImpl.java
+++ le-serviceapps/cdl/src/main/java/com/latticeengines/apps/cdl/mds/impl/ExternalSystemMetadataStoreImpl.java
@@ -38,31 +38,35 @@ public class ExternalSystemMetadataStoreImpl implements ExternalSystemMetadataSt
         if (BusinessEntity.Account.equals(namespace.getCoord2())) {
             cdlNamespaceService.setMultiTenantContext(namespace.getCoord1());
             CDLExternalSystem externalSystem = cdlExternalSystemEntityMgr.findExternalSystem();
+
             if (externalSystem != null) {
                 if (CollectionUtils.isNotEmpty(externalSystem.getCRMIdList())) {
-                    flux = flux.concatWith(Flux.fromIterable(externalSystem.getCRMIdList()).map(id -> toColumnMetadata(id, "CRM")));
+                    flux = flux.concatWith(Flux.fromIterable(externalSystem.getCRMIdList()).map(id -> toColumnMetadata(id, "CRM", externalSystem.getDisplayNameById(id))));
                 }
                 if (CollectionUtils.isNotEmpty(externalSystem.getMAPIdList())) {
-                    flux = flux.concatWith(Flux.fromIterable(externalSystem.getMAPIdList()).map(id -> toColumnMetadata(id, "MAP")));
+                    flux = flux.concatWith(Flux.fromIterable(externalSystem.getMAPIdList()).map(id -> toColumnMetadata(id, "MAP", externalSystem.getDisplayNameById(id))));
                 }
                 if (CollectionUtils.isNotEmpty(externalSystem.getERPIdList())) {
-                    flux = flux.concatWith(Flux.fromIterable(externalSystem.getERPIdList()).map(id -> toColumnMetadata(id, "ERP")));
+                    flux = flux.concatWith(Flux.fromIterable(externalSystem.getERPIdList()).map(id -> toColumnMetadata(id, "ERP", externalSystem.getDisplayNameById(id))));
                 }
                 if (CollectionUtils.isNotEmpty(externalSystem.getOtherIdList())) {
-                    flux = flux.concatWith(Flux.fromIterable(externalSystem.getOtherIdList()).map(id -> toColumnMetadata(id, null)));
+                    flux = flux.concatWith(Flux.fromIterable(externalSystem.getOtherIdList()).map(id -> toColumnMetadata(id, null, externalSystem.getDisplayNameById(id))));
                 }
             }
         }
         return flux;
     }
 
-    private ColumnMetadata toColumnMetadata(String attrName, String type) {
+    private ColumnMetadata toColumnMetadata(String attrName, String type, String displayName) {
         ColumnMetadata cm = new ColumnMetadata();
         cm.setAttrName(attrName);
         cm.enableGroup(ColumnSelection.Predefined.LookupId);
         cm.setSubcategory("Account IDs");
         if (StringUtils.isNotBlank(type)) {
             cm.setSecondaryDisplayName(type);
+        }
+        if (StringUtils.isNotBlank(displayName)) {
+            cm.setDisplayName(displayName);
         }
         return cm;
     }
