58bc050202ca837708fc12cb68bf19be02efd0e8
arahim <arahim@479f5059-62a3-4c39-9342-9852e3a68435>
2018-07-12 18:44:02 +0000

    Review Request 15421: PLS-9363 Use ratings object to populate user's selections on attributes step of CE wizard

--- le-ui/projects/leadprioritization/app/ratingsengine/content/attributes/attributes.component.js
+++ le-ui/projects/leadprioritization/app/ratingsengine/content/attributes/attributes.component.js
@@ -1,5 +1,5 @@
 angular.module('lp.ratingsengine.wizard.attributes', [])
-.controller('RatingsEngineAttributes', function ($q, $state, $stateParams, RatingsEngineStore) {
+.controller('RatingsEngineAttributes', function ($q, $state, $stateParams, RatingsEngineStore, Rating) {
 
     var vm = this;
     angular.extend(vm, {
@@ -13,12 +13,23 @@ angular.module('lp.ratingsengine.wizard.attributes', [])
     });
 
     vm.init = function() {
-        if (RatingsEngineStore.getCustomEventModelingType() == 'LPI') {
+        if (RatingsEngineStore.getCustomEventModelingType() == 'LPI' || Rating.segment == null) {
             vm.disableCDLAttributes = true;
             vm.scoringAttributes['CDL'] = false;
-        } else if ( RatingsEngineStore.getCustomEventModelingType() == 'CDL') {
+            vm.scoringAttributes['CustomFileAttributes'] = Rating.activeModel.AI.advancedModelingConfig.custom_event.dataStores ? 
+                        Rating.activeModel.AI.advancedModelingConfig.custom_event.dataStores.indexOf('CustomFileAttributes') >= 0 : true;
+        } else if ( RatingsEngineStore.getCustomEventModelingType() == 'CDL' || Rating.segment != null) {
             vm.disableTrainingAttributes = true;
+            vm.scoringAttributes['CDL'] = Rating.activeModel.AI.advancedModelingConfig.custom_event.dataStores ? 
+                        Rating.activeModel.AI.advancedModelingConfig.custom_event.dataStores.indexOf('CDL') >= 0 : true;
             vm.scoringAttributes['CustomFileAttributes'] = false;
+        }
+
+        if (Rating.activeModel.AI.advancedModelingConfig.custom_event.dataStores) {
+            console.log(Rating.activeModel.AI.advancedModelingConfig.custom_event.dataStores);
+            Object.keys(vm.scoringAttributes).forEach(function(attr) {
+                vm.scoringAttributes[attr] = Rating.activeModel.AI.advancedModelingConfig.custom_event.dataStores.indexOf(attr) >= 0;
+            });  
         }
         var dataStores = filterDataStores(vm.scoringAttributes);
         RatingsEngineStore.setDataStores(dataStores);
