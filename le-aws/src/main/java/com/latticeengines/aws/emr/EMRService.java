c21f3f281fcfbf425393cd5c7fa3bde160d227aa
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-07-24 08:27:11 +0000

    Review Request 15508

---
+++ le-aws/src/main/java/com/latticeengines/aws/emr/EMRService.java
@@ -0,0 +1,7 @@
+package com.latticeengines.aws.emr;
+
+public interface EMRService {
+
+    String getMasterIp(String clusterName);
+
+}
---
+++ le-aws/src/main/java/com/latticeengines/aws/emr/impl/EMRServiceImpl.java
@@ -0,0 +1,57 @@
+package com.latticeengines.aws.emr.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduceClient;
+import com.amazonaws.services.elasticmapreduce.model.ClusterState;
+import com.amazonaws.services.elasticmapreduce.model.ClusterSummary;
+import com.amazonaws.services.elasticmapreduce.model.DescribeClusterRequest;
+import com.amazonaws.services.elasticmapreduce.model.DescribeClusterResult;
+import com.amazonaws.services.elasticmapreduce.model.Instance;
+import com.amazonaws.services.elasticmapreduce.model.ListClustersRequest;
+import com.amazonaws.services.elasticmapreduce.model.ListClustersResult;
+import com.amazonaws.services.elasticmapreduce.model.ListInstancesRequest;
+import com.amazonaws.services.elasticmapreduce.model.ListInstancesResult;
+import com.latticeengines.aws.emr.EMRService;
+
+@Service("emrService")
+public class EMRServiceImpl implements EMRService {
+
+    private static final Logger log = LoggerFactory.getLogger(EMRServiceImpl.class);
+
+    @Autowired
+    private AmazonElasticMapReduceClient emrClient;
+
+    @Override
+    public String getMasterIp(String clusterName) {
+        ListClustersRequest request = new ListClustersRequest().withClusterStates(ClusterState.RUNNING,
+                ClusterState.WAITING);
+        ListClustersResult clustersResult = emrClient.listClusters(request);
+        String masterIp = null;
+        for (ClusterSummary summary : clustersResult.getClusters()) {
+            if (summary.getName().endsWith(clusterName)) {
+                log.info("Found an EMR cluster named " + summary.getName());
+                String clusterId = summary.getId();
+                DescribeClusterResult cluster = emrClient
+                        .describeCluster(new DescribeClusterRequest().withClusterId(clusterId));
+                String masterDNS = cluster.getCluster().getMasterPublicDnsName();
+                ListInstancesResult instances = emrClient
+                        .listInstances(new ListInstancesRequest().withClusterId(clusterId));
+                for (Instance instance : instances.getInstances()) {
+                    String instancePublicDNS = instance.getPublicDnsName();
+                    String instancePrivateDNS = instance.getPrivateDnsName();
+                    if (masterDNS.equals(instancePublicDNS) || masterDNS.equals(instancePrivateDNS)) {
+                        masterIp = instance.getPrivateIpAddress();
+                        log.info("The private IP of master node in the cluster named " + clusterName + " is "
+                                + masterIp);
+                    }
+                }
+            }
+        }
+        return masterIp;
+    }
+
+}
