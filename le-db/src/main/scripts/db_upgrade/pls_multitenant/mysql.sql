855326217db44730643f2bfcd4e7abc02388c750
jadusumalli <jadusumalli@479f5059-62a3-4c39-9342-9852e3a68435>
2018-07-27 18:11:50 +0000

    Review Request 15523. Fix for PLS-9390

--- le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
+++ le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
@@ -10,14 +10,14 @@ DELIMITER ;
 
 CREATE PROCEDURE `MarketoIntegrationSchema`()
   BEGIN
-    create table `SCORING_REQUEST_CONFIG` (`PID` bigint not null auto_increment, `REQ_CONFIG_ID` varchar(255) not null, `CREATED` datetime not null, `MODEL_UUID` varchar(255) not null, `UPDATED` datetime not null, `MARKETO_CREDENTIAL_ID` bigint not null, `FK_TENANT_ID` bigint not null, primary key (`PID`)) engine=InnoDB;
-    alter table `SCORING_REQUEST_CONFIG` add constraint SCORING_REQUEST_CONFIG.REQ_CONFIG_ID unique (`REQ_CONFIG_ID`);
-    alter table `SCORING_REQUEST_CONFIG` add constraint `FK_SCORINGREQUESTCONFIG_MARKETOCREDENTIALID_MARKETOCREDENTIAL` foreign key (`MARKETO_CREDENTIAL_ID`) references `MARKETO_CREDENTIAL` (`PID`) on delete cascade;
-    alter table `SCORING_REQUEST_CONFIG` add constraint `FK_SCORINGREQUESTCONFIG_FKTENANTID_TENANT` foreign key (`FK_TENANT_ID`) references `TENANT` (`TENANT_PID`) on delete cascade;
+    create table `PLS_MultiTenant`.`SCORING_REQUEST_CONFIG` (`PID` bigint not null auto_increment, `REQ_CONFIG_ID` varchar(255) not null, `CREATED` datetime not null, `MODEL_UUID` varchar(255) not null, `UPDATED` datetime not null, `MARKETO_CREDENTIAL_ID` bigint not null, `FK_TENANT_ID` bigint not null, primary key (`PID`)) engine=InnoDB;
+    alter table `PLS_MultiTenant`.`SCORING_REQUEST_CONFIG` add constraint SCORING_REQUEST_CONFIG.REQ_CONFIG_ID unique (`REQ_CONFIG_ID`);
+    alter table `PLS_MultiTenant`.`SCORING_REQUEST_CONFIG` add constraint `FK_SCORINGREQUESTCONFIG_MARKETOCREDENTIALID_MARKETOCREDENTIAL` foreign key (`MARKETO_CREDENTIAL_ID`) references `PLS_MultiTenant`.`MARKETO_CREDENTIAL` (`PID`) on delete cascade;
+    alter table `PLS_MultiTenant`.`SCORING_REQUEST_CONFIG` add constraint `FK_SCORINGREQUESTCONFIG_FKTENANTID_TENANT` foreign key (`FK_TENANT_ID`) references `PLS_MultiTenant`.`TENANT` (`TENANT_PID`) on delete cascade;
 
-    create table `MARKETO_SCORING_MATCH_FIELD` (`PID` bigint not null auto_increment, `MARKETO_FIELD_NAME` varchar(255), `MODEL_FIELD_NAME` varchar(255), `SCORING_REQUEST_ID` bigint not null, `FK_TENANT_ID` bigint not null, primary key (`PID`)) engine=InnoDB;
-    alter table `MARKETO_SCORING_MATCH_FIELD` add constraint `FK_MARKETOSCORINGMATCHFIELD_SCORINGREQUESTID_SCORINGREQUESTCONFI` foreign key (`SCORING_REQUEST_ID`) references `SCORING_REQUEST_CONFIG` (`PID`) on delete cascade;
-    alter table `MARKETO_SCORING_MATCH_FIELD` add constraint `FK_MARKETOSCORINGMATCHFIELD_FKTENANTID_TENANT` foreign key (`FK_TENANT_ID`) references `TENANT` (`TENANT_PID`) on delete cascade;
+    create table `PLS_MultiTenant`.`MARKETO_SCORING_MATCH_FIELD` (`PID` bigint not null auto_increment, `MARKETO_FIELD_NAME` varchar(255), `MODEL_FIELD_NAME` varchar(255), `SCORING_REQUEST_ID` bigint not null, `FK_TENANT_ID` bigint not null, primary key (`PID`)) engine=InnoDB;
+    alter table `PLS_MultiTenant`.`MARKETO_SCORING_MATCH_FIELD` add constraint `FK_MARKETOSCORINGMATCHFIELD_SCORINGREQUESTID_SCORINGREQUESTCONFI` foreign key (`SCORING_REQUEST_ID`) references `PLS_MultiTenant`.`SCORING_REQUEST_CONFIG` (`PID`) on delete cascade;
+    alter table `PLS_MultiTenant`.`MARKETO_SCORING_MATCH_FIELD` add constraint `FK_MARKETOSCORINGMATCHFIELD_FKTENANTID_TENANT` foreign key (`FK_TENANT_ID`) references `PLS_MultiTenant`.`TENANT` (`TENANT_PID`) on delete cascade;
 
   END;
 //
