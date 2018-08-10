a99f0555b7653264e772741f187766ef89e475ad
lgigli <lgigli@479f5059-62a3-4c39-9342-9852e3a68435>
2018-08-10 22:19:39 +0000

    Review Request 0000 SCSS files loaded in the dev tools

--- le-ui/projects/leadprioritization/webpack.dev.js
+++ le-ui/projects/leadprioritization/webpack.dev.js
@@ -2,32 +2,40 @@ const webpack = require("webpack");
 const merge = require("webpack-merge");
 const path = require("path");
 const common = require("./webpack.common.js");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
   mode: "development",
   devtool: "module-source-map",
   output: {
     devtoolModuleFilenameTemplate(info) {
-      return `file:///${info.absoluteResourcePath.replace(/\\/g, '/')}`;
+      return `file:///${info.absoluteResourcePath.replace(/\\/g, "/")}`;
     }
   },
+  stats: {
+    colors: true
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.s?css/,
         exclude: /node_modules/,
         use: [
-          { loader: "style-loader" },
+          MiniCssExtractPlugin.loader,
           {
             loader: "css-loader",
-            options: {
-              sourceMap: false
-            }
+            options: { sourceMap: true, importLoaders: 1 }
           },
           {
             loader: "sass-loader",
             options: {
-              sourceMap: true
+              sourceMap: true,
+              importLoaders: 1
             }
           }
         ]
