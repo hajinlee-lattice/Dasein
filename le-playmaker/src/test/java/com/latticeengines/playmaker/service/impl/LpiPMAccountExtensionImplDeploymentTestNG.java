a36155ef7eaf8cac8f4a428de0460b80947172a9
perry.liu <perry.liu@479f5059-62a3-4c39-9342-9852e3a68435>
2018-08-10 17:24:14 +0000

    Review Request 15512:removed some comments

--- le-playmaker/src/test/java/com/latticeengines/playmaker/service/impl/LpiPMAccountExtensionImplDeploymentTestNG.java
+++ le-playmaker/src/test/java/com/latticeengines/playmaker/service/impl/LpiPMAccountExtensionImplDeploymentTestNG.java
@@ -28,10 +28,8 @@ import com.latticeengines.testframework.service.impl.TestPlayCreationHelper;
 
 @Listeners({ GlobalAuthCleanupTestListener.class })
 @TestExecutionListeners({ DirtiesContextTestExecutionListener.class })
-@ContextConfiguration(locations = { //"classpath:test-pls-context.xml", 
-		"classpath:test-testframework-cleanup-context.xml", 
-		"classpath:playmakercore-context.xml",
-        "classpath:test-playmaker-context.xml" })
+@ContextConfiguration(locations = { "classpath:test-testframework-cleanup-context.xml",
+        "classpath:playmakercore-context.xml", "classpath:test-playmaker-context.xml" })
 public class LpiPMAccountExtensionImplDeploymentTestNG extends AbstractTestNGSpringContextTests {
 
     private static final Logger log = LoggerFactory.getLogger(LpiPMAccountExtensionImplDeploymentTestNG.class);
