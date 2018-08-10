8710346b360e9b9814400ba59dfb66ef2de3e14a
xu.guo <xu.guo@479f5059-62a3-4c39-9342-9852e3a68435>
2018-08-10 08:21:25 +0000

    review request 15710; pd-2225: load test bw collector.

--- le-datacloud/collection/src/test/java/com/latticeengines/datacloud/collection/service/CollectionDBServiceTestNG.java
+++ le-datacloud/collection/src/test/java/com/latticeengines/datacloud/collection/service/CollectionDBServiceTestNG.java
@@ -1,12 +1,19 @@
 package com.latticeengines.datacloud.collection.service;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import javax.inject.Inject;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
@@ -22,9 +29,65 @@ public class CollectionDBServiceTestNG extends AbstractTestNGSpringContextTests 
     @Value("${datacloud.collection.test.domains}")
     String testDomains;
 
+    List<String> loadDomains(String path) throws Exception
+    {
+        CSVFormat format = CSVFormat.RFC4180.withHeader().withDelimiter(',')
+                .withIgnoreEmptyLines(true).withIgnoreSurroundingSpaces(true);
+
+        List<String> ret = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(path)))) {
+            try (CSVParser parser = new CSVParser(reader, format)) {
+                Map<String, Integer> colMap = parser.getHeaderMap();
+                int domainIdx = colMap.getOrDefault("Domain", -1);
+
+                Iterator<CSVRecord> ite = parser.iterator();
+                while (ite.hasNext()) {
+                    CSVRecord rec = ite.next();
+                    ret.add(rec.get(domainIdx));
+                }
+            }
+        }
+
+        return ret;
+    }
+
     @Test(groups = "functional")
+    public void testLoad() throws Exception {
+        List<String> path_set = new ArrayList<>();
+        path_set.add("../../domains/50kdomains.0.csv");
+        path_set.add("../../domains/50kdomains.1.csv");
+        path_set.add("../../domains/50kdomains.2.csv");
+        path_set.add("../../domains/50kdomains.3.csv");
+
+        List<List<String>> domains_set = new ArrayList<>();
+        for (int i = 0; i < path_set.size(); ++i)
+        {
+            domains_set.add(loadDomains(path_set.get(i)));
+        }
+
+        long lastTriggered = 0;
+        int domains_idx = 0;
+        while (true)
+        {
+            long curMillis = System.currentTimeMillis();
+            if (lastTriggered == 0 || curMillis - lastTriggered > 10 * 60 * 1000)
+            {
+                lastTriggered = curMillis;
+                collectionDBService.addNewDomains(domains_set.get(domains_idx), "builtwith", UUID.randomUUID()
+                        .toString().toUpperCase());
+
+                domains_idx = (domains_idx + 1) % domains_set.size();
+            }
+
+            collectionDBService.service();
+            Thread.sleep(15 * 1000);
+        }
+    }
+
+    //@Test(groups = "functional")
     public void testCollectionDBService() throws Exception {
-        List<String> domains = new ArrayList<String>(Arrays.asList((testDomains).split(",")));
+        /*
+        List<String> domains = new ArrayList<String>(Arrays.asList(("google.com").split(",")));
         collectionDBService.addNewDomains(domains, "builtwith", UUID.randomUUID().toString().toUpperCase());
 
 
@@ -33,6 +96,6 @@ public class CollectionDBServiceTestNG extends AbstractTestNGSpringContextTests 
             collectionDBService.service();
 
             Thread.sleep(15000);
-        }
+        }*/
     }
 }
