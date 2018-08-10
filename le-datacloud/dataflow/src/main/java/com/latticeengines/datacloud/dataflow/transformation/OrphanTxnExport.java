49e4e3fe23df2908b36dc9e428a2c671a56f53f6
xhu <xhu@479f5059-62a3-4c39-9342-9852e3a68435>
2018-08-10 23:24:09 +0000

    review request 15701 : DP-6988

---
+++ le-datacloud/dataflow/src/main/java/com/latticeengines/datacloud/dataflow/transformation/OrphanTxnExport.java
@@ -0,0 +1,66 @@
+package com.latticeengines.datacloud.dataflow.transformation;
+
+import com.latticeengines.dataflow.exposed.builder.Node;
+import com.latticeengines.dataflow.exposed.builder.common.FieldList;
+import com.latticeengines.dataflow.exposed.builder.common.JoinType;
+import com.latticeengines.dataflow.exposed.builder.util.DataFlowUtils;
+import com.latticeengines.domain.exposed.datacloud.dataflow.TransformationFlowParameters;
+import com.latticeengines.domain.exposed.datacloud.transformation.configuration.impl.TransformerConfig;
+import com.latticeengines.domain.exposed.metadata.InterfaceName;
+import org.apache.hadoop.tracing.TraceAdminPB;
+import org.springframework.stereotype.Component;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component(OrphanTxnExport.DATAFLOW_BEAN_NAME)
+public class OrphanTxnExport extends ConfigurableFlowBase<TransformerConfig>{
+    public static final String DATAFLOW_BEAN_NAME = "OrphanTxnExport";
+    public static final String TRANSFORMER_NAME = "OrphanTxnExportTransformer";
+
+    @Override
+    public Node construct(TransformationFlowParameters parameters) {
+        Node srcAccount = addSource(parameters.getBaseTables().get(0));
+        Node srcProduct = addSource(parameters.getBaseTables().get(1));
+        Node srcTxn = addSource(parameters.getBaseTables().get(2));
+        List<String> retainFields = srcTxn.getFieldNames();
+
+        String renamedAccount = createNewName(srcAccount,InterfaceName.AccountId.name());
+        String renamedProduct = createNewName(srcProduct,InterfaceName.ProductId.name());
+
+        srcAccount = renameFields(srcAccount, InterfaceName.AccountId.name(),renamedAccount);
+        srcProduct = renameFields(srcProduct, InterfaceName.ProductId.name(),renamedProduct);
+        srcTxn = srcTxn.join(new FieldList(InterfaceName.AccountId.name()),srcAccount, new FieldList(renamedAccount), JoinType.LEFT);
+        srcTxn = srcTxn.join(new FieldList(InterfaceName.ProductId.name()),srcProduct, new FieldList(renamedProduct), JoinType.LEFT);
+
+        List<String> filterFields = Arrays.asList(renamedAccount,renamedProduct);
+        srcTxn = srcTxn.filter(String.format("%s == null || %s == null",renamedAccount,renamedProduct), new FieldList(filterFields));
+        srcTxn = srcTxn.retain(new FieldList(retainFields));
+        return srcTxn;
+    }
+    public String createNewName(Node node, String previousName){
+        return previousName + "_" + node.getPipeName();
+    }
+
+    public Node renameFields(Node node, String previousName, String newName){
+        List<String> previousNames = node.getFieldNames();
+        List<String> newNames = new ArrayList<>(previousNames);
+        newNames.set(previousNames.indexOf(previousName),newName);
+        return node.rename(new FieldList(previousNames),new FieldList(newNames));
+    }
+
+    @Override
+    public String getDataFlowBeanName() {
+        return DATAFLOW_BEAN_NAME;
+    }
+
+    @Override
+    public String getTransformerName() {
+        return TRANSFORMER_NAME;
+    }
+
+    @Override
+    public Class<? extends TransformerConfig> getTransformerConfigClass() {
+        return TransformerConfig.class;
+    }
+}
---
+++ le-datacloud/etl/src/test/java/com/latticeengines/datacloud/etl/transformation/service/impl/OrphanTxnExportTestNG.java
@@ -0,0 +1,164 @@
+package com.latticeengines.datacloud.etl.transformation.service.impl;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.latticeengines.common.exposed.util.JsonUtils;
+import com.latticeengines.datacloud.core.source.Source;
+import com.latticeengines.datacloud.core.source.impl.GeneralSource;
+import com.latticeengines.datacloud.dataflow.transformation.OrphanTxnExport;
+import com.latticeengines.datacloud.etl.transformation.service.TransformationService;
+import com.latticeengines.domain.exposed.datacloud.manage.TransformationProgress;
+import com.latticeengines.domain.exposed.datacloud.transformation.configuration.impl.PipelineTransformationConfiguration;
+import com.latticeengines.domain.exposed.datacloud.transformation.step.TransformationStepConfig;
+import com.latticeengines.domain.exposed.metadata.InterfaceName;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.commons.lang3.tuple.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class OrphanTxnExportTestNG extends TransformationServiceImplTestNGBase<PipelineTransformationConfiguration>{
+    private static final Logger log = LoggerFactory.getLogger(OrphanTxnExportTestNG.class);
+    GeneralSource source = new GeneralSource("OrphanTxnExportSeedClean");
+    GeneralSource baseSourceAccount = new GeneralSource("OrphanTxnExportAccount");
+    GeneralSource baseSourceProdcut = new GeneralSource("OrphanTxnExportProduct");
+    GeneralSource baseSourceTxn = new GeneralSource("OrphanTxnExportTxn");
+
+    private Object[][] accountData = new Object[][]{
+            //"AccountId", "Name"
+            {"A001", "Husky"},
+            {"A002", "Alaskan Malamute"},
+            {"A003", "Collie"},
+            {"A004", "Chihuahua"},
+            {"A005", "Labrador Retriever"},
+    };
+
+    private Object[][] productData = new Object[][]{
+            //"ProductId", "ProductName"
+            {"P0001", "test_product_1"},
+            {"P0002", "test_product_2"},
+            {"P0003", "test_product_3"},
+            {"P0004", "test_product_4"},
+            {"P0005", "test_product_5"}
+    };
+
+    private Object[][] transactionData = new String[][]{
+            //"TransactionId", "AccountId", "ProductId", "TransactionCount"
+            {"T00200","A001","P0002","200"},
+            {"T01234","A005","P0010","200"},
+            {"T06666","A010","P0088","300"},
+            {"T08080","A004","P0003","150"},
+            {"T18888","A006","P0004","998"}
+    };
+
+    private Object[][] expectedData = new Object[][]{
+            //"TransactionId", "AccountId", "ProductId", "TransactionCount"
+            {"A006","P0004","T18888","998"},
+            {"A010","P0088","T06666","300"},
+            {"A005","P0010","T01234","200"}
+    };
+
+    @Test(groups = "pipeline1")
+    public void testOrphanTxnExport(){
+        prepareAccountData();
+        prepareProductData();
+        prepareTxnData();
+        TransformationProgress progress = createNewProgress();
+        progress = transformData(progress);
+        finish(progress);
+        confirmResultFile(progress);
+        cleanupProgressTables();
+    }
+
+    @Override
+    protected  PipelineTransformationConfiguration createTransformationConfiguration() {
+        PipelineTransformationConfiguration configuration = new PipelineTransformationConfiguration();
+        configuration.setName("OrphanTxnExport");
+        configuration.setVersion(targetVersion);
+
+        TransformationStepConfig step1 = new TransformationStepConfig();
+        List<String> baseSources = new ArrayList<String>();
+        baseSources.add(baseSourceAccount.getSourceName());
+        baseSources.add(baseSourceProdcut.getSourceName());
+        baseSources.add(baseSourceTxn.getSourceName());
+
+        step1.setBaseSources(baseSources);
+        step1.setTransformer(OrphanTxnExport.TRANSFORMER_NAME);
+        step1.setTargetSource(source.getSourceName());
+        step1.setConfiguration("{}");
+
+        List<TransformationStepConfig> steps = new ArrayList<>();
+        steps.add(step1);
+
+        configuration.setSteps(steps);
+        return configuration;
+    }
+
+    private void prepareAccountData(){
+        List<Pair<String, Class<?>>> columns = new ArrayList<>();
+        columns.add(Pair.of(InterfaceName.AccountId.name(), String.class));
+        columns.add(Pair.of(InterfaceName.Name.name(), String.class));
+        uploadBaseSourceData(baseSourceAccount.getSourceName(), baseSourceVersion, columns, accountData);
+    }
+
+    private void prepareProductData(){
+        List<Pair<String, Class<?>>> columns = new ArrayList<>();
+        columns.add(Pair.of(InterfaceName.ProductId.name(), String.class));
+        columns.add(Pair.of(InterfaceName.ProductName.name(), String.class));
+        uploadBaseSourceData(baseSourceProdcut.getSourceName(),baseSourceVersion,columns, productData);
+    }
+
+    private void prepareTxnData() {
+        List<Pair<String, Class<?>>> columns = new ArrayList<>();
+        columns.add(Pair.of(InterfaceName.TransactionId.name(), String.class));
+        columns.add(Pair.of(InterfaceName.AccountId.name(), String.class));
+        columns.add(Pair.of(InterfaceName.ProductId.name(), String.class));
+        columns.add(Pair.of(InterfaceName.TransactionCount.name(), String.class));
+        uploadBaseSourceData(baseSourceTxn.getSourceName(),baseSourceVersion,columns, transactionData);
+    }
+
+
+    @Override
+    protected TransformationService<PipelineTransformationConfiguration> getTransformationService() {
+        return pipelineTransformationService;
+    }
+
+    @Override
+    protected Source getSource() {
+        return source;
+    }
+
+    @Override
+    protected String getPathToUploadBaseData() {
+        return hdfsPathBuilder.constructSnapshotDir(source.getSourceName(), targetVersion).toString();
+    }
+
+    @Override
+    protected String getPathForResult() {
+        Source targetSource = sourceService.findBySourceName(source.getSourceName());
+        String targetVersion = hdfsSourceEntityMgr.getCurrentVersion(targetSource);
+        return hdfsPathBuilder.constructSnapshotDir(source.getSourceName(), targetVersion).toString();
+    }
+
+
+    @Override
+    protected void verifyResultAvroRecords(Iterator<GenericRecord> records) {
+        log.info("Start to verify records one by one.");
+        int rowNum = 0;
+        while (records.hasNext()) {
+            GenericRecord record = records.next();
+            log.info(record.toString());
+            Assert.assertTrue(isObjEquals(record.get(InterfaceName.AccountId.name()), expectedData[rowNum][0]));
+            Assert.assertTrue(isObjEquals(record.get(InterfaceName.ProductId.name()), expectedData[rowNum][1]));
+            Assert.assertTrue(isObjEquals(record.get(InterfaceName.TransactionId.name()), expectedData[rowNum][2]));
+            Assert.assertTrue(isObjEquals(record.get(InterfaceName.TransactionCount.name()), expectedData[rowNum][3]));
+            rowNum++;
+        }
+        Assert.assertEquals(rowNum, expectedData.length);
+    }
+
+}
