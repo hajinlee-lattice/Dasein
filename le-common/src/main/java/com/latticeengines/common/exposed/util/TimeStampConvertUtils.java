6d4d6ae656fc4b434a7fa35d7af2ace023b59018
jhe <jhe@479f5059-62a3-4c39-9342-9852e3a68435>
2018-08-24 08:59:44 +0000

    Review Request 15933: PLS-8557: Not accurate when use natty parser to parse DateTime without time.

--- le-common/src/main/java/com/latticeengines/common/exposed/util/TimeStampConvertUtils.java
+++ le-common/src/main/java/com/latticeengines/common/exposed/util/TimeStampConvertUtils.java
@@ -6,6 +6,10 @@ import java.util.List;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.DateTimeFormatterBuilder;
+import org.joda.time.format.DateTimeParser;
 
 import com.joestelmach.natty.DateGroup;
 import com.joestelmach.natty.Parser;
@@ -14,12 +18,25 @@ public class TimeStampConvertUtils {
 
     public static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
+    private static final DateTimeFormatter DATE_FORMATTER =
+            new DateTimeFormatterBuilder()
+                    .append(null, new DateTimeParser[]{
+                            DateTimeFormat.forPattern("MM-dd-yyyy").getParser(),
+                            DateTimeFormat.forPattern("MM/dd/yyyy").getParser(),
+                            DateTimeFormat.forPattern("yyyy-MM-dd").getParser(),
+                            DateTimeFormat.forPattern("yyyy/MM/dd").getParser()})
+                    .toFormatter();
+
     public static long convertToLong(String date) {
-        LogManager.getLogger(Parser.class).setLevel(Level.OFF);
-        Parser parser = new Parser();
-        List<DateGroup> groups = parser.parse(date);
-        List<Date> dates = groups.get(0).getDates();
-        return dates.get(0).getTime();
+        try {
+            return DATE_FORMATTER.parseLocalDate(date).toDate().getTime();
+        } catch (Exception e) {
+            LogManager.getLogger(Parser.class).setLevel(Level.OFF);
+            Parser parser = new Parser();
+            List<DateGroup> groups = parser.parse(date);
+            List<Date> dates = groups.get(0).getDates();
+            return dates.get(0).getTime();
+        }
     }
 
     public static String convertToDate(long timeStamp){
