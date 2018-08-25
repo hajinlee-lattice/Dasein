7c98053afdf191afe4efbb2b2027ac9752606ae7
dzheng <dzheng@479f5059-62a3-4c39-9342-9852e3a68435>
2018-08-25 02:07:10 +0000

    Review Request 15941: Shell script to create hive table for datacloud source

---
+++ le-dev/scripts/create_dcsrc_hive.sh
@@ -0,0 +1,26 @@
+#This script is only to create hive table for datacloud source
+#!/bin/bash
+
+SOURCE=$1
+VERSION=$2
+
+if [ -z "$SOURCE" ] || [ -z "$VERSION" ]
+then
+	echo "Please provide source name and version"
+	exit
+fi
+
+if [[ ! $VERSION =~ ^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])_[0-5][0-9]-[0-5][0-9]-[0-5][0-9]_UTC ]]
+then
+	echo "Please provide valid version"
+	exit
+fi
+
+TABLE_SRC=$(echo "$SOURCE" | tr '[:upper:]' '[:lower:]')
+TABLE_VER=$(echo "$VERSION" | tr '[:upper:]' '[:lower:]')
+TABLE_VER=$(echo "$TABLE_VER" | tr - _)
+TABLE="ldc_${TABLE_SRC}_${TABLE_VER}"
+
+QUERY="CREATE EXTERNAL TABLE $TABLE ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.avro.AvroSerDe' STORED AS INPUTFORMAT 'org.apache.hadoop.hive.ql.io.avro.AvroContainerInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.avro.AvroContainerOutputFormat' LOCATION '/Pods/Production/Services/PropData/Sources/$SOURCE/Snapshot/$VERSION' TBLPROPERTIES('avro.schema.url'='/Pods/Production/Services/PropData/Sources/$SOURCE/Schema/$VERSION/$SOURCE.avsc');"
+beeline -u jdbc:hive2://bodcprodvhdp50.prod.lattice.local:10015 -n yarn -e "$QUERY"
+echo "Created hive table $TABLE for source $SOURCE@$VERSION"
