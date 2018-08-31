f86427b5a1f1352e37af9225c0965bd43908d836
lgigli <lgigli@479f5059-62a3-4c39-9342-9852e3a68435>
2018-08-31 16:43:26 +0000

    Review Request 0000 Changed menu to auto close when mouse out

--- le-ui/projects/common/widgets/menu/le-menu.js
+++ le-ui/projects/common/widgets/menu/le-menu.js
@@ -4,14 +4,13 @@ import React, { Component } from 'react';
 export default class LeMenu extends Component {
     constructor(props) {
         super(props);
-        if(this.props.state){
-            this.state = this.props.state
-        }else{
-            this.state = { opened: false, name: this.props.name }
-        }
-        
-        this.menuClass = `le-menu ${this.props.menuClass}`;
-        // this.clickHandler = this.clickHandler.bind(this);
+        this.showMenu(false);
+
+        this.state = { opened: false, name: this.props.name, outmain: false, outmenu: false }
+        this.mouseMainEnterHandler = this.mouseMainEnterHandler.bind(this);
+        this.mouseMenuEnterHandler = this.mouseMenuEnterHandler.bind(this);
+        this.mouseMainLeaveHandler = this.mouseMainLeaveHandler.bind(this);
+        this.mouseMenuLeaveHandler = this.mouseMenuLeaveHandler.bind(this);
     }
 
     getImage(image) {
@@ -29,35 +28,93 @@ export default class LeMenu extends Component {
             return null;
         }
     }
-    clickHandler(event) {
-        // event.stopPropagation();
-        this.setState((prevState, props) => {
-            
-            if (!prevState.opened) {
-                this.menuClass = `le-menu le-menu-show ${props.menuClass}`;
-            } else {
-                this.menuClass = `le-menu ${props.menuClass}`;
-            }
+    mouseMainEnterHandler() {
 
+        this.setState({
+            outmain: false,
+            outmenu: true
+        }, () => {
+            if (this.state.opened) {
+                this.updateShowClass();
+            }
+        });
+    }
+    mouseMenuEnterHandler() {
+        this.setState((prevState, props) => {
             return {
-                opened: !prevState.opened
+                outmain: true,
+                outmenu: false
+            }
+        });
+    }
+
+    mouseMainLeaveHandler() {
+        this.setState({
+            outmain: true
+        }, () => {
+            this.updateShowClass();
+        });
+    }
+
+
+    mouseMenuLeaveHandler() {
+        this.setState({
+            outmenu: true
+        }, () => {
+            this.updateShowClass();
+        });
+
+    }
+
+
+    clickHandler(event) {
+        this.setState((prevState, props) => {
+            this.showMenu(!prevState.opened);
+            return {
+                opened: !prevState.opened,
+                outmain: prevState.opened,
+                outmenu: true
+
             }
         })
-        if(this.props.callback){
-            
+        if (this.props.callback) {
             this.props.callback(event, this.props.name);
+        }
+    }
+    showMenu(show) {
+        console.log('show');
+        if (show) {
+            this.menuClass = `le-menu le-menu-show ${this.props.menuClass ? this.props.menuClass : ''}`;
+        } else {
+            this.menuClass = `le-menu ${this.props.menuClass ? this.props.menuClass : ''}`;
+
+        }
+    }
+
+    updateShowClass() {
+        console.log('up', this.state);
+        if (this.state.opened && (!this.state.outmain || !this.state.outmenu)) {
+            this.showMenu(true);
+            this.setState({ opened: true });
+        } else {
+            this.showMenu(false);
+            this.setState({ opened: false });
         }
     }
 
     render() {
 
         return (
-            <div className={this.menuClass} onClick={(event) => { this.clickHandler(event)}}>
-                <div>
+            <div className={this.menuClass} onClick={(event) => { this.clickHandler(event) }}
+                onMouseEnter={this.mouseMainEnterHandler}
+                onMouseLeave={this.mouseMainLeaveHandler}>
+                <div className="menu-container">
                     {this.getImage(this.props.image)}
                     {this.getLabel(this.props.label)}
                 </div>
-                <div className={`menu-content ${this.props.classNames}`}>
+                <div className={`menu-content ${this.props.menuClass ? this.props.menuClass : ''}`}
+                    onMouseEnter={this.mouseMenuEnterHandler}
+                    onMouseLeave={this.mouseMenuLeaveHandler} >
                     {this.props.children}
                 </div>
             </div>
