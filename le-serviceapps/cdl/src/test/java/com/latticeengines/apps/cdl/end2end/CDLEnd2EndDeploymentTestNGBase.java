c8e7c39df5d4aab7f21fa1dd43639be77a78e64f
dzheng <dzheng@479f5059-62a3-4c39-9342-9852e3a68435>
2018-09-04 11:04:59 +0000

    Review Request 16019: Update check point to fix UpdateTransaction end2end test

--- le-serviceapps/cdl/src/test/java/com/latticeengines/apps/cdl/end2end/CDLEnd2EndDeploymentTestNGBase.java
+++ le-serviceapps/cdl/src/test/java/com/latticeengines/apps/cdl/end2end/CDLEnd2EndDeploymentTestNGBase.java
@@ -130,7 +130,7 @@ public abstract class  CDLEnd2EndDeploymentTestNGBase extends CDLDeploymentTestN
     private static final String COLLECTION_DATE_FORMAT = "yyyy-MM-dd-HH-mm-ss";
     private static final Logger log = LoggerFactory.getLogger(CDLEnd2EndDeploymentTestNGBase.class);
 
-    private static final int S3_CHECKPOINTS_VERSION = 18;
+    private static final int S3_CHECKPOINTS_VERSION = 19;
     private static final int S3_CROSS_SELL_CHECKPOINTS_VERSION = 18;
 
     private static final String INITIATOR = "test@lattice-engines.com";
