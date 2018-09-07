1814e064d7b0da39e60b9eb6d9734f023a9347aa
lming <lming@479f5059-62a3-4c39-9342-9852e3a68435>
2018-09-07 19:54:56 +0000

    Review Request 16068: Some export code refactoring

--- le-serviceflows/core/src/main/java/com/latticeengines/serviceflows/workflow/export/BaseExportToS3.java
+++ le-serviceflows/core/src/main/java/com/latticeengines/serviceflows/workflow/export/BaseExportToS3.java
@@ -17,6 +17,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.yarn.configuration.ConfigurationUtils;
 
+import com.latticeengines.aws.s3.S3Service;
 import com.latticeengines.common.exposed.timer.PerformanceTimer;
 import com.latticeengines.common.exposed.util.HdfsUtils;
 import com.latticeengines.common.exposed.util.JsonUtils;
@@ -44,6 +45,9 @@ public abstract class BaseExportToS3<T extends ExportToS3StepConfiguration> exte
 
     @Inject
     protected SourceFileProxy sourceFileProxy;
+
+    @Inject
+    private S3Service s3Service;
 
     @Value("${aws.region}")
     private String awsRegion;
@@ -118,10 +122,8 @@ public abstract class BaseExportToS3<T extends ExportToS3StepConfiguration> exte
         public void run() {
             try (PerformanceTimer timer = new PerformanceTimer("Copying hdfs dir=" + srcDir + " to s3 dir=" + tgtDir)) {
                 try {
-                    Properties properties = new Properties();
-                    Configuration hadoopConfiguration = ConfigurationUtils.createFrom(yarnConfiguration, properties);
-                    String jobName = StringUtils.isNotBlank(tableName) ? tenantId + "~" + tableName : tenantId;
-                    hadoopConfiguration.set(JobContext.JOB_NAME, jobName);
+                    Configuration hadoopConfiguration = createConfiguration();
+                    processTgtDir();
                     HdfsUtils.distcp(hadoopConfiguration, srcDir, tgtDir, queueName);
                     if (StringUtils.isNotBlank(tableName)) {
                         registerDataUnit();
@@ -134,6 +136,32 @@ public abstract class BaseExportToS3<T extends ExportToS3StepConfiguration> exte
                     throw new RuntimeException(msg);
                 }
             }
+        }
+
+        private void processTgtDir() {
+            try {
+                if (HdfsUtils.isDirectory(yarnConfiguration, srcDir)) {
+                    String lastSrcDir = StringUtils.substringAfterLast(srcDir, "/");
+                    String lastTgtDir = StringUtils.substringAfterLast(tgtDir, "/");
+                    if (!lastSrcDir.equals(lastTgtDir)) {
+                        return;
+                    }
+                    String prefix = StringUtils.substringAfter(tgtDir, "s3n://" + s3Bucket);
+                    if (s3Service.isNonEmptyDirectory(s3Bucket, prefix)) {
+                        tgtDir = StringUtils.substringBeforeLast(tgtDir, "/");
+                    }
+                }
+            } catch (Exception ex) {
+                log.warn("Can not access HDFS Dir=" + srcDir);
+            }
+        }
+
+        private Configuration createConfiguration() {
+            Properties properties = new Properties();
+            Configuration hadoopConfiguration = ConfigurationUtils.createFrom(yarnConfiguration, properties);
+            String jobName = StringUtils.isNotBlank(tableName) ? tenantId + "~" + tableName : tenantId;
+            hadoopConfiguration.set(JobContext.JOB_NAME, jobName);
+            return hadoopConfiguration;
         }
 
         private void registerDataUnit() {
@@ -153,6 +181,9 @@ public abstract class BaseExportToS3<T extends ExportToS3StepConfiguration> exte
         String tgtDir;
         String tableName;
 
+        public ExportRequest() {
+        }
+
         public ExportRequest(String srcDir, String tgtDir) {
             super();
             this.srcDir = srcDir;
@@ -165,9 +196,5 @@ public abstract class BaseExportToS3<T extends ExportToS3StepConfiguration> exte
             this.tgtDir = tgtDir;
             this.tableName = tableName;
         }
-
-        public ExportRequest() {
-        }
-
     }
 }
