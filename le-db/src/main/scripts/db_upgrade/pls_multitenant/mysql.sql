066e70cba56af3c37941a76232d77cd1e5ac93b9
jlmehta <jlmehta@479f5059-62a3-4c39-9342-9852e3a68435>
2018-09-11 23:31:41 +0000

    PLS-10247: Upgrade script to create and attach play types to every play Review Request 16109

--- le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
+++ le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
@@ -1,3 +1,66 @@
+CREATE PROCEDURE `CreatePlayTypes`()
+BEGIN
+  DECLARE tenantId INT;
+  DECLARE playTypeCount INT;
+  DECLARE finished INT DEFAULT 0;
+  DECLARE tenants TEXT;
+  DECLARE curs CURSOR FOR  SELECT distinct FK_TENANT_ID FROM PLS_MultiTenant.PLAY;
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished=1;
+  SET tenants = 'Created Play types for tenantIds: ';
+
+  OPEN curs;
+  WHILE NOT finished DO
+    FETCH curs INTO tenantId;
+	Select count(PID) INTO playTypeCount FROM PLS_MultiTenant.PLAY_TYPE WHERE FK_TENANT_ID = TenantId;
+    IF (playTypeCount < 1) THEN
+       INSERT INTO `PLS_MultiTenant`.`PLAY_TYPE`
+		(`CREATED`, `CREATED_BY`, `DISPLAY_NAME`, `ID`, `UPDATED`, `UPDATED_BY`, `FK_TENANT_ID`, `TENANT_ID`)
+		VALUES
+		(NOW(), 'bnguyen@lattice-engines.com', 'List', UUID(), NOW(), 'bnguyen@lattice-engines.com', tenantId, tenantId),
+		(NOW(), 'bnguyen@lattice-engines.com', 'Cross-Sell', UUID(), NOW(), 'bnguyen@lattice-engines.com', tenantId, tenantId),
+		(NOW(), 'bnguyen@lattice-engines.com', 'Prospecting', UUID(), NOW(), 'bnguyen@lattice-engines.com', tenantId, tenantId),
+		(NOW(), 'bnguyen@lattice-engines.com', 'Renewal', UUID(), NOW(), 'bnguyen@lattice-engines.com', tenantId, tenantId),
+		(NOW(), 'bnguyen@lattice-engines.com', 'Upsell', UUID(), NOW(), 'bnguyen@lattice-engines.com', tenantId, tenantId);
+		SET tenants = concat(tenants , tenantId, ', ');
+    END IF;
+  END WHILE;
+  SELECT tenants;
+  CLOSE curs;
+END
+//
+DELIMITER;
+
+CREATE PROCEDURE `AttachPlayTypes`()
+BEGIN
+  DECLARE tenantId INT;
+  DECLARE playId INT;
+  DECLARE finished INT DEFAULT 0;
+  DECLARE ratingEngineType varchar(255);
+
+  DECLARE curs CURSOR FOR SELECT p.PID, p.FK_TENANT_ID, re.TYPE
+	FROM PLS_MultiTenant.PLAY p
+		JOIN PLS_MultiTenant.RATING_ENGINE re ON p.FK_RATING_ENGINE_ID = re.PID;
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished=1;
+  OPEN curs;
+  WHILE NOT finished DO
+    FETCH curs INTO playId, tenantId, ratingEngineType;
+	UPDATE PLS_MultiTenant.PLAY
+	SET FK_PLAY_TYPE = CASE
+		WHEN ratingEngineType = 'RULE_BASED'
+			THEN (SELECT PID FROM PLS_MultiTenant.PLAY_TYPE WHERE FK_TENANT_ID = tenantId AND DISPLAY_NAME = 'List')
+		WHEN ratingEngineType =	'CROSS_SELL'
+			THEN (SELECT PID FROM PLS_MultiTenant.PLAY_TYPE WHERE FK_TENANT_ID = tenantId AND DISPLAY_NAME = 'Cross-Sell')
+		WHEN ratingEngineType = 'CUSTOM_EVENT'
+			THEN (SELECT PID FROM PLS_MultiTenant.PLAY_TYPE WHERE FK_TENANT_ID = tenantId AND DISPLAY_NAME = 'Prospecting')
+		ELSE (SELECT PID FROM PLS_MultiTenant.PLAY_TYPE WHERE FK_TENANT_ID = tenantId AND DISPLAY_NAME = 'List')
+		END
+	WHERE PID = playId;
+  END WHILE;
+  CLOSE curs;
+END
+//
+DELIMITER;
+
 CREATE PROCEDURE `UpdatePLSTables`()
   BEGIN
     ALTER TABLE `PLS_MultiTenant`.`TENANT` ADD COLUMN `CONTRACT` varchar(255) DEFAULT NULL;
@@ -25,11 +88,17 @@ CREATE PROCEDURE `UpdatePLSTables`()
 
     alter table `PLS_MultiTenant`.`PLAY_TYPE` add constraint `FK_PLAYTYPE_FKTENANTID_TENANT` foreign key (`FK_TENANT_ID`) references `PLS_MultiTenant`.`TENANT` (`TENANT_PID`) on delete cascade;
 
+    CALL `CreatePlayTypes`();
+
     alter table `PLS_MultiTenant`.`PLAY` add column `FK_PLAY_TYPE` bigint;
     alter table `PLS_MultiTenant`.`PLAY` add constraint `FK_PLAY_FKPLAYTYPE_PLAYTYPE` foreign key (`FK_PLAY_TYPE`) references `PLS_MultiTenant`.`PLAY_TYPE` (`PID`);
+
+    CALL `AttachPlayTypes`();
+
+    ALTER `PLS_MultiTenant`.`PLAY` Modify `FK_PLAY_TYPE` bigint NOT NULL;
   END;
 //
-DELIMITER ;
+DELIMITER;
 
 CREATE PROCEDURE `CreateDropBoxTable`()
   BEGIN
@@ -47,7 +116,7 @@ CREATE PROCEDURE `CreateDropBoxTable`()
       on delete cascade;
   END;
 //
-DELIMITER ;
+DELIMITER;
 
 CALL `UpdatePLSTables`();
 CALL `CreateDropBoxTable`();
