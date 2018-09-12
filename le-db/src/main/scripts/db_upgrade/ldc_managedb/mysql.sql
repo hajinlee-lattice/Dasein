a76b6131e7a495532f922cf9ea3e3b151e17b836
slin <slin@479f5059-62a3-4c39-9342-9852e3a68435>
2018-09-12 20:28:32 +0000

    review request 16124: PD-2279 Add signature column in DataCloudVersion for DunsGuideBook

--- le-db/src/main/scripts/db_upgrade/ldc_managedb/mysql.sql
+++ le-db/src/main/scripts/db_upgrade/ldc_managedb/mysql.sql
@@ -2,11 +2,36 @@ USE `LDC_ManageDB`;
 
 SET SQL_SAFE_UPDATES = 0;
 
+DROP PROCEDURE IF EXISTS `UpdateDataCloudVersionTable`;
+
 DROP PROCEDURE IF EXISTS `UpdateSchema`;
+
+DELIMITER //
+CREATE PROCEDURE `UpdateDataCloudVersionTable`()
+  BEGIN
+
+    # add column if not exists
+    IF NOT EXISTS(SELECT *
+                  FROM information_schema.COLUMNS
+                  WHERE
+                    TABLE_SCHEMA = 'LDC_ManageDB'
+                    AND TABLE_NAME = 'DataCloudVersion'
+                    AND COLUMN_NAME = 'DynamoTableSignature_DunsGuideBook')
+    THEN
+      ALTER TABLE `DataCloudVersion`
+        ADD COLUMN `DynamoTableSignature_DunsGuideBook` VARCHAR(100)
+        NULL DEFAULT NULL AFTER `DynamoTableSignature_Lookup`;
+
+    END IF;
+
+  END //
+DELIMITER ;
 
 DELIMITER //
 CREATE PROCEDURE `UpdateSchema`()
   BEGIN
+    CALL `UpdateDataCloudVersionTable`();
+
     START TRANSACTION;
 
 create table `CustomerReport` (`PID` bigint not null auto_increment unique, `COMMENT` longtext, `CREATED_TIME` datetime, `ID` varchar(255) not null unique, `JIRA_TICKET` varchar(255), `REPORTED_BY_TENANT` varchar(255), `REPORTED_BY_USER` varchar(255), `REPRODUCEDETAIL` longtext, `SUGGESTED_VALUE` varchar(255), `TYPE` varchar(255), primary key (`PID`)) ENGINE=InnoDB;
