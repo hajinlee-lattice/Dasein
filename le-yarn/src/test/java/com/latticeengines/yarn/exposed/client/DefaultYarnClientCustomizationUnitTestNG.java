881dc3c8c688b68d96030e9a1e56cadddbe77262
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-09-28 04:23:32 +0000

    Review Request 16409 fix yarn unit test

--- le-yarn/src/test/java/com/latticeengines/yarn/exposed/client/DefaultYarnClientCustomizationUnitTestNG.java
+++ le-yarn/src/test/java/com/latticeengines/yarn/exposed/client/DefaultYarnClientCustomizationUnitTestNG.java
@@ -34,7 +34,8 @@ public class DefaultYarnClientCustomizationUnitTestNG {
     @Test(groups = "unit", dataProvider = "settings")
     public void getXmxSetting(int minAllocationInMb, String requestedMemory, String expectedValue) {
         when(yarnConfiguration.getInt(anyString(), anyInt())).thenReturn(minAllocationInMb);
-        customization = new DefaultYarnClientCustomization(yarnConfiguration, null, null, null, null, null);
+        customization = new DefaultYarnClientCustomization(yarnConfiguration, null, null, null,
+                null, null);
         if (requestedMemory != null) {
             containerProperties.setProperty(ContainerProperty.MEMORY.name(), requestedMemory);
         }
@@ -45,11 +46,13 @@ public class DefaultYarnClientCustomizationUnitTestNG {
 
     @DataProvider(name = "settings")
     public Object[][] getSettings() {
-        return new Object[][] { { 1024, "2048", "-Xmx" + (2048 - 512) + "m" }, //
-                { -1, "2048", "-Xmx" + (2048 - 512) + "m" }, //
-                { 2048, "1024", "-Xmx" + (2048 - 512) + "m" }, //
-                { 2048, null, "-Xmx" + (2048 - 512) + "m" }, //
-                { -1, null, "-Xmx1024m" }, //
+        String expected = "-Xms" + (2048 - 512) + "m -Xmx" + +(2048 - 512) + "m";
+        return new Object[][] { //
+                { 1024, "2048", expected }, //
+                { -1, "2048", expected }, //
+                { 2048, "1024", expected }, //
+                { 2048, null, expected }, //
+                { -1, null, "-Xms1024m -Xmx1024m" }, //
         };
     }
 }
