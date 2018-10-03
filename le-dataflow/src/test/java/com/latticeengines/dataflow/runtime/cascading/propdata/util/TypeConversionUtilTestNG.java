5ec1d7a2dc7ea1c6fb1bf4f489942c4cccd6492d
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-10-03 22:18:14 +0000

    Review Request 16460 fix compilation failure

--- le-dataflow/src/test/java/com/latticeengines/dataflow/runtime/cascading/propdata/util/TypeConversionUtilTestNG.java
+++ le-dataflow/src/test/java/com/latticeengines/dataflow/runtime/cascading/propdata/util/TypeConversionUtilTestNG.java
@@ -18,25 +18,35 @@ public class TypeConversionUtilTestNG {
                 Assert.assertEquals(TypeConversionUtil.convertAnyToString(data), expectedOutput);
                 break;
             case ANY_TO_INT:
-                Assert.assertEquals(TypeConversionUtil.convertAnyToInt(String.valueOf(data)), expectedOutput);
+                Assert.assertEquals(TypeConversionUtil.convertAnyToInt("IntField", data),
+                        expectedOutput);
                 break;
             case STRING_TO_INT:
-                Assert.assertEquals(TypeConversionUtil.convertStringToInt(String.valueOf(data)), expectedOutput);
+                Assert.assertEquals(
+                        TypeConversionUtil.convertAnyToInt("StringField", String.valueOf(data)),
+                        expectedOutput);
                 break;
             case ANY_TO_LONG:
-                Assert.assertEquals(TypeConversionUtil.convertAnyToLong(String.valueOf(data)), expectedOutput);
+                Assert.assertEquals(TypeConversionUtil.convertAnyToLong("LongField", data),
+                        expectedOutput);
                 break;
             case STRING_TO_LONG:
-                Assert.assertEquals(TypeConversionUtil.convertStringToLong(String.valueOf(data)), expectedOutput);
+                Assert.assertEquals(
+                        TypeConversionUtil.convertAnyToLong("StringField", String.valueOf(data)),
+                        expectedOutput);
                 break;
             case ANY_TO_DOUBLE:
-                Assert.assertEquals(TypeConversionUtil.convertAnyToDouble(String.valueOf(data)), expectedOutput);
+                Assert.assertEquals(TypeConversionUtil.convertAnyToDouble("DoubleField", data),
+                        expectedOutput);
                 break;
             case ANY_TO_BOOLEAN:
-                Assert.assertEquals(TypeConversionUtil.convertAnyToBoolean(String.valueOf(data)), expectedOutput);
+                Assert.assertEquals(TypeConversionUtil.convertAnyToBoolean("BooleanField", data),
+                        expectedOutput);
                 break;
             case STRING_TO_BOOLEAN:
-                Assert.assertEquals(TypeConversionUtil.convertStringToBoolean(String.valueOf(data)), expectedOutput);
+                Assert.assertEquals(
+                        TypeConversionUtil.convertAnyToBoolean("StringField", String.valueOf(data)),
+                        expectedOutput);
                 break;
             default:
                 log.info("Strategy not found");
@@ -54,34 +64,35 @@ public class TypeConversionUtilTestNG {
                 { 1F, TypeConvertStrategy.ANY_TO_STRING, "1.0" },
                 { 1D, TypeConvertStrategy.ANY_TO_STRING, "1.0" },
                 // AnyToLong
-                { 6L, TypeConvertStrategy.ANY_TO_LONG, 6L},
-                { "2", TypeConvertStrategy.ANY_TO_LONG, 2L},
-                { 7D, TypeConvertStrategy.ANY_TO_LONG, 7L},
-                { 4F, TypeConvertStrategy.ANY_TO_LONG, 4L},
-                { 3, TypeConvertStrategy.ANY_TO_LONG, 3L},
+                { 6L, TypeConvertStrategy.ANY_TO_LONG, 6L },
+                { "2", TypeConvertStrategy.ANY_TO_LONG, 2L },
+                { 7D, TypeConvertStrategy.ANY_TO_LONG, 7L },
+                { 4F, TypeConvertStrategy.ANY_TO_LONG, 4L },
+                { 3, TypeConvertStrategy.ANY_TO_LONG, 3L },
                 // AnyToDouble
-                { 8D, TypeConvertStrategy.ANY_TO_DOUBLE, 8D }, { 5L, TypeConvertStrategy.ANY_TO_DOUBLE, 5D },
+                { 8D, TypeConvertStrategy.ANY_TO_DOUBLE, 8D },
+                { 5L, TypeConvertStrategy.ANY_TO_DOUBLE, 5D },
                 { "89", TypeConvertStrategy.ANY_TO_DOUBLE, 89D },
                 { 6F, TypeConvertStrategy.ANY_TO_DOUBLE, 6D },
                 // AnyToBoolean
-                { 0, TypeConvertStrategy.ANY_TO_BOOLEAN, new Boolean(false) },
-                { 1L, TypeConvertStrategy.ANY_TO_BOOLEAN, new Boolean(true) },
-                { "Y", TypeConvertStrategy.ANY_TO_BOOLEAN, new Boolean(true) },
-                { true, TypeConvertStrategy.ANY_TO_BOOLEAN, new Boolean(true) },
-                { "YES", TypeConvertStrategy.ANY_TO_BOOLEAN, new Boolean(true) },
-                { "TRUE", TypeConvertStrategy.ANY_TO_BOOLEAN, new Boolean(true) },
-                { "1", TypeConvertStrategy.ANY_TO_BOOLEAN, new Boolean(true) },
-                { "N", TypeConvertStrategy.ANY_TO_BOOLEAN, new Boolean(false) },
-                { "NO", TypeConvertStrategy.ANY_TO_BOOLEAN, new Boolean(false) },
-                { "FALSE", TypeConvertStrategy.ANY_TO_BOOLEAN, new Boolean(false) },
-                { "0", TypeConvertStrategy.ANY_TO_BOOLEAN, new Boolean(false) },
+                { 0, TypeConvertStrategy.ANY_TO_BOOLEAN, Boolean.FALSE },
+                { 1L, TypeConvertStrategy.ANY_TO_BOOLEAN, Boolean.TRUE },
+                { "Y", TypeConvertStrategy.ANY_TO_BOOLEAN, Boolean.TRUE },
+                { true, TypeConvertStrategy.ANY_TO_BOOLEAN, Boolean.TRUE },
+                { "YES", TypeConvertStrategy.ANY_TO_BOOLEAN, Boolean.TRUE },
+                { "TRUE", TypeConvertStrategy.ANY_TO_BOOLEAN, Boolean.TRUE },
+                { "1", TypeConvertStrategy.ANY_TO_BOOLEAN, Boolean.TRUE },
+                { "N", TypeConvertStrategy.ANY_TO_BOOLEAN, Boolean.FALSE },
+                { "NO", TypeConvertStrategy.ANY_TO_BOOLEAN, Boolean.FALSE },
+                { "FALSE", TypeConvertStrategy.ANY_TO_BOOLEAN, Boolean.FALSE },
+                { "0", TypeConvertStrategy.ANY_TO_BOOLEAN, Boolean.FALSE },
                 // AnyToInt
-                { 12, TypeConvertStrategy.ANY_TO_INT, 12 }, { 2F, TypeConvertStrategy.ANY_TO_INT, 2 },
+                { 12, TypeConvertStrategy.ANY_TO_INT, 12 },
+                { 2F, TypeConvertStrategy.ANY_TO_INT, 2 },
                 { true, TypeConvertStrategy.ANY_TO_INT, 1 },
                 { 100L, TypeConvertStrategy.ANY_TO_INT, 100 },
                 { "2", TypeConvertStrategy.ANY_TO_INT, 2 },
                 { 6D, TypeConvertStrategy.ANY_TO_INT, 6 },
-                { 3L, TypeConvertStrategy.ANY_TO_INT, 3 }
-        };
+                { 3L, TypeConvertStrategy.ANY_TO_INT, 3 } };
     }
 }
