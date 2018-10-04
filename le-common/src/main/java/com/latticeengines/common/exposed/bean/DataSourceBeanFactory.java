8b094da921bced50e3274c2d73a4d440376dc4ab
jadusumalli <jadusumalli@479f5059-62a3-4c39-9342-9852e3a68435>
2018-10-04 20:10:59 +0000

    Review Request 16464. Fix DB connection issue to support matchapi scaling

--- le-common/src/main/java/com/latticeengines/common/exposed/bean/DataSourceBeanFactory.java
+++ le-common/src/main/java/com/latticeengines/common/exposed/bean/DataSourceBeanFactory.java
@@ -95,7 +95,7 @@ public class DataSourceBeanFactory implements FactoryBean<DataSource> {
         }
         //If MaxPoolSize is not configured at environment level, then use default MaxPoolSize
         maxPoolSize = maxPoolSize > 0 ? maxPoolSize : this.maxPoolSize;
-        int minPoolSize = this.minPoolSize > 0 ? this.minPoolSize : (Math.max(1, maxPoolSize/32));
+        int minPoolSize = this.minPoolSize > 0 ? this.minPoolSize : 0;
         maxPoolSize = maxPoolSize > minPoolSize ? maxPoolSize : Math.max(minPoolSize, 8);
         
         if (log.isInfoEnabled()) {
