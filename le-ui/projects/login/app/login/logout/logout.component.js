d78f1e12fd5d04fd101203ee64f12aaadf977e19
bholland <bholland@479f5059-62a3-4c39-9342-9852e3a68435>
2018-10-10 19:50:24 +0000

    Review Request 0000 - added redirect timer for logged out page to send you to login after a delay

--- le-ui/projects/login/app/login/logout/logout.component.js
+++ le-ui/projects/login/app/login/logout/logout.component.js
@@ -2,12 +2,35 @@ angular.module('login')
 .component('logoutView', {
     templateUrl: 'app/login/logout/logout.component.html',
     controller: function(
-        $state, $location, $timeout, ResourceUtility, LoginService, Banner,
-        SessionTimeoutUtility, BrowserStorageUtility, LoginStore
+        $state, $location, $timeout, $interval, 
+        ResourceUtility, LoginService, Banner, SessionTimeoutUtility, BrowserStorageUtility, LoginStore
     ) {
         var vm = this;
 
-        vm.$onInit = function() {};
+        angular.extend(vm, {
+            redirectDelay: 10 // change to 0 to disable
+        });
+
+        var timer = function(seconds) {
+            if(!seconds) {
+                return false;
+            }
+            var redirectDelaySeconds = seconds,
+                redirectDelayMs = redirectDelaySeconds * 1000,
+                countDown = redirectDelaySeconds;
+
+            $timeout(function() {
+                $state.go("login.form");
+            }, redirectDelayMs + 1000);
+
+            $interval(function(){
+                vm.delay = countDown--
+            }, 1000, redirectDelaySeconds);
+        }
+
+        vm.$onInit = function() {
+            timer(vm.redirectDelay);
+        };
 
         vm.loginClick = function() {
             $state.go("login.form");
