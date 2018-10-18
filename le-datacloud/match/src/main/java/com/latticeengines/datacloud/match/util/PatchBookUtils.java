3502b041654a204c49ddaf2185cebad278a8c9cb
slin <slin@479f5059-62a3-4c39-9342-9852e3a68435>
2018-10-18 19:48:12 +0000

    review request 16609: Add util functions specific to lookup patch

--- le-datacloud/match/src/main/java/com/latticeengines/datacloud/match/util/PatchBookUtils.java
+++ le-datacloud/match/src/main/java/com/latticeengines/datacloud/match/util/PatchBookUtils.java
@@ -2,6 +2,7 @@ package com.latticeengines.datacloud.match.util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -9,6 +10,9 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import com.google.common.collect.Sets;
+import com.latticeengines.domain.exposed.datacloud.match.MatchKeyUtils;
+import com.latticeengines.domain.exposed.datacloud.match.patch.PatchLog;
 import org.apache.commons.lang3.StringUtils;
 
 import com.google.common.base.Preconditions;
@@ -23,11 +27,21 @@ import com.latticeengines.domain.exposed.datacloud.match.patch.PatchBookValidati
  */
 public class PatchBookUtils {
 
+    private static final String DUPLICATE_MATCH_KEY_ERROR = "Duplicate match key combination found : ";
+    private static final String INVALID_EFFECTIVE_DATE_RANGE_ERROR =
+            "ExpireAfter date should not be earlier than EffectiveSince date";
+
     // TODO consider using equals & hashCode implementation for MatchKeyTuple, but probably too complicated
     // TODO currently using serialized format instead
     // Key: patch book type, Value: set of supported match key tuple (in serialized format)
     private static final Map<PatchBook.Type, Set<String>> SUPPORTED_MATCH_KEY_MAP = new HashMap<>();
-    private static final String DUPLICATE_MATCH_KEY_ERROR = "Duplicate match key combination found : ";
+
+    /* lookup patch */
+
+    // serialized match keys that requires patching AM lookup table
+    private static final Set<String> AM_LOOKUP_PATCH_MATCH_KEYS = new HashSet<>();
+    // serialized match keys that requires patching DunsGuideBook table
+    private static final Set<String> DUNS_GUIDE_BOOK_LOOKUP_PATCH_MATCH_KEYS = new HashSet<>();
 
     static {
         // TODO init supported match key tuple here
@@ -45,21 +59,64 @@ public class PatchBookUtils {
         SUPPORTED_MATCH_KEY_MAP.put(PatchBook.Type.Attribute,
                 new HashSet<>(Arrays.asList(domainTuple.buildIdForKey(), dunsTuple.buildIdForKey(),
                         domainDunsTuple.buildIdForKey())));
-        MatchKeyTuple lookupTuple = new MatchKeyTuple.Builder() //
-                .withDomain(MatchKey.Domain.name()) //
-                .withDuns(MatchKey.DUNS.name()) //
-                .withCountry(MatchKey.Country.name()) //
-                .withState(MatchKey.State.name()) //
-                .withCity(MatchKey.City.name()) //
-                .withName(MatchKey.Name.name()) //
-                .build(); //
-        SUPPORTED_MATCH_KEY_MAP.put(PatchBook.Type.Lookup,
-                new HashSet<>(Arrays.asList(lookupTuple.buildIdForKey())));
         MatchKeyTuple domainPatcherType = new MatchKeyTuple.Builder() //
                 .withDuns(MatchKey.DUNS.name()) //
                 .build(); //
         SUPPORTED_MATCH_KEY_MAP.put(PatchBook.Type.Domain,
                 new HashSet<>(Arrays.asList(domainPatcherType.buildIdForKey())));
+
+        /* init match key constants for lookup patch */
+
+        // am lookup
+        AM_LOOKUP_PATCH_MATCH_KEYS.add(new MatchKeyTuple.Builder() //
+                .withDomain(MatchKey.Domain.name()) //
+                .build().buildIdForKey()); // Domain
+        AM_LOOKUP_PATCH_MATCH_KEYS.add(new MatchKeyTuple.Builder() //
+                .withDuns(MatchKey.DUNS.name()) //
+                .build().buildIdForKey()); // DUNS
+        AM_LOOKUP_PATCH_MATCH_KEYS.add(new MatchKeyTuple.Builder() //
+                .withDomain(MatchKey.Domain.name()) //
+                .withCountry(MatchKey.Country.name()) //
+                .build().buildIdForKey()); // Domain, Country
+        AM_LOOKUP_PATCH_MATCH_KEYS.add(new MatchKeyTuple.Builder() //
+                .withDomain(MatchKey.Domain.name()) //
+                .withCountry(MatchKey.Country.name()) //
+                .withState(MatchKey.State.name()) //
+                .build().buildIdForKey()); // Domain, Country, State
+        AM_LOOKUP_PATCH_MATCH_KEYS.add(new MatchKeyTuple.Builder() //
+                .withDomain(MatchKey.Domain.name()) //
+                .withCountry(MatchKey.Country.name()) //
+                .withState(MatchKey.Zipcode.name()) //
+                .build().buildIdForKey()); // Domain, Country, Zipcode
+
+        /* duns guide book */
+        DUNS_GUIDE_BOOK_LOOKUP_PATCH_MATCH_KEYS.add(new MatchKeyTuple.Builder() //
+                .withName(MatchKey.Name.name()) //
+                .build().buildIdForKey()); // Name
+        DUNS_GUIDE_BOOK_LOOKUP_PATCH_MATCH_KEYS.add(new MatchKeyTuple.Builder() //
+                .withName(MatchKey.Name.name()) //
+                .withCountry(MatchKey.Country.name()) //
+                .build().buildIdForKey()); // Name, Country
+        DUNS_GUIDE_BOOK_LOOKUP_PATCH_MATCH_KEYS.add(new MatchKeyTuple.Builder() //
+                .withName(MatchKey.Name.name()) //
+                .withCountry(MatchKey.Country.name()) //
+                .withState(MatchKey.State.name()) //
+                .build().buildIdForKey()); // Name, Country, State
+        DUNS_GUIDE_BOOK_LOOKUP_PATCH_MATCH_KEYS.add(new MatchKeyTuple.Builder() //
+                .withName(MatchKey.Name.name()) //
+                .withCountry(MatchKey.Country.name()) //
+                .withCity(MatchKey.City.name()) //
+                .build().buildIdForKey()); // Name, Country, City
+        DUNS_GUIDE_BOOK_LOOKUP_PATCH_MATCH_KEYS.add(new MatchKeyTuple.Builder() //
+                .withName(MatchKey.Name.name()) //
+                .withCountry(MatchKey.Country.name()) //
+                .withState(MatchKey.State.name()) //
+                .withCity(MatchKey.City.name()) //
+                .build().buildIdForKey()); // Name, Country, State, City
+
+        // all supported match keys for lookup patch
+        SUPPORTED_MATCH_KEY_MAP.put(PatchBook.Type.Lookup,
+                Sets.union(AM_LOOKUP_PATCH_MATCH_KEYS, DUNS_GUIDE_BOOK_LOOKUP_PATCH_MATCH_KEYS));
     }
 
     /**
@@ -68,6 +125,7 @@ public class PatchBookUtils {
      * @param book target patch book, should not be {@literal null}
      */
     public static void standardize(@NotNull PatchBook book) {
+        // TODO implement
     }
 
     /**
@@ -77,7 +135,33 @@ public class PatchBookUtils {
      * @return generated tuple
      */
     public static MatchKeyTuple getMatchKeyValues(@NotNull PatchBook book) {
-        return getMatchKey(book, true);
+        Preconditions.checkNotNull(book);
+        MatchKeyTuple tuple = new MatchKeyTuple();
+        if (StringUtils.isNotBlank(book.getDomain())) {
+            tuple.setDomain(book.getDomain());
+        }
+        if (StringUtils.isNotBlank(book.getDuns())) {
+            tuple.setDuns(book.getDuns());
+        }
+        if (StringUtils.isNotBlank(book.getName())) {
+            tuple.setName(book.getName());
+        }
+        if (StringUtils.isNotBlank(book.getCountry())) {
+            tuple.setCountry(book.getCountry());
+            // for key partition evaluation, not required in patcher
+            tuple.setCountryCode(book.getCountry());
+        }
+        if (StringUtils.isNotBlank(book.getState())) {
+            tuple.setState(book.getState());
+        }
+        if (StringUtils.isNotBlank(book.getCity())) {
+            tuple.setCity(book.getCity());
+        }
+        if (StringUtils.isNotBlank(book.getZipcode())) {
+            tuple.setZipcode(book.getZipcode());
+        }
+
+        return tuple;
     }
 
     /**
@@ -96,7 +180,7 @@ public class PatchBookUtils {
             matchKeyToPIDs.get(tuple.buildIdForValue()).add(entry.getPid());
         }
 
-        List<PatchBookValidationError> errorList = matchKeyToPIDs.entrySet().stream()
+        return matchKeyToPIDs.entrySet().stream()
                 .filter(entry -> entry.getValue().size() > 1) // filter by key
                 .map(temp -> {
                     PatchBookValidationError error = new PatchBookValidationError();
@@ -104,7 +188,6 @@ public class PatchBookUtils {
                     error.setPatchBookIds(temp.getValue());
                     return error;
                 }).collect(Collectors.toList());
-        return errorList;
     }
 
     /**
@@ -114,7 +197,35 @@ public class PatchBookUtils {
      * @return a list of validation errors to show which patch books have unsupported match keys
      */
     public static List<PatchBookValidationError> validateMatchKeySupport(@NotNull List<PatchBook> patchBooks) {
+        // TODO implement
         return null;
+    }
+
+    /**
+     * Validate whether {@link PatchBook#getEffectiveSince()} and {@link PatchBook#getExpireAfter()} is set correctly.
+     * {@literal null} books will be skipped.
+     *
+     * @param patchBooks list of {@link PatchBook} to validate, should not be {@literal null}
+     * @return a list of validation errors
+     */
+    public static List<PatchBookValidationError> validateEffectiveDateRange(@NotNull List<PatchBook> patchBooks) {
+        Preconditions.checkNotNull(patchBooks);
+        List<Long> invalidRangePids = patchBooks
+                .stream()
+                .filter(book -> book != null && book.getEffectiveSince() != null && book.getExpireAfter() != null)
+                // expire time should not be before effective time
+                .filter(book -> book.getExpireAfter().before(book.getEffectiveSince()))
+                .map(PatchBook::getPid)
+                .collect(Collectors.toList());
+        if (invalidRangePids.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        // not including date value in message currently because it is easy to check
+        PatchBookValidationError error = new PatchBookValidationError();
+        error.setMessage(INVALID_EFFECTIVE_DATE_RANGE_ERROR);
+        error.setPatchBookIds(invalidRangePids);
+        return Collections.singletonList(error);
     }
 
     /**
@@ -124,6 +235,7 @@ public class PatchBookUtils {
      * @return a list of validation errors
      */
     public static List<PatchBookValidationError> validatePatchedItems(@NotNull List<PatchBook> patchBooks) {
+        // TODO implement
         return null;
     }
 
@@ -134,7 +246,56 @@ public class PatchBookUtils {
      * @return true if the patch book DO reach EOF, false otherwise
      */
     public static boolean isEndOfLife(@NotNull PatchBook book) {
+        // TODO implement
         return false;
+    }
+
+    /**
+     * Determine whether the input {@link PatchBook} requires to patch AMLookup table
+     *
+     * @param book target patch book, should not be {@literal null}
+     * @return true if we need to patch AMLookup table, false if no
+     */
+    public static boolean shouldPatchAMLookupTable(@NotNull PatchBook book) {
+        Preconditions.checkNotNull(book);
+        if (!PatchBook.Type.Lookup.equals(book.getType())) {
+            return false;
+        }
+        String tupleStr = getMatchKeyValues(book).buildIdForKey();
+        return AM_LOOKUP_PATCH_MATCH_KEYS.contains(tupleStr);
+    }
+
+    /**
+     * Helper to create {@link PatchLog} from given {@link PatchBook}
+     *
+     * @param book target patch book, should not be {@literal null}
+     * @return generated patch log with all related fields copied
+     */
+    public static PatchLog newPatchLog(@NotNull PatchBook book) {
+        PatchLog log = new PatchLog();
+        log.setPatchBookId(book.getPid());
+        log.setPatchedValue(book.getPatchItems());
+        log.setInputMatchKey(getMatchKeyValues(book));
+        return log;
+    }
+
+    /**
+     * Retrieve patch DUNS from {@link PatchBook}
+     *
+     * @param book input patch book
+     * @return retrieved patch DUNS, {@literal null} if no patch domain in the book
+     */
+    public static String getPatchDuns(@NotNull PatchBook book) {
+        return getStringPatchItem(book.getPatchItems(), MatchKeyUtils.AM_FIELD_MAP.get(MatchKey.DUNS));
+    }
+
+    /**
+     * Retrieve patch domain from {@link PatchBook}
+     * @param book input patch book
+     * @return retrieved patch domain, {@literal null} if no patch domain in the book
+     */
+    public static String getPatchDomain(@NotNull PatchBook book) {
+        return getStringPatchItem(book.getPatchItems(), MatchKeyUtils.AM_FIELD_MAP.get(MatchKey.Domain));
     }
 
     /**
@@ -151,9 +312,9 @@ public class PatchBookUtils {
             throw new UnsupportedOperationException(msg);
         }
 
-        MatchKeyTuple tuple = getMatchKey(patchBook, false);
+        MatchKeyTuple tuple = getMatchKeyValues(patchBook);
         Preconditions.checkNotNull(tuple);
-        String tupleStr = tuple.buildIdForValue();
+        String tupleStr = tuple.buildIdForKey();
         for (String supportedTupleStr : SUPPORTED_MATCH_KEY_MAP.get(type)) {
             if (supportedTupleStr.equals(tupleStr)) {
                 return true;
@@ -163,34 +324,23 @@ public class PatchBookUtils {
     }
 
 
+
     /**
-     * Generate a match key tuple contains all fields present in the input patch book
+     * Helper method to retrieve the value of specified key from given patch items and cast it to {@link String}
      *
-     * @param book input patch book, should not be null
-     * @param useValue true to use values provided in the patch book, false to use corresponding {@link MatchKey}
-     * @return generated tuple
+     * @param patchItems input patch items
+     * @param key target key
+     * @return retrieved value, {@literal null} if specified key does not exist or is not {@link String}
      */
-    private static MatchKeyTuple getMatchKey(@NotNull PatchBook book, boolean useValue) {
-        Preconditions.checkNotNull(book);
-        MatchKeyTuple tuple = new MatchKeyTuple();
-        if (StringUtils.isNotBlank(book.getDomain())) {
-            tuple.setDomain(useValue ? book.getDomain() : MatchKey.Domain.name());
+    private static String getStringPatchItem(Map<String, Object> patchItems, @NotNull String key) {
+        if (patchItems == null) {
+            return null;
         }
-        if (StringUtils.isNotBlank(book.getDuns())) {
-            tuple.setDuns(useValue ? book.getDuns() : MatchKey.DUNS.name());
+
+        Object itemObj = patchItems.get(key);
+        if (!(itemObj instanceof String)) {
+            return null;
         }
-        if (StringUtils.isNotBlank(book.getName())) {
-            tuple.setName(useValue ? book.getName() : MatchKey.Name.name());
-        }
-        if (StringUtils.isNotBlank(book.getCountry())) {
-            tuple.setCountry(useValue ? book.getCountry() : MatchKey.Country.name());
-        }
-        if (StringUtils.isNotBlank(book.getState())) {
-            tuple.setState(useValue ? book.getState() : MatchKey.State.name());
-        }
-        if (StringUtils.isNotBlank(book.getCity())) {
-            tuple.setCity(useValue ? book.getCity() : MatchKey.City.name());
-        }
-        return tuple;
+        return (String) itemObj;
     }
 }
--- le-datacloud/match/src/test/java/com/latticeengines/datacloud/match/util/PatchBookUtilsTestNG.java
+++ le-datacloud/match/src/test/java/com/latticeengines/datacloud/match/util/PatchBookUtilsTestNG.java
@@ -3,10 +3,15 @@ package com.latticeengines.datacloud.match.util;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.time.DateUtils;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.google.common.collect.ImmutableMap;
@@ -16,6 +21,8 @@ import com.latticeengines.domain.exposed.datacloud.match.patch.PatchBookValidati
 
 public class PatchBookUtilsTestNG {
     private static final String DUPLICATE_MATCH_KEY_ERROR = "Duplicate match key combination found : ";
+    private static final String TEST_DATE_FORMAT = "yyyy-MM-dd";
+    private static final AtomicLong TEST_COUNTER = new AtomicLong();
 
     @Test(groups = "unit")
     public void testDuplicateMatchKey() {
@@ -37,6 +44,32 @@ public class PatchBookUtilsTestNG {
             Assert.assertNotNull(pidList);
             Collections.sort(e.getPatchBookIds());
             Assert.assertEquals(e.getPatchBookIds(), pidList);
+        }
+    }
+
+    @Test(groups = "unit", dataProvider = "effectiveDateRangeTestData")
+    public void testInvalidEffectiveDateRange(List<DateTest> tests) {
+        List<PatchBook> books = tests.stream().map(test -> test.book).collect(Collectors.toList());
+        List<Long> expectedErrorIds = tests
+                .stream()
+                .filter(test -> !test.isValid)
+                .map(test -> test.book.getPid())
+                .collect(Collectors.toList());
+        List<PatchBookValidationError> errors = PatchBookUtils.validateEffectiveDateRange(books);
+        Assert.assertNotNull(errors);
+        if (expectedErrorIds.isEmpty()) {
+            Assert.assertTrue(errors.isEmpty());
+        } else {
+            // currently only one type of error message
+            Assert.assertEquals(errors.size(), 1);
+            PatchBookValidationError err = errors.get(0);
+            Assert.assertNotNull(err);
+            Assert.assertNotNull(err.getMessage());
+            Assert.assertNotNull(err.getPatchBookIds());
+
+            // validate IDs
+            Collections.sort(err.getPatchBookIds());
+            Assert.assertEquals(err.getPatchBookIds(), expectedErrorIds);
         }
     }
 
@@ -67,5 +100,70 @@ public class PatchBookUtilsTestNG {
                 // Duplicate duns only
                 { PatchBook.Type.Attribute, 11L, null, "429489284" },
                 { PatchBook.Type.Attribute, 12L, null, "429489284" }, };
+    }
+
+    @DataProvider(name = "effectiveDateRangeTestData")
+    private Object[][] provideEffectiveDateRangeTestData() throws Exception {
+        // NOTE ES = EffectiveSince, EA = ExpireAfter
+        return new Object[][] {
+                // Case #1: All valid
+                toObjectArray(
+                        newDateTest("2018-10-18", "2018-10-18", true), // ES == EA
+                        newDateTest(null, "2018-10-18", true), // null ES
+                        newDateTest("2018-10-18", null, true), // null EA
+                        newDateTest(null, null, true), // null ES and EA
+                        newDateTest("2018-10-18", "2018-10-20", true) // ES < EA
+                ),
+                // Case #2: Some invalid entries
+                {
+                        Arrays.asList(
+                                newDateTest("2018-10-18", "2018-10-18", true), // Valid
+                                newDateTest(null, "2018-10-18", true), // Valid
+                                newDateTest("2018-10-18", "2011-01-01", false), // Invalid
+                                newDateTest(null, null, true), // Valid
+                                newDateTest("2018-10-20", "2018-10-18", false) // Invalid
+                        )
+                },
+                // Case #3: All invalid entries
+                {
+                        Arrays.asList(
+                                newDateTest("2018-10-18", "2011-01-01", false), //
+                                newDateTest("2018-10-20", "2018-10-18", false) //
+                        )
+                },
+        };
+    }
+
+    // helper for syntax
+    private Object[] toObjectArray(DateTest... tests) {
+        return new Object[] { Arrays.asList(tests) };
+    }
+
+    // helper to create DateTest, effectiveSince and expireAfter can be null
+    private DateTest newDateTest(String effectiveSince, String expireAfter, boolean isValid) throws Exception {
+        PatchBook book = new PatchBook();
+        book.setPid(TEST_COUNTER.incrementAndGet());
+        book.setEffectiveSince(newDate(effectiveSince));
+        book.setExpireAfter(newDate(expireAfter));
+        return new DateTest(book, isValid);
+    }
+
+    private Date newDate(String dateStr) throws Exception {
+        if (dateStr == null) {
+            return null;
+        }
+        return DateUtils.parseDate(dateStr, TEST_DATE_FORMAT);
+    }
+
+    /*
+     * Test class for testInvalidEffectiveDateRange
+     */
+    private class DateTest {
+        final PatchBook book;
+        final boolean isValid;
+        DateTest(PatchBook book, boolean isValid) {
+            this.book = book;
+            this.isValid = isValid;
+        }
     }
 }
