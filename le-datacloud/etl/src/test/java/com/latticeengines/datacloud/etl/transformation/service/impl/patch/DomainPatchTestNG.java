b5684aa96e2dc51d05829f18e6c7d6c96bedc3d9
dzheng <dzheng@479f5059-62a3-4c39-9342-9852e3a68435>
2018-10-18 02:15:20 +0000

    Review Request 16598: Few cleanup in DomainPatchTestNG

--- le-datacloud/etl/src/test/java/com/latticeengines/datacloud/etl/transformation/service/impl/patch/DomainPatchTestNG.java
+++ le-datacloud/etl/src/test/java/com/latticeengines/datacloud/etl/transformation/service/impl/patch/DomainPatchTestNG.java
@@ -34,6 +34,8 @@ public class DomainPatchTestNG extends PipelineTransformationTestNGBase {
     private GeneralSource source = amsPatched;
 
     private static final String DOM_SRC = "Patch";
+    private static final String VAL_Y = DataCloudConstants.ATTR_VAL_Y;
+    private static final String VAL_N = DataCloudConstants.ATTR_VAL_N;
 
     @Test(groups = "pipeline1")
     public void testTransformation() {
@@ -169,91 +171,57 @@ public class DomainPatchTestNG extends PipelineTransformationTestNGBase {
              * Case 02: Duns in AMSeed does not exist in DomainPatchBook. They
              * are not impacted by patch
              */
-            { "DUNS2", "dom2.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_N, "DnB", "Name2",
-                    20000L }, //
-            { "DUNS2", "dom2_1.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_Y, "Orb", "Name2",
-                    20000L }, //
-            { "DUNS2_1", "dom2.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_Y, "HG", "Name2_1",
-                    21000L }, //
-            { "DUNS2_2", null, DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_N, null, "Name2_2",
-                    22000L }, //
-            { null, "dom2.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_N, "RTS", "NameNull",
-                    null }, //
+            { "DUNS2", "dom2.com", VAL_Y, VAL_N, "DnB", "Name2", 20000L }, //
+            { "DUNS2", "dom2_1.com", VAL_Y, VAL_Y, "Orb", "Name2", 20000L }, //
+            { "DUNS2_1", "dom2.com", VAL_N, VAL_Y, "HG", "Name2_1", 21000L }, //
+            { "DUNS2_2", null, VAL_N, VAL_N, null, "Name2_2", 22000L }, //
+            { null, "dom2.com", VAL_Y, VAL_N, "RTS", "NameNull", null }, //
 
             /* Case 03: input duns in AMSeed is duns-only entry */
-            { "DUNS3", null, DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_N, null, "Name3",
-                    30000L }, //
-            { "DUNS3_1", null, DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_N, null, "Name3_1",
-                    31000L }, //
-            { "DUNS3_2", null, DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_N, null, "Name3_2",
-                    32000L }, //
-            { "DUNS3_3", null, DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_N, null, "Name3_3",
-                    33000L }, //
+            { "DUNS3", null, VAL_N, VAL_N, null, "Name3", 30000L }, //
+            { "DUNS3_1", null, VAL_N, VAL_N, null, "Name3_1", 31000L }, //
+            { "DUNS3_2", null, VAL_N, VAL_N, null, "Name3_2", 32000L }, //
+            { "DUNS3_3", null, VAL_N, VAL_N, null, "Name3_3", 33000L }, //
 
             /*
              * Case 04: input duns in AMSeed only has one row and has domain
              * populated
              */
-            { "DUNS4", "dom4.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name4",
-                    40000L }, //
-            { "DUNS4_1", "dom4_1.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name4_1",
-                    41000L }, //
-            { "DUNS4_2", "dom4_2.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name4_2",
-                    42000L }, //
-            { "DUNS4_3", "dom4_3.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name4_3",
-                    43000L }, //
-            { "DUNS4_4", "dom4_4.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name4_4",
-                    44000L }, //
+            { "DUNS4", "dom4.com", VAL_Y, VAL_Y, "DnB", "Name4", 40000L }, //
+            { "DUNS4_1", "dom4_1.com", VAL_Y, VAL_Y, "DnB", "Name4_1", 41000L }, //
+            { "DUNS4_2", "dom4_2.com", VAL_Y, VAL_Y, "DnB", "Name4_2", 42000L }, //
+            { "DUNS4_3", "dom4_3.com", VAL_Y, VAL_Y, "DnB", "Name4_3", 43000L }, //
+            { "DUNS4_4", "dom4_4.com", VAL_Y, VAL_Y, "DnB", "Name4_4", 44000L }, //
 
             /*
              * Case 05: input duns in AMSeed has multiple rows and all of them
              * have domain populated
              */
-            { "DUNS5_1", "dom5_1_1.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name5_1",
-                    51000L }, //
-            { "DUNS5_1", "dom5_1_2.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name5_1",
-                    51000L }, //
-            { "DUNS5_2", "dom5_2_1.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name5_2",
-                    52000L }, //
-            { "DUNS5_2", "dom5_2_2.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name5_2",
-                    52000L }, //
-            { "DUNS5_3", "dom5_3_1.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name5_3",
-                    53000L }, //
-            { "DUNS5_3", "dom5_3_2.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name5_3",
-                    53000L }, //
-            { "DUNS5_3", "dom5_3_3.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name5_3",
-                    53000L }, //
-            { "DUNS5_4", "dom5_4_1.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name5_4",
-                    54000L }, //
-            { "DUNS5_4", "dom5_4_2.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name5_4",
-                    54000L }, //
-            { "DUNS5_5", "dom5_5_1.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name5_5",
-                    55000L }, //
-            { "DUNS5_5", "dom5_5_2.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name5_5",
-                    55000L }, //
-            { "DUNS5_6", "dom5_6_1.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name5_6",
-                    56000L }, //
-            { "DUNS5_6", "dom5_6_2.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name5_6",
-                    56000L }, //
-            { "DUNS5_6", "dom5_6_3.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name5_6",
-                    56000L }, //
+            { "DUNS5_1", "dom5_1_1.com", VAL_Y, VAL_Y, "DnB", "Name5_1", 51000L }, //
+            { "DUNS5_1", "dom5_1_2.com", VAL_N, VAL_Y, "DnB", "Name5_1", 51000L }, //
+            { "DUNS5_2", "dom5_2_1.com", VAL_Y, VAL_Y, "DnB", "Name5_2", 52000L }, //
+            { "DUNS5_2", "dom5_2_2.com", VAL_N, VAL_Y, "DnB", "Name5_2", 52000L }, //
+            { "DUNS5_3", "dom5_3_1.com", VAL_Y, VAL_Y, "DnB", "Name5_3", 53000L }, //
+            { "DUNS5_3", "dom5_3_2.com", VAL_N, VAL_Y, "DnB", "Name5_3", 53000L }, //
+            { "DUNS5_3", "dom5_3_3.com", VAL_N, VAL_Y, "DnB", "Name5_3", 53000L }, //
+            { "DUNS5_4", "dom5_4_1.com", VAL_Y, VAL_Y, "DnB", "Name5_4", 54000L }, //
+            { "DUNS5_4", "dom5_4_2.com", VAL_N, VAL_Y, "DnB", "Name5_4", 54000L }, //
+            { "DUNS5_5", "dom5_5_1.com", VAL_Y, VAL_Y, "DnB", "Name5_5", 55000L }, //
+            { "DUNS5_5", "dom5_5_2.com", VAL_N, VAL_Y, "DnB", "Name5_5", 55000L }, //
+            { "DUNS5_6", "dom5_6_1.com", VAL_Y, VAL_Y, "DnB", "Name5_6", 56000L }, //
+            { "DUNS5_6", "dom5_6_2.com", VAL_N, VAL_Y, "DnB", "Name5_6", 56000L }, //
+            { "DUNS5_6", "dom5_6_3.com", VAL_N, VAL_Y, "DnB", "Name5_6", 56000L }, //
 
             /*
              * Case 06: input duns in AMSeed has multiple rows, one of them is
              * duns-only entry and others all have domain populated
              */
-            { "DUNS6_1", "dom6_1_1.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name6_1",
-                    61000L }, //
-            { "DUNS6_1", "dom6_1_2.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name6_1",
-                    61000L }, //
-            { "DUNS6_1", null, DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name6_1",
-                    61000L }, //
-            { "DUNS6_2", "dom6_2_1.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name6_2",
-                    62000L }, //
-            { "DUNS6_2", "dom6_2_2.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name6_2",
-                    62000L }, //
-            { "DUNS6_2", null, DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name6_2",
-                    62000L }, //
+            { "DUNS6_1", "dom6_1_1.com", VAL_Y, VAL_Y, "DnB", "Name6_1", 61000L }, //
+            { "DUNS6_1", "dom6_1_2.com", VAL_N, VAL_Y, "DnB", "Name6_1", 61000L }, //
+            { "DUNS6_1", null, VAL_N, VAL_Y, "DnB", "Name6_1", 61000L }, //
+            { "DUNS6_2", "dom6_2_1.com", VAL_Y, VAL_Y, "DnB", "Name6_2", 62000L }, //
+            { "DUNS6_2", "dom6_2_2.com", VAL_N, VAL_Y, "DnB", "Name6_2", 62000L }, //
+            { "DUNS6_2", null, VAL_N, VAL_Y, "DnB", "Name6_2", 62000L }, //
     };
 
     // Schema (same as AMSeed): DUNS, Domain, LE_IS_PRIMARY_DOMAIN,
@@ -263,95 +231,69 @@ public class DomainPatchTestNG extends PipelineTransformationTestNGBase {
              * Case 02: Duns in AMSeed does not exist in DomainPatchBook. They
              * are not impacted by patch
              */
-            { "DUNS2", "dom2.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_N, "DnB", "Name2",
-                    20000L }, //
-            { "DUNS2", "dom2_1.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_Y, "Orb", "Name2",
-                    20000L }, //
-            { "DUNS2_1", "dom2.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_Y, "HG", "Name2_1",
-                    21000L }, //
-            { "DUNS2_2", null, DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_N, null, "Name2_2", 22000L }, //
-            { null, "dom2.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_N, "RTS", "NameNull", null }, //
+            { "DUNS2", "dom2.com", VAL_Y, VAL_N, "DnB", "Name2", 20000L }, //
+            { "DUNS2", "dom2_1.com", VAL_Y, VAL_Y, "Orb", "Name2", 20000L }, //
+            { "DUNS2_1", "dom2.com", VAL_N, VAL_Y, "HG", "Name2_1", 21000L }, //
+            { "DUNS2_2", null, VAL_N, VAL_N, null, "Name2_2", 22000L }, //
+            { null, "dom2.com", VAL_Y, VAL_N, "RTS", "NameNull", null }, //
 
             /* Case 03: input duns in AMSeed is duns-only entry */
             // populate domain for duns-only entry
-            { "DUNS3", "dom3.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_N, DOM_SRC, "Name3",
-                    30000L }, //
+            { "DUNS3", "dom3.com", VAL_N, VAL_N, DOM_SRC, "Name3", 30000L }, //
             // populate domain for duns-only entry, add more rows
-            { "DUNS3_1", "dom3_1_1.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_N, DOM_SRC,
-                    "Name3_1", 31000L }, //
-            { "DUNS3_1", "dom3_1_2.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_N, DOM_SRC,
-                    "Name3_1", 31000L }, //
+            { "DUNS3_1", "dom3_1_1.com", VAL_N, VAL_N, DOM_SRC, "Name3_1", 31000L }, //
+            { "DUNS3_1", "dom3_1_2.com", VAL_N, VAL_N, DOM_SRC, "Name3_1", 31000L }, //
             // populate domain for duns-only entry, add more rows, also try to
             // clean up domain but actually no action taken since there is no
             // domain for duns-only entry)
-            { "DUNS3_2", "dom3_2_1.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_N, DOM_SRC,
-                    "Name3_2", 32000L }, //
-            { "DUNS3_2", "dom3_2_2.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_N, DOM_SRC,
-                    "Name3_2", 32000L }, //
+            { "DUNS3_2", "dom3_2_1.com", VAL_N, VAL_N, DOM_SRC, "Name3_2", 32000L }, //
+            { "DUNS3_2", "dom3_2_2.com", VAL_N, VAL_N, DOM_SRC, "Name3_2", 32000L }, //
             // try to clean up domain but actually no action taken since there
             // is no domain for duns-only entry)
-            { "DUNS3_3", null, DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_N, null, "Name3_3", 33000L }, //
+            { "DUNS3_3", null, VAL_N, VAL_N, null, "Name3_3", 33000L }, //
 
             /*
              * Case 04: input duns in AMSeed only has one row and has domain
              * populated
              */
             // Add more rows
-            { "DUNS4", "dom4.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name4",
-                    40000L }, //
-            { "DUNS4", "dom4_1.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_N, DOM_SRC, "Name4",
-                    40000L }, //
+            { "DUNS4", "dom4.com", VAL_Y, VAL_Y, "DnB", "Name4", 40000L }, //
+            { "DUNS4", "dom4_1.com", VAL_N, VAL_N, DOM_SRC, "Name4", 40000L }, //
             // Domain to add is same as existing domain, do nothing
-            { "DUNS4_1", "dom4_1.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name4_1",
-                    41000L }, //
+            { "DUNS4_1", "dom4_1.com", VAL_Y, VAL_Y, "DnB", "Name4_1", 41000L }, //
             // Set domain to null
-            { "DUNS4_2", null, DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_N, null, "Name4_2",
-                    42000L }, //
+            { "DUNS4_2", null, VAL_N, VAL_N, null, "Name4_2", 42000L }, //
             // Domain to clean up does not exist, no action taken
-            { "DUNS4_3", "dom4_3.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name4_3",
-                    43000L }, //
+            { "DUNS4_3", "dom4_3.com", VAL_Y, VAL_Y, "DnB", "Name4_3", 43000L }, //
             // Update domain - clean up existing domain by updating to
             // domain to add, also add more rows
-            { "DUNS4_4", "dom4_4_1.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_N, DOM_SRC,
-                    "Name4_4", 44000L }, //
-            { "DUNS4_4", "dom4_4_2.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_N, DOM_SRC,
-                    "Name4_4", 44000L }, //
+            { "DUNS4_4", "dom4_4_1.com", VAL_N, VAL_N, DOM_SRC, "Name4_4", 44000L }, //
+            { "DUNS4_4", "dom4_4_2.com", VAL_N, VAL_N, DOM_SRC, "Name4_4", 44000L }, //
 
             /*
              * Case 05: input duns in AMSeed has multiple rows and all of them
              * have domain populated
              */
             // Add more rows
-            { "DUNS5_1", "dom5_1_1.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name5_1",
-                    51000L }, //
-            { "DUNS5_1", "dom5_1_2.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name5_1",
-                    51000L }, //
-            { "DUNS5_1", "dom5_1_3.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_N, DOM_SRC,
-                    "Name5_1", 51000L }, //
-            { "DUNS5_1", "dom5_1_4.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_N, DOM_SRC,
-                    "Name5_1", 51000L }, //
+            { "DUNS5_1", "dom5_1_1.com", VAL_Y, VAL_Y, "DnB", "Name5_1", 51000L }, //
+            { "DUNS5_1", "dom5_1_2.com", VAL_N, VAL_Y, "DnB", "Name5_1", 51000L }, //
+            { "DUNS5_1", "dom5_1_3.com", VAL_N, VAL_N, DOM_SRC, "Name5_1", 51000L }, //
+            { "DUNS5_1", "dom5_1_4.com", VAL_N, VAL_N, DOM_SRC, "Name5_1", 51000L }, //
             // Domain to add is same as existing domain, no action taken
-            { "DUNS5_2", "dom5_2_1.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name5_2",
-                    52000L }, //
-            { "DUNS5_2", "dom5_2_2.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name5_2",
-                    52000L }, //
+            { "DUNS5_2", "dom5_2_1.com", VAL_Y, VAL_Y, "DnB", "Name5_2", 52000L }, //
+            { "DUNS5_2", "dom5_2_2.com", VAL_N, VAL_Y, "DnB", "Name5_2", 52000L }, //
             // Remove some rows
-            { "DUNS5_3", "dom5_3_3.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name5_3",
-                    53000L }, //
+            { "DUNS5_3", "dom5_3_3.com", VAL_N, VAL_Y, "DnB", "Name5_3", 53000L }, //
             // Duns has 2 domains, set one of domain to null, remove the other
             // row
-            { "DUNS5_4", null, DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_N, null, "Name5_4", 54000L }, //
+            { "DUNS5_4", null, VAL_N, VAL_N, null, "Name5_4", 54000L }, //
             // Domain to clean up does not exist
-            { "DUNS5_5", "dom5_5_1.com", DataCloudConstants.ATTR_VAL_Y, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name5_5",
-                    55000L }, //
-            { "DUNS5_5", "dom5_5_2.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name5_5",
-                    55000L }, //
+            { "DUNS5_5", "dom5_5_1.com", VAL_Y, VAL_Y, "DnB", "Name5_5", 55000L }, //
+            { "DUNS5_5", "dom5_5_2.com", VAL_N, VAL_Y, "DnB", "Name5_5", 55000L }, //
             // Update domain - clean up existing domain by updating to domain to
             // add, also remove some rows
-            { "DUNS5_6", "dom5_6_3.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name5_6",
-                    56000L }, //
-            { "DUNS5_6", "dom5_6_4.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_N, DOM_SRC,
-                    "Name5_6", 56000L }, //
+            { "DUNS5_6", "dom5_6_3.com", VAL_N, VAL_Y, "DnB", "Name5_6", 56000L }, //
+            { "DUNS5_6", "dom5_6_4.com", VAL_N, VAL_N, DOM_SRC, "Name5_6", 56000L }, //
 
             /*
              * Case 06: input duns in AMSeed has multiple rows, one of them is
@@ -359,13 +301,11 @@ public class DomainPatchTestNG extends PipelineTransformationTestNGBase {
              */
             // Remove all the rows with domain populated, leave duns-only
             // entry there
-            { "DUNS6_1", null, DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_Y, "DnB", "Name6_1", 61000L }, //
+            { "DUNS6_1", null, VAL_N, VAL_Y, "DnB", "Name6_1", 61000L }, //
             // Populate duns-only entry with domain to add, also add some
             // rows and remove some rows
-            { "DUNS6_2", "dom6_2_3.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_N, DOM_SRC,
-                    "Name6_2", 62000L }, //
-            { "DUNS6_2", "dom6_2_4.com", DataCloudConstants.ATTR_VAL_N, DataCloudConstants.ATTR_VAL_N, DOM_SRC,
-                    "Name6_2", 62000L }, //
+            { "DUNS6_2", "dom6_2_3.com", VAL_N, VAL_N, DOM_SRC, "Name6_2", 62000L }, //
+            { "DUNS6_2", "dom6_2_4.com", VAL_N, VAL_N, DOM_SRC, "Name6_2", 62000L }, //
     };
 
     private void prepareDomainPatchBook() {
