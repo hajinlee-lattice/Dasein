6c3f071679702c909fcbc5f0e68444c90931982f
slin <slin@479f5059-62a3-4c39-9342-9852e3a68435>
2018-10-19 21:17:20 +0000

    review request 16594: PD-2326 PD-2329 Add API endpoints for DataCloud Patcher

--- le-matchapi/src/main/java/com/latticeengines/matchapi/controller/PatchResource.java
+++ le-matchapi/src/main/java/com/latticeengines/matchapi/controller/PatchResource.java
@@ -1,9 +1,32 @@
 package com.latticeengines.matchapi.controller;
 
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Sets;
+import com.latticeengines.common.exposed.validator.annotation.NotNull;
+import com.latticeengines.datacloud.core.entitymgr.DataCloudVersionEntityMgr;
+import com.latticeengines.datacloud.match.entitymgr.PatchBookEntityMgr;
+import com.latticeengines.datacloud.match.exposed.service.PatchBookValidator;
+import com.latticeengines.domain.exposed.datacloud.manage.DataCloudVersion;
+import com.latticeengines.domain.exposed.datacloud.manage.PatchBook;
+import com.latticeengines.domain.exposed.datacloud.match.patch.LookupPatchRequest;
+import com.latticeengines.domain.exposed.datacloud.match.patch.LookupPatchResponse;
+import com.latticeengines.domain.exposed.datacloud.match.patch.PatchBookValidationError;
+import com.latticeengines.domain.exposed.datacloud.match.patch.PatchLog;
+import com.latticeengines.domain.exposed.datacloud.match.patch.PatchMode;
+import com.latticeengines.domain.exposed.datacloud.match.patch.PatchRequest;
+import com.latticeengines.domain.exposed.datacloud.match.patch.PatchResponse;
+import com.latticeengines.domain.exposed.datacloud.match.patch.PatchStatus;
+import com.latticeengines.domain.exposed.datacloud.match.patch.PatchValidationResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,12 +40,26 @@ import com.latticeengines.domain.exposed.datacloud.match.LookupUpdateResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
+import javax.inject.Inject;
+
 @Api(value = "patch", description = "REST resource for account master lookup patch")
 @RestController
 @RequestMapping("/patches")
 public class PatchResource {
 
-    @Autowired
+    private static final Set<PatchStatus> STATUSES_ALLOWED_IN_RESPONSE = Sets.newHashSet(
+            PatchStatus.Failed, PatchStatus.Noop, PatchStatus.NewInactive);
+
+    @Inject
+    private DataCloudVersionEntityMgr dataCloudVersionEntityMgr;
+
+    @Inject
+    private PatchBookEntityMgr patchBookEntityMgr;
+
+    @Inject
+    private PatchBookValidator patchBookValidator;
+
+    @Inject
     private PatchService patchService;
 
     @RequestMapping(value = "", method = RequestMethod.POST, headers = "Accept=application/json")
@@ -32,4 +69,184 @@ public class PatchResource {
         return patchService.patch(requests);
     }
 
+    @RequestMapping(
+            value = "/validate/{patchBookType}", method = RequestMethod.POST, headers = "Accept=application/json")
+    @ResponseBody
+    @ApiOperation(value = "Validate patch book entries with the given type", response = PatchValidationResponse.class)
+    private PatchValidationResponse validatePatchBook(
+            @PathVariable String patchBookType, @RequestBody PatchRequest request) {
+        checkRequired(request);
+        checkAndSetDataCloudVersion(request);
+
+        PatchBook.Type type = getPatchBookType(patchBookType);
+        // FIXME currently, validation for PatchBook.Type == Domain is not implemented yet, remove this when it is done
+        if (type == PatchBook.Type.Domain) {
+            throw new UnsupportedOperationException(
+                    "Validation for 'Domain' patch book entries hasn't been implemented yet");
+        }
+
+        List<PatchBook> books = load(request.getMode(), type);
+
+        return validate(books, type, request);
+    }
+
+    @RequestMapping(value = "/lookup", method = RequestMethod.POST, headers = "Accept=application/json")
+    @ResponseBody
+    @ApiOperation(value = "Patch all patch book entries with lookup type", response = LookupPatchResponse.class)
+    private LookupPatchResponse lookupPatch(@RequestBody LookupPatchRequest request) {
+        checkRequired(request);
+        checkAndSetDataCloudVersion(request);
+
+        LookupPatchResponse response = prepareResponse(request);
+
+        PatchBook.Type type = PatchBook.Type.Lookup;
+        List<PatchBook> books = load(request.getMode(), type);
+
+        // validate
+        PatchValidationResponse validationResponse = validate(books, type, request);
+        response.setValidationResponse(validationResponse);
+        if (!validationResponse.isSuccess()) {
+            // failed validation, do nothing
+            return endPatchResponse(response);
+        }
+
+        // patch entries
+        List<PatchLog> logs = patchService.lookupPatch(
+                books, request.getDataCloudVersion(), request.getMode(), request.isDryRun());
+        // only return important logs as returning all of them will be too much
+        // full logs will be available in log file
+        response.setNotPatchedLogs(logs
+                .stream()
+                .filter(log -> STATUSES_ALLOWED_IN_RESPONSE.contains(log.getStatus()))
+                .collect(Collectors.toList()));
+
+        // upload logs
+        response.setLogFilePath(patchService.uploadPatchLog(logs));
+
+        // calculate stats
+        response.setStats(calculateStats(logs));
+        // cleanup fields
+        response.getNotPatchedLogs().forEach(this::cleanupPatchLog);
+
+        return endPatchResponse(response);
+    }
+
+    /*
+     * Generate patch stats (total # of items, # of items for each PatchStatus)
+     */
+    private PatchResponse.PatchStats calculateStats(@NotNull List<PatchLog> patchLogs) {
+        PatchResponse.PatchStats stats = new PatchResponse.PatchStats();
+        stats.setTotal(patchLogs.size());
+        stats.setStatusMap(patchLogs
+                .stream()
+                // count the frequency
+                .collect(Collectors.toMap(PatchLog::getStatus, l -> 1, Integer::sum)));
+        return stats;
+    }
+
+    /*
+     * trim debug fields (not returning to prevent API response from being too large)
+     */
+    private void cleanupPatchLog(@NotNull PatchLog patchLog) {
+        patchLog.setInputMatchKey(null);
+        patchLog.setOriginalValue(null);
+        patchLog.setPatchedValue(null);
+    }
+
+    /*
+     * set endAt and calculate duration
+     */
+    private LookupPatchResponse endPatchResponse(@NotNull LookupPatchResponse response) {
+        response.setEndAt(new Date());
+        Date end = response.getEndAt();
+        Date start = response.getStartAt();
+        // duration.toString() => ISO-8601 e.g., PT8H6M12.345S
+        Duration duration = Duration.between(start.toInstant(), end.toInstant());
+        // remove PT & change unit to lowercase => 8h6m12.345s
+        String durationStr = duration.toString().substring(2).toLowerCase();
+        // TODO maybe remove sub-second part
+        response.setDuration(durationStr);
+        return response;
+    }
+
+    private LookupPatchResponse prepareResponse(@NotNull LookupPatchRequest request) {
+        LookupPatchResponse response = new LookupPatchResponse();
+        response.setPatchBookType(PatchBook.Type.Lookup);
+        response.setDataCloudVersion(request.getDataCloudVersion());
+        response.setMode(request.getMode());
+        response.setLogLevel(request.getLogLevel());
+        response.setDryRun(request.isDryRun());
+        response.setStartAt(new Date());
+        return response;
+    }
+
+    /*
+     * 1. filter out entries that is end of life
+     * 2. validate remaining entries
+     * 3. configure response with input request and validation result
+     */
+    private PatchValidationResponse validate(
+            @NotNull List<PatchBook> books, @NotNull PatchBook.Type type, @NotNull PatchRequest request) {
+        String dataCloudVersion = request.getDataCloudVersion();
+
+        List<PatchBookValidationError> errors = patchBookValidator.validate(type, dataCloudVersion, books);
+        Preconditions.checkNotNull(errors);
+
+        PatchValidationResponse response = new PatchValidationResponse();
+        response.setSuccess(errors.isEmpty());
+        response.setValidationErrors(errors);
+        response.setPatchBookType(type);
+        response.setDataCloudVersion(dataCloudVersion);
+        response.setMode(request.getMode());
+        return response;
+    }
+
+    private List<PatchBook> load(@NotNull PatchMode mode, PatchBook.Type type) {
+        if (mode == PatchMode.Normal) {
+            return patchBookEntityMgr.findByType(type);
+        } else {
+            // hot fix mode
+            return patchBookEntityMgr.findByTypeAndHotFix(type, true);
+        }
+    }
+
+    /*
+     * case insensitive match
+     */
+    private PatchBook.Type getPatchBookType(String patchBookType) {
+        return Arrays.stream(PatchBook.Type.values())
+                .filter(e -> e.name().equalsIgnoreCase(patchBookType))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("Invalid patch book type"));
+    }
+
+    /*
+     * if dataCloudVersion is provided in request, check if the version exist in DB
+     * else use the current version
+     */
+    private void checkAndSetDataCloudVersion(@NotNull PatchRequest request) {
+        if (StringUtils.isNotBlank(request.getDataCloudVersion())) {
+            // check if the version exists
+            String versionStr = request.getDataCloudVersion();
+            DataCloudVersion version = dataCloudVersionEntityMgr.findVersion(versionStr);
+            if (version == null) {
+                // TODO throw different error
+                String msg = String.format("Provided DataCloudVersion (%s) does not exist", versionStr);
+                throw new IllegalArgumentException(msg);
+            }
+            return;
+        }
+
+        // use the current if not provided version
+        DataCloudVersion version = dataCloudVersionEntityMgr.currentApprovedVersion();
+        Preconditions.checkNotNull(version);
+        request.setDataCloudVersion(version.getVersion());
+    }
+
+    private void checkRequired(PatchRequest request) {
+        if (request == null || request.getMode() == null) {
+            // TODO throw different error
+            throw new IllegalArgumentException("Patch request missing required field");
+        }
+    }
 }
