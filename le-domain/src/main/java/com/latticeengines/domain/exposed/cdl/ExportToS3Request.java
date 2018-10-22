dd3bdec16c307ae52ad97cb7db2b85cca79f9ab0
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-10-22 22:35:28 +0000

    Review Request 16662 export to s3

---
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/cdl/ExportToS3Request.java
@@ -0,0 +1,26 @@
+package com.latticeengines.domain.exposed.cdl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.NONE, getterVisibility = JsonAutoDetect.Visibility.NONE)
+public class ExportToS3Request {
+
+    @JsonProperty("Tenants")
+    private List<String> tenants = new ArrayList<>();
+
+    public List<String> getTenants() {
+        return tenants;
+    }
+
+    public void setTenants(List<String> tenants) {
+        this.tenants = tenants;
+    }
+}
--- le-serviceapps/cdl/src/main/java/com/latticeengines/apps/cdl/controller/ExportToS3Resource.java
+++ le-serviceapps/cdl/src/main/java/com/latticeengines/apps/cdl/controller/ExportToS3Resource.java
@@ -3,6 +3,7 @@ package com.latticeengines.apps.cdl.controller;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 
 import javax.inject.Inject;
 
@@ -21,57 +22,71 @@ import com.latticeengines.apps.cdl.service.ExportToS3Service;
 import com.latticeengines.apps.cdl.service.ExportToS3Service.ExportRequest;
 import com.latticeengines.apps.core.annotation.NoCustomerSpace;
 import com.latticeengines.common.exposed.util.HdfsUtils;
+import com.latticeengines.common.exposed.util.ThreadPoolUtils;
 import com.latticeengines.domain.exposed.camille.CustomerSpace;
+import com.latticeengines.domain.exposed.cdl.ExportToS3Request;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 @Api(value = "serving store", description = "REST resource for serving stores")
 @RestController
-@RequestMapping("/exportToS3")
+@RequestMapping("/export-to-s3")
 public class ExportToS3Resource {
 
     private static final Logger log = LoggerFactory.getLogger(ExportToS3Resource.class);
 
     @Inject
     private Configuration yarnConfiguration;
+
     @Inject
     private ExportToS3Service exportToS3Service;
+
+    private ExecutorService workers = null;
 
     @NoCustomerSpace
     @RequestMapping(method = RequestMethod.POST)
     @ResponseBody
     @ApiOperation(value = "Export tenants' artifacts to S3")
-    public List<String> exportToS3( //
-            @RequestBody String inputTenants) {
+    public List<String> exportToS3(@RequestBody ExportToS3Request request) {
         log.info("Starting Export To S3");
+
+        List<String> inputTenants = new ArrayList<>();
+        if (CollectionUtils.isNotEmpty(request.getTenants())) {
+            request.getTenants().forEach(t -> inputTenants.add(CustomerSpace.parse(t.trim()).toString()));
+        }
+
         log.info("User input tenants=" + inputTenants);
-        if (StringUtils.isBlank(inputTenants)) {
+        if (CollectionUtils.isEmpty(inputTenants)) {
             log.warn("There's no input tenants!");
             return Collections.emptyList();
         }
 
         List<CustomerSpace> customerSpaces = new ArrayList<>();
         List<String> resultCustomers = new ArrayList<>();
-        String[] inputTenantsToken = inputTenants.split("[,;]");
-        for (int i = 0; i < inputTenantsToken.length; i++) {
-            buildCustomers(inputTenantsToken[i].trim(), resultCustomers, customerSpaces);
+        for (String tenant: inputTenants) {
+            buildCustomers(tenant, resultCustomers, customerSpaces);
         }
 
         if (CollectionUtils.isEmpty(resultCustomers)) {
             log.warn("There's not customers selected!");
-            return resultCustomers;
+        } else {
+            List<ExportRequest> requests = new ArrayList<>();
+            ExecutorService workers = getWorkers();
+            workers.submit(() -> {
+                log.info("Exporting to S3 for tenants: " + resultCustomers);
+                for (int i = 0, batchSize = 3; i < resultCustomers.size(); i++) {
+                    exportToS3Service.buildRequests(customerSpaces.get(i), requests);
+                    if ((i % batchSize) == (batchSize - 1) || (i == resultCustomers.size() - 1)) {
+                        exportToS3Service.executeRequests(requests);
+                        exportToS3Service.buildDataUnits(requests);
+                        requests.clear();
+                    }
+                }
+                log.info("Finished Export To S3");
+            });
         }
-        List<ExportRequest> requests = new ArrayList<>();
-        for (int i = 0, batchSize = 3; i < resultCustomers.size(); i++) {
-            exportToS3Service.buildRequests(customerSpaces.get(i), requests);
-            if ((i % batchSize) == (batchSize - 1) || (i == resultCustomers.size() - 1)) {
-                exportToS3Service.executeRequests(requests);
-                exportToS3Service.buildDataUnits(requests);
-                requests.clear();
-            }
-        }
-        log.info("Finished Export To S3");
+
         return resultCustomers;
     }
 
@@ -101,6 +116,17 @@ public class ExportToS3Resource {
         } catch (Exception ex) {
             log.error("Can not get the list of files for tenants=" + tenants, ex);
         }
+    }
+
+    private ExecutorService getWorkers() {
+        if (workers == null) {
+            synchronized (this) {
+                if (workers == null) {
+                    workers = ThreadPoolUtils.getCachedThreadPool("export-s3");
+                }
+            }
+        }
+        return workers;
     }
 
 }
