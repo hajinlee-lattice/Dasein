9433a01cc213266b8d1c5be06dcc1cd18047be23
slin <slin@479f5059-62a3-4c39-9342-9852e3a68435>
2018-10-24 00:09:08 +0000

    review request 16686: Add displayName to PatchStatus and serialize using that field

--- le-domain/src/main/java/com/latticeengines/domain/exposed/datacloud/match/patch/PatchStatus.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/datacloud/match/patch/PatchStatus.java
@@ -1,8 +1,22 @@
 package com.latticeengines.domain.exposed.datacloud.match.patch;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 /**
  * Patch result of one PatchBook entry
  */
 public enum PatchStatus {
-    Patched, Failed, Inactive, NewInactive, Noop
+    Patched("Patched"), Failed("Failed"), Inactive("Inactive"), NewInactive("NewInactive"), Noop("No-op");
+
+    private String displayName;
+
+    PatchStatus(String displayName) {
+        this.displayName = displayName;
+    }
+
+    // serialized to displayName instead of the literal
+    @JsonValue
+    public String getDisplayName() {
+        return displayName;
+    }
 }
