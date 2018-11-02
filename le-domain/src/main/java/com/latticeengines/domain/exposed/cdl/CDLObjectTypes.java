3c5b9fa82d20e4168dcbb20dea86ee19faaea3dc
ygao <ygao@479f5059-62a3-4c39-9342-9852e3a68435>
2018-11-02 17:24:53 +0000

    Review Request 16855: PLS-11055 Atlas: 'Play(s)' in Segment in Use card need to be replaced as 'Campaign(s)'

--- le-domain/src/main/java/com/latticeengines/domain/exposed/cdl/CDLObjectTypes.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/cdl/CDLObjectTypes.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public enum CDLObjectTypes {
     Segment("Segment"), //
     Model("Model"), // i.e. RatingEngine
-    Play("Play"); //
+    Play("Campaign"); //
 
     private static Map<String, CDLObjectTypes> map = new HashMap<>();
 
