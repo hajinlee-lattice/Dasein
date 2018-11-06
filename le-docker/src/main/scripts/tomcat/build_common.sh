62ceefb675dadbe520ca1796c6165948678b97ad
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-11-06 23:22:00 +0000

    Review Request 16475 add common build script for swagger and haproxy

--- le-docker/src/main/scripts/tomcat/build_common.sh
+++ le-docker/src/main/scripts/tomcat/build_common.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
-source ../functions.sh
+build_docker() {
+	IMAGE=$1
+    NO_CACHE=$2
+    sed -i.bak "s|{{TIMESTAMP}}|$(date +%s)|g" Dockerfile
+    if [ "${NO_CACHE}" == "--no-cache" ]; then
+	    docker build --no-cache -t ${IMAGE} . || true
+	else
+	    docker build -t ${IMAGE} . || true
+	fi
+	mv Dockerfile.bak Dockerfile
+}
+
+NO_CACHE=$1
 
 for img in 'haproxy' 'swagger'; do
 
     pushd le-${img} &&
-    build_docker latticeengines/${img} &&
+    build_docker latticeengines/${img} ${NO_CACHE} &&
     popd &
 
 done
