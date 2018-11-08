7ebc213b6ad0850a1683fdf689f7df5da99bf6b7
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-11-08 02:39:16 +0000

    Review Request 16475 add ministack ip alias

---
+++ le-dev/scripts/ministack_ip.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+STACK=$1
+
+PROFILE=$2
+PROFILE=${PROFILE:=default}
+
+if [ -z "${STACK}" ]; then
+    echo "usage: ministack-ip STACK"
+    exit 0
+fi
+
+aws ec2 describe-instances \
+    --filter "Name=tag:le-stack,Values=${STACK}" \
+    --query "Reservations[0].Instances[0].PrivateIpAddress" \
+    --profile ${PROFILE} \
+    --output text
