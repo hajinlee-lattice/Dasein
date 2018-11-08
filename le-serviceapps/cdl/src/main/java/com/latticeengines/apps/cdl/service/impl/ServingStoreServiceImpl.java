980cb0e7db5d1eab85571271ac9b2bcee07bcda6
ygao <ygao@479f5059-62a3-4c39-9342-9852e3a68435>
2018-11-08 22:49:11 +0000

    Review Request 16807: Fix the issue where new-modeling API return less than expected attributes

--- le-serviceapps/cdl/src/main/java/com/latticeengines/apps/cdl/service/impl/ServingStoreServiceImpl.java
+++ le-serviceapps/cdl/src/main/java/com/latticeengines/apps/cdl/service/impl/ServingStoreServiceImpl.java
@@ -16,7 +16,6 @@ import com.latticeengines.db.exposed.util.MultiTenantContext;
 import com.latticeengines.domain.exposed.metadata.ColumnMetadata;
 import com.latticeengines.domain.exposed.metadata.DataCollection;
 import com.latticeengines.domain.exposed.metadata.TableRoleInCollection;
-import com.latticeengines.domain.exposed.propdata.manage.ColumnSelection;
 import com.latticeengines.domain.exposed.query.BusinessEntity;
 import com.latticeengines.domain.exposed.serviceapps.core.AttrState;
 
@@ -63,27 +62,10 @@ public class ServingStoreServiceImpl implements ServingStoreService {
                 if (Boolean.TRUE.equals(cm.getShouldDeprecate()) && !AttrState.Inactive.equals(cm.getAttrState())) {
                     cm.setAttrState(AttrState.Deprecated);
                 }
-
-                if (AttrState.Inactive.equals(cm.getAttrState())) {
-                    // disable these useages if it is inactive attribute.
-                    cm.disableGroup(ColumnSelection.Predefined.Segment);
-                    cm.disableGroup(ColumnSelection.Predefined.Enrichment);
-                    cm.disableGroup(ColumnSelection.Predefined.TalkingPoint);
-                    cm.disableGroup(ColumnSelection.Predefined.CompanyProfile);
-                    cm.disableGroup(ColumnSelection.Predefined.Model);
-                }
-
-                if (AttrState.Deprecated.equals(cm.getAttrState())) {
-                    // disable these useages if it is deprecated attribute.
-                    cm.disableGroup(ColumnSelection.Predefined.Enrichment);
-                    cm.disableGroup(ColumnSelection.Predefined.CompanyProfile);
-                    cm.disableGroup(ColumnSelection.Predefined.Model);
-                }
-
                 return cm;
             });
         } else {
-            flux = Flux.<ColumnMetadata>empty().parallel();
+            flux = Flux.<ColumnMetadata> empty().parallel();
         }
         return flux;
     }
