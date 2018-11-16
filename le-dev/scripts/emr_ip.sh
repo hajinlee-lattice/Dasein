6776c5bb529bf84c707e7dcd2808f75a57ccfd1e
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-11-16 16:36:30 +0000

    Review Request 16864 update emr ip alias

--- le-dev/scripts/emr_ip.sh
+++ le-dev/scripts/emr_ip.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 CLUSTER_NAME=$1
+PROFILE=$2
 PROFILE=${PROFILE:=default}
 
 if [ -z "${CLUSTER_NAME}" ]; then
@@ -14,5 +15,5 @@ aws ec2 describe-instances \
         "Name=tag:aws:elasticmapreduce:instance-group-role,Values=MASTER" \
         "Name=instance-state-name,Values=running" \
     --query 'Reservations[*].Instances[*].NetworkInterfaces[*].PrivateIpAddresses[*].PrivateIpAddress[]' \
-    --profile ${PROFILE} \
+    --profile=${PROFILE} \
     --output text
