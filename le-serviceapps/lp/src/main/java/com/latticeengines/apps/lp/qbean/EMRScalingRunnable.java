e088d86ded90bf50d913be5ac3e3bda4095aad90
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-11-19 21:06:13 +0000

    Review Request 16864 add one more log to emr scaling

--- le-serviceapps/lp/src/main/java/com/latticeengines/apps/lp/qbean/EMRScalingRunnable.java
+++ le-serviceapps/lp/src/main/java/com/latticeengines/apps/lp/qbean/EMRScalingRunnable.java
@@ -168,6 +168,8 @@ public class EMRScalingRunnable implements Runnable {
                     // be conservative about terminating machines
                     scale(target);
                     resetScalingDownCounter();
+                } else {
+                    log.info("Skip scaling down: " + (requested <= running) + " and " + (scalingDownAttempt.get() >= 3));
                 }
             }
         }
