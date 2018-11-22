a2005f442c1c826a6314771ab73ebacc44c42c88
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-11-22 01:39:04 +0000

    Review Request 16959 read cluster id from consul first

--- le-hadoop/src/main/java/com/latticeengines/hadoop/service/impl/EMRCacheServiceImpl.java
+++ le-hadoop/src/main/java/com/latticeengines/hadoop/service/impl/EMRCacheServiceImpl.java
@@ -3,14 +3,20 @@ package com.latticeengines.hadoop.service.impl;
 import javax.inject.Inject;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.latticeengines.aws.emr.EMRService;
+import com.latticeengines.common.exposed.util.Base64Utils;
+import com.latticeengines.common.exposed.util.HttpClientUtils;
 import com.latticeengines.common.exposed.util.RetryUtils;
 import com.latticeengines.domain.exposed.cache.CacheName;
 import com.latticeengines.hadoop.service.EMRCacheService;
@@ -18,6 +24,8 @@ import com.latticeengines.hadoop.service.EMRCacheService;
 @Service("emrCacheService")
 @Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class EMRCacheServiceImpl implements EMRCacheService {
+
+    private static final Logger log = LoggerFactory.getLogger(EMRCacheServiceImpl.class);
 
     @Inject
     private EMRService emrService;
@@ -27,6 +35,9 @@ public class EMRCacheServiceImpl implements EMRCacheService {
 
     @Value("${aws.emr.cluster}")
     private String clusterName;
+
+    @Value("${hadoop.consul.url}")
+    private String consul;
 
     private String mainClusterId;
 
@@ -60,7 +71,20 @@ public class EMRCacheServiceImpl implements EMRCacheService {
 
     @Cacheable(cacheNames = CacheName.Constants.EMRClusterCacheName, key = "T(java.lang.String).format(\"%s|clusterid\", #clusterName)", sync=true)
     public String getClusterIdFromAWS(String clusterName) {
-        return emrService.getClusterId(clusterName);
+        String clusterId = "";
+        try {
+            if (StringUtils.isNotBlank(consul)) {
+                RetryTemplate retry = RetryUtils.getRetryTemplate(5);
+                clusterId = retry.execute(context -> getClusterIdFromConsul(clusterName));
+            }
+        } catch (Exception e) {
+            log.info("Failed to get cluster id from consul.", e);
+            clusterId = "";
+        }
+        if (StringUtils.isBlank(clusterId)) {
+            clusterId = emrService.getClusterId(clusterName);
+        }
+        return clusterId;
     }
 
     @Override
@@ -75,6 +99,19 @@ public class EMRCacheServiceImpl implements EMRCacheService {
     public Boolean isEncrypted(String clusterName) {
         String clusterId = getClusterId(clusterName);
         return emrService.isEncrypted(clusterId);
+    }
+
+    private String getClusterIdFromConsul(String clusterName) {
+        String clusterId = "";
+        String consul = "http://internal-consul-1214146536.us-east-1.elb.amazonaws.com:8500";
+        String emrKvUrl = consul + "/v1/kv/emr/" + clusterName;
+        RestTemplate restTemplate = HttpClientUtils.newRestTemplate();
+        JsonNode json = restTemplate.getForObject(emrKvUrl, JsonNode.class);
+        if (json != null && json.size() >= 1) {
+            String data = json.get(0).get("Value").asText();
+            clusterId = new String(Base64Utils.decodeBase64(data));
+        }
+        return clusterId;
     }
 
 }
