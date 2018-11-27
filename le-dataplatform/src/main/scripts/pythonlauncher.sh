8eed97dcfaf2b93d941f540812cbb8fd3cb92c51
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-11-27 18:08:16 +0000

    Review Request 16934 log SHDP_HD_FSWEB

--- le-dataplatform/src/main/scripts/pythonlauncher.sh
+++ le-dataplatform/src/main/scripts/pythonlauncher.sh
@@ -5,6 +5,7 @@ if [ "${ANACONDA_HOME}" = "" ]; then
     ANACONDA_HOME=/opt/conda
 fi
 echo "  * ANACONDA_HOME: ${ANACONDA_HOME}"
+echo "  * SHDP_HD_FSWEB: ${SHDP_HD_FSWEB}"
 
 echo "  * Activating Anaconda Env: $1"
 source $ANACONDA_HOME/bin/activate $1
