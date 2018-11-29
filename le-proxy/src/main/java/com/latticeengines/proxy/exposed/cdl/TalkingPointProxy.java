82a742520296c96a86133846535aded4c498bf82
perry.liu <perry.liu@479f5059-62a3-4c39-9342-9852e3a68435>
2018-11-29 23:06:44 +0000

    Review Request 17028: 2nd commit removing unnecessary comments

--- le-proxy/src/main/java/com/latticeengines/proxy/exposed/cdl/TalkingPointProxy.java
+++ le-proxy/src/main/java/com/latticeengines/proxy/exposed/cdl/TalkingPointProxy.java
@@ -8,7 +8,6 @@ import com.latticeengines.domain.exposed.cdl.DantePreviewResources;
 import com.latticeengines.domain.exposed.cdl.TalkingPointDTO;
 import com.latticeengines.domain.exposed.cdl.TalkingPointPreview;
 import com.latticeengines.domain.exposed.query.AttributeLookup;
-// import com.latticeengines.network.exposed.dante.TalkingPointInterface;
 import com.latticeengines.proxy.exposed.MicroserviceRestApiProxy;
 import com.latticeengines.proxy.exposed.ProxyInterface;
 import com.latticeengines.security.exposed.serviceruntime.exception.PostResponseErrorHandler;;
@@ -44,30 +43,24 @@ public class TalkingPointProxy extends MicroserviceRestApiProxy implements Proxy
         String url =
                 constructUrl(URL_PREFIX + "/play/" + playName, shortenCustomerSpace(customerSpace))
                         + "?publishedonly=" + publishedOnly;
-        // String url = constructUrl("/play/" + playName) + "?publishedonly=" + publishedOnly;
         return get("findAllByPlayName", url, List.class);
     }
 
     public TalkingPointPreview getTalkingPointPreview(String customerSpace, String playName) {
         String url = constructUrl(URL_PREFIX + "/preview" + "?playName=" + playName,
                 shortenCustomerSpace(customerSpace));
-        // String url = constructUrl("/preview" + "?playName=" + playName + "&customerSpace=" +
-        // customerSpace);
         return get("getTalkingPointPreview", url, TalkingPointPreview.class);
     }
 
     public DantePreviewResources getPreviewResources(String customerSpace) {
         String url =
                 constructUrl(URL_PREFIX + "/previewresources", shortenCustomerSpace(customerSpace));
-        // String url = constructUrl("/previewresources?customerSpace=" + customerSpace);
         return get("getPreviewResources", url, DantePreviewResources.class);
     }
 
     public void publish(String customerSpace, String playName) {
         String url = constructUrl(URL_PREFIX + "/publish" + "?playName=" + playName,
                 shortenCustomerSpace(customerSpace));
-        // String url = constructUrl("/publish" + "?playName=" + playName + "&customerSpace=" +
-        // customerSpace);
         post("publish", url, null, String.class);
     }
 
@@ -75,14 +68,11 @@ public class TalkingPointProxy extends MicroserviceRestApiProxy implements Proxy
     public List<TalkingPointDTO> revert(String customerSpace, String playName) {
         String url = constructUrl(URL_PREFIX + "/revert" + "?playName=" + playName,
                 shortenCustomerSpace(customerSpace));
-        // String url = constructUrl("/revert" + "?playName=" + playName + "&customerSpace=" +
-        // customerSpace);
         return post("revert", url, null, List.class);
     }
 
     public TalkingPointDTO findByName(String customerSpace, String name) {
         String url = constructUrl(URL_PREFIX + "/" + name, shortenCustomerSpace(customerSpace));
-        // String url = constructUrl("/" + name);
         return get("findByName", url, TalkingPointDTO.class);
     }
 
@@ -90,7 +80,6 @@ public class TalkingPointProxy extends MicroserviceRestApiProxy implements Proxy
             String playName) {
         String url = constructUrl(URL_PREFIX + "/attributes/" + playName,
                 shortenCustomerSpace(customerSpace));
-        // String url = constructUrl("/attributes/" + playName);
         List<?> list = get("getAttributesInTalkingPointOfPlay", url, List.class);
         return JsonUtils.convertList(list, AttributeLookup.class);
     }
