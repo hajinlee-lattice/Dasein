8c93686e488549208f34b7d36fa7a1ef7a8f5a27
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-11-30 03:49:11 +0000

    Review Request 17032 try to avoid using ModelAndView

--- le-security/src/main/java/com/latticeengines/security/exposed/serviceruntime/exception/InternalServiceExceptionHandler.java
+++ le-security/src/main/java/com/latticeengines/security/exposed/serviceruntime/exception/InternalServiceExceptionHandler.java
@@ -3,11 +3,12 @@ package com.latticeengines.security.exposed.serviceruntime.exception;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.collect.ImmutableMap;
+import com.latticeengines.common.exposed.util.JsonUtils;
 import com.latticeengines.domain.exposed.exception.LedpCode;
 import com.latticeengines.domain.exposed.exception.LedpException;
 import com.latticeengines.domain.exposed.exception.LoginException;
@@ -17,28 +18,31 @@ public abstract class InternalServiceExceptionHandler extends BaseExceptionHandl
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ModelAndView handleException(RemoteLedpException e) {
+    @ResponseBody
+    public JsonNode handleException(RemoteLedpException e) {
         String stackTrace = e.getCause() != null ? ExceptionUtils.getStackTrace(e.getCause()) : ExceptionUtils
                 .getStackTrace(e);
         if (e.getRemoteStackTrace() != null) {
             stackTrace = stackTrace + "\nCaused remotely by...\n" + e.getRemoteStackTrace();
         }
         logError(e);
-        return getModelAndView(e, stackTrace);
+        return getJsonView(e, stackTrace);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ModelAndView handleException(LedpException e) {
+    @ResponseBody
+    public JsonNode handleException(LedpException e) {
         String stackTrace = e.getCause() != null ? ExceptionUtils.getStackTrace(e.getCause()) : ExceptionUtils
                 .getStackTrace(e);
         logError(e);
-        return getModelAndView(e, stackTrace);
+        return getJsonView(e, stackTrace);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ModelAndView handleException(Exception e) {
+    @ResponseBody
+    public JsonNode handleException(Exception e) {
         String stackTrace = ExceptionUtils.getStackTrace(e);
         if (stackTrace.contains("org.apache.catalina.connector.ClientAbortException")) {
             logWarning(e.getMessage());
@@ -46,28 +50,29 @@ public abstract class InternalServiceExceptionHandler extends BaseExceptionHandl
             logError(e);
             triggerCriticalAlert(e);
         }
-        return getModelAndView(e, stackTrace);
+        return getJsonView(e, stackTrace);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public ModelAndView handleException(LoginException e) {
+    @ResponseBody
+    public JsonNode handleException(LoginException e) {
         String stackTrace = ExceptionUtils.getStackTrace(e);
         logError(e);
         triggerCriticalAlert(e);
-        return getModelAndView(e, stackTrace);
+        return getJsonView(e, stackTrace);
     }
 
-    private ModelAndView getModelAndView(Exception e, String stackTrace) {
-        MappingJackson2JsonView jsonView = new MappingJackson2JsonView();
-        return new ModelAndView(jsonView, ImmutableMap.of("errorCode", LedpCode.LEDP_00002.name(), //
+    private JsonNode getJsonView(Exception e, String stackTrace) {
+        String serialized = JsonUtils.serialize(ImmutableMap.of("errorCode", LedpCode.LEDP_00002.name(), //
                 "errorMsg", emptyStringIfNull(e.getMessage()), "stackTrace", stackTrace));
+        return JsonUtils.deserialize(serialized, JsonNode.class);
     }
 
-    private ModelAndView getModelAndView(LedpException e, String stackTrace) {
-        MappingJackson2JsonView jsonView = new MappingJackson2JsonView();
-        return new ModelAndView(jsonView, ImmutableMap.of("errorCode", e.getCode().name(), //
+    private JsonNode getJsonView(LedpException e, String stackTrace) {
+        String serialized = JsonUtils.serialize(ImmutableMap.of("errorCode", e.getCode().name(), //
                 "errorMsg", emptyStringIfNull(e.getMessage()), "stackTrace", stackTrace));
+        return JsonUtils.deserialize(serialized, JsonNode.class);
     }
 
     private String emptyStringIfNull(Object o) {
