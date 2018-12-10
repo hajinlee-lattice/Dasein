fd14942005d618f677991a2f310cf10cbcd5f197
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-12-10 17:59:49 +0000

    Review Request 16934 always run setup docker

--- le-dev/scripts/setupenv_docker.sh
+++ le-dev/scripts/setupenv_docker.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-if [ "${LE_USING_DOCKER}" == "true" ]; then
-
-    printf "%s\n" "${DOCKER_DATA_ROOT:?You must set DOCKER_DATA_ROOT}"
-
-    sudo mkdir -p ${DOCKER_DATA_ROOT} || true
-    sudo chown -R $USER $DOCKER_DATA_ROOT
-
-    bash $WSHOME/le-dev/scripts/docker_pull.sh
-fi
+printf "%s\n" "${DOCKER_DATA_ROOT:?You must set DOCKER_DATA_ROOT}"
+sudo mkdir -p ${DOCKER_DATA_ROOT} || true
+sudo chown -R $USER $DOCKER_DATA_ROOT
+bash $WSHOME/le-dev/scripts/docker_pull.sh
