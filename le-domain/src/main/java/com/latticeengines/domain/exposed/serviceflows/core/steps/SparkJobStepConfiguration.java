52c799030911d53adc73fec6bd7deb865bf08e80
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-12-10 16:21:30 +0000

    Review Request 16934 ignore unknown property

--- le-domain/src/main/java/com/latticeengines/domain/exposed/serviceflows/core/steps/SparkJobStepConfiguration.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/serviceflows/core/steps/SparkJobStepConfiguration.java
@@ -1,11 +1,13 @@
 package com.latticeengines.domain.exposed.serviceflows.core.steps;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.latticeengines.domain.exposed.workflow.BaseStepConfiguration;
 
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "name")
 @JsonSubTypes({ //
         @JsonSubTypes.Type(value = ProcessMatchResultConfiguration.class, name = "ProcessMatchResultConfiguration") //
@@ -22,4 +24,5 @@ public class SparkJobStepConfiguration extends BaseStepConfiguration {
     public void setCustomer(String customer) {
         this.customer = customer;
     }
+
 }
