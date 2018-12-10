a4158c93491c17398cc37fb2ee687df79e854118
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-12-10 04:19:30 +0000

    Review Request 17119 flatten dpltc alias

--- le-dev/scripts/tcdpl.py
+++ le-dev/scripts/tcdpl.py
@@ -46,8 +46,10 @@ else:
     logger.info('CATALINA_HOME=%s' % CATALINA_HOME)
 
 LE_APPS = ['admin', 'pls', 'microservice', 'playmaker', 'oauth2', 'scoringapi', 'saml', 'matchapi', 'ulysses']
-MS_MODULES = ['dataflowapi', 'eai', 'metadata', 'modeling', 'propdata', 'scoring', 'workflowapi', 'quartz', 'dellebi',
+MS_MODULES = ['ms-core', 'dataflowapi', 'eai', 'metadata', 'modeling', 'propdata', 'scoring', 'workflowapi', 'quartz', 'dellebi',
               'modelquality', 'sqoop', 'datacloudapi', 'objectapi', 'cdl', 'lp']
+
+LE_APPS = [ app for app in LE_APPS + MS_MODULES if app != 'microservice' ]
 
 APP_ROOT = "https://localhost"
 APP_URL = {
@@ -64,36 +66,33 @@ APP_URL = {
 }
 
 PRESETS = {
-    'cdl_pre_checkin': {
-        'apps': ['admin', 'pls', 'microservice', 'matchapi', 'scoringapi'],
-        'modules': ['eai', 'metadata', 'dataflowapi', 'workflowapi', 'modeling', 'scoring', 'datacloudapi', 'cdl',
-                    'objectapi', 'sqoop', 'quartz', 'lp']
+    'cdl': {
+        'apps': ['admin', 'pls', 'lp', 'cdl', 'matchapi', 'metadata',
+                 'scoringapi', 'dataflowapi', 'workflowapi', 'objectapi',
+                 'eai', 'modeling', 'scoring', 'datacloudapi',
+                 'sqoop', 'quartz']
     },
     'lp': {
-        'apps': ['admin', 'pls', 'microservice', 'oauth2', 'scoringapi', 'matchapi', 'playmaker', 'saml'],
-        'modules': ['dataflowapi', 'eai', 'metadata', 'modeling', 'scoring', 'workflowapi', 'lp', 'quartz', 'sqoop',
-                    'cdl']
-    },
-    'cdl': {
-        'apps': ['admin', 'pls', 'microservice', 'playmaker', 'oauth2', 'matchapi', 'ulysses', 'saml', 'scoringapi'],
-        'modules': ['dataflowapi', 'eai', 'metadata', 'workflowapi', 'modeling', 'scoring', 'datacloudapi', 'lp',
-                    'quartz', 'cdl', 'objectapi', 'sqoop']
+        'apps': ['admin', 'pls', 'lp', 'cdl', 'oauth2', 'saml', 'scoringapi', 'matchapi', 'metadata',
+                 'playmaker', 'dataflowapi', 'eai', 'modeling', 'scoring', 'workflowapi',
+                 'quartz', 'sqoop']
     },
     'etl': {
-        'apps': ['microservice', 'matchapi'],
-        'modules': ['metadata', 'workflowapi', 'datacloudapi', 'eai', 'sqoop']
+        'apps': ['microservice', 'matchapi', 'metadata', 'workflowapi', 'datacloudapi', 'eai', 'sqoop']
     },
     'mq': {
-        'apps': ['admin', 'pls', 'microservice', 'oauth2', 'scoringapi', 'matchapi', 'saml'],
-        'modules': ['dataflowapi', 'eai', 'metadata', 'modeling', 'scoring', 'workflowapi', 'lp', 'quartz', 'sqoop',
-                    'modelquality']
+        'apps': ['admin', 'pls', 'oauth2', 'scoringapi', 'matchapi', 'saml',
+                 'dataflowapi', 'eai', 'metadata', 'modeling', 'scoring', 'workflowapi',
+                 'lp', 'quartz', 'sqoop', 'modelquality']
     },
     'all': {
-        'apps': ['admin', 'pls', 'microservice', 'oauth2', 'scoringapi', 'matchapi', 'playmaker', 'ulysses', 'saml'],
-        'modules': ['dataflowapi', 'eai', 'metadata', 'modeling', 'scoring', 'workflowapi', 'lp', 'quartz', 'sqoop',
-                    'datacloudapi', 'cdl', 'objectapi']
+        'apps': ['admin', 'pls', 'oauth2', 'scoringapi', 'matchapi', 'playmaker', 'ulysses', 'saml',
+                 'dataflowapi', 'eai', 'metadata', 'modeling', 'scoring', 'workflowapi', 'lp', 'quartz',
+                 'sqoop', 'datacloudapi', 'cdl', 'objectapi']
     }
 }
+
+PRESETS['cdl_pre_checkin'] = PRESETS['cdl']
 
 
 @contextlib.contextmanager
@@ -106,22 +105,36 @@ def no_ssl_verification():
     requests.Session.request = old_request
 
 
-def cleanupWars():
-    logger.info('clean up existing wars ...')
-    for dirName, subdirList, fileList in os.walk(os.path.join(CATALINA_HOME, "webapps")):
-        for dir in subdirList:
-            if dir == 'ROOT':
-                rmtree(dirName + "/" + dir)
-        for file in fileList:
-            if file[-4:] == '.war':
-                logger.info('removing %s from %s' % (file, dirName))
-                os.remove(dirName + "/" + file)
+def get_webapp_dir_in_tc(app):
+    webapp = 'oauth' if (app == 'oauth2') else app
+    if webapp in MS_MODULES:
+        if webapp == "ms-core":
+            webapp_dir = os.path.join(CATALINA_HOME, "webapps", "ms", "ROOT")
+        else:
+            webapp_dir = os.path.join(CATALINA_HOME, "webapps", "ms", webapp)
+    else:
+        webapp_dir = os.path.join(CATALINA_HOME, 'webapps', webapp)
+    return webapp_dir
 
-    for module in MS_MODULES:
-        dirName = os.path.join(CATALINA_HOME, "webapps", "ms")
-        if (os.path.isdir(dirName + "/" + module)):
-            logger.info('removing %s from %s' % (module, dirName))
-            rmtree(dirName + "/" + module)
+
+def cleanup_webapp_in_tc(app):
+    webapp_dir = get_webapp_dir_in_tc(app)
+    if os.path.isdir(webapp_dir):
+        logger.info('removing %s' % webapp_dir)
+        rmtree(webapp_dir)
+    war_file = webapp_dir + ".war"
+    if os.path.isfile(war_file):
+        logger.info('removing %s' % war_file)
+        os.remove(war_file)
+
+
+def cleanup_wars():
+    logger.info('clean up existing wars ...')
+    for app in LE_APPS:
+        cleanup_webapp_in_tc(app)
+
+    # special treatment for microservice core
+    cleanup_webapp_in_tc('ms-core')
 
     logger.info('clean up workspace ...')
     for dir_name in os.listdir(os.path.join(CATALINA_HOME, "work")):
@@ -131,82 +144,70 @@ def cleanupWars():
             rmtree(dir_path)
 
 
-def deployApp(app, modules):
+def deploy_app(app):
     logger.info('deploying ' + app)
-    if app == 'microservice':
-        deployMs(modules)
-        return
+    # deploy manager app if not already
+    deploy_mgr_app(app)
+    # deploy war from maven project to tomcat
+    prj_war = find_war_in_project(app)
+    if prj_war is not None:
+        cleanup_webapp_in_tc(app)
+        webapp_dir = get_webapp_dir_in_tc(app)
+        webapp_war = webapp_dir + ".war"
+        copyfile(prj_war, webapp_war + ".copy")
+        os.rename(webapp_war + ".copy", webapp_war)
+        logger.info('deployed %s to %s' % (prj_war, webapp_war))
+    else:
+        logger.warning("Cannot find war file built for %s" % app)
 
-    appWar = None
-    targetDir = os.path.join(WSHOME, 'le-' + app, 'target')
-    for file in os.listdir(targetDir):
+
+def find_war_in_project(app):
+    prj = get_project(app)
+    mvn_tgt = os.path.join(prj, 'target')
+    app_war = None
+    for file in os.listdir(mvn_tgt):
         if file[-4:] == '.war':
-            appWar = file
-            logger.info('found %s in %s' % (file, targetDir))
+            app_war = file
+            logger.info('found %s in %s' % (app_war, mvn_tgt))
             break
-
-    if appWar is None:
-        raise IOError("Cannot find war file for app " + app)
-
-    webappName = 'oauth' if (app == 'oauth2') else app
-    webappWar = 'ROOT.war'
-    webappDir = os.path.join(CATALINA_HOME, 'webapps', webappName)
-
-    deployMgrApp(app)
-
-    webappFile = os.path.join(webappDir, webappWar)
-    copyfile(os.path.join(targetDir, appWar), webappFile + ".copy")
-    os.rename(webappFile + ".copy", webappFile)
-
-    logger.info('deployed %s to %s' % (appWar, webappFile))
+    return os.path.join(mvn_tgt, app_war)
 
 
-def deployMs(mods):
-    for module in ['core'] + mods:
-        deployMsModule(module)
+def get_project(app):
+    if app in MS_MODULES:
+        return os.path.join('le-microservice', app)
+    else:
+        return 'le-%s' % app
 
 
-def deployMsModule(module):
-    MSHOME = os.path.join(WSHOME, 'le-microservice')
-
-    moduleDir = os.path.join(MSHOME, module, 'target')
-    moduleWar = None
-    for fn in os.listdir(moduleDir):
-        if fn[-4:] == '.war':
-            moduleWar = fn
-            logger.info('found %s in %s' % (moduleWar, moduleDir))
-            break
-
-    if moduleWar is None:
-        raise IOError("Cannot find war file for module " + module)
-
-    webappWar = 'ROOT.war' if (module == 'core') else module + '.war'
-    webappDir = os.path.join(CATALINA_HOME, 'webapps', 'ms', webappWar)
-    copyfile(os.path.join(moduleDir, moduleWar), webappDir + ".copy")
-    os.rename(webappDir + ".copy", webappDir)
-    logger.info('deployed %s to %s' % (moduleWar, webappDir))
+def get_mgr_app(app):
+    if app == "oauth2":
+        mgr_app = 'oauth'
+    elif app in MS_MODULES:
+        mgr_app = 'ms'
+    else:
+        mgr_app = app
+    return mgr_app
 
 
-def deployMgrApp(app, wait=False):
-    webappName = 'oauth' if (app == 'oauth2') else app
-    webappDir = os.path.join(CATALINA_HOME, 'webapps', webappName)
-
-    if not os.path.isdir(webappDir):
-        os.makedirs(webappDir)
-
-    if not os.path.isdir(os.path.join(webappDir, 'manager')):
+def deploy_mgr_app(app, wait=False):
+    mgr_app = get_mgr_app(app)
+    mgr_app_dir = os.path.join(CATALINA_HOME, 'webapps', mgr_app)
+    if not os.path.isdir(mgr_app_dir):
+        os.makedirs(mgr_app_dir)
+    if not os.path.isdir(os.path.join(mgr_app_dir, 'manager')):
         if os.path.isdir(os.path.join(CATALINA_HOME, 'webapps', 'manager')):
-            copytree(os.path.join(CATALINA_HOME, 'webapps', 'manager'), os.path.join(webappDir, 'manager'))
+            copytree(os.path.join(CATALINA_HOME, 'webapps', 'manager'), os.path.join(mgr_app_dir, 'manager'))
             if wait:
                 for i in range(10):
                     logger.info("Wait %d sec for manager app to start ..." % (10 - i))
                     time.sleep(1)
 
 
-def undeployApp(app, modules):
+def undeploy_app(app):
     global APP_URL
 
-    deployMgrApp(app, wait=True)
+    deploy_mgr_app(app, wait=True)
 
     app_url = APP_URL[app]
 
@@ -217,14 +218,13 @@ def undeployApp(app, modules):
         r = tomcat.connect(mgr_url, 'admin', 'admin')
         if r.ok:
             logger.info("Connected!")
-        if app == 'microservice':
-            for module in modules:
-                undeployPath(tomcat, '/%s' % module)
+        if app in MS_MODULES and app != 'ms-core':
+            undeploy_path(tomcat, '/%s' % app)
         else:
-            undeployPath(tomcat, '/')
+            undeploy_path(tomcat, '/')
 
 
-def undeployPath(tomcat, path):
+def undeploy_path(tomcat, path):
     logger.info("Undeploying %s ..." % path)
     r = tomcat.undeploy(path)
     if r.ok:
@@ -233,7 +233,7 @@ def undeployPath(tomcat, path):
         logger.info(r.status_message)
 
 
-def printWars():
+def print_wars():
     logger.info('checking deployed wars ...')
     for dirName, subdirList, fileList in os.walk(CATALINA_HOME):
         for file in fileList:
@@ -241,37 +241,61 @@ def printWars():
                 logger.info('%s/%s' % (dirName, file))
 
 
-def parseApps(args):
+def run_tc():
+    proc = subprocess.Popen(['bash %s/le-dev/scripts/run-tomcat.sh' % os.environ['WSHOME']], shell=True)
+    if proc:
+        global tomcatPid
+        tomcatPid = proc.pid
+        atexit.register(kill_tc)
+
+
+def wait_tc():
+    global tomcatPid
+    proc = psutil.Process(tomcatPid)
+    proc.wait()
+
+
+def kill_tc():
+    global tomcatPid
+    try:
+        proc = psutil.Process(tomcatPid)
+    except psutil.NoSuchProcess:
+        return
+    childPids = proc.children(recursive=True)
+    for childPid in childPids:
+        os.kill(childPid.pid, signal.SIGKILL)
+    os.kill(tomcatPid, signal.SIGKILL)
+
+
+def parse_apps(args):
     if args.apps or args.modules:
         apps = args.apps.split(',') if args.apps else []
         modules = args.modules.split(',') if args.modules else []
+        apps = [ app for app in apps + modules if app != 'microservice' ]
     else:
         logger.info('using preset ' + args.preset)
+        if args.preset == 'cdl_pre_checkin':
+            logger.warning("WARNING: preset \"cdl_pre_checkin\" is deprecated, please use \"cdl\" instead.")
         apps = PRESETS[args.preset]['apps']
-        modules = PRESETS[args.preset]['modules']
-    if len(modules) > 0 and 'microservice' not in apps:
-        apps.append('microservice')
-
     logger.info('apps = %s' % apps)
-    logger.info('modules = %s' % modules)
-    return apps, modules
+    return apps
 
 
 def cleanup_cli(args):
-    cleanupWars()
-    printWars()
+    cleanup_wars()
+    print_wars()
 
 
 def check_cli(args):
-    printWars()
+    print_wars()
 
 
 def deploy_cli(args):
-    cleanupWars()
-    apps, modules = parseApps(args)
+    cleanup_wars()
+    apps = parse_apps(args)
     for app in apps:
-        deployApp(app, modules)
-    printWars()
+        deploy_app(app)
+    print_wars()
 
 
 def swap_cli(args):
@@ -280,23 +304,23 @@ def swap_cli(args):
         logger.warning("Cannot import tomcat manager, hot swap functionality is disabled.")
         return
 
-    apps, modules = parseApps(args)
+    apps = parse_apps(args)
     for app in apps:
-        undeployApp(app, modules)
+        undeploy_app(app)
 
     for i in range(5):
         logger.info('Sleep %d second ...' % (5 - i))
         time.sleep(1)
 
     for app in apps:
-        deployApp(app, modules)
+        deploy_app(app)
 
-    printWars()
+    print_wars()
 
 
 def run_cli(args):
-    runTc()
-    waitTc()
+    run_tc()
+    wait_tc()
 
 
 def parseCliArgs():
@@ -313,12 +337,10 @@ def parseCliArgs():
     parser1.add_argument('-a', dest='apps', type=str,
                          help='comma separated list of apps to be deployed. Available choices are ' + ', '.join(
                                  LE_APPS))
-    parser1.add_argument('-m', dest='modules', type=str,
-                         help='comma separated list of microservice modules to be deployed. core is implicitly included. Available choices are ' + ', '.join(
-                                 MS_MODULES))
+    parser1.add_argument('-m', dest='modules', type=str, help='DEPRECATED')
     parser1.add_argument('-p', dest='preset', type=str, default='lp',
                          help='preset apps/modules, choose from [' + ', '.join(
-                                 PRESETS) + '], default is lp. If -a or -m is specified, this opt is ignored.')
+                                 PRESETS) + '], default is lp. If -a is specified, this opt is ignored.')
     parser1.set_defaults(func=deploy_cli)
 
     parser1 = commands.add_parser("run")
@@ -328,43 +350,15 @@ def parseCliArgs():
     parser1.add_argument('-a', dest='apps', type=str,
                          help='comma separated list of apps to be deployed. Available choices are ' + ', '.join(
                                  LE_APPS))
-    parser1.add_argument('-m', dest='modules', type=str,
-                         help='comma separated list of microservice modules to be deployed. core is implicitly included. Available choices are ' + ', '.join(
-                                 MS_MODULES))
+    parser1.add_argument('-m', dest='modules', type=str, help='DEPRECATED')
     parser1.add_argument('-p', dest='preset', type=str, default='lp',
                          help='preset apps/modules, choose from [' + ', '.join(
-                                 PRESETS) + '], default is lp. If -a or -m is specified, this opt is ignored.')
+                                 PRESETS) + '], default is lp. If -a is specified, this opt is ignored.')
     parser1.set_defaults(func=swap_cli)
 
     args = parser.parse_args()
 
     return args
-
-
-def runTc():
-    proc = subprocess.Popen(['bash %s/le-dev/scripts/run-tomcat.sh' % os.environ['WSHOME']], shell=True)
-    if proc:
-        global tomcatPid
-        tomcatPid = proc.pid
-        atexit.register(killTc)
-
-
-def waitTc():
-    global tomcatPid
-    proc = psutil.Process(tomcatPid)
-    proc.wait()
-
-
-def killTc():
-    global tomcatPid
-    try:
-        proc = psutil.Process(tomcatPid)
-    except psutil.NoSuchProcess:
-        return
-    childPids = proc.children(recursive=True)
-    for childPid in childPids:
-        os.kill(childPid.pid, signal.SIGKILL)
-    os.kill(tomcatPid, signal.SIGKILL)
 
 
 if __name__ == '__main__':
