354b6576abbf6772fb9b53922b1c1539acd1b1e8
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-12-14 21:35:29 +0000

    Review Request 17182 fix pls and security tests

--- le-pls/src/test/java/com/latticeengines/pls/metrics/impl/PlsMetricsAspectDeploymentTestNG.java
+++ le-pls/src/test/java/com/latticeengines/pls/metrics/impl/PlsMetricsAspectDeploymentTestNG.java
@@ -1,24 +1,5 @@
 package com.latticeengines.pls.metrics.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.commons.codec.digest.DigestUtils;
-import org.mockito.stubbing.Answer;
-import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-
-import com.latticeengines.domain.exposed.security.Ticket;
-import com.latticeengines.pls.controller.ModelSummaryResource;
-import com.latticeengines.pls.functionalframework.PlsDeploymentTestNGBase;
-import com.latticeengines.proxy.exposed.lp.ModelSummaryProxy;
-import com.latticeengines.security.exposed.globalauth.GlobalAuthenticationService;
-import com.latticeengines.testframework.exposed.utils.TestFrameworkUtils;
-
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doAnswer;
@@ -28,18 +9,38 @@ import static org.mockito.Mockito.verify;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
 
-public class PlsMetricsAspectDeploymentTestNG extends PlsDeploymentTestNGBase {
+import java.util.ArrayList;
+import java.util.List;
 
-    @Autowired
+import javax.inject.Inject;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.mockito.stubbing.Answer;
+import org.slf4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.latticeengines.domain.exposed.security.Ticket;
+import com.latticeengines.pls.controller.ModelSummaryResource;
+import com.latticeengines.pls.functionalframework.PlsFunctionalTestNGBase;
+import com.latticeengines.proxy.exposed.lp.ModelSummaryProxy;
+import com.latticeengines.security.exposed.globalauth.GlobalAuthenticationService;
+import com.latticeengines.testframework.exposed.utils.TestFrameworkUtils;
+
+public class PlsMetricsAspectDeploymentTestNG extends PlsFunctionalTestNGBase {
+
+    @Inject
     private ModelSummaryResource modelSummaryResource;
 
-    @Autowired
+    @Inject
     private GlobalAuthenticationService globalAuthenticationService;
 
     private Logger origLog;
     private ModelSummaryProxy origProxy;
 
-    @BeforeClass(groups = "deployment")
+    @BeforeClass(groups = "functional")
     public void beforeClass() throws Exception {
         origLog = PlsMetricsAspectImpl.log;
         origProxy = modelSummaryResource.getModelSummaryProxy();
@@ -47,13 +48,13 @@ public class PlsMetricsAspectDeploymentTestNG extends PlsDeploymentTestNGBase {
         super.setup();
     }
 
-    @AfterClass(groups = "deployment")
+    @AfterClass(groups = "functional")
     public void afterClass() {
         PlsMetricsAspectImpl.log = origLog;
         modelSummaryResource.setModelSummaryProxy(origProxy);
     }
 
-    @Test(groups = "deployment")
+    @Test(groups = "functional")
     public void logMetrics() {
         Logger newLog = mock(Logger.class);
         PlsMetricsAspectImpl.log = newLog;
@@ -65,21 +66,21 @@ public class PlsMetricsAspectDeploymentTestNG extends PlsDeploymentTestNGBase {
             Object[] params = invocation.getArguments();
             logs.add((String) params[0]);
             return logs;
-        }).when(newLog).info(any());
+        }).when(newLog).debug(any());
 
         setupSecurityContext(mainTestTenant, testUser);
         modelSummaryResource.setModelSummaryProxy(mock(ModelSummaryProxy.class));
 
         modelSummaryResource.delete("1");
-        verify(newLog, times(1)).info(anyString());
+        verify(newLog, times(1)).debug(anyString());
         Assert.assertTrue(logs.get(0).contains("Metrics for API=ModelSummaryResource.delete(..) ElapsedTime="));
         Assert.assertTrue(logs.get(0).contains("Track Id="));
         Assert.assertTrue(logs.get(0).contains("User=" + testUser));
 
         modelSummaryResource.getModelSummaries(null);
-        verify(newLog, times(2)).info(anyString());
+        verify(newLog, times(2)).debug(anyString());
 
-        verify(newLog, times(2)).info(anyString());
+        verify(newLog, times(2)).debug(anyString());
 
         String passwd = DigestUtils.sha256Hex(TestFrameworkUtils.GENERAL_PASSWORD);
         Ticket ticket = globalAuthenticationService.authenticateUser(testUser, passwd);
--- le-security/src/test/java/com/latticeengines/security/exposed/globalauth/impl/GlobalTenantManagementServiceImplTestNG.java
+++ le-security/src/test/java/com/latticeengines/security/exposed/globalauth/impl/GlobalTenantManagementServiceImplTestNG.java
@@ -2,24 +2,26 @@ package com.latticeengines.security.exposed.globalauth.impl;
 
 import static org.testng.Assert.assertTrue;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.inject.Inject;
+
 import org.testng.annotations.Test;
 
+import com.latticeengines.common.exposed.util.NamingUtils;
 import com.latticeengines.domain.exposed.security.Tenant;
 import com.latticeengines.security.exposed.globalauth.GlobalTenantManagementService;
 import com.latticeengines.security.functionalframework.SecurityFunctionalTestNGBase;
 
 public class GlobalTenantManagementServiceImplTestNG extends SecurityFunctionalTestNGBase {
-    
-    @Autowired
+
+    @Inject
     private GlobalTenantManagementService globalTenantManagementService;
 
     @Test(groups = "functional")
     public void registerThenDiscardTenant() {
         Tenant newTenant = new Tenant();
-        newTenant.setId("TEST_TENANT_NEW");
-        newTenant.setName("TEST_TENANT_NEW");
-        
+        newTenant.setId(NamingUtils.timestamp("TestTenant"));
+        newTenant.setName("Test Tenant");
+
         assertTrue(globalTenantManagementService.registerTenant(newTenant));
         assertTrue(globalTenantManagementService.discardTenant(newTenant));
     }
