40e331c8baddd0fefdd4cb5601b47fab43a417ae
JLazarus <JLazarus@479f5059-62a3-4c39-9342-9852e3a68435>
2018-12-14 20:07:51 +0000

    Review Request 0000 - forgot to include this file

---
+++ le-ui/projects/common/components/datacloud/attributes/attributes.redux.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const FETCH_ATTRIBUTES = "FETCH_ATTRIBUTES";
+const CLEAR_ATTRIBUTES = "CLEAR_ATTRIBUTES";
+const initialState = { items: [] };
+
+export const actions = {
+    get: () => dispatch => {
+        axios("https://jsonplaceholder.typicode.com/posts").then(response => {
+            console.log(response);
+            dispatch({
+                type: FETCH_ATTRIBUTES,
+                payload: response.data
+            });
+        });
+    },
+    clear: () => dispatch => {
+        return dispatch({
+            type: CLEAR_ATTRIBUTES
+        });
+    }
+};
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_ATTRIBUTES:
+            return {
+                ...state,
+                items: action.payload
+            };
+        case CLEAR_ATTRIBUTES:
+            return {
+                ...state,
+                items: []
+            };
+        default:
+            return state;
+    }
+};
