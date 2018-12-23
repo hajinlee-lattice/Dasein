d1a3dd949ce308f586795c545578d040e406e435
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2018-12-23 17:06:30 +0000

    Review Request 17214 minor change in run-tomcat.sh

--- le-dev/scripts/run-tomcat.sh
+++ le-dev/scripts/run-tomcat.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-if [ -z "${LE_ENVIRONMENT}" ]; then
+if [[ -z "${LE_ENVIRONMENT}" ]]; then
     export LE_ENVIRONMENT=dev
 fi
 
-if [ -z "${LE_CLIENT_ADDRESS}" ]; then
+if [[ -z "${LE_CLIENT_ADDRESS}" ]]; then
     export LE_CLIENT_ADDRESS=localhost
 fi
 
@@ -12,8 +12,13 @@ export LE_PROPDIR=$WSHOME/le-config/conf/env/${LE_ENVIRONMENT}
 echo "Using LE_PROPDIR=${LE_PROPDIR}"
 echo "Using LE_CLIENT_ADDRESS=${LE_CLIENT_ADDRESS}"
 
-if [ ! -z "${J11_HOME}" ]; then
+if [[ ! -z "${J11_HOME}" ]]; then
     export JAVA_HOME=${J11_HOME}
+fi
+
+if [[ ! -z $(java -version 2>&1 |  grep "11.0") ]]; then
+    echo "Java version: $(java -version 2>&1), enabling ZGC"
+    export JAVA_TOOL_OPTIONS="${JAVA_TOOL_OPTIONS} -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -Xlog:gc:/var/log/ledp/gc.log"
 fi
 
 export JAVA_OPTS="-Xmx4g -XX:ReservedCodeCacheSize=1g"
@@ -26,20 +31,20 @@ export JAVA_OPTS="${JAVA_OPTS} -Dcom.latticeengines.registerBootstrappers=true"
 export JAVA_OPTS="${JAVA_OPTS} -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=1098"
 export JAVA_OPTS="${JAVA_OPTS} -Dio.lettuce.core.topology.sort=RANDOMIZE"
 
-if [ "${ENABLE_JACOCO}" == "true" ]; then
+if [[ "${ENABLE_JACOCO}" == "true" ]]; then
     JACOCO_DEST_FILE="${WSHOME}/jacoco/tomcat.exec"
     JACOCO_AGENT_FILE="${WSHOME}/le-dev/jacocoagent.jar"
     export JAVA_OPTS="${JAVA_OPTS} -javaagent:${JACOCO_AGENT_FILE}=includes=com.latticeengines.*,destfile=${JACOCO_DEST_FILE},append=true,jmx=true"
 fi
 
-export CATALINA_CLASSPATH=$CLASSPATH:$TEZ_CONF_DIR:$HADOOP_HOME/etc/hadoop:$HADOOP_HOME/share/hadoop/common
+export CATALINA_CLASSPATH=${CLASSPATH}:${TEZ_CONF_DIR}:${HADOOP_HOME}/etc/hadoop:${HADOOP_HOME}/share/hadoop/common
 
-if [ $# -eq 0 ]; then
+if [[ $# -eq 0 ]]; then
     echo "Starting tomcat normally..."
-    $CATALINA_HOME/bin/catalina.sh run
-elif [ $1 == "daemon" ]; then
+    ${CATALINA_HOME}/bin/catalina.sh run
+elif [[ $1 == "daemon" ]]; then
     echo "Starting tomcat as daemon..."
-    $CATALINA_HOME/bin/startup.sh
+    ${CATALINA_HOME}/bin/startup.sh
 fi
 
 
