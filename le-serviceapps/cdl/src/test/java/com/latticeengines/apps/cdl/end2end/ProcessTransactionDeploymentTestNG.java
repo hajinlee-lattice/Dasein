7c8f43f4fd8958ec491b0c58ffe4a7fe4fc97ade
ygao <ygao@479f5059-62a3-4c39-9342-9852e3a68435>
2019-01-04 00:13:09 +0000

    Review Request 17356: Add test cases for Date type attributes

--- le-serviceapps/cdl/src/test/java/com/latticeengines/apps/cdl/end2end/ProcessTransactionDeploymentTestNG.java
+++ le-serviceapps/cdl/src/test/java/com/latticeengines/apps/cdl/end2end/ProcessTransactionDeploymentTestNG.java
@@ -4,16 +4,25 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.latticeengines.domain.exposed.datacloud.statistics.Bucket;
 import com.latticeengines.domain.exposed.metadata.ColumnMetadata;
 import com.latticeengines.domain.exposed.pls.RatingEngine;
+import com.latticeengines.domain.exposed.query.BucketRestriction;
 import com.latticeengines.domain.exposed.query.BusinessEntity;
+import com.latticeengines.domain.exposed.query.Restriction;
+import com.latticeengines.domain.exposed.query.TimeFilter;
+import com.latticeengines.domain.exposed.query.frontend.FrontEndQuery;
+import com.latticeengines.domain.exposed.query.frontend.FrontEndRestriction;
 import com.latticeengines.domain.exposed.serviceapps.cdl.ReportConstants;
 import com.latticeengines.domain.exposed.workflow.ReportPurpose;
+import com.latticeengines.proxy.exposed.objectapi.EntityProxy;
 
 /**
  * Process Transaction imports after ProcessAccountDeploymentTestNG
@@ -24,12 +33,16 @@ public class ProcessTransactionDeploymentTestNG extends CDLEnd2EndDeploymentTest
 
     public static final String CHECK_POINT = "process2";
 
+    @Inject
+    private EntityProxy entityProxy;
+
     private RatingEngine ratingEngine;
 
     @Test(groups = "end2end")
     public void runTest() throws Exception {
         resumeCheckpoint(ProcessAccountDeploymentTestNG.CHECK_POINT);
         verifyNumAttrsInAccount();
+        verifyDateTypeAttrs();
         new Thread(() -> {
             createTestSegment1();
             createTestSegment2();
@@ -57,12 +70,14 @@ public class ProcessTransactionDeploymentTestNG extends CDLEnd2EndDeploymentTest
         runCommonPAVerifications();
 
         Map<String, Object> transactionReport = new HashMap<>();
-        transactionReport.put(ReportPurpose.CONSOLIDATE_RECORDS_SUMMARY.name() + "_" + ReportConstants.NEW, TRANSACTION_1);
+        transactionReport.put(ReportPurpose.CONSOLIDATE_RECORDS_SUMMARY.name() + "_" + ReportConstants.NEW,
+                TRANSACTION_1);
         transactionReport.put(ReportPurpose.CONSOLIDATE_RECORDS_SUMMARY.name() + "_" + ReportConstants.DELETE, 0L);
         transactionReport.put(ReportPurpose.ENTITY_STATS_SUMMARY.name() + "_" + ReportConstants.TOTAL, TRANSACTION_1);
 
         Map<String, Object> purchaseHistoryReport = new HashMap<>();
-        purchaseHistoryReport.put(ReportPurpose.ENTITY_STATS_SUMMARY.name() + "_" + ReportConstants.TOTAL, PURCHASE_HISTORY_1);
+        purchaseHistoryReport.put(ReportPurpose.ENTITY_STATS_SUMMARY.name() + "_" + ReportConstants.TOTAL,
+                PURCHASE_HISTORY_1);
 
         Map<BusinessEntity, Map<String, Object>> expectedReport = new HashMap<>();
         expectedReport.put(BusinessEntity.Transaction, transactionReport);
@@ -77,26 +92,52 @@ public class ProcessTransactionDeploymentTestNG extends CDLEnd2EndDeploymentTest
         long numProducts = 99;
         long numTransactions = 1000;
 
-//        Assert.assertEquals(countTableRole(BusinessEntity.Account.getBatchStore()), numAccounts);
-//        Assert.assertEquals(countTableRole(BusinessEntity.Contact.getBatchStore()), numContacts);
-//        Assert.assertEquals(countTableRole(BusinessEntity.Product.getBatchStore()), numProducts);
-//        Assert.assertEquals(countTableRole(TableRoleInCollection.ConsolidatedRawTransaction), numTransactions);
-//
-//        Assert.assertEquals(countInRedshift(BusinessEntity.Account), numAccounts);
-//        Assert.assertEquals(countInRedshift(BusinessEntity.Contact), numContacts);
+        // Assert.assertEquals(countTableRole(BusinessEntity.Account.getBatchStore()),
+        // numAccounts);
+        // Assert.assertEquals(countTableRole(BusinessEntity.Contact.getBatchStore()),
+        // numContacts);
+        // Assert.assertEquals(countTableRole(BusinessEntity.Product.getBatchStore()),
+        // numProducts);
+        // Assert.assertEquals(countTableRole(TableRoleInCollection.ConsolidatedRawTransaction),
+        // numTransactions);
+        //
+        // Assert.assertEquals(countInRedshift(BusinessEntity.Account),
+        // numAccounts);
+        // Assert.assertEquals(countInRedshift(BusinessEntity.Contact),
+        // numContacts);
 
-//        Map<BusinessEntity, Long> segment1Counts = ImmutableMap.of( //
-//                BusinessEntity.Account, SEGMENT_1_ACCOUNT_1, BusinessEntity.Contact, SEGMENT_1_CONTACT_1);
-//        verifyTestSegment1Counts(segment1Counts);
-//        Map<BusinessEntity, Long> segment2Counts = ImmutableMap.of( //
-//                BusinessEntity.Account, SEGMENT_2_ACCOUNT_1, BusinessEntity.Contact, SEGMENT_2_CONTACT_1);
-//        verifyTestSegment2Counts(segment2Counts);
+        // Map<BusinessEntity, Long> segment1Counts = ImmutableMap.of( //
+        // BusinessEntity.Account, SEGMENT_1_ACCOUNT_1, BusinessEntity.Contact,
+        // SEGMENT_1_CONTACT_1);
+        // verifyTestSegment1Counts(segment1Counts);
+        // Map<BusinessEntity, Long> segment2Counts = ImmutableMap.of( //
+        // BusinessEntity.Account, SEGMENT_2_ACCOUNT_1, BusinessEntity.Contact,
+        // SEGMENT_2_CONTACT_1);
+        // verifyTestSegment2Counts(segment2Counts);
     }
 
     private void verifyNumAttrsInAccount() {
-        String tableName = dataCollectionProxy.getTableName(mainCustomerSpace, BusinessEntity.Account.getServingStore());
+        String tableName = dataCollectionProxy.getTableName(mainCustomerSpace,
+                BusinessEntity.Account.getServingStore());
         List<ColumnMetadata> cms = metadataProxy.getTableColumns(mainCustomerSpace, tableName);
         Assert.assertTrue(cms.size() < 20000, "Should not have more than 20000 account attributes");
+    }
+
+    private void verifyDateTypeAttrs() {
+        FrontEndQuery query = new FrontEndQuery();
+        query.setMainEntity(BusinessEntity.Account);
+        Bucket bkt = Bucket.dateBkt(TimeFilter.ever());
+        Restriction restriction = new BucketRestriction(BusinessEntity.Account, "user_Test_Date", bkt);
+        query.setAccountRestriction(new FrontEndRestriction(restriction));
+        Long count = entityProxy.getCount(mainCustomerSpace, query);
+        Assert.assertEquals(count, ACCOUNT_1);
+
+        bkt = Bucket.dateBkt(TimeFilter.isEmpty());
+        restriction = new BucketRestriction(BusinessEntity.Account, "user_Test_Date", bkt);
+        query.setAccountRestriction(new FrontEndRestriction(restriction));
+        count = entityProxy.getCount(mainCustomerSpace, query);
+        Assert.assertEquals(count.longValue(), 0);
+        log.info("verify date done");
     }
 
 }
