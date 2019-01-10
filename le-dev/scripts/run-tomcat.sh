684f41a33ebe7070f4739e46c0c1205ba21ad3b2
ysong <ysong@479f5059-62a3-4c39-9342-9852e3a68435>
2019-01-10 01:42:38 +0000

    Review Request 17410 change to tomcat start script

--- le-dev/scripts/run-tomcat.sh
+++ le-dev/scripts/run-tomcat.sh
@@ -16,20 +16,20 @@ if [[ -n "${J11_HOME}" ]]; then
     export JAVA_HOME=${J11_HOME}
 fi
 
-export JAVA_OPTS="-server -Xmx4g -XX:ReservedCodeCacheSize=512m"
-export JAVA_OPTS="${JAVA_OPTS} -Djava.net.preferIPv4Stack=true"
-export JAVA_OPTS="${JAVA_OPTS} -Dsqoop.throwOnError=true"
-export JAVA_OPTS="${JAVA_OPTS} -Djava.library.path=${CATALINA_HOME}/lib"
-export JAVA_OPTS="${JAVA_OPTS} -Djavax.net.ssl.trustStore=/etc/ledp/tls/cacerts"
-export JAVA_OPTS="${JAVA_OPTS} -Dcom.latticeengines.registerBootstrappers=true"
-export JAVA_OPTS="${JAVA_OPTS} -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=1098"
-export JAVA_OPTS="${JAVA_OPTS} -Dio.lettuce.core.topology.sort=RANDOMIZE"
+export CATALINA_OPTS="-server -Xmx4g -XX:ReservedCodeCacheSize=512m"
+export CATALINA_OPTS="${CATALINA_OPTS} -Djava.net.preferIPv4Stack=true"
+export CATALINA_OPTS="${CATALINA_OPTS} -Dsqoop.throwOnError=true"
+export CATALINA_OPTS="${CATALINA_OPTS} -Djava.library.path=${CATALINA_HOME}/lib"
+export CATALINA_OPTS="${CATALINA_OPTS} -Djavax.net.ssl.trustStore=/etc/ledp/tls/cacerts"
+export CATALINA_OPTS="${CATALINA_OPTS} -Dcom.latticeengines.registerBootstrappers=true"
+export CATALINA_OPTS="${CATALINA_OPTS} -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=1098"
+export CATALINA_OPTS="${CATALINA_OPTS} -Dio.lettuce.core.topology.sort=RANDOMIZE"
 
 export JAVA_TOOL_OPTIONS="${JAVA_TOOL_OPTIONS} -XX:+UseNUMA"
 if [[ -n $(java -version 2>&1 |  grep "11.0") ]]; then
     echo "Java version: $(java -version 2>&1)"
     if [[ -n "${JPDA_PORT}" ]]; then
-        export JAVA_OPTS="${JAVA_OPTS} -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:4001"
+        export CATALINA_OPTS="${CATALINA_OPTS} -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:4001"
         export JAVA_TOOL_OPTIONS="${JAVA_TOOL_OPTIONS} -XX:+UseParallelGC"
     else
         export JAVA_TOOL_OPTIONS="${JAVA_TOOL_OPTIONS} -XX:+UnlockExperimentalVMOptions"
@@ -38,7 +38,7 @@ if [[ -n $(java -version 2>&1 |  grep "11.0") ]]; then
     fi
 else
     if [[ -n "${JPDA_PORT}" ]]; then
-        export JAVA_OPTS="${JAVA_OPTS} -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=4001"
+        export CATALINA_OPTS="${CATALINA_OPTS} -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=4001"
     else
         export JAVA_TOOL_OPTIONS="${JAVA_TOOL_OPTIONS} -XX:+UseG1GC"
     fi
@@ -47,7 +47,7 @@ fi
 if [[ "${ENABLE_JACOCO}" == "true" ]]; then
     JACOCO_DEST_FILE="${WSHOME}/jacoco/tomcat.exec"
     JACOCO_AGENT_FILE="${WSHOME}/le-dev/jacocoagent-0.8.2.jar"
-    export JAVA_OPTS="${JAVA_OPTS} -javaagent:${JACOCO_AGENT_FILE}=includes=com.latticeengines.*,destfile=${JACOCO_DEST_FILE},append=true,jmx=true"
+    export CATALINA_OPTS="${CATALINA_OPTS} -javaagent:${JACOCO_AGENT_FILE}=includes=com.latticeengines.*,destfile=${JACOCO_DEST_FILE},append=true,jmx=true"
 fi
 
 export CATALINA_CLASSPATH=${CLASSPATH}:${TEZ_CONF_DIR}:${HADOOP_HOME}/etc/hadoop:${HADOOP_HOME}/share/hadoop/common
