1acd480e97755ae614bef1d4e497e0a20ddd67d1
Yintao Song <ysong@lattice-engines.com>
2019-01-13 20:29:49 +0800

    fix gitignore on ddl sql scripts

--- le-dev/awsmigration/dms/target_sql/setupdb_globalauth_migration.sql
+++
@@ -1,9 +0,0 @@
-DROP SCHEMA IF EXISTS GlobalAuthentication;
-
-CREATE SCHEMA IF NOT EXISTS GlobalAuthentication;
-
-GRANT ALL ON GlobalAuthentication.* TO root@localhost;
-
-USE `GlobalAuthentication`;
-
-source ddl_globalauthentication_mysql5innodb.sql
--- le-dev/awsmigration/dms/target_sql/setupdb_pls_multitenant_migration.sql
+++
@@ -1,9 +0,0 @@
-DROP SCHEMA IF EXISTS PLS_MultiTenant;
-
-CREATE SCHEMA IF NOT EXISTS PLS_MultiTenant;
-
-GRANT ALL ON PLS_MultiTenant.* TO root@localhost;
-
-USE `PLS_MultiTenant`;
-
-source ddl_pls_multitenant_mysql5innodb.sql
