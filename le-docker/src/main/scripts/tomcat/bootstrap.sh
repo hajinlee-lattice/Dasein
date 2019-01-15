6c9d533752ce1204947857bdacf86d8188b4b81f
Yintao Song <ysong@lattice-engines.com>
2019-01-15 15:20:49 +0800

    some enhance to docker scripts

--- le-docker/src/main/scripts/tomcat/bootstrap.sh
+++ le-docker/src/main/scripts/tomcat/bootstrap.sh
@@ -11,32 +11,32 @@ SERVICE="lp"
 source ../functions.sh
 # teardown_simple_service ${SERVICE} ${CLUSTER}
 
-docker run -d --net host \
-    --name ${CLUSTER}_${SERVICE} \
-    -h ${HOSTNAME} \
-    -e LE_ENVIRONMENT=dev \
-    -e LE_STACK=${LE_STACK} \
-    -e ENABLE_JACOCO=false \
-    -e CATALINA_OPTS="-Xmx2g -XX:ReservedCodeCacheSize=512m" \
-    -v ${WSHOME}/le-config/conf/env/dev/latticeengines.properties:/etc/ledp/latticeengines.properties \
-    -v ${WSHOME}/le-docker/src/main/scripts/tomcat/jacoco:/mnt/efs/jacoco:rw \
-    -v ${WSHOME}/le-docker/src/main/scripts/tomcat/logs:/opt/apache-tomcat-8.5.15/logs:rw \
-    -l ${SERVICE}.group=${CLUSTER} \
-    -l cluster=${CLUSTER} \
-    -p 8080:8080 \
-    -p 8443:8443 \
-    latticeengines/${SERVICE}
-
-#docker run -d \
-#    --name ${CLUSTER} \
-#    -h ${CLUSTER}-tomcat \
+#docker run -d --net host \
+#    --name ${CLUSTER}_${SERVICE} \
+#    -h ${HOSTNAME} \
+#    -e LE_ENVIRONMENT=dev \
+#    -e LE_STACK=${LE_STACK} \
+#    -e ENABLE_JACOCO=false \
+#    -e CATALINA_OPTS="-Xmx2g -XX:ReservedCodeCacheSize=512m" \
+#    -v ${WSHOME}/le-config/conf/env/dev/latticeengines.properties:/etc/ledp/latticeengines.properties \
+#    -v ${WSHOME}/le-docker/src/main/scripts/tomcat/jacoco:/mnt/efs/jacoco:rw \
+#    -v ${WSHOME}/le-docker/src/main/scripts/tomcat/logs:/opt/apache-tomcat-8.5.15/logs:rw \
 #    -l ${SERVICE}.group=${CLUSTER} \
 #    -l cluster=${CLUSTER} \
-#    -p 80:8080 \
-#    -p 443:8443 \
-#    -p 1099:1099 \
-#    -p 10001:10001 \
-#    -p 10002:10002 \
-#    latticeengines/tomcatbase
+#    -p 8080:8080 \
+#    -p 8443:8443 \
+#    latticeengines/${SERVICE}
+
+docker run -d \
+    --name ${CLUSTER} \
+    -h ${CLUSTER}-tomcat \
+    -l ${SERVICE}.group=${CLUSTER} \
+    -l cluster=${CLUSTER} \
+    -p 80:8080 \
+    -p 443:8443 \
+    -p 1099:1099 \
+    -p 10001:10001 \
+    -p 10002:10002 \
+    latticeengines/tomcatbase
 
 docker_ps
