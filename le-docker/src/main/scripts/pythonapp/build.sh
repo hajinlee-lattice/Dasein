0a9b415b5aedd3c675dee680407d2629a8a37c5e
Yintao Song <ysong@lattice-engines.com>
2019-01-19 12:45:06 +0800

    remove unused docker scripts

--- le-docker/src/main/scripts/pythonapp/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker rmi -f $(docker images -a --filter "dangling=true" -q --no-trunc) 2> /dev/null
-
-source ../functions.sh
-
-build_docker latticeengines/pythonapp
-
-docker rmi -f $(docker images -a --filter "dangling=true" -q --no-trunc) 2> /dev/null
--- le-docker/src/main/scripts/workflowapp/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker rmi -f $(docker images -a --filter "dangling=true" -q --no-trunc) 2> /dev/null
-
-source ../functions.sh
-
-build_docker latticeengines/workflowapp
-
-docker rmi -f $(docker images -a --filter "dangling=true" -q --no-trunc) 2> /dev/null
