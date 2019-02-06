770837b9374494985cf315e7503a53bae2bc44e1
Dandan Zheng <dzheng@lattice-engines.com>
2019-02-06 14:53:08 -0800

    Some enhancement in DnB remote lookup

--- le-datacloud/match/src/main/java/com/latticeengines/datacloud/match/service/DnbMatchCommandService.java
+++ le-datacloud/match/src/main/java/com/latticeengines/datacloud/match/service/DnbMatchCommandService.java
@@ -1,13 +1,20 @@
 package com.latticeengines.datacloud.match.service;
 
+import java.util.List;
+
 import com.latticeengines.domain.exposed.datacloud.dnb.DnBBatchMatchContext;
 import com.latticeengines.domain.exposed.datacloud.manage.DnBMatchCommand;
 
 public interface DnbMatchCommandService {
+    void dnbMatchCommandCreate(DnBBatchMatchContext contexts);
 
-    void dnbMatchCommandCreate(DnBBatchMatchContext dnbMatchContext);
-    void dnbMatchCommandUpdate(DnBBatchMatchContext dnbMatchContext);
+    void dnbMatchCommandUpdate(DnBBatchMatchContext contexts);
+
+    void dnbMatchCommandUpdateStatus(List<DnBBatchMatchContext> contexts);
+
     void dnbMatchCommandDelete(DnBMatchCommand record);
+
     DnBMatchCommand findRecordByField(String field, Object value);
+
     void finalize(String rootOperationUid);
 }
