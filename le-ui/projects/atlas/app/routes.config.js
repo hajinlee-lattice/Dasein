61d18a40f05f060af60144356dfc846c93191714
Brian Rackle <brackle@lattice-engines.com>
2019-02-13 11:15:07 -0800

    reseting to b22e4bdeecbcfd2c3dc467c93c9e4538d4067be6

--- le-ui/projects/atlas/app/routes.config.js
+++ le-ui/projects/atlas/app/routes.config.js
@@ -287,28 +287,30 @@ export default function ($stateProvider, $urlRouterProvider, $locationProvider) 
                 viewingIteration: true
             },
             resolve: {
-                // ReviewData: function (
-                //     $q,
-                //     $stateParams,
-                //     Model,
-                //     ModelReviewStore,
-                //     DataCloudStore
-                // ) {
-                //     var deferred = $q.defer(),
-                //         modelId = $stateParams.modelId,
-                //         eventTableName =
-                //             Model.EventTableProvenance.EventTableName;
+                ReviewData: function (
+                    $q,
+                    $stateParams,
+                    Model,
+                    ModelReviewStore,
+                    DataCloudStore
+                ) {
+                    var deferred = $q.defer(),
+                        modelId = $stateParams.modelId;
 
-                //     console.log('### resolve ReviewData', Model, modelId, eventTableName);
-                //     ModelReviewStore.GetReviewData(
-                //         modelId
-                //     ).then(function (result) {
-                //         console.log('### ReviewData result', result);
-                //         deferred.resolve(result);
-                //     });
+                    ModelReviewStore.GetReviewData(
+                        modelId
+                    ).then(function (result) {
+                        console.log('### ReviewData result', result);
+                        var warnings = {};
+                        result.forEach(item => {
+                            warnings[item.name] = item;
+                        });
+                        DataCloudStore.setWarnings(warnings);
+                        deferred.resolve(result);
+                    });
 
-                //     return deferred.promise;
-                // },
+                    return deferred.promise;
+                },
                 Enrichments: function ($q, $stateParams, DataCloudStore, ApiHost) {
                     var deferred = $q.defer(),
                         ratingId = $stateParams['rating_id'],
