de49dd1f1b9f439b08da94aa4157317e6219463b
Yintao Song <ysong@lattice-engines.com>
2019-02-15 17:48:21 +0000

    upgrade properties file to tez-0.9.1

--- le-dev/scripts/setupenv_tez.sh
+++ le-dev/scripts/setupenv_tez.sh
@@ -5,7 +5,7 @@ BOOTSTRAP_MODE=$1
 if [[ "${BOOTSTRAP_MODE}" = "bootstrap" ]]; then
     ARTIFACT_DIR=$WSHOME/le-dev/artifacts
 
-    TEZ_VERSION=0.9.0
+    TEZ_VERSION=0.9.1
 
     if [[ ! -f "$ARTIFACT_DIR/tez-${TEZ_VERSION}.tar.gz" ]]; then
         wget --trust-server-names "https://s3.amazonaws.com/latticeengines-dev/artifacts/tez/${TEZ_VERSION}/tez-${TEZ_VERSION}.tar.gz" \
@@ -15,3 +15,6 @@ if [[ "${BOOTSTRAP_MODE}" = "bootstrap" ]]; then
     hdfs dfs -mkdir -p /apps/tez || true
     hdfs dfs -put -f $ARTIFACT_DIR/tez-${TEZ_VERSION}.tar.gz /apps/tez/tez-${TEZ_VERSION}.tar.gz
 fi
+
+cp $WSHOME/le-dev/hadoop/tez-site.xml $HADOOP_CONF_DIR
+cp $WSHOME/le-dev/hadoop/log4j2-tez.xml $HADOOP_CONF_DIR
