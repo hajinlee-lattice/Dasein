68ae8e9e029223dfdca5ff3db89769debc2cad01
Leonardo Gigli <lgigli@lattice-engines.com>
2019-02-19 16:15:01 -0800

    Fix for case externalSystemName== null

---
+++ le-ui/projects/atlas/app/connectors/systems.service.js
@@ -0,0 +1,42 @@
+export const MARKETO = 'Marketo';
+export const SALESFORCE = 'Salesforce';
+export const ELOQUA = 'Eloqua';
+const CRM = {
+    externalSystemType: 'CRM',
+    externalSystemName : SALESFORCE
+};
+const MAP = {
+    externalSystemType: 'MAP',
+    externalSystemName : ELOQUA
+};
+class SystemsService {
+    constructor() {
+        if (!SystemsService.instance) {
+            SystemsService.instance = this;
+        }
+
+        return SystemsService.instance;
+    }
+    cleanupLookupId(systemsList){
+        systemsList.forEach(system => {
+            if(system.externalSystemName == null){
+                switch(system.externalSystemType){
+                    case CRM.externalSystemType:
+                        system.externalSystemName = CRM.externalSystemName;
+                    break;
+                    case MAP.externalSystemType:
+                        system.externalSystemName = MAP.externalSystemName;
+                    break;
+                }
+            }
+        });
+
+    }
+    
+}
+
+const instance = new SystemsService();
+Object.freeze(instance);
+
+export default instance;
+
