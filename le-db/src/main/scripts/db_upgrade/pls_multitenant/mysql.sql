6409958a916526dc273085925358e955da2f0a75
Pramod Gavade <pgavade@lattice-engines.com>
2019-03-01 14:04:37 -0800

    PLS-11711 database update script

--- le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
+++ le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
@@ -1,5 +1,37 @@
 USE `PLS_MultiTenant`;
 
+/* PLS-11711 update the JSON values in PLS_MultiTenant.WORKFLOW_JOB */
+
+UPDATE PLS_MultiTenant.WORKFLOW_JOB
+SET `INPUT_CONTEXT` = JSON_REPLACE(
+		`INPUT_CONTEXT`,
+        '$.EXPORT_OBJECT_TYPE',
+        'Enriched Accounts'
+)
+WHERE TYPE = 'segmentExportWorkflow'
+AND JSON_EXTRACT (`INPUT_CONTEXT`, '$.EXPORT_OBJECT_TYPE') = 'Accounts';
+
+UPDATE PLS_MultiTenant.WORKFLOW_JOB
+SET `INPUT_CONTEXT` = JSON_REPLACE(
+		`INPUT_CONTEXT`,
+        '$.EXPORT_OBJECT_TYPE',
+        'Enriched Contacts (No Account Attributes)'
+)
+WHERE TYPE = 'segmentExportWorkflow'
+AND JSON_EXTRACT (`INPUT_CONTEXT`, '$.EXPORT_OBJECT_TYPE') = 'Contacts';
+
+UPDATE PLS_MultiTenant.WORKFLOW_JOB
+SET `INPUT_CONTEXT` = JSON_REPLACE(
+		`INPUT_CONTEXT`,
+        '$.EXPORT_OBJECT_TYPE',
+        'Enriched Contacts with Account Attributes'
+)
+WHERE TYPE = 'segmentExportWorkflow'
+AND JSON_EXTRACT (`INPUT_CONTEXT`, '$.EXPORT_OBJECT_TYPE') = 'Accounts and Contacts';
+
+/* End PLS-11711 JSON update */
+
+
 CREATE PROCEDURE `Update_CDL_BUSINESS_CALENDAR`()
   BEGIN
   END;
@@ -31,13 +63,13 @@ CREATE PROCEDURE `CreateDropBoxTable`()
 //
 DELIMITER;
 
-CREATE PROCEDURE `CreateDataIntegrationMonitoringTable`() 
+CREATE PROCEDURE `CreateDataIntegrationMonitoringTable`()
   BEGIN
   END;
 //
 DELIMITER;
 
-CREATE PROCEDURE `CreateDataIntegrationMessageTable`() 
+CREATE PROCEDURE `CreateDataIntegrationMessageTable`()
   BEGIN
   END;
 //
