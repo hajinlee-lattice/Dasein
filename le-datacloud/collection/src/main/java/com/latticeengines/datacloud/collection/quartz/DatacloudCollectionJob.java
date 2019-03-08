1169ab7ef96e13b37064691b6bd742726b9e6a63
xu.guo <xu.guo@lattice-engines.com>
2019-03-08 08:16:20 +0800

    pd-2484

--- le-datacloud/collection/src/main/java/com/latticeengines/datacloud/collection/quartz/DatacloudCollectionJob.java
+++ le-datacloud/collection/src/main/java/com/latticeengines/datacloud/collection/quartz/DatacloudCollectionJob.java
@@ -24,8 +24,6 @@ public class DatacloudCollectionJob implements QuartzJobBean {
 
                 collectionDBService.collect();
 
-                collectionDBService.consolidate();
-
                 return true;
 
             }
