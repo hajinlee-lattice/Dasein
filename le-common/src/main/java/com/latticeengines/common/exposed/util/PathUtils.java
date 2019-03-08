82d60706cf7c2a34e33185941657c8bc17af3fa9
Yintao Song <ysong@lattice-engines.com>
2019-03-08 14:59:42 +0000

    minor clean up of eai export

--- le-common/src/main/java/com/latticeengines/common/exposed/util/PathUtils.java
+++ le-common/src/main/java/com/latticeengines/common/exposed/util/PathUtils.java
@@ -5,4 +5,26 @@ public class PathUtils {
     public static String stripoutProtocol(String hdfsPath) {
         return hdfsPath.replaceFirst("[^:]*://[^/]*/", "/");
     }
+
+    public static String toDirWithoutTrailingSlash(String path) {
+        String dirPath = path;
+        if (path.endsWith("/")) {
+            dirPath = path.substring(0, path.lastIndexOf("/"));
+        } else if (path.contains("/")) {
+            String lastPart = path.substring(path.lastIndexOf("/"));
+            if (lastPart.contains(".")) {
+                dirPath = path.substring(0, path.lastIndexOf("/"));
+            }
+        }
+        return dirPath;
+    }
+
+    public static String toAvroGlob(String path) {
+        if (path.endsWith(".avro")) {
+            return path;
+        } else {
+            String glob = path.endsWith("/") ? path.substring(0, path.lastIndexOf("/")) : path;
+            return glob + "/*.avro";
+        }
+    }
 }
