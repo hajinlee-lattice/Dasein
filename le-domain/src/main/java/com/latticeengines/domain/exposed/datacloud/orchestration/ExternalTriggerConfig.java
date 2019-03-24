6c02250be02a9ad6dd97d90267fa71aa72ed7daa
Dandan Zheng <dzheng@lattice-engines.com>
2019-03-23 20:09:45 -0700

    Simplify OrchestrationConfig

--- le-domain/src/main/java/com/latticeengines/domain/exposed/datacloud/orchestration/ExternalTriggerConfig.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/datacloud/orchestration/ExternalTriggerConfig.java
@@ -1,39 +1,44 @@
 package com.latticeengines.domain.exposed.datacloud.orchestration;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.latticeengines.common.exposed.util.JsonUtils;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.NONE, getterVisibility = JsonAutoDetect.Visibility.NONE)
 public class ExternalTriggerConfig extends OrchestrationConfig {
+    @JsonProperty("Engine")
     private DataCloudEngine engine;
+
+    @JsonProperty("EngineName")
     private String engineName;
+
+    @JsonProperty("TriggerStrategy")
     private TriggerStrategy strategy;
 
-    @JsonProperty("Engine")
     public DataCloudEngine getEngine() {
         return engine;
     }
 
-    @JsonProperty("Engine")
     public void setEngine(DataCloudEngine engine) {
         this.engine = engine;
     }
 
-    @JsonProperty("EngineName")
     public String getEngineName() {
         return engineName;
     }
 
-    @JsonProperty("EngineName")
     public void setEngineName(String engineName) {
         this.engineName = engineName;
     }
 
-    @JsonProperty("TriggerStrategy")
     public TriggerStrategy getStrategy() {
         return strategy;
     }
 
-    @JsonProperty("TriggerStrategy")
     public void setStrategy(TriggerStrategy strategy) {
         this.strategy = strategy;
     }
--- le-domain/src/main/java/com/latticeengines/domain/exposed/datacloud/orchestration/PredefinedScheduleConfig.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/datacloud/orchestration/PredefinedScheduleConfig.java
@@ -1,17 +1,22 @@
 package com.latticeengines.domain.exposed.datacloud.orchestration;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.latticeengines.common.exposed.util.JsonUtils;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.NONE, getterVisibility = JsonAutoDetect.Visibility.NONE)
 public class PredefinedScheduleConfig extends OrchestrationConfig {
+    @JsonProperty("CronExpression")
     private String cronExpression;
 
-    @JsonProperty("CronExpression")
     public String getCronExpression() {
         return cronExpression;
     }
 
-    @JsonProperty("CronExpression")
     public void setCronExpression(String cronExpression) {
         this.cronExpression = cronExpression;
     }
