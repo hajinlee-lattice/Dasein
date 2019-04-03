094829f9fa84c6964f697a3aca663de6741e8b38
Lei Ming <lming@lattice-engines.com>
2019-04-03 14:31:22 -0700

    Performance tuning for AddStandardAttributes.java

--- le-serviceflows/core/src/main/java/com/latticeengines/serviceflows/workflow/transformation/AddStandardAttributes.java
+++ le-serviceflows/core/src/main/java/com/latticeengines/serviceflows/workflow/transformation/AddStandardAttributes.java
@@ -1,7 +1,10 @@
 package com.latticeengines.serviceflows.workflow.transformation;
 
+import java.util.Properties;
+
 import javax.inject.Inject;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -20,6 +23,18 @@ public class AddStandardAttributes extends RunDataFlow<AddStandardAttributesConf
     @Inject
     private MetadataProxy metadataProxy;
 
+    @Value("${pls.cdl.transform.cascading.partitions}")
+    protected int cascadingPartitions;
+
+    @Value("${pls.cdl.transform.tez.am.mem.gb}")
+    private int tezAmMemGb; // requested memory for application master
+
+    @Value("${pls.cdl.transform.tez.task.vcores}")
+    private int tezVCores;
+
+    @Value("${pls.cdl.transform.tez.task.mem.gb}")
+    private int tezMemGb;
+
     @Override
     public void onConfigurationInitialized() {
         AddStandardAttributesConfiguration configuration = getConfiguration();
@@ -30,6 +45,18 @@ public class AddStandardAttributes extends RunDataFlow<AddStandardAttributesConf
                 configuration.getSourceSchemaInterpretation());
         parameters.inputSkippedAttributeList = getListObjectFromContext(INPUT_SKIPPED_ATTRIBUTES_KEY, String.class);
         configuration.setDataFlowParams(parameters);
+        configuration.setJobProperties(initJobProperties());
+    }
+
+    protected Properties initJobProperties() {
+        Properties jobProperties = new Properties();
+        int partitions = cascadingPartitions;
+        jobProperties.put("mapreduce.job.reduces", String.valueOf(partitions * 4));
+        jobProperties.put("mapred.reduce.tasks", String.valueOf(partitions * 4));
+        jobProperties.put("tez.task.resource.cpu.vcores", String.valueOf(tezVCores * 2));
+        jobProperties.put("tez.task.resource.memory.mb", String.valueOf(tezMemGb * 1024 * 2));
+        jobProperties.put("tez.am.resource.memory.mb", String.valueOf(tezAmMemGb * 1024 * 2));
+        return jobProperties;
     }
 
     @Override
