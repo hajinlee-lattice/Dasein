a6023b0a46ab4e3f5c842ba5111afcdfff790e0a
Pramod Gavade <pgavade@lattice-engines.com>
2019-04-16 15:07:43 -0700

    PLS-11971 checkStyle issue

--- le-playmaker/src/main/java/com/latticeengines/playmaker/aspect/PlaymakerRateLimitingAspect.java
+++ le-playmaker/src/main/java/com/latticeengines/playmaker/aspect/PlaymakerRateLimitingAspect.java
@@ -7,8 +7,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.latticeengines.domain.exposed.camille.locks.RateLimitedAcquisition;
 import com.latticeengines.domain.exposed.exception.LedpCode;
