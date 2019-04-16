d34f5bdc7c9cb63b1c9c78ed4866d007b00e1a00
LiypLattice <yinpeng.li@lattice-engines.com>
2019-04-16 16:38:03 +0800

    DP-9850(Activity Object definition)

--- le-domain/src/main/java/com/latticeengines/domain/exposed/cdl/ActivityObject.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/cdl/ActivityObject.java
@@ -3,33 +3,15 @@ package com.latticeengines.domain.exposed.cdl;
 import java.util.Date;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.latticeengines.domain.exposed.pls.Action;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.NONE, getterVisibility = JsonAutoDetect.Visibility.NONE)
 public class ActivityObject {
 
-    @JsonProperty
     private String tenantName;
-
-    @JsonProperty
     private Boolean scheduleNow;
-
-    @JsonProperty
     private Date scheduleTime;
-
-    @JsonProperty
     private List<Action> actions;
-
-    @JsonProperty
     private Boolean dataCloudRefresh;
-
-    @JsonProperty
     private Date invokeTime;
 
     public String getTenantName() { return tenantName; }
