488f57a611966e734859acdbaf6e284f20d7ffeb
Yintao Song <ysong@lattice-engines.com>
2019-04-23 16:01:47 +0100

    remove python code that has already been migrated to leds

--- le-dataplatform/src/main/python/pipeline/replace_null_value.py
+++
@@ -1,8 +0,0 @@
-imputations = eval(open('imputations.txt', 'r').read())
-
-def transform(args, record):
-    column = args["column"]
-    value = record[column]
-    if value is None:
-        value = imputations[column]
-    return value
--- le-dataplatform/src/test/python/scripts/pytestrunner.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-source $ANACONDA_HOME/bin/activate $1
-py.test "${@:2}"
