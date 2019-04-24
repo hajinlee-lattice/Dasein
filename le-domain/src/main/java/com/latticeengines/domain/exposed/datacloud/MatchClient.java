a549e2cdeaea23a2e0fd9bcc2b68f40ade2fe554
Sayali Sakhalkar <ssakhalkar@lattice-engines.com>
2019-04-24 15:30:29 -0700

    MatchClient update

--- le-domain/src/main/java/com/latticeengines/domain/exposed/datacloud/MatchClient.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/datacloud/MatchClient.java
@@ -5,7 +5,7 @@ public enum MatchClient {
     // PD126 and PD144 are shut down. Redirect traffic from 126 and 144 to remaining match clients
     PD126("10.51.15.131", 1433, "PropDataMatchDB", "DLTransfer", "Q1nh4HIYGkg4OnQIEbEuiw=="), //
     PD131("10.51.15.131", 1433, "PropDataMatchDB", "DLTransfer", "Q1nh4HIYGkg4OnQIEbEuiw=="), //
-    PD144("10.51.15.130", 1433, "PropDataMatchDB", "DLTransfer", "Q1nh4HIYGkg4OnQIEbEuiw=="), //
+    PD144("10.51.15.131", 1433, "PropDataMatchDB", "DLTransfer", "Q1nh4HIYGkg4OnQIEbEuiw=="), //
     PD130("10.51.15.130", 1433, "PropDataMatchDB", "DLTransfer", "Q1nh4HIYGkg4OnQIEbEuiw=="); //
 
     final String host;
