57639554517ccca66b040a06f1f708510aa3a027
ben holland <bholland@lattice-engines.com>
2019-05-06 14:28:06 -0700

    PLS-12308 - too much work to lose, so checking it in so I cna fix unknown error happeing in routing since last update

--- le-ui/projects/atlas/app/playbook/content/overview/ratings.component.js
+++ le-ui/projects/atlas/app/playbook/content/overview/ratings.component.js
@@ -10,29 +10,37 @@ class RatingsComponent extends Component {
         super(props);
     }
 
-    getRatings() {
-        if(_.get(this.props, 'play.ratings')) {
-            var ratings = {};
-            for(var i in this.props.play.ratings) {
-                var rating = this.props.play.ratings[i];
-                ratings[i] = {
-                    label: rating.bucket,
-                    value: rating.count
+    barChart(play) {
+        var data = {};
+        if(play && play.ratingEngine && play.ratingEngine.bucketMetadata) {
+            var lifts = play.ratingEngine.bucketMetadata;
+            
+            lifts.forEach(function(lift) {
+                data[lift.bucket_name] = {
+                    label: lift.bucket_name,
+                    value: lift.lift,
+                    additionalInfo: [
+                        (lift.lift + '').replace(/(^.*?\d+)(\.\d)?.*/, '$1$2') + 'x',
+                        lift.num_leads
+                    ]
                 }
-            }
+            });
+
             return (
-                <LeBarchart data={ratings}></LeBarchart>
+                <LeBarchart data={data} type={'lift'} orientation={'vertical'} showAdditionalInfo={true} liftsPostfix={'x'} labels={['Rank',null,'Lift','Accounts']}></LeBarchart>
             );
         }
+        return (
+            <div>no lift data</div>
+        );
     }
 
     render() {
         return (
-            <LeVPanel className="main-panel panel">
-                <h2>Account Ratings</h2>
-                <h3>{this.props.play.ratingEngine.displayName}</h3>
-                <div>
-                    {this.getRatings()}
+            <LeVPanel className="main-panel panel ratings-chart">
+                <h2> Ratings</h2>
+                <div class="lift-chart">
+                    {this.barChart(this.props.play)}
                 </div>
             </LeVPanel>
         );
