9a5799041154d9336093141ec3299f70c60f73b7
ben holland <bholland@lattice-engines.com>
2019-05-06 15:16:14 -0700

    PLS-12308 - fixed overview being removed from loader

--- le-ui/projects/atlas/app/react/states.loader.js
+++ le-ui/projects/atlas/app/react/states.loader.js
@@ -1,6 +1,7 @@
 import states from 'atlas/import/templates/multiple/states';
 import s3filestates from 'atlas/import/s3files/s3files.states';
 import connectrosstates from 'atlas/connectors/connectors.states';
+import overviewstates from 'atlas/playbook/content/overview/overview.states';
 import statessingle from 'atlas/import/templates/states';
 // projects/atlas/app/import/templates/states.js
 const modulesStates = [];
@@ -13,6 +14,7 @@ function mergeStates(imported) {
 
 mergeStates(states);
 mergeStates(connectrosstates);
+mergeStates(overviewstates);
 mergeStates(s3filestates);
 mergeStates(statessingle);
 
