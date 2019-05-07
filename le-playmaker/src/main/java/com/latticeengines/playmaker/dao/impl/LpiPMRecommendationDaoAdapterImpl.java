d34d1c00fad5fd1492feeb75a84243d2e0073327
jadusumalli-lattice <jadusumalli@lattice-engines.com>
2019-05-07 16:22:19 -0700

    Fix for PLS-13345. Fix Checkstyle unused import errors

--- le-playmaker/src/main/java/com/latticeengines/playmaker/dao/impl/LpiPMRecommendationDaoAdapterImpl.java
+++ le-playmaker/src/main/java/com/latticeengines/playmaker/dao/impl/LpiPMRecommendationDaoAdapterImpl.java
@@ -1,7 +1,6 @@
 package com.latticeengines.playmaker.dao.impl;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
