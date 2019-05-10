a401f5fe17ac5957d933c769a24f8abe6dd5a4bb
Leonardo Gigli <lgigli@lattice-engines.com>
2019-05-10 09:21:54 -0700

    overview revert

--- le-ui/projects/atlas/app/playbook/content/overview/overview.states.js
+++ le-ui/projects/atlas/app/playbook/content/overview/overview.states.js
@@ -1,13 +1,23 @@
 import OverviewComponent from './overview.component';
 import SummaryContainer from 'atlas/import/templates/components/summary';
+
+import ReactRouter from 'atlas/react/router';
+
 const playbookOverview = {
-  parent: 'home',
-  name: "playbookoverview",
-  url: "/playbookoverview",
-  views: {
-    'summary@': SummaryContainer
-  }
+    parent: 'home',
+    name: "playbookOverview",
+    url: "/overview",
+    resolve: [{
+        token: 'PlaybookWizardStore',
+        resolveFn: () => {
+            return ReactRouter.getRouter()['ngservices'].PlaybookWizardStore;
+        }
+    }],
+    views: {
+        'summary@': SummaryContainer,
+        'main@': OverviewComponent
+    }
 };
 
-const overviewStates = [playbookOverview];
-export default overviewStates;
+const overviewstates = [playbookOverview];
+export default overviewstates;
