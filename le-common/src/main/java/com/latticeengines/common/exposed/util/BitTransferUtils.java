ad94a903e2d46a6aaf20703b320d1a0696b4c307
Joy Xiao <jxiao@lattice-engines.com>
2019-05-11 10:17:56 +0800

    PLS-13388: change format filesize logic

--- le-common/src/main/java/com/latticeengines/common/exposed/util/BitTransferUtils.java
+++ le-common/src/main/java/com/latticeengines/common/exposed/util/BitTransferUtils.java
@@ -2,27 +2,37 @@ package com.latticeengines.common.exposed.util;
 
 public class BitTransferUtils {
 
-    public static String formatSize(Long fileSize) {
-        if (fileSize < 1024) {
-            return fileSize + "B";
-        } else {
-            fileSize = fileSize / 1024;
+//    public static String formatSize(Long fileSize) {
+//        if (fileSize < 1024) {
+//            return fileSize + "B";
+//        } else {
+//            fileSize = fileSize / 1024;
+//        }
+//        if (fileSize < 1024) {
+//            fileSize = fileSize * 100;
+//            return (fileSize / 100) + "."
+//                    + (fileSize % 100) + "B";
+//        } else {
+//            fileSize = fileSize / 1024;
+//        }
+//        if (fileSize < 1024) {
+//            fileSize = fileSize * 100;
+//            return (fileSize / 100) + "."
+//                    + (fileSize % 100) + "MB";
+//        } else {
+//            fileSize = fileSize * 100 / 1024;
+//            return (fileSize / 100) + "."
+//                    + (fileSize % 100) + "GB";
+//        }
+//    }
+
+    public static String formatSize(long bytes) {
+        int unit = 1024;
+        if (bytes < unit) {
+            return bytes + " B";
         }
-        if (fileSize < 1024) {
-            fileSize = fileSize * 100;
-            return (fileSize / 100) + "."
-                    + (fileSize % 100) + "B";
-        } else {
-            fileSize = fileSize / 1024;
-        }
-        if (fileSize < 1024) {
-            fileSize = fileSize * 100;
-            return (fileSize / 100) + "."
-                    + (fileSize % 100) + "MB";
-        } else {
-            fileSize = fileSize * 100 / 1024;
-            return (fileSize / 100) + "."
-                    + (fileSize % 100) + "GB";
-        }
+        int exp = (int) (Math.log(bytes) / Math.log(unit));
+        String pre = String.valueOf(("KMGTPE").charAt(exp - 1));
+        return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
     }
 }
