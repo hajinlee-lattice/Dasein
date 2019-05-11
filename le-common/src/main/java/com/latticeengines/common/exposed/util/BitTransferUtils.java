03f637e43d4a7e3ed37abbf400966987fabb2a9c
Joy Xiao <jxiao@lattice-engines.com>
2019-05-11 10:19:25 +0800

    PLS-13388: change format filesize logic

--- le-common/src/main/java/com/latticeengines/common/exposed/util/BitTransferUtils.java
+++ le-common/src/main/java/com/latticeengines/common/exposed/util/BitTransferUtils.java
@@ -2,30 +2,6 @@ package com.latticeengines.common.exposed.util;
 
 public class BitTransferUtils {
 
-//    public static String formatSize(Long fileSize) {
-//        if (fileSize < 1024) {
-//            return fileSize + "B";
-//        } else {
-//            fileSize = fileSize / 1024;
-//        }
-//        if (fileSize < 1024) {
-//            fileSize = fileSize * 100;
-//            return (fileSize / 100) + "."
-//                    + (fileSize % 100) + "B";
-//        } else {
-//            fileSize = fileSize / 1024;
-//        }
-//        if (fileSize < 1024) {
-//            fileSize = fileSize * 100;
-//            return (fileSize / 100) + "."
-//                    + (fileSize % 100) + "MB";
-//        } else {
-//            fileSize = fileSize * 100 / 1024;
-//            return (fileSize / 100) + "."
-//                    + (fileSize % 100) + "GB";
-//        }
-//    }
-
     public static String formatSize(long bytes) {
         int unit = 1024;
         if (bytes < unit) {
