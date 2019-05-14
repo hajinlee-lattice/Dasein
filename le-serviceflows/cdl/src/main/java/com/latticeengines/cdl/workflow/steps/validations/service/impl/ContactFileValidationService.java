313847ee7fe5118857c8c1390ee96c19b82ce2a2
PenglongLiu <penglong.liu@lattice-engines.com>
2019-05-14 16:31:46 +0800

    modify logic

--- le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/workflow/steps/validations/service/impl/ContactFileValidationService.java
+++ le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/workflow/steps/validations/service/impl/ContactFileValidationService.java
@@ -92,8 +92,8 @@ public class ContactFileValidationService
                                     String lastName = getFieldValue(record, InterfaceName.LastName.name());
                                     String phone = getFieldValue(record, InterfaceName.PhoneNumber.name());
                                     if (StringUtils.isBlank(id) && StringUtils.isBlank(email)
-                                            && (StringUtils.isBlank(firstName) && StringUtils.isBlank(lastName)
-                                            && StringUtils.isBlank(phone))) {
+                                            && (StringUtils.isBlank(firstName) || StringUtils.isBlank(lastName)
+                                                    || StringUtils.isBlank(phone))) {
                                         String lineId = getFieldValue(record, InterfaceName.InternalId.name());
                                         String message = "The contact does not have sufficient information. The contact should have should have at least one of the three mentioned: 1. Contact ID  2. Email 3. First name + last name + phone";
                                         csvFilePrinter.printRecord(lineId, "", message);
