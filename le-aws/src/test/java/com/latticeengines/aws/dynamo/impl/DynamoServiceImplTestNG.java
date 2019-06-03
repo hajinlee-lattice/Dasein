3f3c9e4c61f5ae872a4ef7196e6c3f67a9d4df94
Dandan Zheng <dzheng@lattice-engines.com>
2019-06-03 16:34:47 -0700

    Add test Dybamo table autoscaling enable/disable

--- le-aws/src/test/java/com/latticeengines/aws/dynamo/impl/DynamoServiceImplTestNG.java
+++ le-aws/src/test/java/com/latticeengines/aws/dynamo/impl/DynamoServiceImplTestNG.java
@@ -1,31 +1,91 @@
 package com.latticeengines.aws.dynamo.impl;
 
-import javax.inject.Inject;
-
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import com.amazonaws.services.applicationautoscaling.model.DescribeScalableTargetsResult;
+import com.amazonaws.services.applicationautoscaling.model.DescribeScalingPoliciesResult;
+import com.amazonaws.services.applicationautoscaling.model.ScalableTarget;
+import com.amazonaws.services.applicationautoscaling.model.ScalingPolicy;
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
 import com.amazonaws.services.dynamodbv2.model.TableDescription;
-import com.latticeengines.aws.dynamo.DynamoService;
+import com.latticeengines.common.exposed.aws.DynamoOperation;
 
-@DirtiesContext
-@ContextConfiguration(locations = { "classpath:test-aws-context.xml" })
-public class DynamoServiceImplTestNG extends AbstractTestNGSpringContextTests {
 
-    @Inject
-    private DynamoService dynamoService;
+public class DynamoServiceImplTestNG extends DynamoFunctionalTestNGBase {
+
+    @Value("${datacloud.match.entity.staging.table}")
+    private String entityMatchTable;
+
+    @BeforeClass(groups = "functional")
+    private void setup() {
+        tableName = this.getClass().getSimpleName() + "_" + env + "_" + stack;
+        dynamoService.disableTableAutoScaling(tableName, DynamoOperation.Read);
+        dynamoService.disableTableAutoScaling(tableName, DynamoOperation.Write);
+        dynamoService.deleteTable(tableName);
+
+        long readCapacityUnits = 10;
+        long writeCapacityUnits = 10;
+        String partitionKeyType = ScalarAttributeType.S.name();
+        String sortKeyType = ScalarAttributeType.S.name();
+        dynamoService.createTable(tableName, readCapacityUnits, writeCapacityUnits, PARTITION_KEY, partitionKeyType,
+                SORT_KEY, sortKeyType);
+    }
+
+    @AfterClass(groups = "functional")
+    private void teardown() {
+        dynamoService.disableTableAutoScaling(tableName, DynamoOperation.Read);
+        dynamoService.disableTableAutoScaling(tableName, DynamoOperation.Write);
+        dynamoService.deleteTable(tableName);
+    }
 
     @Test(groups = "functional")
     public void testDescribeTable() {
-        TableDescription description = dynamoService.describeTable("EntitySeedLookup_Staging_20190305");
+        TableDescription description = dynamoService.describeTable(entityMatchTable);
         Assert.assertNotNull(description);
         Assert.assertFalse(description.getKeySchema().isEmpty());
-        description = dynamoService.describeTable("EntitySeedLookup_Serving_20190305");
+        description = dynamoService.describeTable(entityMatchTable);
         Assert.assertNotNull(description);
         Assert.assertFalse(description.getKeySchema().isEmpty());
     }
 
+    @Test(groups = "functional", dataProvider = "dynamoOperationProvider")
+    public void testAutoScaling(DynamoOperation operation) {
+        DescribeScalableTargetsResult target = dynamoService.describeScalableTargetsResult(tableName, operation);
+        Assert.assertTrue(CollectionUtils.isEmpty(target.getScalableTargets()));
+        DescribeScalingPoliciesResult policy = dynamoService.describeAutoScalingPolicy(tableName, operation);
+        Assert.assertTrue(CollectionUtils.isEmpty(policy.getScalingPolicies()));
+
+        dynamoService.enableTableAutoScaling(tableName, operation, 10, 50, 50);
+        target = dynamoService.describeScalableTargetsResult(tableName, operation);
+        Assert.assertTrue(CollectionUtils.isNotEmpty(target.getScalableTargets()));
+        Assert.assertEquals(target.getScalableTargets().size(), 1);
+        ScalableTarget tg = target.getScalableTargets().get(0);
+        Assert.assertEquals(tg.getMinCapacity(), (Integer) 10);
+        Assert.assertEquals(tg.getMaxCapacity(), (Integer) 50);
+        policy = dynamoService.describeAutoScalingPolicy(tableName, operation);
+        Assert.assertTrue(CollectionUtils.isNotEmpty(policy.getScalingPolicies()));
+        Assert.assertEquals(policy.getScalingPolicies().size(), 1);
+        ScalingPolicy sp = policy.getScalingPolicies().get(0);
+        Assert.assertEquals(sp.getTargetTrackingScalingPolicyConfiguration().getTargetValue(), 50.0);
+
+        dynamoService.disableTableAutoScaling(tableName, operation);
+        target = dynamoService.describeScalableTargetsResult(tableName, operation);
+        Assert.assertTrue(CollectionUtils.isEmpty(target.getScalableTargets()));
+        policy = dynamoService.describeAutoScalingPolicy(tableName, operation);
+        Assert.assertTrue(CollectionUtils.isEmpty(policy.getScalingPolicies()));
+    }
+
+    @DataProvider(name = "dynamoOperationProvider")
+    private Object[][] dynamoOperationProvider() {
+        return new Object[][] {
+                { DynamoOperation.Read }, //
+                { DynamoOperation.Write }, //
+        };
+}
 }
