f7efc252610aa4de56bb05747c70821d621873ea
Perry Liu <perry.liu@lattice-engines.com>
2019-06-19 13:36:23 -0700

    removed comments channel config, changes to account and contact limits

--- le-domain/src/main/java/com/latticeengines/domain/exposed/pls/cdl/channel/SalesforceChannelConfig.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/pls/cdl/channel/SalesforceChannelConfig.java
@@ -11,78 +11,8 @@ public class SalesforceChannelConfig implements ChannelConfig {
     @JsonProperty("accountLimit")
     private Long accountLimit;
 
-    // @JsonProperty("showNumAccountsLaunched")
-    // private Boolean showNumAccountsLaunched = Boolean.FALSE;
-    //
-    // @JsonProperty("showNumAccountsSupressed")
-    // private Boolean showNumAccountsSupressed = Boolean.FALSE;
-    //
-    // @JsonProperty("showNumAccountsInSegment")
-    // private Boolean showNumAccountsInSegment = Boolean.FALSE;
-    //
-    // @JsonProperty("showNumContactsLaunched")
-    // private Boolean showNumContactsLaunched = Boolean.FALSE;
-    //
-    // @JsonProperty("showNumContactsSupressed")
-    // private Boolean showNumContactsSupressed = Boolean.FALSE;
-    //
-    // @JsonProperty("showNumContactsInSegment")
-    // private Boolean showNumContactsInSegment = Boolean.FALSE;
-
     @JsonProperty("supressAccountWithoutAccountId")
     private Boolean supressAccountWithoutAccountId = Boolean.FALSE;
-
-    // public Boolean isShowNumAccountsLaunched() {
-    // return showNumAccountsLaunched;
-    // }
-    //
-    // public void setShowNumAccountsLaunched(boolean showNumAccountsLaunched) {
-    // this.showNumAccountsLaunched = showNumAccountsLaunched;
-    // }
-    //
-    // public Boolean isShowNumAccountsSupressed() {
-    // return showNumAccountsSupressed;
-    // }
-    //
-    // public void setShowNumAccountsSupressed(boolean showNumAccountsSupressed)
-    // {
-    // this.showNumAccountsSupressed = showNumAccountsSupressed;
-    // }
-    //
-    // public Boolean isShowNumAccountsInSegment() {
-    // return showNumAccountsInSegment;
-    // }
-    //
-    // public void setShowNumAccountsInSegment(boolean showNumAccountsInSegment)
-    // {
-    // this.showNumAccountsInSegment = showNumAccountsInSegment;
-    // }
-    //
-    // public Boolean isShowNumContactsLaunched() {
-    // return showNumContactsLaunched;
-    // }
-    //
-    // public void setShowNumContactsLaunched(boolean showNumContactsLaunched) {
-    // this.showNumContactsLaunched = showNumContactsLaunched;
-    // }
-    //
-    // public Boolean isShowNumContactsSupressed() {
-    // return showNumContactsSupressed;
-    // }
-    //
-    // public void setShowNumContactsSupressed(boolean showNumContactsSupressed)
-    // {
-    // this.showNumContactsSupressed = showNumContactsSupressed;
-    // }
-    //
-    // public Boolean isShowNumContactsInSegment() {
-    // return showNumContactsInSegment;
-    // }
-    //
-    // public void setShowNumContactsInSegment(boolean showNumContactsInSegment)
-    // {
-    // this.showNumContactsInSegment = showNumContactsInSegment;
-    // }
 
     public Long getAccoutLimit() {
         return accountLimit;
@@ -104,12 +34,6 @@ public class SalesforceChannelConfig implements ChannelConfig {
     public ChannelConfig copyConfig(ChannelConfig config) {
         SalesforceChannelConfig salesforceChannelConfig = this;
         SalesforceChannelConfig newSalesforceChannelConfig = (SalesforceChannelConfig) config;
-        // salesforceChannelConfig.setShowNumAccountsLaunched(newSalesforceChannelConfig.isShowNumAccountsLaunched());
-        // salesforceChannelConfig.setShowNumAccountsSupressed(newSalesforceChannelConfig.isShowNumAccountsSupressed());
-        // salesforceChannelConfig.setShowNumAccountsInSegment(newSalesforceChannelConfig.isShowNumAccountsInSegment());
-        // salesforceChannelConfig.setShowNumContactsLaunched(newSalesforceChannelConfig.isShowNumContactsLaunched());
-        // salesforceChannelConfig.setShowNumContactsSupressed(newSalesforceChannelConfig.isShowNumContactsSupressed());
-        // salesforceChannelConfig.setShowNumContactsInSegment(newSalesforceChannelConfig.isShowNumContactsInSegment());
         salesforceChannelConfig.setAccountLimit(newSalesforceChannelConfig.getAccoutLimit());
         salesforceChannelConfig
                 .setSupressAccountWithoutAccountId(newSalesforceChannelConfig.isSupressAccountWithoutAccountId());
