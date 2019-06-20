7708d04883daa798315567e221210f4efbc071da
slinlattice <slin@lattice-engines.com>
2019-06-20 01:33:47 -0700

    Close meter registry automatically and do a final round of metrics publish

--- le-monitor/src/main/java/com/latticeengines/monitor/micrometer/MeterRegistryConfig.java
+++ le-monitor/src/main/java/com/latticeengines/monitor/micrometer/MeterRegistryConfig.java
@@ -14,6 +14,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 
+import com.latticeengines.common.exposed.util.MetricUtils;
+import com.latticeengines.monitor.util.MonitoringUtils;
+
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
@@ -25,6 +28,7 @@ import io.micrometer.core.instrument.config.MeterFilter;
 @Configuration
 public class MeterRegistryConfig {
 
+    private static final String TAG_EMR_CLUSTER_NAME = "EmrClusterName";
     private static final String TAG_TENANT = "Tenant";
     private static final String DEPLOYMENT_TEST_TENANT_NAME = "DeploymentTestNG";
     private static final String E2E_TEST_TENANT_NAME = "LETest";
@@ -36,23 +40,35 @@ public class MeterRegistryConfig {
     @Value("${monitor.metrics.micrometer.enabled:false}")
     private boolean enableMonitoring;
 
+    @Value("${aws.emr.cluster:}")
+    private String emrClusterName;
+
+    @Value("${hadoop.use.emr}")
+    private Boolean useEmr;
+
+    private String environment = MonitoringUtils.getEnvironment();
+
+    private String stack = MonitoringUtils.getStack();
+
+    private String hostname = MonitoringUtils.getHostName();
+
     @Lazy
     @Bean(name = "rootRegistry")
     @Primary
-    public MeterRegistry rootRegistry(@Lazy @Qualifier("influxMeterRegistry") MeterRegistry influxRegistry) {
+    public MeterRegistry rootRegistry(@Qualifier("influxMeterRegistry") MeterRegistry influxRegistry) {
         log.info("Initializing root meter registry, micrometer monitoring enabled = {}", enableMonitoring);
-        return getCompositeRegistry(influxRegistry);
+        return getCompositeRegistry(false, influxRegistry);
     }
 
     @Lazy
     @Bean(name = "rootHostRegistry")
     public MeterRegistry rootHostRegistry(
-            @Lazy @Qualifier("influxHostMeterRegistry") MeterRegistry influxHostRegistry) {
+            @Qualifier("influxHostMeterRegistry") MeterRegistry influxHostRegistry) {
         log.info("Initializing root host meter registry, micrometer monitoring enabled = {}", enableMonitoring);
-        return getCompositeRegistry(influxHostRegistry);
+        return getCompositeRegistry(true, influxHostRegistry);
     }
 
-    private MeterRegistry getCompositeRegistry(MeterRegistry... registries) {
+    private MeterRegistry getCompositeRegistry(boolean addHostLevelTags, MeterRegistry... registries) {
         CompositeMeterRegistry compositeRegistry = new CompositeMeterRegistry();
         if (!enableMonitoring || registries == null) {
             // return a noop registry
@@ -60,13 +76,35 @@ public class MeterRegistryConfig {
         }
 
         Arrays.stream(registries).filter(Objects::nonNull).forEach(compositeRegistry::add);
-        return addCommonFilters(compositeRegistry);
+        return addCommonTags(addCommonFilters(compositeRegistry), addHostLevelTags);
     }
 
     @Lazy
     @Bean
     public TimedAspect timedAspect(@Lazy @Qualifier("rootRegistry") MeterRegistry registry) {
         return new TimedAspect(registry);
+    }
+
+    private MeterRegistry addCommonTags(MeterRegistry registry, boolean addHostLevelTags) {
+        if (registry == null) {
+            return null;
+        }
+
+        MeterRegistry.Config config = registry.config();
+        // emr tag
+        if (Boolean.TRUE.equals(useEmr) && MonitoringUtils.emrTagEnabled()) {
+            config.commonTags(TAG_EMR_CLUSTER_NAME, emrClusterName);
+        }
+
+        // service level tags
+        config.commonTags(MetricUtils.TAG_ENVIRONMENT, environment, MetricUtils.TAG_STACK, stack);
+
+        // host level tags
+        if (addHostLevelTags) {
+            config.commonTags(MetricUtils.TAG_HOST, hostname);
+        }
+
+        return registry;
     }
 
     private MeterRegistry addCommonFilters(MeterRegistry registry) {
