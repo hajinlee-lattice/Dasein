62729b1baf4675bf1c8eefe4178ccb5e67df0e02
rcao <rcao@lattice-engines.com>
2019-06-24 15:10:45 -0700

    Test added and modify

--- le-datacloud/etl/src/main/java/com/latticeengines/datacloud/etl/transformation/transformer/impl/publish/SourceToS3Publisher.java
+++ le-datacloud/etl/src/main/java/com/latticeengines/datacloud/etl/transformation/transformer/impl/publish/SourceToS3Publisher.java
@@ -1,5 +1,6 @@
 package com.latticeengines.datacloud.etl.transformation.transformer.impl.publish;
 
+import static com.latticeengines.domain.exposed.datacloud.DataCloudConstants.SERVICE_TENANT;
 import static com.latticeengines.domain.exposed.datacloud.DataCloudConstants.TRANSFORMER_SOURCE_TO_S3_PUBLISHER;
 
 import java.util.List;
@@ -13,13 +14,13 @@ import org.apache.hadoop.mapreduce.JobContext;
 import org.codehaus.plexus.util.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.hadoop.configuration.ConfigurationUtils;
 import org.springframework.stereotype.Component;
 
 import com.latticeengines.aws.s3.S3Service;
 import com.latticeengines.common.exposed.util.HdfsUtils;
+import com.latticeengines.common.exposed.util.HdfsUtils.HdfsFileFilter;
 import com.latticeengines.datacloud.core.util.RequestContext;
 import com.latticeengines.datacloud.etl.transformation.transformer.TransformStep;
 import com.latticeengines.datacloud.etl.transformation.transformer.impl.AbstractTransformer;
@@ -33,7 +34,7 @@ import com.latticeengines.yarn.exposed.service.EMREnvService;
 public class SourceToS3Publisher extends AbstractTransformer<TransformerConfig> {
     private static final Logger log = LoggerFactory.getLogger(SourceToS3Publisher.class);
 
-    public static final CustomerSpace TEST_CUSTOMER = CustomerSpace.parse("SourceToS3Publisher");
+    public static final CustomerSpace TEST_CUSTOMER = CustomerSpace.parse(SERVICE_TENANT);
     public static final String TRANSFORMER_NAME = TRANSFORMER_SOURCE_TO_S3_PUBLISHER;
 
     @Resource(name = "distCpConfiguration")
@@ -42,13 +43,13 @@ public class SourceToS3Publisher extends AbstractTransformer<TransformerConfig> 
     @Inject
     private EMREnvService emrEnvService;
 
-    @Autowired
+    @Inject
     private S3Service s3Service;
 
-    @Value("${aws.s3.bucket}")
+    @Value("${datacloud.collection.s3bucket}")
     private String s3Bucket;
 
-    private String name;
+
 
     private String tenantId = TEST_CUSTOMER.getTenantId();
 
@@ -61,7 +62,7 @@ public class SourceToS3Publisher extends AbstractTransformer<TransformerConfig> 
     @Override
     protected boolean validateConfig(TransformerConfig config, List<String> sourceNames) {
         if (!(config.validate(sourceNames))) {
-            RequestContext.logError("SourceNames validation fail.");
+            RequestContext.logError("Validation fails due to no sourceName, or null sourceName.");
             return false;
         }
         return true;
@@ -71,6 +72,8 @@ public class SourceToS3Publisher extends AbstractTransformer<TransformerConfig> 
     @Override
     protected boolean transformInternal(TransformationProgress progress, String workflowDir, TransformStep step) {
         try {
+            String sourceName = step.getTarget().getSourceName();
+
             String hdfsSnapshotDir = getSourceHdfsDir(step, 0);
             String hdfsSchemaDir = getBaseSourceSchemaDir(step, 0);
             String hdfsVersionFilePath = getBaseSourceVersionFilePath(step, 0);
@@ -78,78 +81,109 @@ public class SourceToS3Publisher extends AbstractTransformer<TransformerConfig> 
             String s3SchemaPrefix = gets3nPath(s3Bucket, hdfsSchemaDir);
             String s3VersionFilePrefix = gets3nPath(s3Bucket, hdfsVersionFilePath);
 
-            copyToS3(step, hdfsSnapshotDir, s3SnapshotPrefix, hdfsSchemaDir, s3SchemaPrefix, hdfsVersionFilePath,
-                    s3VersionFilePrefix);
+            boolean dirFlag = true;
+            boolean fileFalg = false;
 
+            copyToS3(sourceName, hdfsSnapshotDir, s3SnapshotPrefix, dirFlag);
+            copyToS3(sourceName, hdfsSchemaDir, s3SchemaPrefix, dirFlag);
+            copyToS3(sourceName, hdfsVersionFilePath, s3VersionFilePrefix, fileFalg);
+
+            validateCopySuccess(hdfsSnapshotDir);
+            if (HdfsUtils.isDirectory(distCpConfiguration, hdfsSchemaDir)) {
+                validateCopySuccess(hdfsSchemaDir);
+            }
+            validateCopySuccess(hdfsVersionFilePath);
 
             step.setTarget(null);
             step.setCount(0L);
             return true;
 
         } catch (Exception e) {
-            log.error("Failed to transform data", e);
+            log.error(e.getMessage());
             return false;
         }
     }
 
 
-
-    private void copyToS3(TransformStep step, String hdfsSnapshotDir, String s3nSnapshotDir, String hdfsSchemaDir,
-            String s3SchemaDir, String hdfsVersionFilePath, String s3VersionFilePrefix) {
+    private void copyToS3(String sourceName, String hdfsDir, String s3nDir, boolean flag) {
         try {
-
-            cleanupPrefix(hdfsSnapshotDir, hdfsSchemaDir, hdfsVersionFilePath);
+            cleanupPrefix(hdfsDir);
 
             String subFolder = "HdfsToS3";
-            Configuration distcpConfiguration = createConfiguration(step, subFolder);
-
-            Properties properties = new Properties();
-            properties.setProperty("mapreduce.application.classpath",
-                    "$HADOOP_CONF_DIR,$HADOOP_MAPRED_HOME/share/hadoop/mapreduce/*"
-                            + ",$HADOOP_MAPRED_HOME/share/hadoop/mapreduce/lib/*,$HADOOP_MAPRED_HOME/share/hadoop/tools/lib/*");
+            Configuration distcpConfiguration = createConfiguration(sourceName, subFolder);
 
             String queue = LedpQueueAssigner.getDefaultQueueNameForSubmission();
             String overwriteQueue = LedpQueueAssigner.overwriteQueueAssignment(queue,
                     emrEnvService.getYarnQueueScheme());
 
-            log.info("hdfsPath: " + hdfsSnapshotDir);
-            log.info("s3nPath: " + s3nSnapshotDir);
+            log.info("hdfsPath: " + hdfsDir);
+            log.info("s3nPath: " + s3nDir);
 
-            HdfsUtils.distcp(ConfigurationUtils.createFrom(distcpConfiguration, properties), hdfsSnapshotDir,
-                    s3nSnapshotDir,
-                    overwriteQueue);
-            if (StringUtils.isNotEmpty(hdfsSchemaDir)) {
-                HdfsUtils.distcp(ConfigurationUtils.createFrom(distcpConfiguration, properties), hdfsSchemaDir,
-                        s3SchemaDir, overwriteQueue);
-            }
-            if (StringUtils.isNotEmpty(hdfsVersionFilePath)) {
-                HdfsUtils.distcp(ConfigurationUtils.createFrom(distcpConfiguration, properties), hdfsVersionFilePath,
-                        s3VersionFilePrefix, overwriteQueue);
+            if (flag) {
+                if (HdfsUtils.fileExists(distcpConfiguration, hdfsDir)
+                        && HdfsUtils.isDirectory(distcpConfiguration, hdfsDir)) {
+                    HdfsUtils.distcp(distcpConfiguration, hdfsDir, s3nDir, overwriteQueue);
+                } else {
+                    String dirName = getDirName(hdfsDir);
+                    String checkDir = "Schema";
+                    if (!HdfsUtils.isDirectory(distcpConfiguration, hdfsDir)
+                            && StringUtils.equals(dirName, checkDir)) {
+                    } else {
+                        throw new RuntimeException("No file exists in dir, or Dir not exist : " + hdfsDir);///
+                    }
+                }
+            } else {
+                if (HdfsUtils.fileExists(distcpConfiguration, hdfsDir)) {
+                    HdfsUtils.distcp(distcpConfiguration, hdfsDir, s3nDir, overwriteQueue);
+                } else {
+                    throw new RuntimeException("File not exist: " + hdfsDir);
+                }
             }
         } catch (Exception e) {
-            throw new RuntimeException("Cannot copy hdfs Dir to s3!" + e.getMessage());
-        }
-
-    }
-
-    private void cleanupPrefix(String s3nSnapshotDir, String s3SchemaDir, String s3VersionFilePrefix) {
-        if (s3Service.isNonEmptyDirectory(s3Bucket, s3nSnapshotDir)) {
-            s3Service.cleanupPrefix(s3Bucket, s3nSnapshotDir);
-        }
-        if (s3Service.isNonEmptyDirectory(s3Bucket, s3SchemaDir)) {
-            s3Service.cleanupPrefix(s3Bucket, s3SchemaDir);
-        }
-        if (s3Service.isNonEmptyDirectory(s3Bucket, s3VersionFilePrefix)) {
-            s3Service.cleanupPrefix(s3Bucket, s3VersionFilePrefix);
+            throw new RuntimeException(String.format("Fail to copy %s to %s", hdfsDir, s3nDir), e);
         }
     }
 
-    private Configuration createConfiguration(TransformStep step, String jobNameSuffix) {
-        name = step.getTarget().getSourceName();
+    private String getDirName(String hdfsDir) {
+        String simplifyName = hdfsDir.substring(0, hdfsDir.lastIndexOf("/"));
+        String dirName = simplifyName.substring(simplifyName.lastIndexOf("/") + 1);
+        return dirName;
+    }
+
+    private void validateCopySuccess(String hdfsDir) {
+        try {
+            String filepath;
+            List<String> files = HdfsUtils.onlyGetFilesForDirRecursive(distCpConfiguration, hdfsDir,
+                    (HdfsFileFilter) null, false);
+
+            for (String key : files) {
+                filepath = key.substring(key.indexOf(hdfsDir));
+                s3Service.objectExist(s3Bucket, filepath);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Missing file in S3 copy from: " + hdfsDir + e.getMessage());
+        }
+    }
+
+    private void cleanupPrefix(String s3nDir) {
+        if (s3Service.isNonEmptyDirectory(s3Bucket, s3nDir)) {
+            s3Service.cleanupPrefix(s3Bucket, s3nDir);
+        }
+    }
+
+    private Configuration createConfiguration(String sourceName, String jobNameSuffix) {
         Configuration hadoopConfiguration = ConfigurationUtils.createFrom(distCpConfiguration, new Properties());
-        String jobName = tenantId + "~" + name + "~" + jobNameSuffix;
+        String jobName = tenantId + "~" + sourceName + "~" + jobNameSuffix;
         hadoopConfiguration.set(JobContext.JOB_NAME, jobName);
-        return hadoopConfiguration;
+        Properties properties = new Properties();
+        properties.setProperty("mapreduce.application.classpath",
+                "$HADOOP_CONF_DIR,$HADOOP_MAPRED_HOME/share/hadoop/mapreduce/*"
+                        + ",$HADOOP_MAPRED_HOME/share/hadoop/mapreduce/lib/*,$HADOOP_MAPRED_HOME/share/hadoop/tools/lib/*");
+        if (StringUtils.isNotEmpty(hadoopConfiguration.get("mapreduce.application.classpath"))) {
+            return hadoopConfiguration;
+        } else {
+            return ConfigurationUtils.createFrom(hadoopConfiguration, properties);
+        }
     }
 
 
@@ -165,3 +199,4 @@ public class SourceToS3Publisher extends AbstractTransformer<TransformerConfig> 
         }
     }
 }
+
--- le-datacloud/etl/src/test/java/com/latticeengines/datacloud/etl/transformation/service/impl/SourceToS3PublisherDeploymentTestNG.java
+++ le-datacloud/etl/src/test/java/com/latticeengines/datacloud/etl/transformation/service/impl/SourceToS3PublisherDeploymentTestNG.java
@@ -1,54 +1,183 @@
 package com.latticeengines.datacloud.etl.transformation.service.impl;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
 import org.apache.avro.generic.GenericRecord;
+import org.apache.hadoop.conf.Configuration;
 import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.testng.annotations.Test;
 
-import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.latticeengines.aws.s3.S3Service;
+import com.latticeengines.common.exposed.util.HdfsUtils;
+import com.latticeengines.common.exposed.util.HdfsUtils.HdfsFileFilter;
 import com.latticeengines.datacloud.core.source.Source;
 import com.latticeengines.datacloud.core.source.impl.GeneralSource;
-import com.latticeengines.datacloud.etl.transformation.service.TransformationService;
 import com.latticeengines.datacloud.etl.transformation.transformer.impl.publish.SourceToS3Publisher;
 import com.latticeengines.domain.exposed.datacloud.manage.TransformationProgress;
 import com.latticeengines.domain.exposed.datacloud.transformation.config.impl.PipelineTransformationConfiguration;
 import com.latticeengines.domain.exposed.datacloud.transformation.step.TransformationStepConfig;
 
-public class SourceToS3PublisherDeploymentTestNG
-        extends TransformationServiceImplTestNGBase<PipelineTransformationConfiguration> {
+public class SourceToS3PublisherDeploymentTestNG extends PipelineTransformationTestNGBase {
 
     private static final Logger log = LoggerFactory.getLogger(SourceToS3PublisherDeploymentTestNG.class);
 
-    GeneralSource baseSourceAccMaster = new GeneralSource("AccountMasterVerify");
-    GeneralSource source = new GeneralSource("LDCDEV_SourceToS3Publisher");
+    private GeneralSource baseSourceAccMaster1 = new GeneralSource("TestSource1");
+    // S3 already exist
+    private GeneralSource baseSourceAccMaster2 = new GeneralSource("TestSource2");
+    // no schema
+    private GeneralSource baseSourceAccMaster3 = new GeneralSource("TestSource3");
+    // multiple files in Snapshot
+    private GeneralSource baseSourceAccMaster4 = new GeneralSource("TestSource4");
+
+    @Autowired
+    protected Configuration yarnConfiguration;
 
     @Autowired
     private S3Service s3Service;
 
-    @Value("${aws.s3.bucket}")
+    @Value("${datacloud.collection.s3bucket}")
     private String s3Bucket;
 
+    private String existingSourceVersion = "2019-06-23_18-45-42_UTC";
 
     @Test(groups = "functional", enabled = true)
-    public void testTransformation() {
+    public void testTransformation() throws IOException {
         prepareData();
         TransformationProgress progress = createNewProgress();
         progress = transformData(progress);
         finish(progress);
-        Assert.assertTrue(s3VerifyPublishExist(progress));
+        Assert.assertTrue(VerifyPublishExists3(progress));
         cleanupProgressTables();
     }
 
-    private void prepareData() {
-        uploadBaseSourceFile(baseSourceAccMaster.getSourceName(), "AccountMaster206", baseSourceVersion);
+    private void prepareData() throws IOException {
+
+        uploadBaseSourceFile(baseSourceAccMaster1.getSourceName(), "AccountMaster206", baseSourceVersion);
+        uploadBaseSourceFile(baseSourceAccMaster2.getSourceName(), "AccountMaster206", existingSourceVersion);
+        uploadBaseSourceFile(baseSourceAccMaster3.getSourceName(), "AccountMaster206", baseSourceVersion);
+        uploadBaseSourceDir(baseSourceAccMaster4.getSourceName(), "", baseSourceVersion);
+
+
+        createSchema(baseSourceAccMaster1, baseSourceVersion);
+        createSchema(baseSourceAccMaster2, existingSourceVersion);
+        createSchema(baseSourceAccMaster4, baseSourceVersion);
+    }
+
+    @Override
+    protected PipelineTransformationConfiguration createTransformationConfiguration() {
+        try {
+            PipelineTransformationConfiguration configuration = new PipelineTransformationConfiguration();
+            configuration.setName("WeeklyHdfsToS3Publish");
+            configuration.setVersion(targetVersion);
+
+            List<TransformationStepConfig> steps = new ArrayList<TransformationStepConfig>();
+
+            TransformationStepConfig step1 = createStep(baseSourceAccMaster1);
+            TransformationStepConfig step2 = createStep(baseSourceAccMaster2);
+            TransformationStepConfig step3 = createStep(baseSourceAccMaster3);
+            TransformationStepConfig step4 = createStep(baseSourceAccMaster4);
+
+            steps.add(step1);
+            steps.add(step2);
+            steps.add(step3);
+            steps.add(step4);
+
+            configuration.setSteps(steps);
+
+            return configuration;
+        } catch (Exception e) {
+            throw new RuntimeException("Transformation Configuration create fail:" + e.getMessage());
+        }
+    }
+
+    @Override
+    protected String getPathForResult() {
+        return hdfsPathBuilder.constructSnapshotDir(baseSourceAccMaster1.getSourceName(), baseSourceVersion).toString();
+    }
+
+    protected String getSnapshotPathForResult(Source baseSourceAccMaster, String baseSourceVersion) {
+        return hdfsPathBuilder.constructSnapshotDir(baseSourceAccMaster.getSourceName(), baseSourceVersion).toString();
+    }
+
+    protected String getSchemaPathForResult(Source baseSourceAccMaster, String baseSourceVersion) {
+        return hdfsPathBuilder.constructSchemaDir(baseSourceAccMaster.getSourceName(), baseSourceVersion).toString();
+    }
+
+    protected String getVerFilePathForResult(Source baseSourceAccMaster, String baseSourceVersion) {
+        Source targetSource = sourceService.findBySourceName(baseSourceAccMaster.getSourceName());
+        return hdfsPathBuilder.constructVersionFile(targetSource).toString();
+    }
+
+
+
+    private boolean VerifyPublishExists3(TransformationProgress progress) {
+        try {
+            Source baseSource1 = sourceService.findBySourceName(baseSourceAccMaster1.getSourceName());
+            Source baseSource2 = sourceService.findBySourceName(baseSourceAccMaster2.getSourceName());
+            Source baseSource3 = sourceService.findBySourceName(baseSourceAccMaster3.getSourceName());
+            Source baseSource4 = sourceService.findBySourceName(baseSourceAccMaster3.getSourceName());
+
+            stepSuccessValidate(baseSource1, baseSourceVersion);
+            stepSuccessValidate(baseSource2, existingSourceVersion);
+            stepSuccessValidate(baseSource3, baseSourceVersion);
+            stepSuccessValidate(baseSource4, baseSourceVersion);
+
+            return true;
+        } catch (Exception e) {
+            throw new RuntimeException("S3 Publish fail:" + e.getMessage());
+        }
+    }
+
+    private void stepSuccessValidate(Source baseSource, String baseSourceVersion) throws IOException {
+
+        String s3SnapshotPath = getSnapshotPathForResult(baseSource, baseSourceVersion);
+        String s3SchemaPath = getSchemaPathForResult(baseSource, baseSourceVersion);
+        String s3VersionFilePath = getVerFilePathForResult(baseSource, baseSourceVersion);
+
+        validateCopySucseess(s3SnapshotPath);
+        if (HdfsUtils.isDirectory(yarnConfiguration, s3SchemaPath)) {
+            validateCopySucseess(s3SchemaPath);
+        }
+        validateCopySucseess(s3VersionFilePath);
+    }
+
+    private void validateCopySucseess(String Prefix) throws IOException {
+            System.out.println("Checking the objects of Prefix:" + Prefix);
+            String filepath;
+
+            List<String> files = HdfsUtils.onlyGetFilesForDirRecursive(yarnConfiguration, Prefix,
+                    (HdfsFileFilter) null, false);
+            for (String key : files) {
+                filepath = key.substring(key.indexOf(Prefix));
+                System.out.println("Key : " + filepath);
+                if (!s3Service.objectExist(s3Bucket, filepath)) {
+                    throw new RuntimeException("File not Exist in S3:" + filepath);
+                }
+            }
+    }
+
+    private TransformationStepConfig createStep(Source baseSourceAccMaster) {
+        TransformationStepConfig step = new TransformationStepConfig();
+        step.setBaseSources(Collections.singletonList(baseSourceAccMaster.getSourceName()));
+        step.setTransformer(SourceToS3Publisher.TRANSFORMER_NAME);
+        step.setTargetSource(source.getSourceName());
+        return step;
+    }
+
+    private void createSchema(Source baseSourceAccMaster, String baseSourceVersion) {
         try {
             extractSchema(baseSourceAccMaster, baseSourceVersion, hdfsPathBuilder
                     .constructSnapshotDir(baseSourceAccMaster.getSourceName(), baseSourceVersion).toString());
@@ -59,97 +188,41 @@ public class SourceToS3PublisherDeploymentTestNG
     }
 
     @Override
-    protected PipelineTransformationConfiguration createTransformationConfiguration() {
-        try {
-            PipelineTransformationConfiguration configuration = new PipelineTransformationConfiguration();
-            configuration.setName("WeeklyHdfsToS3Publish");
-            configuration.setVersion(targetVersion);
+    protected void uploadBaseSourceDir(String baseSource, String baseSourceDir, String baseSourceVersion)
+            throws IOException {
+        String rootDir = "sources/" + baseSourceDir;
 
-            TransformationStepConfig step1 = new TransformationStepConfig();
-            List<String> baseSourcesStep1 = new ArrayList<String>();
-            baseSourcesStep1.add(baseSourceAccMaster.getSourceName());
-            step1.setBaseSources(baseSourcesStep1);
-            step1.setTransformer(SourceToS3Publisher.TRANSFORMER_NAME);
-            step1.setTargetSource(source.getSourceName());
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        Resource[] sourceResources = resolver.getResources(rootDir + "/*.avro");
+        log.info("Resolved resources for " + rootDir);
 
-            List<TransformationStepConfig> steps = new ArrayList<TransformationStepConfig>();
-            steps.add(step1);
-            configuration.setSteps(steps);
-            return configuration;
-        } catch (Exception e) {
-            throw new RuntimeException("Transformation Configuration create fail:" + e.getMessage());
+        int fileIdx = 0;
+        for (Resource resource : sourceResources) {
+            if (resource.getURI().toString().endsWith(".avro")) {
+                InputStream is = resource.getInputStream();
+                String targetPath = hdfsPathBuilder.constructSnapshotDir(baseSource, baseSourceVersion)
+                        .append(String.format("part-%04d.avro", fileIdx)).toString();
+                log.info("Upload " + resource.getURI().toString() + " to " + targetPath);
+                HdfsUtils.copyInputStreamToHdfs(yarnConfiguration, is, targetPath);
+                fileIdx++;
+            }
         }
+        String successPath = hdfsPathBuilder.constructSnapshotDir(baseSource, baseSourceVersion).append("_SUCCESS")
+                .toString();
+        InputStream stream = new ByteArrayInputStream("".getBytes(StandardCharsets.UTF_8));
+        HdfsUtils.copyInputStreamToHdfs(yarnConfiguration, stream, successPath);
+        hdfsSourceEntityMgr.setCurrentVersion(baseSource, baseSourceVersion);
     }
 
     @Override
-    protected TransformationService<PipelineTransformationConfiguration> getTransformationService() {
-        return pipelineTransformationService;
-    }
-
-    @Override
-    protected Source getSource() {
-        return source;
-    }
-
-    @Override
-    protected String getPathToUploadBaseData() {
-        return hdfsPathBuilder.constructSnapshotDir(baseSourceAccMaster.getSourceName(), baseSourceVersion).toString();
-    }
-
-    @Override
-    protected String getPathForResult() {
-        return hdfsPathBuilder.constructSnapshotDir(baseSourceAccMaster.getSourceName(), baseSourceVersion).toString();
-    }
-
-    protected String getSchemaPathForResult() {
-        return hdfsPathBuilder.constructSchemaDir(baseSourceAccMaster.getSourceName(), baseSourceVersion).toString();
-    }
-
-    protected String getVerFilePathForResult() {
-        Source targetSource = sourceService.findBySourceName(baseSourceAccMaster.getSourceName());
-        return hdfsPathBuilder.constructVersionFile(targetSource).toString();
+    protected String getTargetSourceName() {
+        return null;
     }
 
     @Override
     protected void verifyResultAvroRecords(Iterator<GenericRecord> records) {
-        // TODO Auto-generated method stub
-
     }
-
-    private boolean s3VerifyPublishExist(TransformationProgress progress) {
-        String s3SnapshotPath = getPathForResult();
-        String s3SchemaPath = getSchemaPathForResult();
-        String s3VersionFilePath = getVerFilePathForResult();
-
-        return checkS3Objects(s3SnapshotPath, s3SchemaPath, s3VersionFilePath);
-    }
-
-    private boolean checkS3Objects(String s3SnapshotPath, String s3SchemaPath, String s3VersionFilePath) {
-        try {
-            checkS3PrefixObjExist(s3SnapshotPath);
-            checkS3PrefixObjExist(s3SchemaPath);
-            checkS3PrefixObjExist(s3VersionFilePath);
-            return true;
-        } catch (Exception e) {
-            throw new RuntimeException("S3 Publish fail, missing objects:" + e.getMessage());
-        }
-    }
-
-    private boolean checkS3PrefixObjExist(String Prefix) {
-        System.out.println("Checking the objects of Prefix:" + Prefix);
-        if (s3Service.isNonEmptyDirectory(s3Bucket, Prefix)) {
-            List<S3ObjectSummary> Obejects = s3Service.listObjects(s3Bucket, Prefix);
-            for (S3ObjectSummary object : Obejects) {
-                if (!s3Service.objectExist(s3Bucket, object.getKey())) {
-                    throw new RuntimeException();
-                }
-            }
-        } else {
-            throw new RuntimeException();
-        }
-        return true;
-    }
-
 
 }
+
 
