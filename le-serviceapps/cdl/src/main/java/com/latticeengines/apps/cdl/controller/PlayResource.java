979d143f865752b7b9b3c0058c252edb33a0fc26
Jignesh Mehta <jlmehta@lattice-engines.com>
2019-06-26 15:23:48 -0700

    PLS-13965: No need to validate the DisplayName in resource

--- le-serviceapps/cdl/src/main/java/com/latticeengines/apps/cdl/controller/PlayResource.java
+++ le-serviceapps/cdl/src/main/java/com/latticeengines/apps/cdl/controller/PlayResource.java
@@ -155,10 +155,6 @@ public class PlayResource {
         if (play == null) {
             throw new LedpException(LedpCode.LEDP_32000, new String[] { "Play is null" });
         }
-        if (StringUtils.isEmpty(play.getDisplayName())) {
-            throw new LedpException(LedpCode.LEDP_32000,
-                    new String[] { "Play's Display Name cannot be blank is null" });
-        }
 
         return playService.createOrUpdate(play, shouldLoadCoverage, tenant.getId());
     }
