7fc74ef4bcc058589de02ae14f571c8022d0dc53
Raymond Feng <rfeng@lattice-engines.com>
2019-07-16 15:10:31 -0700

    change test structure

--- le-metadata/src/test/java/com/latticeengines/metadata/entitymgr/impl/MigrationTrackEntityMgrImplTestNG.java
+++ le-metadata/src/test/java/com/latticeengines/metadata/entitymgr/impl/MigrationTrackEntityMgrImplTestNG.java
@@ -5,35 +5,67 @@ import java.util.Map;
 
 import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.latticeengines.domain.exposed.metadata.DataCollection;
+import com.latticeengines.domain.exposed.metadata.DataCollectionStatusDetail;
 import com.latticeengines.domain.exposed.metadata.MigrationTrack;
+import com.latticeengines.domain.exposed.metadata.MigrationTrackImportAction;
 import com.latticeengines.domain.exposed.metadata.TableRoleInCollection;
 import com.latticeengines.domain.exposed.security.Tenant;
 import com.latticeengines.metadata.entitymgr.MigrationTrackEntityMgr;
 import com.latticeengines.metadata.functionalframework.MetadataFunctionalTestNGBase;
 
-@ContextConfiguration(locations = { "classpath:test-metadata-context.xml",
-        "classpath:common-testclient-env-context.xml", "classpath:metadata-aspects-context.xml" })
 public class MigrationTrackEntityMgrImplTestNG extends MetadataFunctionalTestNGBase {
 
     @Autowired
-    protected MigrationTrackEntityMgr migrationTrackEntityMgr;
+    private MigrationTrackEntityMgr migrationTrackEntityMgr;
 
-    private final MigrationTrack.Status STATUS = MigrationTrack.Status.SCHEDULED;
-    private final DataCollection.Version VERSION = DataCollection.Version.Blue;
-    private final String STATSNAME = "Test";
-    private final TableRoleInCollection ROLE = TableRoleInCollection.AccountBatchSlim;
-    private Map<TableRoleInCollection, String[]> ACTIVETABLE;
+    private static final MigrationTrack.Status STATUS = MigrationTrack.Status.SCHEDULED;
+    private static final DataCollection.Version VERSION = DataCollection.Version.Blue;
+    private static final String STATSNAME = "Test";
+    private static Tenant tenant1, tenant2;
+    private static Map<TableRoleInCollection, String[]> ACTIVETABLE = new HashMap<>();
+    private static final MigrationTrackImportAction IMPORTACTION = new MigrationTrackImportAction();
+    private static final DataCollectionStatusDetail DETAIL = new DataCollectionStatusDetail();
+    private static final TableRoleInCollection ROLE = TableRoleInCollection.AccountBatchSlim;
+    private static byte[] CUBESDATA = new byte[5];
+
+    private static MigrationTrack track1 = new MigrationTrack();
+    private static MigrationTrack track2 = new MigrationTrack();
 
     @BeforeClass(groups = "functional")
     private void getTestData() {
         super.setup();
+
+        tenant1 = tenantEntityMgr.findByTenantId(customerSpace1);
+        tenant2 = tenantEntityMgr.findByTenantId(customerSpace2);
+        Assert.assertNotNull(tenant1);
+        Assert.assertNotNull(tenant2);
+
+        String[] tableNames = { "This table", "That table" };
+        ACTIVETABLE.put(ROLE, tableNames);
+
+        track1.setStatus(STATUS);
+        track1.setVersion(VERSION);
+        track1.setStatsName(STATSNAME);
+        track1.setTenant(tenant1);
+        track1.setCurActiveTable(ACTIVETABLE);
+        track1.setImportAction(IMPORTACTION);
+        track1.setCollectionStatusDetail((DETAIL));
+        track1.setStatsCubesData(CUBESDATA);
+
+        track2.setStatus(STATUS);
+        track2.setVersion(VERSION);
+        track2.setStatsName(STATSNAME);
+        track2.setTenant(tenant2);
+        track2.setCurActiveTable(ACTIVETABLE);
+        track2.setImportAction(IMPORTACTION);
+        track2.setCollectionStatusDetail((DETAIL));
+        track2.setStatsCubesData(CUBESDATA);
     }
 
     @AfterClass(groups = "functional")
@@ -43,113 +75,39 @@ public class MigrationTrackEntityMgrImplTestNG extends MetadataFunctionalTestNGB
 
     @Test(groups = "functional", dataProvider = "entityProvider")
     public void testCreate(Tenant tenant, MigrationTrack track) {
+        Assert.assertNotNull(migrationTrackEntityMgr);
+        Assert.assertNotNull(track);
+        migrationTrackEntityMgr.create(track);
         Assert.assertNotNull(migrationTrackEntityMgr.findByKey(track));
-        Assert.assertEquals(migrationTrackEntityMgr.findByKey(track).getPid(), track.getPid());
+        Assert.assertEquals(track.getPid(), migrationTrackEntityMgr.findByKey(track).getPid());
     }
 
     @Test(groups = "functional", dataProvider = "entityProvider", dependsOnMethods = { "testCreate" })
-    public void testStatus(Tenant tenant, MigrationTrack track) {
-        Assert.assertEquals(track.getStatus(), migrationTrackEntityMgr.findByKey(track).getStatus());
-        Assert.assertEquals(STATUS, migrationTrackEntityMgr.findByKey(track).getStatus());
-    }
+    public void testRead(Tenant tenant, MigrationTrack track) {
+        MigrationTrack created = migrationTrackEntityMgr.findByKey(track);
 
-    @Test(groups = "functional", dataProvider = "entityProvider", dependsOnMethods = { "testCreate" })
-    public void testVersion(Tenant tenant, MigrationTrack track) {
-        Assert.assertEquals(track.getVersion(), migrationTrackEntityMgr.findByKey(track).getVersion());
-        Assert.assertEquals(VERSION, migrationTrackEntityMgr.findByKey(track).getVersion());
-    }
-
-    @Test(groups = "functional", dataProvider = "entityProvider", dependsOnMethods = { "testCreate" })
-    public void testActiveTable(Tenant tenant, MigrationTrack track) {
-        Assert.assertNotNull(migrationTrackEntityMgr.findByKey(track).getCurActiveTable());
-        Assert.assertNotNull(migrationTrackEntityMgr.findByKey(track).getCurActiveTable().get(ROLE));
-        Assert.assertArrayEquals(ACTIVETABLE.get(ROLE),
-                migrationTrackEntityMgr.findByKey(track).getCurActiveTable().get(ROLE));
-    }
-
-    @Test(groups = "functional", dataProvider = "entityProvider", dependsOnMethods = { "testCreate" })
-    public void testImportAction(Tenant tenant, MigrationTrack track) {
-        Assert.assertEquals(track.getImportAction(), migrationTrackEntityMgr.findByKey(track).getImportAction());
-    }
-
-    @Test(groups = "functional", dataProvider = "entityProvider", dependsOnMethods = { "testCreate" })
-    public void testStatusDetail(Tenant tenant, MigrationTrack track) {
-        Assert.assertNull(track.getCollectionStatusDetail());
-        Assert.assertNull(migrationTrackEntityMgr.findByKey(track).getCollectionStatusDetail());
-    }
-
-    @Test(groups = "functional", dataProvider = "entityProvider", dependsOnMethods = { "testCreate" })
-    public void testStatsCubesData(Tenant tenant, MigrationTrack track) {
-        Assert.assertArrayEquals(track.getStatsCubesData(),
-                migrationTrackEntityMgr.findByKey(track).getStatsCubesData());
-    }
-
-    @Test(groups = "functional", dataProvider = "entityProvider", dependsOnMethods = { "testCreate" })
-    public void testStatsName(Tenant tenant, MigrationTrack track) {
-        Assert.assertEquals(track.getStatsName(), migrationTrackEntityMgr.findByKey(track).getStatsName());
-        Assert.assertEquals(STATSNAME, migrationTrackEntityMgr.findByKey(track).getStatsName());
-    }
-
-    @Test(groups = "functional", dataProvider = "entityProvider", dependsOnMethods = { "testCreate" })
-    public void testDataCollection(Tenant tenant, MigrationTrack track) {
-        Assert.assertEquals(track.getDataCollection(), migrationTrackEntityMgr.findByKey(track).getDataCollection());
-    }
-
-    @Test(groups = "functional", dataProvider = "entityProvider", dependsOnMethods = { "testCreate" })
-    public void testTenant(Tenant tenant, MigrationTrack track) {
-        Assert.assertEquals(tenant.getPid(), migrationTrackEntityMgr.findByKey(track).getTenant().getPid());
+        Assert.assertEquals(STATUS, created.getStatus());
+        Assert.assertEquals(VERSION, created.getVersion());
+        Assert.assertNotNull(created.getCurActiveTable());
+        Assert.assertNotNull(created.getCurActiveTable().get(ROLE));
+        Assert.assertArrayEquals(ACTIVETABLE.get(ROLE), created.getCurActiveTable().get(ROLE));
+        Assert.assertNotNull(created.getImportAction());
+        Assert.assertArrayEquals(IMPORTACTION.getActions().toArray(), created.getImportAction().getActions().toArray());
+        Assert.assertEquals(DETAIL.getEvaluationDate(), created.getCollectionStatusDetail().getEvaluationDate());
+        Assert.assertArrayEquals(CUBESDATA, created.getStatsCubesData());
+        Assert.assertEquals(STATSNAME, created.getStatsName());
+        Assert.assertEquals(tenant.getPid(), created.getTenant().getPid());
     }
 
     @Test(groups = "functional", dataProvider = "entityProvider", dependsOnMethods = { "testCreate" })
     public void testFindByTenant(Tenant tenant, MigrationTrack track) {
-        Assert.assertEquals(tenant.getPid(), migrationTrackEntityMgr.findByTenant(tenant).getTenant().getPid());
+        MigrationTrack created = migrationTrackEntityMgr.findByTenant(tenant);
+
+        Assert.assertEquals(track.getPid(), created.getPid());
     }
 
     @DataProvider(name = "entityProvider")
     public Object[][] enittyProvider() {
-        Tenant tenant1 = tenantEntityMgr.findByTenantId(customerSpace1),
-                tenant2 = tenantEntityMgr.findByTenantId(customerSpace2);
-        MigrationTrack track1, track2;
-
-        Assert.assertNotNull(tenant1);
-        Assert.assertNotNull(tenant2);
-
-        if (ACTIVETABLE == null) {
-            ACTIVETABLE = new HashMap<>();
-            String[] tableNames = { "This table", "That table" };
-            ACTIVETABLE.put(ROLE, tableNames);
-        }
-
-        if (migrationTrackEntityMgr.findByField("FK_TENANT_ID", tenant1.getPid()) == null) {
-            track1 = new MigrationTrack();
-            track1.setStatus(STATUS);
-            track1.setVersion(VERSION);
-            track1.setStatsName(STATSNAME);
-            track1.setTenant(tenant1);
-            track1.setCurActiveTable(ACTIVETABLE);
-            track1.setCollectionStatusDetail(null);
-            track1.setStatsCubesData(new byte[1]);
-            Assert.assertNotNull(track1);
-            migrationTrackEntityMgr.create(track1);
-        } else {
-            track1 = migrationTrackEntityMgr.findByField("FK_TENANT_ID", tenant1.getPid());
-        }
-
-        if (migrationTrackEntityMgr.findByField("FK_TENANT_ID", tenant2.getPid()) == null) {
-            track2 = new MigrationTrack();
-            track2.setStatus(STATUS);
-            track2.setVersion(VERSION);
-            track2.setStatsName(STATSNAME);
-            track2.setTenant(tenant2);
-            track2.setCurActiveTable(ACTIVETABLE);
-            track2.setCollectionStatusDetail((null));
-            track2.setStatsCubesData(new byte[5]);
-            Assert.assertNotNull(track2);
-            migrationTrackEntityMgr.create(track2);
-        } else {
-            track2 = migrationTrackEntityMgr.findByField("FK_TENANT_ID", tenant2.getPid());
-        }
-
         return new Object[][] { { tenant1, track1 }, { tenant2, track2 } };
     }
 
