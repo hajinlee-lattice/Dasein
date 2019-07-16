6e69c3e21458623a52273448b6e59435fbb9e3e0
rcao <rcao@lattice-engines.com>
2019-07-16 14:26:53 -0700

    PD-2751-3 Tag S3 Objects

--- le-datacloud/etl/src/test/java/com/latticeengines/datacloud/etl/transformation/service/impl/SourceToS3PublisherTestNG.java
+++ le-datacloud/etl/src/test/java/com/latticeengines/datacloud/etl/transformation/service/impl/SourceToS3PublisherTestNG.java
@@ -21,7 +21,6 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.testng.Assert;
