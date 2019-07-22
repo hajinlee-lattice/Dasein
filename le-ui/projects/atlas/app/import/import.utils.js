93e8dbfb84fd5c9131fca2e70de30fd4114721a8
Leonardo Gigli <lgigli@lattice-engines.com>
2019-07-22 12:52:29 -0700

    removed import unused

--- le-ui/projects/atlas/app/import/import.utils.js
+++ le-ui/projects/atlas/app/import/import.utils.js
@@ -1,5 +1,3 @@
-import { deepCopy } from "common/app/utilities/ObjectUtilities.js";
-
 /**
  * Account:[
  *  {name: 'the_name', fieldType: 'TEXT', requiredIfNoField: boolean/null, requiredType:'Required'/'NotRequired'}
