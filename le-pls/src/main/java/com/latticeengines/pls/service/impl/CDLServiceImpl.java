b71a53b9c9a7fc195b40653cdee3530d3d33b77d
Joy Xiao <jxiao@lattice-engines.com>
2019-07-24 18:14:35 +0800

    DP-10853: add TimeOut verify

--- le-pls/src/main/java/com/latticeengines/pls/service/impl/CDLServiceImpl.java
+++ le-pls/src/main/java/com/latticeengines/pls/service/impl/CDLServiceImpl.java
@@ -88,6 +88,9 @@ public class CDLServiceImpl implements CDLService {
     @Value("${pls.pa.max.concurrent.limit}")
     private int maxActivePA;
 
+    @Value("${common.le.stack}")
+    private String leStack;
+
     private List<String> templateMappingHeaders = Arrays.asList("Field Type", "Your Field Name", "Lattice Field Name", "Data Type");
 
     private static final String CUSTOM = "Custom";
@@ -97,7 +100,7 @@ public class CDLServiceImpl implements CDLService {
     @Override
     public ApplicationId processAnalyze(String customerSpace, ProcessAnalyzeRequest request) {
         checkPALimit(customerSpace, request);
-        boolean isActivityBasedPA = cdlProxy.isActivityBasedPA();
+        boolean isActivityBasedPA = cdlProxy.isActivityBasedPA(leStack);
         if (isActivityBasedPA) {
             return cdlProxy.scheduleProcessAnalyze(customerSpace, false, request);
         } else {
