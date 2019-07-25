4f8ef14907c1bb7b7c709c2970947b8a38c10c7a
Perry Liu <perry.liu@lattice-engines.com>
2019-07-25 14:59:44 -0700

    changed error message

--- le-domain/src/main/java/com/latticeengines/domain/exposed/exception/LedpCode.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/exception/LedpCode.java
@@ -352,7 +352,7 @@ public enum LedpCode {
     LEDP_18225("Play launch channel given belongs to play: {0} and does not belong play: {1}."),
     LEDP_18226("The input contains more than {0} fields. Please reduce the no. of {1} fields and try again"),
     LEDP_18227("Can't get entity data from cache when cache is in build."),
-    LEDP_18228("No channel found for play {0}"), 
+    LEDP_18228("Play Launch Channel {0} not found."), 
     
 
     // le-security 19000-19100
