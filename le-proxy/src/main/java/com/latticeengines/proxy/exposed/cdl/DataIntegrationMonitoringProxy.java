4dc36853eadd813c9f0c475db8ba2ae2abdfe421
Leonardo Gigli <lgigli@lattice-engines.com>
2019-07-30 15:47:17 -0700

    changed proxy and PlayLaunchExport S3 to match the new signature of the controller for logging messages

--- le-proxy/src/main/java/com/latticeengines/proxy/exposed/cdl/DataIntegrationMonitoringProxy.java
+++ le-proxy/src/main/java/com/latticeengines/proxy/exposed/cdl/DataIntegrationMonitoringProxy.java
@@ -1,7 +1,11 @@
 package com.latticeengines.proxy.exposed.cdl;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Component;
 
+import com.latticeengines.common.exposed.util.JsonUtils;
 import com.latticeengines.domain.exposed.cdl.DataIntegrationStatusMonitorMessage;
 import com.latticeengines.proxy.exposed.MicroserviceRestApiProxy;
 import com.latticeengines.proxy.exposed.ProxyInterface;
@@ -15,8 +19,9 @@ public class DataIntegrationMonitoringProxy extends MicroserviceRestApiProxy imp
         super("cdl");
     }
 
-    public Boolean createOrUpdateStatus(DataIntegrationStatusMonitorMessage message) {
+    public Map<String, Boolean> createOrUpdateStatus(List<DataIntegrationStatusMonitorMessage> messages) {
         String url = constructUrl(URL_PREFIX);
-        return post("create or update data integration status", url, message, Boolean.class);
+        return JsonUtils.convertMap(post("create or update data integration status", url, messages, Map.class),
+                String.class, Boolean.class);
     }
 }
