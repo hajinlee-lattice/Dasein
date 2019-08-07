08cd41da4b939bbf6184bae00e91dbfea878dc9f
Leonardo Gigli <lgigli@lattice-engines.com>
2019-08-07 08:26:41 -0700

    editing mapping

---
+++ le-ui/projects/atlas/app/connectors/iframe.component.js
@@ -0,0 +1,20 @@
+import React, { Component } from "common/react-vendor";
+
+export default class IFrameComponent extends Component {
+	constructor(props) {
+		super(props);
+		this.myRef = React.createRef();
+	}
+
+	componentDidMount() {
+		// this.refs.iframe
+		// 	.getDOMNode()
+		// 	.addEventListener("load", this.props.onLoad);
+		console.log(this.props);
+		this.props.mounted(this.myRef);
+		// this.props.mounted(this.myRef);
+	}
+	render() {
+		return <iframe ref={this.myRef} {...this.props} />;
+	}
+}
