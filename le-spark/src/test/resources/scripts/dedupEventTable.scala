02c2c52d4b00acdab72e9b3c0eb6cc6e6190b1dd
Sayali Sakhalkar <ssakhalkar@lattice-engines.com>
2019-08-19 12:09:18 -0700

    DP-9482 : Migrate dedupEventTable to Spark

--- le-spark/src/test/resources/scripts/dedupEventTable.scala
+++ le-spark/src/test/resources/scripts/dedupEventTable.scala
@@ -10,7 +10,7 @@ val eventTable: DataFrame = lattice.input(0)
 // read config
 val latticeAccId = lattice.params.get("LID_FIELD").asText()
 val latticeId = lattice.params.get("INT_LDC_LID").asText()
-val eventCol =  lattice.params.get("EVENT")
+val eventCol =  lattice.params.get("EVENT").asText()
 val intLdcRemoveCol = lattice.params.get("INT_LDC_REMOVED").asText()
 val intLdcDedupeIdCol = lattice.params.get("INT_LDC_DEDUPE_ID").asText()
 val sort_event = "__Sort_Event__"
@@ -19,15 +19,15 @@ val sort_event = "__Sort_Event__"
 // filter based on INT_LDC_REMOVED column
 val dfEventTableFilter = eventTable.filter(eventTable(intLdcRemoveCol)===0)
 // add sortEvent column mapped to Event : true = 1 and false = 0
-val dfFalseEvent = dfEventTableFilter.filter($"Event"===false).withColumn(sort_event, lit(0))
-val dfTrueEvent = dfEventTableFilter.filter($"Event"===true).withColumn(sort_event, lit(1))
+val dfFalseEvent = dfEventTableFilter.filter(eventTable(eventCol)===false).withColumn(sort_event, lit(0))
+val dfTrueEvent = dfEventTableFilter.filter(eventTable(eventCol)===true).withColumn(sort_event, lit(1))
 // merged output with new column : sortEvent
 val dfMergedEventOutput = dfFalseEvent.union(dfTrueEvent)
 // df with dedupId present and absent
-val dfHasDedupeId = dfMergedEventOutput.filter($"__LDC_DedupeId__".isNotNull)
-val dfNoDedupeId = dfMergedEventOutput.filter($"__LDC_DedupeId__".isNull)
-// dedup based on : group by INT_LDC_DEDUPE_ID column and sory by sortevent column in desc order
-val window_DedupeIdGroupSortDescCol = Window.partitionBy($"__LDC_DedupeId__").orderBy($"__Sort_Event__".desc)
+val dfHasDedupeId = dfMergedEventOutput.filter(eventTable(intLdcDedupeIdCol).isNotNull)
+val dfNoDedupeId = dfMergedEventOutput.filter(eventTable(intLdcDedupeIdCol).isNull)
+// dedup based on : group by INT_LDC_DEDUPE_ID column and sort by sortevent column in desc order
+val window_DedupeIdGroupSortDescCol = Window.partitionBy(eventTable(intLdcDedupeIdCol)).orderBy($"__Sort_Event__".desc)
 val dfMergedEventOutputDeduped = dfMergedEventOutput.withColumn("row_num", row_number() over window_DedupeIdGroupSortDescCol).where($"row_num" === 1).drop("row_num")
 val dfCombinedOutput = dfMergedEventOutputDeduped.union(dfNoDedupeId)
 // dedup by ID by ordering by sort_event column
