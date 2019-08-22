ee06bf04808d1dac672cd9f2960f82a9157c1636
slinlattice <slin@lattice-engines.com>
2019-08-22 15:24:37 -0700

    DP-11214 Fix DropFolderResourceDeploymentTestNG - need to create import system before creating drop folder

--- le-serviceapps/cdl/src/test/java/com/latticeengines/apps/cdl/controller/DropFolderResourceDeploymentTestNG.java
+++ le-serviceapps/cdl/src/test/java/com/latticeengines/apps/cdl/controller/DropFolderResourceDeploymentTestNG.java
@@ -9,13 +9,20 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.latticeengines.apps.cdl.testframework.CDLDeploymentTestNGBase;
+import com.latticeengines.domain.exposed.cdl.S3ImportSystem;
 import com.latticeengines.domain.exposed.query.BusinessEntity;
+import com.latticeengines.proxy.exposed.cdl.CDLProxy;
 import com.latticeengines.proxy.exposed.cdl.DropBoxProxy;
 
 public class DropFolderResourceDeploymentTestNG extends CDLDeploymentTestNGBase {
 
+    private static final String TEST_SYSTEM_NAME = "FirstSystem";
+
     @Inject
     private DropBoxProxy dropBoxProxy;
+
+    @Inject
+    private CDLProxy cdlProxy;
 
     @BeforeClass(groups = "deployment-app")
     public void setup() {
@@ -28,10 +35,21 @@ public class DropFolderResourceDeploymentTestNG extends CDLDeploymentTestNGBase 
         int defaultSize = subFolders.size();
         Assert.assertEquals(defaultSize, 5);
 
-        dropBoxProxy.createTemplateFolder(mainCustomerSpace, "FirstSystem", null, null);
+        // create import system
+        S3ImportSystem sys = new S3ImportSystem();
+        sys.setName(TEST_SYSTEM_NAME);
+        sys.setDisplayName(TEST_SYSTEM_NAME);
+        sys.setPriority(1);
+        sys.setSystemType(S3ImportSystem.SystemType.Other);
+        sys.setTenant(mainTestTenant);
+        cdlProxy.createS3ImportSystem(mainCustomerSpace, sys);
+
+        // create drop folder
+        dropBoxProxy.createTemplateFolder(mainCustomerSpace, TEST_SYSTEM_NAME, null, null);
         subFolders = dropBoxProxy.getAllSubFolders(mainCustomerSpace, null, null, null);
         defaultSize = subFolders.size();
         dropBoxProxy.createTemplateFolder(mainCustomerSpace, null,
                     BusinessEntity.Account.name(), null);
+        // TODO add assertion on result
     }
 }
