1125770c3b23d34c4a1ffb998bb2d9c84d9f16f6
Dandan Zheng <dzheng@lattice-engines.com>
2019-08-28 14:56:08 -0700

    Add more logs for PLS-14499

--- le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/dataflow/OrphanTransactionExportFlow.java
+++ le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/dataflow/OrphanTransactionExportFlow.java
@@ -1,12 +1,16 @@
 package com.latticeengines.cdl.dataflow;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.latticeengines.dataflow.exposed.builder.Node;
@@ -18,6 +22,8 @@ import com.latticeengines.domain.exposed.serviceflows.cdl.dataflow.OrphanTransac
 
 @Component(OrphanTransactionExportFlow.DATAFLOW_BEAN_NAME)
 public class OrphanTransactionExportFlow extends TypesafeDataFlowBuilder<OrphanTransactionExportParameters> {
+
+    private static final Logger log = LoggerFactory.getLogger(OrphanTransactionExportFlow.class);
 
     public static final String DATAFLOW_BEAN_NAME = "orphanTransactionExportFlow";
     public static final String RENAME_PREFIX = "OrphanTransactionExport_";
@@ -52,6 +58,18 @@ public class OrphanTransactionExportFlow extends TypesafeDataFlowBuilder<OrphanT
         List<String> filterFields = Arrays.asList(renamedAccount, renamedProduct);
         result = result.filter(String.format("%s == null || %s == null", renamedAccount, renamedProduct),
                 new FieldList(filterFields));
+        // Only for debugging purpose. Will remove after PLS-14499 is resolved
+        List<String> resFlds = new ArrayList<>(result.getFieldNames());
+        Collections.sort(resFlds);
+        log.info("Current transaction table attributes: " + String.join(",", resFlds));
+        Collections.sort(retainFields);
+        log.info("Transaction table attributes to retain: " + String.join(",", retainFields));
+        Set<String> resFldSet = new HashSet<>(resFlds);
+        List<String> diffFlds = retainFields.stream().filter(fld -> !resFldSet.contains(fld))
+                .collect(Collectors.toList());
+        log.info("Attributes to retain which don't exist in current transaction table: " + String.join(",", diffFlds));
+        retainFields = retainFields.stream().filter(fld -> resFldSet.contains(fld)).collect(Collectors.toList());
+
         result = result.retain(new FieldList(retainFields));
         return result;
     }
