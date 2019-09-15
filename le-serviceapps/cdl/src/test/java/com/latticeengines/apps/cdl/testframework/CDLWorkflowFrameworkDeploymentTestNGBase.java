c4307fb3ca8364c1a53146ba53768b14ae4f5cf1
slinlattice <slin@lattice-engines.com>
2019-09-14 21:03:05 -0700

    Default EntityMatchGA flag to false for cdl deployment tests

--- le-serviceapps/cdl/src/test/java/com/latticeengines/apps/cdl/testframework/CDLWorkflowFrameworkDeploymentTestNGBase.java
+++ le-serviceapps/cdl/src/test/java/com/latticeengines/apps/cdl/testframework/CDLWorkflowFrameworkDeploymentTestNGBase.java
@@ -1,15 +1,9 @@
 package com.latticeengines.apps.cdl.testframework;
 
-import static org.testng.Assert.assertNotNull;
-
 import java.util.Map;
 
 import javax.annotation.Resource;
 
-import org.apache.commons.collections4.MapUtils;
-
-import com.latticeengines.db.exposed.util.MultiTenantContext;
-import com.latticeengines.domain.exposed.admin.LatticeProduct;
 import com.latticeengines.domain.exposed.camille.CustomerSpace;
 import com.latticeengines.testframework.service.impl.GlobalAuthDeploymentTestBed;
 import com.latticeengines.yarn.functionalframework.YarnFunctionalTestNGBase;
@@ -20,18 +14,13 @@ public abstract class CDLWorkflowFrameworkDeploymentTestNGBase extends CDLWorkfl
     protected GlobalAuthDeploymentTestBed testBed;
 
     protected void setupTestEnvironment() {
-        setupTestEnvironment(LatticeProduct.CG);
-    }
-
-    protected void setupTestEnvironment(LatticeProduct product) {
-        testBed.bootstrapForProduct(product);
-        mainTestTenant = testBed.getMainTestTenant();
-        mainTestCustomerSpace = CustomerSpace.parse(mainTestTenant.getId());
-        MultiTenantContext.setTenant(mainTestTenant);
-        testBed.switchToSuperAdmin();
-        assertNotNull(MultiTenantContext.getTenant());
-        checkpointService.setMainTestTenant(mainTestTenant);
-        setupYarnPlatform();
+        try {
+            setupEnd2EndTestEnvironment(null);
+            mainTestCustomerSpace = CustomerSpace.parse(mainTestTenant.getId());
+            setupYarnPlatform();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     protected void setupYarnPlatform() {
@@ -40,13 +29,9 @@ public abstract class CDLWorkflowFrameworkDeploymentTestNGBase extends CDLWorkfl
     }
 
     protected void setupEnd2EndTestEnvironment(Map<String, Boolean> featureFlagMap) throws Exception {
-        if (MapUtils.isEmpty(featureFlagMap)) {
-            setupTestEnvironment();
-        } else {
-            setupTestEnvironmentWithFeatureFlags(featureFlagMap);
-            mainTestTenant = testBed.getMainTestTenant();
-            mainTestCustomerSpace = CustomerSpace.parse(mainTestTenant.getId());
-        }
+        setupTestEnvironmentWithFeatureFlags(featureFlagMap);
+        mainTestTenant = testBed.getMainTestTenant();
+        mainTestCustomerSpace = CustomerSpace.parse(mainTestTenant.getId());
     }
 
 }
