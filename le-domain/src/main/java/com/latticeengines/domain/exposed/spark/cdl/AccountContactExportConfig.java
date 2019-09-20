aed64b43e2c987fc0d093b769e7e6c3110c7f751
lzhao-lattice <LZhao@lattice-engines.com>
2019-09-20 17:26:07 +0800

    PLS-14734: fix duplicate attribute name issue when doing segment export

--- le-domain/src/main/java/com/latticeengines/domain/exposed/spark/cdl/AccountContactExportConfig.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/spark/cdl/AccountContactExportConfig.java
@@ -1,7 +1,6 @@
 package com.latticeengines.domain.exposed.spark.cdl;
 
 import java.util.List;
-import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.latticeengines.domain.exposed.pls.AccountContactExportContext;
@@ -23,9 +22,6 @@ public class AccountContactExportConfig extends SparkJobConfig {
 
     @JsonProperty("DropKeys")
     private List<String> dropKeys;
-
-    @JsonProperty("ContactColumns")
-    private Map<String, String> contactColumnNames;
 
     @Override
     @JsonProperty("Name")
