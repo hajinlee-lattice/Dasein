4dfef48260862ff3b8bce7850b70f4a20c11fcf7
lzhao-lattice <LZhao@lattice-engines.com>
2019-09-26 01:22:51 +0800

    PLS-14871: fix unit test

--- le-serviceapps/cdl/src/test/java/com/latticeengines/apps/cdl/workflow/EntityExportWorkflowDeploymentTestNG.java
+++ le-serviceapps/cdl/src/test/java/com/latticeengines/apps/cdl/workflow/EntityExportWorkflowDeploymentTestNG.java
@@ -249,7 +249,7 @@ public class EntityExportWorkflowDeploymentTestNG extends CDLWorkflowFrameworkDe
 
     protected void verifyCsvGzHeder(Map<String, Integer> headerMap) {
         // make sure header map contains account and contact id
-        Assert.assertTrue(headerMap.containsKey("ID"), "Header map: " + JsonUtils.serialize(headerMap));
+        Assert.assertTrue(headerMap.containsKey("Id"), "Header map: " + JsonUtils.serialize(headerMap));
         Assert.assertTrue(headerMap.containsKey("ContactId"), "Header map: " + JsonUtils.serialize(headerMap));
         Assert.assertTrue(headerMap.containsKey("CEO Name"), "Header map: " + JsonUtils.serialize(headerMap));
         Assert.assertTrue(headerMap.containsKey("Test Date"), "Header map: " + JsonUtils.serialize(headerMap));
