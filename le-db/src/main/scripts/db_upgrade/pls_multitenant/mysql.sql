1d15f301f5b4e21d599336c8e806229677f1c6d8
slinlattice <slin@lattice-engines.com>
2019-10-03 12:44:59 -0700

    Add Stream and Dimension sql table creation ddl to upgrade script

---
+++ le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
@@ -0,0 +1,58 @@
+USE `PLS_MultiTenant`;
+
+CREATE PROCEDURE `UpdatePLSTables`()
+BEGIN
+
+    CREATE TABLE `ATLAS_STREAM`
+    (
+        `PID`               bigint(20)                              NOT NULL AUTO_INCREMENT,
+        `AGGR_ENTITIES`     json                                    NOT NULL,
+        `ATTRIBUTE_DERIVER` json    DEFAULT NULL,
+        `CREATED`           datetime                                NOT NULL,
+        `DATE_ATTRIBUTE`    varchar(50) COLLATE utf8mb4_unicode_ci  NOT NULL,
+        `MATCH_ENTITIES`    json                                    NOT NULL,
+        `NAME`              varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+        `PERIODS`           json                                    NOT NULL,
+        `RETENTION_DAYS`    int(11) DEFAULT NULL,
+        `UPDATED`           datetime                                NOT NULL,
+        `FK_TASK_ID`        bigint(20)                              NOT NULL,
+        `FK_TENANT_ID`      bigint(20)                              NOT NULL,
+        PRIMARY KEY (`PID`),
+        UNIQUE KEY `UKlp9f8nse09hyel7myetcxst50` (`NAME`, `FK_TENANT_ID`),
+        KEY `FK_ATLASSTREAM_FKTASKID_DATAFEEDTASK` (`FK_TASK_ID`),
+        KEY `FK_ATLASSTREAM_FKTENANTID_TENANT` (`FK_TENANT_ID`),
+        CONSTRAINT `FK_ATLASSTREAM_FKTASKID_DATAFEEDTASK` FOREIGN KEY (`FK_TASK_ID`) REFERENCES `DATAFEED_TASK` (`PID`) ON DELETE CASCADE,
+        CONSTRAINT `FK_ATLASSTREAM_FKTENANTID_TENANT` FOREIGN KEY (`FK_TENANT_ID`) REFERENCES `TENANT` (`TENANT_PID`) ON DELETE CASCADE
+    ) ENGINE = InnoDB
+      DEFAULT CHARSET = utf8mb4
+      COLLATE = utf8mb4_unicode_ci;
+
+    CREATE TABLE `ATLAS_STREAM_DIMENSION`
+    (
+        `PID`           bigint(20)                              NOT NULL AUTO_INCREMENT,
+        `CALCULATOR`    json                                    NOT NULL,
+        `CREATED`       datetime                                NOT NULL,
+        `DISPLAY_NAME`  varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+        `GENERATOR`     json                                    NOT NULL,
+        `NAME`          varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+        `UPDATED`       datetime                                NOT NULL,
+        `FK_CATALOG_ID` bigint(20) DEFAULT NULL,
+        `FK_STREAM_ID`  bigint(20)                              NOT NULL,
+        `FK_TENANT_ID`  bigint(20)                              NOT NULL,
+        PRIMARY KEY (`PID`),
+        UNIQUE KEY `UK1sg01cuqrjubjct3t5sdtxir0` (`NAME`, `FK_STREAM_ID`, `FK_TENANT_ID`),
+        KEY `FK_ATLASSTREAMDIMENSION_FKCATALOGID_ATLASCATALOG` (`FK_CATALOG_ID`),
+        KEY `FK_ATLASSTREAMDIMENSION_FKSTREAMID_ATLASSTREAM` (`FK_STREAM_ID`),
+        KEY `FK_ATLASSTREAMDIMENSION_FKTENANTID_TENANT` (`FK_TENANT_ID`),
+        CONSTRAINT `FK_ATLASSTREAMDIMENSION_FKCATALOGID_ATLASCATALOG` FOREIGN KEY (`FK_CATALOG_ID`) REFERENCES `ATLAS_CATALOG` (`PID`) ON DELETE CASCADE,
+        CONSTRAINT `FK_ATLASSTREAMDIMENSION_FKSTREAMID_ATLASSTREAM` FOREIGN KEY (`FK_STREAM_ID`) REFERENCES `ATLAS_STREAM` (`PID`) ON DELETE CASCADE,
+        CONSTRAINT `FK_ATLASSTREAMDIMENSION_FKTENANTID_TENANT` FOREIGN KEY (`FK_TENANT_ID`) REFERENCES `TENANT` (`TENANT_PID`) ON DELETE CASCADE
+    ) ENGINE = InnoDB
+      DEFAULT CHARSET = utf8mb4
+      COLLATE = utf8mb4_unicode_ci;
+
+END;
+//
+DELIMITER;
+
+CALL `UpdatePLSTables`();
