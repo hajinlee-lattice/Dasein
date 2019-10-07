cd6cb728ce3b983ccea1ebc2617ce04d67fe6a6f
Perry Liu <perry.liu@lattice-engines.com>
2019-10-07 02:20:37 -0700

    PLS-14943 Log Referer Header for API Requests

--- le-objectapi/src/main/java/com/latticeengines/objectapi/util/LoggerInterceptor.java
+++ le-objectapi/src/main/java/com/latticeengines/objectapi/util/LoggerInterceptor.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import com.latticeengines.db.exposed.util.MultiTenantContext;
@@ -50,7 +51,7 @@ public class LoggerInterceptor extends HandlerInterceptorAdapter {
             long startTime = System.currentTimeMillis();
             Tenant tenant = MultiTenantContext.getTenant();
             log.info("ObjectAPI Request URL:" + request.getRequestURL().toString() + " Start Time=" + startTime
-                    + " Tenant=" + tenant.getName());
+                    + " Tenant=" + tenant.getName() + " Referrer=" + request.getHeader(HttpHeaders.REFERER));
             request.setAttribute(START_TIME, startTime);
         }
         return true;
@@ -63,7 +64,8 @@ public class LoggerInterceptor extends HandlerInterceptorAdapter {
             long startTime = (Long) request.getAttribute(START_TIME);
             Tenant tenant = MultiTenantContext.getTenant();
             log.info("ObjectAPI Request URL:" + request.getRequestURL().toString() + " Time Taken="
-                    + (System.currentTimeMillis() - startTime) + "ms" + " Tenant=" + tenant.getName());
+                    + (System.currentTimeMillis() - startTime) + "ms" + " Tenant=" + tenant.getName() + " Referrer="
+                    + request.getHeader(HttpHeaders.REFERER));
         }
     }
 }
