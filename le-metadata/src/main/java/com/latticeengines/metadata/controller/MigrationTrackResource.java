4c633ada6a5c6b71894a35dfeecd6030f7bd3358
Raymond Feng <rfeng@lattice-engines.com>
2019-10-11 16:03:58 -0700

    DP-11617 rollback restore original templates marked during migrate workflow

--- le-metadata/src/main/java/com/latticeengines/metadata/controller/MigrationTrackResource.java
+++ le-metadata/src/main/java/com/latticeengines/metadata/controller/MigrationTrackResource.java
@@ -8,6 +8,7 @@ import javax.inject.Inject;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.latticeengines.db.exposed.entitymgr.TenantEntityMgr;
 import com.latticeengines.domain.exposed.camille.CustomerSpace;
+import com.latticeengines.domain.exposed.cdl.ImportMigrateReport;
 import com.latticeengines.domain.exposed.cdl.ImportMigrateTracking;
 import com.latticeengines.domain.exposed.metadata.DataCollection;
 import com.latticeengines.domain.exposed.metadata.MigrationTrack;
 import com.latticeengines.domain.exposed.metadata.TableRoleInCollection;
 import com.latticeengines.domain.exposed.security.Tenant;
 import com.latticeengines.metadata.entitymgr.MigrationTrackEntityMgr;
+import com.latticeengines.proxy.exposed.cdl.CDLProxy;
 
 import io.swagger.annotations.Api;
 
@@ -35,6 +38,9 @@ public class MigrationTrackResource {
 
     @Inject
     private TenantEntityMgr tenantEntityMgr;
+
+    @Inject
+    private CDLProxy cdlProxy;
 
     @GetMapping(value = "")
     @ResponseBody
@@ -114,6 +120,29 @@ public class MigrationTrackResource {
         }
         track.setImportMigrateTracking(importMigrateTracking);
         migrationTrackEntityMgr.update(track);
+        return true;
+    }
+
+    @PostMapping(value = "/tenants/{customerSpace}/restoreAllTemplates")
+    @ResponseBody
+    public Boolean restoreAllTemplates(@PathVariable(name = "customerSpace") String customerSpace) {
+        customerSpace = CustomerSpace.parse(customerSpace).toString();
+        Tenant tenant = tenantEntityMgr.findByTenantId(customerSpace);
+        if (tenant == null) {
+            throw new IllegalArgumentException(String.format("Tenant %s not found", customerSpace));
+        }
+        MigrationTrack track = migrationTrackEntityMgr.findByTenant((tenant));
+        if (track == null) {
+            throw new IllegalArgumentException(String.format("Tenant %s is not tracked for migration", customerSpace));
+        }
+        ImportMigrateTracking tracking = track.getImportMigrateTracking();
+        if (tracking == null || tracking.getReport() == null || tracking.getReport().getBackupTemplateList() == null) {
+            throw new IllegalArgumentException(String.format("Unable to find backup template record for tenant %s.", customerSpace));
+        }
+        List<ImportMigrateReport.BackupInfo> backupTemplateList = tracking.getReport().getBackupTemplateList();
+        for (ImportMigrateReport.BackupInfo backupInfo : backupTemplateList) {
+            cdlProxy.restoreTemplate(customerSpace, backupInfo.getTaskId(), backupInfo.getBackupName(), false);
+        }
         return true;
     }
 }
