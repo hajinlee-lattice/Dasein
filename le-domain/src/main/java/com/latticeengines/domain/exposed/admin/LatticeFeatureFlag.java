0ee1fbedb2670796dfabe88aab10a7b8cf7ca6e6
lzhao-lattice <LZhao@lattice-engines.com>
2019-10-17 16:15:09 +0800

    Atlas-export: retire feature flag for spark export
    Just remove the feature flag and keep the old logical code since download function need to use it. I think after one or two release, we can remove both code change for the old redshift export logic.
    
    tests:
    EntityExportWorkflowDeploymentTestNG
    EntityExportWorkflowWithEMDeploymentTestNG

--- le-domain/src/main/java/com/latticeengines/domain/exposed/admin/LatticeFeatureFlag.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/admin/LatticeFeatureFlag.java
@@ -43,8 +43,7 @@ public enum LatticeFeatureFlag {
     ENABLE_MULTI_TEMPLATE_IMPORT("EnableMultiTemplateImport", "Allows multiple templates for import"), //
     ENABLE_FACEBOOK_INTEGRATION("EnableFacebookIntegration", "Enable Facebook integration"), //
     ENABLE_LINKEDIN_INTEGRATION("EnableLinkedInIntegration", "Enable LinkedIn integration"), //
-    ENABLE_OUTREACH_INTEGRATION("EnableOutreachIntegration", "Enable Outreach integration"), //
-    ENABLE_EXPORT_WITH_SPARK_SQL("EnableExportWithSparkSQL", "Enable segment export using spark SQL"), //
+    ENABLE_OUTREACH_INTEGRATION("EnableOutreachIntegration", "Enable Outreach integration"),
 
     @Deprecated
     QUOTA(true, "Quota", "Quota"), //
