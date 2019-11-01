9eef5f4045708c500dd092b3cd2ec298d43bd311
Lei Ming <lming@lattice-engines.com>
2019-11-01 11:39:59 -0700

    DP-11403 For entity match tenant always fuzzy match for modeling workflow

--- le-domain/src/main/java/com/latticeengines/domain/exposed/serviceflows/cdl/CustomEventMatchWorkflowConfiguration.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/serviceflows/cdl/CustomEventMatchWorkflowConfiguration.java
@@ -226,11 +226,6 @@ public class CustomEventMatchWorkflowConfiguration extends BaseCDLWorkflowConfig
             return this;
         }
 
-        public Builder mapToLatticeAccount(boolean mapToLatticeAccount) {
-            matchCdlAccount.setMapToLatticeAccount(mapToLatticeAccount);
-            return this;
-        }
-
         public CustomEventMatchWorkflowConfiguration build() {
             matchAccountWithIdBuilder.matchType(MatchStepConfiguration.LDC);
             matchAccountWithoutIdBuilder.matchType(MatchStepConfiguration.LDC);
