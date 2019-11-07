61c24281849ae42b4fb14dc0362bd47edd9fb389
Yunlong Gao <ygao@lattice-engines.com>
2019-11-07 09:13:51 -0800

    PLS-15374 Create functional test for Delta Campaign Launch spark script, forgot to add two files

---
+++ le-spark/src/test/java/com/latticeengines/spark/exposed/job/cdl/CampaignLaunchUtils.java
@@ -0,0 +1,217 @@
+package com.latticeengines.spark.exposed.job.cdl;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.collections.CollectionUtils;
+
+import com.google.common.collect.ImmutableList;
+import com.latticeengines.domain.exposed.metadata.InterfaceName;
+import com.latticeengines.domain.exposed.playmakercore.NonStandardRecColumnName;
+import com.latticeengines.domain.exposed.playmakercore.RecommendationColumnName;
+
+public class CampaignLaunchUtils {
+    // ---- start of Facebook Account and Contact columns---
+    public static List<String> facebookRecommendationAccountColumns() {
+        return ImmutableList.<String> builder().addAll(generateAccountColsRecIncludedForFacebook())
+                .addAll(generateAccountColsRecNotIncludedStdForFacebook())
+                .addAll(generateAccountColsRecNotIncludedNonStdForFacebook())
+                .addAll(CollectionUtils.isNotEmpty(generateContactColsForFacebook())
+                        ? Collections.singletonList(RecommendationColumnName.CONTACTS.name())
+                        : Collections.emptyList())
+                .build();
+    }
+
+    public static List<String> facebookRecommendationContactColumns() {
+        return generateContactColsForFacebook();
+    }
+
+    public static List<String> generateAccountColsRecIncludedForFacebook() {
+        return Arrays.asList(InterfaceName.CompanyName.name());
+    }
+
+    public static List<String> generateAccountColsRecNotIncludedStdForFacebook() {
+        return Arrays.asList(InterfaceName.Website.name());
+    }
+
+    public static List<String> generateAccountColsRecNotIncludedNonStdForFacebook() {
+        return Collections.emptyList();
+    }
+
+    public static List<String> generateContactColsForFacebook() {
+        return Arrays.asList(InterfaceName.FirstName.name(), InterfaceName.LastName.name(), InterfaceName.Email.name(),
+                InterfaceName.Country.name(), InterfaceName.PhoneNumber.name(), InterfaceName.PostalCode.name(),
+                InterfaceName.City.name(), InterfaceName.ContactId.name(), InterfaceName.State.name());
+    }
+
+    // ---- end of Facebook Account and Contact columns---
+
+    // ---- start of Google Account and Contact columns---
+    public static List<String> googleRecommendationAccountColumns() {
+        return ImmutableList.<String> builder().addAll(generateAccountColsRecIncludedForGoogle())
+                .addAll(generateAccountColsRecNotIncludedStdForGoogle())
+                .addAll(generateAccountColsRecNotIncludedNonStdForGoogle())
+                .addAll(CollectionUtils.isNotEmpty(generateContactColsForGoogle())
+                        ? Collections.singletonList(RecommendationColumnName.CONTACTS.name())
+                        : Collections.emptyList())
+                .build();
+    }
+
+    public static List<String> googleRecommendationContactColumns() {
+        return generateContactColsForGoogle();
+    }
+
+    public static List<String> generateAccountColsRecIncludedForGoogle() {
+        return Collections.emptyList();
+    }
+
+    public static List<String> generateAccountColsRecNotIncludedStdForGoogle() {
+        return Collections.emptyList();
+    }
+
+    public static List<String> generateAccountColsRecNotIncludedNonStdForGoogle() {
+        return Collections.emptyList();
+    }
+
+    public static List<String> generateContactColsForGoogle() {
+        return Arrays.asList(InterfaceName.FirstName.name(), InterfaceName.LastName.name(), InterfaceName.Email.name(),
+                InterfaceName.Country.name(), InterfaceName.PhoneNumber.name(), InterfaceName.PostalCode.name());
+    }
+
+    // ---- end of Google Account and Contact columns---
+
+    // ---- start of LinkedIn Account and Contact columns---
+    public static List<String> linkedInRecommendationAccountColumns() {
+        return ImmutableList.<String> builder().addAll(generateAccountColsRecIncludedForLinkedIn())
+                .addAll(generateAccountColsRecNotIncludedStdForLinkedIn())
+                .addAll(generateAccountColsRecNotIncludedNonStdForLinkedIn())
+                .addAll(CollectionUtils.isNotEmpty(generateContactColsForLinkedIn())
+                        ? Collections.singletonList(RecommendationColumnName.CONTACTS.name())
+                        : Collections.emptyList())
+                .build();
+    }
+
+    public static List<String> linkedInRecommendationContactColumns() {
+        return generateContactColsForLinkedIn();
+    }
+
+    public static List<String> generateAccountColsRecIncludedForLinkedIn() {
+        return Arrays.asList(RecommendationColumnName.COMPANY_NAME.name()).stream()
+                .map(col -> RecommendationColumnName.RECOMMENDATION_COLUMN_TO_INTERNAL_NAME_MAP.getOrDefault(col, col))
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> generateAccountColsRecNotIncludedStdForLinkedIn() {
+        return Arrays.asList(InterfaceName.Website.name());
+    }
+
+    public static List<String> generateAccountColsRecNotIncludedNonStdForLinkedIn() {
+        return Collections.emptyList();
+    }
+
+    public static List<String> generateContactColsForLinkedIn() {
+        return Arrays.asList(InterfaceName.Email.name());
+    }
+
+    // ---- end of LinkedIn Account and Contact columns---
+
+    // ---- start of S3 Account and Contact columns---
+
+    public static List<String> s3RecommendationAccountColumns() {
+        return ImmutableList.<String> builder().addAll(generateAccountColsRecIncludedForS3())
+                .addAll(generateAccountColsRecNotIncludedStdForS3())
+                .addAll(generateAccountColsRecNotIncludedNonStdForS3())
+                .addAll(CollectionUtils.isNotEmpty(generateContactColsForS3())
+                        ? Collections.singletonList(RecommendationColumnName.CONTACTS.name())
+                        : Collections.emptyList())
+                .build();
+    }
+
+    public static List<String> s3RecommendationContactColumns() {
+        return generateContactColsForS3();
+    }
+
+    public static List<String> generateAccountColsRecIncludedForS3() {
+        return Arrays.asList(RecommendationColumnName.COMPANY_NAME.name(), RecommendationColumnName.ACCOUNT_ID.name(),
+                RecommendationColumnName.DESCRIPTION.name(), RecommendationColumnName.PLAY_ID.name(),
+                RecommendationColumnName.LAUNCH_DATE.name(), RecommendationColumnName.LAUNCH_ID.name(),
+                RecommendationColumnName.DESTINATION_ORG_ID.name(),
+                RecommendationColumnName.DESTINATION_SYS_TYPE.name(),
+                RecommendationColumnName.LAST_UPDATED_TIMESTAMP.name(), RecommendationColumnName.MONETARY_VALUE.name(),
+                RecommendationColumnName.PRIORITY_ID.name(), RecommendationColumnName.PRIORITY_DISPLAY_NAME.name(),
+                RecommendationColumnName.LIKELIHOOD.name(), RecommendationColumnName.LIFT.name(),
+                RecommendationColumnName.RATING_MODEL_ID.name()).stream()
+                .map(col -> RecommendationColumnName.RECOMMENDATION_COLUMN_TO_INTERNAL_NAME_MAP.getOrDefault(col, col))
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> generateAccountColsRecNotIncludedStdForS3() {
+        return Arrays.asList(InterfaceName.Website.name(), InterfaceName.CreatedDate.name());
+    }
+
+    public static List<String> generateAccountColsRecNotIncludedNonStdForS3() {
+        return Arrays.asList(NonStandardRecColumnName.DESTINATION_SYS_NAME.name(),
+                NonStandardRecColumnName.PLAY_NAME.name(), NonStandardRecColumnName.RATING_MODEL_NAME.name(),
+                NonStandardRecColumnName.SEGMENT_NAME.name());
+    }
+
+    public static List<String> generateContactColsForS3() {
+        return ImmutableList.<String> builder().add(InterfaceName.Email.name())
+                .add(InterfaceName.Address_Street_1.name()).add(InterfaceName.PhoneNumber.name())
+                .add(InterfaceName.State.name()).add(InterfaceName.PostalCode.name()).add(InterfaceName.Country.name())
+                .add(InterfaceName.SalesforceContactID.name()).add(InterfaceName.City.name())
+                .add(InterfaceName.ContactId.name()).add(InterfaceName.Name.name()).add(InterfaceName.FirstName.name())
+                .add(InterfaceName.LastName.name()).build();
+    }
+
+    // ---- end of S3 Account and Contact columns---
+
+    // ---- start of Marketo Account and Contact columns---
+
+    public static List<String> marketoRecommendationAccountColumns() {
+        return ImmutableList.<String> builder().addAll(generateAccountColsRecIncludedForMarketo())
+                .addAll(generateAccountColsRecNotIncludedStdForMarketo())
+                .addAll(generateAccountColsRecNotIncludedNonStdForMarketo())
+                .addAll(CollectionUtils.isNotEmpty(generateContactColsForMarketo())
+                        ? Collections.singletonList(RecommendationColumnName.CONTACTS.name())
+                        : Collections.emptyList())
+                .build();
+    }
+
+    public static List<String> marketoRecommendationContactColumns() {
+        return generateContactColsForMarketo();
+    }
+
+    public static List<String> generateAccountColsRecIncludedForMarketo() {
+        return Arrays
+                .asList(RecommendationColumnName.ACCOUNT_ID.name(), RecommendationColumnName.PLAY_ID.name(),
+                        RecommendationColumnName.MONETARY_VALUE.name(), RecommendationColumnName.LIKELIHOOD.name(),
+                        RecommendationColumnName.COMPANY_NAME.name(), RecommendationColumnName.PRIORITY_ID.name(),
+                        RecommendationColumnName.PRIORITY_DISPLAY_NAME.name(), RecommendationColumnName.LIFT.name(),
+                        RecommendationColumnName.RATING_MODEL_ID.name())
+                .stream()
+                .map(col -> RecommendationColumnName.RECOMMENDATION_COLUMN_TO_INTERNAL_NAME_MAP.getOrDefault(col, col))
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> generateAccountColsRecNotIncludedStdForMarketo() {
+        return Arrays.asList(InterfaceName.Website.name());
+    }
+
+    public static List<String> generateAccountColsRecNotIncludedNonStdForMarketo() {
+        return Arrays.asList(NonStandardRecColumnName.DESTINATION_SYS_NAME.name(),
+                NonStandardRecColumnName.PLAY_NAME.name(), NonStandardRecColumnName.RATING_MODEL_NAME.name(),
+                NonStandardRecColumnName.SEGMENT_NAME.name());
+    }
+
+    public static List<String> generateContactColsForMarketo() {
+        return Arrays.asList(InterfaceName.FirstName.name(), InterfaceName.LastName.name(), InterfaceName.Email.name(),
+                InterfaceName.Country.name(), InterfaceName.PhoneNumber.name(), InterfaceName.PostalCode.name(),
+                InterfaceName.Address_Street_1.name(), InterfaceName.City.name(), InterfaceName.ContactName.name(),
+                InterfaceName.State.name());
+    }
+
+    // ---- end of Marketo Account and Contact columns---
+}
---
+++ le-spark/src/test/java/com/latticeengines/spark/exposed/job/cdl/DeltaCampaignLaunchTestNG.java
@@ -0,0 +1,269 @@
+package com.latticeengines.spark.exposed.job.cdl;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.latticeengines.common.exposed.util.CipherUtils;
+import com.latticeengines.domain.exposed.cdl.CDLExternalSystemType;
+import com.latticeengines.domain.exposed.metadata.InterfaceName;
+import com.latticeengines.domain.exposed.metadata.MetadataSegment;
+import com.latticeengines.domain.exposed.playmaker.PlaymakerConstants;
+import com.latticeengines.domain.exposed.pls.AIModel;
+import com.latticeengines.domain.exposed.pls.DeltaCampaignLaunchSparkContext;
+import com.latticeengines.domain.exposed.pls.DeltaCampaignLaunchSparkContext.DeltaCampaignLaunchSparkContextBuilder;
+import com.latticeengines.domain.exposed.pls.Play;
+import com.latticeengines.domain.exposed.pls.PlayLaunch;
+import com.latticeengines.domain.exposed.pls.RatingEngine;
+import com.latticeengines.domain.exposed.pls.RatingEngineType;
+import com.latticeengines.domain.exposed.security.Tenant;
+import com.latticeengines.domain.exposed.spark.SparkJobResult;
+import com.latticeengines.domain.exposed.spark.cdl.CreateDeltaRecommendationConfig;
+import com.latticeengines.spark.testframework.TestJoinTestNGBase;
+
+public class DeltaCampaignLaunchTestNG extends TestJoinTestNGBase {
+
+    private static final Logger log = LoggerFactory.getLogger(DeltaCampaignLaunchTestNG.class);
+
+    private static final String ratingId = RatingEngine.generateIdStr();
+    private static final String destinationAccountId = "D41000001Q3z4EAC";
+    private static final int completeContactPerAccount = 10;
+    private static final int addOrDeleteContactPerAccount = 5;
+    private String addAccountData;
+    private String addContactData;
+    private String deleteAccountData;
+    private String deleteContactData;
+    private String completeContactData;
+    private Object[][] addAccounts;
+    private Object[][] addContacts;
+    private Object[][] deleteAccounts;
+    private Object[][] deleteContacts;
+    private Object[][] completeContacts;
+
+    @Override
+    @BeforeClass(groups = "functional")
+    public void setup() {
+        super.setup();
+        uploadInputAvro();
+    }
+
+    @Test(groups = "functional")
+    public void runTest() {
+        CreateDeltaRecommendationConfig sparkConfig = new CreateDeltaRecommendationConfig();
+        DeltaCampaignLaunchSparkContext deltaCampaignLaunchSparkContext = generateDeltaCampaignLaunchSparkContextForS3();
+        sparkConfig.setDeltaCampaignLaunchSparkContext(deltaCampaignLaunchSparkContext);
+        sparkConfig.setTargetNums(3);
+        SparkJobResult result = runSparkJob(CreateDeltaRecommendationsJob.class, sparkConfig);
+    }
+
+    private DeltaCampaignLaunchSparkContext generateDeltaCampaignLaunchSparkContextForS3() {
+
+        Tenant tenant = new Tenant("DeltaCampaignLaunchTestNG");
+        tenant.setPid(1L);
+        PlayLaunch playLaunch = new PlayLaunch();
+        playLaunch.setCreated(new Date());
+        playLaunch.setId(PlayLaunch.generateLaunchId());
+        playLaunch.setDestinationAccountId(destinationAccountId);
+        playLaunch.setDestinationSysType(CDLExternalSystemType.CRM);
+        MetadataSegment segment = new MetadataSegment();
+        Play play = new Play();
+        play.setTargetSegment(segment);
+        play.setDescription("play description");
+        play.setName(UUID.randomUUID().toString());
+        playLaunch.setPlay(play);
+        long launchTime = new Date().getTime();
+        RatingEngine ratingEngine = new RatingEngine();
+        play.setRatingEngine(ratingEngine);
+        ratingEngine.setId(ratingId);
+        ratingEngine.setType(RatingEngineType.CROSS_SELL);
+        AIModel aiModel = new AIModel();
+        aiModel.setId(AIModel.generateIdStr());
+        aiModel.setCreatedBy(ratingEngine.getCreatedBy());
+        aiModel.setUpdatedBy(ratingEngine.getUpdatedBy());
+        aiModel.setRatingEngine(ratingEngine);
+        ratingEngine.setLatestIteration(aiModel);
+        String saltHint = CipherUtils.generateKey();
+        String key = CipherUtils.generateKey();
+        String pw = CipherUtils.encrypt(dataDbPassword, key, saltHint);
+
+        DeltaCampaignLaunchSparkContext deltaCampaignLaunchSparkContext = new DeltaCampaignLaunchSparkContextBuilder()//
+                .tenant(tenant) //
+                .playName(play.getName()) //
+                .playLaunchId(playLaunch.getId()) //
+                .playLaunch(playLaunch) //
+                .play(play) //
+                .ratingEngine(ratingEngine) //
+                .segment(segment) //
+                .launchTimestampMillis(launchTime) //
+                .ratingId(ratingId) //
+                .publishedIteration(aiModel) //
+                .dataDbDriver(dataDbDriver) //
+                .dataDbUrl(dataDbUrl) //
+                .dataDbUser(dataDbUser) //
+                .saltHint(saltHint) //
+                .encryptionKey(key) //
+                .dataDbPassword(pw) //
+                .build();
+
+        deltaCampaignLaunchSparkContext
+                .setAccountColsRecIncluded(CampaignLaunchUtils.generateAccountColsRecIncludedForS3());
+        deltaCampaignLaunchSparkContext
+                .setAccountColsRecNotIncludedStd(CampaignLaunchUtils.generateAccountColsRecNotIncludedStdForS3());
+        deltaCampaignLaunchSparkContext
+                .setAccountColsRecNotIncludedNonStd(CampaignLaunchUtils.generateAccountColsRecNotIncludedNonStdForS3());
+        deltaCampaignLaunchSparkContext.setContactCols(CampaignLaunchUtils.generateContactColsForS3());
+
+        boolean createRecommendationDataFrame = true;
+        deltaCampaignLaunchSparkContext.setCreateRecommendationDataFrame(createRecommendationDataFrame);
+        boolean createAddCsvDataFrame = true;
+        deltaCampaignLaunchSparkContext.setCreateAddCsvDataFrame(createAddCsvDataFrame);
+        boolean createDeleteCsvDataFrame = true;
+        deltaCampaignLaunchSparkContext.setCreateDeleteCsvDataFrame(createDeleteCsvDataFrame);
+
+        return deltaCampaignLaunchSparkContext;
+    }
+
+    @Override
+    protected void uploadInputAvro() {
+        List<Pair<String, Class<?>>> accountFields = Arrays.asList( //
+                Pair.of(InterfaceName.AccountId.name(), String.class), //
+                Pair.of(InterfaceName.CustomerAccountId.name(), String.class), //
+                Pair.of(destinationAccountId, String.class), //
+                Pair.of(InterfaceName.CompanyName.name(), String.class), //
+                Pair.of(InterfaceName.LDC_Name.name(), String.class), //
+                Pair.of(ratingId + PlaymakerConstants.RatingScoreColumnSuffix, Integer.class), //
+                Pair.of(ratingId, String.class), //
+                Pair.of(ratingId + PlaymakerConstants.RatingEVColumnSuffix, String.class), //
+                Pair.of(InterfaceName.Website.name(), String.class), //
+                Pair.of(InterfaceName.CreatedDate.name(), String.class) //
+        );
+        addAccounts = new Object[][] { //
+                { "0L", "0000", "destinationAccountId", "Lattice", "Lattice Engines", 98, "A", "1000",
+                        "www.lattice-engines.com", "01/01/2019" }, //
+                { "1L", "0001", "destinationAccountId", "DnB", "DnB", 97, "B", "2000", "www.dnb.com", "01/01/2019" }, //
+                { "2L", "0002", "destinationAccountId", "Google", "Google", 98, "C", "3000", "www.google.com",
+                        "01/01/2019" }, //
+                { "3L", "0003", "destinationAccountId", "Facebook", "FB", 93, "E", "1000000", "www.facebook.com",
+                        "01/01/2019" }, //
+                { "4L", "0004", "destinationAccountId", "Apple", "Apple", null, null, null, "www.apple.com",
+                        "01/01/2019" }, //
+                { "5L", "0005", "destinationAccountId", "SalesForce", "SalesForce", null, "A", null,
+                        "www.salesforce.com", "01/01/2019" }, //
+                { "6L", "0006", "destinationAccountId", "Adobe", "Adobe", 98, null, "1000", "www.adobe.com",
+                        "01/01/2019" }, //
+                { "7L", "0007", "destinationAccountId", "Eloqua", "Eloqua", 40, "F", "100", "www.eloqua.com",
+                        "01/01/2019" }, //
+                { "8L", "0008", "destinationAccountId", "Dell", "Dell", 8, "F", "10", "www.dell.com", "01/01/2019" }, //
+                { "9L", "0009", "destinationAccountId", "HP", "HP", 38, "E", "500", "www.hp.com", "01/01/2019" }, //
+                // the following account has no matched contacts
+                { "100L", "0100", "destinationAccountId", "Fake Co", "Fake Co", 3, "F", "5", "", "" } //
+        };
+        addAccountData = uploadHdfsDataUnit(addAccounts, accountFields);
+
+        // the contact schema does not have the Address_Street_1.name for
+        // testing the the case where contact schema is not complete
+        List<Pair<String, Class<?>>> contactFields = Arrays.asList( //
+                Pair.of(InterfaceName.AccountId.name(), String.class), //
+                Pair.of(InterfaceName.ContactId.name(), String.class), //
+                Pair.of(InterfaceName.CustomerContactId.name(), String.class), //
+                Pair.of(InterfaceName.CompanyName.name(), String.class), //
+                Pair.of(InterfaceName.Email.name(), String.class), //
+                Pair.of(InterfaceName.ContactName.name(), String.class), //
+                Pair.of(InterfaceName.City.name(), String.class), //
+                Pair.of(InterfaceName.State.name(), String.class), //
+                Pair.of(InterfaceName.Country.name(), String.class), //
+                Pair.of(InterfaceName.PostalCode.name(), String.class), //
+                Pair.of(InterfaceName.PhoneNumber.name(), String.class), //
+                Pair.of(InterfaceName.Title.name(), String.class), //
+                Pair.of(InterfaceName.FirstName.name(), String.class), //
+                Pair.of(InterfaceName.LastName.name(), String.class), //
+                Pair.of(InterfaceName.CreatedDate.name(), String.class) //
+        );
+
+        addContacts = new Object[(addAccounts.length - 1) * addOrDeleteContactPerAccount][contactFields.size()];
+        for (int i = 0; i < (addAccounts.length - 1); i++) {
+            for (int j = 0; j < addOrDeleteContactPerAccount; j++) {
+                addContacts[addOrDeleteContactPerAccount * i + j][0] = String.valueOf(i) + "L";
+                addContacts[addOrDeleteContactPerAccount * i + j][1] = String
+                        .valueOf(addOrDeleteContactPerAccount * i + j);
+                addContacts[addOrDeleteContactPerAccount * i + j][2] = String.valueOf(addAccounts.length * i + j) + "L";
+                addContacts[addOrDeleteContactPerAccount * i + j][3] = "Kind Inc.";
+                addContacts[addOrDeleteContactPerAccount * i + j][4] = "michael@kind.com";
+                addContacts[addOrDeleteContactPerAccount * i + j][5] = "Michael Jackson";
+                addContacts[addOrDeleteContactPerAccount * i + j][6] = "SMO";
+                addContacts[addOrDeleteContactPerAccount * i + j][7] = "CA";
+                addContacts[addOrDeleteContactPerAccount * i + j][8] = "US";
+                addContacts[addOrDeleteContactPerAccount * i + j][9] = "94404";
+                addContacts[addOrDeleteContactPerAccount * i + j][10] = "650-898-3928";
+                addContacts[addOrDeleteContactPerAccount * i + j][11] = "CEO";
+                addContacts[addOrDeleteContactPerAccount * i + j][12] = "Michael";
+                addContacts[addOrDeleteContactPerAccount * i + j][13] = "Jackson";
+                addContacts[addOrDeleteContactPerAccount * i + j][14] = "08/08/2019";
+            }
+        }
+        addContactData = uploadHdfsDataUnit(addContacts, contactFields);
+
+        deleteAccounts = new Object[][] { //
+                { "10L", "0010", "destinationAccountId", "Some Com", "Some Company", 8, "F", "10", "www.some-com.com",
+                        "01/01/2019" }, //
+                { "11L", "0011", "destinationAccountId", "Random Com", "Random Company", 7, "F", "2",
+                        "www.random-com.com", "01/01/2019" } };
+        deleteAccountData = uploadHdfsDataUnit(deleteAccounts, accountFields);
+
+        deleteContacts = new Object[(deleteAccounts.length) * addOrDeleteContactPerAccount][contactFields.size()];
+        for (int i = 0; i < deleteAccounts.length; i++) {
+            for (int j = 0; j < addOrDeleteContactPerAccount; j++) {
+                deleteContacts[addOrDeleteContactPerAccount * i + j][0] = String.valueOf(i + 10) + "L";
+                deleteContacts[addOrDeleteContactPerAccount * i + j][1] = String
+                        .valueOf(addOrDeleteContactPerAccount * i + j);
+                deleteContacts[addOrDeleteContactPerAccount * i + j][2] = String.valueOf(deleteAccounts.length * i + j)
+                        + "L";
+                deleteContacts[addOrDeleteContactPerAccount * i + j][3] = "Kind Inc.";
+                deleteContacts[addOrDeleteContactPerAccount * i + j][4] = "michael@kind.com";
+                deleteContacts[addOrDeleteContactPerAccount * i + j][5] = "Michael Jackson";
+                deleteContacts[addOrDeleteContactPerAccount * i + j][6] = "SMO";
+                deleteContacts[addOrDeleteContactPerAccount * i + j][7] = "CA";
+                deleteContacts[addOrDeleteContactPerAccount * i + j][8] = "US";
+                deleteContacts[addOrDeleteContactPerAccount * i + j][9] = "94404";
+                deleteContacts[addOrDeleteContactPerAccount * i + j][10] = "650-898-3928";
+                deleteContacts[addOrDeleteContactPerAccount * i + j][11] = "CEO";
+                deleteContacts[addOrDeleteContactPerAccount * i + j][12] = "Michael";
+                deleteContacts[addOrDeleteContactPerAccount * i + j][13] = "Jackson";
+                deleteContacts[addOrDeleteContactPerAccount * i + j][14] = "08/08/2019";
+            }
+        }
+        deleteContactData = uploadHdfsDataUnit(deleteContacts, contactFields);
+
+        completeContacts = new Object[(addAccounts.length - 1) * completeContactPerAccount][contactFields.size()];
+        for (int i = 0; i < (addAccounts.length - 1); i++) {
+            for (int j = 0; j < completeContactPerAccount; j++) {
+                completeContacts[completeContactPerAccount * i + j][0] = String.valueOf(i) + "L";
+                completeContacts[completeContactPerAccount * i + j][1] = String
+                        .valueOf(completeContactPerAccount * i + j);
+                completeContacts[completeContactPerAccount * i + j][2] = String.valueOf(addAccounts.length * i + j)
+                        + "L";
+                completeContacts[completeContactPerAccount * i + j][3] = "Kind Inc.";
+                completeContacts[completeContactPerAccount * i + j][4] = "michael@kind.com";
+                completeContacts[completeContactPerAccount * i + j][5] = "Michael Jackson";
+                completeContacts[completeContactPerAccount * i + j][6] = "SMO";
+                completeContacts[completeContactPerAccount * i + j][7] = "CA";
+                completeContacts[completeContactPerAccount * i + j][8] = "US";
+                completeContacts[completeContactPerAccount * i + j][9] = "94404";
+                completeContacts[completeContactPerAccount * i + j][10] = "650-898-3928";
+                completeContacts[completeContactPerAccount * i + j][11] = "CEO";
+                completeContacts[completeContactPerAccount * i + j][12] = "Michael";
+                completeContacts[completeContactPerAccount * i + j][13] = "Jackson";
+                completeContacts[completeContactPerAccount * i + j][14] = "08/08/2019";
+            }
+        }
+        completeContactData = uploadHdfsDataUnit(completeContacts, contactFields);
+    }
+
+}
