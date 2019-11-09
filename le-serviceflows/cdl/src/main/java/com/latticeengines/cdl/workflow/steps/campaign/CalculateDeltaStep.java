21a83ad2fb910086b5b30aa914e584c55ab50187
Jignesh Mehta <jlmehta@lattice-engines.com>
2019-11-08 16:47:24 -0800

    PLS-15296: Minor fix for DeltaCampaignLaunchWorkflowConfiguration - Correct the implementation to skip GenerateLaunchArtifacts

--- le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/workflow/steps/campaign/CalculateDeltaStep.java
+++ le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/workflow/steps/campaign/CalculateDeltaStep.java
@@ -67,7 +67,7 @@ public class CalculateDeltaStep extends BaseSparkSQLStep<CalculateDeltaStepConfi
         CalculateDeltaStepConfiguration config = getConfiguration();
         CustomerSpace customerSpace = config.getCustomerSpace();
 
-        Play play = playProxy.getPlay(customerSpace.getTenantId(), config.getPlayId());
+        Play play = playProxy.getPlay(customerSpace.getTenantId(), config.getPlayId(), false, false);
         PlayLaunchChannel channel = playProxy.getChannelById(customerSpace.getTenantId(), config.getPlayId(),
                 config.getChannelId());
 
@@ -179,11 +179,8 @@ public class CalculateDeltaStep extends BaseSparkSQLStep<CalculateDeltaStepConfi
                 String.format("Full%sUniverse_%s", audienceType.asBusinessEntity().name(), config.getExecutionId()),
                 fullUniverse, audienceType.getInterfaceName(), getFullUniverseContextKeyByAudienceType(audienceType));
 
-        try {
-            log.info("Counts: " + JsonUtils.serialize(getMapObjectFromContext(DELTA_TABLE_COUNTS, String.class, Long.class)));
-        } catch (Exception e) {
-            log.error("Failed to log counts due to" + e.getMessage());
-        }
+        log.info("Counts: "
+                + JsonUtils.serialize(getMapObjectFromContext(DELTA_TABLE_COUNTS, String.class, Long.class)));
     }
 
     @SuppressWarnings("unchecked")
