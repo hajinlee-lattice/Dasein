a8ceea732c70a243e1bdb622d88c9ff1e953c727
PenglongLiu <penglong.liu@lattice-engines.com>
2019-11-17 10:57:45 +0800

    add some json annotation

--- le-domain/src/main/java/com/latticeengines/domain/exposed/pls/EntityValidationSummary.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/pls/EntityValidationSummary.java
@@ -1,10 +1,13 @@
 package com.latticeengines.domain.exposed.pls;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
-
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = ProductValidationSummary.class, name = "ProductValidationSummary")})
 public class EntityValidationSummary {
-
 
     @JsonProperty("error_line_number")
     private long errorLineNumber;
