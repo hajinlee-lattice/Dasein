4fa9f468e6b58bdeeaf3a8d7c0ba107917d67e10
PenglongLiu <penglong.liu@lattice-engines.com>
2019-12-02 05:38:24 -0500

    PLS-15382 remove unused import

--- le-serviceapps/core/src/test/java/com/latticeengines/apps/core/service/impl/CDLImpactValidatorFunctionalTestNG.java
+++ le-serviceapps/core/src/test/java/com/latticeengines/apps/core/service/impl/CDLImpactValidatorFunctionalTestNG.java
@@ -5,7 +5,6 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
