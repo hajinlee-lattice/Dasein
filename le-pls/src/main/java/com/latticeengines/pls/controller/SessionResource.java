6e58ad9771d5aa21d35c28c8489e98c5599ac2d2
Yintao Song <ysong@lattice-engines.com>
2020-01-09 22:19:39 -0800

    add a simple protected api

---
+++ le-pls/src/main/java/com/latticeengines/pls/controller/SessionResource.java
@@ -0,0 +1,24 @@
+package com.latticeengines.pls.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.latticeengines.db.exposed.util.MultiTenantContext;
+import com.latticeengines.domain.exposed.security.Session;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api("View current session info")
+@RestController
+public class SessionResource {
+
+    @GetMapping(value = "/session")
+    @ResponseBody
+    @ApiOperation(value = "Get current session")
+    public Session getCurrentSession() {
+        return MultiTenantContext.getSession();
+    }
+
+}
