158525ad994f597f7cd9e563d00ffc318ddb21ee
Yintao Song <ysong@lattice-engines.com>
2020-01-16 13:22:35 -0800

    Round 1: attempt to remove old code

--- le-liaison/src/main/java/com/latticeengines/liaison/exposed/service/ConnectionMgrFactory.java
+++
@@ -1,7 +0,0 @@
-package com.latticeengines.liaison.exposed.service;
-
-
-public interface ConnectionMgrFactory {
-
-    ConnectionMgr getConnectionMgr(String type, String... args);
-}
--- le-pls/src/main/java/com/latticeengines/pls/service/impl/TenantConfigServiceImpl.java
+++ le-pls/src/main/java/com/latticeengines/pls/service/impl/TenantConfigServiceImpl.java
@@ -46,7 +46,6 @@ public class TenantConfigServiceImpl implements TenantConfigService {
     public static final String SERVICES_ZNODE = "/Services";
     public static final String PLS_ZNODE = "/PLS";
     public static final String ENRICHMENT_ATTRIBUTES_MAX_NUMBER_ZNODE = "/EnrichAttributesMaxNumber";
-    public static final String INVOKE_TIME = "/InvokeTime";
 
     @Value("${pls.dataloader.rest.api}")
     private String defaultDataLoaderUrl;
@@ -68,7 +67,7 @@ public class TenantConfigServiceImpl implements TenantConfigService {
     private ModelSummaryProxy modelSummaryProxy;
 
     @PostConstruct
-    private void definePlsFeatureFlags() {
+    private void postConstruct() {
         for (PlsFeatureFlag flag : PlsFeatureFlag.values()) {
             if (FeatureFlagClient.getDefinition(flag.getName()) == null) {
                 FeatureFlagClient.setDefinition(flag.getName(), flag.getDefinition());
