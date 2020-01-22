eeca84652b75ee3252cc3823150d8983c3ab817f
lzhao-lattice <LZhao@lattice-engines.com>
2020-01-22 13:33:24 +0800

    Revert "DP-12154: retry upload filed in uploadLocalDirectory method"
    This reverts commit 5181505fc5ed8ae0786afb205a39920d6271768d.

--- le-documentdb/src/main/java/com/latticeengines/documentdb/repository/CrossTenantDocumentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.latticeengines.documentdb.repository;
-
-import com.latticeengines.db.exposed.repository.BaseJpaRepository;
-
-public interface CrossTenantDocumentRepository<T> extends BaseJpaRepository<T, String> {
-
-}
--- le-metadata/src/main/java/com/latticeengines/metadata/entitymgr/impl/DataUnitEntityMgrImpl.java
+++ le-metadata/src/main/java/com/latticeengines/metadata/entitymgr/impl/DataUnitEntityMgrImpl.java
@@ -22,7 +22,6 @@ import com.latticeengines.documentdb.entitymgr.impl.BaseDocumentEntityMgrImpl;
 import com.latticeengines.domain.exposed.metadata.datastore.DataUnit;
 import com.latticeengines.domain.exposed.metadata.datastore.S3DataUnit;
 import com.latticeengines.metadata.entitymgr.DataUnitEntityMgr;
-import com.latticeengines.metadata.repository.document.reader.DataUnitCrossTenantReaderRepository;
 import com.latticeengines.metadata.repository.document.reader.DataUnitReaderRepository;
 import com.latticeengines.metadata.repository.document.writer.DataUnitWriterRepository;
 
@@ -36,9 +35,6 @@ public class DataUnitEntityMgrImpl extends BaseDocumentEntityMgrImpl<DataUnitEnt
 
     @Inject
     private DataUnitReaderRepository readerRepository;
-
-    @Inject
-    private DataUnitCrossTenantReaderRepository dataUnitCrossTenantReaderRepository;
 
     @Override
     public BaseJpaRepository<DataUnitEntity, String> getRepository() {
@@ -74,7 +70,7 @@ public class DataUnitEntityMgrImpl extends BaseDocumentEntityMgrImpl<DataUnitEnt
 
     @Override
     public List<DataUnit> findByStorageType(DataUnit.StorageType storageType) {
-        List<DataUnitEntity> entities = dataUnitCrossTenantReaderRepository.findByStorageType(storageType);
+        List<DataUnitEntity> entities = readerRepository.findByStorageType(storageType);
         return convertList(entities, false);
     }
 
