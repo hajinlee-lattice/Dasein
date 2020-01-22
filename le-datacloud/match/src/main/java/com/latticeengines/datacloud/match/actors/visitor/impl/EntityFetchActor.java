4eb4b39aa726debff6be8970558b5165661e2cef
Yintao Song <ysong@lattice-engines.com>
2020-01-21 16:54:34 -0800

    fix CheckStyle

--- le-datacloud/match/src/main/java/com/latticeengines/datacloud/match/actors/visitor/impl/EntityFetchActor.java
+++ le-datacloud/match/src/main/java/com/latticeengines/datacloud/match/actors/visitor/impl/EntityFetchActor.java
@@ -5,8 +5,6 @@ import javax.annotation.Resource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
