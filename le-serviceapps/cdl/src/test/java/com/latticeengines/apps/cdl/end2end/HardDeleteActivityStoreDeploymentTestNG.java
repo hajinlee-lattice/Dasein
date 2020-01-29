0f2f57ee6c3e70c63e6b6aaaae9d1e5b7841d545
Jinyang He <jhe@lattice-engines.com>
2020-01-29 10:09:38 -0800

    DP-11916: Hard delete for Activity stream Part 3: Add a deployment test for mini stack

---
+++ le-serviceapps/cdl/src/test/java/com/latticeengines/apps/cdl/end2end/HardDeleteActivityStoreDeploymentTestNG.java
@@ -0,0 +1,43 @@
+package com.latticeengines.apps.cdl.end2end;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.avro.generic.GenericRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.latticeengines.domain.exposed.metadata.InterfaceName;
+import com.latticeengines.domain.exposed.metadata.Table;
+import com.latticeengines.domain.exposed.metadata.TableRoleInCollection;
+
+public class HardDeleteActivityStoreDeploymentTestNG extends DeleteActivityStoreDeploymentTestNG {
+
+    private static final Logger log = LoggerFactory.getLogger(HardDeleteActivityStoreDeploymentTestNG.class);
+
+    @Test(groups = "end2end")
+    public void testHardDelete() throws Exception {
+        super.test();
+        registerDeleteData(true);
+
+//        if (isLocalEnvironment()) {
+            // run PA with fake current time
+            processAnalyzeSkipPublishToS3(CURRENT_PA_TIME.toEpochMilli());
+//        } else {
+//            runTestWithRetry(getCandidateFailingSteps(), CURRENT_PA_TIME.toEpochMilli());
+//        }
+        verifyAfterPA();
+    }
+
+    private void verifyAfterPA() throws IOException {
+        log.info("Ids that needs to be removed: " + idSets.toString());
+        Table table = dataCollectionProxy.getTable(customerSpace, TableRoleInCollection.ConsolidatedActivityStream);
+        List<GenericRecord> recordsAfterDelete = getStreamRecords(table);
+        for (GenericRecord record : recordsAfterDelete) {
+            String accountId = record.get(InterfaceName.AccountId.name()).toString();
+            Assert.assertFalse(idSets.contains(accountId), "Should not contain id " + accountId);
+        }
+    }
+}
