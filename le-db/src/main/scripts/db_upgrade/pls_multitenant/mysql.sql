5eca90424ec057ffe6887082f79736481ca8d60f
Jinyang He <jhe@lattice-engines.com>
2020-02-04 12:00:04 -0800

    DCP-234: 4. Update sql script

---
+++ le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
@@ -0,0 +1,33 @@
+USE `PLS_MultiTenant`;
+
+CREATE PROCEDURE `CreateDCPProjectTable`()
+  BEGIN
+    CREATE TABLE `DCP_PROJECT`
+      (
+         `PID`                  BIGINT NOT NULL auto_increment,
+         `CREATED`              DATETIME NOT NULL,
+         `CREATED_BY`           VARCHAR(255) NOT NULL,
+         `DELETED`              BIT,
+         `PROJECT_DESCRIPTION`  VARCHAR(4000),
+         `PROJECT_DISPLAY_NAME` VARCHAR(255),
+         `PROJECT_ID`           VARCHAR(255) NOT NULL,
+         `PROJECT_TYPE`         VARCHAR(255),
+         `ROOT_PATH`            VARCHAR(255),
+         `UPDATED`              DATETIME NOT NULL,
+         `FK_TENANT_ID`         BIGINT NOT NULL,
+         PRIMARY KEY (`PID`)
+      )
+    engine=InnoDB;
+
+    CREATE INDEX IX_PROJECT_ID ON `DCP_PROJECT` (`PROJECT_ID`);
+
+    ALTER TABLE `DCP_PROJECT`
+      ADD CONSTRAINT UX_PROJECT_ID UNIQUE (`FK_TENANT_ID`, `PROJECT_ID`);
+
+    ALTER TABLE `DCP_PROJECT`
+      ADD CONSTRAINT `FK_DCPPROJECT_FKTENANTID_TENANT` FOREIGN KEY (`FK_TENANT_ID`)
+      REFERENCES `TENANT` (`TENANT_PID`) ON DELETE CASCADE;
+
+  END;
+//
+DELIMITER;
