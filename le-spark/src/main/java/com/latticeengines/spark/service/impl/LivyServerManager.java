a3f5fe0bbfc64c1209ae21c6c2f18d133027d7cb
Yizhi Wu <ywu@lattice-engines.com>
2020-02-05 01:09:16 -0800

    PLS-16182: Add the right check for livy server to determine if it's good for use

--- le-spark/src/main/java/com/latticeengines/spark/service/impl/LivyServerManager.java
+++ le-spark/src/main/java/com/latticeengines/spark/service/impl/LivyServerManager.java
@@ -1,7 +1,6 @@
 package com.latticeengines.spark.service.impl;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.Iterator;
 import java.util.Queue;
 import java.util.Random;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -13,8 +12,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.latticeengines.common.exposed.util.ConsulUtils;
+import com.latticeengines.common.exposed.util.HttpClientUtils;
 import com.latticeengines.hadoop.exposed.service.EMRCacheService;
 
 @Component("livyServerManager")
@@ -36,8 +40,14 @@ public class LivyServerManager {
     @Value("${aws.emr.cluster}")
     private String emrClusterName;
 
-    // use a ConcurrentLinkedQueue to maintain server urls
-    // need to consider concurrency situations
+    // Define the upper limit for livy sessions on each server
+    // for load balance and performance purpose
+    private static final int MAX_SESSION_PER_SERVER = 10;
+
+    private RestTemplate restTemplate = HttpClientUtils.newRestTemplate();
+
+    // Use a ConcurrentLinkedQueue to maintain server urls
+    // Need to consider concurrency situations
     private Queue<String> serverQueue = new ConcurrentLinkedQueue<>();
 
     public String getLivyHost() {
@@ -46,9 +56,9 @@ public class LivyServerManager {
 
     private String getNextLivyServerUrl(String emrClusterName) {
         if (serverQueue.size() == 0) {
-            // reload the server queue. Newly added server will also be loaded here.
+            // Reload the server queue. Newly added server will also be loaded here
             if (!populateServerQueue(emrClusterName)) {
-                // if no external livy server, return the EMR internal livy server
+                // If no external livy server, return the EMR internal livy server
                 return emrCacheService.getLivyUrl();
             }
         }
@@ -56,7 +66,7 @@ public class LivyServerManager {
         while (serverQueue.size() != 0) {
             String nextServerUrl = serverQueue.poll();
 
-            if (isLivyServerReachable(nextServerUrl)) {
+            if (isLivyServerGoodForUse(nextServerUrl)) {
                 log.info("Picked livy server " + nextServerUrl);
                 return nextServerUrl;
             } else {
@@ -64,9 +74,7 @@ public class LivyServerManager {
             }
         }
 
-        // no livy server available, might need to trigger the livy server provision
-        // process
-        // for now, we will fall back to the EMR internal livy server
+        // No external livy server available, fall back to the EMR internal livy server
         log.warn("Can't find any available external livy server, fall back to EMR internal livy server...");
         return emrCacheService.getLivyUrl();
     }
@@ -74,8 +82,8 @@ public class LivyServerManager {
     private synchronized boolean populateServerQueue(String emrClusterName) {
         String consulKey = "emr/" + emrClusterName + "/LivyServerIps";
 
-        // first check if the key even exists in consul KV store; if not, return right
-        // away
+        // First check if the key even exists in consul KV store;
+        // If not, return right away
         if (!ConsulUtils.isKeyExists(consul, consulKey)) {
             return false;
         }
@@ -88,14 +96,14 @@ public class LivyServerManager {
 
         String[] serverIps = StringUtils.split(ipStr, '\n');
         log.info("Available livy servers are:");
-        // construct the server queue
+        // Construct the server queue
         for (String ip : serverIps) {
             log.info("http://" + ip + ":8998");
             serverQueue.offer("http://" + ip + ":8998");
         }
 
         if (!serverQueue.isEmpty()) {
-            // shuffle the queue to avoid everyone starts with the first one in consul
+            // Shuffle the queue to avoid everyone starts with the first one in consul
             int shuffle = new Random(System.currentTimeMillis()).nextInt(serverQueue.size());
             for (int i = 0; i < shuffle; i++) {
                 String ip = serverQueue.poll();
@@ -106,16 +114,48 @@ public class LivyServerManager {
         return true;
     }
 
-    private boolean isLivyServerReachable(String serverUrl) {
+    private boolean isLivyServerGoodForUse(String serverUrl) {
+        String url = serverUrl + "/sessions";
+
+        // First, GET all the current sessions on this server
+        String resp;
         try {
-            HttpURLConnection connection = (HttpURLConnection) new URL(serverUrl).openConnection();
-            connection.setRequestMethod("HEAD");
-            int responseCode = connection.getResponseCode();
-            return responseCode == 200;
-        } catch (Exception e) {
-            log.error("Check server availability failed" + e);
+            resp = restTemplate.getForObject(url, String.class);
+        } catch (RestClientException e) {
+            log.error("GET livy session failed on " + url, e);
+            return false;
         }
 
-        return false;
+        // Loop through the sessions:
+        // 1: delete all sessions that are in bad state (save resource)
+        // 2: check against max session upper limit
+        ObjectMapper om = new ObjectMapper();
+        try {
+            JsonNode root = om.readTree(resp);
+            int total = root.get("total").asInt();
+            JsonNode sessions = root.get("sessions");
+            Iterator<JsonNode> iter = sessions.elements();
+            while (iter.hasNext()) {
+                JsonNode json = iter.next();
+                String state = json.get("state").asText();
+                // For non-active state, delete those sessions to save resource
+                if (state.equalsIgnoreCase("error") || state.equalsIgnoreCase("dead")
+                        || state.equalsIgnoreCase("success")) {
+                    int sessionId = json.get("id").asInt();
+                    restTemplate.delete(url + sessionId);
+                    total--;
+                }
+            }
+            // If remaining sessions already exceed upper limit, return false
+            if (total >= MAX_SESSION_PER_SERVER) {
+                log.info(serverUrl + " is already loaded");
+                return false;
+            }
+        } catch (Exception e) {
+            log.error("Failed while loop through sessions on " + url, e);
+            return false;
+        }
+
+        return true;
     }
 }
