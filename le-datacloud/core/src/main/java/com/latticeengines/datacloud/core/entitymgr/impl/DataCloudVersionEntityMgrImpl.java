d81cc05ae04c6acd88b5534abb538cb24eb9bea5
Lei Ming <lming@lattice-engines.com>
2020-02-14 13:17:16 -0800

    PD-3046 Use Specified Data cloud version in property file.

--- le-datacloud/core/src/main/java/com/latticeengines/datacloud/core/entitymgr/impl/DataCloudVersionEntityMgrImpl.java
+++ le-datacloud/core/src/main/java/com/latticeengines/datacloud/core/entitymgr/impl/DataCloudVersionEntityMgrImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -27,21 +28,34 @@ public class DataCloudVersionEntityMgrImpl implements DataCloudVersionEntityMgr 
     @Value("${datacloud.match.latest.data.cloud.major.version}")
     private String latestMajorVersion;
 
+    @Value("${datacloud.match.current.data.cloud.version}")
+    private String currentVersion;
+
     @Override
     @Transactional(value = "propDataManage", readOnly = true, isolation = Isolation.READ_UNCOMMITTED)
     public DataCloudVersion currentApprovedVersion() {
+        if (StringUtils.isNotBlank(currentVersion)) {
+            return findVersion(currentVersion);
+        }
         return latestApprovedForMajorVersion(latestMajorVersion);
     }
 
     @Override
     @Transactional(value = "propDataManage", readOnly = true, isolation = Isolation.READ_UNCOMMITTED)
     public String currentApprovedVersionAsString() {
+        if (StringUtils.isNotBlank(currentVersion)) {
+            return findVersion(currentVersion).getVersion();
+        }
         return latestApprovedForMajorVersion(latestMajorVersion).getVersion();
     }
 
     @Override
     @Transactional(value = "propDataManage", readOnly = true, isolation = Isolation.READ_UNCOMMITTED)
     public DataCloudVersion latestApprovedForMajorVersion(String version) {
+
+        if (StringUtils.isNotBlank(currentVersion)) {
+            return findVersion(currentVersion);
+        }
         String majorVersion = parseMajorVersion(version);
         DataCloudVersion dataCloudVersion = dataCloudVersionDao
                 .latestApprovedForMajorVersion(majorVersion);
