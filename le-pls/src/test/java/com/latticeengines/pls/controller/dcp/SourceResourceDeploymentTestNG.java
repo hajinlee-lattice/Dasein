423142ef16ac2febf05ca6fe4cf1ef43d0243736
Jinyang He <jhe@lattice-engines.com>
2020-03-02 13:58:42 -0800

    DCP-230, DCP-235: 8. Add test for PLS api.

---
+++ le-pls/src/test/java/com/latticeengines/pls/controller/dcp/SourceResourceDeploymentTestNG.java
@@ -0,0 +1,60 @@
+package com.latticeengines.pls.controller.dcp;
+
+import javax.inject.Inject;
+
+import org.apache.commons.lang3.StringUtils;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.latticeengines.db.exposed.util.MultiTenantContext;
+import com.latticeengines.domain.exposed.admin.LatticeProduct;
+import com.latticeengines.domain.exposed.cdl.SimpleTemplateMetadata;
+import com.latticeengines.domain.exposed.dcp.Project;
+import com.latticeengines.domain.exposed.dcp.ProjectDetails;
+import com.latticeengines.domain.exposed.dcp.Source;
+import com.latticeengines.domain.exposed.dcp.SourceRequest;
+import com.latticeengines.domain.exposed.query.EntityType;
+import com.latticeengines.pls.functionalframework.PlsDeploymentTestNGBase;
+import com.latticeengines.testframework.exposed.proxy.pls.TestProjectProxy;
+import com.latticeengines.testframework.exposed.proxy.pls.TestSourceProxy;
+
+public class SourceResourceDeploymentTestNG extends PlsDeploymentTestNGBase {
+
+    @Inject
+    private TestProjectProxy testProjectProxy;
+
+    @Inject
+    private TestSourceProxy testSourceProxy;
+
+    @BeforeClass(groups = "deployment")
+    public void setup() throws Exception {
+        setupTestEnvironmentWithOneTenantForProduct(LatticeProduct.DCP);
+        MultiTenantContext.setTenant(mainTestTenant);
+        attachProtectedProxy(testProjectProxy);
+        attachProtectedProxy(testSourceProxy);
+    }
+
+    @Test(groups = "deployment")
+    public void testCreateAndGetSource() {
+        ProjectDetails projectDetail = testProjectProxy.createProjectWithOutProjectId("testProject",
+                Project.ProjectType.Type1);
+        Assert.assertNotNull(projectDetail);
+        String projectId = projectDetail.getProjectId();
+        SimpleTemplateMetadata simpleTemplateMetadata = new SimpleTemplateMetadata();
+        simpleTemplateMetadata.setEntityType(EntityType.Accounts);
+        SourceRequest sourceRequest = new SourceRequest();
+        sourceRequest.setProjectId(projectId);
+        sourceRequest.setDisplayName("testSource");
+        sourceRequest.setSimpleTemplateMetadata(simpleTemplateMetadata);
+        Source source = testSourceProxy.createSource(sourceRequest);
+        Assert.assertNotNull(source);
+        Assert.assertFalse(StringUtils.isBlank(source.getSourceId()));
+        Assert.assertFalse(StringUtils.isBlank(source.getFullPath()));
+
+        sourceRequest.setDisplayName("testSource2");
+        Source source2 = testSourceProxy.createSource(sourceRequest);
+
+        Assert.assertNotEquals(source.getSourceId(), source2.getSourceId());
+    }
+}
---
+++ le-testframework/src/main/java/com/latticeengines/testframework/exposed/proxy/pls/TestSourceProxy.java
@@ -0,0 +1,33 @@
+package com.latticeengines.testframework.exposed.proxy.pls;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.latticeengines.common.exposed.util.JsonUtils;
+import com.latticeengines.domain.exposed.dcp.Source;
+import com.latticeengines.domain.exposed.dcp.SourceRequest;
+
+@Component("testSourceProxy")
+public class TestSourceProxy extends PlsRestApiProxyBase {
+
+    public TestSourceProxy() {
+        super("pls/source");
+    }
+
+    public Source createSource(SourceRequest sourceRequest) {
+        String url = constructUrl("/");
+        return post("create source", url, sourceRequest, Source.class);
+    }
+
+    public Source getSource(String sourceId) {
+        String url = constructUrl("/sourceId/{sourceId}", sourceId);
+        return get("get source", url, Source.class);
+    }
+
+    public List<Source> getSourcesByProject(String projectId) {
+        String url = constructUrl("/projectId/{projectId}", projectId);
+        List<?> rawList = get("get source list", url, List.class);
+        return JsonUtils.convertList(rawList, Source.class);
+    }
+}
