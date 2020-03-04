f47dba8f72eb7c5bc5299ed58581ee0af0e64ed8
Jinyang He <jhe@lattice-engines.com>
2020-03-04 12:38:58 -0800

    DCP-359: 1. Upload object definition

---
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/dcp/UploadConfig.java
@@ -0,0 +1,56 @@
+package com.latticeengines.domain.exposed.dcp;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.NONE, getterVisibility = JsonAutoDetect.Visibility.NONE)
+public class UploadConfig {
+
+    @JsonProperty("drop_file_path")
+    private String dropFilePath;
+
+    @JsonProperty("upload_file_path")
+    private String uploadFilePath;
+
+    @JsonProperty("processed_file_path")
+    private String processedFilePath;
+
+    @JsonProperty("error_file_path")
+    private String errorFilePath;
+
+    public String getDropFilePath() {
+        return dropFilePath;
+    }
+
+    public void setDropFilePath(String dropFilePath) {
+        this.dropFilePath = dropFilePath;
+    }
+
+    public String getUploadFilePath() {
+        return uploadFilePath;
+    }
+
+    public void setUploadFilePath(String uploadFilePath) {
+        this.uploadFilePath = uploadFilePath;
+    }
+
+    public String getProcessedFilePath() {
+        return processedFilePath;
+    }
+
+    public void setProcessedFilePath(String processedFilePath) {
+        this.processedFilePath = processedFilePath;
+    }
+
+    public String getErrorFilePath() {
+        return errorFilePath;
+    }
+
+    public void setErrorFilePath(String errorFilePath) {
+        this.errorFilePath = errorFilePath;
+    }
+}
