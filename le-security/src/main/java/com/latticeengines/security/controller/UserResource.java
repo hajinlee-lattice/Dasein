81f868b1d0be84192203aa6a34cf8ad43203f396
lzhao-lattice <LZhao@lattice-engines.com>
2020-03-07 01:42:54 +0800

    PLS-16256: Change DB schema

--- le-security/src/main/java/com/latticeengines/security/controller/UserResource.java
+++ le-security/src/main/java/com/latticeengines/security/controller/UserResource.java
@@ -7,7 +7,6 @@ import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
