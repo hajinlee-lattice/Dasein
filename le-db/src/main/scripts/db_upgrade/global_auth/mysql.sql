1bc7f4cf1f2312aa233075e29b0f48a7298c71fc
PenglongLiu <penglong.liu@lattice-engines.com>
2020-03-24 10:10:43 +0800

    PLS-16461 minor change

--- le-db/src/main/scripts/db_upgrade/global_auth/mysql.sql
+++ le-db/src/main/scripts/db_upgrade/global_auth/mysql.sql
@@ -4,8 +4,7 @@ DROP PROCEDURE IF EXISTS `UpdateTable`;
 DELIMITER //
   BEGIN
    CREATE PROCEDURE `UpdateTable`()
-      alter table `SECURITY_IDENTITY_PROVIDER` drop index `ENTITY_ID`;
-      alter table `SECURITY_IDENTITY_PROVIDER` add constraint `UX_IDENTITY_ID` unique (`TENANT_ID`, `ENTITY_ID`);
+      alter table `SECURITY_IDENTITY_PROVIDER` add constraint UX_IDENTITY_ID unique (`TENANT_ID`, `ENTITY_ID`);
 
   END;
 //
