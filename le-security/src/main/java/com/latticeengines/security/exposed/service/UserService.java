6c0cc8c0f9bd4aab9ae8b69f4269e192f1934fc0
lzhao-lattice <LZhao@lattice-engines.com>
2020-03-26 23:36:38 +0800

    PLS-16646: Allow user to specify their Team for a New Segment

--- le-security/src/main/java/com/latticeengines/security/exposed/service/UserService.java
+++ le-security/src/main/java/com/latticeengines/security/exposed/service/UserService.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import com.latticeengines.domain.exposed.auth.GlobalAuthUser;
 import com.latticeengines.domain.exposed.auth.GlobalAuthUserTenantRight;
-import com.latticeengines.domain.exposed.auth.GlobalTeam;
 import com.latticeengines.domain.exposed.pls.RegistrationResult;
 import com.latticeengines.domain.exposed.pls.UserUpdateData;
 import com.latticeengines.domain.exposed.saml.LoginValidationResponse;
