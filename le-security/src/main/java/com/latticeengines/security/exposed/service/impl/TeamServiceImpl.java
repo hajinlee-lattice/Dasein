9db45ece2231b1f4143ca78c63d2851486df4ede
lzhao-lattice <LZhao@lattice-engines.com>
2020-04-07 15:19:41 +0800

    PLS-16648: rights check for team

--- le-security/src/main/java/com/latticeengines/security/exposed/service/impl/TeamServiceImpl.java
+++ le-security/src/main/java/com/latticeengines/security/exposed/service/impl/TeamServiceImpl.java
@@ -1,7 +1,6 @@
 package com.latticeengines.security.exposed.service.impl;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
