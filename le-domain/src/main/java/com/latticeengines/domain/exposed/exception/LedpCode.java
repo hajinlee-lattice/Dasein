7fe92ce2cc291ac970628328a89dc7f522d06c35
Jinyang He <jhe@lattice-engines.com>
2020-04-08 14:30:48 -0700

    DP-12446: Add limitation for S3 import. 1. File size <= 100GB 2. Valid row counts <= 10M

--- le-domain/src/main/java/com/latticeengines/domain/exposed/exception/LedpCode.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/exception/LedpCode.java
@@ -865,6 +865,8 @@ public enum LedpCode {
             "{0} is setup as your primary. This cannot be changed. Don't assign the unique ID of this system to {1} and try again."),
     LEDP_40076("Cannot download file {0}."),
     LEDP_40077("Import using the wrong template {0}, should use {1}."),
+    LEDP_40078("S3 import file exceed size limit(10GB)."),
+    LEDP_40079("Valid import records count exceed limit(10M)."),
 
     // le-domain
     LEDP_41001("PeriodName field {0} is null for record {1}."), //
--- le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/workflow/steps/validations/InputFileValidator.java
+++ le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/workflow/steps/validations/InputFileValidator.java
@@ -37,6 +37,9 @@ import com.latticeengines.serviceflows.workflow.report.BaseReportStep;
 public class InputFileValidator extends BaseReportStep<InputFileValidatorConfiguration> {
     private static final Logger log = LoggerFactory.getLogger(InputFileValidator.class);
     public static final long CATALOG_RECORDS_LIMIT = 10L;
+
+    private static final long GENERAL_RECORDS_LIMIT = 10000000L;
+
     @Inject
     private EaiJobDetailProxy eaiJobDetailProxy;
 
@@ -70,6 +73,7 @@ public class InputFileValidator extends BaseReportStep<InputFileValidatorConfigu
             log.warn(String.format("Avro path is empty for applicationId=%s, tenantId=%s", applicationId, tenantId));
             return;
         }
+        checkImportedRecordsExceedLimit(eaiImportJobDetail);
         BusinessEntity entity = configuration.getEntity();
         boolean enableEntityMatch = configuration.isEnableEntityMatch();
         log.info(String.format("Begin to validate data with entity %s and entity match %s.", entity.name(),
@@ -130,6 +134,16 @@ public class InputFileValidator extends BaseReportStep<InputFileValidatorConfigu
         super.execute();
         // make sure report first, then fail work flow if necessary
         failWorkflowIfNeeded(entity, errorLineInValidationStep, totalFailed, totalRows, entityValidationSummary);
+    }
+
+    private void checkImportedRecordsExceedLimit(EaiImportJobDetail eaiImportJobDetail) {
+        List<String> processedRecords = eaiImportJobDetail.getPRDetail();
+        if (CollectionUtils.size(processedRecords) > 0) {
+            long totalRecords = processedRecords.stream().mapToLong(Long::parseLong).sum();
+            if (totalRecords > GENERAL_RECORDS_LIMIT) {
+                throw new LedpException(LedpCode.LEDP_40079);
+            }
+        }
     }
 
     private void failWorkflowIfNeeded(BusinessEntity entity, long errorLineInValidationStep, long totalFailed,
