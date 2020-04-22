f61c258c04313d6749774328050e10758882033e
Yintao Song <ysong@lattice-engines.com>
2020-04-22 12:28:59 -0700

    enforce templist max size

--- le-objectapi/src/main/java/com/latticeengines/objectapi/service/sparksql/impl/TempListServiceSparkSQLImpl.java
+++ le-objectapi/src/main/java/com/latticeengines/objectapi/service/sparksql/impl/TempListServiceSparkSQLImpl.java
@@ -6,8 +6,10 @@ import java.util.Map;
 
 import javax.inject.Inject;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.latticeengines.domain.exposed.query.CollectionLookup;
@@ -28,6 +30,9 @@ public class TempListServiceSparkSQLImpl implements TempListService {
     @Inject
     private SparkSQLService sparkSQLService;
 
+    @Value("${redshift.templist.maxsize}")
+    private long maxSize;
+
     private LivySession livySession;
 
     @Override
@@ -46,6 +51,10 @@ public class TempListServiceSparkSQLImpl implements TempListService {
     private String createTempListTable(ConcreteRestriction restriction, Class<?> fieldClz) {
         String tableName = TempListUtils.newShortTempTableName();
         CollectionLookup collectionLookup = (CollectionLookup) restriction.getRhs();
+        int size = CollectionUtils.size(collectionLookup.getValues());
+        if (size > maxSize) {
+            throw new IllegalArgumentException("Templist with " + size + " items is not allowed");
+        }
         List<List<Object>> vals = TempListUtils.insertVals(fieldClz, collectionLookup.getValues());
         sparkSQLService.createTempListView(livySession, tableName, fieldClz, vals);
         return tableName;
