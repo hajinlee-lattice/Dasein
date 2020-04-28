60b9770b5dfcd892662e642bfbacc1cc7335ba28
Sayali Sakhalkar <ssakhalkar@lattice-engines.com>
2020-04-27 22:23:12 -0700

    mysql script update for LDC_manageDB database to sync with PROD

--- le-db/src/main/scripts/db_upgrade/ldc_managedb/mysql.sql
+++ le-db/src/main/scripts/db_upgrade/ldc_managedb/mysql.sql
@@ -6,7 +6,13 @@ DROP PROCEDURE IF EXISTS `UpdateDataCloudVersionTable`;
 
 DROP PROCEDURE IF EXISTS `UpdateDecisionGraphTable`;
 
+DROP PROCEDURE IF EXISTS `UpdateAccountMasterFact`;
+
+DROP PROCEDURE IF EXISTS `UpdateIngestionProgress`;
+
 DROP PROCEDURE IF EXISTS `UpdateSchema`;
+
+DROP PROCEDURE IF EXISTS `DropTables`;
 
 DELIMITER //
 CREATE PROCEDURE `UpdateDataCloudVersionTable`()
@@ -17,6 +23,31 @@ DELIMITER ;
 DELIMITER //
 CREATE PROCEDURE `UpdateDecisionGraphTable`()
   BEGIN
+  END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE `UpdateAccountMasterFact`()
+  BEGIN
+	  ALTER TABLE AccountMasterFact
+		DROP COLUMN DataCloudVersion;
+  END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE `UpdateIngestionProgress`()
+  BEGIN
+	  CREATE INDEX IX_VERSION
+		ON IngestionProgress (Version);
+	  CREATE INDEX IX_INGESTION_ID
+		ON IngestionProgress (IngestionId);
+  END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE `DropTables`()
+  BEGIN
+	  DROP TABLE Orchestration_Copy;
   END //
 DELIMITER ;
 
@@ -40,10 +71,13 @@ DELIMITER //
 CREATE PROCEDURE `UpdateSchema`()
   BEGIN
       CALL `UpdateMatchCommandTable`();
+      CALL `UpdateAccountMasterFact`();
+      CALL `UpdateIngestionProgress`();
   END //
 DELIMITER ;
 
 CALL `UpdateSchema`();
+CALL `DropTables`();
 
 
 
