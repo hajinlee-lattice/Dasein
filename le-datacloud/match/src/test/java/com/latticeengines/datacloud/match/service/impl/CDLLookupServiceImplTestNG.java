90de4040b1caf26b23240ec5320885d68b26f568
Jignesh Mehta <jlmehta@lattice-engines.com>
2020-05-05 00:51:36 -0700

    PLS-17034: Minor bug fix in data format for contact LastActivityDates

--- le-datacloud/match/src/test/java/com/latticeengines/datacloud/match/service/impl/CDLLookupServiceImplTestNG.java
+++ le-datacloud/match/src/test/java/com/latticeengines/datacloud/match/service/impl/CDLLookupServiceImplTestNG.java
@@ -1,5 +1,8 @@
 package com.latticeengines.datacloud.match.service.impl;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Map;
 
@@ -7,13 +10,18 @@ import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StreamUtils;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.latticeengines.common.exposed.util.JsonUtils;
 import com.latticeengines.datacloud.match.service.CDLLookupService;
 import com.latticeengines.datacloud.match.testframework.DataCloudMatchFunctionalTestNGBase;
+import com.latticeengines.datafabric.service.datastore.FabricDataService;
+import com.latticeengines.datafabric.service.message.FabricMessageService;
+import com.latticeengines.domain.exposed.metadata.InterfaceName;
 
 public class CDLLookupServiceImplTestNG extends DataCloudMatchFunctionalTestNGBase {
     private static final Logger log = LoggerFactory.getLogger(CDLLookupServiceImplTestNG.class);
@@ -21,7 +29,13 @@ public class CDLLookupServiceImplTestNG extends DataCloudMatchFunctionalTestNGBa
     @Inject
     private CDLLookupService cdlLookupService;
 
-    @BeforeClass(groups = "functional", enabled = false)
+    @Inject
+    private FabricMessageService messageService;
+
+    @Inject
+    private FabricDataService dataService;
+
+    @BeforeClass(groups = "functional")
     public void setup() {
         // Add few records of data in dynamo
         // register a data unit
@@ -33,6 +47,22 @@ public class CDLLookupServiceImplTestNG extends DataCloudMatchFunctionalTestNGBa
                 .lookupContactsByInternalAccountId("QA_CDL_DemoScript_1202", null, null, "0012400001DNrwwAAD", null);
         Assert.assertNotNull(contacts);
         Assert.assertEquals(contacts.size(), 25);
+    }
+
+    @Test(groups = "functional")
+    public void testMerge() throws IOException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream tableRegistryStream = classLoader.getResourceAsStream("cdl/ConsolidatedContact.json");
+        String contactData = StreamUtils.copyToString(tableRegistryStream, Charset.defaultCharset());
+
+        tableRegistryStream = classLoader.getResourceAsStream("cdl/CuratedContact.json");
+        String curatedContactData = StreamUtils.copyToString(tableRegistryStream, Charset.defaultCharset());
+
+        List<Map<String, Object>> merged = ((CDLLookupServiceImpl) cdlLookupService).merge(
+                JsonUtils.convertListOfMaps(JsonUtils.deserialize(contactData, List.class), String.class, Object.class),
+                JsonUtils.convertListOfMaps(JsonUtils.deserialize(curatedContactData, List.class), String.class,
+                        Object.class));
+        merged.forEach(m -> Assert.assertTrue(m.containsKey(InterfaceName.LastActivityDate.name())));
     }
 
     @AfterClass(groups = "functional", enabled = false)
