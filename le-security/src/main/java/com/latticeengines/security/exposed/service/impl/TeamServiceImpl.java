270d7cdbf2c9e67f8593b698a39204ea7fe3d422
lzhao-lattice <LZhao@lattice-engines.com>
2020-05-27 13:45:55 +0800

    fix check style

--- le-security/src/main/java/com/latticeengines/security/exposed/service/impl/TeamServiceImpl.java
+++ le-security/src/main/java/com/latticeengines/security/exposed/service/impl/TeamServiceImpl.java
@@ -10,7 +10,6 @@ import java.util.stream.Collectors;
 import javax.inject.Inject;
 
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
