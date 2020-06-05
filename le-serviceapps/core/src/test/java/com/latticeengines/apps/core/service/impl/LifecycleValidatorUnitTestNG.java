f3a02be652daaf0ca741e1be236ea8b2d91375ec
lzhao-lattice <LZhao@lattice-engines.com>
2020-06-05 19:26:12 +0800

    PLS-17507

--- le-serviceapps/core/src/test/java/com/latticeengines/apps/core/service/impl/LifecycleValidatorUnitTestNG.java
+++ le-serviceapps/core/src/test/java/com/latticeengines/apps/core/service/impl/LifecycleValidatorUnitTestNG.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
