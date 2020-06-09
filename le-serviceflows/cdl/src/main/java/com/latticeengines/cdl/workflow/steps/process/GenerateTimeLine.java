115fe6237743a8e9e80434cc619fa887a09a40ae
Joy Xiao <jxiao@lattice-engines.com>
2020-06-09 16:23:32 +0800

    DP-13151:fix name

--- le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/workflow/steps/process/GenerateTimeLine.java
+++ le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/workflow/steps/process/GenerateTimeLine.java
@@ -139,13 +139,13 @@ public class GenerateTimeLine extends RunSparkJob<TimeLineSparkStepConfiguration
         config.timelineRelatedStreamTables =
                 getTimelineRelatedStreamTables(timeLineList, sourceTables, config.timeLineMap);
         if (MapUtils.isNotEmpty(config.timelineRelatedStreamTables) && MapUtils.isNotEmpty(timelineMaterStoreNameMap)) {
-            config.timelineRelatedRoleTables =
-                    timelineMaterStoreNameMap.entrySet().stream().filter(entry -> config.timelineRelatedRoleTables.keySet().contains(entry.getKey())).map(entry -> Pair.of(entry.getKey(), entry.getValue())).collect(Collectors.toMap(Pair::getKey, Pair::getValue));
+            config.timelineRelatedMaterTables =
+                    timelineMaterStoreNameMap.entrySet().stream().filter(entry -> config.timelineRelatedMaterTables.keySet().contains(entry.getKey())).map(entry -> Pair.of(entry.getKey(), entry.getValue())).collect(Collectors.toMap(Pair::getKey, Pair::getValue));
         } else {
-            config.timelineRelatedRoleTables = new HashMap<>();
+            config.timelineRelatedMaterTables = new HashMap<>();
         }
         toDataUnits(new ArrayList<>(sourceTables.values()), config.rawStreamInputIdx, inputs);
-        toDataUnits(new ArrayList<>(config.timelineRelatedRoleTables.values()), config.roleTableInputIdx, inputs);
+        toDataUnits(new ArrayList<>(config.timelineRelatedMaterTables.values()), config.masterStoreInputIdx, inputs);
         config.setInput(inputs);
         Table contactTable = getContactTable();
         if (contactTable != null) {
