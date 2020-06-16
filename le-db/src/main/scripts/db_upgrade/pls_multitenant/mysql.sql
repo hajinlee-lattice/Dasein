5af29eecfa9fb8dcf64b728a74a74a1714a7f639
Jinyang He <jhe@lattice-engines.com>
2020-06-15 21:34:19 -0700

    DCP-832: Part 2: DB schema update script

--- le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
+++ le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
@@ -15,6 +15,37 @@ CREATE PROCEDURE `UpdateSchema`()
       -- User input section (DDL/DML). This is just a template, developer can modify based on need.
       ALTER TABLE `DCP_UPLOAD` ADD COLUMN `UPLOAD_DIAGNOSTICS` JSON;
 
+      CREATE TABLE `DCP_DATA_REPORT`
+        (
+           `PID`                     BIGINT NOT NULL auto_increment,
+           `BASIC_STATS`             JSON,
+           `CREATED`                 DATETIME NOT NULL,
+           `DATA_SNAPSHOT_TIME`      DATETIME,
+           `DUPLICATION_REPORT`      JSON,
+           `GEO_DISTRIBUTION_REPORT` JSON,
+           `INPUT_PRESENCE_REPORT`   JSON,
+           `LEVEL`                   VARCHAR(20),
+           `MATCH_TO_DUNS_REPORT`    JSON,
+           `OWNER_ID`                VARCHAR(255) NOT NULL,
+           `PARENT_ID`               BIGINT,
+           `REFRESH_TIME`            DATETIME,
+           `UPDATED`                 DATETIME NOT NULL,
+           `FK_TENANT_ID`            BIGINT NOT NULL,
+           PRIMARY KEY (`PID`)
+        )
+      engine=InnoDB;
+
+      CREATE INDEX IX_OWNER_ID_LEVEL ON `DCP_DATA_REPORT` (`OWNER_ID`, `LEVEL`);
+
+      CREATE INDEX IX_PARENT_ID ON `DCP_DATA_REPORT` (`PARENT_ID`);
+
+      ALTER TABLE `DCP_DATA_REPORT`
+        ADD CONSTRAINT IX_ID_LEVEL UNIQUE (`FK_TENANT_ID`, `OWNER_ID`, `LEVEL`);
+
+      ALTER TABLE `DCP_DATA_REPORT`
+        ADD CONSTRAINT `FK_DCPDATAREPORT_FKTENANTID_TENANT` FOREIGN KEY (
+        `FK_TENANT_ID`) REFERENCES `TENANT` (`TENANT_PID`) ON DELETE CASCADE;
+
 
 
   END //
