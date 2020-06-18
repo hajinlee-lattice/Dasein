443a92cea31403515887d1c5474bd5e09074f0b0
Jinyang He <jhe@lattice-engines.com>
2020-06-18 14:30:23 -0700

    pipeline: fix dcp test error.

--- le-spark/src/main/scala/com/latticeengines/spark/exposed/job/dcp/SplitImportMatchResultJob.scala
+++ le-spark/src/main/scala/com/latticeengines/spark/exposed/job/dcp/SplitImportMatchResultJob.scala
@@ -26,8 +26,10 @@ class SplitImportMatchResultJob extends AbstractSparkJob[SplitImportMatchResultC
     val rejectedCsv = filterRejected(input, matchedDunsAttr, rejectedAttrs)
     val dunsCntDF: DataFrame =  acceptedDF.groupBy(matchedDunsAttr).agg(count("*").alias("cnt"))
       .persist(StorageLevel.DISK_ONLY).checkpoint()
-    val uniqueCnt = dunsCntDF.filter(col("cnt") === 1).count()
-    val duplicatedCnt = dunsCntDF.filter(col("cnt") > 1).agg(sum("cnt").cast("long")).first().getLong(0)
+    val uniqueDF: DataFrame = dunsCntDF.filter(col("cnt") === 1)
+    val uniqueCnt = if (uniqueDF == null) 0 else uniqueDF.count()
+    val duplicateDF: DataFrame = dunsCntDF.filter(col("cnt") > 1)
+    val duplicatedCnt = if (duplicateDF == null) 0 else duplicateDF.agg(sum("cnt").cast("long")).first().getLong(0)
     val distinctCount = dunsCntDF.count()
     val duns = new DataReport.DuplicationReport
     duns.setDistinctRecords(distinctCount)
