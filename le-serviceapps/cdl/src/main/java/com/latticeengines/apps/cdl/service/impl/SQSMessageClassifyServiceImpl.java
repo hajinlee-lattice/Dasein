7a6a807606a25c0fcbc3a04967229470031ee492
Jinyang He <jhe@lattice-engines.com>
2020-07-10 11:53:54 -0700

    DP-13502: use single SQS queue for s3 import. Use Single scheduled thread pool instead of Quartz job.

--- le-serviceapps/cdl/src/main/java/com/latticeengines/apps/cdl/service/impl/SQSMessageClassifyServiceImpl.java
+++ le-serviceapps/cdl/src/main/java/com/latticeengines/apps/cdl/service/impl/SQSMessageClassifyServiceImpl.java
@@ -1,37 +1,43 @@
 package com.latticeengines.apps.cdl.service.impl;
 
-import org.quartz.DisallowConcurrentExecution;
-import org.quartz.JobDataMap;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.latticeengines.apps.cdl.service.S3ImportService;
 import com.latticeengines.apps.cdl.service.SQSMessageClassifyService;
 
-@DisallowConcurrentExecution
 @Component("sqsMessageClassifyService")
-public class SQSMessageClassifyServiceImpl extends QuartzJobBean implements SQSMessageClassifyService {
+public class SQSMessageClassifyServiceImpl implements SQSMessageClassifyService {
 
     private static final Logger log = LoggerFactory.getLogger(SQSMessageClassifyServiceImpl.class);
 
-    @Override
-    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        JobDataMap dataMap = jobExecutionContext.getJobDetail().getJobDataMap();
-        boolean importEnabled = dataMap.getBoolean("importEnabled");
-        if (importEnabled) {
-            S3ImportService s3ImportService = (S3ImportService) dataMap.get("s3ImportService");
-            if (s3ImportService == null) {
-                log.error("S3ImportService is NULL, cannot update message URL");
-            } else {
+    @Inject
+    private S3ImportService s3ImportService;
+
+    @Value("${cdl.s3.import.enabled}")
+    private boolean importEnabled;
+
+    @PostConstruct
+    public void initialize() {
+        Executors.newScheduledThreadPool(1).scheduleAtFixedRate(//
+                updateMessageUrlRunnable(), 2, //
+                30, TimeUnit.SECONDS);
+    }
+
+    private Runnable updateMessageUrlRunnable() {
+        return () -> {
+            if (importEnabled) {
                 log.info("Import enabled for current stack, start classify import message!");
                 s3ImportService.updateMessageUrl();
             }
-        } else {
-            log.info("Import is not enabled on current stack. Skip update message url.");
-        }
+        };
     }
 }
