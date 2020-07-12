57796f7c384900541ada33dba7e47eb4ef88ff3b
Jignesh Mehta <jlmehta@lattice-engines.com>
2020-07-12 13:50:23 -0700

    DP-13671: JourneyStage Configuration to datafram translator - Added as a util that can be passed a df and a JourneyStage - Added a generic grouping util - Grouping util is currently only used for Journeystage so it has lot of custom logic, but will need to be evolved to use query layer in future - Minor property name changes to JourneyStage and JourneyStagePredicate classes and using atlas standard enums as required (InterfaceName and query layer's ComparisonType) - Added some unit tests, working on more detailed tests

--- le-spark/src/main/scala/com/latticeengines/spark/exposed/job/cdl/GenerateJourneyStageJob.scala
+++ le-spark/src/main/scala/com/latticeengines/spark/exposed/job/cdl/GenerateJourneyStageJob.scala
@@ -1,8 +1,15 @@
 package com.latticeengines.spark.exposed.job.cdl
 
+import com.latticeengines.domain.exposed.cdl.activity.JourneyStage
+import com.latticeengines.domain.exposed.query.AggregateLookup
 import com.latticeengines.domain.exposed.spark.cdl.JourneyStageJobConfig
+import com.latticeengines.domain.exposed.spark.common.GroupingUtilConfig
 import com.latticeengines.spark.exposed.job.{AbstractSparkJob, LatticeContext}
-import org.apache.spark.sql.SparkSession
+import com.latticeengines.spark.util.GroupingUtil
+import org.apache.spark.sql.functions.lit
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+import scala.collection.JavaConverters._
 
 class GenerateJourneyStageJob extends AbstractSparkJob[JourneyStageJobConfig] {
 
@@ -10,5 +17,11 @@ class GenerateJourneyStageJob extends AbstractSparkJob[JourneyStageJobConfig] {
     val config = lattice.config
     // TODO add real implementation
     lattice.output = lattice.input(config.masterAccountTimeLineIdx) :: lattice.input(config.diffAccountTimeLineIdx) :: Nil
+  }
+
+  def getQualifiedAccounts(df: DataFrame, stage: JourneyStage): DataFrame = {
+    stage.getPredicates.asScala.map(sp => GroupingUtil.getGroupedDf(df, GroupingUtilConfig.from(stage, sp)) //
+      .where(df.col(AggregateLookup.Aggregator.COUNT.name).geq(lit(sp.getNoOfEvents)))) //
+      .reduce(_ union _) //
   }
 }
