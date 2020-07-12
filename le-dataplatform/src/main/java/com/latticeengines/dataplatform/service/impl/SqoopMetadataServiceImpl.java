6b8c1b310f0909aa643266a9ca514e16d20d57d7
Lei Ming <lming@lattice-engines.com>
2020-07-12 13:38:10 -0700

    DP-13539 Completely cut sqoop dependency

--- le-dataplatform/src/main/java/com/latticeengines/dataplatform/service/impl/SqoopMetadataServiceImpl.java
+++ le-dataplatform/src/main/java/com/latticeengines/dataplatform/service/impl/SqoopMetadataServiceImpl.java
@@ -1,38 +1,17 @@
 package com.latticeengines.dataplatform.service.impl;
 
-import java.io.IOException;
-
-import javax.inject.Inject;
-
 import org.apache.avro.Schema;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.sqoop.ConnFactory;
-import org.apache.sqoop.orm.AvroSchemaGenerator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
-import com.cloudera.sqoop.SqoopOptions;
-import com.cloudera.sqoop.manager.ConnManager;
-import com.cloudera.sqoop.metastore.JobData;
 import com.latticeengines.dataplatform.service.SqoopMetadataService;
-import com.latticeengines.db.exposed.service.DbMetadataService;
 import com.latticeengines.domain.exposed.modeling.DataSchema;
 import com.latticeengines.domain.exposed.modeling.DbCreds;
 
-@SuppressWarnings("deprecation")
+@Lazy
+@Deprecated
 @Component("sqoopMetadataService")
 public class SqoopMetadataServiceImpl implements SqoopMetadataService {
-
-    private static final Logger log = LoggerFactory.getLogger(SqoopMetadataServiceImpl.class);
-
-    @Inject
-    private DbMetadataService dbMetadataService;
-
-    @Inject
-    private Configuration yarnConfiguration;
 
     @Override
     public DataSchema createDataSchema(DbCreds creds, String tableName) {
@@ -41,25 +20,8 @@ public class SqoopMetadataServiceImpl implements SqoopMetadataService {
 
     @Override
     public Schema getAvroSchema(DbCreds dbCreds, String tableName) {
-        SqoopOptions options = new SqoopOptions();
-        options.setConnectString(dbMetadataService.getConnectionString(dbCreds));
-
-        if (!StringUtils.isEmpty(dbCreds.getDriverClass())) {
-            options.setDriverClassName(dbCreds.getDriverClass());
-        }
-
-        try {
-            ConnManager connManager = getConnectionManager(options);
-            AvroSchemaGenerator avroSchemaGenerator = new AvroSchemaGenerator(options, connManager, tableName);
-            return avroSchemaGenerator.generate();
-        } catch (IOException e) {
-            log.error(ExceptionUtils.getStackTrace(e));
-            return null;
-        }
+        // TODO: remove all LP2 code
+        return null;
     }
 
-    public ConnManager getConnectionManager(SqoopOptions options) throws IOException {
-        JobData data = new JobData(options, null);
-        return new ConnFactory(yarnConfiguration).getManager(data);
-    }
 }
