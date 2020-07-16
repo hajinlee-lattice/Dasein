c849aba5bbc275b9b07f08540da32decd8535971
slinlattice <slin@lattice-engines.com>
2020-07-16 05:06:59 -0700

    DP-13670 Combine multiple Journey stages into a final stage per account

--- le-spark/src/main/scala/com/latticeengines/spark/exposed/job/cdl/GenerateJourneyStageJob.scala
+++ le-spark/src/main/scala/com/latticeengines/spark/exposed/job/cdl/GenerateJourneyStageJob.scala
@@ -1,31 +1,121 @@
 package com.latticeengines.spark.exposed.job.cdl
 
 import java.time.Instant
+import java.time.temporal.ChronoUnit
 
-import com.latticeengines.domain.exposed.cdl.activity.JourneyStage
-import com.latticeengines.domain.exposed.metadata.InterfaceName
+import com.latticeengines.domain.exposed.cdl.activity.{AtlasStream, JourneyStage}
+import com.latticeengines.domain.exposed.metadata.InterfaceName.{AccountId, PartitionKey, SortKey, StageName}
 import com.latticeengines.domain.exposed.query.AggregateLookup
 import com.latticeengines.domain.exposed.spark.cdl.JourneyStageJobConfig
 import com.latticeengines.domain.exposed.spark.common.GroupingUtilConfig
+import com.latticeengines.domain.exposed.util.TimeLineStoreUtils
+import com.latticeengines.domain.exposed.util.TimeLineStoreUtils.TimelineStandardColumn._
 import com.latticeengines.spark.exposed.job.{AbstractSparkJob, LatticeContext}
-import com.latticeengines.spark.util.GroupingUtil
-import org.apache.spark.sql.functions.lit
+import com.latticeengines.spark.util.{GroupingUtil, MergeUtils}
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 import scala.collection.JavaConverters._
 
 class GenerateJourneyStageJob extends AbstractSparkJob[JourneyStageJobConfig] {
+  private val INTERNAL_PRIORITY_KEY = "__Priority__"
+  private val INTERNAL_STAGENAME_KEY = "__StageName__"
+  private val INTERNAL_TIME_KEY = "__Time__"
+  private val JOURNEY_STAGE_EVENT_TYPE = "Journey Stage Change"
+  private val JOURNEY_STAGE_SOURCE = "Atlas"
+  private val CHANGE_DETECTION_LOOKBACK_DAYS = 30
 
   override def runJob(spark: SparkSession, lattice: LatticeContext[JourneyStageJobConfig]): Unit = {
     val config = lattice.config
-    // TODO add real implementation
-    lattice.output = lattice.input(config.masterAccountTimeLineIdx) :: lattice.input(config.diffAccountTimeLineIdx) :: Nil
+    val stages = config.journeyStages.asScala
+    val defaultStageName = config.defaultStage.getStageName
+    val currTime = Instant.ofEpochMilli(config.currentEpochMilli)
+    val priorityStageMap = stages.map(stage => (stage.getPriority, stage.getStageName)).toMap
+
+    val timelineMaster = lattice.input(config.masterAccountTimeLineIdx)
+    val timelineDiff = lattice.input(config.diffAccountTimeLineIdx)
+
+
+    val priorityToStage = udf((priority: Int) => priorityStageMap(priority))
+
+    val eventTimeCol = EventDate.getColumnName
+    val mergedPriorityDf = stages.map({ stage =>
+      getQualifiedAccounts(timelineMaster, stage, currTime)
+        .select(AccountId.name)
+        .withColumn(INTERNAL_PRIORITY_KEY, lit(stage.getPriority))
+    }).reduce(_ unionByName _)
+      .groupBy(AccountId.name)
+      .agg(min(INTERNAL_PRIORITY_KEY).as(INTERNAL_PRIORITY_KEY))
+    val mergedStageDf = mergedPriorityDf
+      .withColumn(StageName.name, priorityToStage(mergedPriorityDf.col(INTERNAL_PRIORITY_KEY)))
+      .withColumn(eventTimeCol, lit(currTime.toEpochMilli))
+      .drop(INTERNAL_PRIORITY_KEY)
+
+    // calculate changed journey stages and merged master journey stages
+    val (masterStageDf, stageChangeDf) = if (config.masterJourneyStageIdx != null) {
+      val prevAccStageDf = lattice.input(config.masterJourneyStageIdx)
+      val changeDf = prevAccStageDf
+        // re-generate stage events for account that does not change stage but last event is published long time ago
+        .filter(prevAccStageDf.col(eventTimeCol).geq(currTime.minus(CHANGE_DETECTION_LOOKBACK_DAYS, ChronoUnit.DAYS).toEpochMilli))
+        .join(mergedStageDf, Seq(AccountId.name), "outer")
+        .filter(!(prevAccStageDf.col(StageName.name) <=> mergedStageDf.col(StageName.name)))
+        .withColumn(INTERNAL_STAGENAME_KEY, coalesce( //
+          mergedStageDf.col(StageName.name), //
+          lit(defaultStageName) // previously have stage but currently not, set to default stage
+        ))
+        .withColumn(INTERNAL_TIME_KEY, coalesce( //
+          mergedStageDf.col(eventTimeCol), //
+          prevAccStageDf.col(eventTimeCol) //
+        ))
+        .select(AccountId.name, INTERNAL_STAGENAME_KEY, INTERNAL_TIME_KEY)
+        .withColumnRenamed(INTERNAL_STAGENAME_KEY, StageName.name)
+        .withColumnRenamed(INTERNAL_TIME_KEY, eventTimeCol)
+
+      val newAccStageDf = MergeUtils.merge2(prevAccStageDf, changeDf, Seq(AccountId.name), Set(), overwriteByNull = false)
+      (newAccStageDf, changeDf)
+    } else {
+      (mergedStageDf, mergedStageDf)
+    }
+
+    // merge changed stage
+    val stageChangeEventDf = toTimelineEvent(stageChangeDf, config.accountTimeLineVersion, config.accountTimeLineId)
+    val mergedMasterTimeLine = MergeUtils.concat2(timelineMaster, stageChangeEventDf)
+    val mergedDiffTimeLine = MergeUtils.concat2(timelineDiff, stageChangeEventDf)
+
+    // [ master timeline, diff timeline, master journey stage ]
+    lattice.output = mergedMasterTimeLine :: mergedDiffTimeLine :: masterStageDf :: Nil
   }
 
-  def getQualifiedAccounts(df: DataFrame, stage: JourneyStage): DataFrame = {
+  def toTimelineEvent(stageDf: DataFrame, timelineVersion: String, timelineId: String): DataFrame = {
+    val generateId = udf {
+      () => TimeLineStoreUtils.generateRecordId()
+    }
+
+    val generateSortKey = udf {
+      (eventTimeStamp: Long, recordId: String) => TimeLineStoreUtils.generateSortKey(eventTimeStamp, recordId)
+    }
+
+    val generatePartitionKey = udf {
+      accountId: String => TimeLineStoreUtils.generatePartitionKey(timelineVersion, timelineId, accountId)
+    }
+
+    val recordIdCol = RecordId.getColumnName
+
+    val recordDf = stageDf.withColumn(recordIdCol, generateId())
+
+    recordDf
+      .withColumn(PartitionKey.name, generatePartitionKey(recordDf.col(AccountId.name)))
+      .withColumn(SortKey.name, generateSortKey(recordDf.col(EventDate.getColumnName), recordDf.col(recordIdCol)))
+      .withColumn(EventType.getColumnName, lit(JOURNEY_STAGE_EVENT_TYPE))
+      .withColumnRenamed(StageName.name, Detail1.getColumnName)
+      .withColumn(TrackedBySystem.getColumnName, lit(JOURNEY_STAGE_SOURCE))
+      .withColumn(StreamType.getColumnName, lit(AtlasStream.StreamType.JourneyStage.name))
+  }
+
+  def getQualifiedAccounts(df: DataFrame, stage: JourneyStage, currTime: Instant): DataFrame = {
     stage.getPredicates.asScala.map(sp => {
-      val gdf = GroupingUtil.getGroupedDf(df, GroupingUtilConfig.from(stage, sp, Instant.now()))
-      gdf.where(gdf.col(AggregateLookup.Aggregator.COUNT.name).geq(lit(sp.getNoOfEvents))).select(InterfaceName.AccountId.name)
+      val gdf = GroupingUtil.getGroupedDf(df, GroupingUtilConfig.from(stage, sp, currTime))
+      gdf.where(gdf.col(AggregateLookup.Aggregator.COUNT.name).geq(lit(sp.getNoOfEvents))).select(AccountId.name)
     }).reduce(_ union _)
   }
 }
--- le-spark/src/test/java/com/latticeengines/spark/exposed/job/cdl/GenerateJourneyStageJobTestNG.java
+++ le-spark/src/test/java/com/latticeengines/spark/exposed/job/cdl/GenerateJourneyStageJobTestNG.java
@@ -1,74 +1,215 @@
 package com.latticeengines.spark.exposed.job.cdl;
 
+import static com.latticeengines.domain.exposed.cdl.activity.AtlasStream.StreamType.DnbIntentData;
+import static com.latticeengines.domain.exposed.cdl.activity.AtlasStream.StreamType.MarketingActivity;
+import static com.latticeengines.domain.exposed.cdl.activity.AtlasStream.StreamType.Opportunity;
+import static com.latticeengines.domain.exposed.cdl.activity.AtlasStream.StreamType.WebVisit;
+import static com.latticeengines.domain.exposed.metadata.InterfaceName.AccountId;
+import static com.latticeengines.domain.exposed.metadata.InterfaceName.PartitionKey;
+import static com.latticeengines.domain.exposed.metadata.InterfaceName.SortKey;
+import static com.latticeengines.domain.exposed.metadata.InterfaceName.StageName;
+import static com.latticeengines.domain.exposed.util.TimeLineStoreUtils.TimelineStandardColumn.Detail1;
+import static com.latticeengines.domain.exposed.util.TimeLineStoreUtils.TimelineStandardColumn.Detail2;
+import static com.latticeengines.domain.exposed.util.TimeLineStoreUtils.TimelineStandardColumn.EventDate;
+import static com.latticeengines.domain.exposed.util.TimeLineStoreUtils.TimelineStandardColumn.EventType;
+import static com.latticeengines.domain.exposed.util.TimeLineStoreUtils.TimelineStandardColumn.RecordId;
+import static com.latticeengines.domain.exposed.util.TimeLineStoreUtils.TimelineStandardColumn.StreamType;
+import static com.latticeengines.domain.exposed.util.TimeLineStoreUtils.TimelineStandardColumn.TrackedBySystem;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import org.apache.avro.generic.GenericRecord;
 import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.latticeengines.common.exposed.util.JsonUtils;
+import com.latticeengines.common.exposed.validator.annotation.NotNull;
+import com.latticeengines.domain.exposed.cdl.activity.AtlasStream;
+import com.latticeengines.domain.exposed.cdl.activity.JourneyStage;
+import com.latticeengines.domain.exposed.cdl.activity.JourneyStageUtils;
+import com.latticeengines.domain.exposed.metadata.InterfaceName;
 import com.latticeengines.domain.exposed.metadata.datastore.HdfsDataUnit;
 import com.latticeengines.domain.exposed.spark.SparkJobResult;
 import com.latticeengines.domain.exposed.spark.cdl.JourneyStageJobConfig;
+import com.latticeengines.domain.exposed.util.TypeConversionUtil;
 import com.latticeengines.spark.testframework.SparkJobFunctionalTestNGBase;
 
 public class GenerateJourneyStageJobTestNG extends SparkJobFunctionalTestNGBase {
     private static final Logger log = LoggerFactory.getLogger(GenerateJourneyStageJobTestNG.class);
 
-    // TODO change standard columns
+    private static final Long FAKE_CURRENT_TIME = 5L;
+    private static final String JOURNEY_STAGE_EVENT_TYPE = "Journey Stage Change";
 
     private static final List<Pair<String, Class<?>>> TIMELINE_FIELDS = Arrays.asList( //
-            Pair.of("RecordId", String.class), //
-            Pair.of("EventTimestamp", Long.class), //
-            Pair.of("AccountId", String.class), //
-            Pair.of("ContactId", String.class), //
-            Pair.of("EventType", String.class) //
+            Pair.of(PartitionKey.name(), String.class), //
+            Pair.of(SortKey.name(), String.class), //
+            Pair.of(RecordId.getColumnName(), String.class), //
+            Pair.of(EventDate.getColumnName(), Long.class), //
+            Pair.of(AccountId.name(), String.class), //
+            Pair.of(EventType.getColumnName(), String.class), //
+            Pair.of(StreamType.getColumnName(), String.class), //
+            Pair.of(TrackedBySystem.getColumnName(), String.class), //
+            Pair.of(Detail1.getColumnName(), String.class), //
+            Pair.of(Detail2.getColumnName(), String.class) //
+    );
+    private static final List<Pair<String, Class<?>>> JOURNEY_STAGE_FIELDS = Arrays.asList( //
+            Pair.of(AccountId.name(), String.class), //
+            Pair.of(InterfaceName.StageName.name(), String.class), //
+            Pair.of(EventDate.getColumnName(), Long.class) //
     );
 
     @Test(groups = "functional")
-    private void test() {
+    private void testBaseCase() {
         prepareTimelineData();
         JourneyStageJobConfig config = new JourneyStageJobConfig();
         config.masterAccountTimeLineIdx = 0;
         config.diffAccountTimeLineIdx = 1;
-        config.currentEpochMilli = System.currentTimeMillis();
+        config.masterJourneyStageIdx = 2;
+        config.currentEpochMilli = FAKE_CURRENT_TIME;
+
+        List<JourneyStage> stages = JourneyStageUtils.atlasJourneyStages(null);
+        config.journeyStages = new ArrayList<>(stages.subList(0, stages.size() - 1));
+        config.defaultStage = stages.get(stages.size() - 1);
+        config.accountTimeLineVersion = "v1";
+        config.accountTimeLineId = "account360_timeline";
 
         SparkJobResult result = runSparkJob(GenerateJourneyStageJob.class, config);
+        log.info("Result = {}", JsonUtils.serialize(result));
         verifyResult(result);
     }
 
     @Override
     protected List<Function<HdfsDataUnit, Boolean>> getTargetVerifiers() {
-        return Arrays.asList(this::verify, this::verify);
+        return Arrays.asList(verifyTimeLineFn(17, 6), verifyTimeLineFn(7, 6),
+                verifyJourneyStageFn(getExpectedStageNames()));
     }
 
-    private Boolean verify(HdfsDataUnit tgt) {
-        AtomicInteger counter = new AtomicInteger(0);
-        List<String> verifyColumns = TIMELINE_FIELDS.stream().map(Pair::getKey).collect(Collectors.toList());
-        verifyAndReadTarget(tgt).forEachRemaining(record -> {
-            counter.incrementAndGet();
-            log.info(debugStr(record, verifyColumns));
-        });
-        log.info("Number of records = {}", counter.get());
-        return true;
+    private Function<HdfsDataUnit, Boolean> verifyTimeLineFn(int expectedNumRecords,
+            int expectedNumJourneyStageRecords) {
+        return (tgt) -> {
+            AtomicInteger counter = new AtomicInteger(0);
+            AtomicInteger journeyStageCounter = new AtomicInteger(0);
+            List<GenericRecord> records = new ArrayList<>();
+            verifyAndReadTarget(tgt).forEachRemaining(record -> {
+                Assert.assertNotNull(record.get(AccountId.name()),
+                        String.format("found null account id in record %s", record.toString()));
+                String eventType = TypeConversionUtil.toString(record.get(EventType.getColumnName()));
+                if (JOURNEY_STAGE_EVENT_TYPE.equals(eventType)) {
+                    journeyStageCounter.incrementAndGet();
+                    String streamType = TypeConversionUtil.toString(record.get(StreamType.getColumnName()));
+                    Assert.assertEquals(streamType, AtlasStream.StreamType.JourneyStage.name());
+                    Assert.assertNotNull(record.get(Detail1.getColumnName()));
+                }
+                counter.incrementAndGet();
+                records.add(record);
+            });
+            log.info("TimeLine Output:");
+            List<String> cols = TIMELINE_FIELDS.stream().map(Pair::getKey).collect(Collectors.toList());
+            records.forEach(record -> log.info(debugStr(record, cols)));
+
+            Assert.assertEquals(counter.get(), expectedNumRecords);
+            Assert.assertEquals(journeyStageCounter.get(), expectedNumJourneyStageRecords);
+            return true;
+        };
+    }
+
+    private Function<HdfsDataUnit, Boolean> verifyJourneyStageFn(@NotNull Map<String, String> expectedStages) {
+        return (tgt) -> {
+            Map<String, String> stageNames = new HashMap<>();
+            AtomicInteger counter = new AtomicInteger(0);
+            List<GenericRecord> records = new ArrayList<>();
+            verifyAndReadTarget(tgt).forEachRemaining(record -> {
+                String accountId = TypeConversionUtil.toString(record.get(AccountId.name()));
+                String stageName = TypeConversionUtil.toString(record.get(StageName.name()));
+                Assert.assertNotNull(accountId, String.format("found null account id in record %s", record.toString()));
+                Assert.assertNotNull(stageName, String.format("found null stage name in record %s", record.toString()));
+                stageNames.put(accountId, stageName);
+                counter.incrementAndGet();
+                records.add(record);
+            });
+
+            log.info("Journey Stage Output:");
+            List<String> cols = JOURNEY_STAGE_FIELDS.stream().map(Pair::getKey).collect(Collectors.toList());
+            records.forEach(record -> log.info(debugStr(record, cols)));
+
+            Assert.assertEquals(stageNames, expectedStages, "stage name for accounts do not match the expected values");
+            return true;
+        };
     }
 
     private void prepareTimelineData() {
+        // pk, sk, record id, time, account id, event type, stream type, source,
+        // detail1, detail2
         Object[][] masterData = new Object[][] { //
-                { "r1", 1L, "a1", "c1", "WebVisit" }, //
-                { "r2", 3L, "a1", "c1", "WebVisit" }, //
-                { "r4", 5L, "a1", "c1", "WebVisit" }, //
+                /*-
+                 * accounts that only qualify for one stage
+                 */
+                newTimelineRecord("a1", Opportunity, "Closed Won"), // no existing stage
+                newTimelineRecord("a2", WebVisit, "Page 1"), // same as existing stage
+                newTimelineRecord("a3", Opportunity, "Open"), // diff as existing stage
+                /*-
+                 * accounts that qualify for more than one stages
+                 */
+                newTimelineRecord("a4", DnbIntentData, "Model1"), // no existing stage
+                newTimelineRecord("a4", WebVisit, "Page 2"), // Engaged has higher priority
+
+                newTimelineRecord("a5", Opportunity, "Closed"), // same as existing stage
+                newTimelineRecord("a5", Opportunity, "Open"), //
+                newTimelineRecord("a5", DnbIntentData, "Model1"), //
+
+                newTimelineRecord("a6", MarketingActivity, "Email Click"), // diff as existing stage
+                newTimelineRecord("a6", DnbIntentData, "Model1"), //
+                newTimelineRecord("a6", Opportunity, "Closed Won"), //
+
+                /*-
+                 * accounts with no current records (qualify for default stage): a7, a8
+                 */
         };
 
         Object[][] diffData = new Object[][] { //
-                { "r5", 1L, "a1", "c1", "WebVisit" }, //
+                newTimelineRecord("a1", Opportunity, "Closed"), //
+        };
+        Object[][] journeyStageData = new Object[][] { //
+                { "a2", "Engaged", 0L }, //
+                { "a3", "Closed", 0L }, //
+                { "a5", "Closed", 0L }, //
+                { "a6", "Known Engaged", 0L }, //
+                { "a7", "Aware", 0L }, //
+                { "a8", "Closed-Won", 0L }, //
         };
 
         uploadHdfsDataUnit(masterData, TIMELINE_FIELDS);
         uploadHdfsDataUnit(diffData, TIMELINE_FIELDS);
+        uploadHdfsDataUnit(journeyStageData, JOURNEY_STAGE_FIELDS);
+    }
+
+    private Map<String, String> getExpectedStageNames() {
+        Map<String, String> stageNames = new HashMap<>();
+        stageNames.put("a1", "Closed-Won");
+        stageNames.put("a2", "Engaged");
+        stageNames.put("a3", "Opportunity");
+        stageNames.put("a4", "Engaged");
+        stageNames.put("a5", "Closed");
+        stageNames.put("a6", "Closed-Won");
+        stageNames.put("a7", "Dark");
+        stageNames.put("a8", "Dark");
+        return stageNames;
+    }
+
+    private Object[] newTimelineRecord(String accountId, AtlasStream.StreamType type, String detail1) {
+        return new Object[] { //
+                "p", "s", "r", 0L, // not testing pk, sk, uuid and timestamp for now
+                accountId, "fake event", type == null ? null : type.name(), null, detail1, null, //
+        };
     }
 }
