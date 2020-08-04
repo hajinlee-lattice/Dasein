55457b361d815af6d9627053c168de435810f37e
Yintao Song <ysong@lattice-engines.com>
2020-08-04 14:32:39 -0700

    PD-3191 add job functions, titles and LiveRampDestination

---
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/datacloud/tps/LiveRampDestination.java
@@ -0,0 +1,23 @@
+package com.latticeengines.domain.exposed.datacloud.tps;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum LiveRampDestination {
+    Google("Google DV360"), //
+    Tradedesk("The Trade Desk"), //
+    Mediamath("MediaMath"), //
+    Verizon("Verizon OATH"), //
+    AppNexus("AppNexus"), //
+    Adobe("Adobe Audience Manager");
+
+    private final String displayName;
+
+    LiveRampDestination(String displayName) {
+        this.displayName = displayName;
+    }
+
+    @JsonValue
+    public String getDisplayName() {
+        return displayName;
+    }
+}
