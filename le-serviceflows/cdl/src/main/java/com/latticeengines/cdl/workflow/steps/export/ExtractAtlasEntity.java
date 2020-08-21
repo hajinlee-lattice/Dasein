6ec4cc4a4c48b92e45392eacd5b051dd37b9a7af
gengleiLattice <LGeng@lattice-engines.com>
2020-08-21 15:07:53 +0800

    Update ExtractAtlasEntity.java

--- le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/workflow/steps/export/ExtractAtlasEntity.java
+++ le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/workflow/steps/export/ExtractAtlasEntity.java
@@ -243,7 +243,6 @@ public class ExtractAtlasEntity extends BaseSparkSQLStep<EntityExportStepConfigu
         List<ColumnMetadata> cms = schemaMap.getOrDefault(businessEntity, new ArrayList<>());
         cms.add(columnMetadata);
         schemaMap.put(businessEntity, cms);
-        WorkflowStaticContext.putObject(EXPORT_SCHEMA_MAP, schemaMap);
     }
 
     private List<Lookup> convertToLookup(List<List<ColumnMetadata>> columnMetadataList) {
