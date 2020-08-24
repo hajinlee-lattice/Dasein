d48f824ba419ed49b61c53c289ad80068db38de3
Han Zhao <zhaoh@dnb.com>
2020-08-24 16:01:46 -0400

    PLS-18203: Fixing bug in GenerateLaunchArtifactJob

--- le-spark/src/main/scala/com/latticeengines/spark/util/CountryCodeUtils.scala
+++ le-spark/src/main/scala/com/latticeengines/spark/util/CountryCodeUtils.scala
@@ -14,17 +14,21 @@ private[spark] object CountryCodeUtils {
   def convert(input: DataFrame, countryName: String, countryCodeName: String, url: String, user: String,
               passwd: String, key: String, salt: String)
   : DataFrame = {
-    // load data into map once
-    initMap(url, user, passwd, key, salt)
-    val spark = SparkSession.builder().appName("reading country code").getOrCreate()
-    val broadCastDictionary = spark.sparkContext.broadcast(map)
-    val nameFunc:  String => String = name => {
-      val cleanName = LocationUtils.getStandardCountry(name)
-      val result = if (cleanName == null) null else broadCastDictionary.value.get(cleanName)
-      result
+    if (!(input.columns.contains(countryName))) {
+      input
+    } else {
+      // load data into map once
+      initMap(url, user, passwd, key, salt)
+      val spark = SparkSession.builder().appName("reading country code").getOrCreate()
+      val broadCastDictionary = spark.sparkContext.broadcast(map)
+      val nameFunc:  String => String = name => {
+        val cleanName = LocationUtils.getStandardCountry(name)
+        val result = if (cleanName == null) null else broadCastDictionary.value.get(cleanName)
+        result
+      }
+      val nameUDF = udf(nameFunc)
+      input.withColumn(countryCodeName, nameUDF(col(countryName)))
     }
-    val nameUDF = udf(nameFunc)
-    input.withColumn(countryCodeName, nameUDF(col(countryName)))
   }
 
 
