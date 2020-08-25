ff9779ba3dc7ee22095a5bbb709d1a33c09d27c7
Clarence Lucas <lucascl@dnb.com>
2020-08-24 19:47:09 -0500

    DCP-1232 Updated queries so deleted IS null is considered the same as deleted = false.

--- le-serviceapps/dcp/src/main/java/com/latticeengines/apps/dcp/repository/ProjectRepository.java
+++ le-serviceapps/dcp/src/main/java/com/latticeengines/apps/dcp/repository/ProjectRepository.java
@@ -19,7 +19,7 @@ public interface ProjectRepository extends BaseJpaRepository<Project, Long> {
 
     @Query("SELECT p.projectId, p.projectDisplayName, p.rootPath, p.deleted, p.created, p.updated, p.createdBy, " +
             " p.recipientList, p.teamId, p.projectDescription " +
-            " FROM Project AS p where p.deleted = false")
+            " FROM Project AS p where p.deleted = false OR p.deleted IS null")
     List<Object[]> findAllProjects(Pageable pageable);
 
     @Query("SELECT p.projectId, p.projectDisplayName, p.rootPath, p.deleted, p.created, p.updated, p.createdBy, " +
@@ -47,13 +47,8 @@ public interface ProjectRepository extends BaseJpaRepository<Project, Long> {
 
     @Query("SELECT p.projectId, p.projectDisplayName, p.rootPath, p.deleted, p.created, p.updated, p.createdBy, " +
             "p.recipientList, p.teamId, p.projectDescription " +
-            " FROM Project AS p WHERE (p.teamId IN (?1) OR p.teamId IS null) and p.deleted = false")
+            " FROM Project AS p WHERE (p.teamId IN (?1) OR p.teamId IS null) and (p.deleted = false OR p.deleted IS null)")
     List<Object[]> findProjectsInTeamIds(List<String> teamIds, Pageable pageable);
-
-    /* @Query("SELECT p.projectId, p.projectDisplayName, p.rootPath, p.deleted, p.created, p.updated, p.createdBy, " +
-            "p.recipientList, p.teamId, p.projectDescription " +
-            " FROM Project AS p WHERE p.projectId = ?1 AND (p.teamId IN (?2) OR p.teamId IS null) and p.deleted = false")
-    List<Object[]> findProjectInfoByProjectIdInTeamIdsNotIncludingArchived(String projectId, List<String> teamIds); */
 
     @Query("SELECT count(p) from Project AS p where p.deleted = false")
     Long countActiveProjects();
