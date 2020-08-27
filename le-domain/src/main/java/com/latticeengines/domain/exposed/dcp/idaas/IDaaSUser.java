79b2868d83de84ee17becd834c1a06e67b709000
Jinyang He <jhe@lattice-engines.com>
2020-08-27 15:46:44 -0700

    Fix Check Style

--- le-domain/src/main/java/com/latticeengines/domain/exposed/dcp/idaas/IDaaSUser.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/dcp/idaas/IDaaSUser.java
@@ -9,7 +9,6 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.latticeengines.domain.exposed.dcp.idaas.ProductSubscription;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
