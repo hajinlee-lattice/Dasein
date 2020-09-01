f37bae4b85a1babf80448b7d79b03ee4e18c2d2f
Hajin Lee <leeha@dnb.com>
2020-08-31 17:17:06 -0700

    PLS-18270 Cannot transform JSON error

--- le-domain/src/main/java/com/latticeengines/domain/exposed/cdl/EventDetail.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/cdl/EventDetail.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXTERNAL_PROPERTY, property = "type")
 @JsonSubTypes({ //
+        @JsonSubTypes.Type(value = ProgressEventDetail.class, name = "Progress"),
         @JsonSubTypes.Type(value = ProgressEventDetail.class, name = "Completed"),
         @JsonSubTypes.Type(value = ProgressEventDetail.class, name = "InProgress"),
         @JsonSubTypes.Type(value = AudienceEventDetail.class, name = "AudienceCreation"),
