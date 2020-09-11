a893b4965e68b5264717f409d9bb78642dadaa61
Nick Lizotte <lizotten@dnb.com>
2020-09-10 20:49:59 -0400

    DCP-1695 refresh access token before sending request for invitation link

--- le-security/src/main/java/com/latticeengines/security/service/impl/IDaaSServiceImpl.java
+++ le-security/src/main/java/com/latticeengines/security/service/impl/IDaaSServiceImpl.java
@@ -309,6 +309,7 @@ public class IDaaSServiceImpl implements IDaaSService {
 
     @Override
     public InvitationLinkResponse getUserInvitationLink(String email) {
+        refreshToken();
         InvitationLinkResponse response = null;
         try {
             RetryTemplate retryTemplate = RetryUtils.getRetryTemplate(3);
