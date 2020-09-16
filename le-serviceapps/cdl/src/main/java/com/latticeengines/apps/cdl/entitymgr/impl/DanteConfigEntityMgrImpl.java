1769694fc1f94e5ec3f3668567e71ee6b10f2cc5
caor <caor@dnb.com>
2020-09-16 14:56:48 -0700

    Change on Mgr createOrUpdate

--- le-serviceapps/cdl/src/main/java/com/latticeengines/apps/cdl/entitymgr/impl/DanteConfigEntityMgrImpl.java
+++ le-serviceapps/cdl/src/main/java/com/latticeengines/apps/cdl/entitymgr/impl/DanteConfigEntityMgrImpl.java
@@ -57,16 +57,10 @@ public class DanteConfigEntityMgrImpl extends BaseDocumentEntityMgrImpl<DanteCon
     @Transactional(propagation = Propagation.REQUIRED)
     public DanteConfigurationDocument createOrUpdate(String tenantId, DanteConfigurationDocument danteConfig) {
 
-        DanteConfigEntity existing = readerRepository.findByTenantId(tenantId);
-
         DanteConfigEntity danteConfigEntity = new DanteConfigEntity();
         danteConfigEntity.setUuid(UUID.randomUUID().toString());
         danteConfigEntity.setTenantId(tenantId);
         danteConfigEntity.setDocument(danteConfig);
-
-        if (existing != null) {
-            writerRepository.removeByTenantId(tenantId);
-        }
 
         DanteConfigEntity saved = writerRepository.save(danteConfigEntity);
         return saved.getDocument();
