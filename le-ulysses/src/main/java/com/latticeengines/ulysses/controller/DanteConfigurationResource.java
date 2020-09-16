d372aa9a0eb3ff0de95280d5c3efc176a106cfb5
caor <caor@dnb.com>
2020-09-15 23:40:59 -0700

    Update for ulysses DanteConfig Resource

--- le-ulysses/src/main/java/com/latticeengines/ulysses/controller/DanteConfigurationResource.java
+++ le-ulysses/src/main/java/com/latticeengines/ulysses/controller/DanteConfigurationResource.java
@@ -5,7 +5,6 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +20,7 @@ import com.latticeengines.proxy.exposed.cdl.CDLDanteConfigProxy;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
-@Api(value = "DanteConfiguration")
+@Api(value = "DanteConfiguration", description = "Common REST resource to serve configuration for Dante UI")
 @RestController
 @RequestMapping("/danteconfiguration")
 public class DanteConfigurationResource {
@@ -30,26 +29,22 @@ public class DanteConfigurationResource {
     @Inject
     CDLDanteConfigProxy cdlDanteConfigProxy;
 
-    @GetMapping("/{tenantId}")
-    @ResponseBody
-    @ApiOperation(value = "Get a dante configuration by tenantId")
-    public DanteConfigurationDocument getDanteconfigurationByTenantId(@PathVariable String tenantId) {
-        return cdlDanteConfigProxy.getDanteConfiguration(tenantId);
-    }
-
     @GetMapping
     @ResponseBody
     @ApiOperation(value = "Get Dante configuration")
     public FrontEndResponse<DanteConfigurationDocument> getDanteConfiguration() {
         String customerSpace = MultiTenantContext.getShortTenantId();
-        try (PerformanceTimer timer = new PerformanceTimer("Get Dante Configuration | Tenant=" + customerSpace, log)) {
-            DanteConfigurationDocument danteConfigurationDocument = getDanteconfigurationByTenantId(customerSpace);
+        try {
+            PerformanceTimer timer = new PerformanceTimer("get Dante Configuration", log);
+            DanteConfigurationDocument danteConfigurationDocument = cdlDanteConfigProxy
+                    .getDanteConfiguration(customerSpace);
+            timer.close();
             return new FrontEndResponse<>(danteConfigurationDocument);
         } catch (LedpException le) {
-            log.error("Failed to get Dante Configuration document", le);
+            log.error("Failed to get talking point data", le);
             return new FrontEndResponse<>(le.getErrorDetails());
         } catch (Exception e) {
-            log.error("Failed to get Dante Configuration document", e);
+            log.error("Failed to get talking point data", e);
             return new FrontEndResponse<>(new LedpException(LedpCode.LEDP_00002, e).getErrorDetails());
         }
     }
