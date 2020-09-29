ceeed076be457810c024b9b913982f4c7fd8fe9f
Yintao Song <ysong@lattice-engines.com>
2020-09-28 22:35:21 -0700

    fix checkstyle

--- le-datacloud/match/src/main/java/com/latticeengines/datacloud/match/service/VboUsageService.java
+++ le-datacloud/match/src/main/java/com/latticeengines/datacloud/match/service/VboUsageService.java
@@ -1,7 +1,7 @@
 package com.latticeengines.datacloud.match.service;
 
-import com.latticeengines.domain.exposed.datacloud.usage.VboBatchUsageReport;
 import com.latticeengines.domain.exposed.datacloud.usage.SubmitBatchReportRequest;
+import com.latticeengines.domain.exposed.datacloud.usage.VboBatchUsageReport;
 
 public interface VboUsageService {
 
