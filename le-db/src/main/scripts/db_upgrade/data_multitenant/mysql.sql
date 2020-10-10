aa8fa61076f8bb8b2cd1d78b01bc2d847f2c1922
Stephen Lin <slin@lattice-engines.com>
2020-10-09 17:39:24 -0700

    Fix create ActivityAlert table syntax error

--- le-db/src/main/scripts/db_upgrade/data_multitenant/mysql.sql
+++ le-db/src/main/scripts/db_upgrade/data_multitenant/mysql.sql
@@ -14,27 +14,29 @@ DELIMITER //
 CREATE PROCEDURE `UpdateSchema`()
   BEGIN
       -- User input section (DDL/DML). This is just a template, developer can modify based on need.
-      create table `ActivityAlert` (
-            `PID` bigint not null auto_increment,
-            `ALERT_DATA` JSON,
-            `ALERT_NAME` varchar(255) not null,
-            `CATEGORY` varchar(255) not null,
-            `CREATION_TIMESTAMP` datetime not null,
-            `ENTITY_ID` varchar(255) not null,
-            `ENTITY_TYPE` varchar(255) not null,
-            `TENANT_ID` bigint not null,
-            `VERSION` varchar(255) not null,
-         primary key (`PID`, `ENTITY_ID`, `TENANT_ID`))
-         PARTITION BY KEY(`ENTITY_ID`, `TENANT_ID`)
-         PARTITIONS 2000
-         engine=InnoDB;
+      create table `ActivityAlert`
+      (
+          `PID`                bigint       not null auto_increment,
+          `ALERT_DATA`         JSON,
+          `ALERT_NAME`         varchar(255) not null,
+          `CATEGORY`           varchar(255) not null,
+          `CREATION_TIMESTAMP` datetime     not null,
+          `ENTITY_ID`          varchar(255) not null,
+          `ENTITY_TYPE`        varchar(255) not null,
+          `TENANT_ID`          bigint       not null,
+          `VERSION`            varchar(255) not null,
+          primary key (`PID`, `ENTITY_ID`, `TENANT_ID`)
+      )
+          ENGINE = InnoDB
+          PARTITION BY KEY (`ENTITY_ID`, `TENANT_ID`)
+              PARTITIONS 2000;
 
       create index REC_CREATION_TIMESTAMP on `ActivityAlert` (`CREATION_TIMESTAMP`);
       create index REC_VERSION on `ActivityAlert` (`VERSION`);
       create index REC_CATEGORY on `ActivityAlert` (`CATEGORY`);
       alter table `ActivityAlert`
-        add constraint `UK3gmot6qm9hdifa50vhnchx2kq`
-        unique (`ENTITY_ID`, `ENTITY_TYPE`, `TENANT_ID`, `VERSION`, `CREATION_TIMESTAMP`, `ALERT_NAME`);
+          add constraint `UK3gmot6qm9hdifa50vhnchx2kq`
+              unique (`ENTITY_ID`, `ENTITY_TYPE`, `TENANT_ID`, `VERSION`, `CREATION_TIMESTAMP`, `ALERT_NAME`);
 
   END //
 -- ##############################################################
