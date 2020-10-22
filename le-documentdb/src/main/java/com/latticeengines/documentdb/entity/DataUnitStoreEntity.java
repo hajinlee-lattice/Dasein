1ba092b0112f7164dc364efc949695763ad652a6
lzhao <LZhao@lattice-engines.com>
2020-10-22 03:19:49 -0400

    DP-14428

--- le-documentdb/src/main/java/com/latticeengines/documentdb/entity/DataUnitStoreEntity.java
+++ le-documentdb/src/main/java/com/latticeengines/documentdb/entity/DataUnitStoreEntity.java
@@ -1,7 +1,6 @@
 package com.latticeengines.documentdb.entity;
 
 import javax.persistence.Entity;
-import javax.persistence.Index;
 import javax.persistence.Table;
 
 import com.latticeengines.domain.exposed.metadata.datastore.DataUnitStore;
