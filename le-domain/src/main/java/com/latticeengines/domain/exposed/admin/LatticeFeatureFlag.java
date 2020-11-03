8277816ce6a0e50581968a211867529685687292
jxiao <jxiao@lattice-engines.com>
2020-11-03 06:31:13 -0500

    DP-14616:Create feature flag for M41 performance report

--- le-domain/src/main/java/com/latticeengines/domain/exposed/admin/LatticeFeatureFlag.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/admin/LatticeFeatureFlag.java
@@ -48,7 +48,7 @@ public enum LatticeFeatureFlag {
     ATTRIBUTE_TOGGLING("AttributeToggling", "Allow multi click behavior for Attributes during query building"), //
     ENABLE_ACXIOM("EnableAcxiom", "Enable Acxiom in ads platform integrations"), //
     ENABLE_IMPORT_V2("EnableImportV2", "Enable Import UI 2.0"), //
-    SSVI_REPORT("SSVI_REPORT", "SSVI_REPORT"), //
+    SSVI_REPORT("SSVIReport", "SSVIReport"), //
 
     @Deprecated
     QUOTA(true, "Quota", "Quota"), //
