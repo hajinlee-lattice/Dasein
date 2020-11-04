1fc770fa4c4ad6aa9d5eedf47432f186220e5dfb
Sayali Sakhalkar <ssakhalkar@lattice-engines.com>
2020-11-03 18:42:15 -0800

    Dnb seed addin missin cols transformer update

--- le-domain/src/main/java/com/latticeengines/domain/exposed/datacloud/transformation/config/seed/DnBAddMissingColsConfig.java
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/datacloud/transformation/config/seed/DnBAddMissingColsConfig.java
@@ -1,5 +1,7 @@
 package com.latticeengines.domain.exposed.datacloud.transformation.config.seed;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.latticeengines.domain.exposed.datacloud.transformation.config.impl.TransformerConfig;
 
@@ -10,6 +12,12 @@ public class DnBAddMissingColsConfig extends TransformerConfig {
 
     @JsonProperty("DunsField")
     private String duns;
+
+    @JsonProperty("FieldsToRemove")
+    private List<String> fieldsToRemove;
+
+    @JsonProperty("FieldsToPopulateNull")
+    private List<String> fieldsToPopulateNull;
 
     public String getDomain() {
         return domain;
@@ -25,5 +33,21 @@ public class DnBAddMissingColsConfig extends TransformerConfig {
 
     public void setDuns(String duns) {
         this.duns = duns;
+    }
+
+    public List<String> getFieldsToRemove() {
+        return fieldsToRemove;
+    }
+
+    public void setFieldsToRemove(List<String> fieldsToRemove) {
+        this.fieldsToRemove = fieldsToRemove;
+    }
+
+    public List<String> getFieldsToPopulateNull() {
+        return fieldsToPopulateNull;
+    }
+
+    public void setFieldsToPopulateNull(List<String> fieldsToPopulateNull) {
+        this.fieldsToPopulateNull = fieldsToPopulateNull;
     }
 }
