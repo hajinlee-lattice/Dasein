5e443e0b8f6df02c446c09e2c8c428b8d4707de7
lzhao <LZhao@lattice-engines.com>
2020-11-15 21:14:43 -0500

    PLS-19011: add test

--- le-domain/src/test/java/com/latticeengines/domain/exposed/util/ExportUtilsUnitTestNG.java
+++ le-domain/src/test/java/com/latticeengines/domain/exposed/util/ExportUtilsUnitTestNG.java
@@ -22,7 +22,7 @@ public class ExportUtilsUnitTestNG {
     public void testGetDisplayNameMap() {
         List<ColumnMetadata> columnMetadataList = buildMetadataList();
         Map<String, String> displayNameMap = ExportUtils.getDisplayNameMap(ExportEntity.AccountContact, columnMetadataList);
-        assertEquals(displayNameMap.size(), 18);
+        assertEquals(displayNameMap.size(), 22);
         assertEquals(displayNameMap.get(InterfaceName.AccountId.name()), "AccountId");
         assertTrue(displayNameMap.containsValue("My Attributes_Country"));
         assertTrue(displayNameMap.containsValue("My Attributes_Country(2)"));
@@ -31,6 +31,11 @@ public class ExportUtilsUnitTestNG {
         assertEquals(displayNameMap.get("MAILING_STATE_PROVINCE_NAME"), "Mailing State/Province");
         assertTrue(displayNameMap.containsValue("Firmographics_Country"));
         assertTrue(displayNameMap.containsValue("Firmographics_Country(2)"));
+
+        assertEquals(displayNameMap.get("DESTINATION_SYS_NAME"), "Campaign Launch System Name");
+        assertTrue(displayNameMap.containsValue("Country"));
+        assertTrue(displayNameMap.containsValue("Country(2)"));
+        assertTrue(displayNameMap.containsValue("Country(3)"));
 
         assertEquals(displayNameMap.get("Account__EntityCreatedSource"), "Lattice - Created Source");
         assertTrue(displayNameMap.containsValue("Curated Account Attributes_Country"));
@@ -59,6 +64,11 @@ public class ExportUtilsUnitTestNG {
         columnMetadataList.add(buildColumnMetadata(BusinessEntity.Account, Category.FIRMOGRAPHICS, "MAILING_STATE_PROVINCE_NAME", "Mailing State/Province"));
         columnMetadataList.add(buildColumnMetadata(BusinessEntity.Account, Category.FIRMOGRAPHICS, "LDC_Country", "Country"));
         columnMetadataList.add(buildColumnMetadata(BusinessEntity.Account, Category.FIRMOGRAPHICS, "LDC_City", "Country"));
+
+        columnMetadataList.add(buildColumnMetadata(BusinessEntity.Account, null, "DESTINATION_SYS_NAME", "Campaign Launch System Name"));
+        columnMetadataList.add(buildColumnMetadata(BusinessEntity.Account, null, "PLAY_NAME", "Country"));
+        columnMetadataList.add(buildColumnMetadata(BusinessEntity.Account, null, "RATING_MODEL_NAME", "Country"));
+        columnMetadataList.add(buildColumnMetadata(BusinessEntity.Account, null, "SEGMENT_NAME", "Country"));
 
         columnMetadataList.add(buildColumnMetadata(BusinessEntity.CuratedAccount, Category.CURATED_ACCOUNT_ATTRIBUTES, "Account__EntityCreatedSource", "Lattice - Created Source"));
         columnMetadataList.add(buildColumnMetadata(BusinessEntity.CuratedAccount, Category.CURATED_ACCOUNT_ATTRIBUTES, "Account__EntityLastUpdatedDate", "Country"));
