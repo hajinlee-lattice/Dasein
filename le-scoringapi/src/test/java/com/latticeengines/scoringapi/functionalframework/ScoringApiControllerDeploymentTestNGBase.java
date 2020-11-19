cf34abdce4d4d3a46062387d78f2e62651aba5d0
Lei Ming <lming@lattice-engines.com>
2020-11-18 16:23:56 -0800

    fix scoring api test

--- le-scoringapi/src/test/java/com/latticeengines/scoringapi/functionalframework/ScoringApiControllerDeploymentTestNGBase.java
+++ le-scoringapi/src/test/java/com/latticeengines/scoringapi/functionalframework/ScoringApiControllerDeploymentTestNGBase.java
@@ -384,7 +384,7 @@ public class ScoringApiControllerDeploymentTestNGBase extends ScoringApiFunction
         // int[] newExpected = { 96, 89, 96, 94 }; // LDC 2.0.21 or before
         // int[] newExpected = { 68, 87, 89, 85 }; // LDC 2.0.24
         // int[] newExpected = { 68, 89, 78, 85 }; // LDC 2.0.25
-        int[] newExpected = { 68, 87, 78, 85 }; // LDC 2.0.26
+        int[] newExpected = { 68, 87, 75, 85 }; // LDC 2.0.26
         for (int score : newExpected) {
             expectedScores.add(score);
         }
