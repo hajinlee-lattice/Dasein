24cfa8d544ab3a25d4fb60828476f604d7417230
jxiao <jxiao@lattice-engines.com>
2020-11-24 02:16:33 -0500

    DP-14614:Create APIs for dashboard management(fix checkstyle)

--- le-pls/src/main/java/com/latticeengines/pls/controller/VIDashboardResource.java
+++ le-pls/src/main/java/com/latticeengines/pls/controller/VIDashboardResource.java
@@ -2,8 +2,6 @@ package com.latticeengines.pls.controller;
 
 import javax.inject.Inject;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
