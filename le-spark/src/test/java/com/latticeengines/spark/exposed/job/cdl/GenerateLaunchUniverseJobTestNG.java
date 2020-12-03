f5843f8375cd0fd0333666ef01aa081e44b0ee67
Hajin Lee <leeha@dnb.com>
2020-12-02 16:22:58 -0800

    Add a functional test

--- le-spark/src/test/java/com/latticeengines/spark/exposed/job/cdl/GenerateLaunchUniverseJobTestNG.java
+++ le-spark/src/test/java/com/latticeengines/spark/exposed/job/cdl/GenerateLaunchUniverseJobTestNG.java
@@ -1,13 +1,23 @@
 package com.latticeengines.spark.exposed.job.cdl;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.inject.Inject;
 
+import org.apache.avro.file.FileReader;
+import org.apache.avro.generic.GenericRecord;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.latticeengines.common.exposed.util.AvroUtils;
+import com.latticeengines.common.exposed.util.HdfsUtils;
+import com.latticeengines.common.exposed.util.HdfsUtils.HdfsFileFilter;
 import com.latticeengines.common.exposed.util.JsonUtils;
 import com.latticeengines.domain.exposed.metadata.InterfaceName;
 import com.latticeengines.domain.exposed.spark.SparkJobResult;
@@ -30,6 +40,13 @@ public class GenerateLaunchUniverseJobTestNG extends SparkJobFunctionalTestNGBas
 
     private static final String CDL_UPDATED_TIME = InterfaceName.CDLUpdatedTime.name();
     private static final String DESC = "DESC";
+
+    private static final HdfsFileFilter avroFileFilter = new HdfsFileFilter() {
+        @Override
+        public boolean accept(FileStatus file) {
+            return file.getPath().getName().endsWith("avro");
+        }
+    };
 
     @Inject
     private Configuration yarnConfiguration;
@@ -84,7 +101,7 @@ public class GenerateLaunchUniverseJobTestNG extends SparkJobFunctionalTestNGBas
     public void testGenerateLaunchUniverseJobColumnNotFound() throws Exception {
         // When sort column is not found, use ContactId to sort
         GenerateLaunchUniverseJobConfig config = new GenerateLaunchUniverseJobConfig();
-        config.setWorkspace("testGenerateLaunchUniverseJobBothLimits");
+        config.setWorkspace("testGenerateLaunchUniverseJobColumnNotFound");
 
         config.setMaxContactsPerAccount(2L);
         config.setContactsPerAccountSortAttribute("Unknown Column");
@@ -95,7 +112,59 @@ public class GenerateLaunchUniverseJobTestNG extends SparkJobFunctionalTestNGBas
         log.info("TestGenerateLaunchUniverseJobColumnNotFound Results: " + JsonUtils.serialize(result));
 
         Assert.assertEquals(result.getTargets().get(0).getCount().intValue(), 16);
-
     }
 
+    @Test(groups = "functional")
+    public void testGenerateLaunchUniverseJobDefaultSort() throws Exception {
+        // When sort column is not found, use ContactId to sort
+        GenerateLaunchUniverseJobConfig config = new GenerateLaunchUniverseJobConfig();
+        config.setWorkspace("testGenerateLaunchUniverseJobDefaultSort");
+        config.setMaxContactsPerAccount(2L);
+
+        log.info("Config: " + JsonUtils.serialize(config));
+        SparkJobResult result = runSparkJob(GenerateLaunchUniverseJob.class, config);
+        log.info("TestGenerateLaunchUniverseJobDefaultSort Results: " + JsonUtils.serialize(result));
+
+        Assert.assertTrue(verifyDefaultSort(result));
+    }
+
+    private boolean verifyDefaultSort(SparkJobResult result) throws Exception {
+        String hdfsDir = result.getTargets().get(0).getPath();
+        String fieldName = "ContactId";
+        List<String> avroFilePaths = HdfsUtils.getFilesForDir(yarnConfiguration, hdfsDir, avroFileFilter);
+        String filePathStr = avroFilePaths.get(0).toString();
+        int index = 0;
+        log.info("File path is: " + filePathStr);
+    
+        try (FileReader<GenericRecord> reader = AvroUtils.getAvroFileReader(yarnConfiguration, new Path(filePathStr))) {
+            for (GenericRecord record : reader) {
+                String contactId = getString(record, fieldName);
+                String expectedId = getExpectedId(index);
+                if (contactId != expectedId) {
+                    log.info("ContactId in data: " + contactId + " / Expected: " + expectedId);
+                    return false;
+                }
+                index++;
+            }
+        }
+
+        return true;
+    }
+
+    private static String getString(GenericRecord record, String field) throws Exception {
+        String value;
+        try {
+            value = record.get(field).toString();
+        } catch (Exception e) {
+            value = "";
+        }
+        return value;
+    }
+
+    private static String getExpectedId(int index) {
+        List<String> expectedIds = Arrays.asList(
+                "C11", "C12", "C22", "C21", "C3", "C4", "C5", "C6",
+                "C72", "C73", "C81", "C82", "C92", "C91", "C115", "C98");
+        return expectedIds.get(index);
+    }
 }
