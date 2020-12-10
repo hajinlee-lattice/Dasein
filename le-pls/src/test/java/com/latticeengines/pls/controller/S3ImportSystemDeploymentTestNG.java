572bdd4b7c4b865e6e076f4696c2fae19090c8ba
PenglongLiu <penglong.liu@lattice-engines.com>
2020-12-10 15:01:14 +0800

    fix checkstyle

--- le-pls/src/test/java/com/latticeengines/pls/controller/S3ImportSystemDeploymentTestNG.java
+++ le-pls/src/test/java/com/latticeengines/pls/controller/S3ImportSystemDeploymentTestNG.java
@@ -6,8 +6,8 @@ import java.util.Map;
 
 import javax.inject.Inject;
 
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.MapUtils;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
