507d60a517383ad944869a366fee0fd060346a85
Clarence Lucas <lucascl@dnb.com>
2020-12-14 10:11:37 -0600

    DCP-1838 Added new column to DCP_DATA_REPORT table

--- le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
+++ le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
@@ -13,7 +13,13 @@ DELIMITER //
 CREATE PROCEDURE `UpdateSchema`()
   BEGIN
       -- User input section (DDL/DML). This is just a template, developer can modify based on need.
-      
+
+
+      -- DCP-1838, author: lucascl@dnb.com product: D&B Connect
+      ALTER TABLE `DCP_DATA_REPORT`
+          ADD COLUMN `ROLLUP_STATUS` VARCHAR(255);
+
+
   END //
 -- ##############################################################
 
