596b281cc11c5a18cb76c380e039dc5774e6d984
lzhao <LZhao@lattice-engines.com>
2020-12-16 22:22:42 -0500

    DP-15205

--- le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/workflow/steps/campaign/GenerateLaunchUniverse.java
+++ le-serviceflows/cdl/src/main/java/com/latticeengines/cdl/workflow/steps/campaign/GenerateLaunchUniverse.java
@@ -1,4 +1,5 @@
 package com.latticeengines.cdl.workflow.steps.campaign;
+
 import static com.latticeengines.domain.exposed.pls.Play.TapType;
 import static com.latticeengines.workflow.exposed.build.WorkflowStaticContext.ATTRIBUTE_REPO;
 
@@ -18,16 +19,12 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Component;
 
-import com.latticeengines.camille.exposed.Camille;
-import com.latticeengines.camille.exposed.CamilleEnvironment;
-import com.latticeengines.camille.exposed.paths.PathBuilder;
 import com.latticeengines.cdl.workflow.steps.campaign.utils.CampaignFrontEndQueryBuilder;
 import com.latticeengines.cdl.workflow.steps.campaign.utils.CampaignLaunchUtils;
 import com.latticeengines.cdl.workflow.steps.export.BaseSparkSQLStep;
 import com.latticeengines.common.exposed.util.JsonUtils;
 import com.latticeengines.common.exposed.util.RetryUtils;
 import com.latticeengines.domain.exposed.camille.CustomerSpace;
-import com.latticeengines.domain.exposed.camille.Path;
 import com.latticeengines.domain.exposed.cdl.CDLExternalSystemName;
 import com.latticeengines.domain.exposed.exception.LedpCode;
 import com.latticeengines.domain.exposed.exception.LedpException;
@@ -61,13 +58,6 @@ import com.latticeengines.workflow.exposed.util.WorkflowJobUtils;
 public class GenerateLaunchUniverse extends BaseSparkSQLStep<GenerateLaunchUniverseStepConfiguration> {
     private static final Logger log = LoggerFactory.getLogger(GenerateLaunchUniverse.class);
 
-    private static final String CDL = "CDL";
-    private static final String CONTACTS_PER_ACCOUNT_SORT = "/ContactsPerAccountSort";
-    private static final String ATTRIBUTE = "/Attribute";
-    private static final String DIRECTION = "/Direction";
-    private static final String CDL_UPDATED_TIME = "CDLUpdatedTime";
-    private static final String DESC = "DESC";
-
     @Inject
     private PeriodProxy periodProxy;
 
@@ -86,20 +76,15 @@ public class GenerateLaunchUniverse extends BaseSparkSQLStep<GenerateLaunchUnive
     public void execute() {
         GenerateLaunchUniverseStepConfiguration config = getConfiguration();
         CustomerSpace customerSpace = config.getCustomerSpace();
-
         Play play = playProxy.getPlay(customerSpace.getTenantId(), config.getPlayId(), false, false);
         PlayLaunchChannel channel = playProxy.getChannelById(customerSpace.getTenantId(), config.getPlayId(),
                 config.getChannelId());
         if (play == null) {
-            throw new LedpException(LedpCode.LEDP_32000,
-                    new String[] { "No Campaign found by ID: " + config.getPlayId() });
+            throw new LedpException(LedpCode.LEDP_32000, new String[] { "No Campaign found by ID: " + config.getPlayId() });
         }
-
         if (channel == null) {
-            throw new LedpException(LedpCode.LEDP_32000,
-                    new String[] { "No Channel found by ID: " + config.getChannelId() });
+            throw new LedpException(LedpCode.LEDP_32000, new String[] { "No Channel found by ID: " + config.getChannelId() });
         }
-
         PlayLaunch launch = null;
         if (StringUtils.isNotBlank(config.getLaunchId())) {
             launch = playProxy.getPlayLaunch(customerSpace.getTenantId(), config.getPlayId(), config.getLaunchId());
@@ -174,7 +159,6 @@ public class GenerateLaunchUniverse extends BaseSparkSQLStep<GenerateLaunchUnive
     }
 
     private HdfsDataUnit executeSparkJob(FrontEndQuery frontEndQuery) {
-
         RetryTemplate retry = RetryUtils.getRetryTemplate(2);
         return retry.execute(ctx -> {
             if (ctx.getRetryCount() > 0) {
@@ -183,32 +167,23 @@ public class GenerateLaunchUniverse extends BaseSparkSQLStep<GenerateLaunchUnive
             }
             try {
                 startSparkSQLSession(getHdfsPaths(attrRepo), false);
-                long userConfiguredLimit = frontEndQuery.getPageFilter() != null
-                        ? frontEndQuery.getPageFilter().getNumRows()
-                        : 0;
-
+                long userConfiguredLimit = frontEndQuery.getPageFilter() != null ? frontEndQuery.getPageFilter().getNumRows() : 0;
                 if (frontEndQuery.getMainEntity() == BusinessEntity.Account) {
                     long accountsCount = getEntityQueryCount(buildFrontEndQuery(frontEndQuery, BusinessEntity.Account));
-                    campaignLaunchUtils
-                            .checkCampaignLaunchAccountLimitation(limitToCheck(userConfiguredLimit, accountsCount));
+                    campaignLaunchUtils.checkCampaignLaunchAccountLimitation(limitToCheck(userConfiguredLimit, accountsCount));
                 } else {
                     if (contactsDataExists) {
-                        long contactsCount = getEntityQueryCount(
-                                buildFrontEndQuery(frontEndQuery, BusinessEntity.Contact));
-                        campaignLaunchUtils
-                                .checkCampaignLaunchContactLimitation(limitToCheck(userConfiguredLimit, contactsCount));
+                        long contactsCount = getEntityQueryCount(buildFrontEndQuery(frontEndQuery, BusinessEntity.Contact));
+                        campaignLaunchUtils.checkCampaignLaunchContactLimitation(limitToCheck(userConfiguredLimit, contactsCount));
                     }
                 }
-
                 HdfsDataUnit launchDataUniverseDataUnit = getEntityQueryData(frontEndQuery, true);
-
                 log.info("FullLaunchUniverseDataUnit: " + JsonUtils.serialize(launchDataUniverseDataUnit));
                 return launchDataUniverseDataUnit;
             } finally {
                 stopSparkSQLSession();
             }
         });
-
     }
 
     private boolean hasContactsPerAccountLimit(PlayLaunchChannel channel, BusinessEntity mainEntity) {
@@ -218,91 +193,43 @@ public class GenerateLaunchUniverse extends BaseSparkSQLStep<GenerateLaunchUnive
     private HdfsDataUnit executeSparkJobContactsPerAccount(HdfsDataUnit launchDataUniverseDataUnit, //
             Long maxContactsPerAccount, Long maxEntitiesToLaunch, CustomerSpace customerSpace,
             Long contactAccountRatioThreshold) {
-
         RetryTemplate retry = RetryUtils.getRetryTemplate(2);
         return retry.execute(ctx -> {
             if (ctx.getRetryCount() > 0) {
                 log.info("(Attempt=" + (ctx.getRetryCount() + 1) + ") extract entities via Spark SQL.");
                 log.warn("Previous failure:", ctx.getLastThrowable());
             }
-
             try {
                 startSparkSQLSession(getHdfsPaths(attrRepo), false);
-
-                List<String> sortConfig = getSortConfigFromZK(customerSpace);
+                List<String> sortConfig = WorkflowJobUtils.getSortConfigFromZK(customerSpace);
                 String sortAttr = sortConfig.get(0);
                 String sortDir = sortConfig.get(1);
                 Lookup lookup = new AttributeLookup(BusinessEntity.Contact, sortAttr);
                 HdfsDataUnit contactDataUnit = null;
-
                 if (lookup != null) {
                     Set<Lookup> lookupSet = new HashSet<>();
                     Lookup contactId = new AttributeLookup(BusinessEntity.Contact, InterfaceName.ContactId.name());
                     lookupSet.addAll(Arrays.asList(lookup, contactId));
-
                     FrontEndQuery query = new FrontEndQuery();
                     query.setLookups(new ArrayList<>(lookupSet));
                     query.setMainEntity(BusinessEntity.Contact);
-
                     contactDataUnit = getEntityQueryData(query);
                 }
-
                 List<DataUnit> inputUnits = new ArrayList<>();
                 inputUnits.add(launchDataUniverseDataUnit);
-
                 GenerateLaunchUniverseJobConfig config = new GenerateLaunchUniverseJobConfig(
                         getRandomWorkspace(), maxContactsPerAccount, maxEntitiesToLaunch, sortAttr, sortDir,
                         contactDataUnit, contactAccountRatioThreshold);
-
                 config.setInput(inputUnits);
                 log.info("Executing GenerateLaunchUniverseJob with config: " + JsonUtils.serialize(config));
-
                 SparkJobResult result = executeSparkJob(GenerateLaunchUniverseJob.class, config);
                 log.info("GenerateLaunchUniverseJob Results: " + JsonUtils.serialize(result));
-
                 HdfsDataUnit launchUniverseDataUnit = result.getTargets().get(0);
-
                 return launchUniverseDataUnit;
             } finally {
                 stopSparkSQLSession();
             }
         });
-    }
-
-    private List<String> getSortConfigFromZK(CustomerSpace customerSpace) {
-        List<String> sortConfig = new ArrayList<>();
-        Path attrPath = null;
-        Path dirPath = null;
-        String sortAttr = CDL_UPDATED_TIME;
-        String sortDir = DESC;
-
-        try {
-            Camille camille = CamilleEnvironment.getCamille();
-            String podId = CamilleEnvironment.getPodId();
-            attrPath = PathBuilder.buildCustomerSpaceServicePath(podId, customerSpace, CDL)
-                    .append(CONTACTS_PER_ACCOUNT_SORT).append(ATTRIBUTE);
-            if (camille.exists(attrPath)) {
-                sortAttr = camille.get(attrPath).getData();
-                log.info("Found tenant override sort attribute: ", sortAttr);
-            } else {
-                log.info("Tenant sort attribute not found. Using default");
-            }
-
-            dirPath = PathBuilder.buildCustomerSpaceServicePath(podId, customerSpace, CDL)
-                    .append(CONTACTS_PER_ACCOUNT_SORT).append(DIRECTION);
-            if (camille.exists(dirPath)) {
-                sortDir = camille.get(dirPath).getData();
-                log.info("Found tenant override sort direction: ", sortDir);
-            } else {
-                log.info("Tenant sort direction not found. Using default");
-            }
-        } catch (Exception e) {
-            log.warn("Tenant sort config found but unable to read: ", e);
-        }
-
-        sortConfig.add(sortAttr);
-        sortConfig.add(sortDir);
-        return sortConfig;
     }
 
     private long limitToCheck(long userConfiguredLimit, long queryCount) {
