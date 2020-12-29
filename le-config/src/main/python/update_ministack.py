27d6ca9597db75e1d751cd75add2139f64948396
Peng Liu <pliu@lattice-engines.com>
2020-12-29 12:57:04 +0800

    DP-14813 & DP-15374 change to use python3

--- le-config/src/main/python/update_ministack.py
+++ le-config/src/main/python/update_ministack.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import argparse
 import base64
-import httplib
+import http.client
 import json
 import os
 
@@ -50,7 +50,7 @@ def write_to_stack(server, environment, stack, key, value):
     _write_to_consul(server, key, value)
 
 def remove_stack(server, stack):
-    conn = httplib.HTTPConnection(server)
+    conn = http.client.HTTPConnection(server)
     conn.request("DELETE", "/v1/kv/%s?recurse" % stack)
     response = conn.getresponse()
     print(response.status, response.reason)
@@ -60,13 +60,13 @@ def read_from_stack(server, environment, stack, key):
     return _read_from_consul(server, key)
 
 def _write_to_consul(server, key, value):
-    conn = httplib.HTTPConnection(server)
+    conn = http.client.HTTPConnection(server)
     conn.request("PUT", "/v1/kv/%s" % key, value)
     response = conn.getresponse()
     print(response.status, response.reason)
 
 def _read_from_consul(server, key):
-    conn = httplib.HTTPConnection(server)
+    conn = http.client.HTTPConnection(server)
     conn.request("GET", "/v1/kv/%s" % key)
     response = conn.getresponse()
     print(response.status, response.reason)
@@ -74,7 +74,7 @@ def _read_from_consul(server, key):
     return base64.b64decode(json.loads(body)[0]["Value"])
 
 def _remove_from_consul(server, key):
-    conn = httplib.HTTPConnection(server)
+    conn = http.client.HTTPConnection(server)
     conn.request("DELETE", "/v1/kv/%s?recurse" % key)
     response = conn.getresponse()
     print(response.status, response.reason)
