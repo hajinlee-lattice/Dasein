2351efce9dc0ee46cb4d067468e51d3016cfc893
gengleiLattice <lgeng@lattice-engines.com>
2020-12-31 12:03:22 +0800

    Update DataOperationResourceDeploymentTestNG.java

--- le-serviceapps/cdl/src/test/java/com/latticeengines/apps/cdl/controller/DataOperationResourceDeploymentTestNG.java
+++ le-serviceapps/cdl/src/test/java/com/latticeengines/apps/cdl/controller/DataOperationResourceDeploymentTestNG.java
@@ -1,12 +1,16 @@
 package com.latticeengines.apps.cdl.controller;
 
+import java.util.Collections;
+
 import javax.inject.Inject;
 
+import org.springframework.retry.support.RetryTemplate;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.latticeengines.apps.cdl.testframework.CDLDeploymentTestNGBase;
+import com.latticeengines.common.exposed.util.RetryUtils;
 import com.latticeengines.db.exposed.util.MultiTenantContext;
 import com.latticeengines.domain.exposed.cdl.DataDeleteOperationConfiguration;
 import com.latticeengines.domain.exposed.metadata.DataOperation;
@@ -18,14 +22,18 @@ public class DataOperationResourceDeploymentTestNG extends CDLDeploymentTestNGBa
     @Inject
     private CDLProxy cdlProxy;
 
+    private RetryTemplate retry;
+
     @BeforeClass(groups = "deployment-app")
     public void setup() throws Exception {
         setupTestEnvironment();
         MultiTenantContext.setTenant(mainTestTenant);
+        retry = RetryUtils.getRetryTemplate(10, //
+                Collections.singleton(AssertionError.class), null);
     }
 
     @Test(groups = "deployment-app")
-    public void testCrudListSegment() {
+    public void testCrud() {
         String tenantId = mainTestTenant.getId();
         DataDeleteOperationConfiguration configuration = new DataDeleteOperationConfiguration();
         configuration.setEntity(BusinessEntity.Account);
@@ -34,11 +42,13 @@ public class DataOperationResourceDeploymentTestNG extends CDLDeploymentTestNGBa
         String dropPath = cdlProxy.createDataOperation(tenantId, DataOperation.OperationType.DELETE, configuration);
         Assert.assertNotNull(dropPath);
 
-        DataOperation dataOperation = cdlProxy.findDataOperationByDropPath(tenantId, dropPath);
-        Assert.assertNotNull(dataOperation);
-        Assert.assertNotNull(dataOperation);
-        Assert.assertEquals(dataOperation.getDropPath(), dropPath);
-        Assert.assertEquals(((DataDeleteOperationConfiguration)dataOperation.getConfiguration()).getDeleteType(),
-                DataDeleteOperationConfiguration.DeleteType.SOFT);
+        retry.execute(context -> {
+            DataOperation dataOperation = cdlProxy.findDataOperationByDropPath(tenantId, dropPath);
+            Assert.assertNotNull(dataOperation);
+            Assert.assertEquals(dataOperation.getDropPath(), dropPath);
+            Assert.assertEquals(((DataDeleteOperationConfiguration)dataOperation.getConfiguration()).getDeleteType(),
+                    DataDeleteOperationConfiguration.DeleteType.SOFT);
+            return true;
+        });
     }
 }
