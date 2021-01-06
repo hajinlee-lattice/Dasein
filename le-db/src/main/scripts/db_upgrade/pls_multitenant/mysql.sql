31c52595eb03b8eadf88e7e32d9cf22705c533ee
Riley Vuong-Gibson <VuongGibsonR@dnb.com>
2021-01-06 10:12:41 -0600

    reverting mysql change

--- le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
+++ le-db/src/main/scripts/db_upgrade/pls_multitenant/mysql.sql
@@ -13,6 +13,29 @@ DELIMITER //
 CREATE PROCEDURE `UpdateSchema`()
   BEGIN
       -- User input section (DDL/DML). This is just a template, developer can modify based on need.
+      ALTER TABLE `PLS_MultiTenant`.`EXPORT_FIELD_METADATA_DEFAULTS`
+               DROP `HISTORY_ENABLED`;
+
+      create table `DATA_OPERATION`
+          (
+              `PID`             bigint       not null auto_increment,
+              `DROP_PATH`       varchar(255),
+              `OPERATION_TYPE`  varchar(40),
+              `CONFIGURATION`   JSON,
+              `CREATED`     datetime,
+              `UPDATED`     datetime,
+              `FK_TENANT_ID`    bigint       not null,
+              primary key (`PID`)
+          ) engine = InnoDB;
+
+      ALTER TABLE `DATA_OPERATION`
+              ADD CONSTRAINT `FK_DATAOPERATION_FKTENANTID_TENANT` FOREIGN KEY (`FK_TENANT_ID`)
+                  REFERENCES `TENANT` (`TENANT_PID`) ON DELETE CASCADE;
+      CREATE INDEX IX_DROP_PATH ON `DATA_OPERATION` (`DROP_PATH`);
+
+      ALTER TABLE `PLS_MultiTenant`.`ACTIVITY_METRIC_GROUP`
+              ADD COLUMN `CSOverwrite` JSON NULL DEFAULT NULL;
+
       CREATE TABLE `DCP_ENRICHMENT_TEMPLATE` (
         `PID` bigint(20) NOT NULL AUTO_INCREMENT,
         `TEMPLATE_ID` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
