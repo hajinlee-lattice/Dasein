17e6ea8c8d873f2d3f02598b83946eae17e1365c
ZhouP-lattice <ZhouP@DNB.com>
2021-01-07 07:06:09 -0500

    DP-15511: unittest fix use mock other than extends functional base

--- le-serviceapps/cdl/src/test/java/com/latticeengines/apps/cdl/service/impl/CSVDataFeedMetadataServiceImplUnitTestNG.java
+++ le-serviceapps/cdl/src/test/java/com/latticeengines/apps/cdl/service/impl/CSVDataFeedMetadataServiceImplUnitTestNG.java
@@ -1,18 +1,23 @@
 package com.latticeengines.apps.cdl.service.impl;
 
-import javax.inject.Inject;
+import static org.mockito.Mockito.doReturn;
 
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import com.latticeengines.apps.cdl.testframework.CDLFunctionalTestNGBase;
+import com.latticeengines.baton.exposed.service.BatonService;
+import com.latticeengines.domain.exposed.admin.LatticeFeatureFlag;
 import com.latticeengines.domain.exposed.camille.CustomerSpace;
 import com.latticeengines.domain.exposed.cdl.CSVImportConfig;
 import com.latticeengines.domain.exposed.cdl.CSVImportFileInfo;
 import com.latticeengines.domain.exposed.eai.CSVToHdfsConfiguration;
 
-public class CSVDataFeedMetadataServiceImplUnitTestNG extends CDLFunctionalTestNGBase {
+public class CSVDataFeedMetadataServiceImplUnitTestNG {
 
     private CSVImportConfig csvImportConfig;
 
@@ -32,11 +37,16 @@ public class CSVDataFeedMetadataServiceImplUnitTestNG extends CDLFunctionalTestN
 
     private String JOB_IDENTIFIER = "jobIdentifier";
 
-    @Inject
+    @InjectMocks
+    @Spy
     private CSVDataFeedMetadataServiceImpl csvDataFeedMetadataServiceImpl;
+
+    @Mock
+    private BatonService batonService;
 
     @BeforeTest(groups = "unit")
     public void setup() {
+        MockitoAnnotations.initMocks(this);
         importConfig = new CSVToHdfsConfiguration();
         importConfig.setCustomerSpace(CUSTOMER_SPACE);
         importConfig.setTemplateName("templateName");
@@ -49,6 +59,7 @@ public class CSVDataFeedMetadataServiceImplUnitTestNG extends CDLFunctionalTestN
         csvImportFileInfo.setReportFileDisplayName(FILE_DISPLAY_NAME);
         csvImportFileInfo.setFileUploadInitiator(INITIATOR);
         csvImportConfig.setCsvToHdfsConfiguration(importConfig);
+        doReturn(false).when(batonService).isEnabled(CUSTOMER_SPACE, LatticeFeatureFlag.ENABLE_IMPORT_ERASE_BY_NULL);
     }
 
     @Test(groups = "unit")
