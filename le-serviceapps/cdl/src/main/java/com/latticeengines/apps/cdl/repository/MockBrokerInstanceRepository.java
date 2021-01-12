83f7d9e4c36357a70072784ef3b2efe33fcc47a0
lzhao <LZhao@lattice-engines.com>
2021-01-12 04:19:04 -0500

    DP-15393: mock broker

--- le-serviceapps/cdl/src/main/java/com/latticeengines/apps/cdl/repository/MockBrokerInstanceRepository.java
+++ le-serviceapps/cdl/src/main/java/com/latticeengines/apps/cdl/repository/MockBrokerInstanceRepository.java
@@ -11,7 +11,7 @@ import com.latticeengines.domain.exposed.security.Tenant;
 
 public interface MockBrokerInstanceRepository extends BaseJpaRepository<MockBrokerInstance, Long> {
 
-    @Query(value = "select * from MOCK_BROKER_INSTANCE limit :maxRow", nativeQuery = true)
+    @Query(value = "select * from MOCK_BROKER_INSTANCE order by PID desc limit :maxRow", nativeQuery = true)
     List<MockBrokerInstance> findAllWithLimit(@Param("maxRow") int maxRow);
 
     MockBrokerInstance findByTenantAndSourceId(@Param("tenant") Tenant tenant, @Param("sourceId") String sourceId);
