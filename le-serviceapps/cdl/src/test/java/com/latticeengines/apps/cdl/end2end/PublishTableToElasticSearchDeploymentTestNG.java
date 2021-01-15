fb5b8c209c75e44c1147a3dfc82d72d08a062cc3
PenglongLiu <penglong.liu@lattice-engines.com>
2021-01-15 21:40:02 +0800

    add test

--- le-serviceapps/cdl/src/test/java/com/latticeengines/apps/cdl/end2end/PublishTableToElasticSearchDeploymentTestNG.java
+++ le-serviceapps/cdl/src/test/java/com/latticeengines/apps/cdl/end2end/PublishTableToElasticSearchDeploymentTestNG.java
@@ -1,7 +1,10 @@
 package com.latticeengines.apps.cdl.end2end;
 
 import java.io.IOException;
+import java.time.Instant;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -11,31 +14,45 @@ import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.retry.support.RetryTemplate;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.google.common.collect.ImmutableMap;
 import com.latticeengines.camille.exposed.CamilleEnvironment;
 import com.latticeengines.camille.exposed.paths.PathBuilder;
 import com.latticeengines.common.exposed.util.CipherUtils;
 import com.latticeengines.common.exposed.util.HdfsUtils;
 import com.latticeengines.common.exposed.util.JsonUtils;
 import com.latticeengines.common.exposed.util.NamingUtils;
+import com.latticeengines.common.exposed.util.RetryUtils;
 import com.latticeengines.domain.exposed.camille.CustomerSpace;
+import com.latticeengines.domain.exposed.cdl.activity.AtlasStream;
+import com.latticeengines.domain.exposed.cdl.activity.EventFieldExtractor;
+import com.latticeengines.domain.exposed.cdl.activity.TimeLine;
 import com.latticeengines.domain.exposed.elasticsearch.ElasticSearchConfig;
 import com.latticeengines.domain.exposed.elasticsearch.PublishTableToESRequest;
+import com.latticeengines.domain.exposed.metadata.DataCollection;
+import com.latticeengines.domain.exposed.metadata.DataCollectionStatus;
 import com.latticeengines.domain.exposed.metadata.Extract;
 import com.latticeengines.domain.exposed.metadata.Table;
 import com.latticeengines.domain.exposed.metadata.TableRoleInCollection;
 import com.latticeengines.domain.exposed.metadata.datastore.DataUnit;
 import com.latticeengines.domain.exposed.metadata.datastore.ElasticSearchDataUnit;
+import com.latticeengines.domain.exposed.query.ActivityTimelineQuery;
+import com.latticeengines.domain.exposed.query.BusinessEntity;
+import com.latticeengines.domain.exposed.query.DataPage;
 import com.latticeengines.domain.exposed.serviceflows.core.steps.ElasticSearchExportConfig;
+import com.latticeengines.domain.exposed.util.TimeLineStoreUtils;
 import com.latticeengines.domain.exposed.workflow.JobStatus;
 import com.latticeengines.elasticsearch.Service.ElasticSearchService;
 import com.latticeengines.elasticsearch.util.ElasticSearchUtils;
 import com.latticeengines.proxy.exposed.cdl.DataCollectionProxy;
 import com.latticeengines.proxy.exposed.cdl.PublishTableProxy;
+import com.latticeengines.proxy.exposed.cdl.TimeLineProxy;
 import com.latticeengines.proxy.exposed.metadata.DataUnitProxy;
+import com.latticeengines.proxy.exposed.objectapi.ActivityProxy;
 
 public class PublishTableToElasticSearchDeploymentTestNG extends CDLEnd2EndDeploymentTestNGBase {
     private static final Logger log = LoggerFactory.getLogger(PublishTableToElasticSearchDeploymentTestNG.class);
@@ -53,6 +70,12 @@ public class PublishTableToElasticSearchDeploymentTestNG extends CDLEnd2EndDeplo
     @Inject
     private ElasticSearchService elasticSearchService;
 
+    @Inject
+    private TimeLineProxy timeLineProxy;
+
+    @Inject
+    private ActivityProxy activityProxy;
+
     @BeforeClass(groups = {"end2end"})
     @Override
     public void setup() throws Exception {
@@ -64,12 +87,13 @@ public class PublishTableToElasticSearchDeploymentTestNG extends CDLEnd2EndDeplo
     @Test(groups = "end2end")
     public void testTimelineProfile() throws Exception {
 
-
         PublishTableToESRequest request = generateRequest(TableRoleInCollection.TimelineProfile);
         String appId = publishTableProxy.publishTableToES(mainCustomerSpace, request);
-
         JobStatus status = waitForWorkflowStatus(appId, false);
         Assert.assertEquals(status, JobStatus.COMPLETED);
+        // fake data for query
+        prepareTimeline();
+
         ElasticSearchDataUnit unit = (ElasticSearchDataUnit) dataUnitProxy.getByNameAndType(mainCustomerSpace,
                 TableRoleInCollection.TimelineProfile.name(),
                 DataUnit.StorageType.ElasticSearch);
@@ -79,18 +103,36 @@ public class PublishTableToElasticSearchDeploymentTestNG extends CDLEnd2EndDeplo
         String signature = unit.getSignature();
         String indexName = ElasticSearchUtils.constructIndexName(CustomerSpace.shortenCustomerSpace(mainCustomerSpace),
                 entity, signature);
-        boolean exists = elasticSearchService.indexExists(indexName);
-        Assert.assertTrue(exists);
 
-        elasticSearchService.deleteIndex(indexName);
-        exists = elasticSearchService.indexExists(indexName);
-        Assert.assertFalse(exists);
+        RetryTemplate retry = RetryUtils.getRetryTemplate(2, Collections.singleton(AssertionError.class), null);
+        retry.execute(context -> {
+            Assert.assertTrue(elasticSearchService.indexExists(indexName));
+            return true;
+        });
+
+
 
         // get field mapping from index and assert field exists
         Map<String, Object> mappings = elasticSearchService.getSourceMapping(indexName);
         verifyField(mappings, "AccountId", "keyword");
         verifyField(mappings, "ContactId", "keyword");
         verifyField(mappings, "EventTimestamp", "date");
+
+        // query from elastic search
+        ActivityTimelineQuery query = new ActivityTimelineQuery();
+        query.setMainEntity(BusinessEntity.Contact);
+        query.setEntityId("Alert020Contact001");
+        query.setStartTimeStamp(Instant.ofEpochMilli(1607217830000L));
+        query.setStartTimeStamp(Instant.ofEpochMilli(1607217880000L));
+        DataPage dataPage = activityProxy.getData(mainCustomerSpace, null, query);
+        Assert.assertTrue(dataPage.getData().size() > 0);
+
+        elasticSearchService.deleteIndex(indexName);
+
+        retry.execute(context -> {
+            Assert.assertFalse(elasticSearchService.indexExists(indexName));
+            return true;
+        });
 
     }
 
@@ -148,5 +190,32 @@ public class PublishTableToElasticSearchDeploymentTestNG extends CDLEnd2EndDeplo
         return esTableName;
     }
 
+    private void prepareTimeline() {
+        // fake time line
+        String timelineName1 = "timelineName1";
+        TimeLine timeLine1 = new TimeLine();
+        timeLine1.setName(timelineName1);
+        String timelineId = String.format("%s_%s", CustomerSpace.shortenCustomerSpace(mainCustomerSpace),
+                timelineName1);
+        timeLine1.setTimelineId(timelineId);
+        timeLine1.setEntity(BusinessEntity.Contact.name());
+        timeLine1.setStreamTypes(Arrays.asList(AtlasStream.StreamType.WebVisit, AtlasStream.StreamType.MarketingActivity));
+        Map<String, Map<String, EventFieldExtractor>> mappingMap = new HashMap<>();
+
+        mappingMap.put(AtlasStream.StreamType.MarketingActivity.name(),
+                TimeLineStoreUtils.getTimelineStandardMappingByStreamType(AtlasStream.StreamType.MarketingActivity));
+        mappingMap.put(AtlasStream.StreamType.WebVisit.name(),
+                TimeLineStoreUtils.getTimelineStandardMappingByStreamType(AtlasStream.StreamType.WebVisit));
+
+        timeLine1.setEventMappings(mappingMap);
+        timeLineProxy.createTimeline(mainCustomerSpace, timeLine1);
+
+        // fake data collection status
+        DataCollection.Version version = dataCollectionProxy.getActiveVersion(mainCustomerSpace);
+        DataCollectionStatus status = dataCollectionProxy.getOrCreateDataCollectionStatus(mainCustomerSpace, version);
+        Map<String, String> timelineVersionMap = ImmutableMap.of(timelineId, "timelineVersion");
+        status.setTimelineVersionMap(timelineVersionMap);
+        dataCollectionProxy.saveOrUpdateDataCollectionStatus(mainCustomerSpace, status, version);
+    }
 
 }
