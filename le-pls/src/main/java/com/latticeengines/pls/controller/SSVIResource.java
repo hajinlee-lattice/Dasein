9f39cbaab603e2e7305fc6d90f28519b76c41d64
jxiao <jxiao@lattice-engines.com>
2021-01-26 08:30:52 -0500

    Report management API changes to support new BI Tool and finish setup api for list segment.

--- le-pls/src/main/java/com/latticeengines/pls/controller/SSVIResource.java
+++ le-pls/src/main/java/com/latticeengines/pls/controller/SSVIResource.java
@@ -4,13 +4,21 @@ import javax.inject.Inject;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.latticeengines.common.exposed.util.NamingUtils;
 import com.latticeengines.db.exposed.util.MultiTenantContext;
 import com.latticeengines.domain.exposed.camille.CustomerSpace;
 import com.latticeengines.domain.exposed.cdl.dashboard.DashboardResponse;
+import com.latticeengines.domain.exposed.metadata.ListSegment;
+import com.latticeengines.domain.exposed.metadata.ListSegmentSummary;
+import com.latticeengines.domain.exposed.metadata.MetadataSegment;
+import com.latticeengines.domain.exposed.metadata.template.CSVAdaptor;
 import com.latticeengines.pls.service.vidashboard.DashboardService;
 
 import io.swagger.annotations.Api;
@@ -31,5 +39,40 @@ public class SSVIResource {
     public DashboardResponse getDashboardList() {
         CustomerSpace customerSpace = MultiTenantContext.getCustomerSpace();
         return dashboardService.getDashboardList(customerSpace.toString());
+    }
+
+    @PostMapping("/createListSegment")
+    @ResponseBody
+    @ApiOperation("create list segment")
+    public MetadataSegment createSegment(@RequestParam(value = "segmentName", required = false) String segmentName) {
+        CustomerSpace customerSpace = MultiTenantContext.getCustomerSpace();
+        if (segmentName == null) {
+            segmentName = NamingUtils.timestamp(customerSpace.toString());
+        }
+        return dashboardService.createListSegment(customerSpace.toString(), segmentName);
+    }
+
+    @PostMapping("/updateMappings")
+    @ResponseBody
+    @ApiOperation("update list segment field Mappings")
+    public ListSegment createSegment(@RequestParam("segmentName") String segmentName, @RequestBody CSVAdaptor csvAdaptor) {
+        CustomerSpace customerSpace = MultiTenantContext.getCustomerSpace();
+        return dashboardService.updateSegmentFieldMapping(customerSpace.toString(), segmentName, csvAdaptor);
+    }
+
+    @GetMapping("/getMappings")
+    @ResponseBody
+    @ApiOperation("get list segment field Mappings")
+    public ListSegmentSummary getSegmentMappings(@RequestParam("segmentName") String segmentName) {
+        CustomerSpace customerSpace = MultiTenantContext.getCustomerSpace();
+        return dashboardService.getListSegmentMappings(customerSpace.toString(), segmentName);
+    }
+
+    @GetMapping("/getSegmentSummary")
+    @ResponseBody
+    @ApiOperation("get list segment summary")
+    public ListSegmentSummary getSegmentSummary(@RequestParam("segmentName") String segmentName) {
+        CustomerSpace customerSpace = MultiTenantContext.getCustomerSpace();
+        return dashboardService.getListSegmentSummary(customerSpace.toString(), segmentName);
     }
 }
