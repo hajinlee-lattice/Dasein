10b24db458050a40d8ea619c7d181eeddef37f6b
Stephen Lin <slin@lattice-engines.com>
2021-01-26 21:48:47 -0800

    Fix EnrichWebVisitTxfmr bean name

--- le-datacloud/etl/src/main/java/com/latticeengines/datacloud/etl/transformation/transformer/impl/cdl/EnrichWebVisitTxfmr.java
+++ le-datacloud/etl/src/main/java/com/latticeengines/datacloud/etl/transformation/transformer/impl/cdl/EnrichWebVisitTxfmr.java
@@ -1,6 +1,5 @@
 package com.latticeengines.datacloud.etl.transformation.transformer.impl.cdl;
 
-import static com.latticeengines.datacloud.etl.transformation.transformer.impl.cdl.SoftDeleteTxfmr.TRANSFORMER_NAME;
 import static com.latticeengines.domain.exposed.datacloud.DataCloudConstants.TRANSFORMER_ENRICH_WEBVISIT;
 
 import org.springframework.stereotype.Component;
@@ -10,7 +9,7 @@ import com.latticeengines.domain.exposed.spark.cdl.EnrichWebVisitJobConfig;
 import com.latticeengines.spark.exposed.job.AbstractSparkJob;
 import com.latticeengines.spark.exposed.job.cdl.EnrichWebVisitJob;
 
-@Component(TRANSFORMER_NAME)
+@Component(EnrichWebVisitTxfmr.TRANSFORMER_NAME)
 public class EnrichWebVisitTxfmr extends ConfigurableSparkJobTxfmr<EnrichWebVisitJobConfig> {
 
     public static final String TRANSFORMER_NAME = TRANSFORMER_ENRICH_WEBVISIT;
