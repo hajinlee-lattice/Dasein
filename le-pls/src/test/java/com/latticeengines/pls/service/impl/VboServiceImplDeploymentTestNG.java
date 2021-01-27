866a7e6950484677dc3bbb19492e79d0b7240a88
Jacob Warnagieris <warnagierisj@dnb.com>
2021-01-27 15:24:38 -0800

    added code to ensure subscriber state; added run order; addressed PR comments

--- le-pls/src/test/java/com/latticeengines/pls/service/impl/VboServiceImplDeploymentTestNG.java
+++ le-pls/src/test/java/com/latticeengines/pls/service/impl/VboServiceImplDeploymentTestNG.java
@@ -4,8 +4,14 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
 
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 import javax.inject.Inject;
 
+import org.apache.commons.lang3.time.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpMethod;
@@ -19,6 +25,7 @@ import com.latticeengines.db.exposed.util.MultiTenantContext;
 import com.latticeengines.domain.exposed.ResponseDocument;
 import com.latticeengines.domain.exposed.SimpleBooleanResponse;
 import com.latticeengines.domain.exposed.admin.LatticeProduct;
+import com.latticeengines.domain.exposed.dcp.vbo.VboUserSeatUsageEvent;
 import com.latticeengines.domain.exposed.pls.RegistrationResult;
 import com.latticeengines.domain.exposed.pls.UserUpdateData;
 import com.latticeengines.domain.exposed.pls.UserUpdateResponse;
@@ -33,7 +40,7 @@ import com.latticeengines.security.service.VboService;
 
 
 /**
- * $ dpltc deploy -a admin,matchapi,microservice,pls -m metadata,lp
+ * $ dpltc deploy -p dcp && runtc
  */
 
 public class VboServiceImplDeploymentTestNG extends PlsDeploymentTestNGBase {
@@ -61,6 +68,7 @@ public class VboServiceImplDeploymentTestNG extends PlsDeploymentTestNGBase {
         MultiTenantContext.setTenant(mainTestTenant);
         userService.deleteUserByEmail(INTERNAL_USER_EMAIL);
         userService.deleteUserByEmail(EXTERNAL_USER_EMAIL);
+        ensureSubscriberSeatState();
     }
 
     @AfterClass(groups = { "deployment" })
@@ -84,7 +92,7 @@ public class VboServiceImplDeploymentTestNG extends PlsDeploymentTestNGBase {
         }
     }
 
-    @Test(groups = "deployment")
+    @Test(groups = "deployment", dependsOnMethods = {"testRegisterInternalEmail"})
     public void testRegisterExternalEmail() throws InterruptedException {
         // subscriber with open seats: assert success, meter change
         userService.deleteUserByEmail(EXTERNAL_USER_EMAIL);
@@ -98,7 +106,7 @@ public class VboServiceImplDeploymentTestNG extends PlsDeploymentTestNGBase {
         delete(EXTERNAL_USER_EMAIL, false);
     }
 
-    @Test(groups = "deployment")
+    @Test(groups = "deployment", dependsOnMethods = {"testRegisterExternalEmail"})
     public void testUpdateInternalEmail() throws InterruptedException {
         userService.deleteUserByEmail(INTERNAL_USER_EMAIL);
         String[] subscriberNumbers = {SUBSCRIBER_NUMBER_OPEN, SUBSCRIBER_NUMBER_FULL};
@@ -106,19 +114,19 @@ public class VboServiceImplDeploymentTestNG extends PlsDeploymentTestNGBase {
             switchSubscriber(sn);
 
             // update new internal user: assert success, meter change
-            userService.createUser(INTERNAL_USER_EMAIL, getUserReg(INTERNAL_USER_EMAIL));
+            userService.createUser(INTERNAL_USER_EMAIL, createUserReg(INTERNAL_USER_EMAIL));
             update(INTERNAL_USER_EMAIL, true, AccessLevel.EXTERNAL_USER, true);
 
             delete(INTERNAL_USER_EMAIL, true);
         }
     }
 
-    @Test(groups = "deployment")
+    @Test(groups = "deployment", dependsOnMethods = {"testUpdateInternalEmail"})
     public void testUpdateExternalEmail() throws InterruptedException {
         // subscriber with open seats, new external user: assert success, meter change
         userService.deleteUserByEmail(EXTERNAL_USER_EMAIL);
         switchSubscriber(SUBSCRIBER_NUMBER_OPEN);
-        userService.createUser(EXTERNAL_USER_EMAIL, getUserReg(EXTERNAL_USER_EMAIL));
+        userService.createUser(EXTERNAL_USER_EMAIL, createUserReg(EXTERNAL_USER_EMAIL));
         update(EXTERNAL_USER_EMAIL, true, AccessLevel.EXTERNAL_USER, true);
 
         // existing external user: assert success, no meter change
@@ -127,7 +135,7 @@ public class VboServiceImplDeploymentTestNG extends PlsDeploymentTestNGBase {
 
         // subscriber at limit, new external user: assert 403, seat count unchanged
         switchSubscriber(SUBSCRIBER_NUMBER_FULL);
-        userService.createUser(EXTERNAL_USER_EMAIL, getUserReg(EXTERNAL_USER_EMAIL));
+        userService.createUser(EXTERNAL_USER_EMAIL, createUserReg(EXTERNAL_USER_EMAIL));
         update(EXTERNAL_USER_EMAIL, false, AccessLevel.EXTERNAL_USER, true);
         delete(EXTERNAL_USER_EMAIL, false);
         userService.deleteUserByEmail(EXTERNAL_USER_EMAIL);
@@ -140,7 +148,7 @@ public class VboServiceImplDeploymentTestNG extends PlsDeploymentTestNGBase {
 
     private void register(String email, boolean expectSuccess) throws InterruptedException {
         int initialCount = getSeatCount();
-        UserRegistration uReg = getUserReg(email);
+        UserRegistration uReg = createUserReg(email);
         try {
             String json = restTemplate.postForObject(getRestAPIHostPort() + "/pls/users/", uReg, String.class);
             ResponseDocument<RegistrationResult> response = ResponseDocument.
@@ -170,7 +178,7 @@ public class VboServiceImplDeploymentTestNG extends PlsDeploymentTestNGBase {
         assertNotNull(response);
         assertEquals(response.isSuccess(), expectSuccess);
 
-        Thread.sleep(3000);
+//        Thread.sleep(3000);
         int currCount = getSeatCount();
         if (expectSuccess && !EmailUtils.isInternalUser(email)) {
             assertEquals(currCount, initialCount - 1);
@@ -208,10 +216,53 @@ public class VboServiceImplDeploymentTestNG extends PlsDeploymentTestNGBase {
         JsonNode meter = vboService.getSubscriberMeter(mainTestTenant.getSubscriberNumber());
         assertNotNull(meter);
         assertTrue(meter.has("current_usage"));
+        assertNotNull(meter.get("current_usage"));
         return meter.get("current_usage").asInt();
     }
 
-    private UserRegistration getUserReg(String email) {
+    private void ensureSubscriberSeatState() throws InterruptedException {
+        // ensure open
+        int openSeats = getAvailableSeats(SUBSCRIBER_NUMBER_OPEN);
+        while (openSeats < 1) {
+            sendUsageEvent(VboUserSeatUsageEvent.FeatureURI.STDEC, SUBSCRIBER_NUMBER_OPEN);
+            openSeats++;
+        }
+        // ensure full
+        openSeats = getAvailableSeats(SUBSCRIBER_NUMBER_FULL);
+        while (openSeats > 0) {
+            sendUsageEvent(VboUserSeatUsageEvent.FeatureURI.STCT, SUBSCRIBER_NUMBER_FULL);
+            openSeats--;
+        }
+        Thread.sleep(3000);
+        assertTrue(getAvailableSeats(SUBSCRIBER_NUMBER_OPEN) >= 1);
+        assertTrue(getAvailableSeats(SUBSCRIBER_NUMBER_FULL) <= 0);
+    }
+
+    private int getAvailableSeats(String subscriberNumber) {
+        JsonNode meter = vboService.getSubscriberMeter(subscriberNumber);
+        assertNotNull(meter);
+        assertTrue(meter.has("current_usage"));
+        assertTrue(meter.has("limit"));
+        assertNotNull(meter.get("current_usage"));
+        assertNotNull(meter.get("limit"));
+        return meter.get("limit").asInt() - meter.get("current_usage").asInt();
+    }
+
+    private void sendUsageEvent(VboUserSeatUsageEvent.FeatureURI feature, String subscriberNumber) {
+        VboUserSeatUsageEvent usageEvent = new VboUserSeatUsageEvent();
+        Date current_date = new Date();
+        usageEvent.setSubscriberID(subscriberNumber);
+        usageEvent.setFeatureURI(feature);
+        usageEvent.setPOAEID("99");
+        usageEvent.setLUID(null);
+        usageEvent.setTimeStamp(ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_INSTANT));
+        usageEvent.setSubjectCountry("US");
+        usageEvent.setSubscriberCountry("US");
+        usageEvent.setContractTermStartDate(current_date);
+        usageEvent.setContractTermEndDate(DateUtils.addHours(current_date, 1));
+    }
+
+    private UserRegistration createUserReg(String email) {
         UserRegistration userReg = new UserRegistration();
 
         User user = new User();
