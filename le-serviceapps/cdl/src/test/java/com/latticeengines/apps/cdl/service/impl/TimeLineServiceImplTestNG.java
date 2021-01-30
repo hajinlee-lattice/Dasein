256281e51e61985ca86f354a93101db04580234d
Raymond <FengR@dnb.com>
2021-01-29 17:03:15 -0800

    try fixing timeline functional test

--- le-serviceapps/cdl/src/test/java/com/latticeengines/apps/cdl/service/impl/TimeLineServiceImplTestNG.java
+++ le-serviceapps/cdl/src/test/java/com/latticeengines/apps/cdl/service/impl/TimeLineServiceImplTestNG.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.retry.support.RetryTemplate;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import com.latticeengines.apps.cdl.service.TimeLineService;
@@ -26,7 +27,10 @@ import com.latticeengines.domain.exposed.cdl.activity.TimeLine;
 import com.latticeengines.domain.exposed.metadata.InterfaceName;
 import com.latticeengines.domain.exposed.query.BusinessEntity;
 import com.latticeengines.domain.exposed.util.TimeLineStoreUtils;
+import com.latticeengines.testframework.service.impl.SimpleRetryAnalyzer;
+import com.latticeengines.testframework.service.impl.SimpleRetryListener;
 
+@Listeners({SimpleRetryListener.class})
 public class TimeLineServiceImplTestNG extends CDLFunctionalTestNGBase {
 
     private static final Logger log = LoggerFactory.getLogger(TimeLineServiceImplTestNG.class);
@@ -34,30 +38,76 @@ public class TimeLineServiceImplTestNG extends CDLFunctionalTestNGBase {
     private static final String EVENT_TIME = "EventTime";
     private static final String EVENT_TYPE = "EventType";
     private static final String MOTION = "Motion";
+    private static final String TIMELINE_NAME = "timelineName1";
 
     @Inject
     private TimeLineService timeLineService;
 
-    private TimeLine timeLine1;
+    private TimeLine timeline;
+    private TimeLine created;
     private RetryTemplate retry;
 
     @BeforeClass(groups = "functional")
     public void setup() {
         setupTestEnvironment();
+        timeline = setupTestTimeline();
         retry = RetryUtils.getRetryTemplate(10, //
                 Collections.singleton(AssertionError.class), null);
     }
 
-    @Test(groups = "functional")
-    public void testCRUD() {
-        String timelineName1 = "timelineName1";
+    @Test(groups = "functional", retryAnalyzer = SimpleRetryAnalyzer.class)
+    public void testCreate() {
+        created = timeLineService.createOrUpdateTimeLine(mainCustomerSpace, timeline);
+        Assert.assertNotNull(created.getPid());
+        Assert.assertNotNull(created.getTimelineId());
+    }
 
-        timeLine1 = new TimeLine();
-        timeLine1.setName(timelineName1);
-        timeLine1.setTimelineId(String.format("%s_%s", CustomerSpace.shortenCustomerSpace(mainCustomerSpace), timelineName1));
-        timeLine1.setTenant(mainTestTenant);
-        timeLine1.setEntity(BusinessEntity.Account.name());
-        timeLine1.setStreamTypes(Arrays.asList(AtlasStream.StreamType.WebVisit, AtlasStream.StreamType.MarketingActivity));
+    @Test(groups = "functional", dependsOnMethods = "testCreate", retryAnalyzer = SimpleRetryAnalyzer.class)
+    public void testFetch() {
+        TimeLine fetchedByPid = timeLineService.findByPid(mainCustomerSpace, created.getPid());
+        Assert.assertEquals(fetchedByPid.getName(), TIMELINE_NAME);
+        Assert.assertTrue(fetchedByPid.getEventMappings().containsKey(AtlasStream.StreamType.MarketingActivity.name()));
+        Assert.assertTrue(fetchedByPid.getEventMappings().get(AtlasStream.StreamType.MarketingActivity.name()).containsKey(MOTION));
+        Assert.assertEquals(fetchedByPid.getEventMappings().get(AtlasStream.StreamType.MarketingActivity.name()).get(MOTION).getMappingValue(), InterfaceName.ActivityType.name());
+        TimeLine fetchedByTimelineId = timeLineService.findByTimelineId(mainCustomerSpace, created.getTimelineId());
+        Assert.assertEquals(fetchedByTimelineId.getTimelineId(), created.getTimelineId());
+    }
+
+    @Test(groups = "functional", dependsOnMethods = "testFetch", retryAnalyzer = SimpleRetryAnalyzer.class)
+    public void testDelete() {
+        timeLineService.delete(mainCustomerSpace, created);
+        TimeLine fetched = timeLineService.findByTimelineId(mainCustomerSpace, created.getTimelineId());
+        Assert.assertNull(fetched);
+    }
+
+    @Test(groups = "functional", dependsOnMethods = "testDelete")
+    public void testDefault() {
+        timeLineService.createDefaultTimeLine(mainCustomerSpace);
+        AtomicReference<List<TimeLine>> createdAtom = new AtomicReference<>();
+        retry.execute(context -> {
+            createdAtom.set(timeLineService.findByTenant(mainCustomerSpace));
+            Assert.assertEquals(createdAtom.get().size(), 2);
+            return true;
+        });
+        List<TimeLine> timeLines = createdAtom.get();
+        Assert.assertEquals(timeLines.size(), 2);
+        AtomicReference<TimeLine> createdAtom1 = new AtomicReference<>();
+        retry.execute(context -> {
+            createdAtom1.set(timeLineService.findByTenantAndEntity(mainCustomerSpace, BusinessEntity.Account));
+            Assert.assertNotNull(createdAtom1.get());
+            return true;
+        });
+        TimeLine created = createdAtom1.get();
+        Assert.assertEquals(created.getName(), TimeLineStoreUtils.ACCOUNT360_TIMELINE_NAME);
+    }
+
+    private TimeLine setupTestTimeline() {
+        timeline = new TimeLine();
+        timeline.setName(TIMELINE_NAME);
+        timeline.setTimelineId(String.format("%s_%s", CustomerSpace.shortenCustomerSpace(mainCustomerSpace), TIMELINE_NAME));
+        timeline.setTenant(mainTestTenant);
+        timeline.setEntity(BusinessEntity.Account.name());
+        timeline.setStreamTypes(Arrays.asList(AtlasStream.StreamType.WebVisit, AtlasStream.StreamType.MarketingActivity));
         Map<String, Map<String, EventFieldExtractor>> mappingMap = new HashMap<>();
 
         Map<String, EventFieldExtractor> eventTypeExtractorMapForMarketing = new HashMap<>();
@@ -91,54 +141,7 @@ public class TimeLineServiceImplTestNG extends CDLFunctionalTestNGBase {
         mappingMap.put(AtlasStream.StreamType.MarketingActivity.name(), eventTypeExtractorMapForMarketing);
         mappingMap.put(AtlasStream.StreamType.WebVisit.name(), eventTypeExtractorMapForWebVisit);
 
-        timeLine1.setEventMappings(mappingMap);
-        TimeLine created = timeLineService.createOrUpdateTimeLine(mainCustomerSpace, timeLine1);
-        Assert.assertNotNull(created.getPid());
-        Assert.assertNotNull(created.getTimelineId());
-        AtomicReference<TimeLine> createdAtom = new AtomicReference<>();
-        retry.execute(context -> {
-            createdAtom.set(timeLineService.findByPid(mainCustomerSpace, timeLine1.getPid()));
-            Assert.assertNotNull(createdAtom.get());
-            return true;
-        });
-        created = createdAtom.get();
-        Assert.assertEquals(created.getName(), timelineName1);
-        Assert.assertTrue(created.getEventMappings().containsKey(AtlasStream.StreamType.MarketingActivity.name()));
-        Assert.assertTrue(created.getEventMappings().get(AtlasStream.StreamType.MarketingActivity.name()).containsKey(MOTION));
-        Assert.assertEquals(created.getEventMappings().get(AtlasStream.StreamType.MarketingActivity.name()).get(MOTION).getMappingValue(), InterfaceName.ActivityType.name());
-        retry.execute(context -> {
-            createdAtom.set(timeLineService.findByTimelineId(mainCustomerSpace, timeLine1.getTimelineId()));
-            Assert.assertNotNull(createdAtom.get());
-            return true;
-        });
-        created = createdAtom.get();
-        Assert.assertEquals(created.getTimelineId(), timeLine1.getTimelineId());
-        timeLineService.delete(mainCustomerSpace, created);
-        retry.execute(context -> {
-            createdAtom.set(timeLineService.findByTimelineId(mainCustomerSpace, timeLine1.getTimelineId()));
-            Assert.assertNull(createdAtom.get());
-            return true;
-        });
-    }
-
-    @Test(groups = "functional", dependsOnMethods = "testCRUD")
-    public void testDefault() {
-        timeLineService.createDefaultTimeLine(mainCustomerSpace);
-        AtomicReference<List<TimeLine>> createdAtom = new AtomicReference<>();
-        retry.execute(context -> {
-            createdAtom.set(timeLineService.findByTenant(mainCustomerSpace));
-            Assert.assertEquals(createdAtom.get().size(), 2);
-            return true;
-        });
-        List<TimeLine> timeLines = createdAtom.get();
-        Assert.assertEquals(timeLines.size(), 2);
-        AtomicReference<TimeLine> createdAtom1 = new AtomicReference<>();
-        retry.execute(context -> {
-            createdAtom1.set(timeLineService.findByTenantAndEntity(mainCustomerSpace, BusinessEntity.Account));
-            Assert.assertNotNull(createdAtom1.get());
-            return true;
-        });
-        TimeLine created = createdAtom1.get();
-        Assert.assertEquals(created.getName(), TimeLineStoreUtils.ACCOUNT360_TIMELINE_NAME);
+        timeline.setEventMappings(mappingMap);
+        return timeline;
     }
 }
