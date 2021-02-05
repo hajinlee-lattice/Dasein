eb21330b94c86ca6d16fcbc2ce7587668184abe6
Hajin Lee <leeha@dnb.com>
2021-02-05 15:20:57 -0800

    PLS-19763 S3 contact launch limit regression fix
    - Do not inner join if maxContactsPerAccount is null
    - Add a test where maxContactsPerAccont is null and there is a smaller contactsData: ensure end-result is equal to initial input

--- le-spark/src/test/java/com/latticeengines/spark/exposed/job/cdl/GenerateLaunchUniverseJobTestNG.java
+++ le-spark/src/test/java/com/latticeengines/spark/exposed/job/cdl/GenerateLaunchUniverseJobTestNG.java
@@ -1,5 +1,9 @@
 package com.latticeengines.spark.exposed.job.cdl;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -7,21 +11,30 @@ import java.util.Map;
 
 import javax.inject.Inject;
 
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+import org.apache.avro.file.DataFileWriter;
 import org.apache.avro.file.FileReader;
+import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.generic.GenericRecordBuilder;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StreamUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.latticeengines.common.exposed.util.AvroUtils;
 import com.latticeengines.common.exposed.util.HdfsUtils;
 import com.latticeengines.common.exposed.util.HdfsUtils.HdfsFileFilter;
 import com.latticeengines.common.exposed.util.JsonUtils;
 import com.latticeengines.domain.exposed.metadata.InterfaceName;
+import com.latticeengines.domain.exposed.metadata.datastore.DataUnit;
+import com.latticeengines.domain.exposed.metadata.datastore.HdfsDataUnit;
 import com.latticeengines.domain.exposed.spark.SparkJobResult;
 import com.latticeengines.domain.exposed.spark.cdl.GenerateLaunchUniverseJobConfig;
 import com.latticeengines.spark.testframework.SparkJobFunctionalTestNGBase;
@@ -42,6 +55,7 @@ public class GenerateLaunchUniverseJobTestNG extends SparkJobFunctionalTestNGBas
 
     private static final String CDL_UPDATED_TIME = InterfaceName.CDLUpdatedTime.name();
     private static final String DESC = "DESC";
+    private static final String AVRO_EXTENSION = ".avro";
 
     private static final HdfsFileFilter avroFileFilter = new HdfsFileFilter() {
         @Override
@@ -186,6 +200,32 @@ public class GenerateLaunchUniverseJobTestNG extends SparkJobFunctionalTestNGBas
         Assert.assertNotNull(error);
     }
 
+    @Test(groups = "functional")
+    public void testGenerateLaunchUniverseJobContactsData() throws Exception {
+        Schema contactSchema = SchemaBuilder.record("Contact").fields() //
+                .name("ContactId").type(SchemaBuilder.unionOf().nullType().and().stringType().endUnion()).noDefault() //
+                .name("AccountId").type().stringType().noDefault() //
+                .endRecord();
+
+        String fileName = "limitedContacts";
+        createAvroFromJson(fileName,
+                String.format("com/latticeengines/common/exposed/util/SparkCountRecordsTest/%sData.json", fileName),
+                contactSchema, Contact.class, yarnConfiguration);
+        DataUnit contactsData = HdfsDataUnit.fromPath("/tmp/testGenerateLaunchUniverse" + fileName + AVRO_EXTENSION);
+        logHDFSDataUnit(fileName, HdfsDataUnit.fromPath("/tmp/testGenerateLaunchUniverse" + fileName));
+
+        GenerateLaunchUniverseJobConfig config = new GenerateLaunchUniverseJobConfig();
+        config.setWorkspace("testGenerateLaunchUniverseJobContactsData");
+        config.setContactAccountRatioThreshold(10L);
+        config.setContactsData(contactsData);
+
+        log.info("Config: " + JsonUtils.serialize(config));
+        SparkJobResult result = runSparkJob(GenerateLaunchUniverseJob.class, config);
+        log.info("TestGenerateLaunchUniverseJobContactsData Results: " + JsonUtils.serialize(result));
+
+        Assert.assertEquals(result.getTargets().get(0).getCount().intValue(), 20);
+    }
+
     private boolean verifyDefaultSort(SparkJobResult result) throws Exception {
         String hdfsDir = result.getTargets().get(0).getPath();
         String fieldName = "ContactId";
@@ -274,6 +314,75 @@ public class GenerateLaunchUniverseJobTestNG extends SparkJobFunctionalTestNGBas
             return;
         }
         Assert.fail("Failed in testGenerateLaunchUniverseJobContactAccountRatioExceed\n");
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T extends AvroExportable> void createAvroFromJson(String fileName, String jsonPath, Schema schema,
+            Class<T> elementClazz, Configuration yarnConfiguration) throws Exception {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream tableRegistryStream = classLoader.getResourceAsStream(jsonPath);
+        String attributesDoc = StreamUtils.copyToString(tableRegistryStream, Charset.defaultCharset());
+        List<Object> raw = JsonUtils.deserialize(attributesDoc, List.class);
+        List<T> accounts = JsonUtils.convertList(raw, elementClazz);
+        String avroPath = "/tmp/testGenerateLaunchUniverse" + fileName + AVRO_EXTENSION;
+
+        File localFile = new File(avroPath);
+        try (DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<>(new GenericDatumWriter<>(schema))) {
+            dataFileWriter.create(schema, localFile);
+            accounts.forEach(account -> {
+                try {
+                    dataFileWriter.append(account.getAsRecord(schema));
+                } catch (IOException ioe) {
+                    log.warn("failed to write a avro datdum", ioe);
+                }
+            });
+            dataFileWriter.flush();
+        }
+        HdfsUtils.copyLocalToHdfs(yarnConfiguration, localFile.getAbsolutePath(), avroPath);
+        Assert.assertTrue(HdfsUtils.fileExists(yarnConfiguration, avroPath));
+    }
+
+    interface AvroExportable {
+        GenericRecord getAsRecord(Schema schema);
+    }
+
+    static class Contact implements AvroExportable {
+        public String getAccountId() {
+            return accountId;
+        }
+
+        public void setAccountId(String accountId) {
+            this.accountId = accountId;
+        }
+
+        @JsonProperty(value = "AccountId")
+        private String accountId;
+
+        public String getContactId() {
+            return contactId;
+        }
+
+        public void setContactId(String contactId) {
+            this.contactId = contactId;
+        }
+
+        @JsonProperty(value = "ContactId")
+        private String contactId;
+
+        @Override
+        public GenericRecord getAsRecord(Schema schema) {
+            GenericRecordBuilder builder = new GenericRecordBuilder(schema);
+            builder.set("AccountId", this.getAccountId());
+            builder.set("ContactId", this.getContactId());
+            return builder.build();
+        }
+    }
+
+    private void logHDFSDataUnit(String tag, HdfsDataUnit dataUnit) {
+        if (dataUnit == null) {
+            return;
+        }
+        log.info(tag + ", " + dataUnit.toString());
     }
 
 }
