e8fc032ed4d6cb0e22602ec432ee78c005669a85
Hajin Lee <leeha@dnb.com>
2021-03-15 10:20:42 -0700

    DP-15474 Merge graphs and find CCs

---
+++ le-domain/src/main/java/com/latticeengines/domain/exposed/spark/graph/MergeGraphsJobConfig.java
@@ -0,0 +1,20 @@
+package com.latticeengines.domain.exposed.spark.graph;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.latticeengines.domain.exposed.spark.SparkJobConfig;
+
+public class MergeGraphsJobConfig extends SparkJobConfig {
+
+    public static final String NAME = "mergeGraphsJob";
+
+    @Override
+    @JsonProperty("Name")
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public int getNumTargets() {
+        return 2;
+    }
+}
